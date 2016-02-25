0ed10176b25159cfe8d6bc8e0803d19a731070a9
virvar <revalka@gmail.com>
2016-02-25 19:21:08 +0300

    Navigation
    Former-commit-id: 50beafc5a2cd3bf222a2330b49819ab8a9c10d54

--- src/messenger/android/contacts_list.cljs
+++ src/messenger/android/contacts_list.cljs
@@ -7,18 +7,21 @@
    [natal-shell.alert :refer [alert]])
   (:require [om.next :as om :refer-macros [defui]]
             [re-natal.support :as sup]
-            [messenger.state :as state]))
+            [messenger.state :as state]
+            [messenger.android.resources :as res]
+            [messenger.android.chat :refer [chat]]))
 
 (def fake-contacts? true)
 
-(set! js/React (js/require "react-native"))
 (def react-native-contacts (js/require "react-native-contacts"))
 
-(def logo-icon (js/require "./images/logo.png"))
-(def user-no-photo (js/require "./images/no-photo.png"))
-(def online-icon (js/require "./images/online.png"))
-(def seen-icon (js/require "./images/seen.png"))
-(def delivered-icon (js/require "./images/delivered.png"))
+(defn nav-push [nav route]
+  (binding [state/*nav-render* false]
+    (.push nav (clj->js route))))
+
+(defn show-chat [nav]
+  (nav-push nav {:component chat
+                 :name "chat"}))
 
 (defui Contact
   static om/Ident
@@ -30,107 +33,111 @@
   Object
   (render [this]
           (let [{:keys [name photo delivery-status datetime new-messages-count online]}
-                (om/props this)]
-            (view {:style {:flexDirection "row"
-                           :marginTop 5
-                           :marginBottom 5
-                           :paddingLeft 15
-                           :paddingRight 15
-                           :height 75
-                           :transform [{:translateX 0}
-                                       {:translateY 0}]}}
-                  (view {:width 54
-                         :height 54}
-                        ;;; photo
-                        (view {:width 54
-                               :height 54
-                               :borderRadius 50
-                               :backgroundColor "#FFFFFF"
-                               :elevation 6}
-                              (image {:source (if (< 0 (count photo))
-                                                {:uri photo}
-                                                user-no-photo)
-                                      :style {:borderWidth 2
-                                              :borderColor "#FFFFFF"
-                                              :borderRadius 50
-                                              :width 54
-                                              :height 54
-                                              :position "absolute"
-                                              ;; :top 2
-                                              ;; :right 2
-                                              }}))
-                        ;;; online
-                        (when online
-                          (view {:position "absolute"
-                                 :top 41
-                                 :left 36
-                                 :width 12
-                                 :height 12
-                                 :borderRadius 50
-                                 :backgroundColor "#FFFFFF"
-                                 :elevation 6}
-                                (image {:source online-icon
-                                        :style {:width 12
-                                                :height 12}}))))
-                  (view {:style {:flexDirection "column"
-                                 :marginLeft 7
-                                 :marginRight 10
-                                 :flex 1
-                                 :position "relative"}}
-                        ;;; name
-                        (text {:style {:fontSize 15
-                                       :fontFamily "Avenir-Roman"}} name)
-                        ;;; last message
-                        (text {:style {:color "#AAB2B2"
-                                       :fontFamily "Avenir-Roman"
-                                       :fontSize 14
-                                       :marginTop 2
-                                       :paddingRight 10}}
-                              (str "Hi, I'm " name)))
-                  (view {:style {:flexDirection "column"}}
-                        ;;; delivery status
-                        (view {:style {:flexDirection "row"
-                                       :position "absolute"
-                                       :top 0
-                                       :right 0}}
-                              (when delivery-status
-                                (image {:source (if (= (keyword delivery-status) :seen)
-                                                  seen-icon
-                                                  delivered-icon)
-                                        :style {:marginTop 5}}))
-                              ;;; datetime
-                              (text {:style {:fontFamily "Avenir-Roman"
-                                             :fontSize 11
-                                             :color "#AAB2B2"
-                                             :letterSpacing 1
-                                             :lineHeight 15
-                                             :marginLeft 5}}
-                                    datetime))
-                        ;;; new messages count
-                        (when (< 0 new-messages-count)
-                          (view {:style {:position "absolute"
-                                         :right 0
-                                         :bottom 24
-                                         :width 18
-                                         :height 18
-                                         :backgroundColor "#6BC6C8"
-                                         :borderColor "#FFFFFF"
-                                         :borderRadius 50
-                                         :alignSelf "flex-end"}}
-                                (text {:style {:width 18
-                                               :height 17
-                                               :fontFamily "Avenir-Roman"
-                                               :fontSize 10
-                                               :color "#FFFFFF"
-                                               :lineHeight 19
-                                               :textAlign "center"
-                                               :top 1}}
-                                      new-messages-count))))))))
+                (dissoc (om/props this) :om.next/computed)
+                {:keys [nav]} (om/get-computed this)]
+            (touchable-highlight
+             {:onPress (fn [] (show-chat nav))}
+             (view {:style {:flexDirection "row"
+                            :marginTop 5
+                            :marginBottom 5
+                            :paddingLeft 15
+                            :paddingRight 15
+                            :height 75
+                            :transform [{:translateX 0}
+                                        {:translateY 0}]}}
+                   (view {:width 54
+                          :height 54}
+                         ;;; photo
+                         (view {:width 54
+                                :height 54
+                                :borderRadius 50
+                                :backgroundColor "#FFFFFF"
+                                :elevation 6}
+                               (image {:source (if (< 0 (count photo))
+                                                 {:uri photo}
+                                                 res/user-no-photo)
+                                       :style {:borderWidth 2
+                                               :borderColor "#FFFFFF"
+                                               :borderRadius 50
+                                               :width 54
+                                               :height 54
+                                               :position "absolute"
+                                               ;; :top 2
+                                               ;; :right 2
+                                               }}))
+                         ;;; online
+                         (when online
+                           (view {:position "absolute"
+                                  :top 41
+                                  :left 36
+                                  :width 12
+                                  :height 12
+                                  :borderRadius 50
+                                  :backgroundColor "#FFFFFF"
+                                  :elevation 6}
+                                 (image {:source res/online-icon
+                                         :style {:width 12
+                                                 :height 12}}))))
+                   (view {:style {:flexDirection "column"
+                                  :marginLeft 7
+                                  :marginRight 10
+                                  :flex 1
+                                  :position "relative"}}
+                         ;;; name
+                         (text {:style {:fontSize 15
+                                        :fontFamily "Avenir-Roman"}} name)
+                         ;;; last message
+                         (text {:style {:color "#AAB2B2"
+                                        :fontFamily "Avenir-Roman"
+                                        :fontSize 14
+                                        :marginTop 2
+                                        :paddingRight 10}}
+                               (str "Hi, I'm " name)))
+                   (view {:style {:flexDirection "column"}}
+                         ;;; delivery status
+                         (view {:style {:flexDirection "row"
+                                        :position "absolute"
+                                        :top 0
+                                        :right 0}}
+                               (when delivery-status
+                                 (image {:source (if (= (keyword delivery-status) :seen)
+                                                   res/seen-icon
+                                                   res/delivered-icon)
+                                         :style {:marginTop 5}}))
+                               ;;; datetime
+                               (text {:style {:fontFamily "Avenir-Roman"
+                                              :fontSize 11
+                                              :color "#AAB2B2"
+                                              :letterSpacing 1
+                                              :lineHeight 15
+                                              :marginLeft 5}}
+                                     datetime))
+                         ;;; new messages count
+                         (when (< 0 new-messages-count)
+                           (view {:style {:position "absolute"
+                                          :right 0
+                                          :bottom 24
+                                          :width 18
+                                          :height 18
+                                          :backgroundColor "#6BC6C8"
+                                          :borderColor "#FFFFFF"
+                                          :borderRadius 50
+                                          :alignSelf "flex-end"}}
+                                 (text {:style {:width 18
+                                                :height 17
+                                                :fontFamily "Avenir-Roman"
+                                                :fontSize 10
+                                                :color "#FFFFFF"
+                                                :lineHeight 19
+                                                :textAlign "center"
+                                                :top 1}}
+                                       new-messages-count)))))))))
 
 (def contact (om/factory Contact {:keyfn :name}))
 
-(defn render-row [row section-id row-id]
-  (contact (js->clj row :keywordize-keys true)))
+(defn render-row [nav row section-id row-id]
+  (contact (om/computed (js->clj row :keywordize-keys true)
+                        {:nav nav})))
 
 (defn generate-contact [n]
   {:name (str "Contact " n)
@@ -167,14 +174,15 @@
   (componentDidMount [this]
                      (load-contacts))
   (render [this]
-          (let [{:keys [contacts-ds]} (om/props this)]
+          (let [{:keys [contacts-ds]} (om/props this)
+                {:keys [nav]} (om/get-computed this)]
             (view {:style {:flex 1}}
-                  (toolbar-android {:logo logo-icon
+                  (toolbar-android {:logo res/logo-icon
                                     :title "Chats"
                                     :style {:backgroundColor "#e9eaed"
                                             :height 56}})
                   (list-view {:dataSource contacts-ds
-                              :renderRow render-row
+                              :renderRow (partial render-row nav)
                               :style {}})))))
 
 (def contacts-list (om/factory ContactsList))
--- src/messenger/android/core.cljs
+++ src/messenger/android/core.cljs
@@ -1,30 +1,52 @@
 (ns messenger.android.core
   (:require-macros
-   [natal-shell.components :refer [view text image touchable-highlight list-view
+   [natal-shell.components :refer [navigator view text image touchable-highlight list-view
                                    toolbar-android]]
    [natal-shell.data-source :refer [data-source clone-with-rows]]
+   [natal-shell.back-android :refer [add-event-listener]]
    [natal-shell.core :refer [with-error-view]]
    [natal-shell.alert :refer [alert]])
   (:require [om.next :as om :refer-macros [defui]]
             [re-natal.support :as sup]
             [messenger.state :as state]
-            [messenger.android.contacts-list :as contacts-list]))
+            [messenger.android.contacts-list :refer [contacts-list]]
+            [messenger.android.chat :refer [chat]]))
 
-(set! js/React (js/require "react-native"))
 (def app-registry (.-AppRegistry js/React))
+
+(def initialized-atom (atom false))
+(defn init-back-button-handler! [nav]
+  (if (not @initialized-atom)
+    (swap! initialized-atom
+           (fn [initialized]
+             (when (not initialized)
+               (add-event-listener "hardwareBackPress"
+                                   (fn []
+                                     (binding [state/*nav-render* false]
+                                       (when (< 1 (.-length (.getCurrentRoutes nav)))
+                                         (.pop nav)
+                                         true)))))
+             true))))
 
 (defui AppRoot
   static om/IQuery
   (query [this]
-         '[:contacts-ds])
+         '[:page :contacts-ds])
   Object
   (render [this]
-          (let [{:keys [contacts-ds]} (om/props this)]
-            (contacts-list/contacts-list (om/props this)))))
+          (let [{:keys [page contacts-ds]} (om/props this)]
+            (navigator
+             {:initialRoute {:component contacts-list}
+              :renderScene (fn [route nav]
+                             (when state/*nav-render*
+                               (init-back-button-handler! nav)
+                               (let [{:keys [component]} (js->clj route :keywordize-keys true)]
+                                 (component (om/computed (om/props this) {:nav nav})))))}))))
 
 (swap! state/app-state assoc :contacts-ds
        (data-source {:rowHasChanged (fn [row1 row2]
                                       (not= row1 row2))}))
+(swap! state/app-state assoc :component contacts-list)
 
 (defonce RootNode (sup/root-node! 1))
 (defonce app-root (om/factory RootNode))
