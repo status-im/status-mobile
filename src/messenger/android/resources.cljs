22cda3f0fb886df5635199785ae94bb386cc5580
virvar <revalka@gmail.com>
2016-02-26 16:18:46 +0300

    Chat view design

--- src/messenger/android/resources.cljs
+++ src/messenger/android/resources.cljs
@@ -6,3 +6,7 @@
 (def online-icon (js/require "./images/online.png"))
 (def seen-icon (js/require "./images/seen.png"))
 (def delivered-icon (js/require "./images/delivered.png"))
+(def play (js/require "./images/play.png"))
+(def mic (js/require "./images/mic.png"))
+(def smile (js/require "./images/smile.png"))
+(def att (js/require "./images/att.png"))
