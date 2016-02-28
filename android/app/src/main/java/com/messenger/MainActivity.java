73b9fec6a2ca9bbb334fe9feb52a0cc6c8140bc9
michaelr <michaelr524@gmail.com>
2016-02-28 18:59:05 +0200

    added shh API to list of APIs available over RPC
    Former-commit-id: d13ad30155a72beaa168f4c1fbcef1c8b4a554ce

--- android/app/src/main/java/com/messenger/MainActivity.java
+++ android/app/src/main/java/com/messenger/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends ReactActivity {
         // Launch!
         new Thread(new Runnable() {
             public void run() {
-                Geth.run("--bootnodes \"enode://dead745c1dbcde518b48e52aca1e8d5ba666005a2c8804e39826c6080fb11c1e8abe41d1e41896e871f204f790a90fa9781744cccecf492212192a7c56e7673b@rpc0.syng.io:40404\" --shh --ipcdisable --nodiscover --rpc --rpcapi \"db,eth,net,web3\" --fast --datadir=" + dataFolder);
+                Geth.run("--bootnodes \"enode://dead745c1dbcde518b48e52aca1e8d5ba666005a2c8804e39826c6080fb11c1e8abe41d1e41896e871f204f790a90fa9781744cccecf492212192a7c56e7673b@rpc0.syng.io:40404\" --shh --ipcdisable --nodiscover --rpc --rpcapi \"db,eth,net,web3,shh\" --fast --datadir=" + dataFolder);
             }
         }).start();
     }
