4c31f1225a9b02e2c268929112371e42e3c6c963
Jarrad Hope <me@jarradhope.com>
2016-03-06 23:09:55 +0100

    upgrade renatal, react-native, web3

--- env/dev/env/android/main.cljs
+++ env/dev/env/android/main.cljs
@@ -7,7 +7,7 @@
 (enable-console-print!)
 
 (figwheel/watch-and-reload
-  :websocket-url "ws://10.0.3.2:3449/figwheel-ws"
+  :websocket-url "ws://localhost:3449/figwheel-ws"
   :heads-up-display true
   :jsload-callback #(om/add-root! state/reconciler core/AppRoot 1))
 
