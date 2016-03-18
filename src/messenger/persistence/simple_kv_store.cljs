a079f00186e546de3f8220805552867c2e8a365b
michaelr <michaelr524@gmail.com>
2016-03-18 17:11:43 +0200

    encrypt stored identity with password, beginning of msg store

--- src/messenger/persistence/simple_kv_store.cljs
+++ src/messenger/persistence/simple_kv_store.cljs
@@ -8,14 +8,13 @@
     (r/write
       (fn []
         (r/create :kv-store {:key   key
-                             :value (str value)} true))))
+                             :value (with-out-str (pr value))} true))))
   (get [_ key]
     (some-> (r/get-by-field :kv-store :key key)
             (r/single-cljs)
             (r/decode-value)))
   (contains-key? [_ key]
-    (= 0
-       (.-length (r/get-by-field :kv-store :key key))))
+    (r/exists? :kv-store :key key))
   (delete [_ key]
     (-> (r/get-by-field :kv-store :key key)
         (r/single)
