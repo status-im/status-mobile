2c54a06399673f3833032c0451baf5c3e9ccac46
michaelr <michaelr524@gmail.com>
2016-03-23 17:26:54 +0200

    copied ios folder
    Former-commit-id: 70235400211f1dc584a5d8d158de2eca39b9a01b

--- syng-im/ios/MessengerTests/MessengerTests.m
+++ syng-im/ios/MessengerTests/MessengerTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface SyngImTests : XCTestCase
+@interface MessengerTests : XCTestCase
 
 @end
 
-@implementation SyngImTests
+@implementation MessengerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
