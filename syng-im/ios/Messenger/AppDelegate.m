70235400211f1dc584a5d8d158de2eca39b9a01b
michaelr <michaelr524@gmail.com>
2016-03-23 17:26:54 +0200

    copied ios folder

--- syng-im/ios/Messenger/AppDelegate.m
+++ syng-im/ios/Messenger/AppDelegate.m
@@ -42,7 +42,7 @@
 //   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"SyngIm"
+                                                      moduleName:@"Messenger"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
 
