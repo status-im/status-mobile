7817b72bc98bb2c8a3b3d5cb9ecc770c7a5c2ae3
michaelr <michaelr524@gmail.com>
2016-03-23 17:02:53 +0200

    project and android config and build
    Former-commit-id: 7a1e2e32ebe15af13c1b4955d3baec196af25828

--- syng-im/android/app/src/main/java/com/syngim/MainActivity.java
+++ syng-im/android/app/src/main/java/com/syngim/MainActivity.java
@@ -3,11 +3,47 @@ package com.syngim;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+import android.os.Bundle;
+import android.os.Environment;
+import com.github.ethereum.go_ethereum.cmd.Geth;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
+import java.io.File;
+
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import io.realm.react.RealmReactPackage;
 
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Required for android-16 (???)
+        System.loadLibrary("gethraw");
+        System.loadLibrary("geth");
+
+        // Required because of crazy APN settings redirecting localhost
+        Properties properties = System.getProperties();
+        properties.setProperty("http.nonProxyHosts", "localhost|127.0.0.1");
+        properties.setProperty("https.nonProxyHosts", "localhost|127.0.0.1");
+
+        File extStore = Environment.getExternalStorageDirectory();
+
+        final String dataFolder = extStore.exists() ?
+                extStore.getAbsolutePath() :
+                getApplicationInfo().dataDir;
+
+        // Launch!
+        new Thread(new Runnable() {
+            public void run() {
+                Geth.run("--bootnodes enode://e2f28126720452aa82f7d3083e49e6b3945502cb94d9750a15e27ee310eed6991618199f878e5fbc7dfa0e20f0af9554b41f491dc8f1dbae8f0f2d37a3a613aa@139.162.13.89:30303 --shh --ipcdisable --nodiscover --rpc --rpcapi db,eth,net,web3,shh --fast --datadir=" + dataFolder);
+            }
+        }).start();
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -33,8 +69,11 @@ public class MainActivity extends ReactActivity {
    */
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-        new MainReactPackage()
-      );
+        return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                new ReactNativeContacts(),
+                new ReactNativeI18n(),
+                new RealmReactPackage()
+        );
     }
 }
