fe33faa21347e6cde63743c7c00f7da6f23ef284
michaelr <michaelr524@gmail.com>
2016-03-25 13:36:16 +0300

    refactored core
    Former-commit-id: f1d0f641d2e2f56b41bfc14a8fd31416561d2f37

---
+++ syng-im/src/syng_im/components/app_root.cljs
@@ -0,0 +1,21 @@
+(ns syng-im.components.app-root
+  (:require [re-frame.core :refer [subscribe dispatch dispatch-sync]]
+            [syng-im.components.react :refer [view text image touchable-highlight]]))
+
+
+(def logo-img (js/require "./images/cljs.png"))
+
+(defn alert [title]
+  (.alert (.-Alert js/React) title))
+
+(defn app-root []
+  (let [greeting (subscribe [:get-greeting])]
+    (fn []
+      [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
+       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
+       [image {:source logo-img
+               :style  {:width 80 :height 80 :margin-bottom 30}}]
+       [touchable-highlight {:style    {:background-color "#999" :padding 10 :border-radius 5}
+                             :on-press #(alert "HELLO!")}
+        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+
