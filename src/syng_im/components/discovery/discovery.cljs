47c3f01bd52dc0448718f69a136777cdde1bab1b
Adrian Tiberius <adrian.tiberius@gmail.com>
2016-05-05 18:04:17 +0300

    replaced android toolbar with new custom toolbar
    Former-commit-id: c11da58c50f0363be7c3200a76f12a44009a6f0a

--- src/syng_im/components/discovery/discovery.cljs
+++ src/syng_im/components/discovery/discovery.cljs
@@ -8,9 +8,9 @@
                                       view
                                       scroll-view
                                       text
-                                      text-input
-                                      toolbar-android]]
+                                      text-input]]
     [reagent.core :as r]
+    [syng-im.components.toolbar :refer [toolbar]]
     [syng-im.components.discovery.discovery-popular :refer [discovery-popular]]
     [syng-im.components.discovery.discovery-recent :refer [discovery-recent]]
     [syng-im.resources :as res]
@@ -24,66 +24,67 @@
       hashtags
       [])))
 
+(defn title-content [showSearch]
+  (if showSearch
+    [text-input {:underlineColorAndroid "transparent"
+                 ;:value                 (:search @search-input)
+                 :style                 {:flex       1
+                                         :marginLeft 18
+                                         :lineHeight 42
+                                         :fontSize   14
+                                         :fontFamily "Avenir-Roman"
+                                         :color      "#9CBFC0"}
+                 :autoFocus             true
+                 :placeholder           "Type your search tags here"
+                 :onSubmitEditing       (fn [e]
+                                          (let [search (aget e "nativeEvent" "text")
+                                                hashtags (get-hashtags search)]
+                                            (dispatch [:broadcast-status search hashtags])))}]
+    [view {:style {;:flex 1
+                   ;:flexDirection "row"
+                   ;:justifyContent "center"
+                   ;:alignSelf "stretch"
+                   ;:alignItems "center"
+                   }}
+     [text {:style {:color      "#000000de"
+                    :alignSelf "center"
+                    :textAlign  "center"
+                    :fontFamily "sans-serif"
+                    :fontSize   16}} "Discover"]]))
+
+(defn create-fake-discovery []
+  (let [number (rand-int 999)]
+    (do
+      (save-discoveries [{:name         (str "Name " number)
+                          :status       (str "Status This is some longer status to get the second line " number)
+                          :whisper-id   (str number)
+                          :photo        ""
+                          :location     ""
+                          :tags         ["tag1" "tag2" "tag3"]
+                          :last-updated (new js/Date)}])
+      (dispatch [:updated-discoveries]))))
+
 (defn discovery [{:keys [navigator]}]
   (let [showSearch (r/atom false)]
     (fn []
       [view {:style {:flex            1
                      :backgroundColor "#eef2f5"}}
-       [toolbar-android {:titleColor       "#4A5258"
-                         :navIcon          res/menu
-                         :actions          [{:title "Search"
-                                             :icon  res/search
-                                             :show  "always"}]
-                         :style            {:backgroundColor "#eef2f5"
-                                            :justifyContent  "center"
-                                            :height          56
-                                            :elevation       0}
-                         :onIconClicked    (fn []
-                                             (let [number (rand-int 999)]
-                                              (do
-                                                (save-discoveries [{:name         (str "Name " number)
-                                                                    :status       (str "Status This is some longer status to get the second line " number)
-                                                                    :whisper-id   (str number)
-                                                                    :photo        ""
-                                                                    :location     ""
-                                                                    :tags         ["tag1" "tag2" "tag3"]
-                                                                    :last-updated (new js/Date)}])
-                                                (dispatch [:updated-discoveries]))))
-                         ;; temporary dispatch for testing
-                         :onActionSelected (fn [index]
-                                             (if @showSearch
-                                               (reset! showSearch false)
-                                               (reset! showSearch true)))}
-        (if @showSearch
-          [text-input {:underlineColorAndroid "transparent"
-                       ;:value                 (:search @search-input)
-                       :style                 {:flex       1
-                                               :marginLeft 18
-                                               :lineHeight 42
-                                               :fontSize   14
-                                               :fontFamily "Avenir-Roman"
-                                               :color      "#9CBFC0"}
-                       :autoFocus             true
-                       :placeholder           "Type your search tags here"
-                       :onChangeText          (fn [new-text]
-                                                (let [old-text (:search @search-input)]
-                                                  (log/debug (str new-text "-" old-text))))
-                       :onSubmitEditing       (fn [e]
-                                                (let [search (aget e "nativeEvent" "text")
-                                                      hashtags (get-hashtags search)]
-                                                  (dispatch [:broadcast-status search hashtags])))}]
-          [view {:style {;:flex 1
-                         ;:flexDirection "row"
-                         ;:justifyContent "center"
-                         ;:alignSelf "stretch"
-                         ;:alignItems "center"
-                         }}
-          [text {:style {:color      "#000000de"
-                         :alignSelf "center"
-                         :textAlign  "center"
-                         :fontFamily "sans-serif"
-                         :fontSize   16}} "Discover"]])]
-
+       [toolbar {:style {:backgroundColor "#eef2f5"
+                         :elevation 0}
+                 :navigator navigator
+                 :nav-action {:image {:source {:uri "icon_hamburger"}
+                                      :style  {:width      16
+                                               :height     12}}
+                              :handler create-fake-discovery}
+                 :title     "Add Participants"
+                 :content   (title-content @showSearch)
+                 :action    {:image {:source {:uri "icon_search"}
+                                     :style  {:width  17
+                                              :height 17}}
+                             :handler (fn []
+                                        (if @showSearch
+                                          (reset! showSearch false)
+                                          (reset! showSearch true)))}}]
        [scroll-view {:style {}}
         [view {:style {:paddingLeft   30
                        :paddingTop    15
