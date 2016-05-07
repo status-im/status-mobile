3976ea6c70e4277359fc42d4f985b6e090853470
Adrian Tiberius <adrian.tiberius@gmail.com>
2016-05-07 17:53:39 +0300

    moved styles to separate file

--- src/syng_im/components/discovery/discovery_recent.cljs
+++ src/syng_im/components/discovery/discovery_recent.cljs
@@ -9,6 +9,7 @@
                                       view]]
     [syng-im.components.realm :refer [list-view]]
     [syng-im.utils.listview :refer [to-realm-datasource]]
+    [syng-im.components.discovery.styles :as st]
     [syng-im.components.discovery.discovery-popular-list-item :refer [discovery-popular-list-item]]
     [reagent.core :as r]))
 
@@ -19,8 +20,7 @@
   )
 
 (defn render-separator [sectionID, rowID, adjacentRowHighlighted]
-  (let [elem (r/as-element [view {:style {:borderBottomWidth 1
-                                          :borderBottomColor "#eff2f3"}
+  (let [elem (r/as-element [view {:style st/row-separator
                                   :key rowID}])]
     elem))
 
@@ -31,6 +31,5 @@
                 :enableEmptySections true
                 :renderRow  render-row
                 :renderSeparator render-separator
-                :style      {:backgroundColor "white"
-                             :paddingLeft 15}}]
+                :style st/recent-list}]
   ))
