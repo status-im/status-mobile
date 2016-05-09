152626da18b1d062762d873d339688a7c4b15ad7
virvar <revalka@gmail.com>
2016-05-09 12:26:42 +0300

    Validate phone number

--- src/syng_im/components/chat/input/phone.cljs
+++ src/syng_im/components/chat/input/phone.cljs
@@ -2,8 +2,11 @@
   (:require
    [re-frame.core :refer [subscribe dispatch dispatch-sync]]
    [syng-im.components.chat.input.simple-command :refer [simple-command-input-view]]
+   [syng-im.utils.phone-number :refer [valid-mobile-number?]]
    [syng-im.utils.utils :refer [log toast http-post]]
    [syng-im.utils.logging :as log]))
 
 (defn phone-input-view [command]
-  [simple-command-input-view command {:keyboardType "phone-pad"}])
+  [simple-command-input-view command {:keyboardType "phone-pad"}
+   :validator (fn [message]
+                (valid-mobile-number? message))])
