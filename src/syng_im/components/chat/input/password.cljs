867e5392adbc520dfb1507e320c3f366471fd735
Roman Volosovskyi <rasom@ukr.net>
2016-05-09 15:44:06 +0300

    chat inputs styles

--- src/syng_im/components/chat/input/password.cljs
+++ src/syng_im/components/chat/input/password.cljs
@@ -1,9 +1,7 @@
 (ns syng-im.components.chat.input.password
   (:require
-   [re-frame.core :refer [subscribe dispatch dispatch-sync]]
-   [syng-im.components.chat.input.simple-command :refer [simple-command-input-view]]
-   [syng-im.utils.utils :refer [log toast http-post]]
-   [syng-im.utils.logging :as log]))
+   [syng-im.components.chat.input.simple-command
+    :refer [simple-command-input-view]]))
 
 (defn password-input-view [command]
   [simple-command-input-view command {:secureTextEntry true}])
--- src/syng_im/components/chat/suggestions_styles.cljs
+++ src/syng_im/components/chat/suggestions_styles.cljs
@@ -2,7 +2,7 @@
   (:require [syng-im.components.styles :refer [font color-white]]))
 
 (def suggestion-item-container
-  {:flexDirection    "row"
+  {:flexDirection    :row
    :marginVertical   1
    :marginHorizontal 0
    :height           40
@@ -10,15 +10,15 @@
 
 (defn suggestion-background
   [{:keys [color]}]
-  {:flexDirection   "column"
-   :position        "absolute"
+  {:flexDirection   :column
+   :position        :absolute
    :top             10
    :left            60
    :backgroundColor color
    :borderRadius    10})
 
 (def suggestion-text
-  {:marginTop -2
+  {:marginTop        -2
    :marginHorizontal 10
    :fontSize         14
    :fontFamily       font
@@ -26,17 +26,17 @@
 
 (def suggestion-description
   {:flex       1
-   :position   "absolute"
+   :position   :absolute
    :top        7
    :left       190
    :lineHeight 18
    :fontSize   14
    :fontFamily font
-   :color      "black"})
+   :color      :black})
 
 (defn suggestions-container
   [suggestions]
-  {:flexDirection    "row"
+  {:flexDirection    :row
    :marginVertical   1
    :marginHorizontal 0
    :height           (min 105 (* 42 (count suggestions)))
