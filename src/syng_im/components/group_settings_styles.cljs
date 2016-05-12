240569434c31ae5ff73d24307523d7f5331d846f
virvar <revalka@gmail.com>
2016-05-12 14:15:09 +0300

    Group settings styles

---
+++ src/syng_im/components/group_settings_styles.cljs
@@ -0,0 +1,86 @@
+(ns syng-im.components.group-settings-styles
+  (:require [syng-im.components.styles :refer [font
+                                               title-font
+                                               color-white
+                                               color-purple
+                                               chat-background
+                                               online-color
+                                               selected-message-color
+                                               separator-color
+                                               text1-color
+                                               text2-color
+                                               toolbar-background1]]))
+
+(def chat-members-container
+  {:marginBottom 10})
+
+(def save-btn
+  {:margin          10
+   :width           36
+   :height          36
+   :borderRadius    50
+   :backgroundColor color-purple})
+
+(def save-btn-text
+  {:marginTop  7
+   :marginLeft 13
+   :color      color-white
+   :fontFamily font
+   :fontSize   16
+   :lineHeight 20})
+
+(def group-settings
+  {:flex            1
+   :flexDirection   :column
+   :backgroundColor color-white})
+
+(def properties-container
+  {:marginHorizontal 16})
+
+(def chat-name-text
+  {:marginTop    24
+   :marginBottom 16
+   :color        text2-color
+   :fontFamily   font
+   :fontSize     14
+   :lineHeight   20})
+
+(def chat-name-input
+  {:marginLeft -4
+   :fontSize   14
+   :fontFamily font
+   :color      text1-color})
+
+(def members-text
+  {:marginTop    24
+   :marginBottom 16
+   :color        text2-color
+   :fontFamily   font
+   :fontSize     14
+   :lineHeight   20})
+
+(def add-members-icon
+  {:marginVertical   19
+   :marginHorizontal 3
+   :width            17
+   :height           17})
+
+(def add-members-container
+  {:flexDirection :row
+   :marginBottom  16})
+
+(def add-members-text
+  {:marginTop  18
+   :marginLeft 32
+   :color      text2-color
+   :fontFamily font
+   :fontSize   14
+   :lineHeight 20})
+
+(def settings-text
+  {:marginTop    24
+   :marginBottom 16
+   :color        text2-color
+   :fontFamily   font
+   :fontSize     14
+   :lineHeight   20})
