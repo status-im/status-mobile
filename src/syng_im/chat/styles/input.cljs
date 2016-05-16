21f96ae2fb80486e588f25685d9e2f8457951aba
Roman Volosovskyi <rasom@ukr.net>
2016-05-16 16:04:07 +0300

    fix giant 'type'
    Former-commit-id: c2b4a1ec33ff93390338463ccf930f5d8513e0b4

--- src/syng_im/chat/styles/input.cljs
+++ src/syng_im/chat/styles/input.cljs
@@ -6,13 +6,6 @@
                                                text2-color
                                                chat-background
                                                color-black]]))
-(def money-input
-  {:flex       1
-   :marginLeft 8
-   :lineHeight 42
-   :fontSize   32
-   :fontFamily font
-   :color      :black})
 
 (def command-input-and-suggestions-container
   {:flexDirection :column})
--- src/syng_im/chat/views/money.cljs
+++ src/syng_im/chat/views/money.cljs
@@ -1,9 +1,7 @@
 (ns syng-im.chat.views.money
   (:require
-    [syng-im.chat.views.command :refer [simple-command-input-view]]
-    [syng-im.chat.styles.input :as st]))
+    [syng-im.chat.views.command :refer [simple-command-input-view]]))
 
 (defn money-input-view [command]
   [simple-command-input-view command
-   {:keyboardType :numeric
-    :style        st/money-input}])
+   {:keyboardType :numeric}])
