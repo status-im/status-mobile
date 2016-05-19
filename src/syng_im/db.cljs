6e52fca420a9ac5ef129914742578c639fa85e58
Roman Volosovskyi <rasom@ukr.net>
2016-05-19 19:29:17 +0300

    disable 'create button' until new chat will be opened

--- src/syng_im/db.cljs
+++ src/syng_im/db.cljs
@@ -7,27 +7,28 @@
 (def default-view :chat-list)
 
 ;; initial state of app-db
-(def app-db {:identity-password    "replace-me-with-user-entered-password"
-             :identity             "me"
-             :contacts             []
-             :contacts-ids         #{}
-             :selected-contacts    #{}
-             :current-chat-id      "console"
-             :chat                 {:command      nil
-                                    :last-message nil}
-             :chats                {}
-             :chats-updated-signal 0
-             :show-actions         false
-             :new-participants     #{}
-             :signed-up            true
-             :view-id              default-view
-             :navigation-stack     (list default-view)
+(def app-db {:identity-password      "replace-me-with-user-entered-password"
+             :identity               "me"
+             :contacts               []
+             :contacts-ids           #{}
+             :selected-contacts      #{}
+             :current-chat-id        "console"
+             :chat                   {:command      nil
+                                      :last-message nil}
+             :chats                  {}
+             :chats-updated-signal   0
+             :show-actions           false
+             :new-participants       #{}
+             :signed-up              true
+             :view-id                default-view
+             :navigation-stack       (list default-view)
              ;; TODO fix hardcoded values
-             :username             "My Name"
-             :phone-number         "3147984309"
-             :email                "myemail@gmail.com"
-             :status               "Hi, this is my status"
-             :current-tag          nil})
+             :username               "My Name"
+             :phone-number           "3147984309"
+             :email                  "myemail@gmail.com"
+             :status                 "Hi, this is my status"
+             :current-tag            nil
+             :disable-group-creation false})
 
 (def protocol-initialized-path [:protocol-initialized])
 (defn chat-input-text-path [chat-id]
