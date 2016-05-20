5bea3815cd22fea35aecf265a1d144000b979a8a
Jarrad Hope <jarrad@status.im>
2016-05-20 10:33:09 +0200

    lein voom freshen

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [prismatic/schema "1.0.4"]
                  ^{:voom {:repo "https://github.com/status-im/status-lib.git"
                           :branch "syng-rename"}}
-                 [status-im/protocol "0.1.1-20160506_171115-ge2c95c1"]
+                 [status-im/protocol "0.1.1-20160519_164302-g92930a0"]
                  [natal-shell "0.1.6"]]
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-figwheel "0.5.0-2"]]
