583f424e93b2b15b1a5e6d014db73f61a103b88b
virvar <revalka@gmail.com>
2016-05-24 16:34:46 +0300

    Single input

--- src/status_im/chat/views/confirmation_code.cljs
+++
@@ -1,6 +0,0 @@
-(ns status-im.chat.views.confirmation-code
-  (:require
-    [status-im.chat.views.command :refer [simple-command-input-view]]))
-
-(defn confirmation-code-input-view [command input]
-  [simple-command-input-view command input {:keyboardType :numeric}])
