6823acd171eeb4ff5b777a49bc30abbb8e805da8
Roman Volosovskyi <rasom@ukr.net>
2016-05-26 16:46:14 +0300

    :accessibility-label
    Former-commit-id: 63dd0d4a4a63c1d71bcafe6b7e56ecc00fd3c9aa

--- test/status_im/console.clj
+++ test/status_im/console.clj
@@ -2,33 +2,6 @@
   (:require [clojure.test :refer :all]
             [status-im.appium :refer :all]))
 
-(def command-request-icon
-  (str
-    "//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]"
-    "/android.widget.FrameLayout[1]/android.view.ViewGroup[1]"
-    "/android.widget.ScrollView[1]/android.view.ViewGroup[1]"
-    "/android.view.ViewGroup[1]/android.view.ViewGroup[1]"
-    "/android.view.ViewGroup[2]/android.widget.ImageView[1]"))
-
-(def input
-  (str
-    "//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]"
-    "/android.widget.FrameLayout[1]/android.view.ViewGroup[1]"
-    "/android.view.ViewGroup[2]/android.view.ViewGroup[1]"
-    "/android.widget.EditText[1]"))
-
-(def send-button1
-  (str "//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]"
-       "/android.widget.FrameLayout[1]/android.view.ViewGroup[1]"
-       "/android.view.ViewGroup[2]/android.view.ViewGroup[1]"
-       "/android.view.ViewGroup[2]/android.view.ViewGroup[1]"))
-
-(def send-button2
-  (str "//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]"
-       "/android.widget.FrameLayout[1]/android.view.ViewGroup[1]"
-       "/android.view.ViewGroup[2]/android.view.ViewGroup[2]"
-       "/android.view.ViewGroup[2]/android.view.ViewGroup[1]"))
-
 (def message-text
   (str "Your phone number is also required to use the app. Type"
        " the exclamation mark or hit the icon to open the command "
@@ -36,9 +9,9 @@
 
 (deftest console-test
   (let [driver (init)]
-    (click driver command-request-icon)
-    (write driver input "123")
-    (click driver send-button1)
-    (click driver send-button2)
+    (click driver :request-keypair-password)
+    (write driver :command-input "123")
+    (click driver :stage-command)
+    (click driver :send-message)
     (contains-text driver message-text)
     (quit driver)))
