17c1c7600328835f079d54012691a437a517cba6
Roman Volosovskyi <rasom@ukr.net>
2016-05-27 19:44:06 +0300

    clean js/window first attempt
    Former-commit-id: 7694ad9e873d80824a07798de66b96b0f6a96dbb

--- src/status_im/components/invertible_scroll_view.cljs
+++ src/status_im/components/invertible_scroll_view.cljs
@@ -1,8 +1,7 @@
 (ns status-im.components.invertible-scroll-view)
 
-(set! js/window.InvertibleScrollView (js/require "react-native-invertible-scroll-view"))
+(def class (js/require "react-native-invertible-scroll-view"))
 
 (defn invertible-scroll-view [props]
-  (js/React.createElement js/InvertibleScrollView
-                          (clj->js (merge {:inverted true} props))))
+  (js/React.createElement class (clj->js (merge {:inverted true} props))))
 
--- src/status_im/i18n.cljs
+++ src/status_im/i18n.cljs
@@ -2,22 +2,11 @@
   (:require
     [status-im.translations.en :as en]))
 
-(set! js/window.I18n (js/require "react-native-i18n"))
-(set! (.-fallbacks js/I18n) true)
-(set! (.-defaultSeparator js/I18n) "/")
+(def i18n (js/require "react-native-i18n"))
+(set! (.-fallbacks i18n) true)
+(set! (.-defaultSeparator i18n) "/")
 
-(set! (.-translations js.I18n) (clj->js {:en en/translations}))
+(set! (.-translations i18n) (clj->js {:en en/translations}))
 
 (defn label [path & options]
-  (.t js/I18n (name path) (clj->js options)))
-
-(comment
-  (defn deep-merge [& maps]
-    (if (every? map? maps)
-      (apply merge-with deep-merge maps)
-      (last maps)))
-
-  (defn add-translations [new-translations]
-    (let [translations (.-translations js/I18n)]
-      (set! (.-translations js/I18n) (clj->js (deep-merge (js->clj translations) new-translations)))))
-  )
+  (.t i18n (name path) (clj->js options)))
