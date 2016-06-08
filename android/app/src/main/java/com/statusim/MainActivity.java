1f0a4214b814da17d596f368c962c30152843327
Jarrad Hope <jarrad@status.im>
2016-06-08 12:43:02 +0200

    warn user if root before starting geth

--- android/app/src/main/java/com/statusim/MainActivity.java
+++ android/app/src/main/java/com/statusim/MainActivity.java
@@ -8,6 +8,10 @@ import com.facebook.react.shell.MainReactPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import android.os.Bundle;
 import android.os.Environment;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnCancelListener;
 import com.github.ethereum.go_ethereum.cmd.Geth;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -18,20 +22,20 @@ import java.util.List;
 import java.util.Properties;
 import java.io.File;
 
+
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import io.realm.react.RealmReactPackage;
 
+
 public class MainActivity extends ReactActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+    protected void startStatus() {
         // Required for android-16 (???)
         System.loadLibrary("gethraw");
         System.loadLibrary("geth");
 
-        // Required because of crazy APN settings redirecting localhost
+        // Required because of crazy APN settings redirecting localhost (found in GB)
         Properties properties = System.getProperties();
         properties.setProperty("http.nonProxyHosts", "localhost|127.0.0.1");
         properties.setProperty("https.nonProxyHosts", "localhost|127.0.0.1");
@@ -48,6 +52,38 @@ public class MainActivity extends ReactActivity {
                 Geth.run("--bootnodes enode://e2f28126720452aa82f7d3083e49e6b3945502cb94d9750a15e27ee310eed6991618199f878e5fbc7dfa0e20f0af9554b41f491dc8f1dbae8f0f2d37a3a613aa@139.162.13.89:30303 --shh --ipcdisable --nodiscover --rpc --rpcapi db,eth,net,web3,shh,admin --fast --datadir=" + dataFolder);
             }
         }).start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(!RootUtil.isDeviceRooted()) {
+            startStatus();
+        } else {
+            AlertDialog dialog = new AlertDialog.Builder(MainActivity.this).setMessage(getResources().getString(R.string.root_warning))
+                    .setPositiveButton(getResources().getString(R.string.root_okay), new OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            startStatus();
+                        }
+                    }).setNegativeButton(getResources().getString(R.string.root_cancel), new OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            MainActivity.this.finishAffinity();
+                        }
+                    }).setOnCancelListener(new OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            dialog.dismiss();
+                            MainActivity.this.finishAffinity();
+                        }
+                    }).create();
+            dialog.show();
+        }
+
     }
 
     /**
