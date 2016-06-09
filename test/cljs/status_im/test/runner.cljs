75649d46aa4eda76e95bf06700d47d900c4b1328
Roman Volosovskyi <rasom@ukr.net>
2016-06-09 16:06:39 +0300

    tests
    Former-commit-id: 84747296959ca9de8185c026967d61d12e60cace

--- test/cljs/status_im/test/runner.cljs
+++ test/cljs/status_im/test/runner.cljs
@@ -1,5 +1,7 @@
 (ns status-im.test.runner
   (:require [doo.runner :refer-macros [doo-tests]]
-            [status-im.test.handlers]))
+            [status-im.test.handlers]
+            [status-im.test.commands.handlers]))
 
-(doo-tests 'status-im.test.handlers)
+(doo-tests 'status-im.test.handlers
+           'status-im.test.commands.handlers)
