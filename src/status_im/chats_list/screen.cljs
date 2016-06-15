b9e3e213a1695423958c2f019105ffc86e72b0f6
virvar <revalka@gmail.com>
2016-06-15 16:41:08 +0300

    Grey action bar. Hide navbar on chat list scroll.
    Former-commit-id: 9ed4cc03e3e6852f47688fcd9bac39df2d12274d

--- src/status_im/chats_list/screen.cljs
+++ src/status_im/chats_list/screen.cljs
@@ -1,4 +1,5 @@
 (ns status-im.chats-list.screen
+  (:require-macros [status-im.utils.views :refer [defview]])
   (:require [re-frame.core :refer [subscribe dispatch]]
             [status-im.components.react :refer [list-view
                                                 list-item
@@ -13,33 +14,55 @@
                                                         action-button-item]]
             [status-im.components.drawer.view :refer [drawer-view open-drawer]]
             [status-im.components.styles :refer [color-blue
+                                                 toolbar-background1
                                                  toolbar-background2]]
             [status-im.components.toolbar :refer [toolbar]]
             [status-im.components.icons.ionicons :refer [icon]]
             [status-im.i18n :refer [label]]
-            [status-im.chats-list.styles :as st]))
+            [status-im.chats-list.styles :as st]
+            [status-im.components.tabs.styles :refer [tabs-height]]))
 
-(defn chats-list-toolbar []
+(defview chats-list-toolbar []
+  [chats-scrolled? [:get :chats-scrolled?]]
   [toolbar {:nav-action {:image   {:source {:uri :icon_hamburger}
                                    :style  st/hamburger-icon}
                          :handler open-drawer}
             :title      (label :t/chats)
-            :background-color toolbar-background2
+            :background-color (if chats-scrolled?
+                                toolbar-background1
+                                toolbar-background2)
             ;; TODO implement search
             :action     {:image   {:source {:uri :icon_search}
                                    :style  st/search-icon}
                          :handler (fn [])}}])
 
 (defn chats-list []
-  (let [chats (subscribe [:get :chats])]
+  (let [chats (subscribe [:get :chats])
+        chat-scrolled? (subscribe [:get :chats-scrolled?])
+        container-height (r/atom 0)
+        content-height (r/atom 0)]
+    (dispatch [:set :chats-scrolled? false])
     (fn []
       [drawer-view
        [view st/chats-container
         [chats-list-toolbar]
-        [list-view {:dataSource (to-datasource @chats)
-                    :renderRow  (fn [row _ _]
-                                  (list-item [chat-list-item row]))
-                    :style      st/list-container}]
+        [list-view {:dataSource          (to-datasource @chats)
+                    :renderRow           (fn [row _ _]
+                                           (list-item [chat-list-item row]))
+                    :style               st/list-container
+                    ;;; if "maximazing" chat list will make scroll to 0,
+                    ;;; then disable maximazing
+                    :onLayout            (fn [event]
+                                           (when-not @chat-scrolled?
+                                             (let [height (.. event -nativeEvent -layout -height)]
+                                               (reset! container-height height))))
+                    :onContentSizeChange (fn [width height]
+                                           (reset! content-height height))
+                    :onScroll            (fn [e]
+                                           (let [offset (.. e -nativeEvent -contentOffset -y)
+                                                 min-content-height (+ @container-height tabs-height)
+                                                 scrolled? (and (< 0 offset) (< min-content-height @content-height))]
+                                             (dispatch [:set :chats-scrolled? scrolled?])))}]
         [action-button {:buttonColor color-blue
                         :offsetY     16
                         :offsetX     16}
