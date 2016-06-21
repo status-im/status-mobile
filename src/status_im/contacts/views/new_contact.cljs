a9d2ed271114274a3bde5cad7f0fcc41e858e10b
Adrian Tiberius <adrian.tiberius@gmail.com>
2016-06-21 17:15:46 +0300

    applied new design
    Former-commit-id: 9f405ee18d9407198a6752040ccb9342f3d7be87

--- src/status_im/contacts/views/new_contact.cljs
+++ src/status_im/contacts/views/new_contact.cljs
@@ -19,8 +19,8 @@
                                                  toolbar-title-text
                                                  button-input-container
                                                  button-input
-                                                 white-form-text-input]]
-            [status-im.qr-scanner.views.import-button :refer [import-button]]
+                                                 form-text-input]]
+            [status-im.qr-scanner.views.scan-button :refer [scan-button]]
             [status-im.i18n :refer [label]]
             [status-im.contacts.styles :as st]))
 
@@ -28,51 +28,46 @@
 
 (def toolbar-title
   [view toolbar-title-container
-   [text {:style (merge toolbar-title-text {:color color-white})}
-    (label :t/new-contact)]])
+   [text {:style toolbar-title-text}
+    (label :t/add-new-contact)]])
 
 (defview contact-name-input [name]
   []
   [text-input
-   {:underlineColorAndroid color-white
-    :placeholderTextColor  color-white
-    :style                 white-form-text-input
+   {:underlineColorAndroid "#0000001f"
+    :placeholderTextColor  "#838c93de"
+    :style                 form-text-input
     :autoFocus             true
-    :placeholder           (label :t/contact-name)
+    :placeholder           (label :t/name)
     :onChangeText          #(dispatch [:set-in [:new-contact :name] %])}
    name])
 
 (defview contact-whisper-id-input [whisper-identity]
   [view button-input-container
    [text-input
-    {:underlineColorAndroid color-white
-     :placeholderTextColor  color-white
-     :style                 (merge white-form-text-input button-input)
+    {:underlineColorAndroid "#0000001f"
+     :placeholderTextColor  "#838c93de"
+     :style                 (merge form-text-input button-input)
      :autoFocus             true
-     :placeholder           (label :t/whisper-identity)
+     :placeholder           (label :t/address)
      :onChangeText          #(dispatch [:set-in [:new-contact :whisper-identity] %])}
     whisper-identity]
-   [import-button #(dispatch [:scan-qr-code {:toolbar-title (label :t/new-contact)} :set-new-contact-from-qr])]])
+   [scan-button #(dispatch [:scan-qr-code {:toolbar-title (label :t/new-contact)} :set-new-contact-from-qr])]])
 
 (defview new-contact []
   [{:keys [name whisper-identity phone-number] :as new-contact} [:get :new-contact]]
   [drawer-view
    [view st/contact-form-container
-    [linear-gradient {:colors ["rgba(182, 116, 241, 1)" "rgba(107, 147, 231, 1)" "rgba(43, 171, 238, 1)"]
-                      :start [0, 0]
-                      :end [0.5, 1]
-                      :locations [0, 0.8 ,1]
-                      :style  st/gradient-background}]
-
-    [toolbar {:background-color :transparent
-              :nav-action     {:image   {:source {:uri :icon_back_white}
+    [toolbar {:background-color :white
+              :nav-action     {:image   {:source {:uri :icon_back}
                                          :style  icon-back}
                                :handler  #(dispatch [:navigate-back])}
               :custom-content   toolbar-title
-              :action           {:image   {:source {:uri :icon_add}
+              :action           {:image   {:source {:uri :icon_ok_blue}
                                            :style  icon-search}
                                  :handler #(dispatch [:add-new-contact new-contact])}}]
     [view st/form-container
-     [contact-whisper-id-input whisper-identity]
      [contact-name-input name]
-     ]]])
+     [contact-whisper-id-input whisper-identity]]
+    [view st/address-explication-container
+     [text {:style st/address-explication} (label :t/address-explication)]]]])
