cbb56e499cdf2e312eed5783b30fa2a33c09c13e
Roman Volosovskyi <rasom@ukr.net>
2016-06-22 16:32:05 +0300

    check uniqueness
    Former-commit-id: 4181541ae268cb5c427209ac51a90c3b16584088

--- src/status_im/contacts/validations.cljs
+++ src/status_im/contacts/validations.cljs
@@ -8,8 +8,8 @@
   (not (realm/exists? :contacts :whisper-identity identity)))
 
 (s/def ::unique-identity unique-identity?)
-(s/def ::name (s/and ::not-empty-string ::unique-identity))
-(s/def ::whisper-identity ::not-empty-string)
+(s/def ::name ::not-empty-string)
+(s/def ::whisper-identity (s/and ::not-empty-string ::unique-identity))
 
 (s/def ::contact (s/keys :req-un [::name ::whisper-identity]
                          :opt-un [::phone ::photo-path ::address]))
