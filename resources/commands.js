58675d5509a140b64934d6fd1ad04f1d098ad4f3
Roman Volosovskyi <rasom@ukr.net>
2016-06-29 14:28:43 +0300

    locations

--- resources/commands.js
+++ resources/commands.js
@@ -1,7 +1,36 @@
 status.command({
     name: "location",
     description: "Send location",
-    color: "#9a5dcf"
+    color: "#9a5dcf",
+    preview: function (params) {
+        var text =  status.components.text(
+            {
+                style: {
+                    marginTop: 5,
+                    marginHorizontal: 0,
+                    fontSize: 14,
+                    fontFamily: "font",
+                    color: "black"
+                }
+            }, params.value);
+        var uri = "https://maps.googleapis.com/maps/api/staticmap?center="
+            + params.value
+            + "&size=100x100&maptype=roadmap&key=AIzaSyBNsj1qoQEYPb3IllmWMAscuXW0eeuYqAA&language=en"
+            + "&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C"
+            + params.value;
+
+        var image = status.components.image(
+            {
+                source: {uri: uri},
+                style: {
+                    width: 100,
+                    height: 100
+                }
+            }
+        );
+
+        return status.components.view({}, [text, image]);
+    }
 }).param({
     name: "address",
     type: status.types.TEXT,
