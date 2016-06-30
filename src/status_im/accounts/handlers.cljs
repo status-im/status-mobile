ea7c5b1665a0ab2aa15b01d073086137a002bcb3
Adrian Tiberius <adrian.tiberius@gmail.com>
2016-06-30 18:06:05 +0300

    created new accounts realm object and fixed new create account flow
    Former-commit-id: c878fba6f13f458d4918ea02cab19e03f9d0b3fe

---
+++ src/status_im/accounts/handlers.cljs
@@ -0,0 +1,32 @@
+(ns status-im.accounts.handlers
+  (:require [status-im.models.accounts :as accounts]
+            [re-frame.core :refer [register-handler after dispatch debug]]
+            [status-im.utils.logging :as log]
+            [status-im.components.react :refer [geth]]
+            [status-im.utils.types :refer [json->clj]]
+            [clojure.string :as str]))
+
+
+(defn save-account [_ [_ account]]
+  (accounts/save-accounts [account]))
+
+(register-handler :add-account
+  (-> (fn [db [_ {:keys [address] :as account}]]
+          (update db :accounts assoc address account))
+      ((after save-account))))
+
+(defn account-created [result]
+  (let [data (json->clj result)
+        public-key (:pubkey data)
+        address (:address data)]
+    (log/debug "Created account: " result)
+    (when (not (str/blank? public-key))
+      (do
+        (dispatch [:initialize-protocol public-key])
+        (dispatch [:add-account {:address address
+                                 :public-key public-key}])))))
+
+(register-handler :create-account
+  (-> (fn [db [_ password]]
+          (.createAccount geth password (fn [result] (account-created result)))
+        db)))
