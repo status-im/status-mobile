78e0cb86b894eb3359c11c29a31f6eaf651fb8ec
Roman Volosovskyi <rasom@ukr.net>
2016-07-02 15:03:31 +0300

    fix staged response name
    Former-commit-id: 5749f76be55cfd998caf67dc0857755c17438398

--- src/status_im/chat/views/staged_command.cljs
+++ src/status_im/chat/views/staged_command.cljs
@@ -11,12 +11,15 @@
   (dispatch [:unstage-command staged-command]))
 
 (defn simple-command-staged-view [staged-command]
-  (let [command (:command staged-command)]
+  (let [{:keys [type name] :as command} (:command staged-command)]
     [view st/staged-command-container
      [view st/staged-command-background
       [view st/staged-command-info-container
        [view (st/staged-command-text-container command)
-        [text {:style st/staged-command-text} (str "!" (:name command))]]
+        [text {:style st/staged-command-text}
+         (if (= :command type)
+           (str "!" name)
+           name)]]
        [touchable-highlight {:style   st/staged-command-cancel
                              :onPress #(cancel-command-input staged-command)}
         [image {:source res/icon-close-gray
