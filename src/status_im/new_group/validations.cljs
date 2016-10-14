ca914f207fde47775f6a33f5646d84d62c5a747b
Roman Volosovskyi <rasom@ukr.net>
2016-10-14 09:44:38 +0300

    validate command's name (#235)
    Former-commit-id: ac199ba2fa18a70ebb302aea2b78cd5066d94998

--- src/status_im/new_group/validations.cljs
+++ src/status_im/new_group/validations.cljs
@@ -2,14 +2,13 @@
   (:require [cljs.spec :as s]
             [status-im.utils.phone-number :refer [valid-mobile-number?]]
             [status-im.constants :refer [console-chat-id wallet-chat-id]]
-            [clojure.string :as str]))
-
-(def homoglyph-finder (js/require "homoglyph-finder"))
+            [clojure.string :as str]
+            [status-im.utils.homoglyph :as h]))
 
 (defn not-illegal-name? [username]
   (let [username (some-> username (str/trim))]
-    (and (not (.isMatches homoglyph-finder username console-chat-id))
-         (not (.isMatches homoglyph-finder username wallet-chat-id)))))
+    (and (not (h/matches username console-chat-id))
+         (not (h/matches username wallet-chat-id)))))
 
 (s/def ::not-empty-string (s/and string? not-empty))
 (s/def ::not-illegal-name not-illegal-name?)
