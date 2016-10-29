58aaacf044fd9c5b13bb3b4f831ce6fc8d8783f2
Roman Volosovskyi <rasom@ukr.net>
2016-10-29 23:02:49 +0300

    fix #394

--- resources/wallet.js
+++ resources/wallet.js
@@ -1,5 +1,12 @@
-function wallet() {
+function wallet(params) {
     var url = 'http://127.0.0.1:3450';
+
+    if (params.url && params.url !== "undefined" && params.url != "") {
+        url = params.url;
+        if (!/^[a-zA-Z-_]+:/.test(url)) {
+            url = 'http://' + url;
+        }
+    }
 
     return {webViewUrl: url};
 }
@@ -11,7 +18,7 @@ status.command({
     fullscreen: true,
     suggestionsTrigger: 'on-send',
     params: [{
-        name: "webpage",
+        name: "url",
         suggestions: wallet,
         type: status.types.TEXT
     }]
