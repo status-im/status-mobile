bbe18a56f10c1aabc1612bf0e1e35efae51125cb
Jarrad <me@jarradhope.com>
2016-11-13 10:47:34 +0700

    build-tools-24.0.2 required for Instabug

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -17,7 +17,7 @@ echo 'export PATH=$GRADLE_HOME/bin:$PATH' >> ~/.bash_profile
 echo 'export PATH=$ANDROID_HOME/tools:$PATH' >> ~/.bash_profile
 echo 'export PATH=$ANDROID_HOME/platform-tools:$PATH' >> ~/.bash_profile
 echo 'export PATH=$ANDROID_HOME/build-tools/$(ls $ANDROID_HOME/build-tools | sort | tail -1):$PATH' >> ~/.bash_profile
-( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --all --filter android-23,tools,platform-tools,build-tools-23.0.1,build-tools-23.0.2,build-tools-23.0.3,extra-android-m2repository,extra-google-m2repository --no-ui --force
+( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --all --filter android-23,tools,platform-tools,build-tools-23.0.1,build-tools-23.0.2,build-tools-23.0.3,build-tools-24.0.2,extra-android-m2repository,extra-google-m2repository --no-ui --force
 source ~/.bash_profile
 echo "y" | android update sdk -a --no-ui --filter sys-img-armeabi-v7a-android-23,sys-img-x86-android-23
 brew outdated node || brew unlink node && brew install node
