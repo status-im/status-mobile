3e4b943e72fad8d8a140385b7aeebc58cc572786
alwx <alwxndr@gmail.com>
2016-11-30 23:31:40 +0300

    No more error when tapping on command suggestion icon (#517)

--- resources/console.js
+++ resources/console.js
@@ -1613,7 +1613,7 @@ function suggestionsContainerStyle(suggestionsCount) {
         height: Math.min(150, (56 * suggestionsCount)),
         backgroundColor: "white",
         borderRadius: 5,
-        flex: 1
+        flexGrow: 1
     };
 }
 
