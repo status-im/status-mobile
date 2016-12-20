9745d0bda84872d309a588deebae0138a14a6247
Roman Volosovskyi <rasom@ukr.net>
2016-12-20 16:33:56 +0200

    prevent extra loading of Web3

--- src/status_im/utils/js_resources.cljs
+++ src/status_im/utils/js_resources.cljs
@@ -6,5 +6,7 @@
 (def status-js (slurp "resources/status.js"))
 (def wallet-js (str commands-js (slurp "resources/wallet.js")))
 (def webview-js (slurp "resources/webview.js"))
-(def web3 (slurp "resources/web3.0_16_0.min.js"))
+(def web3 (str "if (typeof Web3 == 'undefined') {"
+               (slurp "resources/web3.0_16_0.min.js")
+               "}"))
 (def web3-init (slurp "resources/web3_init.js"))
