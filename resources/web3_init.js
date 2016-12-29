323a91760f6df613fb16f3f3c2b2c9d76b2da0b7
Roman Volosovskyi <rasom@ukr.net>
2016-12-29 20:28:30 +0200

    add StatusHttpProview for accessing web3 in webview

--- resources/web3_init.js
+++ resources/web3_init.js
@@ -1,1 +1,65 @@
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+var callbackId = 0;
+var callbacks = {};
+
+function httpCallback(id, data) {
+    var result = data;
+    var error = null;
+
+    console.log(data);
+
+    try {
+        result = JSON.parse(data);
+    } catch(e) {
+        error = {message: "InvalidResponse"};
+    }
+
+
+    callbacks[id](error, result);
+}
+
+var StatusHttpProvider = function (host, timeout) {
+    this.host = host || 'http://localhost:8545';
+    this.timeout = timeout || 0;
+};
+
+StatusHttpProvider.prototype.send = function (payload) {
+    throw new Error('You tried to send "'+ payload.method +'" synchronously. Synchronous requests are not supported, sorry.');
+};
+
+StatusHttpProvider.prototype.sendAsync = function (payload, callback) {
+
+    var messageId = callbackId++;
+    callbacks[messageId] = callback;
+    if(typeof StatusBridge == "undefined") {
+        var data = {
+            payload: JSON.stringify(payload),
+            callbackId: JSON.stringify(messageId)
+        };
+
+        webkit.messageHandlers.sendRequest.postMessage(JSON.stringify(data));
+    } else {
+        StatusBridge.sendRequest(JSON.stringify(messageId), JSON.stringify(payload));
+    }
+};
+
+/**
+ * Synchronously tries to make Http request
+ *
+ * @method isConnected
+ * @return {Boolean} returns true if request haven't failed. Otherwise false
+ */
+StatusHttpProvider.prototype.isConnected = function() {
+    try {
+        this.sendAsync({
+            id: 9999999999,
+            jsonrpc: '2.0',
+            method: 'net_listening',
+            params: []
+        }, function(){});
+        return true;
+    } catch(e) {
+        return false;
+    }
+};
+
+web3 = new Web3(new StatusHttpProvider("http://localhost:8545"));
