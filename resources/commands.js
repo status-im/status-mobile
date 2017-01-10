66c9e97eefb46ca973895648a7ca7049aea9c548
Gustavo Nunes <gustavonunesedl@gmail.com>
2017-01-10 01:30:32 -0200

    Prevent non-positive-number amounts for send / request commands

--- resources/commands.js
+++ resources/commands.js
@@ -752,6 +752,7 @@ function validateSend(params, context) {
 
     try {
         var val = web3.toWei(params.amount, "ether");
+        if (val <= 0) { throw new Error(); }
     } catch (err) {
         return {
             errors: [
@@ -895,4 +896,19 @@ status.command({
             }
         };
     },
+    validator: function(params) {
+        try {
+            var val = web3.toWei(params.amount, "ether");
+            if (val <= 0) { throw new Error(); }
+        } catch (err) {
+            return {
+                errors: [
+                    status.components.validationMessage(
+                        I18n.t('validation_title'),
+                        I18n.t('validation_invalid_number')
+                    )
+                ]
+            };
+        }
+    }
 });
