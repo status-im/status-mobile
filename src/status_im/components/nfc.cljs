3fffa6e6e9db3c01a76052738abe2a9c0c2dc3cb
alwx <alwxndr@gmail.com>
2017-01-26 10:16:07 +0300

    NFC access for Dapps (#670)

---
+++ src/status_im/components/nfc.cljs
@@ -0,0 +1,30 @@
+(ns status-im.components.nfc
+  (:require [cljs.spec :as s]
+            [status-im.utils.platform :as platform]))
+
+(def class
+  (when platform/android?
+    (js/require "nfc-react-native")))
+
+(def android-only-error "NFC API is available only on Android")
+
+(defn get-card-id [on-success on-error]
+  (if platform/android?
+    (-> (.getCardId class)
+        (.then on-success)
+        (.catch on-error))
+    (on-error android-only-error)))
+
+(defn read-tag [sectors on-success on-error]
+  (if platform/android?
+    (-> (.readTag class (clj->js sectors))
+        (.then on-success)
+        (.catch on-error))
+    (on-error android-only-error)))
+
+(defn write-tag [sectors card-id on-success on-error]
+  (if platform/android?
+    (-> (.writeTag class (clj->js sectors) card-id)
+        (.then on-success)
+        (.catch on-error))
+    (on-error android-only-error)))
