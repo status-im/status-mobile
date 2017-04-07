c0a8caf85b3f1b2ca2e3198d8909002c58baff2b
Andrey Shovkoplyas <motor4ik@gmail.com>
2017-04-07 18:29:32 +0300

    fixes https://github.com/status-im/status-react/issues/981

--- src/status_im/transactions/views/password_form.cljs
+++ src/status_im/transactions/views/password_form.cljs
@@ -2,28 +2,20 @@
   (:require-macros [status-im.utils.views :refer [defview]])
   (:require [re-frame.core :as rf]
             [status-im.components.react :as rn]
-            [status-im.components.styles :as common-st]
-            [status-im.components.text-field.view :as text-field]
+            [status-im.components.text-input-with-label.view :refer [text-input-with-label]]
             [status-im.transactions.styles.password-form :as st]
             [status-im.i18n :as i18n]))
 
 (defview view [transaction-quantity]
   [wrong-password? [:wrong-password?]]
   (let [error? wrong-password?]
-    [rn/view (st/password-container error?)
-     [rn/text {:style st/password-title}
-      (i18n/label-pluralize transaction-quantity :t/enter-password-transactions)]
-     [text-field/text-field
-      {:editable               true
-       :secure-text-entry      true
-       :label-hidden?          true
-       :error                  (when error? (i18n/label :t/wrong-password))
-       :error-color            common-st/color-light-red2
-       :placeholder            (i18n/label :t/password)
-       :placeholder-text-color common-st/color-white-transparent
-       :line-color             common-st/color-light-blue
-       :focus-line-height      2
-       :wrapper-style          st/password-input-wrapper
-       :input-style            st/password-input
-       :auto-focus             true
-       :on-change-text         #(rf/dispatch [:set-in [:confirm-transactions :password] %])}]]))
+    [rn/view st/password-container
+     [text-input-with-label
+      {:label             (i18n/label :t/password)
+       :description       (i18n/label-pluralize transaction-quantity :t/enter-password-transactions)
+       :on-change-text   #(rf/dispatch [:set-in [:confirm-transactions :password] %])
+       :style             {:color :white}
+       :auto-focus        true
+       :secure-text-entry true
+       :error             (when error? (i18n/label :t/wrong-password))}]]))
+
