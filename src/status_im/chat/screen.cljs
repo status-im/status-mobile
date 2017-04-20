bafa2820e1532e2558002caf78fa0d507eef0dbc
Roman Volosovskyi <rasom@ukr.net>
2017-04-20 21:18:26 +0300

    undo bug/#1057 fix

--- src/status_im/chat/screen.cljs
+++ src/status_im/chat/screen.cljs
@@ -168,6 +168,7 @@
                 :renderScrollComponent     #(invertible-scroll-view (js->clj %))
                 :onEndReached              (when-not loaded? #(dispatch [:load-more-messages]))
                 :enableEmptySections       true
+                :keyboardShouldPersistTaps true
                 :dataSource                (to-datasource-inverted messages)}]))
 
 (defview chat []
