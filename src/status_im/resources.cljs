b507b24c9dc37c25a6a8db97c2b1d51e3cd68be9
WhySoS3rious <sydhelly@hotmail.com>
2017-05-08 16:18:20 +0200

    Added Winsome.io to the dapps repertory

--- src/status_im/resources.cljs
+++ src/status_im/resources.cljs
@@ -28,6 +28,7 @@
    :gnosis (js/require "./images/contacts/gnosis.png")
    :melonport (js/require "./images/contacts/melonport.png")
    :bchat (js/require "./images/contacts/bchat.png")
+   :winsome (js/require "./images/contacts/winsome.png")
    :dentacoin (js/require "./images/contacts/Dentacoin.png")
    :augur (js/require "./images/contacts/augur.png")
    :ethlance (js/require "./images/contacts/ethlance.png")
