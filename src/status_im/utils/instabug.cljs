fd967a488a81c212183ff3bbc05a90abb78f5691
Roman Volosovskyi <rasom@ukr.net>
2017-05-19 14:52:09 +0300

    next attempt

--- src/status_im/utils/instabug.cljs
+++ src/status_im/utils/instabug.cljs
@@ -3,12 +3,6 @@
 
 (def instabug-rn (js/require "instabug-reactnative"))
 
-(defn init-instabug []
-  (when-not js/goog.DEBUG
-    (.startWithToken instabug-rn
-                     "b239f82a9cb00464e4c72cc703e6821e"
-                     (.. instabug-rn -invocationEvent -shake))))
-
 (defn log [str]
   (if js/goog.DEBUG
     (log/debug str)
