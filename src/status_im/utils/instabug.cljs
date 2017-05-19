bdb85413d52f1167bd86e138962b960a0696aede
Roman Volosovskyi <rasom@ukr.net>
2017-05-19 17:06:19 +0300

    logging to Instabug

--- src/status_im/utils/instabug.cljs
+++ src/status_im/utils/instabug.cljs
@@ -7,4 +7,21 @@
   (if js/goog.DEBUG
     (log/debug str)
     (let [a nil]
-      (.IBGLog a #_instabug-rn str))))
+      (.IBGLog instabug-rn str))))
+
+(defn instabug-appender []
+  {:enabled?   true
+   :async?     false
+   :min-level  nil
+   :rate-limit nil
+   :output-fn  :inherit
+
+   :fn
+               (fn [data]
+                 (let [{:keys [level ?ns-str ?err output_]} data]
+                   (log (force output_))))})
+
+(log/merge-config! {:appenders {:instabug (instabug-appender)
+                                ;; useful for local debug
+                                ;; :print    (log/println-appender)
+                                }})
