18832f64dcc45ad5405b4af37e316df99fc5a3e8
newnils <newnils@users.noreply.github.com>
2017-06-11 01:27:44 +0200

    Create de.cljs
    Line 146: What do you mean by keypair-password-command-description? - I translated the words 'keypair-password' to "Passwort Schlüsselpaar" but I don't know the what is going to happen here.
    
    Line 200: I would be carefull with the words account and wallet. I would suggest using the word "Account" for 'account' and "Konto" for 'wallet'. "Konto" is more related to money then "Account".
    
    If you have any questions concerning my translation, feel free to contact me at slack (newnils). :)

--- src/status_im/translations/de.cljs
+++ src/status_im/translations/de.cljs
@@ -4,7 +4,7 @@
   {
    ;common
    :members-title                         "Mitglieder"
-   :not-implemented                       "!nicht implementiert"
+   :not-implemented                       "nicht implementiert"
    :chat-name                             "Chatname"
    :notifications-title                   "Benachrichtigungen und Sounds"
    :offline                               "Offline"
@@ -69,7 +69,7 @@
    :phone-number                          "Telefonnumer"
    :email                                 "E-Mail"
    :profile-no-status                     "Kein Status"
-   :add-to-contacts                       "Zu Kontakte hinzufügen"
+   :add-to-contacts                       "Zu Kontakten hinzufügen"
    :error-incorrect-name                  "Bitte wählen Sie einen anderen Namen"
    :error-incorrect-email                 "Inkorrekte E-Mail"
 
@@ -89,7 +89,7 @@
    :phew-here-is-your-passphrase          "*Puh* das war schwer, hier ist Ihr Passphrase, *bitte aufschreiben und sicher aufbewahren! *Sie benötigen dies, um Ihren Account wiederherzustellen."
    :here-is-your-passphrase               "Hier ist Ihr Passphrase, *bitte aufschreiben und sicher verwahren!*Sie benötigen dies, um Ihren Account wiederherzustellen."
    :written-down                          "Stellen Sie sicher, dass Sie es sicher aufgeschrieben haben"
-   :phone-number-required                 "Tippen Sie hier, um Ihre Telefonnummer einzugeben & ich werde Ihre Freunde finden"
+   :phone-number-required                 "Tippen Sie hier, um Ihre Telefonnummer einzugeben, damit ich Ihre Freunde finden kann"
    :intro-status                          "Chatten Sie mit mir, um Ihren Account einzurichten und Ihre Einstellungen zu ändern!"
    :intro-message1                        "Willkommen beim Status.\nTippen Sie auf diese Nachricht, um Ihr Passwort einzurichten und loszulegen!"
    :account-generation-message            "Eine Sekunde, ich muss wahnsinnig schwierige Berechnungen durchführen, um Ihr Konto zu generieren!"
@@ -100,9 +100,9 @@
    :new-group-chat                        "Neuer Gruppenchat"
 
    ;discover
-   :discover                             "Entdecken"
+   :discover                              "Entdecken"
    :none                                  "Nichts"
-   :search-tags                           "Geben Sie Ihre Suchbegriffe hier ein"
+   :search-tags                           "Geben Sie hier Ihre Suchbegriffe ein"
    :popular-tags                          "Beliebte Begriffe"
    :recent                                "Kürzlich"
    :no-statuses-discovered                "Kein Status gefunden"
@@ -143,7 +143,7 @@
    :confirmation-code-request-text        "Bestätigungscode anfragen"
    :send-command-description              "Position senden"
    :request-command-description           "Anfrage senden"
-   :keypair-password-command-description  ""
+   :keypair-password-command-description  "Passwort Schlüsselpaar"
    :help-command-description              "Hilfe"
    :request                               "Anfrage"
    :chat-send-eth                         "{{amount}} ETH"
@@ -151,8 +151,8 @@
    :chat-send-eth-from                    "{{amount}} ETH von {{chat-name}}"
 
    ;new-group
-   :group-chat-name                       "Chatname"
-   :empty-group-chat-name                 "Bitte geben Sie einen Namen ein"
+   :group-chat-name                       "Gruppen-Chatname"
+   :empty-group-chat-name                 "Bitte geben Sie einen Gruppennamen ein"
    :illegal-group-chat-name               "Bitte wählen Sie einen anderen Namen"
 
    ;participants
@@ -169,7 +169,7 @@
 
    ;new-contact
    :add-new-contact                       "Neuen Kontakt hinzufügen"
-   :import-qr                             "Import"
+   :import-qr                             "Importiere QR-Code"
    :scan-qr                               "QR-Code scannen"
    :name                                  "Name"
    :whisper-identity                      "Identität flüstern"
@@ -197,7 +197,7 @@
 
    ;accounts
    :recover-access                        "Zugriff wiederherstellen"
-   :add-account                           "Konto hinzufügen"
+   :add-account                           "Account hinzufügen"
 
    ;wallet-qr-code
    :done                                  "Fertig"
@@ -206,7 +206,7 @@
    ;validation
    :invalid-phone                         "Ungültige Telefonnummer"
    :amount                                "Betrag"
-   :not-enough-eth                        (str "Nicht genug EHT auf dem Konto"
+   :not-enough-eth                        (str "Sie haben nicht genug ETH auf Ihrem Konto"
                                                "({{balance}} ETH)")
    ;transactions
    :confirm-transactions                  {:one   "Transaktion bestätigen"
@@ -215,9 +215,9 @@
    :status                                "Status"
    :pending-confirmation                  "Bestätigung in Bearbeitung"
    :recipient                             "Empfänger"
-   :one-more-item                         "Noch ein Objekt"
+   :one-more-item                         "Noch ein weiteres Objekt"
    :fee                                   "Gebühr"
    :value                                 "Wert"
 
    ;:webview
-   :web-view-error                        "Ups, Fehler"})
+   :web-view-error                        "Es ist leider zu einem Fehler bei der Darstellung der Web-Ansicht gekommen"
