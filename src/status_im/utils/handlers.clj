06bff32f79a87c7bca85e5927ce2f605ea451e87
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2017-07-11 20:03:49 +0300

    fix handlers-> macro

--- src/status_im/utils/handlers.clj
+++ src/status_im/utils/handlers.clj
@@ -5,8 +5,14 @@
    All functions are expected to accept [db event] as parameters.
    If one handler returns a modified db it will be used as parameters for subsequent handlers."
   [& forms]
-  (let [db (gensym "db")
-        event (gensym "event")]
+  (let [db     (gensym "db")
+        event  (gensym "event")
+        new-db (gensym "new-db")]
     `(fn [~db ~event]
-       (let [~@(interleave (repeat db) (map #(list 'or (list % db event) db) forms))]
+       (let [~@(interleave (repeat db)
+                           (map (fn [form]
+                                  `(let [~new-db (~form ~db ~event)]
+                                     (if (map? ~new-db)
+                                       ~new-db
+                                       ~db))) forms))]
          ~db))))
