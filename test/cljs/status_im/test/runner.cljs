a36e0ef27466afee8381d96338c5fd820e4e44ce
Oskar Thorén <me@oskarth.com>
2017-08-17 14:30:15 +0200

    utils: Support ERC20 balance-of in order to get token balance
    This is necessary in order to display token balance in Wallet.

--- test/cljs/status_im/test/runner.cljs
+++ test/cljs/status_im/test/runner.cljs
@@ -6,7 +6,8 @@
             [status-im.test.handlers]
             [status-im.test.utils.utils]
             [status-im.test.utils.money]
-            [status-im.test.utils.clocks]))
+            [status-im.test.utils.clocks]
+            [status-im.test.utils.erc20]))
 
 (enable-console-print!)
 
@@ -22,4 +23,5 @@
            'status-im.test.handlers
            'status-im.test.utils.utils
            'status-im.test.utils.money
-           'status-im.test.utils.clocks)
+           'status-im.test.utils.clocks
+           'status-im.test.utils.erc20)
