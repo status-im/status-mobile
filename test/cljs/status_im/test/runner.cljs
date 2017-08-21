2ff20eb8300e2ad0be5e4de8030f374e95277f40
Andrey Shovkoplyas <motor4ik@gmail.com>
2017-08-21 17:13:40 +0300

    accounts tests

--- test/cljs/status_im/test/runner.cljs
+++ test/cljs/status_im/test/runner.cljs
@@ -1,6 +1,7 @@
 (ns status-im.test.runner
   (:require [doo.runner :refer-macros [doo-tests]]
-            [status-im.test.contacts.handlers]
+            [status-im.test.contacts.events]
+            [status-im.test.accounts.events]
             [status-im.test.chat.models.input]
             [status-im.test.components.main-tabs]
             [status-im.test.handlers]
@@ -19,13 +20,15 @@
 
 (set! goog.DEBUG false)
 
-(doo-tests 'status-im.test.contacts.handlers
-           'status-im.test.chat.models.input
-           'status-im.test.components.main-tabs
-           'status-im.test.handlers
-           'status-im.test.utils.utils
-           'status-im.test.utils.money
-           'status-im.test.utils.clocks
-           'status-im.test.utils.erc20
-           'status-im.test.utils.random
-           'status-im.test.utils.gfycat.core)
+(doo-tests
+  'status-im.test.accounts.events
+  'status-im.test.contacts.events
+  'status-im.test.chat.models.input
+  'status-im.test.components.main-tabs
+  'status-im.test.handlers
+  'status-im.test.utils.utils
+  'status-im.test.utils.money
+  'status-im.test.utils.clocks
+  'status-im.test.utils.erc20
+  'status-im.test.utils.random
+  'status-im.test.utils.gfycat.core)
