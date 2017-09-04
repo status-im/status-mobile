654249a939393b07c6b7765cd65cf929e86afdd3
Oskar Thorén <me@oskarth.com>
2017-09-04 19:34:05 +0200

    Push notifications: iOS request permissions, listen for token, flags
    - Asks for permissions on iOS at startup
    - Introduce NOTIFICATIONS_WIP_ENABLED flag
    - Surface FCM Token and put it in clipboard for QA manual end to end testing
    - Listen for FCM RefreshToken event at root

--- src/status_im/utils/notifications.cljs
+++ src/status_im/utils/notifications.cljs
@@ -1,10 +1,29 @@
 (ns status-im.utils.notifications
-  (:require [status-im.react-native.js-dependencies :as rn]))
+  (:require [status-im.react-native.js-dependencies :as rn]
+            [status-im.utils.config :as config]
+            [status-im.utils.utils :as utils]
+            [status-im.components.react :refer [copy-to-clipboard]]
+            [taoensso.timbre :as log]))
 
 ;; Work in progress namespace responsible for push notifications and interacting
 ;; with Firebase Cloud Messaging.
 
+;; NOTE: Only need to explicitly request permissions on iOS.
+(defn request-permissions []
+  (.requestPermissions (.-default rn/react-native-fcm)))
+
 (defn get-fcm-token []
-  (let [fcm (aget rn/react-native-fcm "default")]
-    (.then ((.-getFCMToken fcm))
-           (fn [x] (println "*** FCM TOKEN" x)))))
+    (-> (.getFCMToken (aget rn/react-native-fcm "default"))
+        (.then (fn [x]
+                 (when config/notifications-wip-enabled?
+                   (log/info "FCM token" x)
+                   (copy-to-clipboard x)
+                   (utils/show-popup "INFO" (str "FCM Token in clipboard: " x)))))))
+
+(defn on-refresh-fcm-token []
+  (.on (.-default rn/react-native-fcm)
+       (.-RefreshToken (.-FCMEvent rn/react-native-fcm))
+       (fn [x]
+         (log/info "FCM token refreshed" x)
+         (copy-to-clipboard x)
+         (utils/show-popup "INFO" (str "FCM token (refreshed) in clipboard: " x)))))
