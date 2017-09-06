525e1486b9053832c3a994568e022acb9526f236
angusiguess <ethdem@gmail.com>
2017-09-06 16:55:49 -0400

    Initial Commit for first send screen:
    - Update toolbar text
    - Add frame for camera/qr
    - Add button for choose contacts
    - Add button for address from clipboard
    - Add button for browse photos
    
    Set recipient from clipboard.
    
    Added functionality and event handling necessary to set recipient
    from clipboard.
    
    Add choose-recipient event to assoc into the database
    
    Add QR code scanning to first screen
    
    Add vector icons for copy and select image
    
    Fix svg
    
    Fix rebase, localize labels
    
    Add checkmark icon (this is a placeholder, current svg won't render)
    
    Remove leak from develop
    
    Vertical alignment fixes
    
    Vertically align maps in view
    
    Address review comments
    
    Fix rebase reference problems/styling issues
    
    Small style tweaks, fix icon
    
    Fix small design issues
    
    Indentation fixes
    
    Address review points
    
    Fix indentation
    
    Prevent permissions request on ios
    
    On ios, a permissions request results in a null reference error,
    do a platform check before performing a permissions request.
    
    Fix compile error
    
    Restyle for ios, colours, layout
    
    Update icons on ios
    
    Restyle disabled button
    
    Browse photos button disabled
    
    Add viewfinder and platform specific styling.
    
    Viewfinder and style changes
    
    Add some torch functionality, fix toolbar, style issues
    
    Refactor view style, add translucent bits to viewfinder
    
    Address pull request feedback
    
    Replace a style by defstyle

--- src/status_im/ui/screens/wallet/send/styles.cljs
+++ src/status_im/ui/screens/wallet/send/styles.cljs
@@ -1,17 +1,18 @@
 (ns status-im.ui.screens.wallet.send.styles
   (:require-macros [status-im.utils.styles :refer [defnstyle defstyle]])
-  (:require [status-im.components.styles :as styles]))
+  (:require [status-im.components.styles :as styles]
+            [status-im.utils.platform :as platform]))
 
 (def wallet-container
   {:flex             1
-   :background-color styles/color-blue2})
+   :background-color styles/color-blue4})
 
 (def wallet-modal-container
   {:flex             1
-   :background-color styles/color-blue4})
+   :background-color styles/color-blue3})
 
 (def toolbar
-  {:background-color styles/color-blue5
+  {:background-color styles/color-blue4
    :elevation        0
    :padding-bottom   10})
 
@@ -48,14 +49,17 @@
 (def choose-recipient-label
   {:color :white})
 
-(def recipient-buttons
+(defstyle recipient-buttons
   {:flex-direction    :column
    :margin-horizontal 28
-   :margin-vertical   10
-   :border-radius     5
-   :background-color  styles/color-blue5})
+   :margin-vertical   20
+   :border-radius     8
+   :ios {:background-color styles/color-blue6}})
 
 (def recipient-icon {:margin-right 20})
+
+(def recipient-icon-disabled {:margin-right 20
+                              :opacity 0.3})
 
 (def recipient-button
   {:flex-direction  :row
@@ -64,38 +68,84 @@
    :margin-left     20})
 
 (def recipient-button-text
-  {:color     :white
-   :font-size 17})
+  {:color      :white
+   :align-self :center
+   :font-size  14})
 
-(defn recipient-touchable [divider?]
-  (cond-> {:border-color styles/color-gray-transparent-medium-light}
-    divider? (assoc :border-bottom-width 1)))
+(def recipient-button-text-disabled
+  (merge recipient-button-text {:color "rgba(255, 255, 255, 0.3)"}))
+
+(defnstyle recipient-touchable [divider?]
+  (cond-> {:border-color styles/color-gray-transparent-light}
+    divider? (assoc :ios {:border-bottom-width 1})))
+
+(def recipient-touchable-disabled
+  {:background-color           styles/color-blue4
+   :border-bottom-left-radius  8
+   :border-bottom-right-radius 8
+   :border-left-width          1
+   :border-bottom-width        1
+   :border-right-width         1
+   :border-color               "rgba(255, 255, 255, 0.3)"})
 
 (def qr-container
-  {:flex              1
-   :margin-horizontal 32})
+  {:flex              1})
 
 (def preview
   {:flex            1
    :justify-content :flex-end
    :align-items     :center})
 
-(defn outer-bezel [{:keys [height width]}]
-  {:position      :absolute
-   :top           -10
-   :left          -10
-   :border-radius 20
-   :border-color  styles/color-blue2
-   :border-width  20
-   :height        (+ height 20)
-   :width         (+ width 20)})
+(def corner-dimensions
+  {:position :absolute
+   :width    40
+   :height   40})
 
-(defn inner-bezel [{:keys [height width]}]
-  {:top           5
-   :left          5
-   :position      :absolute
-   :border-radius 20
-   :border-color  :white
-   :border-width  5
-   :height        (- height 10)
-   :width         (- width 10)})
+(defn corner-left-bottom [dimension]
+  (let [viewport-offset 0.1666]
+    (merge corner-dimensions {:bottom (* viewport-offset dimension)
+                              :left   (* viewport-offset dimension)})))
+
+(defn corner-right-bottom [dimension]
+  (let [viewport-offset 0.1666]
+    (merge corner-dimensions {:right  (* viewport-offset dimension)
+                              :bottom (* viewport-offset dimension)})))
+
+(defn corner-left-top [dimension]
+  (let [viewport-offset 0.1666]
+    (merge corner-dimensions {:top  (* viewport-offset dimension)
+                              :left (* viewport-offset dimension)})))
+
+(defn corner-right-top [dimension]
+  (let [viewport-offset 0.1666]
+    (merge corner-dimensions {:top   (* viewport-offset dimension)
+                              :right (* viewport-offset dimension)})))
+
+(def viewfinder-port {:position :absolute
+                      :left     0
+                      :top      0
+                      :bottom   0
+                      :right    0
+                      :flex     1})
+
+(defn viewfinder-translucent [height width side]
+  (let [viewport-offset 0.1666
+        height-offset (* viewport-offset height)
+        width-offset (* viewport-offset width)]
+    (cond-> {:position         :absolute
+             :background-color :black
+             :opacity          0.7}
+      (= :top side) (assoc :height height-offset
+                           :width  width)
+      (= :right side) (assoc :height (- height height-offset)
+                             :width width-offset
+                             :bottom 0
+                             :right 0)
+      (= :bottom side) (assoc :height height-offset
+                              :width (- width width-offset)
+                              :bottom 0
+                              :left 0)
+      (= :left side) (assoc :height (- height (* 2 height-offset))
+                            :width width-offset
+                            :top height-offset
+                            :left 0))))
