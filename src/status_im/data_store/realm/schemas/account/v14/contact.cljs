1b0409fdc9abea780a02988e89477dbdb7a10fae
Oskar Thorén <me@oskarth.com>
2017-09-18 15:43:14 +0200

    Bugfix: correct realm schema for release
    In release 0.9.11 we'll have v13, so we don't want to modify this in develop.
    This moves Push notification related changes into a new migration as opposed to
    merging them into one.

--- src/status_im/data_store/realm/schemas/account/v14/contact.cljs
+++ src/status_im/data_store/realm/schemas/account/v14/contact.cljs
@@ -1,4 +1,4 @@
-(ns status-im.data-store.realm.schemas.account.v13.contact
+(ns status-im.data-store.realm.schemas.account.v14.contact
   (:require [taoensso.timbre :as log]))
 
 (def schema {:name       :contact
