08e639597be97719fbbf7e2b610fd6f5c40ccaee
Eric Dvorsak <eric@dvorsak.fr>
2017-10-05 14:33:13 +0200

    [bug] fix 2003 unsigned transaction count upon expiration
    this fixed involved changing the swiper so that a function can be passed as
    title instead of a string.
    this allows developpers to pass custom elements as titles and still use style
    and variables that are computed when tab is built

--- src/status_im/components/tabs/views.cljs
+++ src/status_im/components/tabs/views.cljs
@@ -9,20 +9,25 @@
             [status-im.components.tabs.styles :as tabs.styles]
             [status-im.utils.platform :as platform]))
 
-(defn- tab [{:keys [view-id title icon-active icon-inactive style-active selected-view-id on-press]}]
+(defn- tab [{:keys [view-id title title-fn icon-active icon-inactive style-active selected-view-id on-press]
+             :or {title-fn react/text}}]
   (let [active? (= view-id selected-view-id)
         text-only? (nil? icon-active)]
-    [react/touchable-highlight {:style    (merge tabs.styles/tab (if (and active? style-active) style-active))
+    [react/touchable-highlight {:style    (merge tabs.styles/tab
+                                                 (when (and active? style-active)
+                                                   style-active))
                                 :disabled active?
-                                :on-press  #(if on-press (on-press view-id) (dispatch [:navigate-to-tab view-id]))}
+                                :on-press #(if on-press
+                                             (on-press view-id)
+                                             (dispatch [:navigate-to-tab view-id]))}
      [react/view {:style tabs.styles/tab-container}
       (when-let [icon (if active? icon-active icon-inactive)]
         [react/view
          [vi/icon icon (tabs.styles/tab-icon active?)]])
-      [react/view
-       [react/text (merge (if text-only? {:uppercase? (get-in platform/platform-specific [:uppercase?])})
-                          {:style (tabs.styles/tab-title active? text-only?)})
-        title]]]]))
+      [react/view {:style tabs.styles/tab-title-wrapper}
+       (title-fn {:uppercase? (and text-only? (get-in platform/platform-specific [:uppercase?]))
+                  :style      (tabs.styles/tab-title active? text-only?)}
+                 title)]]]))
 
 (defn- create-tab [index data selected-view-id on-press style-active]
   (let [data (merge data {:key              index
@@ -90,28 +95,29 @@
         scroll-ended      (async/chan 10)]
     (r/create-class
       {:component-did-mount
-                     #(start-scrolling-loop scroll-start scroll-ended)
+       #(start-scrolling-loop scroll-start scroll-ended)
        :component-will-update
-                     (fn []
-                       (if @swiped?
-                         (reset! swiped? false)
-                         (when @main-swiper
-                           (let [to (scroll-to tab-list @prev-view-id @view-id)]
-                             (async/put! scroll-start [@main-swiper to])))))
+       (fn []
+         (if @swiped?
+           (reset! swiped? false)
+           (when @main-swiper
+             (let [to (scroll-to tab-list @prev-view-id @view-id)]
+               (async/put! scroll-start [@main-swiper to])))))
        :display-name "swipable-tabs"
        :reagent-render
-                     (fn []
-                       [react/view {:style style}
-                        [tabs {:selected-view-id @view-id
-                               :tab-list         tab-list
-                               :style            style-tabs
-                               :style-tab-active style-tab-active
-                               :on-press         on-view-change}]
-                        [react/swiper (merge tabs.styles/swiper
-                                             {:index                  (get-tab-index tab-list @view-id)
-                                              :ref                    #(reset! main-swiper %)
-                                              :loop                   false
-                                              :on-momentum-scroll-end (on-scroll-end on-view-change tab-list swiped? scroll-ended view-id)})
-                         (doall
-                           (map-indexed (fn [index {screen :screen}]
-                                          (with-meta screen {:key index})) tab-list))]])})))
+       (fn []
+         [react/view {:style style}
+          [tabs {:selected-view-id @view-id
+                 :tab-list         tab-list
+                 :style            style-tabs
+                 :style-tab-active style-tab-active
+                 :on-press         on-view-change}]
+          [react/swiper (merge tabs.styles/swiper
+                               {:index                  (get-tab-index tab-list @view-id)
+                                :ref                    #(reset! main-swiper %)
+                                :loop                   false
+                                :on-momentum-scroll-end (on-scroll-end on-view-change tab-list swiped? scroll-ended view-id)})
+           (doall
+            (map-indexed (fn [index {screen :screen}]
+                           (with-meta screen {:key index}))
+                         tab-list))]])})))
