4cc7d3479754af8e7157f06c758d5d66f19df132
Oskar Thorén <me@oskarth.com>
2017-10-13 15:38:39 +0200

    Update feature flags & enable push notifications in production build
    - Comment on flag lifecycle
    - Explicitly declare all flags

--- src/status_im/utils/config.cljs
+++ src/status_im/utils/config.cljs
@@ -9,6 +9,14 @@
 
 (defn enabled? [v] (= "1" v))
 
+;; NOTE(oskarth): Feature flag deprecation lifecycles. We want to make sure
+;; flags stay up to date and are removed once behavior introduced is stable.
+;;
+;; TESTFAIRY_ENABLED - indefinite
+;; NOTIFICATIONS_WIP_ENABLED - in 0.9.12 release, remove in develop if all goes well
+;; STUB_STATUS_GO - TBD - roman knows
+;; NETWORK_SWITCHING - TBD - roman knows
+
 (def testfairy-enabled? (enabled? (get-config :TESTFAIRY_ENABLED)))
 (def notifications-wip-enabled? (enabled? (get-config :NOTIFICATIONS_WIP_ENABLED 0)))
 (def stub-status-go? (enabled? (get-config :STUB_STATUS_GO 0)))
