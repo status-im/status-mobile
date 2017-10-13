e47f598cf88a7b833c1f233cab8a3726fbdf17eb
Eric Dvorsak <eric@dvorsak.fr>
2017-10-13 18:07:04 +0200

    show warning when app is syncing

--- src/status_im/ui/screens/wallet/views.cljs
+++ src/status_im/ui/screens/wallet/views.cljs
@@ -4,9 +4,15 @@
             [status-im.components.icons.vector-icons :as vector-icons]
             [status-im.i18n :as i18n]))
 
-(defn error-message-view [error-container-style error-message-style]
+(defn message-view [error-container-style error-message-style icon-container-style label]
   [react/view {:style error-container-style}
    [react/view {:style styles/error-container}
     [vector-icons/icon :icons/exclamation_mark {:color           :white
-                                                :container-style styles/error-exclamation}]
-    [react/text {:style error-message-style} (i18n/label :t/wallet-error)]]])
+                                                :container-style icon-container-style}]
+    [react/text {:style error-message-style} label]]])
+
+(defn error-message-view [error-container-style error-message-style]
+  [message-view error-container-style error-message-style styles/error-exclamation (i18n/label :t/wallet-error)])
+
+(defn wallet-syncing [error-container-style error-message-style]
+  [message-view error-container-style error-message-style styles/warning-exclamation (i18n/label :t/sync-in-progress)])
