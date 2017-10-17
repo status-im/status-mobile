43dab5b17440a3f8a57df0cb48b573916aba6115
Julien Eluard <julien.eluard@gmail.com>
2017-10-17 10:03:53 +0200

    Fixed warnings when compiling tests

--- test/cljs/status_im/react_native/js_dependencies.cljs
+++ test/cljs/status_im/react_native/js_dependencies.cljs
@@ -38,5 +38,6 @@
 (def vector-icons           #js {:default #js {}})
 (def webview-bridge         #js {:default #js {}})
 (def svg                    #js {:default #js {}})
+(def react-native-fcm       #js {:default #js {}})
 
 
