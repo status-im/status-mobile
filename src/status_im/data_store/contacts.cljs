1eba86b7bf8f824feab0c77008932f5ae28dafd6
alwx <alwxndr@gmail.com>
2017-10-17 23:45:59 +0200

    Command scopes spec fix, removed contacts leftovers

--- src/status_im/data_store/contacts.cljs
+++ src/status_im/data_store/contacts.cljs
@@ -2,32 +2,9 @@
   (:require [status-im.data-store.realm.contacts :as data-store])
   (:refer-clojure :exclude [exists?]))
 
-(defn- command->map-item
-  [[_ {:keys [name] :as command}]]
-  [(keyword name) command])
-
-(defn- enrich-with-owner-id [owner-id]
-  (fn [[k v]]
-    [k (assoc v :owner-id owner-id)]))
-
-(defn- commands-map->commands-list
-  [commands-map]
-  (or (if (and commands-map (map? commands-map))
-        (vals commands-map)
-        commands-map)
-      '()))
-
 (defn get-all
   []
-  (map
-    (fn [{:keys [commands responses whisper-identity] :as contact}]
-      (assoc contact
-        :commands (->> commands
-                       (map command->map-item)
-                       (map (enrich-with-owner-id whisper-identity))
-                       (into {}))
-        :responses (into {} (map command->map-item responses))))
-    (data-store/get-all-as-list)))
+  (data-store/get-all-as-list))
 
 (defn get-by-id
   [whisper-identity]
@@ -41,8 +18,7 @@
                      (assoc :pending? (boolean (if contact-db
                                                  (if (nil? pending?) pending-db? pending?)
                                                  pending?)))
-                     (update :commands commands-map->commands-list)
-                     (update :responses commands-map->commands-list))]
+                     (dissoc :commands :responses :global-command))]
     (data-store/save contact' (boolean contact-db))))
 
 (defn save-all
