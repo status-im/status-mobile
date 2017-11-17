03439b12c6602f018392fc17de5a4fb34e27e112
Goran Jovic <goranjovic@gmail.com>
2017-11-17 16:03:28 +0100

    issue #1951 - added a hardcoded snt entry under asset list - hidden behind ERC20_ENABLED flag

--- src/status_im/utils/config.cljs
+++ src/status_im/utils/config.cljs
@@ -16,11 +16,13 @@
 ;; TESTFAIRY_ENABLED - indefinite
 ;; STUB_STATUS_GO - indefinite
 ;; NOTIFICATIONS_WIP_ENABLED - in 0.9.12 release, remove in develop if all goes well
+;; ERC20_ENABLED - until idea #3 is merged, remove in develop when ready
 
 (def testfairy-enabled? (enabled? (get-config :TESTFAIRY_ENABLED)))
 (def notifications-wip-enabled? (enabled? (get-config :NOTIFICATIONS_WIP_ENABLED 0)))
 (def stub-status-go? (enabled? (get-config :STUB_STATUS_GO 0)))
 (def mainnet-networks-enabled? (enabled? (get-config :MAINNET_NETWORKS_ENABLED 0)))
+(def erc20-enabled? (enabled? (get-config :ERC20_ENABLED 0)))
 (def log-level
   (-> (get-config :LOG_LEVEL "error")
       string/lower-case
