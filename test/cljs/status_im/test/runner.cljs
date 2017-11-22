b509a1e2536fa6a32a7982984634aaa8773fa6bc
pablodip <pablodip@gmail.com>
2017-11-22 15:36:06 +0100

    add macro to run cljs tests without specifying all namespaces

--- test/cljs/status_im/test/runner.cljs
+++ test/cljs/status_im/test/runner.cljs
@@ -1,4 +1,5 @@
 (ns status-im.test.runner
+  (:require-macros [status-im.test.runner-macros])
   (:require [doo.runner :refer-macros [doo-tests]]
             [status-im.test.chat.events]
             [status-im.test.contacts.events]
@@ -28,23 +29,4 @@
 
 (set! goog.DEBUG false)
 
-(doo-tests
- 'status-im.test.utils.async
- 'status-im.test.chat.events
- 'status-im.test.accounts.events
- ;;'status-im.test.contacts.events
- 'status-im.test.profile.events
- 'status-im.test.wallet.events
- 'status-im.test.bots.events
- 'status-im.test.wallet.transactions.subs
- 'status-im.test.chat.models.input
- 'status-im.test.i18n
- 'status-im.test.utils.utils
- 'status-im.test.utils.money
- 'status-im.test.utils.clocks
- 'status-im.test.utils.eip.eip681
- 'status-im.test.utils.erc20
- 'status-im.test.utils.random
- 'status-im.test.utils.gfycat.core
- 'status-im.test.utils.signing-phrase.core
- 'status-im.test.utils.transducers)
+(status-im.test.runner-macros/doo-cljs-tests)
