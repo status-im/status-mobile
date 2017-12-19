a5c48046b044f47c58c682af5363b562f2ffeeda
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2017-12-19 13:25:42 +0200

    Linked react-native-randombytes for desktop. Login screen removed from views.cljs since it breakes prod build

--- src/status_im/ui/screens/views.cljs
+++ src/status_im/ui/screens/views.cljs
@@ -3,32 +3,13 @@
   (:require [re-frame.core :refer [dispatch]]
             [status-im.components.react :refer [view modal]]
             [status-im.components.styles :as common-styles]
-            [status-im.ui.screens.accounts.login.views :refer [login]]
             [status-im.ui.screens.profile.views :refer [profile]]))
 
 (defn validate-current-view
-  [current-view signed-up?]
-  (if (or (contains? #{:login} current-view)
-          signed-up?)
-    current-view
-    :profile))
+      [current-view signed-up?]
+      (
+        current-view
+        :profile))
 
 (defview main []
-         (letsubs [signed-up? [:signed-up?]
-
-                   modal-view [:get :modal]]
-                  (when true
-                    (let [view-id :profile
-                          current-view (validate-current-view view-id signed-up?)]
-                      (let [component (case current-view
-                                        :login login
-                                        :profile profile
-                                        (throw (str "Unknown view: " current-view))
-                                        )]
-                        [view common-styles/flex
-                         [component]]
-                        )
-                      ))
-
-                  ))
-          
+         [view [profile]])
