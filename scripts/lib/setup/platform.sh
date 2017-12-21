68de4e8bc3d5bcf9fb258b09dbbea6008577deb8
Julien Eluard <julien.eluard@gmail.com>
2017-12-21 10:28:55 +0100

    Make sure setup script runs on linux

--- scripts/lib/setup/platform.sh
+++ scripts/lib/setup/platform.sh
@@ -10,14 +10,9 @@ function is_linux() {
   [[ "$OS" =~ Linux ]]
 }
 
-function is_ubuntu() {
-  ! is_linux && return 1
-  (lsb_release -a 2>/dev/null | grep -q Ubuntu) && (command -v apt >/dev/null)
-}
-
 function exit_unless_os_supported() {
-  if ! is_macos && ! is_ubuntu; then
-    cecho "@red[[This install script currently supports Mac OS X and Ubuntu \
+  if ! is_macos && ! is_linux; then
+    cecho "@red[[This install script currently supports Mac OS X and Linux \
 via apt. To manually install, please visit the wiki for more information:]]
 
     @blue[[https://wiki.status.im/Building_Status]]"
