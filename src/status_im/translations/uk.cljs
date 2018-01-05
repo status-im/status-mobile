dafb4ac12d011258ed6c392ec9b10a4e90e6e481
MrVuuu <33403759+MrVuuu@users.noreply.github.com>
2018-01-05 05:34:48 +0200

    uk.cjs

--- src/status_im/translations/uk.cljs
+++ src/status_im/translations/uk.cljs
@@ -7,7 +7,7 @@
    :not-implemented                       "!не реалізовано"
    :chat-name                             "Назва групи"
    :notifications-title                   "Сповіщення та звуки"
-   :offline                               "Оффлайн"
+   :offline                               "Офлайн"
    :search-for                            "Шукати..."
    :cancel                                "Відміна"
    :next                                  "Далі"
@@ -21,7 +21,7 @@
    :node-unavailable                      "Не знайдено ethereum вузол"
    :yes                                   "Так"
    :no                                    "Ні"
-
+    
    :camera-access-error                   "Щоб надати доступ до камери, перейдіть до налаштувань вашої системи та переконайтеся, що вибранo Status > Камера"
    :photos-access-error                   "Щоб надати доступ до фото, перейдіть до налаштувань вашої системи та переконайтеся, що вибранo Status > Фото"
 
@@ -52,8 +52,9 @@
    ;;sync
    :sync-in-progress                      "Синхронізація..."
    :sync-synced                           "Синхронізовано"
-
+    
    ;messages
+   :status-sending                        "Відправлення"
    :status-pending                        "Очікує відправлення"
    :status-sent                           "Надіслано"
    :status-seen-by-everyone               "Переглянуто всіма"
@@ -104,8 +105,7 @@
    :sharing-cancel                        "Відміна"
 
    :browsing-title                        "Переглянути"
-   :browsing-browse                       "@browse"
-   :browsing-open-in-web-browser          "Відрити у веб-браузері"
+   :browsing-open-in-web-browser          "Відкрити у веб-браузері"
    :browsing-cancel                       "Відміна"
 
    ;;sign-up
@@ -122,7 +122,7 @@
    :intro-message1                        "Вітаємо у Status!\nТоркніться цього повідомлення, щоб встановити пароль і почати роботу."
    :account-generation-message            "Почекайте секунду, маю виконати страшенно складні розрахунки, щоб створити ваш обліковий запис!"
    :move-to-internal-failure-message      "Нам треба перемістити деякі важливі файли з зовнішньої на внутрішню пам'ять. Для цього нам потрібен ваш дозвіл. Ми не будемо використовувати зовнішню пам'ять у майбутніх версіях."
-   :debug-enabled
+   :debug-enabled                         "Запущений сервер налагоджування! Тепер ви можете виконати * status-dev-cli scan *, щоб знайти сервер з вашого комп'ютера в мережі."
 
    ;;phone types
    :phone-e164                            "Міжнародний 1"
@@ -188,7 +188,7 @@
    :chat-settings                         "Налаштування чату"
    :edit                                  "Редагувати"
    :add-members                           "Додати співрозмовників"
-
+    
    ;;commands
    :chat-send-eth                         "{{amount}} ETH"
 
@@ -211,7 +211,7 @@
    :delete-group-prompt                   "Це не вплине на список ваших контактів"
    :contact-s                             {:one   "контакт"
                                            :other "контакти"}
-
+    
    ;;protocol
    :received-invitation                   "отримано запрошення до групи"
    :removed-from-chat                     "видалив вас з групи"
@@ -230,7 +230,6 @@
    :can-not-add-yourself                  "Ви не можете додати себе"
    :unknown-address                       "Невідома адреса"
 
-
    ;;login
    :connect                               "Підключитися"
    :address                               "Адреса"
@@ -240,7 +239,7 @@
    :wrong-password                        "Невірний пароль"
    :enter-password                        "Введіть пароль"
 
-   ;;recover
+      ;;recover
    :passphrase                            "Кодова фраза"
    :recover                               "Відновити"
    :twelve-words-in-correct-order         "12 слів у правильному порядку"
@@ -277,77 +276,91 @@
    :confirmations-helper-text             "Будь ласка, зачекайте щонайменше 12 підтверджень, щоб переконатися, що транзакція оброблена надійно"
    :copy-transaction-hash                 "Скопіювати хеш транзакції"
    :open-on-etherscan                     "Відрити на Etherscan.io"
+   :incoming                              "Отримані"
+   :outgoing                              "Відправлені"
+   :pending                               "В очікуванні"
+   :postponed                             "Відкладені"
 
-   ;:webview
+   ;;webview
    :web-view-error                        "ой, помилка"
 
    ;;testfairy warning
-   :testfairy-title                       "Увага!"
-   :testfairy-message                     "Ви використовуєте додаток з 'нічних збірок'. З метою тестування ця збірка включає в себе запис сесії, якщо активне з'єднання Wi-Fi, тому всі ваші взаємодії з цим додатком зберігаються (як відео чи журнали) і можуть використовуватися нашою командою розробників для вивчення можливих проблем. Збережені відео / журнали не включають ваші паролі. Запис виконується лише в тому випадку, якщо додаток встановлено з нічного збірки. Нічого не записується, якщо додаток встановлено з PlayStore або TestFlight."
+   :testfairy-title                       "Попередження!"
+   :testfairy-message                     "Ви використовуєте додаток, встановлений з нічної збірки. Для цілей тестування ця збірка включає в себе запис сеансу, якщо використовується з'єднання Wi-Fi, так що всі ваші взаємодії з цим додатком зберігаються (як відео і логи) і можуть використовуватися нашою командою розробників для вивчення можливих проблем. Збережені відео / логи не включають в себе ваші паролі. Запис виконується тільки в тому випадку, якщо додаток встановлено з нічного збирання. Нічого не записується, якщо додаток встановлено з PlayStore або TestFlight."
 
    ;; wallet
    :wallet                                "Гаманець"
    :wallets                               "Гаманці"
    :your-wallets                          "Ваші гаманці"
    :main-wallet                           "Основний Гаманець"
-   :wallet-error                          "Помилка завантаження даних"
-   :wallet-send                           "Відправлено"
-   :wallet-request                        "Запит"
-   :wallet-exchange                       "Обмін"
+   :wallet-error                          "Помилка при завантаженні даних"
+   :wallet-send                           "Відправити"
+   :wallet-send-token                     "Відправити {{symbol}}"
+   :wallet-request                        "Запросити"
+   :wallet-exchange                       "Обміняти"
    :wallet-assets                         "Активи"
    :wallet-add-asset                      "Додати актив"
    :wallet-total-value                    "Загальна вартість"
    :wallet-settings                       "Налаштування гаманця"
-   :signing-phrase-description            "Введіть свій пароль, щоб підписати транзакцію. Переконайтеся, що наведені вище слова співпадають з вашою кодовою фразою-підписом"
+   :wallet-manage-assets                  "Управління Активами"
+   :signing-phrase-description            "Підпишіть транзакцію, ввівши свій пароль. Переконайтеся, що слова вище відповідають вашій секретній фразі підпису"
    :wallet-insufficient-funds             "Недостатньо коштів"
+   :wallet-my-token                       "Мій {{symbol}}"
+   :wallet-market-value                   "Ринкова вартість"
    :request-transaction                   "Запит транзакції"
-   :send-request                          "Відправити запит"
+   :send-request                          "Надіслати запит"
    :share                                 "Поділитися"
    :eth                                   "ETH"
    :currency                              "Валюта"
    :usd-currency                          "USD"
    :transactions                          "Транзакції"
    :transaction-details                   "Деталі транзакції"
-   :transaction-failed                    "Транзакція не вдалася"
+   :transaction-failed                    "Не вдалося виконати транзакцію"
    :transactions-sign                     "Підписати"
-   :transactions-sign-all                 "Підписати всі"
-   :transactions-sign-transaction         "Підписати транзакції"
-   :transactions-sign-later               "Підписати потім"
+   :transactions-sign-all                 "Підписати все"
+   :transactions-sign-transaction         "Підписати транзакцію"
+   :transactions-sign-later               "Підписати пізніше"
    :transactions-delete                   "Видалити транзакцію"
-   :transactions-delete-content           "Транзакція буде видалена з списку 'Непідписані'"
+   :transactions-delete-content           "Транзакція буде видалена зі списку 'Непідписані'"
    :transactions-history                  "Історія"
    :transactions-unsigned                 "Непідписані"
    :transactions-history-empty            "У вашій історії ще немає транзакцій"
-   :transactions-unsigned-empty           "У вас немає непідписних транзакцій"
-   :transactions-filter-title             "Фільтрувати історію"
+   :transactions-unsigned-empty           "У вас немає жодних непідписаних транзакцій"
+   :transactions-filter-title             "Фільтри історії"
    :transactions-filter-tokens            "Токени"
    :transactions-filter-type              "Тип"
-   :transactions-filter-select-all        "Вибрати всі"
-   :view-transaction-details              "Переглянути деталі транзакції"
-   :transaction-description               "Будь ласка, зачекайте щонайменше 12 підтверджень, щоб переконатися, що транзакція оброблена надійно"
+   :transactions-filter-select-all        "Вибрати все"
+   :view-transaction-details              "Подивитися деталі транзакції"
+   :transaction-description               "Будь ласка, почекайте як мінімум 12 підтверджень, щоб переконатися, що транзакція оброблена безпечно"
    :transaction-sent                      "Транзакція відправлена"
-   :transaction-moved-text                "Транзакція залишиться у списку 'Непідписані' протягом наступних 5 хвилин"
-   :transaction-moved-title               "Транзакція перенесена"
+   :transaction-moved-text                "Транзакція буде знаходитися в списку 'Непідписані' протягом 5 хвилин"
+   :transaction-moved-title               "Транзакція переміщена"
    :sign-later-title                      "Підписати транзакцію пізніше?"
    :sign-later-text                       "Перейдіть до історії транзакцій, щоб підписати цю транзакцію"
-   :not-applicable                        "Не застосовується для непідписаних транзакцій"
+   :not-applicable                        "Не можна застосувати до непідписаних транзакцій"
 
    ;; Wallet Send
    :wallet-choose-recipient               "Вибрати Одержувача"
    :wallet-choose-from-contacts           "Вибрати зі Списку Контактів"
-   :wallet-address-from-clipboard         "Використовувати скопійовану раніше адресу"
-   :wallet-invalid-address                "Невірна адреса: \n {{data}}"
-   :wallet-browse-photos                  "Перегляд Фото"
-   :validation-amount-invalid-number      "Сума повинна бути числом"
-   :validation-amount-is-too-precise      "Сума занадто точна. Найменша одиниця, яку ви можете надіслати, є 1 Wei (1x10^-18 ETH)"
+   :wallet-address-from-clipboard         "Використати Адресу з буферу обміну"
+   :wallet-invalid-address                "Недійсна адреса: \n {{data}}"
+   :wallet-invalid-chain-id               "Мережа не відповідає: \n {{data}}"
+   :wallet-browse-photos                  "Переглянути Фотографії"
+   :validation-amount-invalid-number      "Сума недійсна"
+   :validation-amount-is-too-precise      "Занадто багато цифр після десяткової коми. Найменша сума, яку ви можете надіслати це 1 Wei (1x10^-18 ETH)"
+
+
 
    ;; network settings
    :new-network                           "Нова мережа"
    :add-network                           "Додати мережу"
    :add-new-network                       "Додати нову мережу"
+   :add-wnode                             "Додати поштовий сервер"
    :existing-networks                     "Існуючі мережі"
+   ;; TODO(dmitryn): come up with better description/naming. Suggested namings: Mailbox and Master Node
+   :existing-wnodes                       "Існуючі поштові сервери"
    :add-json-file                         "Додати JSON файл"
-   :paste-json-as-text                    "Вставити JSON файл"
+   :paste-json-as-text                    "Вставити JSON як текст"
    :paste-json                            "Вставити JSON"
    :specify-rpc-url                       "Зазначити RPC URL"
    :edit-network-config                   "Редагувати конфігурацію мережі"
@@ -357,6 +370,7 @@
    :rpc-url                               "RPC URL"
    :remove-network                        "Видалити мережу"
    :network-settings                      "Налаштування мережі"
+   :offline-messaging-settings            "Офлайн налаштування повідомлень"
    :edit-network-warning                  "Обережно, редагування мережевих даних може вимкнути цю мережу"
    :connecting-requires-login             "Підключення до іншої мережі вимагає входу в систему"
    :close-app-title                       "Увага!"
