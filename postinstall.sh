314d425187fe439088d9e4fb3120e383b85044d9
Eric Dvorsak <eric@dvorsak.fr>
2018-01-17 00:52:15 +0100

    Clean up build scripts

--- postinstall.sh
+++ postinstall.sh
@@ -3,5 +3,3 @@
 # rn-nodeify
 # temporary hack due to https://github.com/facebook/react-native/issues/4968
 ./node_modules/.bin/rn-nodeify --install "assert,zlib,buffer,inherits,console,constants,crypto,dns,domain,events,http,https,os,path,process,punycode,querystring,fs,stream,string_decoder,timers,tty,url,util,net,vm" --hack;
-npm install --save react@16.0.0;
-npm install --save react-native-tcp@3.2.1;
