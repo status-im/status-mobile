8b1288e20b5c15f71be49c265736abfacd1a7d8f
Eric Dvorsak <eric@dvorsak.fr>
2018-02-28 16:38:46 +0100

    add merge-fx macro
    this macro allows safe merging of cofx, making complex events easier
    to understand while avoiding the risk to overwrite the db effect when
    threading multiple effects creating functions
    
    Signed-off-by: Eric Dvorsak <eric@dvorsak.fr>

--- src/status_im/utils/handlers.cljs
+++ src/status_im/utils/handlers.cljs
@@ -107,3 +107,17 @@
        (remove (fn [{:keys [dapp? pending?]}]
                  (or pending? dapp?)))
        (map :whisper-identity)))
+
+(defn update-db [cofx fx]
+  (if-let [db (:db fx)]
+    (assoc cofx :db db)
+    cofx))
+
+(defn safe-merge [fx new-fx]
+  (if (:merging-fx-with-common-keys fx)
+    fx
+    (let [common-keys (clojure.set/intersection (into #{} (keys fx))
+                                                (into #{} (keys new-fx)))]
+      (if (empty? (disj common-keys :db))
+        (merge fx new-fx)
+        {:merging-fx-with-common-keys common-keys}))))
