250a355e459393e6d90bd11fba43fb5848574c4c
Eric Dvorsak <eric@dvorsak.fr>
2018-03-05 04:29:26 +0100

    Revert "Use re-frame fork with js/setTimeout as next-tick"
    This reverts commit ac9b5cd976d6a4aff3e820dcdb39a6a8e0255486.

--- src/status_im/core.cljs
+++ src/status_im/core.cljs
@@ -1,6 +1,7 @@
 (ns status-im.core
   (:require [status-im.utils.error-handler :as error-handler]
             [status-im.ui.components.react :as react]
+            [re-frame.core :as re-frame]
             [reagent.core :as reagent]
             [status-im.native-module.core :as status]
             [taoensso.timbre :as log]
@@ -14,4 +15,5 @@
   (log/set-level! config/log-level)
   (error-handler/register-exception-handler!)
   (status/init-jail)
-  (.registerComponent react/app-registry "StatusIm" #(reagent/reactify-component app-root)))
+  (.registerComponent react/app-registry "StatusIm" #(reagent/reactify-component app-root))
+  (re-frame/dispatch-sync [:initialize-app]))
