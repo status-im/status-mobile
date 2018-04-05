e9af024e0d149a9aa44a3625bc74ebeebf7d65a4
Andrey Shovkoplyas <motor4ik@gmail.com>
2018-04-05 14:09:55 +0300

    fixed help improve screen
    Signed-off-by: Andrey Shovkoplyas <motor4ik@gmail.com>

--- src/status_im/ui/screens/usage_data/events.cljs
+++ src/status_im/ui/screens/usage_data/events.cljs
@@ -4,9 +4,12 @@
 
 (handlers/register-handler-fx
   :help-improve-handler
-  (fn [{db :db} [_ yes? address next]]
-    (merge (accounts/account-update {:sharing-usage-data? yes?} {:db db})
-           {:dispatch-n [(if yes?
-                           [:register-mixpanel-tracking address]
-                           [:unregister-mixpanel-tracking])
-                         next]})))
+  (fn [{{:accounts/keys [accounts current-account-id] :as db} :db} [_ yes? address next]]
+    (let [{:keys [sharing-usage-data?]} (get accounts current-account-id)]
+      (merge (accounts/account-update {:sharing-usage-data? yes?} {:db db})
+             {:dispatch-n [(if yes?
+                             [:register-mixpanel-tracking address]
+                             (when (and next sharing-usage-data?)
+                               [:unregister-mixpanel-tracking]))
+                           (or next [:navigate-to-clean :home])]}))))
+
