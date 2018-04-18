c4611cf18580b1f8171583641f17c9aed47ec163
Goran Jovic <goranjovic@gmail.com>
2018-04-18 14:17:10 +0200

    feature #3916 - added a few ropsten erc20 tokens, rearranged icon resources
    Signed-off-by: Andrey Shovkoplyas <motor4ik@gmail.com>

--- src/status_im/utils/ethereum/macros.clj
+++ src/status_im/utils/ethereum/macros.clj
@@ -3,19 +3,19 @@
             [clojure.java.io :as io]))
 
 (defn icon-path
-  [symbol]
-  (let [s (str "./resources/images/tokens/" (name symbol) ".png")]
+  [network symbol]
+  (let [s (str "./resources/images/tokens/" (name network) "/" (name symbol) ".png")]
     (if (.exists (io/file s))
       `(js/require ~s)
       `(js/require "./resources/images/tokens/default.png"))))
 
-(defn- token->icon [{:keys [icon symbol]}]
+(defn- token->icon [network {:keys [icon symbol]}]
   ;; Tokens can define their own icons.
   ;; If not try to make one using a local image as resource, if it does not exist fallback to default.
-  (or icon (icon-path symbol)))
+  (or icon (icon-path network symbol)))
 
 (defmacro resolve-icons
   "In react-native arguments to require must be static strings.
    Resolve all icons at compilation time so no variable is used."
-  [tokens]
-  (mapv #(assoc-in % [:icon :source] (token->icon %)) tokens))
+  [network tokens]
+  (mapv #(assoc-in % [:icon :source] (token->icon network %)) tokens))
