e0dd5efdccb1d42753900ac944e9670dfda27992
kwingram25 <kwingram25@gmail.com>
2018-04-18 13:33:20 -0700

    refine URL matching, tests

--- test/cljs/status_im/test/chat/views/message.cljs
+++ test/cljs/status_im/test/chat/views/message.cljs
@@ -14,10 +14,15 @@
   (is (= (lazy-seq [{:text "" :url? false}
                     {:text "https://www.google.com/?gfe_rd=cr&dcr=0&ei=P9-CWuyBGaro8AeqkYGQDQ&gws_rd=cr&fg=1" :url? true}])
          (message/parse-url "https://www.google.com/?gfe_rd=cr&dcr=0&ei=P9-CWuyBGaro8AeqkYGQDQ&gws_rd=cr&fg=1")))
-  (is (= (lazy-seq [{:text "Status - " :url? false}
-                    {:text "https://github.com/status-im/status-react" :url? true}
+  (is (= (lazy-seq [{:text "Status -" :url? false}
+                    {:text " https://github.com/status-im/status-react" :url? true}
                     {:text " a Mobile Ethereum Operating System" :url? false}
                     nil])
          (message/parse-url "Status - https://github.com/status-im/status-react a Mobile Ethereum Operating System")))
   (is (= (lazy-seq [{:text "Browse, chat and make payments securely on the decentralized web." :url? false} nil])
-         (message/parse-url "Browse, chat and make payments securely on the decentralized web."))))
+         (message/parse-url "Browse, chat and make payments securely on the decentralized web.")))
+  (is (= (lazy-seq [{:text "web3.eth.blockNumber" :url? false} nil])
+         (message/parse-url "web3.eth.blockNumber")))
+  (is (= (lazy-seq [{:text "" :url? false}
+                    {:text "www.web3.com" :url? true}])
+         (message/parse-url "www.web3.com"))))
