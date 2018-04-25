6a604adbe4058dcb6d51f4d1dc240498cfbc5ca7
Andy Tudhope <tuddy0525@gmail.com>
2018-04-25 11:48:48 +0200

    Fixes 3869: Add ATT for easy testing

--- src/status_im/utils/ethereum/tokens.cljs
+++ src/status_im/utils/ethereum/tokens.cljs
@@ -382,7 +382,7 @@
       {:name     "Aragon Test Token"
        :symbol   :ATT
        :decimals 8
-       :address  "0xfff28fb56b2a53597c55e1a767c906f339335025"}
+       :address  "0x00a8e52df8f4f1f4b67bded9ae6090b35489a973"}
       {:name     "Handy Test Token"
        :symbol   :HND
        :decimals 18
