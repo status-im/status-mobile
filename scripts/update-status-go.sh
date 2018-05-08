2d6cb4fd04816e695a5def5da0e64b702c83392f
Adam Babik <a.babik@designfortress.com>
2018-05-08 09:58:04 +0200

    add update-status-go.sh script
    Signed-off-by: Adam Babik <a.babik@designfortress.com>

--- scripts/update-status-go.sh
+++ scripts/update-status-go.sh
@@ -2,9 +2,22 @@
 
 set -eof pipefail
 
-if [ $# -eq 0 ]
-then
-    echo "Need to supply a status-go version"
+usage() {
+    printf "%s is a tool for upgrading status-go to a given version.\n" "$(basename "$0")"
+    printf "The given version must be uploaded to Artifactory first.\n\n"
+    printf "Usage:\n\n"
+    printf "    %s version\n\n" "$(basename "$0")"
+    printf "Example:\n\n"
+    printf "    %s develop-g12345678\n" "$(basename "$0")"
+}
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    usage
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    echo "Need to provide a status-go version"
     exit 1
 fi
 
