tspoff/status-react

testrail report for extended automated tests

test/appium/tests/marks.py
@@ -1,0 +1,7 @@
+import pytest
+
+pr = pytest.mark.pr
+testrail_case_id = pytest.mark.testrail_case_id
+chat = pytest.mark.chat
+all = pytest.mark.all
+transaction = pytest.mark.trnasaction

status-im/status-mobile

Signed-off-by: yevh-berdnyk <ie.berdnyk@gmail.com>

test/appium/tests/marks.py
@@ -1,0 +1,7 @@
+import pytest
+
+pr = pytest.mark.pr
+testrail_case_id = pytest.mark.testrail_case_id
+chat = pytest.mark.chat
+all = pytest.mark.all
+transaction = pytest.mark.transaction