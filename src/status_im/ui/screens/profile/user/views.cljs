6dec4876a26cc35ed85a51cb3a91bbfcef7b6166
Andrey Shovkoplyas <motor4ik@gmail.com>
2018-05-16 12:37:38 +0300

    move networks under dev mode
    Signed-off-by: Andrey Shovkoplyas <motor4ik@gmail.com>

--- src/status_im/ui/screens/profile/user/views.cljs
+++ src/status_im/ui/screens/profile/user/views.cljs
@@ -126,11 +126,11 @@
        [react/text @(re-frame/subscribe [:get-app-version])]]]]))
 
 (defview advanced [{:keys [network networks dev-mode?]}]
-  (letsubs [advanced?                     [:get :my-profile/advanced?]
+  (letsubs [advanced? [:get :my-profile/advanced?]
             {:keys [sharing-usage-data?]} [:get-current-account]]
     [react/view
      [react/touchable-highlight {:on-press #(re-frame/dispatch [:set :my-profile/advanced? (not advanced?)])
-                                 :style styles/advanced-button}
+                                 :style    styles/advanced-button}
       [react/view {:style styles/advanced-button-container}
        [react/view {:style styles/advanced-button-container-background}
         [react/view {:style styles/advanced-button-row}
@@ -139,29 +139,30 @@
          [icons/icon (if advanced? :icons/up :icons/down) {:color colors/blue}]]]]]
      (when advanced?
        [react/view
-        [profile.components/settings-item
-         {:label-kw :t/network
-          :value (get-in networks [network :name])
-          :action-fn #(re-frame/dispatch [:navigate-to :network-settings])
-          :accessibility-label :network-button}]
+        (when dev-mode?
+          [profile.components/settings-item
+           {:label-kw            :t/network
+            :value               (get-in networks [network :name])
+            :action-fn           #(re-frame/dispatch [:navigate-to :network-settings])
+            :accessibility-label :network-button}])
         (when config/offline-inbox-enabled?
           [profile.components/settings-item-separator])
         (when config/offline-inbox-enabled?
           [profile.components/settings-item
-           {:label-kw :t/offline-messaging
-            :action-fn #(re-frame/dispatch [:navigate-to :offline-messaging-settings])
+           {:label-kw            :t/offline-messaging
+            :action-fn           #(re-frame/dispatch [:navigate-to :offline-messaging-settings])
             :accessibility-label :offline-messages-settings-button}])
-        [profile.components/settings-item-separator]
-        [profile.components/settings-switch-item
-         {:label-kw :t/dev-mode
-          :value dev-mode?
-          :action-fn #(re-frame/dispatch [:switch-dev-mode %])}]
         [profile.components/settings-item-separator]
         [profile.components/settings-item
          {:label-kw            :t/help-improve?
           :value               (i18n/label (if sharing-usage-data? :on :off))
           :action-fn           #(re-frame/dispatch [:navigate-to :usage-data [:navigate-back]])
-          :accessibility-label :help-improve}]])]))
+          :accessibility-label :help-improve}]
+        [profile.components/settings-item-separator]
+        [profile.components/settings-switch-item
+         {:label-kw  :t/dev-mode
+          :value     dev-mode?
+          :action-fn #(re-frame/dispatch [:switch-dev-mode %])}]])]))
 
 (defview my-profile []
   (letsubs [{:keys [public-key] :as current-account} [:get-current-account]
