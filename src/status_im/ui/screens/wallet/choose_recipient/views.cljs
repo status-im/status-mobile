e972cc4537d02f44c4f13db12da197941995845c
Andrey Shovkoplyas <motor4ik@gmail.com>
2018-05-25 14:48:46 +0300

    [#3414]
    Signed-off-by: Andrey Shovkoplyas <motor4ik@gmail.com>

--- src/status_im/ui/screens/wallet/choose_recipient/views.cljs
+++ src/status_im/ui/screens/wallet/choose_recipient/views.cljs
@@ -44,9 +44,9 @@
   (int (* 2 (/ (min height width) 3))))
 
 (defview choose-recipient []
-  (letsubs [dimensions        (react/get-dimensions "window")
-            camera-flashlight [:wallet.send/camera-flashlight]
-            view              [:get :view-id]]
+  (letsubs [read-once?        (atom false)
+            dimensions        (react/get-dimensions "window")
+            camera-flashlight [:wallet.send/camera-flashlight]]
     [react/view {:style styles/qr-code}
      [status-bar/status-bar {:type :transparent}]
      [toolbar-view camera-flashlight]
@@ -61,7 +61,9 @@
                        :aspect        :fill
                        :captureAudio  false
                        :torchMode     (camera/set-torch camera-flashlight)
-                       :onBarCodeRead #(re-frame/dispatch [:wallet/fill-request-from-url (camera/get-qr-code-data %) nil])}]]
+                       :onBarCodeRead #(when-not @read-once?
+                                         (reset! read-once? true)
+                                         (re-frame/dispatch [:wallet/fill-request-from-url (camera/get-qr-code-data %) nil]))}]]
       [viewfinder dimensions (size dimensions)]]
      [bottom-buttons/bottom-button
       [button/button {:disabled?           false
