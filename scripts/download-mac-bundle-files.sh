5deba5d8e11bfdf662701853b957716979dff207
Max Risuhin <risuhin.max@gmail.com>
2018-05-28 01:16:18 +0300

    macdeployqtfix step

--- scripts/download-mac-bundle-files.sh
+++ scripts/download-mac-bundle-files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 filename="StatusIm.app.zip"
-fileid="1yPTGcPe5DZd3ubzAgUBp3aAQRAOK9eKQ"
+#fileid="1yPTGcPe5DZd3ubzAgUBp3aAQRAOK9eKQ"
+fileid="1AVi8QvIB8zNF6oMsMBgOqmxa5nl1xdWC"
+rm -rf ./cookie
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
