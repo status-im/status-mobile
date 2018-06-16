30b2ba05af273c445423a877187913ed73528cc2
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2018-06-16 18:10:07 +0300

    Chat design implemented

---
+++ src/status_im/ui/screens/desktop/main/chat/styles.cljs
@@ -0,0 +1,42 @@
+(ns status-im.ui.screens.desktop.main.chat.styles
+  (:require [status-im.ui.components.colors :as colors]))
+
+(defn message-box [{:keys [outgoing] :as message}]
+  (let [align     (if outgoing :flex-end :flex-start)
+        color (if outgoing "#dce2fb" "white")]
+    {:align-self align
+     :background-color color
+     :border-radius 8
+     :padding-left 12
+     :padding-right 12
+     :padding-top 10
+     :padding-bottom 10
+     :max-width 340}))
+
+(defn message-row [{:keys [outgoing first-in-group?] :as message}]
+  (let [padding-horizontal (if outgoing :padding-right :padding-left)
+        padding-top-value (if first-in-group? 16 8)]
+    {padding-horizontal 24
+     :padding-top padding-top-value}))
+
+(defn message-timestamp-placeholder []
+  {:color      "transparent"
+   :font-size  10
+   :align-self :flex-end
+   :opacity    0.5
+   :text-align :right
+   :text-align-vertical :center
+   :width 60})
+
+(defn message-timestamp []
+  (merge (message-timestamp-placeholder)
+  {:color     colors/gray
+  :position :absolute
+  :bottom   0
+  :right    -5}))
+
+(def author
+  {:color    colors/gray
+   :font-size 12
+   :margin-left 48
+   :margin-bottom 4})
--- src/status_im/ui/screens/desktop/main/chat/views.cljs
+++ src/status_im/ui/screens/desktop/main/chat/views.cljs
@@ -10,7 +10,10 @@
             [status-im.utils.identicon :as identicon]
             [status-im.utils.datetime :as time]
             [status-im.ui.components.styles :as styles]
-            [status-im.ui.components.react :as react]))
+            [status-im.ui.components.react :as react]
+            [status-im.ui.components.colors :as colors]
+            [status-im.chat.views.message.datemark :as message-datemark]
+            [status-im.ui.screens.desktop.main.chat.styles :as desktop-message-styles]))
 
 (views/defview toolbar-chat-view []
   (views/letsubs [{:keys [chat-id name public-key public? group-chat]} [:get-current-chat] 
@@ -39,19 +42,18 @@
 (views/defview message-author-name [{:keys [outgoing from] :as message}]
   (views/letsubs [current-account [:get-current-account]
                   incoming-name   [:get-contact-name-by-identity from]]
-    (if outgoing
-      [react/text {:style message.style/author} (:name current-account)]
       (let [name (or incoming-name (gfycat/generate-gfy from))]
         [react/touchable-highlight {:on-press #(re-frame/dispatch [:show-contact-dialog from name (boolean incoming-name)])}
-         [react/text {:style message.style/author} name]]))))
+         [react/text {:style desktop-message-styles/author} name]])))
 
 (def photo-style
-  {:borderRadius 15
-   :width        30
-   :height       30})
+  {:borderRadius 20
+   :width        40
+   :height       40
+   :margin-right 8  })
 
 (views/defview member-photo [from]
-  (views/letsubs [photo-path nil];[:photo-path from]]
+  (views/letsubs [photo-path nil]
     [react/view
      [react/image {:source {:uri (if (string/blank? photo-path)
                                    (identicon/identicon from)
@@ -67,17 +69,44 @@
                                      photo-path)}
                      :style  photo-style}]])))
 
-(defn message [text me? {:keys [outgoing message-id chat-id message-status user-statuses timestamp
-                                from current-public-key content-type group-chat type value] :as message}]
+(views/defview message-with-timestamp [text {:keys [timestamp] :as message} style]
+ [react/view {:style style}
+  [react/view {:style {:flex-direction :row :flex-wrap :wrap}}
+   [react/text {:style {:font-size 14 }}
+    text]
+   [react/text {:style (desktop-message-styles/message-timestamp-placeholder)}
+    (time/timestamp->time timestamp)]
+   [react/text {:style (desktop-message-styles/message-timestamp)}
+    (time/timestamp->time timestamp)]
+   ]])
+
+
+(views/defview text-only-message [text message]
+  [react/view {:style (desktop-message-styles/message-row message)  }
+    [message-with-timestamp text message (desktop-message-styles/message-box message)]])
+
+(views/defview photo-placeholder []
+  [react/view {:style photo-style}])
+
+(views/defview message-with-name-and-avatar [text {:keys [from first-in-group? last-in-group?] :as message}]
+     [react/view {:style (desktop-message-styles/message-row message)}
+      [react/view {:style {:flex-direction :column}}
+       (when first-in-group?
+         [message-author-name message])
+       [react/view {:style {:flex-direction :row}}
+         (if last-in-group?
+           [member-photo from]
+           [photo-placeholder])
+        [message-with-timestamp text message (desktop-message-styles/message-box message)]]]])
+
+(defn message [text me? {:keys [ message-id chat-id message-status user-statuses from
+                                current-public-key content-type group-chat outgoing type value] :as message}]
   (if (= type :datemark)
     ^{:key (str "datemark" message-id)}
-    [react/view {:style {:margin-vertical 20}}
-     [react/text
-      (string/capitalize (last (string/split value #"-")))]
-     [react/view {:style {:height 1 :background-color "#e8ebec"}}]]
+    [message-datemark/chat-datemark value]
     (when (= content-type constants/text-content-type)
-      (reagent.core/create-class
-        {:component-did-mount
+     (reagent.core/create-class
+       {:component-did-mount
          #(when (and message-id
                      chat-id
                      (not outgoing)
@@ -86,22 +115,13 @@
             (re-frame/dispatch [:send-seen! {:chat-id    chat-id
                                              :from       from
                                              :message-id message-id}]))
-         :reagent-render
+       :reagent-render
          (fn []
-           ^{:key (str "message" message-id)}
-           [react/view {:style {:flex-direction :row :flex 1 :margin-vertical 12}}
-            (if outgoing
-              [my-photo from]
-              [member-photo from])
-            [react/view {:style {:padding-horizontal 12 :background-color :white :border-radius 8 :flex 1}}
-             [react/view {:style {:flex-direction :row}}
-              [message-author-name message]
-              [react/view {:style {:flex 1}}]
-              [react/text {:style {:color styles/color-gray4 :font-size 12}} (time/timestamp->time timestamp)]]
-             ;;TODO use https://github.com/status-im/status-react/pull/3299
-             ;;[rn-hl/hyperlink {:linkStyle {:color "#2980b9"} :on-press #(re-frame/dispatch [:show-link-dialog %1])}
-             [react/text
-              text]]])}))))
+          ^{:key (str "message" message-id)}
+          (if (and group-chat (not outgoing))
+            [message-with-name-and-avatar text message]
+            [text-only-message text message]))})
+      )))
 
 (views/defview messages-view [{:keys [chat-id group-chat]}]
   (views/letsubs [chat-id* (atom nil)
@@ -113,7 +133,7 @@
               (js/setTimeout #(when scroll-ref (.scrollToEnd @scroll-ref)) 400))
           messages (re-frame/subscribe [:get-current-chat-messages])
           current-public-key (re-frame/subscribe [:get-current-public-key])]
-      [react/view {:style {:flex 1 :background-color :white :margin-horizontal 16}}
+      [react/view {:style {:flex 1 :background-color "#eef2f5"}}
        [react/scroll-view {:scrollEventThrottle    16
                            :on-scroll              (fn [e]
                                                     (let [ne (.-nativeEvent e)
@@ -131,9 +151,10 @@
              ^{:key (str message index)}
              [message content (= from @current-public-key) (assoc message-obj :group-chat group-chat)]))]]])))
 
+
 (views/defview chat-text-input []
   (views/letsubs [inp-ref (atom nil)]
-    [react/view {:style {:height 90 :margin-horizontal 16  :background-color :white :border-radius 12}}
+    [react/view {:style {:height 68 :margin-horizontal 16  :background-color :white :border-radius 12}}
      [react/view {:style {:flex-direction :row :margin-horizontal 16 :margin-top 16 :flex 1 :margin-bottom 16}}
       [react/view {:style {:flex 1}}
        [react/text-input {:placeholder    "Type a message..."
@@ -167,7 +188,5 @@
   (views/letsubs [current-chat [:get-current-chat]]
     [react/view {:style {:flex 1 :background-color :white}}
      [toolbar-chat-view]
-     [react/view {:style {:height 1 :background-color "#e8ebec" :margin-horizontal 16}}]
      [messages-view current-chat]
-     [react/view {:style {:height 1 :background-color "#e8ebec" :margin-horizontal 16}}]
      [chat-text-input]]))
