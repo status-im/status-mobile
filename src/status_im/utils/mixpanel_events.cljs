nau101/status-react

Track collectible events in MixPanel [#4799]

src/status_im/utils/mixpanel_events.cljs
@@ -147,6 +147,12 @@
    {:label      "Tap"
     :trigger    [:navigation-replace :wallet-transaction-sent]
     :properties {:target :wallet-transaction-sent}}
+   {:label      "Tap"
+    :trigger    [:navigate-to :display-collectible]
+    :properties {:target :display-collectible}}
+   {:label      "Tap"
+    :trigger    [:open-collectible-in-browser]
+    :properties {:target :open-collectible-in-browser}}
 
    ;;Profile
    {:label      "Tap"

status-im/status-mobile

Signed-off-by: Andrey Shovkoplyas <motor4ik@gmail.com>

src/status_im/utils/mixpanel_events.cljs
@@ -146,6 +146,12 @@
    {:label      "Tap"
     :trigger    [:send-transaction-message]
     :properties {:target :wallet-transaction-sent}}
+   {:label      "Tap"
+    :trigger    [:navigate-to :collectibles-list]
+    :properties {:target :collectibles-list}}
+   {:label      "Tap"
+    :trigger    [:open-collectible-in-browser]
+    :properties {:target :open-collectible-in-browser}}
 
    ;;Profile
    {:label      "Tap"