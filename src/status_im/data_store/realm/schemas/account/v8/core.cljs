nau101/status-react

Use partitioned topic for discovery
Currently we use a single topic for discovery.
This provides the best obscurity at the cost of bandwidth, as a message
sent on the discovery topic will be received by any peer.
This PR changes this behavior and start listening on a partitioned
topic.
Each pk will be hashed to a limited number of topics.

Everytime someone is in a conversation with someone from another topic
they will have to listen as well to avoid loosing obscurity, because we
only forward messages that we also advertise in the bloom filter.

The choice for the number of partitions depends on 2 factors:

1) The expected number of users using the network
2) The average number of contacts each user

Any change to the discovery topic will need to be split across 3
releases, to avoid breaking compatibility:

1) Listen to the new and old topic, publish to the old topic
2) Listen to the new and old topic, publish to the new topic
3) Listen to the new topic, publish to the new topic

This is step 1.

src/status_im/data_store/realm/schemas/account/v8/core.cljs
@@ -1,0 +1,25 @@
+(ns status-im.data-store.realm.schemas.account.v8.core
+  (:require [status-im.data-store.realm.schemas.account.v5.chat :as chat]
+            [status-im.data-store.realm.schemas.account.v8.transport :as transport]
+            [status-im.data-store.realm.schemas.account.v1.contact :as contact]
+            [status-im.data-store.realm.schemas.account.v7.message :as message]
+            [status-im.data-store.realm.schemas.account.v1.request :as request]
+            [status-im.data-store.realm.schemas.account.v1.user-status :as user-status]
+            [status-im.data-store.realm.schemas.account.v1.local-storage :as local-storage]
+            [status-im.data-store.realm.schemas.account.v2.mailserver :as mailserver]
+            [status-im.data-store.realm.schemas.account.v1.browser :as browser]
+            [taoensso.timbre :as log]))
+
+(def schema [chat/schema
+             transport/schema
+             contact/schema
+             message/schema
+             request/schema
+             mailserver/schema
+             user-status/schema
+             local-storage/schema
+             browser/schema])
+
+(defn migration [old-realm new-realm]
+  (log/debug "migrating v8 account database: " old-realm new-realm)
+  (message/migration old-realm new-realm))

src/status_im/transport/message/v1/protocol.cljs
@@ -15,13 +15,14 @@
 (defn init-chat
   "Initialises chat on protocol layer.
   If topic is not passed as argument it is derived from `chat-id`"
-  [{:keys [chat-id topic resend?]
+  [{:keys [chat-id topic one-to-one resend?]
     :or   {topic   (transport.utils/get-topic chat-id)}}
    {:keys [db]}]
   {:db (assoc-in db
                  [:transport/chats chat-id]
-                 (transport.db/create-chat {:topic   topic
-                                            :resend? resend?}))})
+                 (transport.db/create-chat {:topic      topic
+                                            :one-to-one one-to-one
+                                            :resend?    resend?}))})
 
 #_(defn requires-ack [message-id chat-id {:keys [db] :as cofx}]
     {:db (update-in db [:transport/chats chat-id :pending-ack] conj message-id)})
@@ -52,7 +53,9 @@
                  :message       (merge {:sig     current-public-key
                                         :pubKey  chat-id
                                         :payload payload
-                                        :topic   (transport.utils/get-topic constants/contact-discovery)}
+                                        :topic   (if config/partitioned-topic-enabled?
+                                                   (transport.filters/partitioned-topic chat-id)
+                                                   transport.filters/discovery-topic)}
                                        whisper-opts)}]}))
 
 (defn- prepare-recipients [public-keys db]

status-im/status-mobile

src/status_im/data_store/realm/schemas/account/v8/core.cljs
@@ -1,0 +1,25 @@
+(ns status-im.data-store.realm.schemas.account.v8.core
+  (:require [status-im.data-store.realm.schemas.account.v5.chat :as chat]
+            [status-im.data-store.realm.schemas.account.v8.transport :as transport]
+            [status-im.data-store.realm.schemas.account.v1.contact :as contact]
+            [status-im.data-store.realm.schemas.account.v7.message :as message]
+            [status-im.data-store.realm.schemas.account.v1.request :as request]
+            [status-im.data-store.realm.schemas.account.v1.user-status :as user-status]
+            [status-im.data-store.realm.schemas.account.v1.local-storage :as local-storage]
+            [status-im.data-store.realm.schemas.account.v2.mailserver :as mailserver]
+            [status-im.data-store.realm.schemas.account.v1.browser :as browser]
+            [taoensso.timbre :as log]))
+
+(def schema [chat/schema
+             transport/schema
+             contact/schema
+             message/schema
+             request/schema
+             mailserver/schema
+             user-status/schema
+             local-storage/schema
+             browser/schema])
+
+(defn migration [old-realm new-realm]
+  (log/debug "migrating v8 account database: " old-realm new-realm)
+  (transport/migration old-realm new-realm))

src/status_im/transport/message/v1/protocol.cljs
@@ -4,6 +4,7 @@
             [status-im.constants :as constants]
             [status-im.chat.core :as chat]
             [status-im.transport.db :as transport.db]
+            [status-im.transport.filters :as transport.filters]
             [status-im.transport.message.core :as message]
             [status-im.transport.utils :as transport.utils]))
 
@@ -15,13 +16,14 @@
 (defn init-chat
   "Initialises chat on protocol layer.
   If topic is not passed as argument it is derived from `chat-id`"
-  [{:keys [chat-id topic resend?]
+  [{:keys [chat-id topic one-to-one resend?]
     :or   {topic   (transport.utils/get-topic chat-id)}}
    {:keys [db]}]
   {:db (assoc-in db
                  [:transport/chats chat-id]
-                 (transport.db/create-chat {:topic   topic
-                                            :resend? resend?}))})
+                 (transport.db/create-chat {:topic      topic
+                                            :one-to-one one-to-one
+                                            :resend?    resend?}))})
 
 #_(defn requires-ack [message-id chat-id {:keys [db] :as cofx}]
     {:db (update-in db [:transport/chats chat-id :pending-ack] conj message-id)})
@@ -52,7 +54,9 @@
                  :message       (merge {:sig     current-public-key
                                         :pubKey  chat-id
                                         :payload payload
-                                        :topic   (transport.utils/get-topic constants/contact-discovery)}
+                                        :topic   (if config/partitioned-topic-enabled?
+                                                   (transport.filters/partition-topic chat-id)
+                                                   transport.filters/discovery-topic)}
                                        whisper-opts)}]}))
 
 (defn- prepare-recipients [public-keys db]