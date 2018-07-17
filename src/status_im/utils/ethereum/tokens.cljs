cd88c5dafe50284fe170e211520997358d17e91c
Chase <chasecole@protonmail.com>
2018-07-17 11:06:20 -0500

    Add SPANK token to mainnet assets
    Re-submission of a previous pull request to add SPANK to be displayable as an asset in Status.

--- src/status_im/utils/ethereum/tokens.cljs
+++ src/status_im/utils/ethereum/tokens.cljs
@@ -367,6 +367,10 @@
                     :name     "Domraider"
                     :address  "0x9af4f26941677c706cfecf6d3379ff01bb85d5ab"
                     :decimals 8}
+                   {:symbol   :SPANK
+                    :name     "SpankChain"
+                    :address  "0x42d6622deCe394b54999Fbd73D108123806f6a18"
+                    :decimals 18}
                    ;; NOTE(goranjovic) : the following three tokens are removed from the Manage Assets list
                    ;; and automatically removed from user's selection by a migration. However, we still need
                    ;; them listed here in order to correctly display any previous transactions the user had
