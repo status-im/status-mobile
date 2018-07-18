53ea7c1df47c8c748937ff9b0e0581ec2c547b2d
Kenneth Tilton <kentilton@gmail.com>
2018-07-18 08:19:35 -0400

    reduce indentation on :home-items sub

--- src/status_im/ui/screens/home/subs.cljs
+++ src/status_im/ui/screens/home/subs.cljs
@@ -3,12 +3,13 @@
             [status-im.utils.utils :as utils]
             [status-im.models.browser :as browser]))
 
-(re-frame/reg-sub :home-items
-                  :<- [:get-active-chats]
-                  :<- [:browsers]
-                  (fn [[chats browsers]]
-                    ;; Status react issue #3604:
-                    ;; de-dupe visits to same URL on home/chat list
-                    (sort-by #(-> % second :timestamp) >
-                             (merge chats
-                                    (utils/distinct-by-group browsers browser/get-current-url :timestamp)))))
+(re-frame/reg-sub
+ :home-items
+ :<- [:get-active-chats]
+ :<- [:browsers]
+ (fn [[chats browsers]]
+    ;; Status react issue #3604:
+    ;; de-dupe visits to same URL on home/chat list
+   (sort-by #(-> % second :timestamp) >
+            (merge chats
+                   (utils/distinct-by-group browsers browser/get-current-url :timestamp)))))
