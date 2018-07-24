c0697c0bacba9766c880dc0ee6561d7a2063109d
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2018-07-24 21:41:07 +0300

    Latest commits from desktop branch (everything after 9th July)

--- src/status_im/ui/screens/desktop/main/chat/styles.cljs
+++ src/status_im/ui/screens/desktop/main/chat/styles.cljs
@@ -62,34 +62,53 @@
   {:flex             1
    :background-color colors/gray-lighter})
 
+(def img-container
+  {:height          56
+   :justify-content :center})
+
 (def messages-scrollview-inner
   {:padding-vertical 46})
 
 (def photo-style
-  {:borderRadius 20
-   :width        40
-   :height       40
-   :margin-right 8})
+  {:border-radius 20
+   :width         40
+   :height        40
+   :margin-right  8})
+
+(def photo-style-toolbar
+  {:border-radius 32
+   :width         32
+   :height        32
+   :margin-right  8})
+
+(defn topic-image [color]
+  (merge photo-style-toolbar
+         {:background-color color
+          :align-items      :center
+          :justify-content  :center}))
+
+(def topic-text
+  {:font-size 18
+   :color     colors/white})
 
 (def toolbar-chat-view
-  {:align-items     :center
-   :padding         11
+  {:margin-left     11
    :justify-content :center})
 
 (def toolbar-chat-name
   {:font-size   16
    :color       :black
-   :font-weight "600"})
+   :font-weight "300"})
 
 (def add-contact
   {:background-color :white
    :border-radius    6
    :margin-top       3
-   :padding          4})
+   :padding-top      1})
 
 (def add-contact-text
   {:font-size 14
-   :color     colors/gray})
+   :color     colors/blue})
 
 (def message-text
   {:font-size 14})
@@ -114,3 +133,40 @@
 (def chat-view
   {:flex             1
    :background-color :white})
+
+(defn contact-card-text [color]
+  {:color     color
+   :font-size 14})
+
+(def contact-card-gray-text
+  {:color     colors/gray
+   :font-size 14})
+
+(def chat-profile-body
+  {:margin 32})
+
+(def chat-profile-row
+  {:flex-direction :row
+   :align-items    :center
+   :margin-bottom  10})
+
+(def chat-profile-icon-container
+  {:background-color (colors/alpha colors/blue 0.1)
+   :justify-content  :center
+   :align-items      :center
+   :border-radius    15
+   ;; not sure idiomatic reagent for multiple style values
+   ;; for margin
+   :width            22
+   :height           22
+   :margin-right     10})
+
+(defn chat-profile-icon [color]
+  {:tint-color color
+   :width      15
+   :height     15})
+
+(def chat-profile-contact-code
+  {:color         colors/gray
+   :margin-top    10
+   :margin-bottom 5})
