0a313ef525549d6873c2cd32002249413e156b25
Adam Babik <a.babik@designfortress.com>
2018-07-27 12:32:58 +0200

    Update status-go to 0.10.3
    Signed-off-by: Adam Babik <a.babik@designfortress.com>

--- src/status_im/utils/web3_provider.cljs
+++ src/status_im/utils/web3_provider.cljs
@@ -3,10 +3,13 @@
             [status-im.native-module.core :as status]
             [status-im.js-dependencies :as dependencies]))
 
-(defn make-web3 []
+;; This Web3 object will allow access to private RPC calls
+;; It should be only used for internal application needs and never provided to any
+;; 3rd parties (DApps, etc)
+(defn make-internal-web3 []
   (dependencies/Web3.
    #js {:sendAsync (fn [payload callback]
-                     (status/call-web3
+                     (status/call-web3-private
                       (.stringify js/JSON payload)
                       (fn [response]
                         (if (= "" response)
