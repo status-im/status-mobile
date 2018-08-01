0562015fa86394c96715db387dce5f066f80a47a
Vitaliy Vlasov <siphiuel@gmail.com>
2018-08-01 14:35:14 -0600

    Fix status display by providing public key in message data

--- src/status_im/ui/components/connectivity/view.cljs
+++ src/status_im/ui/components/connectivity/view.cljs
@@ -3,7 +3,6 @@
   (:require [re-frame.core :as re-frame]
             [reagent.core :as reagent]
             [status-im.ui.components.react :as react]
-            [taoensso.timbre :as log]
             [status-im.ui.components.connectivity.styles :as styles]
             [status-im.i18n :as i18n]))
 
