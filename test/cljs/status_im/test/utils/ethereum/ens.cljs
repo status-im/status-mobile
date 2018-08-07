1769bfb695c786520aa532ffff0b6f14efbbba25
Eric Dvorsak <eric@dvorsak.fr>
2018-08-07 13:27:19 +0200

    fix tests

--- test/cljs/status_im/test/utils/ethereum/ens.cljs
+++ test/cljs/status_im/test/utils/ethereum/ens.cljs
@@ -3,8 +3,8 @@
             [status-im.utils.ethereum.ens :as ens]))
 
 (deftest namehash
-  (is (= ens/default-namehash (ens/namehash nil)))
-  (is (= ens/default-namehash (ens/namehash "")))
+  (is (= (str "0x" ens/default-namehash) (ens/namehash nil)))
+  (is (= (str "0x" ens/default-namehash) (ens/namehash "")))
   (is (= "0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae"
          (ens/namehash "eth")))
   (is (= "0xde9b09fd7c5f901e23a3f19fecc54828e9c848539801e86591bd9801b019f84f"
