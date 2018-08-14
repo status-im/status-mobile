d673bb2ae8e725ed187535fae6d199148faf44d0
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2018-08-14 12:03:52 +0300

    Fixed problem with adding new contact on desktop

--- src/status_im/ui/screens/desktop/main/add_new/views.cljs
+++ src/status_im/ui/screens/desktop/main/add_new/views.cljs
@@ -15,7 +15,7 @@
 (views/defview new-contact []
   (views/letsubs [new-contact-identity [:get :contacts/new-identity]
                   contacts             [:all-added-people-contacts]
-                  chat-error           [:new-contact-error-message]
+                  chat-error           [:new-identity-error]
                   topic                [:get :public-group-topic]
                   topic-error          [:new-topic-error-message]
                   account              [:get-current-account]
