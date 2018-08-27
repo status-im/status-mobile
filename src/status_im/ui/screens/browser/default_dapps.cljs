888bd5c2d6b7750f4ef0ad8da05342622d9a5971
Julien Eluard <julien.eluard@gmail.com>
2018-08-27 09:15:49 +0200

    [# 5603] Updated bounties.network address
    Signed-off-by: Julien Eluard <julien.eluard@gmail.com>

--- src/status_im/ui/screens/browser/default_dapps.cljs
+++ src/status_im/ui/screens/browser/default_dapps.cljs
@@ -60,7 +60,7 @@
             :photo-path  "contacts://name-bazaar"
             :description "ENS name marketplace"}
            {:name        "The Bounties Network"
-            :dapp-url    "https://moksha.bounties.network/"
+            :dapp-url    "https://berlin.bounties.network/"
             :photo-path  "contacts://bounties-network"
             :description "Bounties on any task, paid in any token"}
            {:name        "Emoon"
