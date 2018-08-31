bdb8688af890715c9e7c9a94be1133c93ad27224
Dmitry Novotochinov <dmitry.novot@gmail.com>
2018-08-31 22:33:40 +0300

    cljfmt formatting fix
    Signed-off-by: Dmitry Novotochinov <dmitry.novot@gmail.com>

--- src/status_im/ui/screens/extensions/events.cljs
+++ src/status_im/ui/screens/extensions/events.cljs
@@ -1,7 +1,7 @@
 (ns status-im.ui.screens.extensions.events
   (:require [re-frame.core :as re-frame]
-            [pluto.registry :as registry] 
-            [status-im.utils.handlers :as handlers] 
+            [pluto.registry :as registry]
+            [status-im.utils.handlers :as handlers]
             status-im.ui.screens.extensions.add.events))
 
 (handlers/register-handler-db
