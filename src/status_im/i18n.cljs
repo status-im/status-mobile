bd3f9570b30462a60989b9a77c8f4d4514936191
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2018-09-07 12:36:33 +0300

    [#5649] Change react-native-i18n language files to js (Lokalise)
    move translations to JSON
    read translations from JSON
    remove old clojurescript translations files
    reload clojurescript on changes in translations folder

--- src/status_im/i18n.cljs
+++ src/status_im/i18n.cljs
@@ -1,132 +1,24 @@
 (ns status-im.i18n
+  (:require-macros [status-im.i18n :as i18n])
   (:require
    [cljs.spec.alpha :as spec]
    [status-im.react-native.js-dependencies :as rn-dependencies]
-   [status-im.translations.af :as af]
-   [status-im.translations.ar :as ar]
-   [status-im.translations.bel :as be]
-   [status-im.translations.cs :as cs]
-   [status-im.translations.da :as da]
-   [status-im.translations.de :as de]
-   [status-im.translations.de-ch :as de-ch]
-   [status-im.translations.el :as el]
-   [status-im.translations.en :as en]
-   [status-im.translations.es :as es]
-   [status-im.translations.es-ar :as es-ar]
-   [status-im.translations.es-mx :as es-mx]
-   [status-im.translations.fi :as fi]
-   [status-im.translations.fr :as fr]
-   [status-im.translations.fr-ch :as fr-ch]
-   [status-im.translations.fy :as fy]
-   [status-im.translations.he :as he]
-   [status-im.translations.hi :as hi]
-   [status-im.translations.hu :as hu]
-   [status-im.translations.id :as id]
-   [status-im.translations.it :as it]
-   [status-im.translations.it-ch :as it-ch]
-   [status-im.translations.ja :as ja]
-   [status-im.translations.ko :as ko]
-   [status-im.translations.la :as la]
-   [status-im.translations.lt :as lt]
-   [status-im.translations.lv :as lv]
-   [status-im.translations.ms :as ms]
-   [status-im.translations.nb :as nb]
-   [status-im.translations.ne :as ne]
-   [status-im.translations.nl :as nl]
-   [status-im.translations.pl :as pl]
-   [status-im.translations.pt-br :as pt-br]
-   [status-im.translations.pt-pt :as pt-pt]
-   [status-im.translations.ro :as ro]
-   [status-im.translations.ru :as ru]
-   [status-im.translations.sl :as sl]
-   [status-im.translations.sr-rs-cyrl :as sr-rs-cyrl]
-   [status-im.translations.sr-rs-latn :as sr-rs-latn]
-   [status-im.translations.sv :as sv]
-   [status-im.translations.sw :as sw]
-   [status-im.translations.th :as th]
-   [status-im.translations.tr :as tr]
-   [status-im.translations.uk :as uk]
-   [status-im.translations.ur :as ur]
-   [status-im.translations.vi :as vi]
-   [status-im.translations.zh-hans :as zh-hans]
-   [status-im.translations.zh-hant :as zh-hant]
-   [status-im.translations.zh-hant-tw :as zh-hant-tw]
-   [status-im.translations.zh-hant-sg :as zh-hant-sg]
-   [status-im.translations.zh-hant-hk :as zh-hant-hk]
-   [status-im.translations.zh-wuu :as zh-wuu]
-   [status-im.translations.zh-yue :as zh-yue]
    [status-im.utils.js-resources :refer [default-contacts]]
-   [taoensso.timbre :as log]
    [clojure.string :as string]
-   [clojure.set :as set]))
+   [clojure.set :as set]
+   [status-im.utils.types :as types]))
 
 (set! (.-fallbacks rn-dependencies/i18n) true)
 (set! (.-defaultSeparator rn-dependencies/i18n) "/")
 
 ;; translations
-#_(def translations-by-locale {:af          af/translations
-                               :ar          ar/translations
-                               :be          be/translations
-                               :cs          cs/translations
-                               :da          da/translations
-                               :de          de/translations
-                               :de-ch       de-ch/translations
-                               :el          el/translations
-                               :en          en/translations
-                               :es          es/translations
-                               :es-ar       es-ar/translations
-                               :es-mx       es-mx/translations
-                               :fi          fi/translations
-                               :fr          fr/translations
-                               :fr-ch       fr-ch/translations
-                               :fy          fy/translations
-                               :he          he/translations
-                               :hi          hi/translations
-                               :hu          hu/translations
-                               :id          id/translations
-                               :it          it/translations
-                               :it-ch       it-ch/translations
-                               :ja          ja/translations
-                               :ko          ko/translations
-                               :la          la/translations
-                               :lt          lt/translations
-                               :lv          lv/translations
-                               :ms          ms/translations
-                               :nb          nb/translations
-                               :ne          ne/translations
-                               :nl          nl/translations
-                               :pl          pl/translations
-                               :pt-br       pt-br/translations
-                               :pt-pt       pt-pt/translations
-                               :ro          ro/translations
-                               :ru          ru/translations
-                               :sl          sl/translations
-                               :sr          sr-rs-cyrl/translations
-                               :sr-RS_#Latn sr-rs-latn/translations
-                               :sr-RS_#Cyrl sr-rs-cyrl/translations
-                               :sv          sv/translations
-                               :sw          sw/translations
-                               :th          th/translations
-                               :tr          tr/translations
-                               :uk          uk/translations
-                               :ur          ur/translations
-                               :vi          vi/translations
-                               :zh          zh-hans/translations
-                               :zh-hans     zh-hans/translations
-                               :zh-hans-cn  zh-hans/translations
-                               :zh-hans-mo  zh-hans/translations
-                               :zh-hant     zh-hant/translations
-                               :zh-hant-tw  zh-hant-tw/translations
-                               :zh-hant-sg  zh-hant-sg/translations
-                               :zh-hant-hk  zh-hant-hk/translations
-                               :zh-hant-cn  zh-hant/translations
-                               :zh-hant-mo  zh-hant/translations
-                               :zh-wuu      zh-wuu/translations
-                               :zh-yue      zh-yue/translations})
-(def translations-by-locale {:en          en/translations}) ; Temporarily disable all languages except English
+(def translations-by-locale
+  (->> (i18n/translations [:en :ko :ru])
+       (map (fn [[k t]] [k (types/json->clj t)]))
+       (into {})))
 
 ;; english as source of truth
-(def labels (set (keys en/translations)))
+(def labels (set (keys (:en translations-by-locale))))
 
 (spec/def ::label labels)
 (spec/def ::labels (spec/coll-of ::label :kind set? :into #{}))
