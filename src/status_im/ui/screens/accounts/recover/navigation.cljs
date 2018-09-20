4d7373f8029a57288af150d2d82255ca3229ec48
yenda <eric@status.im>
2018-09-20 18:42:07 +0200

    fix account-recover screen errors
    when typing a wrong recovery phrase or password, navigating back and
    returning to account-recover screen, errors about recovery phrase and
    password were not cleared
    
    get rid of navigation/preload-data in the process as it is too clunky
    
    TODO: eventually use screen-params in the future for screen related data
    Signed-off-by: yenda <eric@status.im>

--- src/status_im/ui/screens/accounts/recover/navigation.cljs
+++
@@ -1,6 +0,0 @@
-(ns status-im.ui.screens.accounts.recover.navigation
-  (:require [status-im.ui.screens.navigation :as nav]))
-
-(defmethod nav/preload-data! :recover
-  [db]
-  (update db :accounts/recover dissoc :password :passphrase :processing?))
