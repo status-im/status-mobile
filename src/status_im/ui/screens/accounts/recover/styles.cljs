d823bfd71c92d97a8a1d6c5c1812ee9a533e704b
Goran Jovic <goranjovic@gmail.com>
2018-10-05 12:06:58 +0200

    bug #6135 - made place for recovery phrase error tooltip on desktop
    Signed-off-by: Goran Jovic <goranjovic@gmail.com>

--- src/status_im/ui/screens/accounts/recover/styles.cljs
+++ src/status_im/ui/screens/accounts/recover/styles.cljs
@@ -1,12 +1,14 @@
 (ns status-im.ui.screens.accounts.recover.styles
+  (:require-macros [status-im.utils.styles :refer [defstyle]])
   (:require [status-im.ui.components.colors :as colors]))
 
 (def screen-container
   {:flex             1
    :background-color colors/white})
 
-(def inputs-container
-  {:margin 16})
+(defstyle inputs-container
+  {:margin  16
+   :desktop {:padding-top 15}})
 
 (def password-input
   {:margin-top 10})
