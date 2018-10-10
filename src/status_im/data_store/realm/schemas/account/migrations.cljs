yaovicoder/status-react

Show raw payload for messages in dev mode, as an option.

src/status_im/data_store/realm/schemas/account/migrations.cljs
@@ -86,3 +86,6 @@
 
 (defn v13 [old-realm new-realm]
   (log/debug "migrating v13 account database"))
+
+(defn v14 [old-realm new-realm]
+  (log/debug "migrating v14 account database"))

status-im/status-mobile

src/status_im/data_store/realm/schemas/account/migrations.cljs
@@ -96,4 +96,7 @@
                   (aset message "content-type" "command")))))
 
 (defn v15 [old-realm new-realm]
-  (log/debug "migrating v13 account database"))
+  (log/debug "migrating v15 account database"))
+
+(defn v16 [old-realm new-realm]
+  (log/debug "migrating v16 account database"))