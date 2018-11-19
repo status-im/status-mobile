1ba7feb439415806d94bdeb29e621843a264a8e4
yenda <eric@status.im>
2018-11-19 12:23:56 +0100

    [refactor] move contact.subs and utils.contacts to contact.db
    functions defined in subs need to be reusable and testable independently
    from re-frame framework. they are moved into db namespace for that purpose
    
    Signed-off-by: yenda <eric@status.im>

--- src/status_im/ui/screens/profile/contact/views.cljs
+++ src/status_im/ui/screens/profile/contact/views.cljs
@@ -1,15 +1,15 @@
 (ns status-im.ui.screens.profile.contact.views
-  (:require-macros [status-im.utils.views :refer [defview letsubs]])
   (:require [re-frame.core :as re-frame]
+            [status-im.contact.db :as contact.db]
             [status-im.i18n :as i18n]
-            [status-im.utils.contacts :as utils.contacts]
-            [status-im.ui.components.react :as react]
             [status-im.ui.components.list.views :as list]
-            [status-im.ui.components.toolbar.view :as toolbar]
+            [status-im.ui.components.react :as react]
             [status-im.ui.components.status-bar.view :as status-bar]
-            [status-im.ui.screens.profile.contact.styles :as styles]
+            [status-im.ui.components.toolbar.view :as toolbar]
             [status-im.ui.screens.profile.components.styles :as profile.components.styles]
-            [status-im.ui.screens.profile.components.views :as profile.components]))
+            [status-im.ui.screens.profile.components.views :as profile.components]
+            [status-im.ui.screens.profile.contact.styles :as styles])
+  (:require-macros [status-im.utils.views :refer [defview letsubs]]))
 
 (defn profile-contact-toolbar []
   [toolbar/toolbar {}
@@ -64,7 +64,7 @@
 (defview profile []
   (letsubs [identity        [:get-current-contact-identity]
             maybe-contact   [:get-current-contact]]
-    (let [contact (or maybe-contact (utils.contacts/public-key->new-contact identity))]
+    (let [contact (or maybe-contact (contact.db/public-key->new-contact identity))]
       [react/view profile.components.styles/profile
        [status-bar/status-bar]
        [profile-contact-toolbar]
