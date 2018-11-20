ce3fe5721052c9f1406241f64c1c4b66615b7ade
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2018-11-20 18:13:21 +0200

    [slow sign in] faster get-unviewed-messages

--- src/status_im/data_store/messages.cljs
+++ src/status_im/data_store/messages.cljs
@@ -82,16 +82,16 @@
 
 (defn- get-unviewed-messages
   [public-key]
-  (into {}
-        (map (fn [[chat-id user-statuses]]
-               [chat-id (into #{} (map :message-id) user-statuses)]))
-        (group-by :chat-id
-                  (-> @core/account-realm
-                      (core/get-by-fields
-                       :user-status
-                       :and {:public-key public-key
-                             :status     "received"})
-                      (core/all-clj :user-status)))))
+  (-> @core/account-realm
+      (core/get-by-fields
+       :user-status
+       :and {:public-key public-key
+             :status     "received"})
+      (.reduce (fn [acc msg _ _]
+                 (let [chat-id (aget msg "chat-id")
+                       message-id (aget msg "message-id")]
+                   (update acc chat-id (fnil conj #{}) message-id)))
+               {})))
 
 (re-frame/reg-cofx
  :data-store/get-unviewed-messages
