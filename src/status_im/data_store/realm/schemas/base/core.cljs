schemar/status-react

feature #6509 - validating token config against their contracts; moved token info from compile time list to app-db; fixed any discrepancies in existing info

src/status_im/data_store/realm/schemas/base/core.cljs
@@ -52,6 +52,8 @@
           extension/v12
           account/v13])
 
+(def v14 v13)
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -91,4 +93,7 @@
                :migration     migrations/v12}
               {:schema        v13
                :schemaVersion 13
-               :migration     migrations/v13}])
+               :migration     migrations/v13}
+              {:schema        v14
+               :schemaVersion 14
+               :migration     migrations/v14}])

status-im/status-mobile

Signed-off-by: Goran Jovic <goranjovic@gmail.com>

src/status_im/data_store/realm/schemas/base/core.cljs
@@ -64,6 +64,8 @@
           extension/v12
           account/v15])
 
+(def v17 v16)
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -112,4 +114,7 @@
                :migration     migrations/v15}
               {:schema        v16
                :schemaVersion 16
-               :migration     migrations/v16}])
+               :migration     migrations/v16}
+              {:schema        v17
+               :schemaVersion 17
+               :migration     migrations/v17}])