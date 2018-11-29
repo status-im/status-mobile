d4ca0851d797e9ecc65d561f51dcd641234ee77f
Pedro Pombeiro <PombeirP@users.noreply.github.com>
2018-11-29 17:28:57 +0100

    Fix check for yarn
    Signed-off-by: Pedro Pombeiro <pombeirp@users.noreply.github.com>

--- scripts/run-pre-build-check.sh
+++ scripts/run-pre-build-check.sh
@@ -9,6 +9,11 @@ PLATFORM=""
 EXPECTED_NODE_VERSION="v9.3.0" # note the 'v' in front, that is how node does versioning
 EXPECTED_YARN_VERSION="1.12.3" # note the lack of 'v' in front. inconsistent. :(
 
+function program_exists() {
+  local program=$1
+  command -v "$program" >/dev/null 2>&1
+}
+
 #if no arguments passed, inform user about possible ones
 
 if [ $# -eq 0 ]; then
@@ -18,8 +23,14 @@ else
   PLATFORM=$1
 fi
 
-node_version=$(node -v)
+if ! program_exists node || ! program_exists yarn; then
+  echo -e "${YELLOW}********************************************************************************************"
+  echo -e "Please open another console to reload the environment, and then run make setup if necessary."
+  echo -e "********************************************************************************************${NC}"
+  exit 1
+fi
 
+node_version=$(node -v)
 if [[ $node_version != $EXPECTED_NODE_VERSION ]]; then
   echo -e "${YELLOW}+ node version $node_version is installed. node version $EXPECTED_NODE_VERSION is recommended.${NC}"
 fi
