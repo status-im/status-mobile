zhouribin/status-react

[refactor] contact model: remove unused fields and add `blocked?`
- dapp related fields are removed since it is not used anymore
- blocked? field is added for futur block user feature

src/status_im/data_store/realm/schemas/account/migrations.cljs
@@ -316,3 +316,6 @@
                 message-type (aget last-message "message-type")]
             (aset chat "last-message-content" content)
             (aset chat "last-message-type" message-type)))))))
+
+(defn v29 [old-realm new-realm]
+  (log/debug "migrating v29 account database"))

status-im/status-mobile

src/status_im/data_store/realm/schemas/account/migrations.cljs
@@ -339,3 +339,6 @@
                 content-type (aget last-message "content-type")]
             (aset chat "last-message-content" content)
             (aset chat "last-message-content-type" content-type)))))))
+
+(defn v33 [old-realm new-realm]
+  (log/debug "migrating v33 account database"))