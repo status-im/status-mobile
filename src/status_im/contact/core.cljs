386c678cafa108884daa806e1c61dc083042b407
Kris Calabio <kriscalabio@gmail.com>
2019-03-18 12:53:16 -0700

    After dismissing error, no redirect to home and new qr can be scanned.
    Signed-off-by: Pedro Pombeiro <pombeirp@users.noreply.github.com>

--- src/status_im/contact/core.cljs
+++ src/status_im/contact/core.cljs
@@ -271,7 +271,7 @@
     (if (some? validation-result)
       {:utils/show-popup {:title (i18n/label :t/unable-to-read-this-code)
                           :content validation-result
-                          :on-dismiss #(re-frame/dispatch [:navigate-to-clean :home])}}
+                          :on-dismiss #(re-frame/dispatch [:qr-scanner.ui/qr-code-error-dismissed])}}
       (fx/merge cofx
                 fx
                 (if config/partitioned-topic-enabled?
