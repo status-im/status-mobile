21f30bea13d02e16a6e3d814529ca959f7bd72da
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-04-05 13:35:51 +0300

    attempt to fix [#7897]

--- src/status_im/ui/screens/routing/modals.cljs
+++ src/status_im/ui/screens/routing/modals.cljs
@@ -3,12 +3,14 @@
 (def modal-screens
   [{:name    :wallet-send-modal-stack
     :screens [:wallet-send-transaction-modal
+              :enter-pin
               :wallet-transaction-sent-modal
               :wallet-transaction-fee]
     :config  {:initialRouteName :wallet-send-transaction-modal}}
    {:name    :wallet-send-modal-stack-with-onboarding
     :screens [:wallet-onboarding-setup-modal
               :wallet-send-transaction-modal
+              :enter-pin
               :wallet-transaction-sent-modal
               :wallet-transaction-fee]
     :config  {:initialRouteName :wallet-onboarding-setup-modal}}
