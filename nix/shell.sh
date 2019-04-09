7b309d48751b0de2e91132b0fe890759cf8c0406
Jakub Sokołowski <jakub@status.im>
2019-04-09 17:14:32 +0200

    prevent nix setup running multiple times
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/shell.sh
+++ nix/shell.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
+#
+# This script is used by the Makefile to have an implicit nix-shell.
+#
+
 GREEN='\033[0;32m'
 NC='\033[0m'
 
 export TERM=xterm # fix for colors
 shift # we remove the first -c from arguments
+
 if ! command -v "nix" >/dev/null 2>&1; then
-  echo -e "${GREEN}Setting up environment...${NC}"
-  ./scripts/setup
+  if [ -f ~/.nix-profile/etc/profile.d/nix.sh ]; then
+    . ~/.nix-profile/etc/profile.d/nix.sh
+  else
+    echo -e "${GREEN}Setting up environment...${NC}"
+    ./scripts/setup
+  fi
 fi
-if command -v "nix" >/dev/null 2>&1 || [ -f ~/.nix-profile/etc/profile.d/nix.sh ]; then
+
+if command -v "nix" >/dev/null 2>&1; then
   echo -e "${GREEN}Configuring Nix shell...${NC}";
   if [[ $@ == "ENTER_NIX_SHELL" ]]; then
-    . ~/.nix-profile/etc/profile.d/nix.sh && exec nix-shell
+    exec nix-shell
   else
-    . ~/.nix-profile/etc/profile.d/nix.sh && exec nix-shell --run "$@"
+    exec nix-shell --run "$@"
   fi
 fi
