2fc0fef9adc2701b1b354e8738460af927a6de47
Siddharth Verma <sid@nuofox.com>
2019-04-10 20:33:18 +0530

    Link change

--- src/status_im/ui/screens/browser/default_dapps.cljs
+++ src/status_im/ui/screens/browser/default_dapps.cljs
@@ -74,7 +74,7 @@
              :photo-path  "contacts://superrare"
              :description "Buy, sell and collect unique digital creations by artists around the world"}
             {:name        "Nuo"
-             :dapp-url    "https://nuo.network/?utm_source=wallet&utm_medium=status"
+             :dapp-url    "https://app.nuo.network/lend/?utm_source=wallet&utm_medium=status"
              :photo-path  "contacts://nuo"
              :description "The non-custodial way to lend, borrow or margin trade cryptocurrency."}]}
    {:title (i18n/label :t/default-dapps-fun-games)
