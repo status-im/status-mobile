187464021af1cfc75ee59564fc8806893a6bb36b
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-04-15 15:00:08 +0300

    fix tests

--- test/cljs/status_im/test/transport/core.cljs
+++ test/cljs/status_im/test/transport/core.cljs
@@ -17,8 +17,8 @@
     (testing "it adds the discover filters"
       (is (= {:web3 nil :private-key-id "1" :topics '({:topic   "topic-4"
                                                        :chat-id "4"}
-                                                      {:topic   "0x2af2e6e7"
-                                                       :chat-id :discovery-topic}
+                                                      #_{:topic   "0x2af2e6e7"
+                                                         :chat-id :discovery-topic}
                                                       {:topic   "0xf8946aac"
                                                        :chat-id :discovery-topic})}
              (:shh/add-discovery-filters (transport/init-whisper cofx)))))
