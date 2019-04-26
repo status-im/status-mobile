e252b3d30d189b98e11a1e8608b99c7c9911bb20
Julien Eluard <julien.eluard@gmail.com>
2019-04-26 09:36:18 +0200

    Revert "After dismissing error, no redirect to home and new qr can be"
    This reverts commit 386c678cafa108884daa806e1c61dc083042b407.

--- src/status_im/ui/screens/qr_scanner/views.cljs
+++ src/status_im/ui/screens/qr_scanner/views.cljs
@@ -24,26 +24,26 @@
   (re-frame/dispatch [:qr-scanner.callback/scan-qr-code-success identifier (camera/get-qr-code-data data)]))
 
 (defview qr-scanner []
-  (letsubs [{identifier :current-qr-context
-             barcode-read-sub? :barcode-read?} [:get-screen-params]
-            camera-initialized? (reagent/atom false)]
-    (let [barcode-read? barcode-read-sub?]
-      [react/view styles/barcode-scanner-container
-       [qr-scanner-toolbar (or (:toolbar-title identifier) (i18n/label :t/scan-qr)) identifier]
-       [camera/camera {:onBarCodeRead #(if (:multiple? identifier)
-                                         (on-barcode-read identifier %)
-                                         (when-not barcode-read?
-                                           (on-barcode-read identifier %)))
-                       :ref           #(reset! camera-initialized? true)
-                       :captureAudio  false
-                       :style         styles/barcode-scanner}]
-       [react/view styles/rectangle-container
-        [react/view styles/rectangle
-         [react/image {:source {:uri :corner_left_top}
-                       :style  styles/corner-left-top}]
-         [react/image {:source {:uri :corner_right_top}
-                       :style  styles/corner-right-top}]
-         [react/image {:source {:uri :corner_right_bottom}
-                       :style  styles/corner-right-bottom}]
-         [react/image {:source {:uri :corner_left_bottom}
-                       :style  styles/corner-left-bottom}]]]])))
+  (letsubs [{identifier :current-qr-context} [:get-screen-params]
+            camera-initialized? (reagent/atom false)
+            barcode-read? (reagent/atom false)]
+    [react/view styles/barcode-scanner-container
+     [qr-scanner-toolbar (or (:toolbar-title identifier) (i18n/label :t/scan-qr)) identifier]
+     [camera/camera {:onBarCodeRead #(if (:multiple? identifier)
+                                       (on-barcode-read identifier %)
+                                       (when-not @barcode-read?
+                                         (do (reset! barcode-read? true)
+                                             (on-barcode-read identifier %))))
+                     :ref           #(reset! camera-initialized? true)
+                     :captureAudio  false
+                     :style         styles/barcode-scanner}]
+     [react/view styles/rectangle-container
+      [react/view styles/rectangle
+       [react/image {:source {:uri :corner_left_top}
+                     :style  styles/corner-left-top}]
+       [react/image {:source {:uri :corner_right_top}
+                     :style  styles/corner-right-top}]
+       [react/image {:source {:uri :corner_right_bottom}
+                     :style  styles/corner-right-bottom}]
+       [react/image {:source {:uri :corner_left_bottom}
+                     :style  styles/corner-left-bottom}]]]]))
