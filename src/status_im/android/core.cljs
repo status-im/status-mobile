15f6003def0a71591fc8691122e992a854fb5e4d
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-05-14 09:19:52 +0300

    [experiment] lighter entry point

--- src/status_im/android/core.cljs
+++ src/status_im/android/core.cljs
@@ -1,60 +1,33 @@
 (ns status-im.android.core
-  (:require [reagent.core :as reagent]
-            [re-frame.core :refer [subscribe dispatch dispatch-sync]]
-            status-im.utils.db
-            status-im.ui.screens.db
-            status-im.ui.screens.events
-            status-im.subs
-            status-im.data-store.core
-            [status-im.ui.screens.views :as views]
-            [status-im.ui.components.react :as react]
-            [status-im.native-module.core :as status]
-            [status-im.notifications.core :as notifications]
-            [status-im.core :as core]
-            [status-im.react-native.js-dependencies :as rn-dependencies]
-            [status-im.utils.snoopy :as snoopy]
-            [taoensso.timbre :as log]))
+  (:require [reagent.core :as reagent]))
 
-(defn app-state-change-handler [state]
-  (dispatch [:app-state-change state]))
+(def react-native (js/require "react-native"))
+(def view (reagent/adapt-react-class (.-View react-native)))
+(def text (reagent/adapt-react-class (.-Text react-native)))
 
-(defn on-languages-change [event]
-  (set! (.-locale rn-dependencies/i18n) (.-language event)))
+(def native-modules (.-NativeModules react-native))
+(def splash-screen (.-SplashScreen native-modules))
+
+(defn main []
+  (reagent/create-class
+   {:reagent-render
+    (fn []
+      [view
+       {:width            100
+        :height           100
+        :background-color :green}
+       [text
+        "Empty init screen"]])}))
 
 (defn app-root [props]
-  (let [keyboard-height (subscribe [:keyboard-height])]
-    (reagent/create-class
-     {:component-will-mount
-      (fn []
-        (.addListener react/keyboard
-                      "keyboardDidShow"
-                      (fn [e]
-                        (let [h (.. e -endCoordinates -height)]
-                          (dispatch [:hide-tab-bar])
-                          (when-not (= h @keyboard-height)
-                            (dispatch [:set :keyboard-height h])
-                            (dispatch [:set :keyboard-max-height h])))))
-        (.addListener react/keyboard
-                      "keyboardDidHide"
-                      (fn [_]
-                        (dispatch [:show-tab-bar])
-                        (when-not (zero? @keyboard-height)
-                          (dispatch [:set :keyboard-height 0]))))
-        (.hide react/splash-screen)
-        (.addEventListener react/app-state "change" app-state-change-handler)
-        (.addEventListener rn-dependencies/react-native-languages "change" on-languages-change))
-      :component-did-mount
-      (fn [this]
-        (dispatch [:set-initial-props (reagent/props this)]))
-      :component-will-unmount
-      (fn []
-        (.stop react/http-bridge)
-        (.removeEventListener react/app-state "change" app-state-change-handler)
-        (.removeEventListener rn-dependencies/react-native-languages "change" on-languages-change))
-      :display-name "root"
-      :reagent-render views/main})))
+  (reagent/create-class
+   {:component-will-mount
+                    (fn []
+                      (.hide splash-screen))
+    :display-name   "root"
+    :reagent-render main}))
 
 (defn init []
-  (status/set-soft-input-mode status/adjust-resize)
-  (core/init app-root)
-  (snoopy/subscribe!))
+  (.registerComponent (.-AppRegistry react-native)
+                      "StatusIm"
+                      #(reagent/reactify-component app-root)))
