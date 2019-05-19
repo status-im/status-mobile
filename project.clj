25b185d595b94a6d3db3e553685fe09a55db51fa
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-05-19 22:06:04 +0300

    _

--- project.clj
+++ project.clj
@@ -117,7 +117,6 @@
                                                          :output-dir         "target/android-prod"
                                                          :static-fns         true
                                                          :optimize-constants true
-                                                         :pretty-print       true
                                                          :optimizations      :advanced
                                                          :infer-externs      true
                                                          :externs            ["ex.js"]
