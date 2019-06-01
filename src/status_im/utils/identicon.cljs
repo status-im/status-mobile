f63f6e271bb16a1c9ffe1e7ca7e5f648e0d1e18d
yenda <eric@status.im>
2019-06-01 15:07:54 +0200

    [fix] remove unused libraries

--- src/status_im/utils/identicon.cljs
+++ src/status_im/utils/identicon.cljs
@@ -4,10 +4,18 @@
 (def default-size 40)
 
 (defn identicon
-  ([hash] (identicon hash (clj->js {:background [255 255 255 255]
+  ([hash] (identicon hash (clj->js {:background [255 255 255]
+                                    :saturation 0.5
+                                    :brightness 0.7
                                     :margin     0.24
                                     :size       default-size})))
   ([hash options]
-   (str "data:image/png;base64,"
-        (let [identicon-js (dependencies/identicon-js)]
-          (str (new identicon-js hash options))))))
+   (when hash
+     (try
+       (str "data:image/png;base64,"
+            (let [identicon-js (dependencies/identicon-js)]
+              (str (new identicon-js hash options))))
+       ;; the identicon-js lib requires a hash with a length of at least 14
+       ;; since we only run this when there is a hash, it only happens with
+       ;; dummy values during testing
+       (catch :default e)))))
