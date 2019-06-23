ae52fd07f4175470dc581e386ea28b743bf65aaf
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-06-23 17:51:32 +0300

    add back tooltip-triangle icon

--- src/status_im/ui/components/tooltip/views.cljs
+++ src/status_im/ui/components/tooltip/views.cljs
@@ -17,7 +17,9 @@
      [react/animated-view {:style (styles/tooltip-animated bottom-anim-value opacity-value)}
       [react/view (styles/tooltip-text-container color)
        [react/text {:style (styles/tooltip-text font-size)} label]]
-      [vector-icons/icon :icons/tooltip-triangle {:color color :style styles/tooltip-triangle}]]]))
+      [vector-icons/icon :icons/tooltip-triangle (assoc
+                                                  styles/tooltip-triangle
+                                                  :color color)]]]))
 
 (views/defview bottom-tooltip-info [label on-close]
   (views/letsubs [bottom-anim-value (animation/create-value -150)
@@ -25,9 +27,10 @@
     {:component-did-mount (animations/animate-tooltip -150 bottom-anim-value opacity-value -10)}
     [react/view styles/bottom-tooltip-container
      [react/animated-view {:style (styles/tooltip-animated bottom-anim-value opacity-value)}
-      [vector-icons/icon :icons/tooltip-triangle {:color           colors/gray
-                                                  :style           styles/tooltip-triangle
-                                                  :container-style {:transform [{:rotate "180deg"}]}}]
+      [vector-icons/icon :icons/tooltip-triangle (assoc
+                                                  styles/tooltip-triangle
+                                                  :color colors/gray
+                                                  :container-style {:transform [{:rotate "180deg"}]})]
       [react/view styles/bottom-tooltip-text-container
        [react/text {:style styles/bottom-tooltip-text} label]
        [react/touchable-highlight {:on-press on-close
