cd975575916b34611230ea6e399567f417f20501
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-06-24 08:40:55 +0300

    remove message options animation (dead code)

--- src/status_im/ui/screens/chat/bottom_info.cljs
+++ src/status_im/ui/screens/chat/bottom_info.cljs
@@ -1,16 +1,6 @@
 (ns status-im.ui.screens.chat.bottom-info
-  (:require [reagent.core :as reagent]
-            [status-im.ui.components.animation :as anim]
-            [status-im.ui.components.react :as react]
+  (:require [status-im.ui.components.react :as react]
             [status-im.ui.screens.chat.styles.main :as styles]))
-
-(defn- container-animation-logic
-  [{:keys [to-value val]}]
-  (fn [_]
-    (anim/start
-     (anim/spring val {:toValue  to-value
-                       :friction 6
-                       :tension  40}))))
 
 (defn overlay
   [{:keys [on-click-outside]} items]
@@ -21,16 +11,6 @@
    items])
 
 (defn container
-  [height & _]
-  (let [anim-value    (anim/create-value 1)
-        context       {:to-value height
-                       :val      anim-value}
-        on-update     (container-animation-logic context)]
-    (reagent/create-class
-     {:component-did-update
-      on-update
-      :display-name "container"
-      :reagent-render
-      (fn [height & children]
-        [react/animated-view {:style (styles/bottom-info-container height)}
-         (into [react/view] children)])})))
+  [height & children]
+  [react/view {:style (styles/bottom-info-container height)}
+   (into [react/view] children)])
