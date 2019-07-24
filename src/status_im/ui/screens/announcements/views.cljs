0aea1ad9b23b5652520da6610c593d3ef2627d48
Andrey Shovkoplyas <motor4ik@gmail.com>
2019-07-24 15:43:18 +0200

    [#8629] Fixes to v1 warning

--- src/status_im/ui/screens/announcements/views.cljs
+++ src/status_im/ui/screens/announcements/views.cljs
@@ -62,33 +62,28 @@
                                             :height 19}]]]]]]))))
 
 (defn public-launch-announcement []
-  [react/view {:flex-grow 1 :flex-direction :column}
+  [react/view {:flex 1}
    [status-bar/status-bar {:type :modal-white}]
-   [react/scroll-view {:content-container-style {:align-items :stretch}}
-    [react/view {:justify-content :space-between
-                 :flex-direction  :row}
-     [react/touchable-highlight
-      {:on-press            #(re-frame/dispatch [:navigate-back])
-       :style               {:width           56
-                             :height          56
-                             :justify-content :center
-                             :align-items     :center}
-       :accessibility-label :done-button}
-      [react/view
-       [icons/icon :main-icons/close {}]]]
-     [react/view {:flex            1
-                  :justify-content :center
-                  :align-items     :center
-                  :margin-right    56}
-      [react/image {:source (:large-badge resources/ui)
-                    :style  {:margin-top 4}}]]]
-    [react/text {:style {:margin-top    12
+   [react/scroll-view
+    #_[react/view {:justify-content :space-between
+                   :flex-direction  :row}
+       #_[react/touchable-highlight {:on-press            #(re-frame/dispatch [:navigate-back])
+                                     :style               {:width           56
+                                                           :height          56
+                                                           :justify-content :center
+                                                           :align-items     :center}
+                                     :accessibility-label :done-button}
+          [icons/icon :main-icons/close {}]]]
+    [react/view {:justify-content :center
+                 :align-items     :center}
+     [react/image {:source (:large-badge resources/ui)
+                   :style  {:margin-top 4}}]]
+    [react/text {:style {:margin-top    0
                          :margin-bottom 8
                          :typography    :header
                          :text-align    :center}}
      (i18n/label :t/status-launch-modal-title)]
     [react/text {:style {:margin-top        8
-                         :margin-bottom     16
                          ;; :color colors/gray
                          :line-height       21
                          :font-size         15
@@ -97,14 +92,14 @@
      (i18n/label :t/status-launch-modal-text)]
     [react/text {:style {:color             colors/gray
                          :margin-horizontal 16
-                         :margin-top        14
+                         :margin-top        22
                          :margin-bottom     6
                          :font-size         15
                          :text-align        :left}}
      (i18n/label :t/status-launch-modal-h2-what-new)]
     [react/view {:flex-direction    :row
                  :margin-horizontal 16
-                 :margin-vertical   6
+                 :margin-top   6
                  :align-items       :center}
      [react/text {:style {:font-size 20}}
       "🔑"]
@@ -113,7 +108,7 @@
       (i18n/label :t/status-launch-modal-new-key)]]
     [react/view {:flex-direction    :row
                  :margin-horizontal 16
-                 :margin-vertical   6
+                 :margin-top 8
                  :align-items       :center}
      [react/text {:style {:font-size 20}}
       "🗂"]
@@ -122,7 +117,7 @@
       (i18n/label :t/status-launch-modal-multi-account)]]
     [react/view {:flex-direction    :row
                  :margin-horizontal 16
-                 :margin-vertical   6
+                 :margin-top 8
                  :align-items       :center}
      [react/text {:style {:font-size 20}}
       "💳"]
@@ -131,7 +126,7 @@
       (i18n/label :t/status-launch-modal-keycard)]]
     [react/view {:flex-direction    :row
                  :margin-horizontal 16
-                 :margin-vertical   6
+                 :margin-top 8
                  :align-items       :center}
      [react/text {:style {:font-size 20}}
       "💬"]
@@ -140,7 +135,7 @@
       (i18n/label :t/status-launch-modal-ens-in-chat)]]
     [react/view {:flex-direction    :row
                  :margin-horizontal 16
-                 :margin-vertical   6
+                 :margin-top 8
                  :align-items       :center}
      [react/text {:style {:font-size 20}}
       "🏇"]
@@ -150,7 +145,6 @@
     [react/text {:style {:color             colors/gray
                          :margin-horizontal 16
                          :margin-top        6
-                         :margin-bottom     24
                          :font-size         15
                          :text-align        :left}}
      (i18n/label :t/status-launch-modal-much-more)]
@@ -158,6 +152,7 @@
                  :border-top-width    1
                  :padding-horizontal  16
                  :padding-top         10
+                 :margin-top 12
                  :padding-bottom      12
                  :margin-bottom       24
                  :border-top-color    colors/gray-light
@@ -176,7 +171,9 @@
       (i18n/label :t/status-launch-modal-before-you-update-text3)]
      [react/text {:style {:padding-top 10
                           :color colors/blue}
-                  :on-press #(println "TODO link to Learn more")}
+                  :on-press #(do
+                               (re-frame/dispatch [:navigate-back])
+                               (re-frame/dispatch [:browser.ui/open-url "https://v1-changes-explained.status.im"]))}
       (i18n/label :t/status-launch-modal-before-you-update-learn-more)]]
     [react/text {:style {:color             colors/gray
                          :margin-horizontal 16
@@ -185,20 +182,21 @@
                          :text-align        :left}}
      (i18n/label :t/status-launch-modal-when-title)]
     [react/nested-text {:style {:line-height 22
-                                :margin-bottom 16
-                                :padding-bottom 76
                                 :margin-horizontal 16}}
      (i18n/label :t/status-launch-modal-when-text1)
-     [{:style {:color colors/blue}}
+     [{:style {:color colors/blue} :on-press #(do
+                                                (re-frame/dispatch [:navigate-back])
+                                                (re-frame/dispatch [:chat.ui/start-public-chat "status"]))}
       (i18n/label :t/status-launch-modal-when-text2)]
      (i18n/label :t/status-launch-modal-when-text3)
-     [{:style {:color colors/blue}}
-      (i18n/label :t/status-launch-modal-when-text4)]]]
-   [react/view {:align-items :center :background-color colors/white
-                :position :absolute :left 0 :right 0 :bottom 0}
-    [components.common/button {:button-style {:margin-vertical    16
-                                              :padding-horizontal 32
-                                              :justify-content    :center
-                                              :align-items        :center}
-                               :on-press     #(re-frame/dispatch [:navigate-back])
-                               :label        (i18n/label :t/got-it)}]]])
+     [{:style {:color colors/blue} :on-press #(do
+                                                (re-frame/dispatch [:navigate-back])
+                                                (re-frame/dispatch [:browser.ui/open-url "https://twitter.com/ethstatus"]))}
+      (i18n/label :t/status-launch-modal-when-text4)]]
+    [react/view {:align-items :center}
+     [components.common/button {:button-style {:margin-vertical    30
+                                               :padding-horizontal 32
+                                               :justify-content    :center
+                                               :align-items        :center}
+                                :on-press     #(re-frame/dispatch [:navigate-back])
+                                :label        (i18n/label :t/got-it)}]]]])
