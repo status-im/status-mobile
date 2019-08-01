35a665b21eb86f0ade3012fda3b469f63ff41f89
Andrey Shovkoplyas <motor4ik@gmail.com>
2019-08-01 12:34:09 +0200

    commit 3 remove status module

--- modules/react-native-status/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-import { NativeModules } from 'react-native';
-module.exports = NativeModules.Status;
--- modules/react-native-status/ios/RCTStatus/RCTStatus.h
+++
@@ -1,9 +0,0 @@
-#import <sys/utsname.h>
-#import <Foundation/Foundation.h>
-#import <React/RCTBridgeModule.h>
-#import "Statusgo/Statusgo.h"
-#import "RCTLog.h"
-
-@interface Status : NSObject <RCTBridgeModule, StatusgoSignalHandler>
-- (void)handleSignal:(NSString *)signal;
-@end
