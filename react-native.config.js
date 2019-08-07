b16b25a5ffcc74549a9dc57618eaa740b60a04a3
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-08-07 11:27:59 +0200

    Disable RN autolinking for reported modules

---
+++ react-native.config.js
@@ -0,0 +1,136 @@
+module.exports = {
+    dependencies: {
+        'react-native-background-timer': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-camera': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-config': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-dialogs': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-fs': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-gesture-handler': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-http-bridge': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-image-crop-picker': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-image-resizer': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-keychain': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-languages': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-screens': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-securerandom': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-shake': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-splash-screen': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-status-keycard': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-svg': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-touch-id': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-webview': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-webview-bridge': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'react-native-mail': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+        'realm': {
+            platforms: {
+                android: null,
+                // ios: null,
+            },
+        },
+    },
+};
