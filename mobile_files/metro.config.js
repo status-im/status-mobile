6332e6f265f8b2bc2a6585c5a583334fd1d638b3
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-08-27 10:45:07 +0200

    Remove react-native-svg, react-native-svg-transformer and react-native-webview

--- mobile_files/metro.config.js
+++ mobile_files/metro.config.js
@@ -1,22 +1,17 @@
-const { getDefaultConfig } = require("metro-config");
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: false,
-            },
-        }),
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
-  };
-})();
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
