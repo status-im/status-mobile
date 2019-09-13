4f7cb549d9930bbef89be0c82a4bfac01206e4e8
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-09-14 00:01:39 +0200

    Upgrade from hermesvm@0.1.1 to hermes-engine@0.2.1
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/mobile/android/maven-and-npm-deps/default.nix
+++ nix/mobile/android/maven-and-npm-deps/default.nix
@@ -149,13 +149,9 @@ let
               'nodeExecutableAndArgs: ["${nodejs}/bin/node"'
 
           # Fix bugs in Hermes usage (https://github.com/facebook/react-native/issues/25601#issuecomment-510856047)
-          # - Make script always include release Hermes VM, as the debug version is currently way too slow in npm package <= 0.1.1 (https://github.com/facebook/hermes/issues/73)
           # - Make PR builds also count as release builds
           # - Fix issue where hermes command is being called with same input/output file
           substituteInPlace ${projectBuildDir}/node_modules/react-native/react.gradle \
-            --replace \
-              'def isRelease = targetName.toLowerCase().contains("release")' \
-              'def isRelease = true' \
             --replace \
               'targetName.toLowerCase().contains("release")' \
               '!targetName.toLowerCase().contains("debug")' \
@@ -170,6 +166,9 @@ let
               commandLine(getHermesCommand(), "-emit-binary", "-out", jsBundleFile, jsBundleFileIn, *hermesFlags)
               ' \
             || exit
+
+          # Remove legacy hermesvm folder (can be removed once we upgrade RN to a version which depends on newer hermes-engine package instead of hermesvm)
+          rm -r ${projectBuildDir}/node_modules/hermesvm
 
           # Patch dependencies which are not yet ported to AndroidX
           npx jetify
