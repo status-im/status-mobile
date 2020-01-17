99b4f654c5936baf5fbb2c2f1c55741469e9a299
Gheorghe Pinzaru <feross95@gmail.com>
2020-01-17 18:47:44 +0300

    Add absolute border to hide iOs black borders

--- src/status_im/ui/components/video.cljs
+++ src/status_im/ui/components/video.cljs
@@ -1,7 +1,19 @@
 (ns status-im.ui.components.video
   (:require [reagent.core :as reagent]
+            [status-im.ui.components.react :as react]
             [goog.object :as object]))
 
 (def rn-video (js/require "react-native-video"))
 
-(def video (reagent/adapt-react-class (object/get rn-video "default")))
+(def video-player (reagent/adapt-react-class (object/get rn-video "default")))
+
+(defn video [props]
+  [react/view {:style (:style props)}
+   [video-player props]
+   [react/view {:style {:position     :absolute
+                        :top          0
+                        :bottom       0
+                        :left         0
+                        :right        0
+                        :border-color :white
+                        :border-width 2}}]])
