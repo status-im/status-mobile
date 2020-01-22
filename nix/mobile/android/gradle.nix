c9e7d811edc3c00b7c86c721dfdd1dff4613d86e
Jakub Sokołowski <jakub@status.im>
2020-01-22 15:08:59 +0100

    nix: fix Gradle download links by using https
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/mobile/android/gradle.nix
+++ nix/mobile/android/gradle.nix
@@ -9,7 +9,7 @@ gradleGen.gradleGen rec {
   nativeVersion = "0.14";
 
   src = fetchurl {
-    url = "http://services.gradle.org/distributions/${name}-bin.zip";
+    url = "https://services.gradle.org/distributions/${name}-bin.zip";
     sha256 = "16671jp5wdr3q6p91h6szkgcxg3mw9wpgp6hjygbimy50lv34ls9";
   };
 }
