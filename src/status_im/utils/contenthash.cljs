8e489a63f97c698c8364f7254b8dbd5705fe2136
Andrey Shovkoplyas <motor4ik@gmail.com>
2020-02-10 17:15:59 +0100

    back to infura
    Signed-off-by: Andrey Shovkoplyas <motor4ik@gmail.com>

--- src/status_im/utils/contenthash.cljs
+++ src/status_im/utils/contenthash.cljs
@@ -58,7 +58,7 @@
 (defn url [hex]
   (let [{:keys [namespace hash]} (decode (ethereum/normalized-hex hex))]
     (case namespace
-      :ipfs (str "https://cloudflare-ipfs.com/ipfs/" hash)
+      :ipfs (str "https://ipfs.infura.io/ipfs/" hash)
       "")))
 
 (fx/defn cat
