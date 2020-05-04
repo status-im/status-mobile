3116b04a345abe79f6026a41b6676d99f874864d
Jakub Sokołowski <jakub@status.im>
2020-05-04 18:32:47 +0200

    nix: fix installing Nix
    we didn't source profile after running nix/scripts/setup.sh
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/scripts/source.sh
+++ nix/scripts/source.sh
@@ -4,17 +4,24 @@
 
 GIT_ROOT=$(cd "${BASH_SOURCE%/*}" && git rev-parse --show-toplevel)
 
+# Location of profile script for Nix that adjusts PATH
+NIX_PROFILE_SH="${HOME}/.nix-profile/etc/profile.d/nix.sh"
+
 function source_nix() {
   # Just stop if Nix is already available
   if [[ -x $(command -v nix) ]]; then
     return
-  elif [[ -f "${HOME}/.nix-profile/etc/profile.d/nix.sh" ]]; then
+  elif [[ -f "${NIX_PROFILE_SH}" ]]; then
     # Load Nix profile if it exists
-    source "${HOME}/.nix-profile/etc/profile.d/nix.sh"
+    source "${NIX_PROFILE_SH}"
+    return
   else
     # Setup Nix if not available
     ${GIT_ROOT}/nix/scripts/setup.sh
   fi
+
+  # Load Nix profile
+  source "${NIX_PROFILE_SH}"
 
   # Verify Nix is available
   if [[ ! -x $(command -v nix) ]]; then
