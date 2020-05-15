64b8b9f1704d9f10704501193f8100c0fcc531d0
Jakub Sokołowski <jakub@status.im>
2020-05-15 12:19:07 +0200

    nix: add support for more build inputs in mergeSh
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/lib/mergeSh.nix
+++ nix/lib/mergeSh.nix
@@ -1,9 +1,20 @@
 # this is an utility for mergeing shells created with mkShell
-# TODO: make this an attribute of mkShell result set.
+# TODO: make this an attribute of mkShell result set. Or just use inputsFrom.
 { lib }:
 
-super: shells: super.overrideAttrs(super: with lib; {
-  inputsFrom = (super.inputsFrom or []) ++ unique (catAttrs "inputsFrom" shells);
-  buildInputs = (super.buildInputs or []) ++ unique (catAttrs "buildInputs" shells);
-  shellHook = (super.shellHook or "") + concatStrings (catAttrs "shellHook" shells);
-})
+super: shells:
+super.overrideAttrs (super:
+  let
+    inherit (lib) attrByPath unique catAttrs concatStrings;
+
+    mergeAttrsFor = attrName:
+      (attrByPath [attrName] [] super) ++ unique (catAttrs attrName shells);
+  in {
+    inputsFrom = mergeAttrsFor "inputsFrom";
+    buildInputs = mergeAttrsFor "buildInputs";
+    nativeBuildInputs = mergeAttrsFor "nativeBuildInputs";
+    propagatedBuildInputs = mergeAttrsFor "propagatedBuildInputs";
+    propagatedNativeBuildInputs = mergeAttrsFor "propagatedNativeBuildInputs";
+    # shellHook is a string, not a list
+    shellHook = (super.shellHook or "") + concatStrings (catAttrs "shellHook" shells);
+  })
