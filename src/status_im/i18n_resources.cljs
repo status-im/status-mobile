46ebda8eeb2d490494daef8a91c85b0412123e75
jinhojang6 <jinho5051@gmail.com>
2020-06-01 23:08:42 +0900

    Update translations
    Signed-off-by: Andrey Shovkoplyas <motor4ik@gmail.com>

--- src/status_im/i18n_resources.cljs
+++ src/status_im/i18n_resources.cljs
@@ -9,7 +9,7 @@
 
 ;; translations
 (def translations-by-locale
-  (->> (i18n/translations [:ar :en :es :es_419 :fil :fr :it :ko :ru :in :id :tr :zh :zh_Hant :zh_TW])
+  (->> (i18n/translations [:ar :de :en :es :es_419 :fil :fr :id :in :it :ko :pt_BR :ru :tr :zh :zh_Hant :zh_TW])
        (map (fn [[k t]]
               (let [k' (-> (name k)
                            (string/replace "_" "-")
