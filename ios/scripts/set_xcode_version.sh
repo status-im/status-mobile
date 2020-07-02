b95056f3cea850f3cca01198c4e749d0fabf7bc8
Jakub Sokołowski <jakub@status.im>
2020-07-02 19:04:35 +0200

    ci: fix what version is set for iOS PR builds
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- ios/scripts/set_xcode_version.sh
+++ ios/scripts/set_xcode_version.sh
@@ -6,29 +6,29 @@
 # To use this script in Xcode, add the script's path to a "Run Script" build
 # phase for your application target.
 
+set -x
 set -o errexit
 set -o nounset
 
 # First, check for git in $PATH
 hash git 2>/dev/null || { echo >&2 "Git required, not installed.  Aborting build number update script."; exit 0; }
 
-# Alternatively, we could use Xcode's copy of the Git binary,
-# but old Xcodes don't have this.
-#GIT=$(xcrun -find git)
-
-# Run Script build phases that operate on product files of the target that defines them should use the value of this build setting [TARGET_BUILD_DIR]. But Run Script build phases that operate on product files of other targets should use ?BUILT_PRODUCTS_DIR? instead.
+# Run Script build phases that operate on product files of the target
+# that defines them should use the value of this build setting [TARGET_BUILD_DIR].
+# But Run Script build phases that operate on product files of other targets should use ?BUILT_PRODUCTS_DIR? instead.
 INFO_PLIST="${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
 
 if [[ $(git ls-files -m "StatusIm/Info.plist") = *"Info.plist"* ]]; then
     echo "version was set in Info.plist"
-else
-    RELEASE_VERSION=$(cat ../../VERSION)
-    BUILD_NO=$(bash ../scripts/version/build_no.sh)
-
-    # For debugging:
-    echo "SHORT VERSION: $RELEASE_VERSION"
-    echo "BUILD NO: $BUILD_NO"
-
-    /usr/libexec/PlistBuddy -c "Add :CFBundleVersion string $BUILD_NO" "$INFO_PLIST" 2>/dev/null || /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NO" "$INFO_PLIST"
-    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $RELEASE_VERSION" "$INFO_PLIST"
+    exit 0
 fi
+
+RELEASE_VERSION=$(cat ../VERSION)
+BUILD_NO=$(bash ../scripts/version/build_no.sh)
+
+# For debugging:
+echo "SHORT VERSION: ${RELEASE_VERSION}"
+echo "BUILD NO: ${BUILD_NO}"
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${BUILD_NO}" "${INFO_PLIST}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${RELEASE_VERSION}" "${INFO_PLIST}"
