517a1be27ce97d785018c8fbb51870579b33b6e7
Jakub Sokołowski <jakub@status.im>
2020-07-20 17:00:08 +0200

    nix: disable Gradle daemon for builds
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/mobile/android/release.nix
+++ nix/mobile/android/release.nix
@@ -143,6 +143,7 @@ in stdenv.mkDerivation rec {
     ${adhocEnvVars} ${pkgs.gradle}/bin/gradle \
       ${toString gradleOpts} \
       --offline --stacktrace \
+      -Dorg.gradle.daemon=false \
       -Dmaven.repo.local='${deps.gradle}' \
       -PversionCode=${toString buildNumber} \
       assemble${gradleBuildType} \
