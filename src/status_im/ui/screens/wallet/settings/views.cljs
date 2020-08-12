b1c6f5b06ef630475b01badb7f409e9725f65bb9
Gheorghe Pinzaru <feross95@gmail.com>
2020-08-12 16:29:50 +0300

    Cleanup wallet assets list

--- src/status_im/ui/screens/wallet/settings/views.cljs
+++ src/status_im/ui/screens/wallet/settings/views.cljs
@@ -4,15 +4,11 @@
             [status-im.ui.components.chat-icon.screen :as chat-icon]
             [status-im.ui.components.list.views :as list]
             [status-im.ui.components.react :as react]
-            [status-im.ui.components.styles :as components.styles]
             [quo.core :as quo]
             [reagent.core :as reagent]
             [status-im.ui.components.topbar :as topbar]
             [status-im.ui.components.search-input.view :as search-input]
-            [status-im.ui.components.colors :as colors])
-  (:require-macros [status-im.utils.views :refer [defview letsubs]]))
-
-(defonce search-active? (reagent/atom false))
+            [status-im.ui.components.colors :as colors]))
 
 (defn toolbar []
   [topbar/topbar
@@ -43,70 +39,70 @@
          :on-press #(hide-sheet-and-dispatch
                      [:wallet.custom-token.ui/remove-pressed token])}])]))
 
-(defn render-token
-  [_]
-  (reagent/create-class
-   {:should-component-update
-    (fn [_ [_ old-token] [_ new-token]]
-      (not= (:checked? old-token) (:checked? new-token)))
-    :reagent-render
-    (fn [{:keys [symbol name icon color checked?] :as token}]
-      [quo/list-item {:active        checked?
-                      :accessory     :checkbox
-                      :animated      false
-                      :icon          (if icon
-                                       [list/item-image icon]
-                                       [chat-icon/custom-icon-view-list name color])
-                      :title         name
-                      :subtitle      (clojure.core/name symbol)
-                      :on-press      #(re-frame/dispatch
-                                       [:wallet.settings/toggle-visible-token (keyword symbol) (not checked?)])
-                      :on-long-press #(re-frame/dispatch
-                                       [:bottom-sheet/show-sheet
-                                        {:content (custom-token-actions-view token)}])}])}))
+(def atom-eq (atom {}))
 
-(defn- render-token-wrapper
-  [token]
-  [render-token token])
+(defn render-token [{:keys [symbol name icon color checked?] :as token}]
+  [quo/list-item {:active        checked?
+                  :accessory     :checkbox
+                  :animated      false
+                  :icon          (if icon
+                                   [list/item-image icon]
+                                   [chat-icon/custom-icon-view-list name color])
+                  :title         name
+                  :subtitle      (clojure.core/name symbol)
+                  :on-press      #(re-frame/dispatch
+                                   [:wallet.settings/toggle-visible-token (keyword symbol) (not checked?)])
+                  :on-long-press #(re-frame/dispatch
+                                   [:bottom-sheet/show-sheet
+                                    {:content (custom-token-actions-view token)}])}])
 
-(defview manage-assets []
-  (letsubs [{search-filter                           :search-filter
-             {custom-tokens true default-tokens nil} :tokens} [:wallet/filtered-grouped-chain-tokens]]
-    {:component-will-unmount #(do
-                                (re-frame/dispatch [:search/token-filter-changed nil])
-                                (reset! search-active? false))}
-    [react/view (merge components.styles/flex {:background-color colors/white})
-     [toolbar]
-     [react/view {:style components.styles/flex}
-      [react/view {:padding-horizontal 16
-                   :padding-vertical   10}
-       [search-input/search-input
-        {:search-active? search-active?
-         :search-filter  search-filter
-         :on-cancel      #(re-frame/dispatch [:search/token-filter-changed nil])
-         :on-focus       (fn [search-filter]
-                           (when-not search-filter
-                             (re-frame/dispatch [:search/token-filter-changed ""])))
-         :on-change      (fn [text]
-                           (re-frame/dispatch [:search/token-filter-changed text]))}]]
-      [list/section-list
-       {:header
-        [react/view {:margin-top 16}
-         [quo/list-item
-          {:theme :accent
-           :title (i18n/label :t/add-custom-token)
-           :icon  :main-icons/add
-           :on-press
-           #(re-frame/dispatch [:navigate-to :wallet-add-custom-token])}]]
-        :sections                    (concat
-                                      (when (seq custom-tokens)
-                                        [{:title (i18n/label :t/custom)
-                                          :data  custom-tokens}])
-                                      [{:title (i18n/label :t/default)
-                                        :data  default-tokens}])
-        :key-fn                      :address
-        :stickySectionHeadersEnabled false
-        :render-section-header-fn
-        (fn [{:keys [title]}]
-          [quo/list-header title])
-        :render-fn                   render-token-wrapper}]]]))
+(defn manage-assets []
+  (reagent/with-let [search-active? (reagent/atom false)
+                     tokens (re-frame/subscribe [:wallet/filtered-grouped-chain-tokens])]
+    (let [{search-filter        :search-filter
+           {custom-tokens  true
+            default-tokens nil} :tokens} @tokens]
+      [react/view {:style {:flex             1
+                           :background-color colors/white}}
+       [toolbar]
+       [react/view {:style {:flex 1}}
+        [react/view {:padding-horizontal 16
+                     :padding-vertical   10}
+         [search-input/search-input
+          {:search-active? search-active?
+           :search-filter  search-filter
+           :on-cancel      #(re-frame/dispatch [:search/token-filter-changed nil])
+           :on-focus       (fn [search-filter]
+                             (when-not search-filter
+                               (re-frame/dispatch [:search/token-filter-changed ""])))
+           :on-change      (fn [text]
+                             (re-frame/dispatch [:search/token-filter-changed text]))}]]
+        [list/section-list
+         {:header
+          [react/view {:margin-top 16}
+           [quo/list-item
+            {:theme :accent
+             :title (i18n/label :t/add-custom-token)
+             :icon  :main-icons/add
+             :on-press
+             #(re-frame/dispatch [:navigate-to :wallet-add-custom-token])}]]
+          :sections                    (concat
+                                        (when (seq custom-tokens)
+                                          [{:title (i18n/label :t/custom)
+                                            :data  custom-tokens}])
+                                        [{:title (i18n/label :t/default)
+                                          :data  default-tokens}])
+          :key-fn                      (fn [{:keys [address checked?]}]
+                                         (str address checked?))
+          :get-item-layout             (fn [_ idx]
+                                         #js {:length 64
+                                              :offset (* 64 idx)
+                                              :index  idx})
+          :stickySectionHeadersEnabled false
+          :render-section-header-fn
+          (fn [{:keys [title]}]
+            [quo/list-header title])
+          :render-fn                   render-token}]]])
+    (finally
+      (re-frame/dispatch [:search/token-filter-changed nil])
+      (reset! search-active? false))))
