889d4c16a4612ca7ff4eb419bc0d20166493fa86
andrey <motor4ik@gmail.com>
2020-09-15 14:29:41 +0200

    [#11163] No margin between "Block this user" button and main tabs panel on user profile view on iOS

--- src/status_im/ui/screens/profile/contact/styles.cljs
+++ src/status_im/ui/screens/profile/contact/styles.cljs
@@ -2,12 +2,13 @@
   (:require [status-im.ui.components.colors :as colors]))
 
 (def block-action-label
-  {:color colors/red
-   :padding-top      26
-   :margin-left      16})
+  {:color         colors/red
+   :margin-top    26
+   :margin-bottom 50
+   :margin-left   16})
 
 (def contact-profile-details-container
-  {:padding-top      26})
+  {:padding-top 26})
 
 (def contact-profile-detail-share-icon
   {:color colors/gray-transparent-40})
