969f8f4d8ad022302082567ad997827e3dbb5c09
ismael <ismaeldm1995@gmail.com>
2021-07-31 12:45:44 +0200

    Add quick way to permanently hide android background service notification
    Signed-off-by: Andrea Maria Piana <andrea.maria.piana@gmail.com>

--- modules/react-native-status/android/src/main/java/im/status/ethereum/pushnotifications/ForegroundService.java
+++ modules/react-native-status/android/src/main/java/im/status/ethereum/pushnotifications/ForegroundService.java
@@ -8,6 +8,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.provider.Settings;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -30,6 +31,10 @@ public class ForegroundService extends Service {
         Context context = getApplicationContext();
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
+
+        Intent intent = null;
+        String notificationContentText = null;
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationManager notificationManager =
                 context.getSystemService(NotificationManager.class);
@@ -38,31 +43,49 @@ public class ForegroundService extends Service {
                                                                   NotificationManager.IMPORTANCE_HIGH);
             channel.setShowBadge(false);
             notificationManager.createNotificationChannel(channel);
-        }
-        Class intentClass;
-        String packageName = context.getPackageName();
-        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
-        String className = launchIntent.getComponent().getClassName();
-        try {
-            intentClass =  Class.forName(className);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return 0;
-        }
 
-        Intent intent = new Intent(context, intentClass);
-        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            // Create intent that takes the user to the notification channel settings so they can hide it
+
+            intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+            intent.putExtra(Settings.EXTRA_CHANNEL_ID, CHANNEL_ID);
+
+            notificationContentText = context.getResources().getString(R.string.tap_to_hide_notification);
+            
+        } else {
+
+            // For older versions of android intent takes the user to the Status app
+
+            Class intentClass;
+            String packageName = context.getPackageName();
+            Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+            String className = launchIntent.getComponent().getClassName();
+            try {
+                intentClass =  Class.forName(className);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                return 0;
+            }
+            
+            intent = new Intent(context, intentClass);
+            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            notificationContentText = context.getResources().getString(R.string.keep_status_running);
+
+        }
+        
+
+      
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         Intent stopIntent = new Intent(PushNotificationHelper.ACTION_TAP_STOP);
         PendingIntent stopPendingIntent = PendingIntent.getBroadcast(context, 0, stopIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
-        String content = context.getResources().getString(R.string.keep_status_running);
         Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_stat_notify_status)
             .setContentTitle(context.getResources().getString(R.string.background_service_opened))
-            .setContentText(content)
+            .setContentText(notificationContentText)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setCategory(NotificationCompat.CATEGORY_MESSAGE)
             .setContentIntent(pendingIntent)
