80913c40a76b7117e94a7883266971622f6ea25f
Marco Süß <msuess@mailbox.org>
2022-01-12 15:18:56 +0100

    justify sticker images with space-between

--- src/status_im/ui/screens/stickers/views.cljs
+++ src/status_im/ui/screens/stickers/views.cljs
@@ -105,7 +105,7 @@
            [price-badge price id owned pending])]
         [react/view {:style {:padding-top 8 :flex 1}}
          [react/scroll-view {:keyboard-should-persist-taps :handled :style {:flex 1}}
-          [react/view {:flex-direction :row :flex-wrap :wrap :justify-content :center}
+          [react/view {:flex-direction :row :flex-wrap :wrap :justify-content :space-between}
            (for [{:keys [hash]} stickers]
              ^{:key hash}
              [sticker-image hash])]]]]
