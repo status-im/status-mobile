bea0ae57dff79329bf10e8ce8eb4f8b8752b37a9
Marco Süß <msuess@mailbox.org>
2022-01-17 13:07:34 +0100

    center sticker view

--- src/status_im/ui/components/image_with_loader.cljs
+++ src/status_im/ui/components/image_with_loader.cljs
@@ -14,18 +14,17 @@
 
 (defn image-with-loader [props]
   (let [{:keys [source style]} props
-        loaded? (reagent/atom nil)
+        loaded? (reagent/atom false)
         error? (reagent/atom false)]
     (fn []
-      [:<>
+      [react/view
        (when @error?
          [placeholder {:style style}
           [icons/icon :main-icons/cancel]])
        (when-not (or @loaded? @error?)
          [placeholder {:style style}
           [react/activity-indicator {:animating true}]])
-       (when (and (or (nil? @loaded?) @loaded?)
-                  (not @error?))
+       (when (not @error?)
          [react/fast-image {:onError #(reset! error? true)
                             :onLoad #(reset! loaded? true)
                             :style (if @loaded? style {})
