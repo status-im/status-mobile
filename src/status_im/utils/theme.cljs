cf606b921ac8ff18915964a46ce3f6b19d48e941
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2022-03-04 19:34:17 +0530

    remove deprecated react-native-dark-mode dependency  (#13136)

--- src/status_im/utils/theme.cljs
+++ src/status_im/utils/theme.cljs
@@ -1,13 +1,14 @@
 (ns status-im.utils.theme
-  (:require ["react-native-dark-mode" :as react-native-dark-mode]
+  (:require ["react-native" :refer (Appearance)]
             [oops.core :refer [oget ocall]]))
 
-(def event-emitter (oget react-native-dark-mode "eventEmitter"))
-(def initial-mode (atom (oget react-native-dark-mode "initialMode")))
+(def initial-mode (atom (ocall Appearance "getColorScheme")))
 
 (defn add-mode-change-listener [callback]
-  (ocall event-emitter "on" "currentModeChanged" #(do (reset! initial-mode %)
-                                                      (callback (keyword %)))))
+  (ocall Appearance "addChangeListener" #(let [mode (oget % "colorScheme")]
+                                           (when-not (= mode @initial-mode)
+                                             (reset! initial-mode mode)
+                                             (callback (keyword mode))))))
 
 (defn is-dark-mode []
   (= @initial-mode "dark"))
