ba38f3ec74eb08fbc1c586f0d90107c9d736560d
andrey <motor4ik@gmail.com>
2022-04-04 16:38:51 +0200

    turn off new wallet design
    Signed-off-by: andrey <motor4ik@gmail.com>

--- src/status_im/ui/screens/screens.cljs
+++ src/status_im/ui/screens/screens.cljs
@@ -121,8 +121,8 @@
             [status-im.ui.screens.wallet.recipient.views :as recipient]
             [status-im.ui.screens.wallet.send.views :as wallet.send]
             [quo2.screens.main :as quo2.preview]
-            [status-im.utils.config :as config]
-            [quo2.foundations.colors :as quo2.colors]))
+            [status-im.utils.config :as config]))
+            ;[quo2.foundations.colors :as quo2.colors]))
 
 (def components
   [{:name      :chat-toolbar
@@ -358,10 +358,11 @@
            {:name      :wallet
             :insets    {:top false}
             :on-focus  [:wallet/tab-opened]
-            :options   {:statusBar {:backgroundColor quo2.colors/neutral-5}}
-            :component wallet.accounts/accounts-overview}
+            ;;TODO wallet redesign
+            ;;:options   {:statusBar {:backgroundColor quo2.colors/neutral-5}}
+            :component wallet.accounts/accounts-overview-old}
            {:name      :wallet-account
-            ;;TODO dynamic title
+            ;;TODO dynamic titleaccounts-overview
             :options   {:topBar {:visible false}}
             :component wallet.account/account}
            {:name      :add-new-account
