899e4e91748af5fc246f83fc82b47689fb29f777
yqrashawn <namy.19@gmail.com>
2022-07-23 23:03:50 +0800

    feat: deps and ios
    Signed-off-by: yqrashawn <namy.19@gmail.com>

--- ios/Dummy.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Dummy.swift
-//  StatusIm
-//
-//  Created by Gheorghe on 01.05.2020.
-//  Copyright © 2020 Status. All rights reserved.
-//
-
-import Foundation
--- src/status_im/navigation/core.cljs
+++ src/status_im/navigation/core.cljs
@@ -1,21 +1,21 @@
 (ns status-im.navigation.core
   (:require
    ["react-native" :as rn]
-   [clojure.set :as clojure.set]
    ["react-native-gesture-handler" :refer (gestureHandlerRootHOC)]
    ["react-native-navigation" :refer (Navigation)]
+   [clojure.set :as clojure.set]
    [quo.components.text-input :as quo.text-input]
    [quo.design-system.colors :as quo.colors]
    [re-frame.core :as re-frame]
+   [status-im.multiaccounts.login.core :as login-core]
    [status-im.navigation.roots :as roots]
+   [status-im.navigation.state :as state]
    [status-im.ui.components.icons.icons :as icons]
    [status-im.ui.components.react :as react]
    [status-im.ui.screens.views :as views]
    [status-im.utils.fx :as fx]
    [status-im.utils.platform :as platform]
-   [taoensso.timbre :as log]
-   [status-im.multiaccounts.login.core :as login-core]
-   [status-im.navigation.state :as state]))
+   [taoensso.timbre :as log]))
 
 (def debug? ^boolean js/goog.DEBUG)
 
