1e59707737758a0e73f088be533300f9dd11dad0
Ibrahem Khalil <33176106+vampirekiddo@users.noreply.github.com>
2022-07-24 07:59:38 +0200

    Opacity around box

--- src/quo2/components/qr_scan_animation.cljs
+++ src/quo2/components/qr_scan_animation.cljs
@@ -2,11 +2,15 @@
     (:require [quo.design-system.colors :as colors]
               [quo.react :as react]
               [quo.react-native :as rn]
-              [quo2.reanimated :as reanimated]
-              [status-im.switcher.constants :as constants]))
+              [quo2.reanimated :as reanimated]))
 
 (defn corner [border1 border2 corner]
-  [reanimated/view (assoc {:border-color colors/white-persist :width 60 :height 60} border1 2 border2 2 corner 10)])
+  [reanimated/view (assoc {:border-color colors/white-persist
+                           :width 60
+                           :height 60}
+                          border1 2
+                          border2 2
+                          corner 10)])
 
 (def viewfinder-port
   {:position        :absolute
@@ -14,16 +18,18 @@
    :top             0
    :bottom          0
    :z-index         100 
-   :right           0
+   :right           0 
    :align-items     :center
-   :justify-content :center
+   :justify-content :center 
    :flex            1})
 
 (defn- viewfinder [size]
   [:f>
    (fn []
      [rn/view {:style viewfinder-port}
-      [reanimated/view {:style (reanimated/apply-animations-to-style
+      [rn/view {:style {:border-width 400
+                        :border-color "rgba(0,0,0,0.9)"}}
+       [reanimated/view {:style (reanimated/apply-animations-to-style
                                 {:width size
                                  :height size}
                                 {:width size
@@ -34,29 +40,20 @@
         [corner :border-top-width :border-right-width :border-top-right-radius]]
        [rn/view {:flex-direction :row :justify-content :space-between}
         [corner :border-bottom-width :border-left-width :border-bottom-left-radius]
-        [corner :border-bottom-width :border-right-width :border-bottom-right-radius]]]])])
+        [corner :border-bottom-width :border-right-width :border-bottom-right-radius]]]]])])
 
 (defn preview
   []
   [:f>
-     (fn []
-  (let [initial-dimensions (reanimated/use-shared-value 100)
-        _ (react/effect! (fn []
-                           (reanimated/animate-shared-value-with-delay initial-dimensions 350 400 :easing2 300) []))] 
-       [:<> 
-        [rn/image-background {:source (js/require "../resources/images/ui/hardwallet-card.png")
-                              :image-style {:opacity 0.7}
-                              :style {:width "100%"
+   (fn []
+     (let [initial-dimensions (reanimated/use-shared-value 150)
+           _ (react/effect! (fn []
+                              (reanimated/animate-shared-value-with-delay initial-dimensions 350 200 :easing2 300) []))]
+       [:<>
+        [rn/image-background {:style {:width "100%"
+                                      :background-color "red"
                                       :height "100%"}}
          [rn/view {:style {:justify-content :center
-                           :z-index 0
                            :align-items :center
                            :height "100%"}}
           [viewfinder initial-dimensions]]]]))])
-
-;; [reanimated/view {:style (reanimated/apply-animations-to-style
-;;                           {:width initial-dimensions
-;;                            :height initial-dimensions}
-;;                           styles)}]
-;; (reanimated/animate-shared-value-with-timing
-;;  initial-dimensions 300 300 :easing3)
