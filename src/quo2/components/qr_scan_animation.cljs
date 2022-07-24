f7c773f116fbcd2223cca72d3dd6bd6d88ec1de9
Ibrahem Khalil <33176106+vampirekiddo@users.noreply.github.com>
2022-07-24 08:33:00 +0200

    Linting

--- src/quo2/components/qr_scan_animation.cljs
+++ src/quo2/components/qr_scan_animation.cljs
@@ -1,8 +1,8 @@
-(ns quo2.components.qr-scan-animation 
-    (:require [quo.design-system.colors :as colors]
-              [quo.react :as react]
-              [quo.react-native :as rn]
-              [quo2.reanimated :as reanimated]))
+(ns quo2.components.qr-scan-animation
+  (:require [quo.design-system.colors :as colors]
+            [quo.react :as react]
+            [quo.react-native :as rn]
+            [quo2.reanimated :as reanimated]))
 
 (defn corner [border1 border2 corner]
   [reanimated/view (assoc {:border-color colors/white-persist
@@ -17,10 +17,10 @@
    :left            0
    :top             0
    :bottom          0
-   :z-index         100 
-   :right           0 
+   :z-index         100
+   :right           0
    :align-items     :center
-   :justify-content :center 
+   :justify-content :center
    :flex            1})
 
 (defn- viewfinder [size]
@@ -30,17 +30,17 @@
       [rn/view {:style {:border-width 400
                         :border-color "rgba(0,0,0,0.9)"}}
        [reanimated/view {:style (reanimated/apply-animations-to-style
-                                {:width size
-                                 :height size}
-                                {:width size
-                                 :height size 
-                                 :justify-content :space-between})}
-       [rn/view {:flex-direction :row :justify-content :space-between}
-        [corner :border-top-width :border-left-width :border-top-left-radius]
-        [corner :border-top-width :border-right-width :border-top-right-radius]]
-       [rn/view {:flex-direction :row :justify-content :space-between}
-        [corner :border-bottom-width :border-left-width :border-bottom-left-radius]
-        [corner :border-bottom-width :border-right-width :border-bottom-right-radius]]]]])])
+                                 {:width size
+                                  :height size}
+                                 {:width size
+                                  :height size
+                                  :justify-content :space-between})}
+        [rn/view {:flex-direction :row :justify-content :space-between}
+         [corner :border-top-width :border-left-width :border-top-left-radius]
+         [corner :border-top-width :border-right-width :border-top-right-radius]]
+        [rn/view {:flex-direction :row :justify-content :space-between}
+         [corner :border-bottom-width :border-left-width :border-bottom-left-radius]
+         [corner :border-bottom-width :border-right-width :border-bottom-right-radius]]]]])])
 
 (defn preview
   []
