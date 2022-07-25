beaff6cf06bd1ee939050e79f6f78014f7e508d0
Ibrahem Khalil <33176106+vampirekiddo@users.noreply.github.com>
2022-07-25 19:32:43 +0200

    Push viewfinder outside a bit

--- src/quo2/components/qr_scan_animation.cljs
+++ src/quo2/components/qr_scan_animation.cljs
@@ -2,14 +2,26 @@
   (:require [quo.design-system.colors :as colors]
             [quo.react :as react]
             [quo.react-native :as rn]
-            [quo2.reanimated :as reanimated]))
+            [quo2.reanimated :as reanimated]
+            [status-im.utils.dimensions :as dimensions]))
 
-(defn corner [border1 border2]
-  [reanimated/view (assoc {:border-color colors/white-persist
-                           :width 60
-                           :height 60}
-                          border1 2
-                          border2 2)])
+(defn corner [border1 border2 outside-border] 
+  (let [{:keys [width height]} (dimensions/window)]
+    [reanimated/view (merge (assoc {:border-color colors/white-persist
+                             :width (* 0.1 width)
+                             :height (* 0.05 height)
+                             :position :absolute}
+                            border1 3
+                            border2 3)
+                            (case outside-border
+                              :top-left {:top -4
+                                         :left -2}
+                              :top-right {:top -4
+                                          :right -2}
+                              :bottom-left {:bottom -4
+                                            :left -2}
+                              :bottom-right {:bottom -4
+                                             :right -2}))]))
 
 (def viewfinder-port
   {:position        :absolute
@@ -22,7 +34,7 @@
    (fn []
      [rn/view {:style viewfinder-port}
 
-      [rn/view {:style {:border-width 400
+      [rn/view {:style {:border-width 700
                         :border-color "rgba(0,0,0,0.7)"}}
        [reanimated/view {:style (reanimated/apply-animations-to-style
                                  {:width size
@@ -31,24 +43,29 @@
                                   :height size
                                   :justify-content :space-between})}
         [rn/view {:flex-direction :row :justify-content :space-between}
-         [corner :border-top-width :border-left-width]
-         [corner :border-top-width :border-right-width]]
+         [corner :border-top-width :border-left-width :top-left]
+         [corner :border-top-width :border-right-width :top-right]]
         [rn/view {:flex-direction :row :justify-content :space-between}
-         [corner :border-bottom-width :border-left-width]
-         [corner :border-bottom-width :border-right-width]]]]])])
+         [corner :border-bottom-width :border-left-width :bottom-left]
+         [corner :border-bottom-width :border-right-width :bottom-right]]]]])])
 
 (defn preview
   []
   [:f>
    (fn []
-     (let [initial-dimensions (reanimated/use-shared-value 150)
+     (let [{:keys [width]} (dimensions/window)
+           size (reanimated/use-shared-value (* width 0.4))
            _ (react/effect! (fn []
-                              (reanimated/animate-shared-value-with-delay initial-dimensions 350 200 :easing2 300) []))]
+                              (reanimated/animate-shared-value-with-delay size
+                                                                          (* width 0.8)
+                                                                          400
+                                                                          :easing2 200)
+                              []))]
        [:<>
-        [rn/image-background {:style {:width "100%"
-                                      :height "100%"
-                                      :background-color "red"}}
+        [rn/image-background {:source (js/require "../resources/images/ui/test.jpeg")
+                              :style {:width "100%"
+                                      :height "100%"}}
          [rn/view {:style {:justify-content :center
                            :align-items :center
                            :height "100%"}}
-          [viewfinder initial-dimensions]]]]))])
+          [viewfinder size]]]]))])
