da62c66153323fbe3816ad9794de811337fecde4
Ibrahem Khalil <33176106+vampirekiddo@users.noreply.github.com>
2022-07-26 19:18:25 +0200

    User avatar component preview
    Clean

--- src/quo2/screens/user_avatar.cljs
+++ src/quo2/screens/user_avatar.cljs
@@ -23,7 +23,11 @@
                  {:label "Identicon Ring"
                   :key   :ring?
                   :type  :boolean}
+<<<<<<< HEAD
                  {:label "Full name separated by space"
+=======
+                 {:label "Full name seperated by space"
+>>>>>>> e2adcb504 (User avatar component preview)
                   :key   :full-name
                   :type  :text}
                  {:label "Profile Picture URL"
