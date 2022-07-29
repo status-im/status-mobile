e8ff5cdd5bd7e0f2ef029db4389e8d7dd9d096a7
yqrashawn <namy.19@gmail.com>
2022-07-29 22:19:31 +0800

    fix: consistent status bar color

--- android/app/src/main/java/im/status/ethereum/MainActivity.java
+++ android/app/src/main/java/im/status/ethereum/MainActivity.java
@@ -125,14 +125,16 @@ public class MainActivity extends NavigationActivity
         switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
             case Configuration.UI_MODE_NIGHT_YES:
                 setTheme(R.style.DarkTheme);
+                SplashScreen.show(this, R.style.DarkTheme, R.id.lottie);
                 break;
             case Configuration.UI_MODE_NIGHT_NO:
                 setTheme(R.style.LightTheme);
+                SplashScreen.show(this, R.style.LightTheme, R.id.lottie);
                 break;
             default:
                 setTheme(R.style.LightTheme);
+                SplashScreen.show(this, R.style.LightTheme, R.id.lottie);
         }
-        SplashScreen.show(this, R.id.lottie);
         SplashScreen.setAnimationFinished(true);
         // Make sure we get an Alert for every uncaught exceptions
         registerUncaughtExceptionHandler(MainActivity.this);
