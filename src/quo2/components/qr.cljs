b87747dab15db0682b6639218558f0014a68bee8
siddarthkay <siddarthkay@gmail.com>
2022-08-08 17:54:03 +0530

    staging progress on using urls for images

--- src/quo2/components/qr.cljs
+++ src/quo2/components/qr.cljs
@@ -13,6 +13,8 @@
 ;This is the space where the core component would live
 ;Where all the various options would be passed and depending on the options
 ;the final view would be rendered
+(def tab-container-radius 10)
+(def tab-item-height 15)
 
 (def qr-container-radius 16)
 (def static-qr-code-url "https://status.im")
@@ -67,6 +69,97 @@
    :font-size 16
    :max-width max-width})
 
+(def information-icon-container
+  {:position :absolute
+   :right 0})
+
+(def account-subtype-label
+  {:color colors/white
+   :font-size tab-item-height
+   :line-height 22
+   :font-weight :500})
+
+(defn account-subtype-tab [selected]
+  {:border-radius tab-container-radius
+   :align-items :center
+   :justify-content :flex-start
+   :padding-horizontal 8
+   :padding-vertical 3
+   :margin-horizontal 5
+   :margin-bottom 20
+   :background-color (if (= selected true)
+                       colors/white-opa-20
+                       colors/white-opa-5)})
+
+(def account-subtype-row
+  {:flex-direction :row
+   :width :89.3%})
+
+(def multichain-address-share-button-container
+  {:position :absolute
+   :padding 8
+   :right 0
+   :background-color colors/white-opa-5
+   :border-radius 10
+   :top 5})
+
+(defn multichain-address-content [max-width]
+  {:color colors/white
+   :align-self :flex-start
+   :padding-top 2
+   :font-weight :500
+   :font-size 16
+   :max-width max-width})
+
+(def seperator-text
+  {:color colors/white})
+
+(defn network-text-style [text-color]
+  {:color text-color})
+
+(def mulichain-address-string-container
+  {:flex-direction :row})
+
+(def mulichain-address-column
+  {:flex-direction :column})
+
+(def mulichain-address-container
+  {:flex-direction :row
+   :width :88%})
+
+(def divider-line-container
+  {:border-width 0.5
+   :border-color colors/white-opa-20
+   :border-style "dashed"
+   :width :88%
+   :margin-bottom 20})
+
+(def network-share-icon-style
+  {:position :absolute
+   :padding 8
+   :border-radius 10
+   :background-color colors/white-opa-5
+   :top 10
+   :right 0})
+
+(defn wallet-icon-style [first-icon?]
+  {:margin-vertical 0
+   :border-width 2
+   :border-left-color :transparent
+   :border-radius 32
+   :width 32
+   :height 32
+   :z-index (if first-icon? 5 99) ;;pathetic attempt to make it look like the icons are cutting into each other
+   :margin-left (if first-icon? 0 -10)})
+
+(def wallet-icons-container
+  {:flex-direction :row
+   :align-self :flex-start
+   :padding-vertical 20
+   :margin-left 20
+   :width :88%})
+
+
 (defn profile-address-qr-comp [qr-code-url link-to-profile]
   (let [window-width @(re-frame/subscribe [:dimensions/window-width])]
     [rn/view {:style {:background-color colors/neutral-80-opa-80 }}
@@ -91,11 +184,85 @@
       ]
      ]
     )
-  )
+)
+
+(defn legacy-wallet-address-view [address-text window-width]
+  [rn/view {:style profile-address-container}
+   [rn/view {:style profile-address-column}
+    [rn/text {:style profile-address-label} (i18n/label :t/wallet-address)]
+    [copyable-text/copyable-text-view {:copied-text address-text :container-style copyable-text-container-style}
+     [rn/text {:style (profile-address-content (* window-width 0.7)) :ellipsize-mode :middle :number-of-lines 1} address-text]]]
+   [rn/touchable-highlight {:style address-share-button-container}
+    [icons/icon :main-icons/share-icon20 {:color colors/white :width 20 :height 20}]]])
+
+(defn multichain-wallet-address-view [address-text multi-chain-info multichain-wallet-address-with-network-chain window-width]
+  [:<>
+   [rn/view {:style wallet-icons-container}
+    (for [single-chain multi-chain-info] ^{:key single-chain}
+                                         [react/image {:source (get single-chain :network-icon-image) :style (wallet-icon-style (= single-chain (first multi-chain-info)))}])
+    [rn/touchable-highlight {:style network-share-icon-style}
+     [icons/icon :main-icons/customize-icon20 {:color colors/white :width 20 :height 20}]]]
+
+   [rn/view {:style divider-line-container}]
+
+   [rn/view {:style mulichain-address-container}
+    [rn/view {:style mulichain-address-column}
+     [rn/view {:style mulichain-address-string-container}
+
+      (for
+        [wallet-item multi-chain-info] ^{:key wallet-item}
+                                       [:<>
+                                        [rn/text {:style (network-text-style (get wallet-item :network-text-color))} (get wallet-item :network-type)]
+                                        (when-not (= wallet-item (last multi-chain-info)) [rn/text {:style seperator-text} ":"])])]
+
+     [copyable-text/copyable-text-view {:copied-text multichain-wallet-address-with-network-chain :container-style copyable-text-container-style}
+      [rn/text {:style (multichain-address-content (* window-width 0.6)) :ellipsize-mode :middle :number-of-lines 1} address-text]]]
+
+    [rn/touchable-highlight {:style multichain-address-share-button-container}
+     [icons/icon :main-icons/share-icon20 {:color colors/white :width 20 :height 20}]]]])
 
 
-(defn qr [type-of-qr]
-      (case type-of-qr
-             "profile" [profile-address-qr-comp static-qr-code-url static-link-to-profile]
+(defn legacy-address-qr-comp [qr-url legacy-wallet-address multichain-wallet-address multi-chain-info multichain-wallet-address-with-network-chain]
+  (let [selected-sub-account-index (reagent/atom 2)
+        window-width @(re-frame/subscribe [:dimensions/window-width])]
+    (fn []
+      [:<>
+       [rn/view {:style {:background-color colors/neutral-80-opa-80 }}
+       [rn/view {:style (qr-code-container window-width true)}
+
+        [rn/view {:style account-subtype-row}
+
+         [rn/touchable-highlight {:style (account-subtype-tab (= @selected-sub-account-index 1))}
+          [rn/text {:style account-subtype-label
+                    :on-press #(reset! selected-sub-account-index 1)} (i18n/label :t/legacy)]]
+
+         [rn/touchable-highlight {:style (account-subtype-tab (= @selected-sub-account-index 2))}
+          [rn/text {:style account-subtype-label
+                    :on-press #(reset! selected-sub-account-index 2)} (i18n/label :t/multichain)]]
+
+         [rn/touchable-highlight {:style information-icon-container}
+          [icons/icon :main-icons/info-tooltip
+           {:color "rgba(255, 255, 255, 0.4)"
+            :width 22
+            :height 22}]]]
+
+        [qr-code-viewer/qr-code-view (* window-width 0.808) qr-url 12 colors/white]
+        (cond (= @selected-sub-account-index 1)
+              [legacy-wallet-address-view
+               legacy-wallet-address
+               window-width]
+              :else
+              [multichain-wallet-address-view
+               multichain-wallet-address
+               multi-chain-info
+               multichain-wallet-address-with-network-chain
+               window-width])
+        ]]]))
+)
+
+
+(defn qr [{:keys [type url profile wallet-address multichain-wallet-address multi-chain-info multichain-wallet-address-with-network-chain]}]
+      (case type :profile [profile-address-qr-comp url profile]
+            type :legacy [legacy-address-qr-comp url wallet-address multichain-wallet-address multi-chain-info multichain-wallet-address-with-network-chain]
         )
   )
--- src/quo2/screens/qr_screen.cljs
+++ src/quo2/screens/qr_screen.cljs
@@ -44,8 +44,68 @@
 ;         (if @icon :main-icons2/placeholder @label)]]])))
 
 
+;the idea here is to make a list of resources and their urls which would go
+;to the resources folder and for this screen all the icons and assets would come from this
+;map of resources
+(defn qr-resources []
+    {"Etherium" {:url "https://s3-alpha-sig.figma.com/img/8ab2/37c4/8f2b8f259003612f956d3448d4d22264?Expires=1661126400&Signature=DObJFoz5rwgED4MoIcgzEUkYDp50A5d3VnkR5Amy2EZt~fbcabySPqxSpAVp0axvVQqiGaBl2wTUyt757BKbfK8JH0Nu~nAc-gYzujzzJPAvgyMEYb8o7568fqQxYnYwGORDduhWZX7WIsZksIQdd~RL4Npmyzo8HpMmNXeCMXjJW8UdptPfmUlk5tIjA8XvHqnWZYxJ8Jm8XkhfnX-fFa1AArCVWElisJDa5YHm07aNEsGFD-wh5JTaLKcoM7jdPpcmUnj-UMy4JfzsuSkGaSy-403Np65iraGAxGmn6wCFxdV4eLVcME6jyagEQqcgsYAWyRQkfY-hBx28fouiqg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                 :name "Etherium"
+                 :width 40
+                 }
+     "Arbitrum" {:url "https://s3-alpha-sig.figma.com/img/a493/c328/b9b6cb2db1f451513d705938f8cfb89e?Expires=1661126400&Signature=dmSMuhKKkImEWSw9-czU2XGdi82xD6RKHxkv3yobUQ5ibRQQ47s7Ca67Zt--EBhvAi2TSoz2MYNF7xAQqZp9F3ZUCQWd-UgcMCzrJWamR~KrYKSygNg8icUcKUjSPSlPsZD5Egfm3SinSMSoo0-y24SIR1XEmieGyG5E0JIJoSrYZE8SLIEJUzp9xNTXSjEUrwBi3Bojfte1EGIIy5uuiz0QLIjNPCCTe4IsKEE7aHm1zOwW~IW4qvhMwqkxDdnaj9~8ZP6hZgQ2JESnpq5DLmlVfBnWQ9xK88Im12MxejC7YIPnPvHFU8WduQjb8~X9Cs1NqN46wcntE4XW3RjJsQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                 :name "Arbitrum"
+                 :width 40
+                 }
+     "Hermez" {:url "https://s3-alpha-sig.figma.com/img/8ef2/0db3/e2582832749a9ddfbc5729a7b107957f?Expires=1661126400&Signature=K8-N5ZBhBzDz0qx9ovs3eJW4xpRSicX3W1Kq8Qk2HzArEn1Xi3UJdX~t67565samTC~AY1~30-KZlZZISsiQnF-Tl0AjA1J0XBSKMjk7DNuhnKkeoHLkGyCz8MD~Q9UfR8aKklFkXCTY0EekvwyXkDKTgtY6c2hP3Oy10Qpyt~HyTgvQ9qweO-hg14B4V44iRDuzYwj-cWwlH5Cu1wBCZrfO4AdKnoJJnnzUE~m2Z0eogQfY7qz-LQJ2envzFGmCXS38lZg3~MIjy0izIfoDq1LiaaUcYgP~S~gVjHv2KyI2YrvyTcsVw-sPQGLhBOkvhSG2tKbYISHnvPXIzmU6PA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                 :name "Hermez"
+                 :width 40
+                 }
+     "Optimism" {:url "https://s3-alpha-sig.figma.com/img/371f/dbf2/07b948fd03e4397e4bbb687978b385cb?Expires=1661126400&Signature=aSF2EcWzwDgdJks13xAAT3TNkUF6vYD5G0VQG8ZuWvRhK0hD0o4N6t0IjWKZGxW1EOwv0ERpAV2MxcuIz32wx9WCpG3loKL-hTvMDjTJ7F6aiER9e5jZlNUdqgzsn6Sm1pIIl8~O35NHsW-qtwZTDoPoH2~TJ3ozI6UbMwHnrtoCwzkkrWxL2MxtIWG-Y8j8sFilhVAgZpwgbpgLAere2PI3iQoRYAG18psM3wI7vTvA-PcSdqICh0OUPPEQCb7miL5Hy10UiPX2Huv3Gv8DQmOIIrjAfOau-0UPZYpUcS9Oh0fH78jNN~bpeJlazqYmLNd9SdPHoYw7mcbWxLmaFw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                 :name "Optimism"
+                 :width 40
+                 }
+     "zkSync" {:url "https://s3-alpha-sig.figma.com/img/6a3d/8678/cb17273e2fbc1f49f040df9f15425fd1?Expires=1661126400&Signature=TIBuqx87m5QSduVzTVqPPHprUL0lKQZlAiY~0auefKx5w7VXJdoBrzlSBXDzi9j3q5tRszJxG1whudt7ZH4Xou8lx36Rb3NoYfEQ0rPkDVgRqL3DmjStS5Hk5dScRTOYoDH6ntZI1uUCGnXccOrDliI9esbNkPHm~9KtCZVNKEVQjBr~dJCFvksOCBvIkD5v0GrJsRfM9r8xRW0z7cOkIcMljPgJ2mVI9PGtzIYGnlgwTdcUU7Loopp7yKA7s1dn-ExQ2Nrf39DOOcCISeNhuNwodyaxn-GuetaL0fviL0JEPedED3IW5FJaYG6rUVJYkHBkP2OFMU8bh4ef6tRjpg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                 :name "zkSync"
+                 :width 40
+                 }
+     }
+  )
+
+
 (defn preview-qr []
-  [quo2/qr "profile"]
+;  this works as expected
+;  [quo2/qr {:type :profile
+;            :url "https://status.im"
+;            :profile "status.app/u/zQ34e2ahd1835eqacc17f6asas12adjie8"}]
+
+  [quo2/qr {:type :legacy
+            :url "https://status.im"
+            :wallet-address "0x04a8b2fe1c6388a030f3190bc6f3dc3650f324d049fe459d3d4b1ebab79491ad1f3e84f3190b98d09a6facaebdf8b18f51201219c116c72ea10704470ea9006995"
+            :multichain-wallet-address "0x04a8b2fe1c6388a030f3190bc6f3dc3650f324d049fe459d3d4b1ebab79491ad1f3e84f3190b98d09a6facaebdf8b18f51201219c116c72ea10704470ea9006995"
+            :multi-chain-info (list
+                               {:network-type "eth"
+                                :name "Ethereum"
+                                :network-text-color "rgba(76, 180, 239, 1)"
+                                :network-icon-image (resources/get-image :ethereum)}
+                               {:network-type "arb"
+                                :name "Arbitrum"
+                                :network-text-color "rgba(63, 174, 249, 1)"
+                                :network-icon-image (resources/get-image :arbitrum)}
+                               {:network-type "her"
+                                :name "Hermez"
+                                :network-text-color "rgba(251, 143, 97, 1)"
+                                :network-icon-image (resources/get-image :hermez)}
+                               {:network-type "opt"
+                                :name "Optimism"
+                                :network-text-color "rgba(230, 95, 92, 1)"
+                                :network-icon-image (resources/get-image :optimism)}
+                               {:network-type "zks"
+                                :name "zkSync"
+                                :network-text-color "rgba(139, 141, 250, 1)"
+                                :network-icon-image (resources/get-image :zksync)})
+            :multichain-wallet-address-with-network-chain "eth:arb:her:opt:zks:0x04a8b2fe1c6388a030f3190bc6f3dc3650f324d049fe459d3d4b1ebab79491ad1f3e84f3190b98d09a6facaebdf8b18f51201219c116c72ea10704470ea9006995"
+            }]
+
 ;  [rn/view {:background-color (:ui-background @colors/theme)
 ;            :flex             1}
 ;   [rn/flat-list {:flex                      1
