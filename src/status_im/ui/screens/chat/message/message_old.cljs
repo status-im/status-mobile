064fcbd8855e7700da6d1274495eecf0516d09da
Omar Basem <founder@stiiick.com>
2022-08-16 18:22:47 +0400

    using defn and get instead of defview and letsubs

--- src/status_im/ui/screens/chat/message/message_old.cljs
+++ src/status_im/ui/screens/chat/message/message_old.cljs
@@ -88,8 +88,8 @@
 
 (defview quoted-message
   [_ {:keys [from parsed-text image audio sticker id] :as message} outgoing current-public-key public? pinned chat-id]
-  (letsubs [contact-name [:contacts/contact-name-by-identity from]
-            replied-message (@(re-frame/subscribe [:chats/chat-messages chat-id]) id)] ;; message replied to
+  (let [contact-name [:contacts/contact-name-by-identity from]
+            replied-message (get @(re-frame/subscribe [:chats/chat-messages chat-id]) id)] ;; message replied to
     [react/view {:style (style/quoted-message-container (and outgoing (not pinned)))}
      [react/view {:style style/quoted-message-author-container}
       [chat.utils/format-reply-author
