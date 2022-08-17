bcbdbde1f9394a63d6236661fc72d1600d0133cc
Erik Seppanen <esep@protonmail.com>
2022-08-17 07:58:35 -0400

    Fix indentation
    Signed-off-by: Erik Seppanen <esep@protonmail.com>
    
    Clean

--- src/status_im/utils/test.cljs
+++ src/status_im/utils/test.cljs
@@ -69,12 +69,16 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 0f2334617 (Fix indentation)
 =======
 >>>>>>> 0f2334617 (Fix indentation)
                                            (callback
                                             (.multiAccountDeriveAddresses
                                              native-status
                                              json)))
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 =======
@@ -89,6 +93,8 @@
 >>>>>>> 0f2334617 (Fix indentation)
 =======
 >>>>>>> 94f73d267 ([#13791] Extend integration tests to create a new wallet account)
+=======
+>>>>>>> 0f2334617 (Fix indentation)
             :initKeystore (fn [key-uid callback]
                             (callback
                              (.initKeystore
