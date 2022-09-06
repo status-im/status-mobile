81992db10a10a20f25a34b75f377855921f4126c
ibrkhalil <vampirekid017@gmail.com>
2022-09-06 12:03:19 +0200

    Reactions component

--- src/quo2/screens/main.cljs
+++ src/quo2/screens/main.cljs
@@ -24,6 +24,7 @@
             [quo2.screens.token-tag :as token-tag]
             [quo2.screens.wallet-user-avatar :as wallet-user-avatar]
             [quo2.screens.user-avatar :as user-avatar]
+            [quo2.screens.reacts :as reacts]
             [re-frame.core :as re-frame]))
 
 (def screens [{:name      :quo2-texts
@@ -34,7 +35,7 @@
                :component wallet-user-avatar/preview-wallet-user-avatar}
               {:name      :quo2-user-avatar
                :insets    {:top false}
-               :component user-avatar/preview-user-avatar}
+               :component reacts/preview-reacts}
               {:name      :quo2-button
                :insets    {:top false}
                :component button/preview-button}
