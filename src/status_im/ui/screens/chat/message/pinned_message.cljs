38a406b45d2b25fd2fa92816908101bdd6952233
Omar Basem <omarbasem.0101@gmail.com>
2022-10-12 13:58:51 +0400

    feat: show pinned messages new ui

--- src/status_im/ui/screens/chat/message/pinned_message.cljs
+++ src/status_im/ui/screens/chat/message/pinned_message.cljs
@@ -23,7 +23,7 @@
                         :justify-content :space-between
                         :flex            1
                         :padding-right   20}}
-    [message/chat-message
+    [message-old/chat-message
      (assoc message
             :group-chat group-chat
             :public? public?
@@ -86,3 +86,67 @@
         :disabled (nil? @selected-unpin)
         :theme (if (nil? @selected-unpin) :disabled :negative)}
        (i18n/label :t/unpin)]]]))
+
+(defn message-render-fn []
+  [react/text-class "asdf"]
+  ;[{:keys [outgoing type] :as message}
+  ;                       idx
+  ;                       _
+  ;                       {:keys [group-chat public? community? current-public-key
+  ;                               chat-id show-input? message-pin-enabled edit-enabled in-pinned-view?]}]
+  ;[react/view {:style (when (and platform/android? (not in-pinned-view?)) {:scaleY -1})}
+  ; (if (= type :datemark)
+  ;   [message-datemark/chat-datemark (:value message)]
+  ;   [message/chat-message
+  ;    (assoc message
+  ;      :incoming-group (and group-chat (not outgoing))
+  ;      :group-chat group-chat
+  ;      :public? public?
+  ;      :community? community?
+  ;      :current-public-key current-public-key
+  ;      :show-input? show-input?
+  ;      :message-pin-enabled message-pin-enabled
+  ;      :edit-enabled edit-enabled)])]
+  )
+
+(defn pinned-messages-list [chat-id]
+  (let [
+        ;pinned-messages (vec (vals @(re-frame/subscribe [:chats/pinned chat-id])))
+        ]
+  [react/view {:style {:padding-horizontal 20}}
+   [react/text-class {:style (merge typography/heading-1 typography/font-semi-bold)} (i18n/label :t/pinned-messages)]
+   [list/flat-list
+    :data [1 2 3]
+    :render-fn message-render-fn
+    :key-fn first
+    ]]
+  ))
+
+(defn pinned-banner [chat-id]
+  (let [pinned-messages (<sub [:chats/pinned chat-id])
+        latest-pin-text (get-in (first (vals pinned-messages)) [:content :text])
+        pins-count (count (seq pinned-messages))]
+    (when (> pins-count 0)
+  [react/touchable-opacity-class
+                      {:style {:height 50
+                       :background-color quo2.colors/primary-50-opa-20
+                       :flex-direction :row
+                       :align-items :center
+                       :padding-horizontal 20
+                       :padding-vertical 10}
+                       :active-opacity 1
+                       :on-press (fn [actions]
+                                    (re-frame/dispatch [:bottom-sheet/show-sheet
+                                                        {:content #(pinned-messages-list chat-id)}]))}
+   [message/pin-icon "#000000"]
+   [react/text-class {:number-of-lines 1
+                :style (merge typography/paragraph-2 {:margin-left 10 :margin-right 50})} latest-pin-text]
+   [react/view {:style {:position :absolute
+                        :right 22
+                        :height 20
+                        :width 20
+                        :border-radius 8
+                        :justify-content :center
+                        :align-items :center
+                        :background-color quo2.colors/neutral-80-opa-5}}
+    [react/text-class {:style (merge typography/label typography/font-medium)} pins-count]]])))
