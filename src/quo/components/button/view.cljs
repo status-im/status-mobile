f19bf88bb8acb1536482ad4607480498a6881b5c
yqrashawn <namy.19@gmail.com>
2022-10-24 22:31:38 +0800

    style: another commit

--- src/quo/components/button/view.cljs
+++ src/quo/components/button/view.cljs
@@ -1,14 +1,16 @@
 (ns quo.components.button.view
-  (:require [quo.components.animated.pressable :as animation]
-            [quo.react-native :as rn]
-            [quo.haptic :as haptic]
-            [quo.design-system.colors :as colors]
-            [quo.design-system.spacing :as spacing]
-            [quo.components.text :as text]
-            ;; FIXME:
-            [status-im.ui.components.icons.icons :as icons]))
+  (:require
+   [quo.components.animated.pressable   :as animation]
+   [quo.components.text                 :as text]
+   [quo.design-system.colors            :as colors]
+   [quo.design-system.spacing           :as spacing]
+   [quo.haptic                          :as haptic]
+   [quo.react-native                    :as rn]
+   ;; FIXME:
+   [status-im.ui.components.icons.icons :as icons]))
 
-(defn style-container [type]
+(defn style-container
+  [type]
   (merge {:height          44
           :align-items     :center
           :justify-content :center
@@ -19,14 +21,16 @@
            :icon      {}
            nil)))
 
-(defn content-style [type]
+(defn content-style
+  [type]
   (case type
     :primary   (:base spacing/padding-horizontal)
     :secondary (:x-tiny spacing/padding-horizontal)
     :icon      (:tiny spacing/padding-horizontal)
     nil))
 
-(defn themes [theme]
+(defn themes
+  [theme]
   (case theme
     :main         {:icon-color       (:icon-04 @colors/theme)
                    :background-color (:interactive-02 @colors/theme)
@@ -54,44 +58,47 @@
                    :text-color       (:text-01 @colors/theme)
                    :border-color     (:ui-01 @colors/theme)}))
 
-(defn button [{:keys [on-press disabled type theme before after
-                      haptic-feedback haptic-type on-long-press on-press-start
-                      accessibility-label loading border-radius style]
-               :or   {theme           :main
-                      type            :primary
-                      haptic-feedback true
-                      border-radius   8
-                      haptic-type     :selection}}
-              children]
-  (let [theme' (cond
-                 disabled :disabled
-                 :else    theme)
+(defn button
+  [{:keys [on-press disabled type theme before after
+           haptic-feedback haptic-type on-long-press on-press-start
+           accessibility-label loading border-radius style]
+    :or   {theme           :main
+           type            :primary
+           haptic-feedback true
+           border-radius   8
+           haptic-type     :selection}}
+   children]
+  (let [theme'                                                        (cond
+                                                                        disabled :disabled
+                                                                        :else    theme)
         {:keys [icon-color background-color text-color border-color]}
         (themes theme')
 
-        optional-haptic (fn []
-                          (when haptic-feedback
-                            (haptic/trigger haptic-type)))]
-    [animation/pressable (merge {:bg-color            background-color
-                                 :border-radius       border-radius
-                                 :type                type
-                                 :disabled            disabled
-                                 :accessibility-label accessibility-label}
-                                (when border-color
-                                  {:border-color border-color
-                                   :border-width 1})
-                                (when on-press
-                                  {:on-press (fn []
-                                               (optional-haptic)
-                                               (on-press))})
-                                (when on-long-press
-                                  {:on-long-press (fn []
-                                                    (optional-haptic)
-                                                    (on-long-press))})
-                                (when on-press-start
-                                  {:on-press-start (fn []
-                                                     (optional-haptic)
-                                                     (on-press-start))}))
+        optional-haptic                                               (fn []
+                                                                        (when haptic-feedback
+                                                                          (haptic/trigger haptic-type)))]
+    [animation/pressable
+     (merge {:bg-color            background-color
+             :border-radius       border-radius
+             :type                type
+             :disabled            disabled
+             :accessibility-label accessibility-label}
+            (when border-color
+              {:border-color border-color
+               :border-width 1})
+            (when on-press
+              {:on-press (fn []
+                           (optional-haptic)
+                           (on-press))})
+            (when on-long-press
+              {:on-long-press (fn []
+                                (optional-haptic)
+                                (on-long-press))})
+            (when on-press-start
+              {:on-press-start (fn []
+                                 (optional-haptic)
+                                 (on-press-start))}))
+
      [rn/view {:style (merge (style-container type) style)}
       (when before
         [rn/view
@@ -99,17 +106,19 @@
       (when loading
         [rn/view {:style {:position :absolute}}
          [rn/activity-indicator]])
-      [rn/view {:style (merge (content-style type)
-                              (when loading
-                                {:opacity 0}))}
+      [rn/view
+       {:style (merge (content-style type)
+                      (when loading
+                        {:opacity 0}))}
        (cond
          (= type :icon)
          [icons/icon children {:color icon-color}]
 
          (string? children)
-         [text/text {:weight          :medium
-                     :number-of-lines 1
-                     :style           {:color text-color}}
+         [text/text
+          {:weight          :medium
+           :number-of-lines 1
+           :style           {:color text-color}}
           children]
 
          (vector? children)
