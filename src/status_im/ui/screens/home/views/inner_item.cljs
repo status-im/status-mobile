88011083593c5e901222be05e7602f02fd7f6495
ibrkhalil <vampirekid017@gmail.com>
2022-10-30 03:05:27 +0200

    Fix Initial case of creating a group

--- src/status_im/ui/screens/home/views/inner_item.cljs
+++ src/status_im/ui/screens/home/views/inner_item.cljs
@@ -204,8 +204,11 @@
      (first @(re-frame/subscribe [:contacts/contact-two-names-by-identity chat-id])))])
 
 (defn home-list-item [home-item opts]
-  (let [{:keys [chat-id chat-name color group-chat muted emoji highlight edit? public? unviewed-messages-count members]} home-item
-        background-color (when highlight (colors/get-color :interactive-02))]
+  (let [{:keys [chat-id chat-name color group-chat muted emoji highlight edit? public? unviewed-messages-count contacts users]} home-item
+        background-color (when highlight (colors/get-color :interactive-02))
+        group-members-public-keys (->> (concat contacts (keys users))
+                                       (into #{})
+                                       (remove nil?))]
     [react/touchable-opacity (merge {:style {:height 64 :background-color background-color}} opts)
      [:<>
       (when (pos? unviewed-messages-count)
@@ -245,7 +248,7 @@
                                 :margin-right 4}}]
             [quo2.text/text {:weight :medium
                              :style {:color (quo2.colors/theme-colors quo2.colors/neutral-50 quo2.colors/neutral-40)}}
-             (i18n/label :t/members-count {:count (count members)})]]
+             (i18n/label :t/members-count {:count (count group-members-public-keys)})]]
            [quo2.text/text {:monospace true
                             :weight :medium
                             :style {:color (quo2.colors/theme-colors quo2.colors/neutral-50 quo2.colors/neutral-40)}
