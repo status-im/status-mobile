2f4d8accaeae132cfa5661a4667760e68954dbbe
Christoph Pader <christoph@margelo.io>
2022-11-11 11:56:59 +0100

    feat: implement selectors with blurred background

--- src/quo2/components/selectors/selectors.cljs
+++ src/quo2/components/selectors/selectors.cljs
@@ -1,14 +1,19 @@
 (ns quo2.components.selectors.selectors
   (:require [react-native.core :as rn]
             [quo2.foundations.colors :as colors]
-            [quo2.theme :as theme]
             [reagent.core :as reagent]
             [quo2.components.icon :as icons]))
 
-(defn- get-color [checked? disabled?]
-  (if checked?
+(defn- get-color [checked? disabled? blurred-background?]
+  (cond
+    checked?
     (colors/custom-color-by-theme
      :primary 50 60 (when disabled? 30) (when disabled? 30))
+    blurred-background?
+    (colors/theme-colors
+     (colors/alpha colors/neutral-80 (if disabled? 0.05 0.1))
+     (colors/alpha colors/white (if disabled? 0.05 0.1)))
+    :else
     (colors/theme-colors
      (colors/alpha colors/neutral-20 (if disabled? 0.4 1))
      (colors/alpha colors/neutral-70 (if disabled? 0.3 1)))))
@@ -21,7 +26,7 @@
 
 (defn checkbox-prefill [{:keys [default-checked?]}]
   (let [checked? (reagent/atom (or default-checked? false))]
-    (fn [{:keys [on-change disabled? container-style]}]
+    (fn [{:keys [on-change disabled? blurred-background? container-style]}]
       [rn/touchable-without-feedback
        {:on-press (handle-press disabled? on-change checked?)}
        [rn/view
@@ -30,9 +35,13 @@
                  {:height 21
                   :width 21
                   :border-radius 6
-                  :background-color (colors/theme-colors
-                                     (colors/alpha colors/neutral-20 (if disabled? 0.3 1))
-                                     (colors/alpha colors/neutral-70 (if disabled? 0.3 1)))})
+                  :background-color (if blurred-background?
+                                      (colors/theme-colors
+                                       (colors/alpha colors/neutral-80 (if disabled? 0.05 0.1))
+                                       (colors/alpha colors/white (if disabled? 0.05 0.1)))
+                                      (colors/theme-colors
+                                       (colors/alpha colors/neutral-20 (if disabled? 0.3 1))
+                                       (colors/alpha colors/neutral-70 (if disabled? 0.3 1))))})
          :accessibility-label (str "checkbox-" (if @checked? "on" "off"))
          :accessibility-role  :checkbox}
         (when @checked?
@@ -47,7 +56,7 @@
 
 (defn checkbox [{:keys [default-checked?]}]
   (let [checked? (reagent/atom (or default-checked? false))]
-    (fn [{:keys [on-change disabled? container-style]}]
+    (fn [{:keys [on-change disabled? blurred-background? container-style]}]
       [rn/touchable-without-feedback
        {:on-press (handle-press disabled? on-change checked?)}
        [rn/view
@@ -59,13 +68,19 @@
          {:style {:flex             1
                   :border-radius    6
                   :border-width     (if @checked? 0 1)
-                  :background-color (if @checked?
-                                      (get-color @checked? disabled?)
+                  :background-color (cond
+                                      @checked?
+                                      (get-color @checked? disabled? blurred-background?)
+                                      blurred-background?
+                                      (colors/theme-colors
+                                       colors/white-opa-5
+                                       colors/white-opa-10)
+                                      :else
                                       (colors/theme-colors
                                        colors/white
                                        colors/neutral-80-opa-40))
                   :border-color     (if @checked? :none
-                                        (get-color @checked? disabled?))}
+                                        (get-color @checked? disabled? blurred-background?))}
           :accessibility-label (str "checkbox-" (if @checked? "on" "off"))
           :accessibility-role  :checkbox}
          (when @checked?
@@ -78,7 +93,7 @@
 
 (defn radio [{:keys [default-checked?]}]
   (let [checked? (reagent/atom (or default-checked? false))]
-    (fn [{:keys [on-change disabled? container-style]}]
+    (fn [{:keys [on-change disabled? blurred-background? container-style]}]
       [rn/touchable-without-feedback
        {:on-press (handle-press disabled? on-change checked?)}
        [rn/view
@@ -88,9 +103,10 @@
                   :width 20
                   :border-radius 20
                   :border-width 1
-                  :border-color (get-color @checked? disabled?)
-                  :background-color (colors/theme-colors colors/white
-                                                         (colors/alpha colors/neutral-80 0.4))})
+                  :border-color (get-color @checked? disabled? blurred-background?)
+                  :background-color (when-not blurred-background?
+                                      (colors/theme-colors colors/white
+                                                           (colors/alpha colors/neutral-80 0.4)))})
          :accessibility-label (str "radio-" (if @checked? "on" "off"))
          :accessibility-role  :checkbox}
 
@@ -98,7 +114,7 @@
                   {:margin-left :auto
                    :height 14
                    :width 14
-                   :background-color (when @checked? (get-color @checked? disabled?))
+                   :background-color (when @checked? (get-color @checked? disabled? blurred-background?))
                    :border-radius 20
                    :margin-right :auto
                    :margin-top :auto
@@ -106,7 +122,7 @@
 
 (defn toggle [{:keys [default-checked?]}]
   (let [checked? (reagent/atom (or default-checked? false))]
-    (fn [{:keys [on-change disabled? container-style]}]
+    (fn [{:keys [on-change disabled? blurred-background? container-style]}]
       [rn/touchable-without-feedback
        {:on-press (handle-press disabled? on-change checked?)}
        [rn/view
@@ -115,17 +131,20 @@
                  {:height 20
                   :width 30
                   :border-radius 20
-                  :background-color (get-color @checked? disabled?)})
+                  :background-color (get-color @checked? disabled? blurred-background?)})
          :accessibility-label (str "toggle-" (if @checked? "on" "off"))
          :accessibility-role  :checkbox}
         [rn/view {:style
                   {:margin-left (if @checked? 12 2)
                    :height 16
                    :width 16
-                   :background-color (colors/alpha colors/white
-                                                   (if (theme/dark?)
-                                                     (if disabled? 0.3 1)
-                                                     1))
+                   :background-color (if blurred-background?
+                                       (colors/theme-colors
+                                        (colors/alpha colors/white (if disabled? 0.4 1))
+                                        (colors/alpha colors/white (if disabled? 0.3 1)))
+                                       (colors/theme-colors
+                                        (colors/alpha colors/white 1)
+                                        (colors/alpha colors/white (if disabled? 0.4 1))))
                    :border-radius 20
                    :margin-right :auto
                    :margin-top :auto
