89fb9a20b9bc3b70a636ac12ab4952aea659044c
Jamie Caprani <jamiecaprani@gmail.com>
2022-11-14 18:44:55 +0000

    three

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,14 @@ module.exports = {
                 "react-native-reanimated/plugin"
             ]
         },
+        "production":{
+            "presets": [
+                "module:metro-react-native-babel-preset"
+            ],
+            "plugins": [
+                "react-native-reanimated/plugin"
+            ]
+        },
         "test": {
             "presets": [
                 "module:metro-react-native-babel-preset",
