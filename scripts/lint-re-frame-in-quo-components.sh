59d94deed7c3d62cac3edd39f5887f92183c46dc
andrey <motor4ik@gmail.com>
2022-11-16 08:58:05 +0100

    typo

--- scripts/lint-re-frame-in-quo-components.sh
+++ scripts/lint-re-frame-in-quo-components.sh
@@ -5,7 +5,7 @@ CHANGES=$(git diff --no-ext-diff --diff-filter=d --cached --unified=0 --no-prefi
 INVALID_CHANGES=$(echo "$CHANGES" | grep -E '(re-frame/dispatch|rf/dispatch|re-frame/subscribe|rf/subscribe|rf/sub|<sub|>evt|status-im)')
 
 if test -n "$INVALID_CHANGES"; then
-    echo "re-frame dispatch/subscribe ans status-im are not allowed in quo/quo2 components"
+    echo "re-frame dispatch/subscribe and status-im are not allowed in quo/quo2 components"
     echo ''
     echo "$INVALID_CHANGES"
     exit 1
