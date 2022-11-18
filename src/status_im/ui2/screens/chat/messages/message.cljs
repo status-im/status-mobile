da589a6966a4e66d34b98b29a1e30f430d703c7e
yqrashawn <namy.19@gmail.com>
2022-11-18 09:25:56 +0800

    fix: system-message icons and labels

--- src/status_im/ui2/screens/chat/messages/message.cljs
+++ src/status_im/ui2/screens/chat/messages/message.cljs
@@ -317,9 +317,10 @@
       [system-message/system-message
        {:type             :deleted
         :label            (if deleted? :message-deleted :message-deleted-for-you)
-        :labels           {:pinned-a-message (i18n/label :pinned-a-message)
-                           :message-deleted  (i18n/label :message-deleted)
-                           :added            (i18n/label :added)}
+        :labels           {:pinned-a-message        (i18n/label :pinned-a-message)
+                           :message-deleted         (i18n/label :message-deleted)
+                           :message-deleted-for-you (i18n/label :message-deleted-for-you)
+                           :added                   (i18n/label :added)}
         :timestamp-str    timestamp-str
         :non-pressable?   true
         :animate-landing? (if (or deleted-undoable-till deleted-for-me-undoable-till)
