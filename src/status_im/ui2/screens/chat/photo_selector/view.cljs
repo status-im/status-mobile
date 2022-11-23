8af428e8a5c69ceb2a2002d73b212125da355826
Omar Basem <omarbasem.0101@gmail.com>
2022-11-23 09:47:17 +0400

    feat: photo-selector

--- src/status_im/ui2/screens/chat/photo_selector/view.cljs
+++ src/status_im/ui2/screens/chat/photo_selector/view.cljs
@@ -7,40 +7,75 @@
             [utils.re-frame :as rf]
             [reagent.core :as reagent]
             [quo2.components.notifications.info-count :as info-count]
-            [react-native.linear-gradient :as linear-gradient]))
+            [react-native.linear-gradient :as linear-gradient]
+            [status-im.ui2.screens.chat.photo-selector.style :as style]))
 
 (def selected (reagent/atom []))
 
 (defn small-image [item]
-  [rn/image {:source {:uri item}
-             :style  {:width  56
-                      :height 56}}])
+  [rn/view
+   [rn/image {:source {:uri item}
+              :style  {:width         56
+                       :height        56
+                       :border-radius 8
+                       :margin-bottom 20}}]
+   [rn/touchable-opacity
+    {:on-press (fn [] (reset! selected (vec (filter #(-> % (not= item)) @selected))))
+     :style    (style/remove-photo-container)}
+    [quo2/icon :i/close {:color colors/white :size 12}]]])
 
 (defn bottom-gradient []
-  [linear-gradient/linear-gradient
-   {:colors [:black :transparent]
-    :start  {:x 0 :y 1}
-    :end    {:x 0 :y 0}
-    :style  {:width    "100%"
-             :height   174
-             :position :absolute
-             :bottom   0}}
-   [rn/flat-list {:key-fn     (fn [item] item)
-                  :render-fn  small-image
-                  :data       @selected
-                  :horizontal true
-                  }]
-   [rn/view {:style {:flex-direction  :row
-                     :justify-content :space-between
-                     :bottom          40}}
-    [quo2/button {:type     :grey
-                  :style    {:flex 0.48}
-                  :on-press #(println "add text")}
-     (i18n/label :t/add-text)]
-    [quo2/button {:style    {:flex 0.48}
-                  :on-press #(println "test")}
-     (i18n/label :t/send)]]
-   ])
+  [:f>
+   (fn []
+     (let [safe-area (safe-area/use-safe-area)]
+       (when (> (count @selected) 0)
+         [linear-gradient/linear-gradient
+          {:colors [:black :transparent]
+           :start  {:x 0 :y 1}
+           :end    {:x 0 :y 0}
+           :style  (style/gradient-container safe-area)}
+          [rn/flat-list {:key-fn                  (fn [item] item)
+                         :render-fn               small-image
+                         :data                    @selected
+                         :horizontal              true
+                         :content-container-style {:padding-horizontal 20 :margin-top 12}
+                         :separator               [rn/view {:style {:width 12}}]}]
+          [rn/view {:style (style/buttons-container safe-area)}
+           [quo2/button {:type     :grey
+                         :style    {:flex 0.48}
+                         :on-press #(js/alert "Add text: to be implemented")}
+            (i18n/label :t/add-text)]
+           [quo2/button {:style    {:flex 0.48}
+                         :before   :send
+                         :on-press #(do
+                                      (rf/dispatch [:chat.ui/send-current-message])
+                                      (reset! selected [])
+                                      (rf/dispatch [:bottom-sheet/hide]))}
+            (str (i18n/label :t/send) " " (when (> (count @selected) 1) (count @selected)))]]])))])
+
+(defn clear-button []
+  (when (> (count @selected) 0)
+    [rn/touchable-opacity {:on-press #(reset! selected [])
+                           :style    (style/clear-container)}
+     [quo2/text {:weight :medium} (i18n/label :t/clear)]]))
+
+(defn image [item index window-width]
+  [rn/touchable-opacity
+   {:active-opacity 1
+    :on-press       (fn []
+                      (if (some #{item} @selected)
+                        (do
+                          (reset! selected (vec (filter #(-> % (not= item)) @selected)))
+                          (rf/dispatch [:chat.ui/image-unselected item]))
+                        (do
+                          (swap! selected conj item)
+                          (rf/dispatch [:chat.ui/camera-roll-pick item]))))}
+   [rn/image {:source {:uri item}
+              :style  (style/image window-width index)}]
+   (when (some #{item} @selected)
+     [rn/view {:style (style/overlay window-width)}])
+   (when (some #{item} @selected)
+     [info-count/info-count (+ (.indexOf @selected item) 1) (style/image-count)])])
 
 (defn photo-selector []
   (rf/dispatch [:chat.ui/camera-roll-get-photos 20])
@@ -52,58 +87,21 @@
            camera-roll-photos (rf/sub [:camera-roll-photos])]
        [rn/view {:style {:height (- window-height (:top safe-area))}}
         [rn/touchable-opacity
-         {:on-press #(rf/dispatch [:bottom-sheet/hide])
-          :style
-          {:background-color (colors/theme-colors colors/neutral-10 colors/neutral-80)
-           :width            32
-           :height           32
-           :border-radius    10
-           :justify-content  :center
-           :align-items      :center
-           :margin-left      20
-           :margin-bottom    24}}
+         {:on-press #(js/alert "Camera: not implemented")
+          :style    (style/camera-button-container)}
          [quo2/icon :i/camera {:color (colors/theme-colors colors/neutral-100 colors/white)}]]
         [rn/view {:style {:flex-direction :row
                           :position       :absolute
                           :align-self     :center}}
          [quo2/text {:weight :medium} (i18n/label :t/recent)]
-         [rn/view {:style {:background-color (colors/theme-colors colors/neutral-10 colors/neutral-80)
-                           :width            14
-                           :height           14
-                           :border-radius    7
-                           :justify-content  :center
-                           :align-items      :center
-                           :margin-left      7
-                           :margin-top       4}}
+         [rn/view {:style (style/chevron-container)}
           [quo2/icon :i/chevron-down {:color (colors/theme-colors colors/neutral-100 colors/white)}]]]
+        [clear-button]
         [rn/flat-list {:key-fn                  (fn [item] item)
-                       :render-fn               (fn [item index]
-                                                  [rn/touchable-opacity
-                                                   {:active-opacity 1
-                                                    :on-press       (fn []
-                                                                      (if (some #{item} @selected)
-                                                                        (reset! selected (filter #(-> % (not= item)) @selected))
-                                                                        (swap! selected conj item)))}
-                                                   [rn/image {:source {:uri item}
-                                                              :style  {:width         (- (/ window-width 3) 0.67)
-                                                                       :height        (/ window-width 3)
-                                                                       :margin-left   (when (not= (mod index 3) 0) 1)
-                                                                       :margin-bottom 1}}]
-                                                   (when (some #{item} @selected)
-                                                     [rn/view {:style {:position         :absolute
-                                                                       :width            (- (/ window-width 3) 0.67)
-                                                                       :height           (/ window-width 3)
-                                                                       :background-color (colors/theme-colors colors/white-opa-40 colors/neutral-80-opa-40)}}])
-                                                   (when (some #{item} @selected)
-                                                     [info-count/info-count (+ (.indexOf @selected item) 1) {:width         24
-                                                                                                             :height        24
-                                                                                                             :border-radius 8
-                                                                                                             :top           8
-                                                                                                             :right         8}])])
+                       :render-fn               (fn [item index] (image item index window-width))
                        :data                    camera-roll-photos
                        :num-columns             3
                        :content-container-style {:width          "100%"
-                                                 :padding-bottom 40}
+                                                 :padding-bottom (+ (:bottom safe-area) 100)}
                        :style                   {:border-radius 20}}]
-        [bottom-gradient]
-        ]))])
+        [bottom-gradient]]))])
