81b88d1f2b8ea7fd04e9c3fd33b4b74e6745def1
Siddarth Kumar <siddarthkay@gmail.com>
2022-11-30 15:11:15 +0530

    simplify the key-uid retrieval

--- src/status_im2/contexts/quo_preview/qr/qr.cljs
+++ src/status_im2/contexts/quo_preview/qr/qr.cljs
@@ -10,18 +10,20 @@
 (def qr-final-url (reagent/atom "https://media.qrtiger.com/blog/2021/01/imgpsh_fullsize_anim_800.jpeg"))
 
 (defn playground-view []
+
   [:<>
   [rn/view
    [quo2-button/button
     {:style {:margin-vertical 8}
      :on-press #(re-frame/dispatch [:get-qr-from-media-server])}
-    "hey"
+    "get QR from status-go"
     ]
    ]
-   [rn/view
+   [rn/view {:style {:flex-direction :row
+                     :justify-content :center}}
        [rn/image {:source {:uri           @qr-final-url}
-                  :style  {:width         256
-                           :height        256
+                  :style  {:width         303
+                           :height        303
                            :margin-top    50
                            :border-radius 4
                            :margin-right  4}}
@@ -34,23 +36,17 @@
 (fx/defn get-qr-from-media-server
   {:events [:get-qr-from-media-server]}
   [{:keys [db]}]
-  (let [port (get db :mediaserver/port)
-        url "cs2:5vd6SL:KFC:26gAouU6D6A4dCs9LK7jHmXZ3gjVdPczvX7yeusZRHTeR:3HxJ9Qr4H351dPoXjQYsdPX4tK6tV6TkdsHk1xMZEZmL:3"
-        all-accounts (get db :multiaccounts/multiaccounts)
-        all-key-uids (keys all-accounts)
-        all-vals (vals all-accounts)
-;        second-account (nth all-vals 1)
-        first-account (nth all-vals 0)
-        keyuid (get first-account :key-uid)
-        qr-hardcoded-url (str "https://localhost:" port "/QRImagesWithLogo?qrurl=" (js/btoa url) "&keyUid=" keyuid "&imageName=thumbnail")
-        ]
-    (log/info "all :multiaccounts/multiaccounts account =====>" all-accounts)
-    (log/info "all-vals =====>" all-vals)
-;    (log/info "second multiaccount/accounts account =====>" second-account)
-    (log/info "first multiaccount/accounts account =====>" first-account)
-    (reset! qr-final-url qr-hardcoded-url)
-    (log/info "qr-url from media server " qr-hardcoded-url)
-    )
+     (let [port (get db :mediaserver/port)
+           url "https://github.com/yeqown/go-qrcode/"
+           multiaccount (get db :multiaccount)
+           keyuid (get multiaccount :key-uid)
+           qr-hardcoded-url (str "https://localhost:" port "/QRImagesWithLogo?qrurl=" (js/btoa url) "&keyUid=" keyuid "&imageName=thumbnail")
+           ]
+
+       (reset! qr-final-url qr-hardcoded-url)
+       (log/info "qr-url from media server " qr-hardcoded-url)
+       )
+
 )
 
 
