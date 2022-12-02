182b7ed585959039bebcda6b543804a660ff6883
Icaro Motta <icaro.ldm@gmail.com>
2022-12-02 07:34:01 -0300

    Add missing new line

--- src/status_im/test_helpers.clj
+++ src/status_im/test_helpers.clj
@@ -33,6 +33,7 @@
   :args (s/cat :sub-name keyword?
                :args (s/coll-of symbol? :count 1)
                :body (s/* any?)))
+
 (defmacro deftest-sub
   "Defines a test based on `sub-name`, executes `body` and restores the app db.
 
