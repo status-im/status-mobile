51cf65c7400d66a16c39f7f3191a93306023556a
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2022-12-13 10:05:03 +0000

    Communities now correctly listed in pending/joined sections.

--- src/status_im2/contexts/communities/overview/style.cljs
+++ src/status_im2/contexts/communities/overview/style.cljs
@@ -1,17 +1,20 @@
 (ns status-im2.contexts.communities.overview.style
-  (:require [react-native.platform :as platform]
-            [quo2.foundations.colors :as colors]))
+  (:require [react-native.platform :as platform]))
 
 (def preview-user
   {:flex-direction :row
    :align-items    :center
    :margin-top     20})
 
-(def blur-channel-header {:blur-amount 32
-                          :blur-type :xlight
-                          :overlay-color (if platform/ios? colors/white-opa-70 :transparent)
-                          :style {:position :absolute
-                                  :top (if platform/ios? 56 60)
-                                  :height 34
-                                  :width "100%"
-                                  :flex 1}})
+(def blur-channel-header
+  {:position :absolute
+   :top (if platform/ios? 56 60)
+   :height 34
+   :width "100%"
+   :flex 1})
+
+(def join-button
+  {:width "100%"
+   :margin-top 20
+   :margin-left :auto
+   :margin-right :auto})
