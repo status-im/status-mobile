50371f0fb8208645b413f2dd1d835c02e2bc7033
Ajay Sivan <ajayesivan@gmail.com>
2022-12-15 12:19:26 +0530

    Using separator component instead of custom view

--- src/quo2/components/dividers/date.cljs
+++ src/quo2/components/dividers/date.cljs
@@ -1,6 +1,7 @@
 (ns quo2.components.dividers.date
   (:require [react-native.core :as rn]
             [clojure.string :as string]
+            [quo2.components.separator :as separator]
             [quo2.components.markdown.text :as text]
             [quo2.foundations.colors :as colors]))
 
@@ -10,9 +11,7 @@
    [text/text {:weight              :medium
                :accessibility-label :message-datemark-text
                :size :label
-               :style {:color (colors/theme-colors colors/neutral-50 colors/neutral-40)}}
+               :style {:color (colors/theme-colors colors/neutral-50 colors/neutral-40)
+                       :margin-bottom 4}}
     (string/capitalize value)]
-   [rn/view {:width "100%"
-             :height 1
-             :background-color (colors/theme-colors colors/neutral-10 colors/neutral-90)
-             :margin-top 4}]])
+   [separator/separator]])
