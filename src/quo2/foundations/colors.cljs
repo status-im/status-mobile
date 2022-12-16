b86dfdb8b7d743e70959e9af7937c1b9a0a732e4
Omar Basem <omarbasem.0101@gmail.com>
2022-12-16 08:48:45 +0400

    lint
    https://github.com/status-im/status-go/compare/684e9654...b4bdfd3d

--- src/quo2/foundations/colors.cljs
+++ src/quo2/foundations/colors.cljs
@@ -212,21 +212,6 @@
       (let [base-color (get-in colors-map [(keyword color) suffix])]
         (if opacity (alpha base-color (/ opacity 100)) base-color))))))
 
-(defn- opacity->hex [opacity]
-  (case opacity
-    0 "00"
-    10 "1A"
-    20 "33"
-    30 "4d"
-    40 "66"
-    50 "80"
-    60 "99"
-    70 "B3"
-    80 "CC"
-    90 "E6"
-    "FF"))
-
-
 (defn custom-color-by-theme
   "(custom-color-by-theme color suffix-light suffix-dark opacity-light opacity-dark)
    color         :primary/:purple/...
