e21b8d43963532f23eab4d09ae0e490f25c0e05f
Omar Basem <omarbasem.0101@gmail.com>
2022-12-23 07:18:09 +0400

    Group details screen (3) (#14494)
    * group details screen (3)

--- src/status_im/ui2/screens/common/contact_list/view.cljs
+++ src/status_im/ui2/screens/common/contact_list/view.cljs
@@ -1,21 +1,19 @@
 (ns status-im.ui2.screens.common.contact-list.view
-  (:require [quo2.core :as quo]
-            [react-native.core :as rn]
+  (:require [react-native.core :as rn]
             [status-im2.common.contact-list-item.view :as contact-list-item]
-            [utils.re-frame :as rf]))
+            [utils.re-frame :as rf]
+            [quo2.core :as quo]))
 
-(defn contacts-section-header
-  [{:keys [title]}]
+(defn contacts-section-header [{:keys [title]}]
   [quo/divider-label {:label title}])
 
-(defn contact-list
-  [data]
-  (let [contacts (rf/sub [:contacts/filtered-active-sections])]
+(defn contact-list [data]
+  (let [contacts (if (:group data) (rf/sub [:contacts/add-members-sections]) (rf/sub [:contacts/filtered-active-sections]))]
     [rn/section-list
      {:key-fn                         :title
       :sticky-section-headers-enabled false
       :sections                       contacts
       :render-section-header-fn       contacts-section-header
-      :content-container-style        {:padding-bottom 120}
-      :render-data                    data
-      :render-fn                      contact-list-item/contact-list-item}]))
+      :content-container-style        {:padding-bottom 20}
+      :render-data data
+      :render-fn  contact-list-item/contact-list-item}]))
--- src/status_im2/common/contact_list_item/view.cljs
+++ src/status_im2/common/contact_list_item/view.cljs
@@ -1,15 +1,14 @@
 (ns status-im2.common.contact-list-item.view
-  (:require [quo2.core :as quo]
-            [quo2.foundations.colors :as colors]
+  (:require [quo2.foundations.colors :as colors]
+            [status-im2.contexts.chat.home.chat-list-item.style :as style]
+            [utils.re-frame :as rf]
+            [status-im2.common.home.actions.view :as actions]
+            [quo2.core :as quo]
             [react-native.core :as rn]
             [react-native.platform :as platform]
-            [status-im2.common.home.actions.view :as actions]
-            [status-im2.contexts.chat.home.chat-list-item.style :as style]
-            [utils.address :as utils.address]
-            [utils.re-frame :as rf]))
+            [utils.address :as utils.address]))
 
-(defn open-chat
-  [chat-id]
+(defn open-chat [chat-id]
   (let [view-id (rf/sub [:view-id])]
     (when (= view-id :shell-stack)
       (rf/dispatch [:dismiss-keyboard])
@@ -18,62 +17,58 @@
         (rf/dispatch [:chat.ui/navigate-to-chat chat-id]))
       (rf/dispatch [:search/home-filter-changed nil]))))
 
-(defn action-icon
-  [{:keys [public-key] :as item} {:keys [icon group added] :as extra-data}]
-  (let [{:keys [contacts]} group
-        member?            (contains? contacts public-key)]
-    [rn/touchable-opacity
-     {:on-press #(rf/dispatch [:bottom-sheet/show-sheet
-                               {:content (fn [] [actions/actions item extra-data])}])
-      :style    {:position :absolute
-                 :right    20}}
+(defn action-icon [{:keys [public-key] :as item} {:keys [icon group] :as extra-data}]
+  (let [{:keys [contacts admins]} group
+        member?           (contains? contacts public-key)
+        current-pk        (rf/sub [:multiaccount/public-key])
+        admin?            (get admins current-pk)]
+    [rn/touchable-opacity {:on-press #(rf/dispatch [:bottom-sheet/show-sheet
+                                                    {:content (fn [] [actions/actions item extra-data])}])
+                           :style    {:position :absolute
+                                      :right    20}}
      (if (= icon :options)
        [quo/icon :i/options {:size 20 :color (colors/theme-colors colors/neutral-50 colors/neutral-40)}]
-       [quo/checkbox
-        {:default-checked? member?
-         :on-change        (fn [selected]
-                             (if selected
-                               (swap! added conj public-key)
-                               (reset! added (remove #(= % public-key) @added))))}])]))
+       [quo/checkbox {:default-checked? member?
+                      :accessibility-label :contact-toggle-check
+                      :disabled?        (and member? (not admin?))
+                      :on-change        (fn [selected]
+                                          (if-not member?
+                                            (if selected
+                                              (rf/dispatch [:select-participant public-key true])
+                                              (rf/dispatch [:deselect-participant public-key true]))
+                                            (if selected
+                                              (rf/dispatch [:undo-deselect-member public-key true])
+                                              (rf/dispatch [:deselect-member public-key true]))))}])]))
 
-(defn contact-list-item
-  [item _ _ extra-data]
+(defn contact-list-item [item _ _ extra-data]
   (let [{:keys [public-key ens-verified added? images]} item
-        display-name                                    (first (rf/sub
-                                                                [:contacts/contact-two-names-by-identity
-                                                                 public-key]))
-        photo-path                                      (when (seq images)
-                                                          (rf/sub [:chats/photo-path public-key]))
-        current-pk                                      (rf/sub [:multiaccount/public-key])]
-    [rn/touchable-opacity
-     (merge {:style          (style/container)
-             :active-opacity 1
-             :on-press       #(open-chat public-key)
-             :on-long-press  #(rf/dispatch [:bottom-sheet/show-sheet
-                                            {:content (fn [] [actions/actions item extra-data])}])})
-     [quo/user-avatar
-      {:full-name         display-name
-       :profile-picture   photo-path
-       :status-indicator? true
-       :online?           true
-       :size              :small
-       :ring?             false}]
+        display-name (first (rf/sub [:contacts/contact-two-names-by-identity public-key]))
+        photo-path   (when (seq images) (rf/sub [:chats/photo-path public-key]))
+        current-pk   (rf/sub [:multiaccount/public-key])
+        online? (rf/sub [:visibility-status-updates/online? public-key])]
+    [rn/touchable-opacity (merge {:style          (style/container)
+                                  :accessibility-label :contact
+                                  :active-opacity 1
+                                  :on-press       #(open-chat public-key)
+                                  :on-long-press  #(rf/dispatch [:bottom-sheet/show-sheet
+                                                                 {:content (fn [] [actions/actions item extra-data])}])})
+     [quo/user-avatar {:full-name         display-name
+                       :profile-picture   photo-path
+                       :status-indicator? true
+                       :online?           online?
+                       :size              :small
+                       :ring?             false}]
      [rn/view {:style {:margin-left 8}}
       [rn/view {:style {:flex-direction :row}}
        [quo/text {:weight :semi-bold} display-name]
        (if ens-verified
          [rn/view {:style {:margin-left 5 :margin-top 4}}
-          [quo/icon :i/verified
-           {:no-color true :size 12 :color (colors/theme-colors colors/success-50 colors/success-60)}]]
+          [quo/icon :i/verified {:no-color true :size 12 :color (colors/theme-colors colors/success-50 colors/success-60)}]]
          (when added?
            [rn/view {:style {:margin-left 5 :margin-top 4}}
-            [quo/icon :i/contact
-             {:no-color true
-              :size     12
-              :color    (colors/theme-colors colors/primary-50 colors/primary-60)}]]))]
-      [quo/text
-       {:size  :paragraph-1
-        :style {:color (colors/theme-colors colors/neutral-50 colors/neutral-40)}}
+            [quo/icon :i/contact {:no-color true :size 12 :color (colors/theme-colors colors/primary-50 colors/primary-60)}]]))]
+      [quo/text {:size  :paragraph-1
+                 :style {:color (colors/theme-colors colors/neutral-50 colors/neutral-40)}}
        (utils.address/get-shortened-address public-key)]]
      (when-not (= current-pk public-key)
        [action-icon item extra-data])]))
