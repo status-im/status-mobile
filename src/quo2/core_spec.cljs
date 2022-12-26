97780d4e7d5edbacf607e023260eaae102dee998
Jamie Caprani <jamiecaprani@gmail.com>
2022-12-26 09:14:26 +0000

    chore: use banner from quo library

--- src/quo2/core_spec.cljs
+++ src/quo2/core_spec.cljs
@@ -1,5 +1,5 @@
 (ns quo2.core-spec
-  (:require [quo2.components.banners.--tests--.banner-component-spec]
+  (:require [quo2.components.banners.banner.component-spec]
             [quo2.components.buttons.--tests--.buttons-component-spec]
             [quo2.components.counter.--tests--.counter-component-spec]
             [quo2.components.dividers.--tests--.divider-label-component-spec]
