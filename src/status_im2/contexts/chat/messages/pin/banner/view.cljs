1e01b1d40ecf83d0be5e0eddb93bd1495a66e5cf
ibrkhalil <vampirekid017@gmail.com>
2022-12-27 17:24:21 +0200

    Clean

--- src/status_im2/contexts/chat/messages/pin/banner/view.cljs
+++ src/status_im2/contexts/chat/messages/pin/banner/view.cljs
@@ -4,20 +4,20 @@
 
 (defn banner
   [chat-id]
-  (let [pinned-messages    (rf/sub [:chats/pinned chat-id])
+  (let [pinned-messages (rf/sub [:chats/pinned chat-id])
 
-        latest-pin-id      (-> pinned-messages
-                               vals
-                               last
-                               (get :message-id))
-        latest-pin-content (-> [:chats/chat-messages chat-id]
-                               rf/sub
-                               (get latest-pin-id)
-                               (get-in [:content :text]))
-        pins-count         (count (seq pinned-messages))]
+        latest-pin-id   (-> pinned-messages
+                            vals
+                            last
+                            (get :message-id))
+        latest-pin-text (-> [:chats/chat-messages chat-id]
+                            rf/sub
+                            (get latest-pin-id)
+                            (get-in [:content :text]))
+        pins-count      (count (seq pinned-messages))]
     (when (> pins-count 0)
       ;; TODO (flexsurfer) this should be banner component in quo2
       [pinned-banner/pinned-banner
-       {:latest-pin-text latest-pin-content
+       {:latest-pin-text latest-pin-text
         :pins-count      pins-count
         :on-press        #(rf/dispatch [:bottom-sheet/show-sheet :pinned-messages-list chat-id])}])))
