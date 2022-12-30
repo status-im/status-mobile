dd253af0bb727189cf87fab3a454f871b168d608
ibrahem <vampirekid017@gmail.com>
2022-12-30 17:21:32 +0200

    Clean

--- src/status_im2/common/home/actions/view.cljs
+++ src/status_im2/common/home/actions/view.cljs
@@ -1,13 +1,12 @@
 (ns status-im2.common.home.actions.view
-  (:require ;;TODO move to
-   ;;status-im2
-   [i18n.i18n :as i18n]
-   [quo2.components.drawers.action-drawers :as drawer]
-   [status-im.chat.models :as chat.models]
-   [status-im2.common.bottom-sheet.view :refer [close-bottom-sheet-fn]]
-   [status-im2.common.confirmation-drawer.view :as confirmation-drawer] ;;TODO move to
-   [status-im2.common.constants :as constants]
-   [utils.re-frame :as rf]))
+  (:require [i18n.i18n :as i18n]
+            [quo2.components.drawers.action-drawers :as drawer]
+            [status-im.chat.models :as chat.models]
+            [status-im2.common.confirmation-drawer.view :as confirmation-drawer] ;;TODO move to
+                                                                                 ;;status-im2
+            [status-im2.common.constants :as constants]
+            [utils.re-frame :as rf]
+            [status-im2.common.bottom-sheet.view :refer [close-bottom-sheet-fn]]))
 
 (defn- entry
   [{:keys [icon label on-press danger? sub-label chevron? add-divider? accessibility-label]}]
