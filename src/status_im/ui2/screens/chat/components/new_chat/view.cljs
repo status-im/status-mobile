bb13230ba51860d3b53ca225a381df6c138cb98a
ibrkhalil <vampirekid017@gmail.com>
2023-01-03 23:05:12 +0200

    Issue 4

--- src/status_im/ui2/screens/chat/components/new_chat/view.cljs
+++ src/status_im/ui2/screens/chat/components/new_chat/view.cljs
@@ -13,7 +13,8 @@
             [quo2.components.markdown.text :as text]
             [status-im.ui.components.invite.events :as invite.events]
             [status-im.ui2.screens.chat.components.new-chat.styles :as style]
-            [status-im2.common.bottom-sheet.view :as bottom-sheet]))
+            [status-im2.common.bottom-sheet.view :as bottom-sheet]
+            [quo.react :as quo.react]))
 
 (defn- hide-sheet-and-dispatch
   [event]
@@ -44,17 +45,17 @@
      :size   :paragraph-1
      :style  {:margin-bottom 2
               :margin-top    20}}
-    "You have no contacts"]
+    (i18n/label :t/you-have-no-contacts)]
    [text/text
     {:weight :regular
      :size   :label
      :style  {:margin-bottom 20}}
-    "Invite your friends and family to Status"]
+    (i18n/label :t/invite-friends-and-family)]
    [quo2/button
     {:type     :primary
      :style    {:margin-bottom 12}
      :on-press #(rf/dispatch [::invite.events/share-link nil])}
-    "Invite friends"]
+    (i18n/label :t/invite-friends)]
    [quo2/button
     {:type     :grey
      :on-press #(hide-sheet-and-dispatch [:open-modal :new-contact])}
@@ -62,62 +63,66 @@
 
 (defn contact-selection-list
   []
-  (let [contacts                                     (rf/sub
-                                                      [:contacts/sorted-and-grouped-by-first-letter])
-        selected-contacts-count                      (rf/sub [:selected-contacts-count])
-        one-contact-selected?                        (= selected-contacts-count 1)
-        contacts-selected?                           (pos? selected-contacts-count)
-        {:keys [names public-key]}                   (-> contacts first :data first)
-        added?                                       (reagent/atom '())
-        {:keys [nickname ens-name three-words-name]} names
-        first-username                               (or ens-name nickname three-words-name)
-        no-contacts?                                 (empty? contacts)]
-    [react/view {:style {:flex 1}}
-     [quo2/button
-      {:type                      :grey
-       :icon                      true
-       :on-press                  #(bottom-sheet/close-bottom-sheet-fn nil)
-       :style                     style/contact-selection-close
-       :override-background-color (quo2.colors/theme-colors quo2.colors/neutral-10
-                                                            quo2.colors/neutral-90)}
-      :i/close]
-     [react/view style/contact-selection-heading
-      [quo2/text
-       {:weight :semi-bold
-        :size   :heading-1
-        :style  {:color (quo2.colors/theme-colors quo2.colors/neutral-100 quo2.colors/white)}}
-       (i18n/label :t/new-chat)]
-      (when-not no-contacts?
-        [quo2/text
-         {:size   :paragraph-2
-          :weight :regular
-          :style  {:color (quo2.colors/theme-colors quo2.colors/neutral-40 quo2.colors/neutral-50)}}
-         (i18n/label :t/selected-count-from-max
-                     {:selected (inc selected-contacts-count)
-                      :max      constants/max-group-chat-participants})])]
-     [react/view
-      {:style {:height        430
-               :margin-bottom -20}}
-      (if no-contacts?
-        [no-contacts-view]
-        [contact-list/contact-list
-         {:icon              :check
-          :group             nil
-          :added?            added?
-          :search?           false
-          :start-a-new-chat? true
-          :on-toggle         on-toggle}])]
-     (when contacts-selected?
-       [toolbar/toolbar
-        {:show-border? false
-         :center       [button/button
-                        {:type                :primary
-                         :accessibility-label :next-button
-                         :on-press            #(do
-                                                 (if one-contact-selected?
-                                                   (hide-sheet-and-dispatch [:chat.ui/start-chat
-                                                                             public-key])
-                                                   (hide-sheet-and-dispatch [:navigate-to :new-group])))}
-                        (if one-contact-selected?
-                          (i18n/label :t/chat-with {:selected-user first-username})
-                          (i18n/label :t/setup-group-chat))]}])]))
+  [:f>
+   (fn []
+     (quo.react/effect! #(rf/dispatch [:clear-contacts]) [])
+     (let [contacts                                     (rf/sub
+                                                         [:contacts/sorted-and-grouped-by-first-letter])
+           selected-contacts-count                      (rf/sub [:selected-contacts-count])
+           window-height                                (rf/sub [:dimensions/window-height])
+           one-contact-selected?                        (= selected-contacts-count 1)
+           contacts-selected?                           (pos? selected-contacts-count)
+           {:keys [names public-key]}                   (-> contacts first :data first)
+           added?                                       (reagent/atom '())
+           {:keys [nickname ens-name three-words-name]} names
+           first-username                               (or ens-name nickname three-words-name)
+           no-contacts?                                 (empty? contacts)]
+       [react/view {:style {:height (* window-height 0.95)}}
+        [quo2/button
+         {:type                      :grey
+          :icon                      true
+          :on-press                  #(bottom-sheet/close-bottom-sheet-fn nil)
+          :style                     style/contact-selection-close
+          :override-background-color (quo2.colors/theme-colors quo2.colors/neutral-10
+                                                               quo2.colors/neutral-90)}
+         :i/close]
+        [react/view style/contact-selection-heading
+         [quo2/text
+          {:weight :semi-bold
+           :size   :heading-1
+           :style  {:color (quo2.colors/theme-colors quo2.colors/neutral-100 quo2.colors/white)}}
+          (i18n/label :t/new-chat)]
+         (when-not no-contacts?
+           [quo2/text
+            {:size   :paragraph-2
+             :weight :regular
+             :style  {:color (quo2.colors/theme-colors quo2.colors/neutral-40 quo2.colors/neutral-50)}}
+            (i18n/label :t/selected-count-from-max
+                        {:selected (inc selected-contacts-count)
+                         :max      constants/max-group-chat-participants})])]
+        [react/view
+         {:style {:height        430
+                  :margin-bottom -20}}
+         (if no-contacts?
+           [no-contacts-view]
+           [contact-list/contact-list
+            {:icon              :check
+             :group             nil
+             :added?            added?
+             :search?           false
+             :start-a-new-chat? true
+             :on-toggle         on-toggle}])]
+        (when contacts-selected?
+          [toolbar/toolbar
+           {:show-border? false
+            :center       [button/button
+                           {:type                :primary
+                            :accessibility-label :next-button
+                            :on-press            #(do
+                                                    (if one-contact-selected?
+                                                      (hide-sheet-and-dispatch [:chat.ui/start-chat
+                                                                                public-key])
+                                                      (hide-sheet-and-dispatch [:navigate-to :new-group])))}
+                           (if one-contact-selected?
+                             (i18n/label :t/chat-with {:selected-user first-username})
+                             (i18n/label :t/setup-group-chat))]}])]))])
