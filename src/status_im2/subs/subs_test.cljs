cfc6523bd38b9e2f4407a5ac876f7a6cc1ace696
Omar Basem <omarbasem.0101@gmail.com>
2023-01-05 09:18:26 +0400

    lint

--- src/status_im2/subs/subs_test.cljs
+++ src/status_im2/subs/subs_test.cljs
@@ -29,18 +29,18 @@
 (deftest login-ma-keycard-pairing
   (testing "returns nil when no :multiaccounts/login"
     (let [res (onboarding/login-ma-keycard-pairing
-                {:multiaccounts/login nil
-                 :multiaccounts/multiaccounts
-                 {"0x1" {:keycard-pairing "keycard-pairing-code"}}}
-                {})]
+               {:multiaccounts/login         nil
+                :multiaccounts/multiaccounts
+                {"0x1" {:keycard-pairing "keycard-pairing-code"}}}
+               {})]
       (is (nil? res))))
 
   (testing "returns :keycard-pairing when :multiaccounts/login is present"
     (let [res (onboarding/login-ma-keycard-pairing
-                {:multiaccounts/login {:key-uid "0x1"}
-                 :multiaccounts/multiaccounts
-                 {"0x1" {:keycard-pairing "keycard-pairing-code"}}}
-                {})]
+               {:multiaccounts/login         {:key-uid "0x1"}
+                :multiaccounts/multiaccounts
+                {"0x1" {:keycard-pairing "keycard-pairing-code"}}}
+               {})]
       (is (= res "keycard-pairing-code")))))
 
 (def messages-state
