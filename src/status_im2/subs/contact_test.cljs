9108585f120a6d5e4dcb0c2a6a6fc6354e50d009
ibrahem <vampirekid017@gmail.com>
2023-01-07 18:06:57 +0200

    Lint

--- src/status_im2/subs/contact_test.cljs
+++ src/status_im2/subs/contact_test.cljs
@@ -1,7 +1,7 @@
 (ns status-im2.subs.contact-test
   (:require [cljs.test :refer [is testing]]
             [re-frame.db :as rf-db]
-            [status-im.test-helpers :as h]
+            [test-helpers.unit :as h]
             status-im2.subs.contact
             [utils.re-frame :as rf]))
 
