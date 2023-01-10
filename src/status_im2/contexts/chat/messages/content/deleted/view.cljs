c46064b47611a1df5dd0c1d3da91d2ec10fc7eab
yqrashawn <namy.19@gmail.com>
2023-01-10 15:04:56 +0800

    feat: add :deleted-by
    Signed-off-by: yqrashawn <namy.19@gmail.com>

--- src/status_im2/contexts/chat/messages/content/deleted/view.cljs
+++ src/status_im2/contexts/chat/messages/content/deleted/view.cljs
@@ -1,16 +1,20 @@
 (ns status-im2.contexts.chat.messages.content.deleted.view
-  (:require [utils.i18n :as i18n]
-            [quo2.core :as quo]))
+  (:require [quo2.core :as quo]
+            [react-native.core :as rn]
+            [utils.i18n :as i18n]))
 
 (defn deleted-message
-  [{:keys [deleted? deleted-undoable-till timestamp-str deleted-for-me-undoable-till]}]
-  [quo/system-message
-   {:type             :deleted
-    :label            (if deleted? :message-deleted :message-deleted-for-you)
-    :labels           {:pinned-a-message        (i18n/label :t/pinned-a-message)
-                       :message-deleted         (i18n/label :t/message-deleted-for-everyone)
-                       :message-deleted-for-you (i18n/label :t/message-deleted-for-you)
-                       :added                   (i18n/label :t/added)}
-    :timestamp-str    timestamp-str
-    :non-pressable?   true
-    :animate-landing? (or deleted-undoable-till deleted-for-me-undoable-till)}])
+  [{:keys [deleted? deleted-by deleted-undoable-till timestamp-str deleted-for-me-undoable-till]}]
+  (let [msg [quo/system-message
+             {:type             :deleted
+              :label            (if deleted? :message-deleted :message-deleted-for-you)
+              :labels           {:pinned-a-message        (i18n/label :t/pinned-a-message)
+                                 :message-deleted         (i18n/label :t/message-deleted-for-everyone)
+                                 :message-deleted-for-you (i18n/label :t/message-deleted-for-you)
+                                 :added                   (i18n/label :t/added)}
+              :timestamp-str    timestamp-str
+              :non-pressable?   true
+              :animate-landing? (or deleted-undoable-till deleted-for-me-undoable-till)}]]
+    (if deleted-by
+      [rn/view {:style {:border-width 1 :border-color :blue}} msg]
+      msg)))
