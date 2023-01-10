02a1c3597f0f775bb1c9dd358ef655ea56b9b590
yqrashawn <namy.19@gmail.com>
2023-01-10 10:02:23 +0800

    feat: undo delete with toast (#14618)

--- src/status_im2/contexts/chat/messages/delete_message/events_test.cljs
+++ src/status_im2/contexts/chat/messages/delete_message/events_test.cljs
@@ -15,11 +15,46 @@
           (let [result-message (get-in (delete-message/delete {:db db} message 1000)
                                        [:db :messages cid mid])]
             (is (= (:id result-message) mid))
-            (is (true? (:deleted? result-message)))
-            (is (= (:deleted-undoable-till result-message) 1001))))
+            (is (true? (:deleted? result-message)) "mark message :deleted?")
+            (is (= (:deleted-undoable-till result-message) 1001) "set message undo timelimit")))
+        (testing "delete with pending deletes"
+          (let [db      (-> db
+                            (update-in [:messages cid "pending-delete-message"]
+                                       assoc
+                                       :deleted?              true
+                                       :deleted-undoable-till 0
+                                       :whisper-timestamp     0)
+                            (update-in [:toasts :toasts :delete-message-for-everyone]
+                                       assoc
+                                       :message-deleted-for-everyone-count 1
+                                       :message-deleted-for-everyone-undos [{:message-id
+                                                                             "pending-delete-message"
+                                                                             :chat-id    cid}]))
+                effects (delete-message/delete {:db db} message 1000)]
+            (is (= (get-in effects [:db :messages cid mid :deleted-undoable-till])
+                   (get-in effects [:db :messages cid "pending-delete-message" :deleted-undoable-till])
+                   1001)
+                "sync all pending delete undo timelimit")
+            (let [upsert-toast (-> effects :dispatch-n second)]
+              (is (= (-> upsert-toast last :message-deleted-for-everyone-count) 2)
+                  "+1 pending deletes")
+              (is
+               (and
+                (-> upsert-toast
+                    last
+                    :message-deleted-for-everyone-undos
+                    first
+                    :message-id
+                    (= "pending-delete-message"))
+                (-> upsert-toast
+                    last
+                    :message-deleted-for-everyone-undos
+                    second
+                    :message-id
+                    (= mid)))
+               "pending deletes are in order"))))
         (testing "return nil if message not in db"
-          (is (= (delete-message/delete {:db {:messages []}} message 1000)
-                 nil)))))))
+          (is (= (delete-message/delete {:db {:messages []}} message 1000) nil)))))))
 
 (deftest undo-delete
   (let [db      {:messages {cid {mid {:id mid :whisper-timestamp 1}}}}
@@ -30,14 +65,11 @@
                                         [:messages cid mid]
                                         assoc
                                         :deleted?              true
-                                        :deleted-undoable-till
-                                        (+ (datetime/timestamp) 1000))
-              result-message (get-in (delete-message/undo {:db db} message)
-                                     [:db :messages cid mid])]
+                                        :deleted-undoable-till (+ (datetime/timestamp) 1000))
+              result-message (get-in (delete-message/undo {:db db} message) [:db :messages cid mid])]
           (is (= (:id result-message) mid))
           (is (nil? (:deleted? result-message)))
           (is (nil? (:deleted-undoable-till result-message)))))
-
       (testing "remain deleted when undo after timelimit"
         (let [db             (update-in db
                                         [:messages cid mid]
@@ -48,47 +80,46 @@
           (is (= (:id result-message) mid))
           (is (nil? (:deleted-undoable-till result-message)))
           (is (true? (:deleted? result-message)))))
-
       (testing "return nil if message not in db"
-        (is (= (delete-message/undo {:db {:messages []}} message)
-               nil))))))
+        (is (= (delete-message/undo {:db {:messages []}} message) nil))))))
 
 (deftest delete-and-send
-  (let [db      {:messages {cid {mid {:id mid}}}}
+  (let [db      {:messages {cid {mid {:id mid :deleted? true :deleted-undoable-till 0}}}}
         message {:message-id mid :chat-id cid}]
     (testing "delete and send"
       (testing "dispatch right rpc call fx"
-        (let [expected-db {:messages {cid {mid {:id mid}}}}
-              effects     (delete-message/delete-and-send {:db db} message)
+        (let [expected-db {:messages {cid {mid {:id mid :deleted? true}}}}
+              effects     (delete-message/delete-and-send {:db db} message false)
               result-db   (:db effects)
               rpc-calls   (:json-rpc/call effects)]
           (is (= result-db expected-db))
           (is (= (count rpc-calls) 1))
-          (is (= (-> rpc-calls
-                     first
-                     :method)
-                 "wakuext_deleteMessageAndSend"))
-          (is (= (-> rpc-calls
-                     first
-                     :params
-                     count)
-                 1))
-          (is (= (-> rpc-calls
-                     first
-                     :params
-                     first)
-                 mid))))
+          (is (= (-> rpc-calls first :method) "wakuext_deleteMessageAndSend"))
+          (is (= (-> rpc-calls first :params count) 1))
+          (is (= (-> rpc-calls first :params first) mid))))
       (testing "clean undo timer"
-        (let [expected-db {:messages {cid {mid {:id mid}}}}
+        (let [expected-db {:messages {cid {mid {:id mid :deleted? true}}}}
               effects     (delete-message/delete-and-send
                            {:db (update-in db
-                                           [:messages cid mid
-                                            :deleted-undoable-till]
+                                           [:messages cid mid :deleted-undoable-till]
                                            (constantly (datetime/timestamp)))}
-                           message)
+                           message
+                           false)
               result-db   (:db effects)]
           (is (= result-db expected-db))))
+      (testing "before deleted locally"
+        (let [effects (delete-message/delete-and-send
+                       {:db (update-in db [:messages cid mid] dissoc :deleted?)}
+                       message
+                       false)]
+          (is (-> effects :db nil?) "not delete and send")))
+      (testing "before undo timelimit"
+        (with-redefs [datetime/timestamp (constantly 1)]
+          (let [effects (delete-message/delete-and-send
+                         {:db (update-in db [:messages cid mid] assoc :deleted-undoable-till 2)}
+                         message
+                         false)]
+            (is (-> effects :db nil?)))))
       (testing "return nil if message not in db"
-        (is (= (delete-message/delete-and-send {:db {:messages []}}
-                                               message)
+        (is (= (delete-message/delete-and-send {:db {:messages []}} message false)
                nil))))))
--- src/status_im2/contexts/chat/messages/delete_message_for_me/events.cljs
+++ src/status_im2/contexts/chat/messages/delete_message_for_me/events.cljs
@@ -1,8 +1,11 @@
 (ns status-im2.contexts.chat.messages.delete-message-for-me.events
-  (:require [status-im.chat.models.message-list :as message-list]
-            [utils.datetime :as datetime]
-            [taoensso.timbre :as log]
-            [utils.re-frame :as rf]))
+  (:require
+   [i18n.i18n :as i18n]
+   [quo2.foundations.colors :as colors]
+   [status-im.chat.models.message-list :as message-list]
+   [taoensso.timbre :as log]
+   [utils.datetime :as datetime]
+   [utils.re-frame :as rf]))
 
 (defn- update-db-clear-undo-timer
   [db chat-id message-id]
@@ -42,15 +45,37 @@
   {:events [:chat.ui/delete-message-for-me]}
   [{:keys [db]} {:keys [chat-id message-id]} undo-time-limit-ms]
   (when (get-in db [:messages chat-id message-id])
-    (assoc
-     (message-list/rebuild-message-list
-      {:db (update-db-delete-locally db chat-id message-id undo-time-limit-ms)}
-      chat-id)
-     :utils/dispatch-later
-     [{:dispatch [:chat.ui/delete-message-for-me-and-sync
-                  {:chat-id    chat-id
-                   :message-id message-id}]
-       :ms       undo-time-limit-ms}])))
+    (let [existing-undo-toast (get-in db [:toasts :toasts :delete-message-for-me])
+          toast-count         (inc (get existing-undo-toast :message-deleted-for-me-count 0))
+          existing-undos      (-> existing-undo-toast
+                                  (get :message-deleted-for-me-undos [])
+                                  (conj {:message-id message-id :chat-id chat-id}))]
+      (assoc
+       (message-list/rebuild-message-list
+        {:db (reduce
+              ;; sync all pending deletes' undo timelimit, extend to the latest one
+              (fn [db-acc {:keys [message-id chat-id]}]
+                (update-db-delete-locally db-acc chat-id message-id undo-time-limit-ms))
+              db
+              existing-undos)}
+        chat-id)
+       :dispatch-n           [[:toasts/close :delete-message-for-me]
+                              [:toasts/upsert :delete-message-for-me
+                               {:icon :info
+                                :icon-color colors/danger-50-opa-40
+                                :message-deleted-for-me-count toast-count
+                                :message-deleted-for-me-undos existing-undos
+                                :text (i18n/label-pluralize toast-count
+                                                            :t/message-deleted-for-you-count)
+                                :duration undo-time-limit-ms
+                                :undo-duration (/ undo-time-limit-ms 1000)
+                                :undo-on-press #(do (rf/dispatch
+                                                     [:chat.ui/undo-all-delete-message-for-me])
+                                                    (rf/dispatch [:toasts/close
+                                                                  :delete-message-for-me]))}]]
+       :utils/dispatch-later [{:dispatch [:chat.ui/delete-message-for-me-and-sync
+                                          {:chat-id chat-id :message-id message-id}]
+                               :ms       undo-time-limit-ms}]))))
 
 (rf/defn undo
   {:events [:chat.ui/undo-delete-message-for-me]}
@@ -60,17 +85,37 @@
      {:db (update-db-undo-locally db chat-id message-id)}
      chat-id)))
 
+(rf/defn undo-all
+  {:events [:chat.ui/undo-all-delete-message-for-me]}
+  [{:keys [db]}]
+  (when-let [pending-undos (get-in db
+                                   [:toasts :toasts :delete-message-for-me
+                                    :message-deleted-for-me-undos])]
+    {:dispatch-n (mapv #(vector :chat.ui/undo-delete-message-for-me %) pending-undos)}))
+
+(defn- check-before-delete-and-sync
+  "Make sure message alredy deleted-for-me? locally and undo timelimit has passed"
+  [db chat-id message-id]
+  (let [message                                                (get-in db [:messages chat-id message-id])
+        {:keys [deleted-for-me? deleted-for-me-undoable-till]} message]
+    (and deleted-for-me?
+         deleted-for-me-undoable-till
+         (>= (datetime/timestamp) deleted-for-me-undoable-till))))
+
 (rf/defn delete-and-sync
   {:events [:chat.ui/delete-message-for-me-and-sync]}
-  [{:keys [db]} {:keys [message-id chat-id]}]
-  (when (get-in db [:messages chat-id message-id])
-    {:db            (update-db-clear-undo-timer db chat-id message-id)
-     :json-rpc/call [{:method      "wakuext_deleteMessageForMeAndSync"
-                      :params      [chat-id message-id]
-                      :js-response true
-                      :on-error    #(log/error "failed to delete message for me, message id: "
-                                               {:message-id message-id :error %})
-                      :on-success  #(rf/dispatch [:sanitize-messages-and-process-response %])}]}))
+  [{:keys [db]} {:keys [message-id chat-id]} force?]
+  (when-let [message (get-in db [:messages chat-id message-id])]
+    (when (or force? (check-before-delete-and-sync db chat-id message-id))
+      {:db            (update-db-clear-undo-timer db chat-id message-id)
+       :json-rpc/call [{:method      "wakuext_deleteMessageForMeAndSync"
+                        :params      [chat-id message-id]
+                        :js-response true
+                        :on-error    #(log/error
+                                       "failed to delete message for me, message id: "
+                                       {:message-id message-id :error %})
+                        :on-success  #(rf/dispatch [:sanitize-messages-and-process-response
+                                                    %])}]})))
 
 (defn- filter-pending-sync-messages
   "traverse all messages find not yet synced deleted-for-me? messages"
@@ -86,4 +131,4 @@
   {:events [:chat.ui/sync-all-deleted-for-me-messages]}
   [{:keys [db] :as cofx}]
   (let [pending-sync-messages (reduce-kv filter-pending-sync-messages [] (:messages db))]
-    (apply rf/merge cofx (map delete-and-sync pending-sync-messages))))
+    (apply rf/merge cofx (map #(delete-and-sync % true) pending-sync-messages))))
