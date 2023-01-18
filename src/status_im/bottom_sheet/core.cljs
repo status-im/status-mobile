1e6068d46cc91f923acf5c373bfd382e45c4d8fe
ibrahem <vampirekid017@gmail.com>
2023-01-18 12:49:13 +0200

    Lint

--- src/status_im/bottom_sheet/core.cljs
+++ src/status_im/bottom_sheet/core.cljs
@@ -34,16 +34,13 @@
 (rf/defn hide-bottom-sheet
   {:events [:bottom-sheet/hide]}
   [{:keys [db]} on-cancel]
-  (let [animation-delay (get-in db
-                                [:bottom-sheet/config :animation-delay]
-                                constants/bottom-sheet-animation-delay)]
-    {:dispatch       [:bottom-sheet/update-config
-                      {:config :show-bottom-sheet?
-                       :value  false}]
-     :dispatch-later [{:dispatch [:bottom-sheet/hide-navigation-overlay]
-                       :ms       constants/bottom-sheet-animation-delay}
-                      {:dispatch [:bottom-sheet/reset on-cancel]
-                       :ms       constants/bottom-sheet-animation-delay}]}))
+  {:dispatch       [:bottom-sheet/update-config
+                    {:config :show-bottom-sheet?
+                     :value  false}]
+   :dispatch-later [{:dispatch [:bottom-sheet/hide-navigation-overlay]
+                     :ms       constants/bottom-sheet-animation-delay}
+                    {:dispatch [:bottom-sheet/reset on-cancel]
+                     :ms       constants/bottom-sheet-animation-delay}]})
 
 (rf/defn hide-bottom-sheet-navigation-overlay
   {:events [:bottom-sheet/hide-navigation-overlay]}
