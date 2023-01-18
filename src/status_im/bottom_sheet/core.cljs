861858ffc2f395c5aaadc04bd1979354eeb3183c
ibrahem <vampirekid017@gmail.com>
2023-01-18 13:13:12 +0200

    cleanup

--- src/status_im/bottom_sheet/core.cljs
+++ src/status_im/bottom_sheet/core.cljs
@@ -21,7 +21,6 @@
 (rf/defn reset-bottom-sheet
   {:events [:bottom-sheet/reset]}
   [{:keys [db]} on-cancel]
-  (when (fn? on-cancel) (on-cancel))
   {:db (assoc db
               :bottom-sheet/config
               {:content-height      nil
@@ -37,19 +36,11 @@
   {:dispatch       [:bottom-sheet/update-config
                     {:config :show-bottom-sheet?
                      :value  false}]
-   :dispatch-later [{:dispatch [:bottom-sheet/hide-navigation-overlay]
-                     :ms       constants/bottom-sheet-animation-delay}
-                    {:dispatch [:bottom-sheet/reset]
+   :dispatch-later [{:dispatch [:bottom-sheet/reset nil]
                      :ms       constants/bottom-sheet-animation-delay}]})
 
 (rf/defn hide-bottom-sheet-and-dispatch
   {:events [:bottom-sheet/hide-and-dispatch]}
   [{:keys [db]} on-cancel]
-  {:bottom-sheet/hide nil
-   :dispatch-later [{:dispatch [:bottom-sheet/reset on-cancel]
-                     :ms       constants/bottom-sheet-animation-delay}]})
-
-(rf/defn hide-bottom-sheet-navigation-overlay
-  {:events [:bottom-sheet/hide-navigation-overlay]}
-  [{}]
-  {:hide-bottom-sheet-and-dispatch nil})
+  (when (fn? on-cancel) (on-cancel))
+  {:bottom-sheet/hide nil})
