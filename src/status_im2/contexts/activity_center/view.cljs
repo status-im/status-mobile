acb51d61e40805b4e48ba3aa289a43873c36d078
Icaro Motta <icaro.ldm@gmail.com>
2023-01-24 20:18:47 -0300

    Add accessibility labels

--- src/status_im2/contexts/activity_center/view.cljs
+++ src/status_im2/contexts/activity_center/view.cljs
@@ -66,30 +66,39 @@
       :default-active      filter-type
       :data                [{:id    types/no-type
                              :label (i18n/label :t/all)}
-                            {:id                types/admin
-                             :label             (i18n/label :t/admin)
-                             :notification-dot? (contains? types-with-unread types/admin)}
-                            {:id                types/mention
-                             :label             (i18n/label :t/mentions)
-                             :notification-dot? (contains? types-with-unread types/mention)}
-                            {:id                types/reply
-                             :label             (i18n/label :t/replies)
-                             :notification-dot? (contains? types-with-unread types/reply)}
-                            {:id                types/contact-request
-                             :label             (i18n/label :t/contact-requests)
-                             :notification-dot? (contains? types-with-unread types/contact-request)}
-                            {:id                types/contact-verification
-                             :label             (i18n/label :t/identity-verification)
-                             :notification-dot? (contains? types-with-unread types/contact-verification)}
-                            {:id                types/tx
-                             :label             (i18n/label :t/transactions)
-                             :notification-dot? (contains? types-with-unread types/tx)}
-                            {:id                types/membership
-                             :label             (i18n/label :t/membership)
-                             :notification-dot? (contains? types-with-unread types/membership)}
-                            {:id                types/system
-                             :label             (i18n/label :t/system)
-                             :notification-dot? (contains? types-with-unread types/system)}]}]))
+                            {:id                  types/admin
+                             :label               (i18n/label :t/admin)
+                             :accessibility-label :tab-admin
+                             :notification-dot?   (contains? types-with-unread types/admin)}
+                            {:id                  types/mention
+                             :label               (i18n/label :t/mentions)
+                             :accessibility-label :tab-mention
+                             :notification-dot?   (contains? types-with-unread types/mention)}
+                            {:id                  types/reply
+                             :label               (i18n/label :t/replies)
+                             :accessibility-label :tab-reply
+                             :notification-dot?   (contains? types-with-unread types/reply)}
+                            {:id                  types/contact-request
+                             :label               (i18n/label :t/contact-requests)
+                             :accessibility-label :tab-contact-request
+                             :notification-dot?   (contains? types-with-unread types/contact-request)}
+                            {:id                  types/contact-verification
+                             :label               (i18n/label :t/identity-verification)
+                             :accessibility-label :tab-contact-verification
+                             :notification-dot?   (contains? types-with-unread
+                                                             types/contact-verification)}
+                            {:id                  types/tx
+                             :label               (i18n/label :t/transactions)
+                             :accessibility-label :tab-tx
+                             :notification-dot?   (contains? types-with-unread types/tx)}
+                            {:id                  types/membership
+                             :label               (i18n/label :t/membership)
+                             :accessibility-label :tab-membership
+                             :notification-dot?   (contains? types-with-unread types/membership)}
+                            {:id                  types/system
+                             :label               (i18n/label :t/system)
+                             :accessibility-label :tab-system
+                             :notification-dot?   (contains? types-with-unread types/system)}]}]))
 
 (defn header
   []
