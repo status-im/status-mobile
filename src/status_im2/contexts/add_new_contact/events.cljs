848a137e347b8f86f27c9284ce601ca61c17ec4c
Erik Seppanen <esep@protonmail.com>
2023-01-27 10:11:15 -0500

    Support join.status.im links for adding a contact (#14814)

--- src/status_im2/contexts/add_new_contact/events.cljs
+++ src/status_im2/contexts/add_new_contact/events.cljs
@@ -10,11 +10,6 @@
             [status-im.utils.utils :as utils]))
 
 (re-frame/reg-fx
- :contacts/resolve-public-key-from-ens-name
- (fn [{:keys [chain-id ens-name on-success on-error]}]
-   (ens/pubkey chain-id ens-name on-success on-error)))
-
-(re-frame/reg-fx
  :contacts/decompress-public-key
  (fn [{:keys [public-key on-success on-error]}]
    (status/compressed-key->public-key
@@ -23,50 +18,73 @@
       (let [{:keys [error]} (types/json->clj resp)]
         (if error
           (on-error error)
-          (on-success
-           (str "0x" (subs resp 5)))))))))
+          (on-success (str "0x" (subs resp 5)))))))))
+
+(re-frame/reg-fx
+ :contacts/resolve-public-key-from-ens-name
+ (fn [{:keys [chain-id ens-name on-success on-error]}]
+   (ens/pubkey chain-id ens-name on-success on-error)))
+
+(defn fx-callbacks
+  [input ens-name]
+  {:on-success (fn [pubkey]
+                 (rf/dispatch
+                  [:contacts/set-new-identity-success
+                   input ens-name pubkey]))
+   :on-error   (fn [err]
+                 (rf/dispatch
+                  [:contacts/set-new-identity-error err input]))})
+
+(defn identify-type
+  [input]
+  (let [regex           #"https?://join.status.im/u/(.+)"
+        id              (as-> (utils/safe-trim input) $
+                          (if-some [[_ match] (re-matches regex $)]
+                            match
+                            $)
+                          (if (empty? $) nil $))
+        public-key?     (validators/valid-public-key? id)
+        compressed-key? (validators/valid-compressed-key? id)
+        type            (cond (empty? id)     :empty
+                              public-key?     :public-key
+                              compressed-key? :compressed-key
+                              :else           :ens-name)
+        ens-name        (when (= type :ens-name)
+                          (stateofus/ens-name-parse id))]
+    {:input    input
+     :id       id
+     :type     type
+     :ens-name ens-name}))
 
 (rf/defn set-new-identity
   {:events [:contacts/set-new-identity]}
   [{:keys [db]} input]
-  (let [input           (utils/safe-trim input)
-        public-key?     (validators/valid-public-key? input)
-        compressed-key? (validators/valid-compressed-key? input)
-        ens-name        (if compressed-key? nil (stateofus/ens-name-parse input))
-        on-success      (fn [pubkey]
-                          (rf/dispatch
-                           [:contacts/set-new-identity-success
-                            input ens-name pubkey]))
-        on-error        (fn [err]
-                          (rf/dispatch
-                           [:contacts/set-new-identity-error err input]))]
-    (cond
-      (empty? input) {:db (dissoc db :contacts/new-identity)}
-      public-key?    {:db (assoc db
-                                 :contacts/new-identity
-                                 {:input      input
-                                  :public-key input
-                                  :ens-name   nil
-                                  :state      :error
-                                  :error      :uncompressed-key})}
-      :else
-      (cond->
-        {:db (assoc db
-                    :contacts/new-identity
-                    {:input      input
-                     :public-key nil
-                     :ens-name   nil
-                     :state      :searching
-                     :error      nil})}
-        compressed-key?       (assoc :contacts/decompress-public-key
-                                     {:public-key input
-                                      :on-success on-success
-                                      :on-error   on-error})
-        (not compressed-key?) (assoc :contacts/resolve-public-key-from-ens-name
-                                     {:chain-id   (ethereum/chain-id db)
-                                      :ens-name   ens-name
-                                      :on-success on-success
-                                      :on-error   on-error})))))
+  (let [{:keys [input id type ens-name]} (identify-type input)]
+    (case type
+      :empty          {:db (dissoc db :contacts/new-identity)}
+      :public-key     {:db (assoc db
+                                  :contacts/new-identity
+                                  {:input      input
+                                   :public-key id
+                                   :state      :error
+                                   :error      :uncompressed-key})}
+      :compressed-key {:db
+                       (assoc db
+                              :contacts/new-identity
+                              {:input input
+                               :state :searching})
+                       :contacts/decompress-public-key
+                       (merge {:public-key id}
+                              (fx-callbacks id ens-name))}
+      :ens-name       {:db
+                       (assoc db
+                              :contacts/new-identity
+                              {:input input
+                               :state :searching})
+                       :contacts/resolve-public-key-from-ens-name
+                       (merge {:chain-id (ethereum/chain-id db)
+                               :ens-name ens-name}
+                              (fx-callbacks id ens-name))})))
 
 (rf/defn set-new-identity-success
   {:events [:contacts/set-new-identity-success]}
@@ -76,19 +94,16 @@
               {:input      input
                :public-key pubkey
                :ens-name   ens-name
-               :state      :valid
-               :error      nil})})
+               :state      :valid})})
 
 (rf/defn set-new-identity-error
   {:events [:contacts/set-new-identity-error]}
   [{:keys [db]} error input]
   {:db (assoc db
               :contacts/new-identity
-              {:input      input
-               :public-key nil
-               :ens-name   nil
-               :state      :error
-               :error      :invalid})})
+              {:input input
+               :state :error
+               :error :invalid})})
 
 (rf/defn clear-new-identity
   {:events [:contacts/clear-new-identity :contacts/new-chat-focus]}
--- src/status_im2/contexts/add_new_contact/events_test.cljs
+++ src/status_im2/contexts/add_new_contact/events_test.cljs
@@ -2,52 +2,57 @@
   (:require [cljs.test :refer-macros [deftest is]]
             [status-im2.contexts.add-new-contact.events :as core]))
 
-(def init-db
-  {:networks/current-network "mainnet_rpc"
-   :networks/networks        {"mainnet_rpc"
-                              {:id     "mainnet_rpc"
-                               :config {:NetworkId 1}}}})
+(def ukey
+  "0x045596a7ff87da36860a84b0908191ce60a504afc94aac93c1abd774f182967ce694f1bf2d8773cd59f4dd0863e951f9b7f7351c5516291a0fceb73f8c392a0e88")
+(def ckey "zQ3shWj4WaBdf2zYKCkXe6PHxDxNTzZyid1i75879Ue9cX9gA")
+(def ens "esep")
 
-(defn search-db
-  [input]
-  {:contacts/new-identity {:input      input
-                           :public-key nil
-                           :ens-name   nil
-                           :state      :searching
-                           :error      nil}})
+(deftest identify-empty-string
+  (is (= (core/identify-type "")
+         {:input    ""
+          :id       nil
+          :type     :empty
+          :ens-name nil})))
 
 (deftest search-empty-string
-  (let [input    ""
-        expected {:db init-db}
-        actual   (core/set-new-identity {:db init-db} input)]
-    (is (= actual expected))))
+  (is (= (core/set-new-identity {:db {:contacts/new-identity :foo}} "")
+         {:db {}})))
 
-(deftest search-ens
-  (let [input    "esep"
-        clean    (fn [db]
-                   (-> db
-                       (assoc-in [:contacts/resolve-public-key-from-ens-name :on-success] nil)
-                       (assoc-in [:contacts/resolve-public-key-from-ens-name :on-error] nil)))
-        expected {:db                                        (merge init-db (search-db input))
-                  :contacts/resolve-public-key-from-ens-name
-                  {:chain-id   1
-                   :ens-name   (str input ".stateofus.eth")
-                   :on-success nil
-                   :on-error   nil}}
-        actual   (core/set-new-identity {:db init-db} input)]
-    (is (= (clean actual) expected))))
+(deftest identify-uncompressed-key
+  (is (= (core/identify-type ukey)
+         {:input    ukey
+          :id       ukey
+          :type     :public-key
+          :ens-name nil})))
 
-(deftest search-compressed-key
-  (let [input    "zQ3shWj4WaBdf2zYKCkXe6PHxDxNTzZyid1i75879Ue9cX9gA"
-        clean    (fn [db]
-                   (-> db
-                       (assoc-in [:contacts/decompress-public-key :on-success] nil)
-                       (assoc-in [:contacts/decompress-public-key :on-error] nil)))
-        expected {:db                             (merge init-db (search-db input))
-                  :contacts/decompress-public-key
-                  {:public-key input
-                   :on-success nil
-                   :on-error   nil}}
-        actual   (core/set-new-identity {:db init-db} input)]
-    (is (= (clean actual) expected))))
+(deftest search-uncompressed-key
+  (is (= (core/set-new-identity {:db {}} ukey)
+         {:db {:contacts/new-identity
+               {:input      ukey
+                :public-key ukey
+                :state      :error
+                :error      :uncompressed-key}}})))
+
+(deftest identify-ens
+  (let [ens-name (str ens ".stateofus.eth")]
+    (is (= (core/identify-type ens)
+           {:input    ens
+            :id       ens
+            :type     :ens-name
+            :ens-name ens-name}))))
+
+(deftest identify-compressed-key
+  (is (= (core/identify-type ckey)
+         {:input    ckey
+          :id       ckey
+          :type     :compressed-key
+          :ens-name nil})))
+
+(deftest identify-link-key
+  (let [input (str "https://join.status.im/u/" ckey)]
+    (is (= (core/identify-type input)
+           {:input    input
+            :id       ckey
+            :type     :compressed-key
+            :ens-name nil}))))
 
