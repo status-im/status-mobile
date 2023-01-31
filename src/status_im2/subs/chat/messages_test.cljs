cbd920f0707c9426b650cc9dabf28970ef5ce20c
Jamie Caprani <jamiecaprani@gmail.com>
2023-01-31 14:57:17 +0000

    chore: fix sorting of pinned messages (#14874)

--- src/status_im2/subs/chat/messages_test.cljs
+++ src/status_im2/subs/chat/messages_test.cljs
@@ -1,6 +1,9 @@
 (ns status-im2.subs.chat.messages-test
   (:require [cljs.test :refer [deftest is testing]]
             [status-im2.subs.chat.messages :as messages]
+            [test-helpers.unit :as h]
+            [utils.re-frame :as rf]
+            [re-frame.db :as rf-db]
             [status-im2.constants :as constants]))
 
 (def messages-state
@@ -50,3 +53,102 @@
       (is (= {:type  :datemark
               :value "Dec 31, 1999"}
              d2)))))
+
+(def pinned-messages-state
+  {:0xChat {:0x2 {:chat-id    :0xChat
+                  :message-id :0x2
+                  :pinned-at  2000
+                  :pinned-by  :test-user}
+            :0x1 {:chat-id    :0xChat
+                  :message-id :0x1
+                  :pinned-at  1000
+                  :pinned-by  :test-user}
+            :0x3 {:chat-id    :0xChat
+                  :message-id :0x3
+                  :pinned-at  3000
+                  :pinned-by  :test-user}}})
+
+(def pinned-messages-state-with-1-new-local-message
+  {:0xChat {:0x3 {:chat-id    :0xChat
+                  :message-id :0x3
+                  :pinned-at  nil
+                  :pinned-by  :test-user}
+            :0x2 {:chat-id    :0xChat
+                  :message-id :0x2
+                  :pinned-at  3000
+                  :pinned-by  :test-user}
+            :0x1 {:chat-id    :0xChat
+                  :message-id :0x1
+                  :pinned-at  2000
+                  :pinned-by  :test-user}}})
+
+(def pinned-messages-state-with-2-new-local-messages
+  {:0xChat {:0x3 {:chat-id    :0xChat
+                  :message-id :0x3
+                  :pinned-at  nil
+                  :pinned-by  :test-user}
+            :0x4 {:chat-id    :0xChat
+                  :message-id :0x4
+                  :pinned-at  nil
+                  :pinned-by  :test-user}
+            :0x2 {:chat-id    :0xChat
+                  :message-id :0x2
+                  :pinned-at  3000
+                  :pinned-by  :test-user}
+            :0x1 {:chat-id    :0xChat
+                  :message-id :0x1
+                  :pinned-at  2000
+                  :pinned-by  :test-user}}})
+
+(h/deftest-sub :chats/pinned-sorted-list
+  [sub-name]
+  (testing "It sorts three messages with pinned-at property"
+    (swap! rf-db/app-db assoc :pin-messages pinned-messages-state)
+    (is (= [{:chat-id    :0xChat
+             :message-id :0x1
+             :pinned-at  1000
+             :pinned-by  :test-user}
+            {:chat-id    :0xChat
+             :message-id :0x2
+             :pinned-at  2000
+             :pinned-by  :test-user}
+            {:chat-id    :0xChat
+             :message-id :0x3
+             :pinned-at  3000
+             :pinned-by  :test-user}]
+           (rf/sub [sub-name :0xChat]))))
+  (testing "It sorts messages from backend with pinned-at property and 1 new local pinned message"
+    (swap! rf-db/app-db assoc :pin-messages pinned-messages-state-with-1-new-local-message)
+    (is (= [{:chat-id    :0xChat
+             :message-id :0x1
+             :pinned-at  2000
+             :pinned-by  :test-user}
+            {:chat-id    :0xChat
+             :message-id :0x2
+             :pinned-at  3000
+             :pinned-by  :test-user}
+            {:chat-id    :0xChat
+             :message-id :0x3
+             :pinned-at  nil
+             :pinned-by  :test-user}]
+           (rf/sub [sub-name :0xChat]))))
+  (testing "It sorts messages from backend with pinned-at property and 2 new local pinned messages"
+    (swap! rf-db/app-db assoc :pin-messages pinned-messages-state-with-2-new-local-messages)
+    (is (= [{:chat-id    :0xChat
+             :message-id :0x1
+             :pinned-at  2000
+             :pinned-by  :test-user}
+            {:chat-id    :0xChat
+             :message-id :0x2
+             :pinned-at  3000
+             :pinned-by  :test-user}
+            {:chat-id    :0xChat
+             :message-id :0x3
+             :pinned-at  nil
+             :pinned-by  :test-user}
+            {:chat-id    :0xChat
+             :message-id :0x4
+             :pinned-at  nil
+             :pinned-by  :test-user}]
+           (rf/sub [sub-name :0xChat])))))
+
