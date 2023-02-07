f8b5b35063a0269cfdca5c20a335562c87f2d8df
John Ngei <johnmutuku628@gmail.com>
2023-02-07 12:58:38 +0300

    Update community home scrolling to support sticky header and tabs (#14929)
    * communities home sticky tabs
    
    * Communities home screen scroll behaviours
    
    * refactored scroll-page component

--- src/status_im2/contexts/communities/home/view.cljs
+++ src/status_im2/contexts/communities/home/view.cljs
@@ -1,37 +1,19 @@
 (ns status-im2.contexts.communities.home.view
   (:require [utils.i18n :as i18n]
             [quo2.core :as quo]
-            [react-native.core :as rn]
+            [quo2.foundations.colors :as colors]
             [reagent.core :as reagent]
+            [react-native.core :as rn]
             [status-im2.common.home.view :as common.home]
+            [status-im2.common.scroll-page.view :as scroll-page]
             [status-im2.contexts.communities.menus.community-options.view :as options]
+            [status-im2.contexts.communities.home.style :as style]
             [utils.re-frame :as rf]))
 
-(defn render-fn
-  [id]
-  (let [community-item (rf/sub [:communities/home-item id])]
-    [quo/communities-membership-list-item
-     {:on-press      #(rf/dispatch [:navigate-to :community-overview id])
-      :on-long-press #(rf/dispatch
-                       [:bottom-sheet/show-sheet
-                        {:content       (fn []
-                                          [options/community-options-bottom-sheet id])
-                         :selected-item (fn []
-                                          [quo/communities-membership-list-item {} community-item])}])}
-     community-item]))
-
-(defn get-item-layout-js
-  [_ index]
-  #js {:length 64 :offset (* 64 index) :index index})
-
-(defn home-community-segments
-  [selected-tab]
+(defn community-segments
+  [selected-tab padding-top]
   [rn/view
-   {:style {:padding-bottom     12
-            :padding-top        16
-            :margin-top         8
-            :height             60
-            :padding-horizontal 20}}
+   {:style (style/community-segments padding-top)}
    [quo/tabs
     {:size           32
      :on-change      #(reset! selected-tab %)
@@ -41,17 +23,25 @@
                       {:id :opened :label (i18n/label :t/opened) :accessibility-label :opened-tab}]}]])
 
 (defn communities-list
-  [community-ids]
-  [rn/flat-list
-   {:key-fn                            identity
-    :get-item-layout                   get-item-layout-js
-    :keyboard-should-persist-taps      :always
-    :shows-horizontal-scroll-indicator false
-    :data                              community-ids
-    :render-fn                         render-fn
-    :content-container-style           {:padding-bottom 30}}])
+  [communities-ids]
+  [rn/view
+   (map-indexed
+    (fn [index id]
+      (let [community (rf/sub [:communities/home-item id])]
+        ^{:key index}
+        [quo/communities-membership-list-item
+         {:on-press      #(rf/dispatch [:navigate-to :community-overview id])
+          :on-long-press #(rf/dispatch
+                           [:bottom-sheet/show-sheet
+                            {:content       (fn []
+                                              [options/community-options-bottom-sheet id])
+                             :selected-item (fn []
+                                              [quo/communities-membership-list-item {} community])}])}
+         community]))
+    communities-ids)])
 
-(defn segments-community-lists
+
+(defn render-communities-segments
   [selected-tab]
   (let [ids-by-user-involvement (rf/sub [:communities/community-ids-by-user-involvement])
         tab                     @selected-tab]
@@ -74,20 +64,70 @@
          :icon :i/info}
         (i18n/label :t/error)])]))
 
+(defn communities-header
+  [selected-tab padding-top]
+  [:<>
+   [rn/view {:style {:padding-vertical 8}}
+    [quo/discover-card
+     {:on-press            #(rf/dispatch [:navigate-to :discover-communities])
+      :title               (i18n/label :t/discover)
+      :description         (i18n/label :t/whats-trending)
+      :accessibility-label :communities-home-discover-card}]]
+   [community-segments selected-tab padding-top]])
+
+(defn home-page-comunity-lists
+  [{:keys [selected-tab padding-top]}]
+  (fn []
+    [rn/view {:style {:flex 1}}
+     [communities-header selected-tab padding-top]
+     [render-communities-segments selected-tab]]))
+
+(defn home-sticky-header
+  [{:keys [selected-tab scroll-height padding-top]}]
+  (fn []
+    (when (> @scroll-height 80)
+      [rn/view
+       {:style style/blur-tabs-header}
+       [community-segments selected-tab padding-top]])))
+
+(defn communities-screen-content
+  []
+  (let [scroll-height (reagent/atom 0)
+        selected-tab  (reagent/atom :joined)]
+    (fn []
+      [scroll-page/scroll-page
+       {:name             (i18n/label :t/communities)
+        :on-scroll        #(reset! scroll-height %)
+        :top-nav          (fn []
+                            [common.home/top-nav
+                             {:type        :default
+                              :hide-search true
+                              :style       {:background-color :transparent}}])
+        :title-colum      (fn []
+                            [common.home/title-column
+                             {:label               (i18n/label :t/communities)
+                              :handler             #(rf/dispatch [:bottom-sheet/show-sheet :add-new {}])
+                              :accessibility-label :new-chat-button}])
+        :background-color (colors/theme-colors
+                           colors/white
+                           colors/neutral-95)
+        :navigate-back?   :false
+        :height           (if (> @scroll-height 80)
+                            208
+                            156)}
+       [home-sticky-header
+        {:selected-tab  selected-tab
+         :scroll-height scroll-height
+         :padding-top   8}]
+       [home-page-comunity-lists
+        {:selected-tab selected-tab
+         :padding-top  16
+         :height       60}]])))
+
 (defn home
   []
-  (let [selected-tab (reagent/atom :joined)]
-    (fn []
-      [:<>
-       [common.home/top-nav {:type :default :hide-search true}]
-       [common.home/title-column
-        {:label               (i18n/label :t/communities)
-         :handler             #(rf/dispatch [:bottom-sheet/show-sheet :add-new {}])
-         :accessibility-label :new-chat-button}]
-       [quo/discover-card
-        {:on-press            #(rf/dispatch [:navigate-to :discover-communities])
-         :title               (i18n/label :t/discover)
-         :description         (i18n/label :t/whats-trending)
-         :accessibility-label :communities-home-discover-card}]
-       [home-community-segments selected-tab]
-       [segments-community-lists selected-tab]])))
+  [rn/view
+   {:style (style/home-communities-container (colors/theme-colors
+                                              colors/white
+                                              colors/neutral-95))}
+   [communities-screen-content]])
