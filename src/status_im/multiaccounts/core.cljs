8dead0941023c61dd4c5f0b34a947da7b81afa33
Vitaliy Vlasov <siphiuel@protonmail.com>
2023-02-17 17:33:12 +0200

    Fix for empty display-name

--- src/status_im/multiaccounts/core.cljs
+++ src/status_im/multiaccounts/core.cljs
@@ -68,9 +68,10 @@
 (defn displayed-name
   "Use preferred name, display-name, name or alias in that order"
   [{:keys [name display-name preferred-name alias public-key ens-verified]}]
-  (let [ens-name (or preferred-name
-                     display-name
-                     name)]
+  (let [display-name (if (string/blank? display-name) nil display-name)
+        ens-name     (or preferred-name
+                         display-name
+                         name)]
     ;; Preferred name is our own otherwise we make sure it's verified
     (if (or preferred-name (and ens-verified name))
       (let [username (stateofus/username ens-name)]
