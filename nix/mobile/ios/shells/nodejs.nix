e2169cbbd4bd15500372b53166bff4423fb5251b
Jakub Sokołowski <jakub@status.im>
2023-02-22 18:24:23 +0100

    ios: use --openssl-legacy-provider with iOS builds
    Fixes issue with following failure:
    ```
    Failed to construct transformer:  Error: error:0308010C:digital envelope routines::unsupported
        at new Hash (node:internal/crypto/hash:71:19)
        at Object.createHash (node:crypto:130:10)
        at stableHash (/Users/jenkins/workspace/status-mobile/platforms/ios/node_modules/metro-cache/src/stableHash.js:19:8)
        at JsTransformer.getCacheKey (/Users/jenkins/workspace/status-mobile/platforms/ios/node_modules/metro/src/JSTransformer/worker.js:471:7)
        at getTransformCacheKey (/Users/jenkins/workspace/status-mobile/platforms/ios/node_modules/metro/src/DeltaBundler/Transformer/getTransformCacheKey.js:39:29)
        at new Transformer (/Users/jenkins/workspace/status-mobile/platforms/ios/node_modules/metro/src/DeltaBundler/Transformer.js:147:28)
        at /Users/jenkins/workspace/status-mobile/platforms/ios/node_modules/metro/src/Bundler.js:54:29
        at runMicrotasks (<anonymous>)
        at processTicksAndRejections (node:internal/process/task_queues:96:5) {
      opensslErrorStack: [ 'error:03000086:digital envelope routines::initialization error' ],
      library: 'digital envelope routines',
      reason: 'unsupported',
      code: 'ERR_OSSL_EVP_UNSUPPORTED'
    ```
    https://roytuts.com/how-to-fix-err_ossl_evp_unsupported-in-react-js-application/
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/mobile/ios/shells/nodejs.nix
+++ nix/mobile/ios/shells/nodejs.nix
@@ -1,6 +1,9 @@
 { mkShell, deps }:
 
 mkShell {
+  # Fix for ERR_OSSL_EVP_UNSUPPORTED error.
+  NODE_OPTIONS = "--openssl-legacy-provider";
+
   # check if node modules changed and if so install them
   shellHook = ''
     export STATUS_MOBILE_HOME=$(git rev-parse --show-toplevel)
