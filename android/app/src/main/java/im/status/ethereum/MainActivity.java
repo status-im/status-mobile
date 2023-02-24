8eed47e45dc3dddef74dcda1527ff4912daa33ca
Siddarth Kumar <siddarthkay@gmail.com>
2023-02-24 20:32:41 +0530

    adding turbo modules support

--- android/app/src/main/java/im/status/ethereum/MainActivity.java
+++ android/app/src/main/java/im/status/ethereum/MainActivity.java
@@ -261,4 +261,25 @@ public class MainActivity extends NavigationActivity
             mPermissionListener = null;
         }
     }
+     /**
+       * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+       * you can specify the rendered you wish to use (Fabric or the older renderer).
+       */
+      @Override
+      protected ReactActivityDelegate createReactActivityDelegate() {
+        return new MainActivityDelegate(this, getMainComponentName());
+      }
+      public static class MainActivityDelegate extends ReactActivityDelegate {
+        public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+          super(activity, mainComponentName);
+        }
+        @Override
+        protected ReactRootView createRootView() {
+          ReactRootView reactRootView = new ReactRootView(getContext());
+          // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+          reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+          return reactRootView;
+        }
+      }
+
 }
