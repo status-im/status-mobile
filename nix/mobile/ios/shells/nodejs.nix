4fe8b81bf647cb8254e575b6e7129e597efa5c47
Jakub Sokołowski <jakub@status.im>
2023-03-01 17:27:43 +0100

    ios: fix xcode using system Node.js version
    As discovered in:
    https://github.com/status-im/status-mobile/pull/15225
    
    The attempt to fix this in:
    https://github.com/status-im/status-mobile/pull/15180
    
    But it doesn't appear to work, so instead I'm allowing an override of
    `NODE_BINARY` variable and spetting it when defining the Nix shell.
    
    The key things here are:
    
    * Xcode injects its own paths into `PATH` which breaks Nix env.
    * Combining Nix shells with `inputsFrom` does not inherit all vars.
    
    It's important to set these variables in `shellHook` and not elsewhere.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/mobile/ios/shells/nodejs.nix
+++ nix/mobile/ios/shells/nodejs.nix
@@ -1,11 +1,14 @@
-{ mkShell, deps }:
+{ mkShell, nodejs, deps }:
 
 mkShell {
-  # Fix for ERR_OSSL_EVP_UNSUPPORTED error.
-  NODE_OPTIONS = "--openssl-legacy-provider";
-
-  # check if node modules changed and if so install them
+  # Check if node modules changed and if so install them.
   shellHook = ''
+    # Fix for ERR_OSSL_EVP_UNSUPPORTED error.
+    export NODE_OPTIONS="--openssl-legacy-provider";
+
+    # Fix Xcode using system Node.js version.
+    export NODE_BINARY="${nodejs}/bin/node";
+
     export STATUS_MOBILE_HOME=$(git rev-parse --show-toplevel)
     "$STATUS_MOBILE_HOME/nix/scripts/node_modules.sh" ${deps.nodejs-patched}
   '';
