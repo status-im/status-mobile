075957392dfc7669dcc8681ada20f61e5dadeb37
ibrkhalil <vampirekid017@gmail.com>
2023-03-03 14:26:00 +0200

    Use props instead of styling

--- src/status_im2/contexts/chat/messages/content/text/style.cljs
+++ src/status_im2/contexts/chat/messages/content/text/style.cljs
@@ -1,6 +1,5 @@
 (ns status-im2.contexts.chat.messages.content.text.style
-  (:require [quo2.foundations.colors :as colors]
-            [quo2.foundations.typography :as typography]))
+  (:require [quo2.foundations.colors :as colors]))
 
 (def block
   {:border-radius      6
@@ -17,18 +16,3 @@
   {:background-color (colors/theme-colors colors/neutral-5 colors/white-opa-5)
    :border-width     1
    :border-color     (colors/theme-colors colors/neutral-20 colors/white-opa-20)})
-
-(defn message-default-style
-  []
-  (assoc typography/paragraph-1 :color (colors/theme-colors colors/neutral-100 colors/white)))
-
-(defn default-text-style
-  []
-  {:max-font-size-multiplier 1.25
-   :style                    (message-default-style)})
-
-(defn edited-style
-  []
-  (-> (default-text-style)
-      (update :style assoc :color (colors/theme-colors colors/neutral-40 colors/neutral-50))
-      (update :style merge typography/label)))
