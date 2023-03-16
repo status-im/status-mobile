51843e0b418bbfe3cb31b78ecb4d6859a04111ee
Omar Basem <omarbasem.0101@gmail.com>
2023-03-16 11:35:06 +0400

    updates

--- src/status_im2/contexts/chat/photo_selector/view.cljs
+++ src/status_im2/contexts/chat/photo_selector/view.cljs
@@ -1,11 +1,12 @@
 (ns status-im2.contexts.chat.photo-selector.view
   (:require
+    [react-native.gesture :as gesture]
     [react-native.navigation :as navigation]
     [react-native.platform :as platform]
     [react-native.reanimated :as reanimated]
     [status-im2.constants :as constants]
     [utils.i18n :as i18n]
-    [react-native.safe-area :as safe-area]
+    [oops.core :refer [oget]]
     [quo2.components.notifications.info-count :as info-count]
     [quo2.core :as quo]
     [quo2.foundations.colors :as colors]
@@ -16,6 +17,8 @@
     [status-im.utils.core :as utils]
     [quo.react]
     [utils.re-frame :as rf]))
+
+(def ^:const drag-threshold 100)
 
 (defn on-press-confirm-selection
   [selected]
@@ -103,70 +106,102 @@
     [quo/icon (if photos? :i/chevron-down :i/chevron-up)
      {:color (colors/theme-colors colors/neutral-100 colors/white)}]]])
 
+(defn drag-gesture
+  [translate-y opacity]
+  (->
+    (gesture/gesture-pan)
+    (gesture/enabled true)
+    (gesture/max-pointers 1)
+    (gesture/on-update (fn [e]
+                         (let [translation (oget e "translationY")
+                               progress    (Math/abs (/ translation drag-threshold))]
+                           (reanimated/set-shared-value translate-y translation)
+                           (reanimated/set-shared-value opacity (- 1 (/ progress 5)))
+                           ;(anim/set-val (if x? pan-x pan-y) translation)
+                           ;(anim/set-val opacity (- 1 progress))
+                           ;(anim/set-val layout (* progress -20))
+                           )))
+    (gesture/on-end (fn [e]
+                      (if (> (Math/abs (oget e "translationY")) drag-threshold)
+                        (do
+                          ;(anim/animate background-color "rgba(0,0,0,0)")
+                          ;(anim/animate opacity 0)
+                          (reanimated/set-shared-value opacity (reanimated/with-timing 0 (clj->js {:duration 100})))
+                          (rf/dispatch [:navigate-back])
+                          )
+                        (do
+                          (reanimated/set-shared-value opacity (reanimated/with-timing 1))
+                          (reanimated/set-shared-value translate-y (reanimated/with-timing 0))
+                          ;#(reset! set-full-height? true)
+                          ;(anim/animate (if x? pan-x pan-y) 0)
+                          ;(anim/animate opacity 1)
+                          ;(anim/animate layout 0)
+                          ))))))
+
 (defn photo-selector
   []
   [:f>
-   (let [temporary-selected (reagent/atom [])] ; used when switching albums
+   (let [{:keys [insets]} (rf/sub [:get-screen-params])
+         temporary-selected (reagent/atom [])] ; used when switching albums
      (fn []
        (let [selected        (reagent/atom []) ; currently selected
              selected-images (rf/sub [:chats/sending-image]) ; already selected and dispatched
              selected-album  (or (rf/sub [:camera-roll/selected-album]) (i18n/label :t/recent))
-             bg-color        (reanimated/use-shared-value "rgba(0,0,0,0)")]
+             opacity        (reanimated/use-shared-value 0)
+             translate-y     (reanimated/use-shared-value 0)]
          (rn/use-effect
            (fn []
-             (reanimated/animate-shared-value-with-delay-default-easing bg-color "rgba(9, 16, 28, 0.7)" 300 50)
-             ;(js/setTimeout #(reanimated/set-shared-value bg-color  (reanimated/with-timing "rgba(9, 16, 28, 0.7)")) 300)
+             (reanimated/animate-shared-value-with-delay-default-easing opacity 1 300 (if platform/ios? 300 100))
              (rf/dispatch [:chat.ui/camera-roll-get-photos 20 nil selected-album])
              (if (seq selected-images)
                (reset! selected (vec (vals selected-images)))
                (reset! selected @temporary-selected)))
            [selected-album])
-         ;[safe-area/consumer
-         ; (fn [insets]
-            (let [insets {:top 20 :bottom 20}
-                  window-width       (:width (rn/get-window))
-                  camera-roll-photos (rf/sub [:camera-roll/photos])
-                  end-cursor         (rf/sub [:camera-roll/end-cursor])
-                  loading?           (rf/sub [:camera-roll/loading-more])
-                  has-next-page?     (rf/sub [:camera-roll/has-next-page])]
-              [rn/view {:style {:flex        1
-                                :padding-top (if platform/ios? (navigation/status-bar-height) (+ (navigation/status-bar-height) 30))}}
-               [reanimated/view {:style (reanimated/apply-animations-to-style {:background-color bg-color}
-                                                                              {:position :absolute
-                                                                               :top      0
-                                                                               :bottom   0
-                                                                               :left     0
-                                                                               :right    0})}]
-               [rn/view {:style {:margin-top              0 :background-color :white
-                                 :border-top-left-radius  20
-                                 :border-top-right-radius 20
-                                 :overflow :hidden}}
-                [rn/view
-                 {:style style/buttons-container}
-                 (when true
-                   [rn/touchable-opacity
-                    {:active-opacity 1
-                     :on-press       (fn []
-                                       (reanimated/set-shared-value bg-color (reanimated/with-timing "transparent" (clj->js {:duration 100})))
-                                       (rf/dispatch [:navigate-back]))
-                     :style          (style/close-button-container)}
-                    [quo/icon :i/close
-                     {:size 20 :color (colors/theme-colors colors/black colors/white)}]])
-                 [album-title true selected-album selected temporary-selected]
-                 [clear-button selected]]
-                [rn/flat-list
-                 {:key-fn                  identity
-                  :render-fn               image
-                  :render-data             {:window-width window-width :selected selected}
-                  :data                    camera-roll-photos
-                  :num-columns             3
-                  :content-container-style {:width          "100%"
-                                            :padding-bottom (+ (:bottom insets) 100)
-                                            :padding-top    80}
-                  :on-end-reached          #(rf/dispatch [:camera-roll/on-end-reached end-cursor
-                                                          selected-album loading?
-                                                          has-next-page?])}]
-                [bottom-gradient selected-images insets selected]
-                ]])
-            ;)]
-         )))])
+         (let [window-width       (:width (rn/get-window))
+               camera-roll-photos (rf/sub [:camera-roll/photos])
+               end-cursor         (rf/sub [:camera-roll/end-cursor])
+               loading?           (rf/sub [:camera-roll/loading-more])
+               has-next-page?     (rf/sub [:camera-roll/has-next-page])]
+           [rn/view {:style {:flex        1
+                             :padding-top (if platform/ios? (navigation/status-bar-height) (+ (navigation/status-bar-height) 20))}}
+            [reanimated/view {:style (reanimated/apply-animations-to-style {:opacity opacity}
+                                                                           {:background-color "rgba(9, 16, 28, 0.7)"
+                                                                            :position :absolute
+                                                                            :top      0
+                                                                            :bottom   0
+                                                                            :left     0
+                                                                            :right    0})}]
+            [gesture/gesture-detector {:gesture (drag-gesture translate-y opacity)}
+             [reanimated/view {:style (reanimated/apply-animations-to-style
+                                        {:transform [{:translateY translate-y}]}
+                                        {:margin-top              0 :background-color :white
+                                         :border-top-left-radius  20
+                                         :border-top-right-radius 20
+                                         :overflow                :hidden})}
+              [rn/view
+               {:style style/buttons-container}
+               (when true
+                 [rn/touchable-opacity
+                  {:active-opacity 1
+                   :on-press       (fn []
+                                     (reanimated/set-shared-value opacity (reanimated/with-timing 0 (clj->js {:duration 100})))
+                                     (rf/dispatch [:navigate-back]))
+                   :style          (style/close-button-container)}
+                  [quo/icon :i/close
+                   {:size 20 :color (colors/theme-colors colors/black colors/white)}]])
+               [album-title true selected-album selected temporary-selected]
+               [clear-button selected]]
+              [gesture/flat-list
+               {:key-fn                  identity
+                :render-fn               image
+                :render-data             {:window-width window-width :selected selected}
+                :data                    camera-roll-photos
+                :num-columns             3
+                :content-container-style {:width          "100%"
+                                          :padding-bottom (+ (:bottom insets) 100)
+                                          :padding-top    80}
+                :on-end-reached          #(rf/dispatch [:camera-roll/on-end-reached end-cursor
+                                                        selected-album loading?
+                                                        has-next-page?])}]
+              [bottom-gradient selected-images insets selected]]]
+            ]))))])
