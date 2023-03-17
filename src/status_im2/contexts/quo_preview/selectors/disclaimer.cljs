2a53fb9cd87a8ffce8eb94aebc6ea57257d75713
Ulises M <ulises.ssb@hotmail.com>
2023-03-17 12:48:17 -0600

    Add checked? property & dark blur variant to disclaimer component

--- src/status_im2/contexts/quo_preview/selectors/disclaimer.cljs
+++ src/status_im2/contexts/quo_preview/selectors/disclaimer.cljs
@@ -1,27 +1,48 @@
 (ns status-im2.contexts.quo-preview.selectors.disclaimer
   (:require [quo2.components.buttons.button :as button]
-            [quo2.components.selectors.disclaimer.view :as quo]
+            [quo2.components.selectors.disclaimer.view :as disclaimer]
             [quo2.foundations.colors :as colors]
             [react-native.core :as rn]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            [status-im2.contexts.quo-preview.preview :as preview]))
+
+(def descriptor
+  [{:label "Checked:"
+    :key   :checked?
+    :type  :boolean}
+   {:label "Blur (only for dark theme):"
+    :key   :blur?
+    :type  :boolean}])
 
 (defn cool-preview
   []
-  (let [checked? (reagent/atom false)]
+  (let [state (reagent/atom {:checked? false
+                             :blur?    true})]
     (fn []
-      [rn/view
-       {:margin-bottom      50
-        :padding-vertical   16
-        :padding-horizontal 20}
-       [rn/view
-        {:padding-vertical 60
-         :align-items      :center}
-        [quo/view
-         {:container-style {:margin-bottom 40}
-          :on-change       #(swap! checked? not)}
-         "I agree with the community rules"]
-        [button/button
-         {:disabled (not @checked?)}
+      [rn/view {:style {:flex 1}}
+       [rn/view {:style {:flex 1}}
+        [preview/customizer state descriptor]]
+       [rn/view {:style {:padding-horizontal 15}}
+        (when (and (:blur? @state) (quo2.theme/dark?))
+          [rn/view
+           {:style {:position :absolute
+                    :top      0
+                    :bottom   0
+                    :left     0
+                    :right    0}}
+           [preview/blur-view
+            {:style                 {:flex        1
+                                     :align-items :center}
+             :show-blur-background? true}]])
+        [rn/view
+         {:style {:margin-vertical 50
+                  :width           "100%"}}
+         [disclaimer/view
+          {:blur?     (:blur? @state)
+           :checked?  (:checked? @state)
+           :on-change #(swap! state update :checked? not)}
+          "I agree with the community rules"]]
+        [button/button {:disabled (not (:checked? @state))}
          "submit"]]])))
 
 (defn preview-disclaimer
