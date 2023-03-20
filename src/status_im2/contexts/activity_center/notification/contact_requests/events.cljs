fc3992f23d7f54c65d29a7d57ca6c7d99f82f9ed
Icaro Motta <icaro.ldm@gmail.com>
2023-03-20 10:33:47 -0300

    Remove feature to cancel outgoing contact request

--- src/status_im2/contexts/activity_center/notification/contact_requests/events.cljs
+++ src/status_im2/contexts/activity_center/notification/contact_requests/events.cljs
@@ -19,8 +19,7 @@
   (log/error "Failed to accept contact-request"
              {:error      error
               :event      :activity-center.contact-requests/accept
-              :contact-id contact-id})
-  nil)
+              :contact-id contact-id}))
 
 (rf/defn decline-contact-request
   {:events [:activity-center.contact-requests/decline]}
@@ -38,32 +37,4 @@
   (log/error "Failed to decline contact-request"
              {:error      error
               :event      :activity-center.contact-requests/decline
-              :contact-id contact-id})
-  nil)
-
-(rf/defn cancel-outgoing-contact-request
-  {:events [:activity-center.contact-requests/cancel-outgoing]}
-  [{:keys [db]} {:keys [contact-id notification-id]}]
-  (when-let [notification (ac-events/get-notification db notification-id)]
-    {:json-rpc/call
-     [{:method     "wakuext_cancelOutgoingContactRequest"
-       :params     [{:id contact-id}]
-       :on-success #(rf/dispatch [:activity-center.contact-requests/cancel-outgoing-success
-                                  notification])
-       :on-error   #(rf/dispatch [:activity-center.contact-requests/cancel-outgoing-error contact-id
-                                  %])}]}))
-
-(rf/defn cancel-outgoing-contact-request-success
-  {:events [:activity-center.contact-requests/cancel-outgoing-success]}
-  [_ notification]
-  {:dispatch [:activity-center.notifications/reconcile
-              [(assoc notification :deleted true)]]})
-
-(rf/defn cancel-outgoing-contact-request-error
-  {:events [:activity-center.contact-requests/cancel-outgoing-error]}
-  [_ contact-id error]
-  (log/error "Failed to cancel outgoing contact-request"
-             {:error      error
-              :event      :activity-center.contact-requests/cancel-outgoing
-              :contact-id contact-id})
-  nil)
+              :contact-id contact-id}))
