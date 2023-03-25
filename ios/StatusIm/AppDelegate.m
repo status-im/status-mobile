cf0ad27d4121563a3d71c95bc8e4b0ab2e96c77e
Siddarth Kumar <siddarthkay@gmail.com>
2023-03-25 18:10:29 +0530

    go back to working react-native 0.67.5

--- ios/StatusIm/AppDelegate.m
+++ ios/StatusIm/AppDelegate.m
@@ -29,31 +29,34 @@
 
 #import <Security/Security.h>
 
-// adding this for v0.68.x
-#import <React/RCTAppSetupUtils.h>
-#if RCT_NEW_ARCH_ENABLED
-#import <React/CoreModulesPlugins.h>
-#import <React/RCTCxxBridgeDelegate.h>
-#import <React/RCTFabricSurfaceHostingProxyRootView.h>
-#import <React/RCTSurfacePresenter.h>
-#import <React/RCTSurfacePresenterBridgeAdapter.h>
-#import <ReactCommon/RCTTurboModuleManager.h>
-#import <react/config/ReactNativeConfig.h>
-@interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
-  RCTTurboModuleManager *_turboModuleManager;
-  RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
-  std::shared_ptr<const facebook::react::ReactNativeConfig> _reactNativeConfig;
-  facebook::react::ContextContainer::Shared _contextContainer;
-  }
-@end
-#endif
-
 //TODO: properly import the framework
-extern "C" NSString* StatusgoImageServerTLSCert();
+extern NSString* StatusgoImageServerTLSCert();
 
 @interface StatusDownloaderOperation : SDWebImageDownloaderOperation
   + (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler;
 @end
+
+ /*
+#if DEBUG
+#import <FlipperKit/FlipperClient.h>
+#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+ */
+ /*
+static void InitializeFlipper(UIApplication *application) {
+  FlipperClient *client = [FlipperClient sharedClient];
+  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
+  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+  [client addPlugin:[FlipperKitReactPlugin new]];
+  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+  [client start];
+       }
+#endif
+  */
 
 @implementation AppDelegate
 {
@@ -62,19 +65,6 @@ extern "C" NSString* StatusgoImageServerTLSCert();
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  
-  RCTAppSetupPrepareApp(application);
-   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  [ReactNativeNavigation bootstrapWithBridge:bridge];
-   #if RCT_NEW_ARCH_ENABLED
-       _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
-       _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
-       _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
-       _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
-       bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
-   #endif
-
-  
   #if DEBUG
     //InitializeFlipper(application);
   #endif
@@ -100,6 +90,7 @@ extern "C" NSString* StatusgoImageServerTLSCert();
       dictionaryWithObject:[NSNumber numberWithBool:NO] forKey:@"BLANK_PREVIEW"];
   [[NSUserDefaults standardUserDefaults] registerDefaults:appDefaults];
 
+  [ReactNativeNavigation bootstrapWithDelegate:self launchOptions:launchOptions];
 
   [RNSplashScreen show];
 
@@ -134,7 +125,7 @@ extern "C" NSString* StatusgoImageServerTLSCert();
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
@@ -203,47 +194,6 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
   completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
 }
 
-#if RCT_NEW_ARCH_ENABLED
-
-#pragma mark - RCTCxxBridgeDelegate
-
-- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
-{
-  _turboModuleManager = [[RCTTurboModuleManager alloc] initWithBridge:bridge
-                                                             delegate:self
-                                                            jsInvoker:bridge.jsCallInvoker];
-  return RCTAppSetupDefaultJsExecutorFactory(bridge, _turboModuleManager);
-}
-
-#pragma mark RCTTurboModuleManagerDelegate
-
-- (Class)getModuleClassFromName:(const char *)name
-{
-  return RCTCoreModulesClassProvider(name);
-}
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
-                                                      jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
-{
-  return nullptr;
-}
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
-                                                     initParams:
-                                                         (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-  return nullptr;
-}
-
-- (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
-{
-  return RCTAppSetupDefaultModuleFromClass(moduleClass);
-}
-
-#endif
-
-
-
 @end
 
 @implementation StatusDownloaderOperation
@@ -257,7 +207,7 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
   pemCert = [pemCert stringByReplacingOccurrencesOfString:@"\n-----END CERTIFICATE-----" withString:@""];
   NSData *derCert = [[NSData alloc] initWithBase64EncodedString:pemCert options:NSDataBase64DecodingIgnoreUnknownCharacters];
   SecCertificateRef certRef = SecCertificateCreateWithData(NULL, (__bridge_retained CFDataRef) derCert);
-  CFArrayRef certArrayRef = CFArrayCreate(NULL, (const void **)&(certRef), 1, NULL);
+  CFArrayRef certArrayRef = CFArrayCreate(NULL, (void *)&certRef, 1, NULL);
   SecTrustSetAnchorCertificates(challenge.protectionSpace.serverTrust, certArrayRef);
 
   SecTrustResultType trustResult;
@@ -298,4 +248,3 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
 }
 
 @end
-
