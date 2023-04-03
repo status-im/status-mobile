e55a9326d66e30631147f7903f066a411bffe08d
Omar Basem <omarbasem.0101@gmail.com>
2023-04-03 18:31:29 +0400

    fix: image desktop (#15562)
    * fix: image desktop not opening

--- src/status_im2/contexts/chat/photo_selector/view.cljs
+++ src/status_im2/contexts/chat/photo_selector/view.cljs
@@ -13,8 +13,7 @@
     [status-im2.contexts.chat.photo-selector.style :as style]
     [status-im.utils.core :as utils]
     [quo.react]
-    [utils.re-frame :as rf]
-    [react-native.safe-area :as safe-area]))
+    [utils.re-frame :as rf]))
 
 (defn on-press-confirm-selection
   [selected]
@@ -25,13 +24,13 @@
   (rf/dispatch [:navigate-back]))
 
 (defn bottom-gradient
-  [selected-images insets selected]
+  [selected-images bottom-inset selected]
   (when (or (seq @selected) (seq selected-images))
     [linear-gradient/linear-gradient
      {:colors [:black :transparent]
       :start  {:x 0 :y 1}
       :end    {:x 0 :y 0}
-      :style  (style/gradient-container insets)}
+      :style  (style/gradient-container bottom-inset)}
      [quo/button
       {:style               {:align-self        :stretch
                              :margin-horizontal 20
@@ -109,30 +108,32 @@
 
 (defn photo-selector
   [{:keys [scroll-enabled on-scroll]}]
-  [safe-area/consumer
-   (fn [insets]
-     [:f>
-      (let [temporary-selected (reagent/atom [])] ; used when switching albums
-        (fn []
-          (let [selected        (reagent/atom []) ; currently selected
-                selected-images (rf/sub [:chats/sending-image]) ; already selected and dispatched
-                selected-album  (or (rf/sub [:camera-roll/selected-album]) (i18n/label :t/recent))]
-            (rn/use-effect
-             (fn []
-               (rf/dispatch [:chat.ui/camera-roll-get-photos 20 nil selected-album])
-               (if (seq selected-images)
-                 (reset! selected (vec (vals selected-images)))
-                 (reset! selected @temporary-selected)))
-             [selected-album])
+  [:f>
+   (let [{:keys [bottom-inset]} (rf/sub [:screen-params]) ; TODO:
+                                                          ; https://github.com/status-im/status-mobile/issues/15535
+         temporary-selected     (reagent/atom [])] ; used when switching albums
+     (fn []
+       (let [selected        (reagent/atom [])     ; currently selected
+             selected-images (rf/sub [:chats/sending-image]) ; already selected and dispatched
+             selected-album  (or (rf/sub [:camera-roll/selected-album]) (i18n/label :t/recent))]
+         (rn/use-effect
+          (fn []
+            (rf/dispatch [:chat.ui/camera-roll-get-photos 20 nil selected-album])
+            (if (seq selected-images)
+              (reset! selected (vec (vals selected-images)))
+              (reset! selected @temporary-selected)))
+          [selected-album])
+         [:f>
+          (fn []
             (let [window-width       (:width (rn/get-window))
                   camera-roll-photos (rf/sub [:camera-roll/photos])
                   end-cursor         (rf/sub [:camera-roll/end-cursor])
                   loading?           (rf/sub [:camera-roll/loading-more])
                   has-next-page?     (rf/sub [:camera-roll/has-next-page])]
-              [rn/view {:flex 1}
+              [:<>
                [rn/view
                 {:style style/buttons-container}
-                [album-title true selected-album selected temporary-selected insets]
+                [album-title true selected-album selected temporary-selected]
                 [clear-button selected]]
                [gesture/flat-list
                 {:key-fn                  identity
@@ -141,11 +142,11 @@
                  :data                    camera-roll-photos
                  :num-columns             3
                  :content-container-style {:width          "100%"
-                                           :padding-bottom (+ (:bottom insets) 100)
+                                           :padding-bottom (+ (:bottom bottom-inset) 100)
                                            :padding-top    64}
                  :on-scroll               on-scroll
                  :scroll-enabled          scroll-enabled
                  :on-end-reached          #(rf/dispatch [:camera-roll/on-end-reached end-cursor
                                                          selected-album loading?
                                                          has-next-page?])}]
-               [bottom-gradient selected-images insets selected]]))))])])
+               [bottom-gradient selected-images bottom-inset selected]]))])))])
