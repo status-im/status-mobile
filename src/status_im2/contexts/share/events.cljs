a091b081290ff94b854a7403cfc1f5ca0b5cc41b
Siddarth Kumar <siddarthkay@gmail.com>
2023-04-04 10:47:42 +0530

    move copy to events namespace

--- src/status_im2/contexts/share/events.cljs
+++ src/status_im2/contexts/share/events.cljs
@@ -1,7 +1,10 @@
 (ns status-im2.contexts.share.events
   (:require [utils.re-frame :as rf]
             [status-im2.common.toasts.events :as toasts]
-            [quo2.foundations.colors :as colors]))
+            [quo2.foundations.colors :as colors]
+            ;;TODO(siddarthkay) : move the components below over to status-im2 ns
+            ;; issue -> https://github.com/status-im/status-mobile/issues/15549
+            [status-im.ui.components.react :as react]))
 
 (rf/defn open-profile-share-view
   {:events [:share/open]}
@@ -14,11 +17,12 @@
                :blur-view-props            {:blur-amount 20
                                             :blur-type   :dark}}]})
 
-(rf/defn show-successfully-copied-toast
-  {:events [:share/show-successfully-copied-toast]}
-  [cofx toast-label]
+(rf/defn copy-text-and-show-toast
+  {:events [:share/copy-text-and-show-toast]}
+  [cofx text-to-copy post-copy-message]
+  (react/copy-to-clipboard text-to-copy)
   (toasts/upsert cofx
                  {:icon           :correct
                   :icon-color     colors/success-50
                   :override-theme :dark
-                  :text           toast-label}))
+                  :text           post-copy-message}))
