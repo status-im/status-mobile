e0a524dff7d04ba475c0601845c412f507fdec0c
Omar Basem <omarbasem.0101@gmail.com>
2023-04-08 02:34:12 +0400

    updates

--- src/status_im2/contexts/chat/messages/bottom_sheet_composer/constants.cljs
+++ src/status_im2/contexts/chat/messages/bottom_sheet_composer/constants.cljs
@@ -1,20 +1,28 @@
 (ns status-im2.contexts.chat.messages.bottom-sheet-composer.constants
-  (:require [react-native.platform :as platform]))
+  (:require
+    [quo2.foundations.typography :as typography]
+    [react-native.platform :as platform]))
 
 (def ^:const handle-container-height 20)
 
-(def ^:const input-height (if platform/ios? 32 44))
+(def ^:const input-height (if platform/ios? 32 42))
 
 (def ^:const actions-container-height 56)
 
 (def ^:const composer-default-height (+ handle-container-height input-height actions-container-height))
 
+(def ^:const multiline-minimized-height (+ input-height 18))
+
 (def ^:const images-container-height 76)
 
-(def ^:const top-gradient-height 80)
+(def ^:const extra-content-offset (if platform/ios? 6 0))
+
+(def ^:const content-change-threshold 10)
 
 (def ^:const drag-threshold 30)
 
-(def ^:const ios-extra-offset 12)
-
 (def ^:const velocity-threshold -1000)
+
+(def ^:const background-threshold 0.75)
+
+(def ^:const line-height (:line-height typography/paragraph-1))
