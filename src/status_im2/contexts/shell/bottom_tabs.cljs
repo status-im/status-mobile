2782abc1269b46cbb1e46177b7489c95c9e5e765
Ibrahem <vampirekid017@gmail.com>
2023-04-08 15:53:27 +0200

    Lint

--- src/status_im2/contexts/shell/bottom_tabs.cljs
+++ src/status_im2/contexts/shell/bottom_tabs.cljs
@@ -6,7 +6,8 @@
             [status-im2.contexts.shell.style :as style]
             [status-im2.contexts.shell.animation :as animation]
             [status-im2.contexts.shell.constants :as shell.constants]
-            [quo2.components.navigation.bottom-nav-tab :as bottom-nav-tab]))
+            [quo2.components.navigation.bottom-nav-tab :as bottom-nav-tab]
+            [react-native.gesture :as gesture]))
 
 (defn blur-overlay-params
   [style]
@@ -17,28 +18,36 @@
    :overlay-color :transparent})
 
 (defn bottom-tab
-  [icon stack-id shared-values notifications-data]
-  [bottom-nav-tab/bottom-nav-tab
-   (assoc (get notifications-data stack-id)
-          :test-ID             stack-id
-          :icon                icon
-          :icon-color-anim     (get
-                                shared-values
-                                (get shell.constants/tabs-icon-color-keywords stack-id))
-          :on-press            #(animation/bottom-tab-on-press stack-id)
-          :accessibility-label (str (name stack-id) "-tab"))])
+  ([icon stack-id shared-values notifications-data]
+   (bottom-tab icon stack-id shared-values notifications-data nil))
+  ([icon stack-id shared-values notifications-data double-tap-gesture]
+   [bottom-nav-tab/bottom-nav-tab
+    (cond-> (assoc (get notifications-data stack-id)
+                   :test-ID             stack-id
+                   :icon                icon
+                   :icon-color-anim     (get
+                                         shared-values
+                                         (get shell.constants/tabs-icon-color-keywords stack-id))
+                   :on-press            #(animation/bottom-tab-on-press stack-id)
+                   :accessibility-label (str (name stack-id) "-tab"))
+      double-tap-gesture (assoc :double-tap-gesture double-tap-gesture))]))
 
 (defn bottom-tabs
   []
   [:f>
    (fn []
-     (let [notifications-data (rf/sub [:shell/bottom-tabs-notifications-data])
-           pass-through?      (rf/sub [:shell/shell-pass-through?])
-           shared-values      @animation/shared-values-atom
-           original-style     (style/bottom-tabs-container pass-through?)
-           animated-style     (reanimated/apply-animations-to-style
-                               {:height (:bottom-tabs-height shared-values)}
-                               original-style)]
+     (let [notifications-data          (rf/sub [:shell/bottom-tabs-notifications-data])
+           pass-through?               (rf/sub [:shell/shell-pass-through?])
+           shared-values               @animation/shared-values-atom
+           original-style              (style/bottom-tabs-container pass-through?)
+           animated-style              (reanimated/apply-animations-to-style
+                                        {:height (:bottom-tabs-height shared-values)}
+                                        original-style)
+           messages-double-tap-gesture (-> (gesture/gesture-tap)
+                                           (gesture/number-of-taps 2)
+                                           (gesture/on-start
+                                            (fn [_event]
+                                              (rf/dispatch [:messages-home-view/select-tab :recent]))))]
        (animation/load-stack @animation/selected-stack-id)
        (reanimated/set-shared-value (:pass-through? shared-values) pass-through?)
        [reanimated/view {:style animated-style}
@@ -46,6 +55,7 @@
           [blur/view (blur-overlay-params style/bottom-tabs-blur-overlay)])
         [rn/view {:style (style/bottom-tabs)}
          [bottom-tab :i/communities :communities-stack shared-values notifications-data]
-         [bottom-tab :i/messages :chats-stack shared-values notifications-data]
+         [bottom-tab :i/messages :chats-stack shared-values notifications-data
+          messages-double-tap-gesture]
          [bottom-tab :i/wallet :wallet-stack shared-values notifications-data]
          [bottom-tab :i/browser :browser-stack shared-values notifications-data]]]))])
