1acc1f44f4f4fe10858dc268fa78f6f3edffd5ad
Icaro Motta <icaro.ldm@gmail.com>
2023-04-11 16:02:33 -0300

    Refactor: use better names

--- src/quo2/components/links/url_preview_list/view.cljs
+++ src/quo2/components/links/url_preview_list/view.cljs
@@ -6,21 +6,6 @@
     [react-native.core :as rn]
     [reagent.core :as reagent]))
 
-(defn- url-preview-separator
-  []
-  [rn/view {:style style/url-preview-separator}])
-
-(defn- url-preview-item
-  [{:keys [title body loading? logo]} _ _ {:keys [width on-clear loading-message]}]
-  [url-preview/view
-   {:logo            logo
-    :title           title
-    :body            body
-    :loading?        loading?
-    :loading-message loading-message
-    :on-clear        on-clear
-    :container-style {:width width}}])
-
 (defn- use-scroll-to-last-item
   [flat-list-ref item-count]
   (rn/use-effect
@@ -40,6 +25,27 @@
            (js/clearTimeout timer-id)))))
    [item-count]))
 
+(defn- separator
+  []
+  [rn/view {:style style/url-preview-separator}])
+
+(defn- item-component
+  [{:keys [title body loading? logo]} _ _ {:keys [width on-clear loading-message]}]
+  [url-preview/view
+   {:logo            logo
+    :title           title
+    :body            body
+    :loading?        loading?
+    :loading-message loading-message
+    :on-clear        on-clear
+    :container-style {:width width}}])
+
+(defn- calculate-width
+  [preview-width horizontal-spacing ^js e]
+  (reset! preview-width
+    (- (oops/oget e "nativeEvent.layout.width")
+       (* 2 horizontal-spacing))))
+
 (defn- view-component
   []
   (let [preview-width (reagent/atom 0)
@@ -55,19 +61,16 @@
        [rn/flat-list
         {:ref                               #(reset! flat-list-ref %)
          :key-fn                            key-fn
-         :on-layout                         (fn [^js e]
-                                              (let [width (- (oops/oget e "nativeEvent.layout.width")
-                                                             (* 2 horizontal-spacing))]
-                                                (reset! preview-width width)))
+         :on-layout                         #(calculate-width preview-width horizontal-spacing %)
          :horizontal                        true
          :deceleration-rate                 :fast
          :on-scroll-to-index-failed         identity
          :content-container-style           {:padding-horizontal horizontal-spacing}
-         :separator                         [url-preview-separator]
+         :separator                         [separator]
          :snap-to-interval                  (+ @preview-width style/url-preview-gap)
          :shows-horizontal-scroll-indicator false
          :data                              data
-         :render-fn                         url-preview-item
+         :render-fn                         item-component
          :render-data                       {:width           @preview-width
                                              :on-clear        on-clear
                                              :loading-message loading-message}}]])))
