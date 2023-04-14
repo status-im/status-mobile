f7b483a50495ba518a54934fd3b327a5b653e036
Siddarth Kumar <siddarthkay@gmail.com>
2023-04-14 16:38:07 +0530

    Revert "go to 0.68.6"
    This reverts commit a1d6ffd9da63b3fb59eb1803e80c96d21f986450.

--- ios/StatusIm/AppDelegate.m
+++ ios/StatusIm/AppDelegate.m
@@ -125,7 +125,7 @@ static void InitializeFlipper(UIApplication *application) {
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
