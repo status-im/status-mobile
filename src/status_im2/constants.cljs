3ba381dd5f85b5e381cc7bfa99785ba0a9fa1fb5
Ibrahem <vampirekid017@gmail.com>
2023-04-16 17:12:39 +0200

    Clean up

--- src/status_im2/constants.cljs
+++ src/status_im2/constants.cljs
@@ -41,7 +41,6 @@
 (def ^:const mute-for-8-hours-type 3)
 (def ^:const mute-for-1-week 4)
 (def ^:const mute-till-unmuted 5)
-(def ^:const mute-for-1-min 6)
 
 (def ^:const activity-center-mark-all-as-read-undo-time-limit-ms 4000)
 (def ^:const activity-center-max-unread-count 99)
