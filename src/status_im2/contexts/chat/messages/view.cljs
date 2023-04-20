a3d83c8a639630bc7dce78f0042dd59821abae84
Omar Basem <omarbasem.0101@gmail.com>
2023-04-20 23:28:29 +0400

    revert pod

--- src/status_im2/contexts/chat/messages/view.cljs
+++ src/status_im2/contexts/chat/messages/view.cljs
@@ -73,8 +73,7 @@
      (fn [insets]
        [rn/keyboard-avoiding-view
         {:style                  {:position :relative :flex 1}
-         :keyboardVerticalOffset (- (:bottom insets))
-         }
+         :keyboardVerticalOffset (- (:bottom insets))}
         [page-nav]
         [pin.banner/banner chat-id]
         [messages.list/messages-list chat insets]
