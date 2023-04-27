2eed30be8f035a56f5a1c503eaaed61ea9f7b91b
Siddarth Kumar <siddarthkay@gmail.com>
2023-04-27 19:25:49 +0530

    (chore) : Improve QR code quo2 component (#15755)
    This PR adds the ability to :
    
    - Generate a QR by passing any url.
    - See different error correction levels.
    - Shows the media server url for that specific QR code.
    - Moved media server URI generation logic to `image_server.cljs`

--- src/utils/image_server.cljs
+++ src/utils/image_server.cljs
@@ -5,6 +5,8 @@
 (def ^:const identicons-action "/messages/identicons")
 (def ^:const account-images-action "/accountImages")
 (def ^:const contact-images-action "/contactImages")
+(def ^:const generate-qr-action "/GenerateQRCode")
+(def ^:const status-profile-base-url "https://join.status.im/u/")
 
 (defn timestamp [] (datetime/timestamp))
 
@@ -13,6 +15,15 @@
   (case theme
     :light 1
     :dark  2))
+
+(defn correction-level->index
+  [level]
+  (case (keyword level)
+    :low     1
+    :medium  2
+    :quart   3
+    :highest 4
+    4))
 
 (defn get-identicons-uri
   [port public-key theme]
@@ -58,3 +69,45 @@
        "&clock="
        clock
        "&addRing=1"))
+
+(defn get-account-qr-image-uri
+  [{:keys [key-uid public-key port qr-size]}]
+  (let [profile-qr-url         (str status-profile-base-url public-key)
+        base-64-qr-url         (js/btoa profile-qr-url)
+        profile-image-type     "large"
+        error-correction-level (correction-level->index :highest)
+        superimpose-profile?   true
+        media-server-url       (str image-server-uri-prefix
+                                    port
+                                    generate-qr-action
+                                    "?level="
+                                    error-correction-level
+                                    "&url="
+                                    base-64-qr-url
+                                    "&keyUid="
+                                    key-uid
+                                    "&allowProfileImage="
+                                    superimpose-profile?
+                                    "&size="
+                                    qr-size
+                                    "&imageName="
+                                    profile-image-type)]
+    media-server-url))
+
+(defn get-qr-image-uri-for-any-url
+  [{:keys [url port qr-size error-level]}]
+  (let [qr-url-base64          (js/btoa url)
+        error-correction-level (correction-level->index error-level)
+        superimpose-profile?   false
+        media-server-url       (str image-server-uri-prefix
+                                    port
+                                    generate-qr-action
+                                    "?level="
+                                    error-correction-level
+                                    "&url="
+                                    qr-url-base64
+                                    "&allowProfileImage="
+                                    superimpose-profile?
+                                    "&size="
+                                    qr-size)]
+    media-server-url))
