c05332c035e8e64aa41feb8df6c7cd6a4009cb69
codemaster115 <punctualityman@outlook.com>
2023-05-15 08:39:20 -0500

    fix: lint

--- src/quo2/foundations/resources.cljs
+++ src/quo2/foundations/resources.cljs
@@ -1,10 +1,10 @@
 (ns quo2.foundations.resources)
 
 (def ui
-  {:keycard-logo                  (js/require "../resources/images/ui2/keycard-logo.png")
-   :keycard-chip-light            (js/require "../resources/images/ui2/keycard-chip-light.png")
-   :keycard-chip-dark             (js/require "../resources/images/ui2/keycard-chip-dark.png")
-   :keycard-watermark             (js/require "../resources/images/ui2/keycard-watermark.png")})
+  {:keycard-logo       (js/require "../resources/images/ui2/keycard-logo.png")
+   :keycard-chip-light (js/require "../resources/images/ui2/keycard-chip-light.png")
+   :keycard-chip-dark  (js/require "../resources/images/ui2/keycard-chip-dark.png")
+   :keycard-watermark  (js/require "../resources/images/ui2/keycard-watermark.png")})
 
 (defn get-image
   [k]
