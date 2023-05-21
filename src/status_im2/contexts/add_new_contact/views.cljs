f304481ff0fbaefd43e20a8145cea7171b0633bd
Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>
2023-05-21 21:53:37 +0530

    [Fix] Keyboard dismiss in add contact modal
    Signed-off-by: Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>

--- src/status_im2/contexts/add_new_contact/views.cljs
+++ src/status_im2/contexts/add_new_contact/views.cljs
@@ -51,64 +51,65 @@
             show-paste-button? (and (not (string/blank? @clipboard))
                                     (string/blank? @default-value)
                                     (string/blank? input))]
-
-        [rn/view (style/container-outer)
-         [rn/view style/container-inner
-          [quo/button
-           (merge (style/button-close)
-                  {:on-press
-                   (fn []
-                     (reset! clipboard nil)
-                     (reset! default-value nil)
-                     (rf/dispatch [:contacts/clear-new-identity])
-                     (rf/dispatch [:navigate-back]))}) :i/close]
-          [quo/text (style/text-title)
-           (i18n/label :t/add-a-contact)]
-          [quo/text (style/text-subtitle)
-           (i18n/label :t/find-your-friends)]
-          [quo/text (style/text-description)
-           (i18n/label :t/ens-or-chat-key)]
-          [rn/view style/container-text-input
-           [rn/view (style/text-input-container invalid?)
-            [rn/text-input
-             (merge (style/text-input)
-                    {:default-value  (or scanned @default-value input)
-                     :placeholder    (i18n/label :t/type-some-chat-key)
-                     :on-change-text (fn [v]
-                                       (reset! default-value v)
-                                       (debounce/debounce-and-dispatch
-                                        [:contacts/set-new-identity v nil]
-                                        600))})]
-            (when show-paste-button?
-              [quo/button
-               (merge style/button-paste
-                      {:on-press
-                       (fn []
-                         (reset! default-value @clipboard)
-                         (rf/dispatch
-                          [:contacts/set-new-identity @clipboard nil]))})
-               (i18n/label :t/paste)])]
+        [rn/touchable-without-feedback
+         {:on-press rn/dismiss-keyboard!}
+         [rn/view (style/container-outer)
+          [rn/view style/container-inner
            [quo/button
-            (merge style/button-qr
-                   {:on-press #(rf/dispatch
-                                [::qr-scanner/scan-code
-                                 {:handler :contacts/qr-code-scanned}])})
-            :i/scan]]
-          (when invalid?
-            [rn/view style/container-invalid
-             [quo/icon :i/alert style/icon-invalid]
-             [quo/text style/text-invalid
-              (i18n/label (or msg :t/invalid-ens-or-key))]])
-          (when (= state :valid)
-            [found-contact public-key])]
-         [rn/view
-          [quo/button
-           (merge (style/button-view-profile state)
-                  {:on-press
-                   (fn []
-                     (reset! clipboard nil)
-                     (reset! default-value nil)
-                     (rf/dispatch [:contacts/clear-new-identity])
-                     (rf/dispatch [:navigate-back])
-                     (rf/dispatch [:chat.ui/show-profile public-key ens]))})
-           (i18n/label :t/view-profile)]]]))))
+            (merge (style/button-close)
+                   {:on-press
+                    (fn []
+                      (reset! clipboard nil)
+                      (reset! default-value nil)
+                      (rf/dispatch [:contacts/clear-new-identity])
+                      (rf/dispatch [:navigate-back]))}) :i/close]
+           [quo/text (style/text-title)
+            (i18n/label :t/add-a-contact)]
+           [quo/text (style/text-subtitle)
+            (i18n/label :t/find-your-friends)]
+           [quo/text (style/text-description)
+            (i18n/label :t/ens-or-chat-key)]
+           [rn/view style/container-text-input
+            [rn/view (style/text-input-container invalid?)
+             [rn/text-input
+              (merge (style/text-input)
+                     {:default-value  (or scanned @default-value input)
+                      :placeholder    (i18n/label :t/type-some-chat-key)
+                      :on-change-text (fn [v]
+                                        (reset! default-value v)
+                                        (debounce/debounce-and-dispatch
+                                         [:contacts/set-new-identity v nil]
+                                         600))})]
+             (when show-paste-button?
+               [quo/button
+                (merge style/button-paste
+                       {:on-press
+                        (fn []
+                          (reset! default-value @clipboard)
+                          (rf/dispatch
+                           [:contacts/set-new-identity @clipboard nil]))})
+                (i18n/label :t/paste)])]
+            [quo/button
+             (merge style/button-qr
+                    {:on-press #(rf/dispatch
+                                 [::qr-scanner/scan-code
+                                  {:handler :contacts/qr-code-scanned}])})
+             :i/scan]]
+           (when invalid?
+             [rn/view style/container-invalid
+              [quo/icon :i/alert style/icon-invalid]
+              [quo/text style/text-invalid
+               (i18n/label (or msg :t/invalid-ens-or-key))]])
+           (when (= state :valid)
+             [found-contact public-key])]
+          [rn/view
+           [quo/button
+            (merge (style/button-view-profile state)
+                   {:on-press
+                    (fn []
+                      (reset! clipboard nil)
+                      (reset! default-value nil)
+                      (rf/dispatch [:contacts/clear-new-identity])
+                      (rf/dispatch [:navigate-back])
+                      (rf/dispatch [:chat.ui/show-profile public-key ens]))})
+            (i18n/label :t/view-profile)]]]]))))
