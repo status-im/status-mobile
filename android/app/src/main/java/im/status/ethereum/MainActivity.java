079c9c887d1491ddffd1ad65f261677e2e209681
Siddarth Kumar <siddarthkay@gmail.com>
2023-05-23 20:29:08 +0530

    debug fix

--- android/app/src/main/java/im/status/ethereum/MainActivity.java
+++ android/app/src/main/java/im/status/ethereum/MainActivity.java
@@ -32,6 +32,7 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import com.reactnativenavigation.NavigationActivity;
 import com.facebook.react.modules.core.PermissionListener;
 import androidx.core.splashscreen.SplashScreen;
+import com.facebook.react.ReactActivity;
 
 import java.util.Properties;
 import im.status.ethereum.module.StatusThreadPoolExecutor;
@@ -260,22 +261,22 @@ public class MainActivity extends NavigationActivity
        * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
        * you can specify the rendered you wish to use (Fabric or the older renderer).
        */
-    @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new MainActivityDelegate(this, getMainComponentName());
-    }
-
-    public static class MainActivityDelegate extends ReactActivityDelegate {
-        public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
-            super(activity, mainComponentName);
-        }
-
-        @Override
-        protected ReactRootView createRootView() {
-            ReactRootView reactRootView = new ReactRootView(getContext());
-            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-            reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
-            return reactRootView;
-        }
-    }
+//     @Override
+//     protected ReactActivityDelegate createReactActivityDelegate() {
+//         return new MainActivityDelegate(this, getMainComponentName());
+//     }
+//
+//     public static class MainActivityDelegate extends ReactActivityDelegate {
+//         public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+//             super(activity, mainComponentName);
+//         }
+//
+//         @Override
+//         protected ReactRootView createRootView() {
+//             ReactRootView reactRootView = new ReactRootView(getContext());
+//             // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+//             reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+//             return reactRootView;
+//         }
+//     }
 }
