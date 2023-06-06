68da5175c01c08abbbd64b84b27e681e8ece35d8
Alexander <alwxndr@gmail.com>
2023-06-06 12:47:17 +0100

    Correct top bar offsets and positions on Android + correct blur as a bonus ✨ (#16148)
    * Correct top bar offsets and positions on Android
    
    * Style fixes
    
    * Style fixes

--- src/status_im2/contexts/chat/messages/navigation/view.cljs
+++ src/status_im2/contexts/chat/messages/navigation/view.cljs
@@ -2,9 +2,10 @@
   (:require [quo2.core :as quo]
             [quo2.foundations.colors :as colors]
             [re-frame.db]
+            [react-native.blur :as blur]
             [react-native.core :as rn]
+            [react-native.platform :as platform]
             [react-native.reanimated :as reanimated]
-            [react-native.safe-area :as safe-area]
             [status-im2.contexts.chat.messages.navigation.style :as style]
             [status-im2.contexts.chat.messages.pin.banner.view :as pin.banner]
             [status-im2.constants :as constants]
@@ -14,9 +15,7 @@
 
 (defn f-navigation-view
   [{:keys [scroll-y]}]
-  (let [insets                   (safe-area/get-insets)
-        status-bar-height        (:top insets)
-        {:keys [group-chat chat-id chat-name emoji
+  (let [{:keys [group-chat chat-id chat-name emoji
                 chat-type]
          :as   chat}             (rf/sub [:chats/current-chat-chat-view])
         all-loaded?              (rf/sub [:chats/all-loaded? chat-id])
@@ -52,14 +51,18 @@
                                                          {:extrapolateLeft  "clamp"
                                                           :extrapolateRight "clamp"})]
     [rn/view {:style style/navigation-view}
-     [reanimated/blur-view
-      {:blurAmount   32
-       :blurType     (colors/theme-colors :xlight :dark)
-       :overlayColor :transparent
-       :style        (style/animated-blur-view all-loaded? opacity-animation status-bar-height)}]
+     [reanimated/view
+      {:style (style/animated-background-view all-loaded? opacity-animation)}]
+
+     [reanimated/view {:style (style/animated-blur-view all-loaded? opacity-animation)}
+      [blur/view
+       {:blur-amount 20
+        :blur-type   (colors/theme-colors :light :dark)
+        :blur-radius (if platform/ios? 20 10)
+        :style       {:flex 1}}]]
 
      [rn/view
-      [rn/view {:style (style/header-container status-bar-height)}
+      [rn/view {:style style/header-container}
        [rn/touchable-opacity
         {:active-opacity      1
          :on-press            #(rf/dispatch [:navigate-back])
@@ -104,7 +107,7 @@
           [quo/icon :i/options {:size 20 :color (colors/theme-colors colors/black colors/white)}]])]
 
       [reanimated/view
-       {:style (style/animated-pinned-banner all-loaded? banner-opacity-animation status-bar-height)}
+       {:style (style/animated-pinned-banner all-loaded? banner-opacity-animation)}
        [pin.banner/banner chat-id]]]]))
 
 (defn navigation-view
