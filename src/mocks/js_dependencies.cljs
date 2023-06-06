281d963e2958b5edd9c4fe1a688c9839cb4b8e65
Brian Sztamfater <brian@status.im>
2023-06-06 13:29:25 +0100

    add library mock

--- src/mocks/js_dependencies.cljs
+++ src/mocks/js_dependencies.cljs
@@ -342,6 +342,8 @@ globalThis.__STATUS_MOBILE_JS_IDENTITY_PROXY__ = new Proxy({}, {get() { return (
   (clj->js {:default                    #js {}
             :useDeviceOrientationChange #js {}}))
 
+(def react-native-transparent-video #js {:default #js {}})
+
 (def wallet-connect-client
   #js
    {:default       #js {}
@@ -404,6 +406,7 @@ globalThis.__STATUS_MOBILE_JS_IDENTITY_PROXY__ = new Proxy({}, {get() { return (
     "@react-native-async-storage/async-storage"     async-storage
     "react-native-svg"                              react-native-svg
     "react-native-orientation-locker"               react-native-orientation-locker
+    "react-native-transparent-video"                react-native-transparent-video
     "../src/js/worklets/core.js"                    worklet-factory
     "../src/js/worklets/shell.js"                   #js {}
     "../src/js/worklets/bottom_sheet.js"            #js {}
