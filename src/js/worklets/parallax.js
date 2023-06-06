13350cb6279409d27c977f7ca75541ab8660f19f
Brian Sztamfater <brian@status.im>
2023-06-06 12:45:08 +0100

    adjust inverted input ranges for each OS

--- src/js/worklets/parallax.js
+++ src/js/worklets/parallax.js
@@ -5,6 +5,7 @@ import {
     interpolate,
     SensorType
 } from 'react-native-reanimated';
+import { Platform } from 'react-native';
 
 const PI = Math.PI;
 
@@ -20,14 +21,14 @@ export function sensorAnimatedImage(zIndex, offset) {
         const top = withTiming(
             interpolate(
                 pitch,
-                [-PI, PI],
+                Platform.OS === 'ios' ? [-PI, PI] : [PI, -PI],
                 [(-offset * 3) / zIndex - offset, (offset * 3) / zIndex - offset]),
             { duration: 10 }
         );
         const left = withTiming(
             interpolate(
                 roll,
-                [-1, 1],
+                [1, -1],
                 [(-offset * 2) / zIndex - offset, (offset * 2) / zIndex - offset]),
             { duration: 10 }
         );
