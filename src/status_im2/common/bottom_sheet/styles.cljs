aafe6a634912359b4b363a2b8215010b91178fb0
Omar Basem <omarbasem.0101@gmail.com>
2023-06-07 10:32:23 +0400

    fix: bottom sheet padding

--- src/status_im2/common/bottom_sheet/styles.cljs
+++ src/status_im2/common/bottom_sheet/styles.cljs
@@ -14,7 +14,7 @@
    :margin-vertical  8})
 
 (defn sheet
-  [{:keys [top bottom]} window-height override-theme padding-bottom-override shell?]
+  [{:keys [top bottom]} window-height override-theme shell?]
   {:position                :absolute
    :max-height              (- window-height top 20)
    :z-index                 1
@@ -25,10 +25,11 @@
    :border-top-right-radius 20
    :overflow                (when shell? :hidden)
    :flex                    1
-   :padding-bottom          (or padding-bottom-override (max 20 bottom))
+   :padding-bottom          (+ bottom 8)
    :background-color        (if shell?
                               :transparent
-                              (colors/theme-colors colors/white colors/neutral-90 override-theme))})
+                              (colors/theme-colors colors/white colors/neutral-90 override-theme))
+  })
 
 (def shell-bg
   {:position         :absolute
--- src/status_im2/common/bottom_sheet/view.cljs
+++ src/status_im2/common/bottom_sheet/view.cljs
@@ -1,4 +1,5 @@
 (ns status-im2.common.bottom-sheet.view
+<<<<<<< HEAD
   (:require [utils.re-frame :as rf]
             [react-native.core :as rn]
             [quo2.foundations.colors :as colors]
@@ -9,6 +10,19 @@
             [react-native.hooks :as hooks]
             [react-native.blur :as blur]
             [reagent.core :as reagent]))
+=======
+  (:require
+    [react-native.safe-area :as safe-area]
+    [utils.re-frame :as rf]
+    [react-native.core :as rn]
+    [quo2.foundations.colors :as colors]
+    [react-native.reanimated :as reanimated]
+    [status-im2.common.bottom-sheet.styles :as styles]
+    [react-native.gesture :as gesture]
+    [oops.core :as oops]
+    [react-native.hooks :as hooks]
+    [react-native.blur :as blur]))
+>>>>>>> 72fabd4b2 (fix: bottom sheet padding)
 
 (def duration 450)
 (def timing-options #js {:duration duration})
@@ -52,6 +66,7 @@
            (show translate-y bg-opacity)
            (hide translate-y bg-opacity window-height on-close))))))
 
+<<<<<<< HEAD
 (defn f-view
   [_ _]
   (let [sheet-height (reagent/atom 0)]
@@ -87,6 +102,36 @@
                                       shell?))
             :on-layout #(reset! sheet-height (oops/oget % "nativeEvent" "layout" "height"))}
            (when shell? [blur/ios-view {:style styles/shell-bg}])
+=======
+(defn view
+  [{:keys [hide?]}
+   {:keys [content override-theme selected-item on-close shell?]}]
+  (let [insets                  (safe-area/get-insets)
+        {window-height :height} (rn/get-window)
+        bg-opacity              (reanimated/use-shared-value 0)
+        translate-y             (reanimated/use-shared-value window-height)
+        sheet-gesture           (get-sheet-gesture translate-y bg-opacity window-height on-close)]
+    (rn/use-effect
+     #(if hide? (hide translate-y bg-opacity window-height on-close) (show translate-y bg-opacity))
+     [hide?])
+    (hooks/use-back-handler #(do (when (fn? on-close)
+                                   (on-close))
+                                 (rf/dispatch [:hide-bottom-sheet])
+                                 true))
+    [rn/view {:flex 1}
+     ;; backdrop
+     [rn/touchable-without-feedback {:on-press #(rf/dispatch [:hide-bottom-sheet])}
+      [reanimated/view
+       {:style (reanimated/apply-animations-to-style
+                {:opacity bg-opacity}
+                {:flex 1 :background-color colors/neutral-100-opa-70})}]]
+     ;; sheet
+     [gesture/gesture-detector {:gesture sheet-gesture}
+      [reanimated/view
+       {:style (reanimated/apply-animations-to-style
+                {:transform [{:translateY translate-y}]}
+                (styles/sheet insets window-height override-theme shell?))}
+>>>>>>> 72fabd4b2 (fix: bottom sheet padding)
 
            (when selected-item
              [rn/view
