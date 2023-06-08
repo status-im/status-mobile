1a358f286587412f83c4044abc03f8a6e92708cd
Jamie Caprani <jamiecaprani@gmail.com>
2023-06-08 16:21:14 +0100

    smaller videos

--- src/status_im2/common/resources.cljs
+++ src/status_im2/common/resources.cljs
@@ -76,10 +76,10 @@
    :snt  (js/require "../resources/images/tokens/mainnet/SNT.png")})
 
 (def parallax-video
-  {:biometrics    [(js/require "../resources/videos2/biometrics_layer_01.mp4")
-                   (js/require "../resources/videos2/biometrics_layer_02.mp4")
-                   (js/require "../resources/videos2/biometrics_layer_03.mp4")
-                   (js/require "../resources/videos2/biometrics_layer_04.mp4")]
+  {:biometrics    [(js/require "../resources/videos2/enable_biometrics_0.mp4")
+                   (js/require "../resources/videos2/enable_biometrics_1.mp4")
+                   (js/require "../resources/videos2/enable_biometrics_2.mp4")
+                   (js/require "../resources/videos2/enable_biometrics_3.mp4")]
    :notifications [(js/require "../resources/videos2/notifications_layer_01.mp4")
                    (js/require "../resources/videos2/notifications_layer_02.mp4")
                    (js/require "../resources/videos2/notifications_layer_03.mp4")
