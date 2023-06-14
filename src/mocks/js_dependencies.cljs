e8808eec90d71c86208a32f8d3e2d881c15ccd05
Jamie Caprani <jamiecaprani@gmail.com>
2023-06-14 13:22:36 +0100

    feat: add parallax component

--- src/mocks/js_dependencies.cljs
+++ src/mocks/js_dependencies.cljs
@@ -342,6 +342,8 @@ globalThis.__STATUS_MOBILE_JS_IDENTITY_PROXY__ = new Proxy({}, {get() { return (
   (clj->js {:default                    #js {}
             :useDeviceOrientationChange #js {}}))
 
+(def react-native-transparent-video #js {:default #js {}})
+
 (def wallet-connect-client
   #js
    {:default       #js {}
@@ -403,6 +405,7 @@ globalThis.__STATUS_MOBILE_JS_IDENTITY_PROXY__ = new Proxy({}, {get() { return (
     "react-native-share"                            react-native-share
     "@react-native-async-storage/async-storage"     async-storage
     "react-native-svg"                              react-native-svg
+    "react-native-transparent-video"                react-native-transparent-video
     "react-native-orientation-locker"               react-native-orientation-locker
     "../src/js/worklets/core.js"                    worklet-factory
     "../src/js/worklets/shell/bottom_tabs.js"       #js {}
@@ -413,6 +416,7 @@ globalThis.__STATUS_MOBILE_JS_IDENTITY_PROXY__ = new Proxy({}, {get() { return (
     "../src/js/worklets/scroll_view.js"             #js {}
     "../src/js/worklets/onboarding_carousel.js"     #js {}
     "../src/js/worklets/lightbox.js"                #js {}
+    "../src/js/worklets/parallax.js"                #js {}
     "./fleets.js"                                   default-fleets
     "@walletconnect/client"                         wallet-connect-client
     "../translations/ar.json"                       (js/JSON.parse (slurp "./translations/ar.json"))
---
+++ src/status_im2/common/parallax/view.cljs
@@ -0,0 +1,52 @@
+(ns status-im2.common.parallax.view
+  (:require [react-native.reanimated :as reanimated]
+            [react-native.transparent-video :as transparent-video]
+            [react-native.core :as rn]
+            [utils.worklets.parallax :as worklets.parallax]
+            [status-im2.common.parallax.style :as style]))
+
+(defn f-sensor-animated-video
+  [{:keys [offset stretch order source disable-parallax?]}]
+  (let [double-stretch          (* 2 stretch)
+        {window-width  :width
+         window-height :height} (rn/get-window)
+        image-style             (if (not disable-parallax?)
+                                  (worklets.parallax/sensor-animated-image order offset stretch)
+                                  {:top    0
+                                   :right  0
+                                   :bottom 0
+                                   :left   0})]
+    (fn []
+      [reanimated/view
+       {:needsOffscreenAlphaCompositing true
+        :style                          [(style/container-view
+                                          (+ window-width double-stretch)
+                                          (+ window-height double-stretch))
+                                         image-style]}
+       [transparent-video/view
+        {:source source
+         :style  style/video}]])))
+
+(defn sensor-animated-video
+  [props]
+  [:f> f-sensor-animated-video props])
+
+(defn f-video
+  [{:keys [layers disable-parallax? offset stretch]
+    :or   {offset  50
+           stretch 0}}]
+  [rn/view
+   {:style style/outer-container}
+   (map-indexed (fn [idx layer]
+                  [sensor-animated-video
+                   {:key               (str layer idx)
+                    :source            layer
+                    :offset            offset
+                    :stretch           stretch
+                    :order             (inc idx)
+                    :disable-parallax? disable-parallax?}])
+                layers)])
+
+(defn video
+  [props]
+  [:f> f-video props])
