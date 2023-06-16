a82e90c29de169a30263bf09c2722bf39bbf369c
DQ_Arjun <dq_arjun@DQ-Arjuns-MacBook-Pro.local>
2023-06-16 15:13:18 +0530

    network code need to remove & fix UI

--- src/status_im2/contexts/scan/style.cljs
+++ src/status_im2/contexts/scan/style.cljs
@@ -1,6 +1,6 @@
 (ns status-im2.contexts.scan.style
   (:require [quo2.foundations.colors :as colors]
-            [react-native.platform :as platform]))
+            [status-im.utils.platform :as platform]))
 
 (def screen-padding 20)
 
@@ -17,86 +17,158 @@
   {:margin-bottom 12
    :margin-left   screen-padding})
 
+
 (def header-heading
   {:padding-horizontal screen-padding
    :padding-vertical   12
    :color              colors/white})
 
-(def qr-code-container
-  {:padding           12
-   :border-radius     16
-   :margin-top        12
-   :margin-bottom     4
-   :margin-horizontal screen-padding
-   :background-color  colors/white-opa-5
-   :flex-direction    :column
-   :justify-content   :center})
+(def flex-spacer {:flex 1})
 
-(def emoji-hash-container
-  {:border-radius     16
-   :margin-top        12
-   :margin-horizontal screen-padding
-   :background-color  colors/white-opa-5
-   :flex-direction    :row
-   :justify-content   :flex-start
-   :align-items       :flex-start})
-
-(def profile-address-column
-  {:flex-direction :column})
-
-(def emoji-address-column
-  {:flex-direction :column})
-
-(def profile-address-label
-  {:color       colors/white-opa-40
-   :padding-top 10})
-
-(def profile-address-content
-  {:color       colors/white
-   :align-self  :flex-start
-   :padding-top 2})
-
-(def profile-address-container
-  {:flex-direction    :row
-   :justify-content   :flex-start
-   :margin-top        6
-   :margin-horizontal 4})
-
-(def emoji-address-container
-  {:flex-direction    :row
-   :justify-content   :flex-start
-   :margin-top        6
-   :margin-horizontal 4})
-
-(def emoji-hash-label
-  {:color          colors/white-opa-40
-   :margin-top     8
-   :padding-bottom (if platform/ios? 2 0)
-   :padding-left   12})
-
-
-(def scan-button-container
+(def absolute-fill
   {:position :absolute
-   :right    0
-   :top      16})
+   :top      0
+   :bottom   0
+   :left     0
+   :right    0})
 
-(def emoji-scan-button-container
-  {:position :absolute
-   :right    4
-   :top      16})
+(def hole
+  (merge absolute-fill
+         {:z-index 2 :opacity 0.95}))
 
-(def emoji-hash-content
-  {:color          colors/white
-   :align-self     :flex-start
-   :padding-top    4
-   :padding-bottom 12
-   :padding-left   12
-   :font-size      14})
+(defn root-container
+  [padding-top]
+  {:z-index     5
+   :flex        1
+   :padding-top padding-top})
+
+(def header-container
+  {:flex-direction     :row
+   :justify-content    :space-between
+   :padding-horizontal screen-padding
+   :margin-vertical    12})
+
+(def header-text
+  {:padding-horizontal screen-padding
+   :padding-top        12
+   :padding-bottom     8
+   :color              colors/white})
+
+(def header-sub-text
+  {:padding-horizontal screen-padding
+   :color              colors/white})
 
 (def tabs-container
   {:padding-horizontal screen-padding
-   :margin-vertical    8})
+   :margin-top         20})
 
-(def wip-style
-  {:color      colors/white
-   :text-align :center})
+(def scan-qr-code-container
+  {:margin-top 19})
+
+(def qr-view-finder
+  {:margin-horizontal screen-padding
+   :height            1
+   :display           :flex})
+
+(defn qr-view-finder-container
+  [size]
+  {:width           size
+   :height          size
+   :justify-content :space-between
+   :margin-left     -1
+   :margin-top      -1})
+
+(defn viewfinder-container
+  [viewfinder]
+  {:position :absolute
+   :left     (:x viewfinder)
+   :top      (:y viewfinder)})
+
+(def view-finder-border-container
+  {:flex-direction  :row
+   :justify-content :space-between})
+
+(def camera-flash-button
+  {:position :absolute
+   :right    20
+   :bottom   20})
+
+(defn border
+  [border1 border2 corner]
+  (assoc {:border-color colors/white
+          :width        78
+          :height       78}
+         border1
+         2
+         border2
+         2
+         corner
+         16))
+
+(defn border-tip
+  [top bottom right left]
+  {:background-color colors/white
+   :position         :absolute
+   :top              top
+   :bottom           bottom
+   :right            right
+   :left             left
+   :height           2
+   :width            2
+   :border-radius    2})
+
+(def viewfinder-text
+  {:color       colors/white-opa-70
+   :text-align  :center
+   :padding-top 16})
+
+(def camera-permission-container
+  {:height            335
+   :margin-horizontal screen-padding
+   :background-color  colors/white-opa-5
+   :border-color      colors/white-opa-10
+   :border-width      1
+   :border-radius     12
+   :border-style      :dashed
+   :align-items       :center
+   :justify-content   :center})
+
+(def enable-camera-access-header
+  {:color colors/white})
+
+(def enable-camera-access-sub-text
+  {:color         colors/white-opa-70
+   :margin-bottom 16})
+
+(def enter-sync-code-container
+  {:margin-top      20
+   :justify-content :center
+   :align-items     :center})
+
+(defn bottom-container
+  [padding-bottom]
+  {:z-index                 6
+   :padding-top             12
+   :padding-bottom          padding-bottom
+   :background-color        colors/white-opa-5
+   :border-top-left-radius  20
+   :border-top-right-radius 20
+   :align-items             :center
+   :justify-content         :center})
+
+(def bottom-text
+  {:color          colors/white
+   :padding-bottom 12})
+
+(def camera-style
+  {:height           "100%"
+   :borderRadius     16
+   :background-color :white-opa-5})
+
+(def camera-container
+  {:position      :absolute
+   :top           (if platform/android? 40 0)
+   :left          0
+   :right         0
+   :bottom        0
+   :border-radius 16})
--- src/status_im2/contexts/scan/view.cljs
+++ src/status_im2/contexts/scan/view.cljs
@@ -1,105 +1,261 @@
 (ns status-im2.contexts.scan.view
-  (:require [utils.i18n :as i18n]
+  (:require [clojure.string :as string]
+            [oops.core :as oops]
             [quo2.core :as quo]
-            [react-native.core :as rn]
-            [status-im2.contexts.scan.style :as style]
-            [utils.re-frame :as rf]
-            [reagent.core :as reagent]
             [quo2.foundations.colors :as colors]
-            [clojure.string :as string]
+            [react-native.camera-kit :as camera-kit]
+            [react-native.core :as rn]
             [react-native.blur :as blur]
-            [status-im.ui.components.list-selection :as list-selection]
-            [utils.image-server :as image-server]
+            [react-native.hole-view :as hole-view]
+            [react-native.permissions :as permissions]
+            [react-native.safe-area :as safe-area]
+            [reagent.core :as reagent]
+            [status-im2.contexts.scan.style :as style]
+            [utils.i18n :as i18n]
+            [utils.re-frame :as rf]
+            [status-im2.contexts.syncing.utils :as sync-utils]
+            [status-im.utils.platform :as platform]
             [react-native.navigation :as navigation]))
 
-(defn header
+;; Android allow local network access by default. So, we need this check on iOS only.
+(defonce preflight-check-passed? (reagent/atom (if platform/ios? false true)))
+
+(defonce camera-permission-granted? (reagent/atom false))
+
+(defn request-camera-permission
+  []
+  (rf/dispatch
+   [:request-permissions
+    {:permissions [:camera]
+     :on-allowed  #(reset! camera-permission-granted? true)
+     :on-denied   #(rf/dispatch
+                    [:toasts/upsert
+                     {:icon           :i/info
+                      :icon-color     colors/danger-50
+                      :override-theme :light
+                      :text           (i18n/label :t/camera-permission-denied)}])}]))
+
+(defn perform-preflight-check
+  "Performing the check for the first time
+   will trigger local network access permission in iOS.
+   This permission is required for local pairing
+   https://github.com/status-im/status-mobile/issues/16135"
+  []
+  (rf/dispatch [:syncing/preflight-outbound-check #(reset! preflight-check-passed? %)]))
+
+(defn- header
   []
   [:<>
    [quo/button
     {:icon                true
      :type                :blur-bg
      :size                32
-     :accessibility-label :close-shell-scan-tab
+     :accessibility-label :close-scan-scan-tab
      :override-theme      :dark
      :style               style/header-button
      :on-press            #(rf/dispatch [:navigate-back])}
     :i/close]
    [quo/text
     {:size   :heading-1
-     :weight :semi-bold 
+     :weight :semi-bold
+     :override-theme      :dark
      :style  style/header-heading}
-    (i18n/label :t/scan)]
-    ])
+    (i18n/label :t/scan-qr)]])
 
-(defn abbreviated-url
-  "The goal here is to generate a string that begins with
-   join.status.im/u/ joined with the 1st 5 characters
-   of the compressed public key followed by an ellipsis followed by
-   the last 12 characters of the compressed public key"
-  [base-url public-pk]
-  (let [first-part-of-public-pk (subs public-pk 0 5)
-        ellipsis                "..."
-        public-pk-size          (count public-pk)
-        last-part-of-public-pk  (subs public-pk (- public-pk-size 12) (- public-pk-size 1))
-        abbreviated-url         (str base-url first-part-of-public-pk ellipsis last-part-of-public-pk)]
-    abbreviated-url))
+(defn get-labels-and-on-press-method
+  []
+  (if @camera-permission-granted?
+    {:title-label-key       :t/enable-access-to-local-network
+     :description-label-key :t/to-pair-with-your-other-device-in-the-network
+     :button-icon           :i/world
+     :button-label          :t/enable-network-access
+     :accessibility-label   :perform-preflight-check
+     :on-press              perform-preflight-check}
+    {:title-label-key       :t/enable-access-to-camera
+     :description-label-key :t/to-scan-a-qr-enable-your-camera
+     :button-icon           :i/camera
+     :button-label          :t/enable-camera
+     :accessibility-label   :request-camera-permission
+     :on-press              request-camera-permission}))
 
-(defn profile-tab
-  [window-width]
-  (let [multiaccount    (rf/sub [:multiaccount])
-        emoji-hash      (string/join (get multiaccount :emoji-hash))
-        qr-size         (int (- window-width 64))
-        public-pk       (get multiaccount :compressed-key)
-        abbreviated-url (abbreviated-url image-server/status-profile-base-url-without-https public-pk)
-        profile-url     (str image-server/status-profile-base-url public-pk)
-        port            (rf/sub [:mediaserver/port])
-        key-uid         (get multiaccount :key-uid)
-        source-uri      (image-server/get-account-qr-image-uri {:key-uid    key-uid
-                                                                :public-key public-pk
-                                                                :port       port
-                                                                :qr-size    qr-size})]
+(defn- camera-access-permission-view
+  []
+  (let [{:keys [title-label-key
+                description-label-key
+                button-icon
+                button-label
+                accessibility-label
+                on-press]} (get-labels-and-on-press-method)]
+    [rn/view {:style style/camera-permission-container}
+     [quo/text
+      {:size   :paragraph-1
+       :weight :medium
+       :style  style/enable-camera-access-header}
+      (i18n/label title-label-key)]
+     [quo/text
+      {:size   :paragraph-2
+       :weight :regular
+       :style  style/enable-camera-access-sub-text}
+      (i18n/label description-label-key)]
+     [quo/button
+      {:before              button-icon
+       :type                :primary
+       :size                32
+       :accessibility-label accessibility-label
+       :override-theme      :dark
+       :customization-color :blue
+       :on-press            on-press}
+      (i18n/label button-label)]]))
+
+(defn- qr-scan-hole-area
+  [qr-view-finder]
+  [rn/view
+   {:style     style/qr-view-finder
+    :on-layout (fn [event]
+                 (let [layout      (js->clj (oops/oget event "nativeEvent.layout")
+                                            :keywordize-keys
+                                            true)
+                       view-finder (assoc layout :height (:width layout))]
+                   (reset! qr-view-finder view-finder)))}])
+
+(defn- border
+  [border1 border2 corner]
+  [rn/view {:style (style/border border1 border2 corner)}])
+
+(defn- border-tip
+  [{:keys [top bottom left right]}]
+  [rn/view
+   {:style (style/border-tip top bottom right left)}])
+
+(defn- viewfinder
+  [qr-view-finder]
+  (let [size (+ (:width qr-view-finder) 2)]
     [:<>
-     [rn/view {:style style/qr-code-container}
-      [quo/qr-code
-       {:source {:uri source-uri}
-        :width  qr-size
-        :height qr-size}]
-      
-         ]
-
-     
-        ]))
-
-(defn wallet-tab
-  []
-  [rn/text {:style style/wip-style} "not implemented"])
-
-(defn tab-content
-  [window-width]
-  (let [selected-tab (reagent/atom :profile)]
-    (fn []
-      [:<>
-       [header]
-       [rn/view {:style style/tabs-container}
-        [quo/segmented-control
-         {:size           28
-          :blur?          true
-          :override-theme :dark
-          :on-change      #(reset! selected-tab %)
-          :default-active :profile
-          :data           [{:id    :profile
-                            :label (i18n/label :t/profile)}
-                           
-                            ]}]]
-       [profile-tab window-width]
-         ])))
-
-(defn view
-  []
-  (let [window-width (rf/sub [:dimensions/window-width])]
-    (fn []
+     [rn/view {:style (style/viewfinder-container qr-view-finder)}
       [rn/view
+       {:style (style/qr-view-finder-container size)}
+       [rn/view
+        {:style style/view-finder-border-container}
+        [rn/view
+         [border :border-top-width :border-left-width :border-top-left-radius]
+         [border-tip {:right -1 :top 0}]
+         [border-tip {:left 0 :bottom -1}]]
+        [rn/view
+         [border :border-top-width :border-right-width :border-top-right-radius]
+         [border-tip {:right 0 :bottom -1}]
+         [border-tip {:left -1 :top 0}]]]
+       [rn/view {:flex-direction :row :justify-content :space-between}
+        [rn/view
+         [border :border-bottom-width :border-left-width :border-bottom-left-radius]
+         [border-tip {:right -1 :bottom 0}]
+         [border-tip {:left 0 :top -1}]]
+        [rn/view
+         [border :border-bottom-width :border-right-width :border-bottom-right-radius]
+         [border-tip {:right 0 :top -1}]
+         [border-tip {:left -1 :bottom 0}]]]
+       [quo/button
+        {:icon                      true
+         :type                      :blur-bg
+         :size                      32
+         :accessibility-label       :camera-flash
+         :override-background-color colors/neutral-80-opa-40
+         :style                     style/camera-flash-button}
+        :i/flashlight-off]]
+      [quo/text
+       {:size   :paragraph-2
+        :weight :regular
+        :style  style/viewfinder-text}
+       (i18n/label :t/ensure-qr-code-is-in-focus-to-scan)]]]))
+
+(defn- scan-qr-code-tab
+  [qr-view-finder]
+  [:<>
+   [rn/view {:style style/scan-qr-code-container}]
+   (when (empty? @qr-view-finder)
+     [qr-scan-hole-area qr-view-finder])
+   (if (and @preflight-check-passed?
+            @camera-permission-granted?
+            (boolean (not-empty @qr-view-finder)))
+     [viewfinder @qr-view-finder]
+     [camera-access-permission-view])])
+
+
+(defn- check-qr-code-data
+  [event]
+  (let [connection-string        (string/trim (oops/oget event "nativeEvent.codeStringValue"))
+        valid-connection-string? (sync-utils/valid-connection-string? connection-string)]
+    (if valid-connection-string?
+      (rf/dispatch [:syncing/input-connection-string-for-bootstrapping connection-string])
+      (rf/dispatch [:toasts/upsert
+                    {:icon           :i/info
+                     :icon-color     colors/danger-50
+                     :override-theme :light
+                     :text           (i18n/label :t/error-this-is-not-a-sync-qr-code)}]))))
+
+(defn render-camera
+  [show-camera? qr-view-finder camera-ref on-read-code show-holes?]
+  (when (and show-camera? (:x qr-view-finder))
+    [:<>
+     [rn/view {:style style/camera-container}
+      [camera-kit/camera
+       {:ref            #(reset! camera-ref %)
+        :style          style/camera-style
+        :camera-options {:zoomMode :off}
+        :scan-barcode   true
+        :on-read-code   on-read-code}]]
+     [hole-view/hole-view
+      {:style style/hole
+       :holes (if show-holes?
+                [(merge qr-view-finder
+                        {:borderRadius 16})]
+                [])}
+      [blur/view
+       {:style            style/absolute-fill
+        :blur-amount      10
+        :blur-type        :transparent
+        :overlay-color    colors/neutral-80-opa-80
+        :background-color colors/neutral-80-opa-80}]]]))
+
+(defn f-view
+  [{:keys [title show-bottom-view? background]}]
+  (let [insets         (safe-area/get-insets)
+        qr-view-finder (reagent/atom {})]
+    (fn []
+      (let [camera-ref                       (atom nil)
+            read-qr-once?                    (atom false)
+            ;; The below check is to prevent scanning of any QR code
+            ;; when the user is in syncing progress screen
+            user-in-syncing-progress-screen? (= (rf/sub [:view-id]) :syncing-progress)
+            on-read-code                     (fn [data]
+                                               (when (and (not @read-qr-once?)
+                                                          (not user-in-syncing-progress-screen?))
+                                                 (reset! read-qr-once? true)
+                                                 (js/setTimeout (fn []
+                                                                  (reset! read-qr-once? false))
+                                                                3000)
+                                                 (check-qr-code-data data)))
+            show-camera?                     (and @camera-permission-granted?
+                                                  @preflight-check-passed?)
+            show-holes?                      (and show-camera?
+                                                  (boolean (not-empty @qr-view-finder)))]
+        (rn/use-effect
+         (fn []
+           (when-not @camera-permission-granted?
+             (permissions/permission-granted? :camera
+                                              #(reset! camera-permission-granted? %)
+                                              #(reset! camera-permission-granted? false)))))
+        [:<>
+         background
+         [render-camera show-camera? @qr-view-finder camera-ref on-read-code show-holes?]
+         
+         [rn/view 
        {:flex        1
         :padding-top (navigation/status-bar-height)}
        [blur/view style/blur]
-       [tab-content window-width]])))
+         [header]          
+          [scan-qr-code-tab qr-view-finder]
+          [rn/view {:style style/flex-spacer}]]]))))
+
+(defn view
+  [props]
+  [:f> f-view props])
