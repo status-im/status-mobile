57e85db6aa9493d96fc0903991ff20a5637e9a5f
yqrashawn <namy.19@gmail.com>
2023-06-19 10:39:10 +0800

    feat: new image server handlers

--- src/utils/image_server.cljs
+++ src/utils/image_server.cljs
@@ -7,6 +7,7 @@
 
 (def ^:const image-server-uri-prefix "https://localhost:")
 (def ^:const account-images-action "/accountImages")
+(def ^:const account-initials-action "/accountInitials")
 (def ^:const contact-images-action "/contactImages")
 (def ^:const generate-qr-action "/GenerateQRCode")
 (def ^:const status-profile-base-url "https://join.status.im/u/")
@@ -54,7 +55,22 @@
     4))
 
 (defn get-account-image-uri
-  [{:keys [port public-key image-name key-uid theme ring?]}]
+  "fn to get the avatar uri when multiaccount has custom image set
+  not directly called, check `get-account-image-uri-fn`
+
+  color formats (for all color options):
+  #RRGGBB
+  #RRGGBBAA
+  rgb(255,255,255)
+  rgba(255,255,255,0.1) note alpha is 0-1
+
+  non-placeholder-avatar: requires at least one of `public-key` or `key-uid`
+  placeholder-avatar: pass image file path as `image-name`
+
+  `indicator-size` is outer indicator radius
+  `indicator-size` - `indicator-border` is inner indicator radius"
+  [{:keys [port public-key image-name key-uid size theme indicator-size
+           indicator-border indicator-color ring?]}]
   (str image-server-uri-prefix
        port
        account-images-action
@@ -64,15 +80,130 @@
        key-uid
        "&imageName="
        image-name
+       "&size="
+       size
        "&theme="
        (current-theme-index theme)
        "&clock="
        (timestamp)
+       "&indicatorColor="
+       (js/encodeURIComponent indicator-color)
+       "&indicatorSize="
+       indicator-size
+       "&indicatorBorder="
+       indicator-border
        "&addRing="
        (if ring? 1 0)))
 
+(defn get-account-image-uri-fn
+  "pass the result fn to user-avatar component as `:profile-picture`
+
+  use this fn in subs to set multiaccount `:images` as [{:fn ...}]
+  pass the image to user-avatar
+  user-avatar can fill the rest style related options
+
+  set `override-ring?` to a non-nil value to override `ring?`, mainly used to
+  hide ring for account with ens name
+
+  check `get-account-image-uri` for color formats"
+  [{:keys [port public-key key-uid image-name theme override-ring?]}]
+  (fn [{:keys [size indicator-size indicator-border indicator-color ring?
+               override-theme]}]
+    (get-account-image-uri
+     {:port             port
+      :image-name       image-name
+      :size             size
+      :public-key       public-key
+      :key-uid          key-uid
+      :theme            (if (nil? override-theme) theme override-theme)
+      :indicator-size   indicator-size
+      :indicator-border indicator-border
+      :indicator-color  indicator-color
+      :ring?            (if (nil? override-ring?) ring? override-ring?)})))
+
+(defn get-initials-avatar-uri
+  "fn to get the avatar uri when account/contact/placeholder has no custom pic set
+  not directly called, check `get-account-initials-uri-fn`
+
+  multiaccount: at least one of `key-uid`, `public-key` is required to render the ring
+  contact: `public-key` is required to render the ring
+
+  check `get-account-image-uri` for color formats
+  check `get-font-file-ready` for `font-file`
+
+  `uppercase-ratio` is the uppercase-height/line-height for `font-file`"
+  [{:keys [port public-key key-uid theme ring? length size background-color color
+           font-size font-file uppercase-ratio indicator-size indicator-border
+           indicator-color full-name]}]
+  (str
+   image-server-uri-prefix
+   port
+   account-initials-action
+   "?publicKey="
+   public-key
+   "&keyUid="
+   key-uid
+   "&length="
+   length
+   "&size="
+   size
+   "&bgColor="
+   (js/encodeURIComponent background-color)
+   "&color="
+   (js/encodeURIComponent color)
+   "&fontSize="
+   font-size
+   "&fontFile="
+   (js/encodeURIComponent font-file)
+   "&uppercaseRatio="
+   uppercase-ratio
+   "&theme="
+   (current-theme-index theme)
+   "&clock="
+   "&name="
+   (js/encodeURIComponent full-name)
+   (timestamp)
+   "&indicatorColor="
+   (js/encodeURIComponent indicator-color)
+   "&indicatorSize="
+   indicator-size
+   "&indicatorBorder="
+   indicator-border
+   "&addRing="
+   (if ring? 1 0)))
+
+(defn get-initials-avatar-uri-fn
+  "return a fn that calls `get-account-initials-uri`
+  pass the fn to user-avatar component to fill the style related options
+
+  check `get-account-image-uri` for color formats
+  check `get-font-file-ready` for `font-file`
+
+  check `get-account-image-uri-fn` for `override-ring?`"
+  [{:keys [port public-key key-uid theme override-ring? font-file]}]
+  (fn [{:keys [full-name length size background-color font-size color
+               indicator-size indicator-border indicator-color ring?
+               override-theme]}]
+    (get-initials-avatar-uri {:port             port
+                              :public-key       public-key
+                              :key-uid          key-uid
+                              :full-name        full-name
+                              :length           length
+                              :size             size
+                              :background-color background-color
+                              :theme            (if (nil? override-theme) theme override-theme)
+                              :ring?            (if (nil? override-ring?) ring? override-ring?)
+                              :font-size        font-size
+                              :color            color
+                              :font-file        font-file
+                              :uppercase-ratio  (:uppercase-ratio constants/initials-avatar-font-conf)
+                              :indicator-size   indicator-size
+                              :indicator-border indicator-border
+                              :indicator-color  indicator-color})))
+
 (defn get-contact-image-uri
-  [port public-key image-name clock theme]
+  [{:keys [port public-key image-name clock theme indicator-size indicator-border
+           indicator-color size ring?]}]
   (str image-server-uri-prefix
        port
        contact-images-action
@@ -80,11 +211,34 @@
        public-key
        "&imageName="
        image-name
+       "&size="
+       size
        "&theme="
        (current-theme-index theme)
        "&clock="
        clock
-       "&addRing=1"))
+       "&indicatorColor="
+       (js/encodeURIComponent indicator-color)
+       "&indicatorSize="
+       indicator-size
+       "&indicatorBorder="
+       indicator-border
+       "&addRing="
+       (if ring? 1 0)))
+
+(defn get-contact-image-uri-fn
+  [{:keys [port public-key image-name theme override-ring? clock]}]
+  (fn [{:keys [size indicator-size indicator-border indicator-color ring? override-theme]}]
+    (get-contact-image-uri {:port             port
+                            :image-name       image-name
+                            :public-key       public-key
+                            :size             size
+                            :theme            (if (nil? override-theme) theme override-theme)
+                            :clock            clock
+                            :indicator-size   indicator-size
+                            :indicator-border indicator-border
+                            :indicator-color  indicator-color
+                            :ring?            (if (nil? override-ring?) ring? override-ring?)})))
 
 (defn get-account-qr-image-uri
   [{:keys [key-uid public-key port qr-size]}]
--- src/utils/image_server_test.cljs
+++ src/utils/image_server_test.cljs
@@ -8,10 +8,36 @@
      sut/timestamp           (constantly "timestamp")]
     (t/is
      (=
-      (sut/get-account-image-uri {:port       "port"
-                                  :public-key "public-key"
-                                  :image-name "image-name"
-                                  :key-uid    "key-uid"
-                                  :theme      "theme"
-                                  :ring?      true})
-      "https://localhost:port/accountImages?publicKey=public-key&keyUid=key-uid&imageName=image-name&theme=theme&clock=timestamp&addRing=1"))))
+      (sut/get-account-image-uri {:port            "port"
+                                  :public-key      "public-key"
+                                  :image-name      "image-name"
+                                  :key-uid         "key-uid"
+                                  :theme           :dark
+                                  :indicator-size  "indicator-size"
+                                  :indicator-color "rgba(9,16,28,0.08)"
+                                  :ring?           true})
+      "https://localhost:port/accountImages?publicKey=public-key&keyUid=key-uid&imageName=image-name&size=&theme=:dark&clock=timestamp&indicatorColor=rgba(9%2C16%2C28%2C0.08)&indicatorSize=indicator-size&indicatorBorder=&addRing=1"))))
+
+(t/deftest get-account-initials-uri
+  (with-redefs
+    [sut/current-theme-index identity
+     sut/timestamp           (constantly "timestamp")]
+    (t/is
+     (=
+      (sut/get-initials-avatar-uri
+       {:port             "port"
+        :public-key       "public-key"
+        :key-uid          "key-uid"
+        :full-name        "full-name"
+        :length           "length"
+        :size             "size"
+        :theme            :light
+        :ring?            "ring?"
+        :background-color "background-color"
+        :color            "#0E162000"
+        :font-size        "font-size"
+        :font-file        "/font/Inter Medium.otf"
+        :uppercase-ratio  "uppercase-ratio"
+        :indicator-size   "indicator-size"
+        :indicator-color  "#0E1620"})
+      "https://localhost:port/accountInitials?publicKey=public-key&keyUid=key-uid&length=length&size=size&bgColor=background-color&color=%230E162000&fontSize=font-size&fontFile=%2Ffont%2FInter%20Medium.otf&uppercaseRatio=uppercase-ratio&theme=:light&clock=&name=full-nametimestamp&indicatorColor=%230E1620&indicatorSize=indicator-size&indicatorBorder=&addRing=1"))))
