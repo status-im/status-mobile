d02f169d2dfe13e83ac5a14bffdac2b971c0b03e
yqrashawn <namy.19@gmail.com>
2023-06-19 10:40:36 +0800

    feat: add :ens-name? for multiaccount (to use before login)

--- src/status_im2/contexts/profile/rpc.cljs
+++ src/status_im2/contexts/profile/rpc.cljs
@@ -1,9 +1,11 @@
 (ns status-im2.contexts.profile.rpc
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [status-im.ethereum.ens :as ens]))
 
 (defn rpc->profiles-overview
   [{:keys [customizationColor keycard-pairing] :as profile}]
   (-> profile
       (dissoc :customizationColor)
       (assoc :customization-color (keyword customizationColor))
+      (assoc :ens-name? (ens/is-valid-eth-name? (:name profile)))
       (assoc :keycard-pairing (when-not (string/blank? keycard-pairing) keycard-pairing))))
