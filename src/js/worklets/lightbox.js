c80992b6bf48a8bd2aed91be67dd6b7574a8fa6b
Rahul Pratap <rahulpsingh@users.noreply.github.com>
2023-06-19 19:09:12 +0530

    Update prettier config for the project. (#16303)
    * Update prettier config for the project.
    
    * Add prettier to make lint-fix.

--- src/js/worklets/lightbox.js
+++ src/js/worklets/lightbox.js
@@ -1,10 +1,8 @@
 import { useDerivedValue } from 'react-native-reanimated';
 
 export function infoLayout(input, isTop) {
-    return useDerivedValue(
-        function () {
-            'worklet'
-            return isTop ? input.value : -input.value
-        }
-    );
+  return useDerivedValue(function () {
+    'worklet';
+    return isTop ? input.value : -input.value;
+  });
 }
