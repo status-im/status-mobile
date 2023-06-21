1078285413f437b5e98f521b33a32ed88f73a8d9
Jamie Caprani <jamiecaprani@gmail.com>
2023-06-21 14:22:52 +0100

    update validation

--- src/status_im2/contexts/onboarding/create_profile/view.cljs
+++ src/status_im2/contexts/onboarding/create_profile/view.cljs
@@ -16,6 +16,9 @@
             [status-im2.constants :as c]
             [react-native.platform :as platform]))
 
+;; NOTE - validation should match with Desktop
+;; https://github.com/status-im/status-desktop/blob/2ba96803168461088346bf5030df750cb226df4c/ui/imports/utils/Constants.qml#L468
+;; 
 (def emoji-regex
   (new
    js/RegExp
@@ -24,11 +27,10 @@
 (defn has-emojis [s] (re-find emoji-regex s))
 (def common-names ["Ethereum" "Bitcoin"])
 (defn has-common-names [s] (pos? (count (filter #(string/includes? s %) common-names))))
-(def special-characters-regex (new js/RegExp #"[^a-zA-Z\d\s-._]" "i"))
-(defn has-special-characters [s] (re-find special-characters-regex s))
+(def status-regex (new js/RegExp #"^[a-zA-Z0-9\-_ ]+$"))
+(defn has-special-characters [s] (not (re-find status-regex s)))
 (def min-length 5)
 (defn length-not-valid [s] (< (count (string/trim (str s))) min-length))
-
 
 (defn validation-message
   [s]
@@ -39,6 +41,8 @@
     (string/ends-with? s "-eth") (i18n/label :t/ending-not-allowed {:ending "-eth"})
     (string/ends-with? s "_eth") (i18n/label :t/ending-not-allowed {:ending "_eth"})
     (string/ends-with? s ".eth") (i18n/label :t/ending-not-allowed {:ending ".eth"})
+    (string/starts-with? s " ")  (i18n/label :t/start-with-space)
+    (string/ends-with? s " ")    (i18n/label :t/ends-with-space)
     (has-common-names s)         (i18n/label :t/are-not-allowed {:check (i18n/label :t/common-names)})
     (has-emojis s)               (i18n/label :t/are-not-allowed {:check (i18n/label :t/emojis)})
     :else                        nil))
@@ -166,7 +170,7 @@
                                                        :display-name @full-name
                                                        :color        @custom-color}]))
            :style                     style/continue-button
-           :disabled                  (or (not (seq @full-name)) @validation-msg)}
+           :disabled                  (or (not valid-name?) (not (seq @full-name)))}
           (i18n/label :t/continue)]]]])
     (finally
      (oops/ocall show-listener "remove")
