7c0799db2a3c4a1b55b57e45851219c4e183d26d
DQ_Arjun <dq_arjun@DQ-Arjuns-MacBook-Pro.local>
2023-06-22 11:24:02 +0530

    code clean

--- src/status_im2/contexts/scan/view.cljs
+++ src/status_im2/contexts/scan/view.cljs
@@ -1,12 +1,8 @@
 (ns status-im2.contexts.scan.view
   (:require [react-native.core :as rn]
             [status-im2.contexts.scan.style :as style]
-            [status-im2.contexts.syncing.scan-sync-code.view :as scan-sync-code]
-            [react-native.navigation :as navigation]
             [status-im2.common.scan-qr-code.view :as scan-qr-code]
             [utils.re-frame :as rf]
-            [react-native.blur :as blur]
-            [status-im2.contexts.onboarding.common.background.view :as background]
             [react-native.safe-area :as safe-area]
             [quo2.core :as quo]
             [utils.i18n :as i18n]))
@@ -33,11 +29,5 @@
   []
   (let [insets         (safe-area/get-insets)]
   [:<>
-  [:f> scan-qr-code/view header]
-    ;; [background/view true]
-    ;; [rn/view {:style (style/root-container (:top insets))}
-    ;; [header]
-        ;; [:f> scan-qr-code/view header]
-      ;; [rn/view {:style style/flex-spacer}]]
-  ]
+  [:f> scan-qr-code/view header]]
    ))
