33b2e12e3b8b5c8e86ca6999f4a890b9601f4eeb
Jakub Sokołowski <jakub@status.im>
2023-06-23 10:43:12 +0200

    android: fix Gradle deps, drop Flipper, upgrade Fresco
    There are a few fixes here:
    
    * Changes format of Gradle deps to `:buildEnvironment` and `:dependencies`:
        ```
        Cannot locate tasks that match '::buildEnvironment'. The path should not include an empty segment (try ':buildEnvironment' instead).
        ```
    * Fix for GNU Parallel tool to use stdin for list of arguments:
        ```
        nix/deps/gradle/generate.sh: line 55: parallel: Argument list too long
        ```
    * Update of Facebook Fresco dependencies from `2.2.0` to `2.5.0` to fix:
        ```
         > echo com.facebook.fresco:animated-gif:2.2.0 | ./go-maven-resolver > /dev/null
        finder.go:121: error: 'no pom data' for: <Dep ID=com.facebook.fresco:imagepipeline-native:2.2.0 O=false S=compile >
        finder.go:121: error: 'no pom data' for: <Dep ID=com.facebook.fresco:memory-type-native:2.2.0 O=false S=compile >
        finder.go:121: error: 'no pom data' for: <Dep ID=com.facebook.fresco:imagepipeline:2.2.0 O=false S=compile >
        finder.go:121: error: 'no pom data' for: <Dep ID=com.facebook.fresco:drawee:2.2.0 O=false S=runtime >
        finder.go:121: error: 'no pom data' for: <Dep ID=com.facebook.fresco:memory-type-java:2.2.0 O=false S=compile >
        finder.go:121: error: 'no pom data' for: <Dep ID=com.facebook.fresco:fbcore:2.2.0 O=false S=runtime >
        ```
    * Remove Flipper to avoid pulling in Fresco `2.2.0` versions:
        ```
        +--- com.facebook.flipper:flipper-fresco-plugin:0.54.0
        |    +--- com.facebook.fresco:fresco:2.2.0 -> 2.5.0
        |    +--- com.facebook.fresco:flipper:2.2.0
        |    \--- com.facebook.fresco:stetho:2.2.0
        ```
    
    Thanks for help from Sidarth.
    
    Resolves: https://github.com/status-im/status-mobile/issues/16367
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- android/app/src/main/java/im/status/ethereum/MainApplication.java
+++ android/app/src/main/java/im/status/ethereum/MainApplication.java
@@ -82,36 +82,6 @@ public class MainApplication extends NavigationApplication {
         OkHttpClientProvider.setOkHttpClientFactory(new StatusOkHttpClientFactory());
 
         WebView.setWebContentsDebuggingEnabled(BuildConfig.DEBUG_WEBVIEW == "1");
-        //initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
-    /**
-     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-     * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-     *
-     * @param context
-     * @param reactInstanceManager
-     */
-    private static void initializeFlipper(
-          Context context, ReactInstanceManager reactInstanceManager) {
-        if (BuildConfig.DEBUG) {
-            try {
-                /*
-                  We use reflection here to pick up the class that initializes Flipper,
-                  since Flipper library is not available in release mode
-                */
-                Class<?> aClass = Class.forName("im.status.ethereum.ReactNativeFlipper");
-                aClass
-                    .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-                    .invoke(null, context, reactInstanceManager);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+
 }
