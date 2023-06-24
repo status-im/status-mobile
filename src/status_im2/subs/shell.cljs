ad90fd7a1c123d91c2d06b7a7fab310e9a93b14c
Ibrahem <vampirekid017@gmail.com>
2023-06-24 15:58:02 +0300

    Use cond

--- src/status_im2/subs/shell.cljs
+++ src/status_im2/subs/shell.cljs
@@ -190,19 +190,20 @@
    (let [{:keys [chats-stack community-stack]}
          (reduce
           (fn [acc [_ {:keys [unviewed-messages-count unviewed-mentions-count chat-type muted]}]]
-            (when-not muted
-              (case chat-type
-                constants/community-chat-type
-                (-> acc
-                    (update-in [:community-stack :unviewed-messages-count] + unviewed-messages-count)
-                    (update-in [:community-stack :unviewed-mentions-count] + unviewed-mentions-count))
-
-                (constants/private-group-chat-type constants/one-to-one-chat-type)
-                (-> acc
-                    (update-in [:chats-stack :unviewed-messages-count] + unviewed-messages-count)
-                    (update-in [:chats-stack :unviewed-mentions-count] + unviewed-mentions-count))
-
-                acc)))
+            (cond
+              (and (not muted)
+                   (= chat-type constants/community-chat-type))
+              (-> acc
+                  (update-in [:community-stack :unviewed-messages-count] + unviewed-messages-count)
+                  (update-in [:community-stack :unviewed-mentions-count] + unviewed-mentions-count))
+              
+              (and (not muted)
+                   (#{constants/private-group-chat-type constants/one-to-one-chat-type} chat-type))
+              (-> acc
+                  (update-in [:chats-stack :unviewed-messages-count] + unviewed-messages-count)
+                  (update-in [:chats-stack :unviewed-mentions-count] + unviewed-mentions-count))
+              :else
+              acc))
           {:chats-stack     {:unviewed-messages-count 0 :unviewed-mentions-count 0}
            :community-stack {:unviewed-messages-count 0 :unviewed-mentions-count 0}}
           chats)]
