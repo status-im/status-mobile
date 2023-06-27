db7fb0bb36b5704825c94d3ff93fb07dd7db0499
Jamie Caprani <jamiecaprani@gmail.com>
2023-06-27 13:41:49 +0100

    chore: added another component test

--- src/quo2/components/markdown/list/component_spec.cljs
+++ src/quo2/components/markdown/list/component_spec.cljs
@@ -1,6 +1,8 @@
 (ns quo2.components.markdown.list.component-spec
   (:require [quo2.components.markdown.list.view :as list]
-            [test-helpers.component :as h]))
+            [test-helpers.component :as h]
+            [react-native.core :as rn]
+            [quo2.core :as quo]))
 
 (h/describe "tests for markdown/list component"
   (h/test "renders component with title"
@@ -22,4 +24,14 @@
 
   (h/test "renders step component when index is valid"
     (h/render [list/view {:index 1}])
-    (h/is-truthy (h/get-by-label-text :step-counter))))
+    (h/is-truthy (h/get-by-label-text :step-counter)))
+
+  (h/test "renders decription with a context tag component"
+    (h/render [list/view
+               {:index       1
+                :description [rn/view {:style {:flex-direction :row :align-items :center}}
+                              [quo/text {} "Lorem ipsum "]
+                              [quo/context-tag {:size :small} "some-path" "dolor"]
+                              [quo/text {} " sit amet."]]}])
+    (h/is-truthy (h/get-by-text "Lorem ipsum"))
+    (h/is-truthy (h/get-by-label-text :user-avatar))))
