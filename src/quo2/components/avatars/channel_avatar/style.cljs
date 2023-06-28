2b90409a3d39d1a7da4b3068e3fd5335e47f53c9
Icaro Motta <icaro.ldm@gmail.com>
2023-06-28 14:52:32 -0300

    Extract to common let binding

--- src/quo2/components/avatars/channel_avatar/style.cljs
+++ src/quo2/components/avatars/channel_avatar/style.cljs
@@ -5,21 +5,23 @@
 
 (defn outer-container
   [{:keys [big? color]}]
-  {:width            (if big? 32 24)
-   :height           (if big? 32 24)
-   :border-radius    (if big? 32 24)
-   :justify-content  :center
-   :align-items      :center
-   :background-color (colors/theme-alpha color 0.1 0.1)})
+  (let [size (if big? 32 24)]
+    {:width            size
+     :height           size
+     :border-radius    size
+     :justify-content  :center
+     :align-items      :center
+     :background-color (colors/theme-alpha color 0.1 0.1)}))
 
 (defn lock-container
   [{:keys [big?]}]
-  {:position         :absolute
-   :left             (if big? 20 12)
-   :top              (if big? 20 12)
-   :background-color (colors/theme-colors colors/white colors/neutral-90)
-   :border-radius    (* 2 lock-icon-size)
-   :padding          2})
+  (let [distance (if big? 20 12)]
+    {:position         :absolute
+     :left             distance
+     :top              distance
+     :background-color (colors/theme-colors colors/white colors/neutral-90)
+     :border-radius    (* 2 lock-icon-size)
+     :padding          2}))
 
 (def lock-icon
   {:width  lock-icon-size
