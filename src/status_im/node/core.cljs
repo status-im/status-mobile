1e8ea3fd16d27519bbae2de5e9b290f92ed275ee
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2023-07-01 10:44:32 +0100

    Enable lightclient
    https://github.com/status-im/status-go/compare/67050429...67050429

--- src/status_im/node/core.cljs
+++ src/status_im/node/core.cljs
@@ -70,7 +70,7 @@
    :Host           "0.0.0.0"
    :AutoUpdate     true
    ;; Temporary fix until https://github.com/status-im/status-go/issues/3024 is resolved
-   :Nameserver     "1.1.1.1"
+   :Nameserver     "8.8.8.8"
    :PeerExchange   true
    :Port           0
    :UDPPort        0})
