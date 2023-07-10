91431fecd5ad7e156d1860053f4e5c00bf64fd11
Omar Basem <omarbasem.0101@gmail.com>
2023-07-10 10:40:59 +0400

    feat: album selector animation

--- src/status_im2/contexts/chat/photo_selector/album_selector/view.cljs
+++ src/status_im2/contexts/chat/photo_selector/album_selector/view.cljs
@@ -3,19 +3,21 @@
     [quo2.core :as quo]
     [react-native.core :as rn]
     [react-native.gesture :as gesture]
+    [react-native.reanimated :as reanimated]
     [utils.i18n :as i18n]
     [utils.re-frame :as rf]
     [quo2.foundations.colors :as colors]
     [status-im2.contexts.chat.photo-selector.album-selector.style :as style]))
 
 (defn render-album
-  [{title :title size :count uri :uri} index _ {:keys [album? selected-album]}]
+  [{title :title size :count uri :uri} index _ {:keys [album? selected-album top]}]
   (let [selected? (= selected-album title)]
     [rn/touchable-opacity
      {:on-press            (fn []
                              (rf/dispatch [:chat.ui/camera-roll-select-album title])
                              (rf/dispatch [:photo-selector/get-photos-for-selected-album])
-                             (reset! album? false))
+                             (reanimated/animate top (:height (rn/get-window)))
+                             (js/setTimeout #(reset! album? false) 300))
       :style               (style/album-container selected?)
       :accessibility-label (str "album-" index)}
      [rn/image
@@ -52,20 +54,33 @@
   [item index]
   (str (:title item) index))
 
-(defn album-selector
-  [{:keys [scroll-enabled on-scroll]} album? selected-album]
+(defn- f-album-selector
+  [{:keys [scroll-enabled on-scroll]} album? selected-album top]
   (let [albums          (rf/sub [:camera-roll/albums])
         albums-sections [{:title no-title :data (:smart-albums albums)}
-                         {:title (i18n/label :t/my-albums) :data (:my-albums albums)}]]
-    [gesture/section-list
-     {:data                           albums-sections
-      :sections                       albums-sections
-      :render-data                    {:album? album? :selected-album selected-album}
-      :render-fn                      render-album
-      :sticky-section-headers-enabled false
-      :render-section-header-fn       section-header
-      :content-container-style        {:padding-top    64
-                                       :padding-bottom 40}
-      :key-fn                         key-fn
-      :scroll-enabled                 @scroll-enabled
-      :on-scroll                      on-scroll}]))
+                         {:title (i18n/label :t/my-albums) :data (:my-albums albums)}]
+        window-height   (:height (rn/get-window))]
+    [reanimated/view
+     {:style (reanimated/apply-animations-to-style {:top top}
+                                                   {:position         :absolute
+                                                    :z-index          1
+                                                    :background-color :white
+                                                    :left             0
+                                                    :right            0})}
+     [gesture/section-list
+      {:data                           albums-sections
+       :sections                       albums-sections
+       :render-data                    {:album? album? :selected-album selected-album :top top}
+       :render-fn                      render-album
+       :sticky-section-headers-enabled false
+       :render-section-header-fn       section-header
+       :content-container-style        {:padding-top    64
+                                        :padding-bottom 40}
+       :key-fn                         key-fn
+       :scroll-enabled                 @scroll-enabled
+       :on-scroll                      on-scroll
+       :style                          {:height window-height}}]]))
+
+(defn album-selector
+  [sheet album? selected-album top]
+  [:f> f-album-selector sheet album? selected-album top])
