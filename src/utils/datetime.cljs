0067468d5794f599886a54537e60f61620942a59
Ibrahem Khalil <vampirekid017@gmail.com>
2023-07-16 15:02:32 +0300

    Fix failing mute till test (#16453)

--- src/utils/datetime.cljs
+++ src/utils/datetime.cljs
@@ -296,35 +296,3 @@
 (def ^:const go-default-time
   "Zero value for golang's time var"
   "0001-01-01T00:00:00Z")
-
-(defn- add-leading-zero
-  [input-string]
-  (if (> 10 input-string)
-    (str "0" input-string)
-    input-string))
-
-(defn format-mute-till
-  [muted-till-string]
-  (let [parsed-time       (t.format/parse (t.format/formatters :date-time-no-ms) muted-till-string)
-        hours-and-minutes (str (add-leading-zero (t/hour (t/plus parsed-time time-zone-offset)))
-                               ":"
-                               (add-leading-zero (t/minute parsed-time)))
-        when-to-unmute    (cond (= go-default-time
-                                   muted-till-string)   (i18n/label :t/until-you-turn-it-back-on)
-                                (today? parsed-time)    (str hours-and-minutes " today")
-                                (tomorrow? parsed-time) (str hours-and-minutes " tomorrow")
-                                :else                   (str hours-and-minutes
-                                                             " "
-                                                             (i18n/label
-                                                              (keyword "t"
-                                                                       (get int->weekday
-                                                                            (t/day-of-week
-                                                                             parsed-time))))
-                                                             " "
-                                                             (t/day parsed-time)
-                                                             " "
-                                                             (i18n/label
-                                                              (keyword "t"
-                                                                       (get months
-                                                                            (t/month parsed-time))))))]
-    when-to-unmute))
