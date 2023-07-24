8b9114967198451a5c7ea5fd84c7bba36ab2c269
Omar Basem <omarbasem.0101@gmail.com>
2023-07-24 16:21:43 +0400

    review

--- src/quo2/components/wallet/network_amount/view.cljs
+++ src/quo2/components/wallet/network_amount/view.cljs
@@ -2,6 +2,7 @@
   (:require
     [clojure.string :as str]
     [quo2.components.markdown.text :as text]
+    [quo2.foundations.colors :as colors]
     [react-native.core :as rn]
     [status-im2.common.resources :as resources]
     [quo2.components.wallet.network-amount.style :as style]))
@@ -17,6 +18,7 @@
      :size  :paragraph-2
      :style style/text}
     (str amount " " (str/upper-case (clj->js token)))]
-   [rn/image
-    {:source (resources/ui :div)
-     :style  {:width 1 :height 8}}]])
+   [rn/view
+    {:style {:width            1
+             :height           8
+             :background-color (colors/theme-colors colors/neutral-40 colors/neutral-50)}}]])
