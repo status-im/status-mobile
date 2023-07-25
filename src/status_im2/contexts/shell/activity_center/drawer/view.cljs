ec6800216bc2bd5f50191406644e5fe57b4c6379
mmilad75 <55688834+mmilad75@users.noreply.github.com>
2023-07-25 18:06:00 +0330

    Notification Centre - add Empty Content screen (#16715)
    * Notification Centre - add Empty Content screen (#16715)

---
+++ src/status_im2/contexts/shell/activity_center/drawer/view.cljs
@@ -0,0 +1,23 @@
+(ns status-im2.contexts.shell.activity-center.drawer.view
+  (:require [utils.re-frame :as rf]
+            [quo2.core :as quo]
+            [utils.i18n :as i18n]
+            [quo2.foundations.colors :as colors]))
+
+(defn options
+  []
+  (let [unread-count (rf/sub [:activity-center/unread-count])]
+    [quo/action-drawer
+     [[{:icon           :i/mark-as-read
+        :override-theme :dark
+        :label          (i18n/label :t/mark-all-notifications-as-read)
+        :disabled?      (zero? unread-count)
+        :on-press       (fn []
+                          (rf/dispatch [:activity-center.notifications/mark-all-as-read-locally
+                                        (fn []
+                                          {:icon           :up-to-date
+                                           :icon-color     colors/success-50
+                                           :text           (i18n/label :t/notifications-marked-as-read
+                                                                       {:count unread-count})
+                                           :override-theme :dark})])
+                          (rf/dispatch [:hide-bottom-sheet]))}]]]))
