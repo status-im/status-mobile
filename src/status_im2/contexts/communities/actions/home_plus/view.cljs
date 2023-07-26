c130b5d6cf4a0439213a6f6b66bb7e5333e5e7f8
frank <lovefree103@gmail.com>
2023-07-27 03:07:31 +0800

    fast create community to test

--- src/status_im2/contexts/communities/actions/home_plus/view.cljs
+++ src/status_im2/contexts/communities/actions/home_plus/view.cljs
@@ -1,6 +1,7 @@
 (ns status-im2.contexts.communities.actions.home-plus.view
   (:require [quo2.core :as quo]
-            [utils.re-frame :as rf]))
+            [utils.re-frame :as rf]
+            [status-im2.config :as config]))
 
 (defn view
   []
@@ -12,4 +13,17 @@
      {:icon                :i/communities
       :accessibility-label :create-community
       :label               "Create community (only for e2e)"
-      :on-press            #(rf/dispatch [:legacy-only-for-e2e/open-create-community])}]]])
+      :on-press            #(rf/dispatch [:legacy-only-for-e2e/open-create-community])}
+     (when config/fast-create-community-enabled?
+       {:icon                :i/communities
+        :accessibility-label :create-closed-community
+        :label               "Create closed community"
+        :on-press            #(rf/dispatch [:fast-create-community/create-closed-community])}
+       {:icon                :i/communities
+        :accessibility-label :create-open-community
+        :label               "Create open community"
+        :on-press            #(rf/dispatch [:fast-create-community/create-open-community])}
+       {:icon                :i/communities
+        :accessibility-label :create-token-gated-community
+        :label               "Create token-gated community"
+        :on-press            #(rf/dispatch [:fast-create-community/create-token-gated-community])})]]])
