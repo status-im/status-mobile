baa6727809a7575b14aa56a78c5df63382c98524
Omar Basem <omarbasem.0101@gmail.com>
2023-07-27 09:23:17 +0400

    review

--- src/status_im2/contexts/chat/camera/view.cljs
+++ src/status_im2/contexts/chat/camera/view.cljs
@@ -14,6 +14,26 @@
     [status-im2.contexts.chat.camera.style :as style]
     [utils.re-frame :as rf]))
 
+(defn retake
+  [flash uri]
+  (let [current-flash @flash]
+    (when platform/android?
+      (reset! flash false) ; On Android, setting flash needs to be delayed until camera has initialized
+      (js/setTimeout #(reset! flash current-flash) 300))
+    (reset! uri nil)))
+
+(defn handle-orientation
+  [current-orientation rotate]
+  (orientation/use-device-orientation-change (fn [result]
+                                               (reset! current-orientation result)
+                                               (cond
+                                                 (= result orientation/landscape-left)
+                                                 (reanimated/animate rotate "90deg")
+                                                 (= result orientation/landscape-right)
+                                                 (reanimated/animate rotate "-90deg")
+                                                 :else
+                                                 (reanimated/animate rotate "0deg")))))
+
 (defn- f-zoom-button
   [{:keys [value current-zoom rotate]}]
   (let [selected? (= @current-zoom value)
@@ -47,94 +67,108 @@
                  (camera-kit/capture @camera-ref #(reset! uri %)))
      :style    style/inner-circle}]])
 
-(defn retake
-  [flash uri]
-  (let [current-flash @flash]
-    (when platform/android?
-      (reset! flash false) ; On Android, setting flash needs to be delayed until camera has initialized
-      (js/setTimeout #(reset! flash current-flash) 300))
-    (reset! uri nil)))
+(defn zoom-buttons
+  []
+  (let [current-zoom (reagent/atom 1)]
+    (fn [top insets rotate]
+      [rn/view {:style (style/zoom-container top insets)}
+       [zoom-button {:value 0.5 :current-zoom current-zoom :rotate rotate}]
+       [zoom-button {:value 1 :current-zoom current-zoom :rotate rotate}]
+       [zoom-button {:value 2 :current-zoom current-zoom :rotate rotate}]
+       [zoom-button {:value 3 :current-zoom current-zoom :rotate rotate}]])))
+
+
+(defn- f-bottom-area
+  [{:keys [top insets uri camera-ref rotate]} back flip-camera]
+  [rn/view {:style (style/bottom-area top insets @uri)}
+   [quo/text {:style style/photo-text} (i18n/label :t/photo-caps)]
+   [rn/view {:style style/actions-container}
+    [quo/text
+     {:on-press            back
+      :style               {:font-size 17
+                            :color     colors/white}
+      :accessibility-label :cancel}
+     (i18n/label :t/cancel)]
+    [snap-button camera-ref uri]
+    [reanimated/touchable-opacity
+     {:style    (reanimated/apply-animations-to-style {:transform [{:rotate rotate}]} {})
+      :on-press flip-camera}
+     [quo/icon :i/rotate-camera
+      {:size 48 :color colors/white :accessibility-label :flip-camera}]]]])
+
+(defn bottom-area
+  [{:keys [flash camera-type] :as args}]
+  (let [back        #(rf/dispatch [:navigate-back])
+        flip-camera (fn []
+                      (reset! flash false)
+                      (reset! camera-type (if (= @camera-type camera-kit/camera-type-back)
+                                            camera-kit/camera-type-front
+                                            camera-kit/camera-type-back)))]
+    [:f> f-bottom-area args back flip-camera]))
+
+(defn- f-camera-screen
+  [{:keys [camera-ref uri camera-type current-orientation flash toggle-flash]}]
+  (let [window                 (rn/get-window)
+        {:keys [width height]} window
+        camera-window-height   (* width 1.33)
+        insets                 (safe-area/get-insets)
+        top                    (/ (- height camera-window-height (:bottom insets)) 2)
+        top-landscape          (/ (- height (* width 0.75) (:bottom insets)) 2)
+        portrait?              (= @current-orientation orientation/portrait)
+        rotate                 (reanimated/use-shared-value "0deg")
+        retake                 #(retake flash uri)
+        use-photo              (fn []
+                                 (rf/dispatch [:photo-selector/camera-roll-pick {:uri @uri}])
+                                 (rf/dispatch [:navigate-back]))]
+    (handle-orientation current-orientation rotate)
+    [rn/view {:style style/screen-container}
+     [reanimated/touchable-opacity
+      {:active-opacity 1
+       :on-press       toggle-flash
+       :style          (style/flash-container rotate @uri)}
+      (when-not @flash
+        [rn/view {:style style/cancel-dash}])
+      [quo/icon :i/flash-camera
+       {:color colors/white
+        :size  24}]]
+     (if @uri
+       [fast-image/fast-image
+        {:style  (style/image width camera-window-height (if portrait? top top-landscape) portrait?)
+         :source {:uri @uri}}]
+       [camera-kit/camera
+        {:ref         #(reset! camera-ref %)
+         :style       (style/camera-window width camera-window-height top)
+         :flash-mode  (if @flash :on :off)
+         :camera-type @camera-type}])
+     (when-not @uri
+       [zoom-buttons top insets rotate])
+     [rn/view {:style (style/confirmation-container insets @uri)}
+      [quo/text
+       {:on-press retake
+        :style    {:font-size 17
+                   :color     colors/white}}
+       (i18n/label :t/retake)]
+      [quo/text
+       {:on-press use-photo
+        :style    {:font-size 17
+                   :color     colors/white}}
+       (i18n/label :t/use-photo)]]
+     [bottom-area
+      {:top         top
+       :insets      insets
+       :uri         uri
+       :camera-type camera-type
+       :camera-ref  camera-ref
+       :flash       flash
+       :rotate      rotate}]]))
 
 (defn camera-screen
   []
-  (let [camera-ref          (atom nil)
-        uri                 (reagent/atom nil)
-        current-zoom        (reagent/atom "1")
-        camera-type         (reagent/atom camera-kit/camera-type-back)
-        flash               (reagent/atom false)
-        current-orientation (atom orientation/portrait)]
-    [:f>
-     (fn []
-       (let [window                 (rn/get-window)
-             {:keys [width height]} window
-             camera-window-height   (* width 1.33)
-             insets                 (safe-area/get-insets)
-             top                    (/ (- height camera-window-height (:bottom insets)) 2)
-             top-landscape          (/ (- height (* width 0.75) (:bottom insets)) 2)
-             portrait?              (= @current-orientation orientation/portrait)
-             rotate                 (reanimated/use-shared-value "0deg")]
-         (orientation/use-device-orientation-change (fn [result]
-                                                      (reset! current-orientation result)
-                                                      (case result
-                                                        orientation/landscape-left
-                                                        (reanimated/animate rotate "90deg")
-                                                        orientation/landscape-right
-                                                        (reanimated/animate rotate "-90deg")
-                                                        (reanimated/animate rotate "0deg"))))
-         [rn/view {:style style/screen-container}
-          [reanimated/touchable-opacity
-           {:active-opacity 1
-            :on-press       #(reset! flash (not @flash))
-            :style          (style/flash-container rotate @uri)}
-           (when-not @flash
-             [rn/view {:style style/cancel-dash}])
-           [quo/icon :i/flash-camera
-            {:color colors/white
-             :size  24}]]
-          (if @uri
-            [fast-image/fast-image
-             {:style  (style/image width camera-window-height (if portrait? top top-landscape) portrait?)
-              :source {:uri @uri}}]
-            [camera-kit/camera
-             {:ref         #(reset! camera-ref %)
-              :style       (style/camera-window width camera-window-height top)
-              :flash-mode  (if @flash :on :off)
-              :camera-type @camera-type}])
-          (when-not @uri
-            [rn/view {:style (style/zoom-container top insets)}
-             [zoom-button {:value "0.5" :current-zoom current-zoom :rotate rotate}]
-             [zoom-button {:value "1" :current-zoom current-zoom :rotate rotate}]
-             [zoom-button {:value "2" :current-zoom current-zoom :rotate rotate}]
-             [zoom-button {:value "3" :current-zoom current-zoom :rotate rotate}]])
-          [rn/view {:style (style/confirmation-container insets @uri)}
-           [quo/text
-            {:on-press #(retake flash uri)
-             :style    {:font-size 17
-                        :color     colors/white}}
-            (i18n/label :t/retake)]
-           [quo/text
-            {:on-press (fn []
-                         (rf/dispatch [:photo-selector/camera-roll-pick {:uri @uri}])
-                         (rf/dispatch [:navigate-back]))
-             :style    {:font-size 17
-                        :color     colors/white}}
-            (i18n/label :t/use-photo)]]
-          [rn/view {:style (style/bottom-area top insets @uri)}
-           [quo/text {:style style/photo-text} (i18n/label :t/PHOTO)]
-           [rn/view {:style style/actions-container}
-            [quo/text
-             {:on-press            #(rf/dispatch [:navigate-back])
-              :style               {:font-size 17
-                                    :color     colors/white}
-              :accessibility-label :cancel}
-             (i18n/label :t/cancel)]
-            [snap-button camera-ref uri]
-            [reanimated/touchable-opacity
-             {:style    (reanimated/apply-animations-to-style {:transform [{:rotate rotate}]} {})
-              :on-press (fn []
-                          (reset! flash false)
-                          (reset! camera-type (if (= @camera-type camera-kit/camera-type-back)
-                                                camera-kit/camera-type-front
-                                                camera-kit/camera-type-back)))}
-             [quo/icon :i/rotate-camera
-              {:size 48 :color colors/white :accessibility-label :flip-camera}]]]]]))]))
+  (let [flash (reagent/atom false)
+        args  {:camera-ref          (atom nil)
+               :uri                 (reagent/atom nil)
+               :camera-type         (reagent/atom camera-kit/camera-type-back)
+               :current-orientation (atom orientation/portrait)
+               :flash               flash
+               :toggle-flash        #(swap! flash not)}]
+    [:f> f-camera-screen args]))
