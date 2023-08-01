2290f91910ebd68acceabc578e2a4f511160f58d
Omar Basem <omarbasem.0101@gmail.com>
2023-08-01 14:08:02 +0400

    update test

--- src/quo2/components/wallet/network_bridge/component_spec.cljs
+++ src/quo2/components/wallet/network_bridge/component_spec.cljs
@@ -7,33 +7,33 @@
     (h/render [network-bridge/view
                {:amount  "50 SNT"
                 :network :ethereum
-                :state   :default}])
+                :status   :default}])
     (h/is-truthy (h/get-by-text "50 SNT")))
 
   (h/test "Network label renders"
     (h/render [network-bridge/view
                {:amount  "50 SNT"
                 :network :optimism
-                :state   :default}])
+                :status   :default}])
     (h/is-truthy (h/get-by-text "Optimism")))
 
   (h/test "Locked state"
     (h/render [network-bridge/view
                {:amount  "50 SNT"
                 :network :optimism
-                :state   :locked}])
+                :status   :locked}])
     (h/is-truthy (h/get-by-label-text :lock)))
 
   (h/test "Loading state"
     (h/render [network-bridge/view
                {:amount  "50 SNT"
                 :network :optimism
-                :state   :loading}])
+                :status   :loading}])
     (h/is-truthy (h/get-by-label-text :loading)))
 
   (h/test "Disabled state"
     (h/render [network-bridge/view
                {:amount  "50 SNT"
                 :network :optimism
-                :state   :disabled}])
+                :status   :disabled}])
     (h/has-style (h/get-by-label-text :container) {:opacity 0.3})))
