2819c204002c30d8d979b0d402c453714fe1ed15
erikseppanen <erikseppanen@users.noreply.github.com>
2023-08-04 11:43:39 -0400

    Quo2 wallet: add wallet overview (#16855)

--- src/quo2/core_spec.cljs
+++ src/quo2/core_spec.cljs
@@ -49,7 +49,8 @@
     [quo2.components.settings.category.component-spec]
     [quo2.components.share.share-qr-code.component-spec]
     [quo2.components.tags.status-tags-component-spec]
+    [quo2.components.wallet.account-card.component-spec]
     [quo2.components.wallet.network-amount.component-spec]
     [quo2.components.wallet.network-bridge.component-spec]
-    [quo2.components.wallet.account-card.component-spec]
-    [quo2.components.wallet.token-input.component-spec]))
+    [quo2.components.wallet.token-input.component-spec]
+    [quo2.components.wallet.wallet-overview.component-spec]))
