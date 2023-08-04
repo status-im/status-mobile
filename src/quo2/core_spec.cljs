9c75a799bbb6d708e9ef26fa4d721eb1fbc86f0d
Omar Basem <omarbasem.0101@gmail.com>
2023-08-04 08:46:51 +0400

    add test

--- src/quo2/core_spec.cljs
+++ src/quo2/core_spec.cljs
@@ -52,5 +52,11 @@
     [quo2.components.wallet.account-card.component-spec]
     [quo2.components.wallet.network-amount.component-spec]
     [quo2.components.wallet.network-bridge.component-spec]
+<<<<<<< HEAD
     [quo2.components.wallet.token-input.component-spec]
     [quo2.components.wallet.wallet-overview.component-spec]))
+=======
+    [quo2.components.wallet.account-card.component-spec]
+    [quo2.components.wallet.token-input.component-spec]
+    [quo2.components.list-items.token-value.component-spec]))
+>>>>>>> 44d5d4d15 (add test)
