d6b0299b5803bba982222227b4d7834f9db90c22
Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>
2023-08-07 20:36:26 +0530

    [feature] Add tests for progress bar component
    Signed-off-by: Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>

--- src/quo2/components/wallet/progress_bar/style.cljs
+++ src/quo2/components/wallet/progress_bar/style.cljs
@@ -3,28 +3,25 @@
 
 (defn- border-and-background-color
   [customization-color]
-  {:light {:pending   {:background-color colors/neutral-5
-                       :border-color     colors/neutral-80-opa-5}
-           :confirmed {:background-color colors/success-50
-                       :border-color     colors/neutral-80-opa-5}
-           :finalized {:background-color (colors/custom-color customization-color 50)
-                       :border-color     colors/neutral-80-opa-5}
-           :error     {:background-color colors/danger-50
-                       :border-color     colors/neutral-80-opa-5}}
-   :dark  {:pending   {:background-color colors/neutral-80
-                       :border-color     colors/neutral-70}
-           :confirmed {:background-color colors/success-60
-                       :border-color     colors/white-opa-5}
-           :finalized {:background-color (colors/custom-color customization-color 60)
-                       :border-color     colors/neutral-80-opa-5}
-           :error     {:background-color colors/danger-60
-                       :border-color     colors/white-opa-5}}})
+  {:light {:pending   {:border-color     colors/neutral-80-opa-5
+                       :background-color colors/neutral-5}
+           :confirmed {:border-color     colors/neutral-80-opa-5
+                       :background-color colors/success-50}
+           :finalized {:border-color     colors/neutral-80-opa-5
+                       :background-color (colors/custom-color customization-color 50)}
+           :error     {:border-color     colors/neutral-80-opa-5
+                       :background-color colors/danger-50}}
+   :dark  {:pending   {:border-color     colors/neutral-70
+                       :background-color colors/neutral-80}
+           :confirmed {:border-color     colors/white-opa-5
+                       :background-color colors/success-60}
+           :finalized {:border-color     colors/neutral-80-opa-5
+                       :background-color (colors/custom-color customization-color 60)}
+           :error     {:border-color     colors/white-opa-5
+                       :background-color colors/danger-60}}})
 
 (defn root-container
-  [{:keys [customization-color state theme]
-    :or   {customization-color :blue
-           state               :pending
-           theme               :light}}]
+  [{:keys [customization-color state theme]}]
   (let [{:keys [background-color border-color]} (get-in (border-and-background-color customization-color)
                                                         [theme state])]
     {:height           12
