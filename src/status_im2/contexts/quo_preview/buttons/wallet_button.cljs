438fd892c3a1d77d3f95d287c95cc6aeff071baf
Omar Basem <omarbasem.0101@gmail.com>
2023-08-14 12:15:57 +0400

    lint

--- src/status_im2/contexts/quo_preview/buttons/wallet_button.cljs
+++ src/status_im2/contexts/quo_preview/buttons/wallet_button.cljs
@@ -4,7 +4,6 @@
     [quo2.foundations.colors :as colors]
     [react-native.core :as rn]
     [reagent.core :as reagent]
-    [status-im2.common.resources :as resources]
     [status-im2.contexts.quo-preview.preview :as preview]))
 
 (def descriptor
