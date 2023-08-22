6910821cbfe809e47cc9359762977858f8b31a85
Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>
2023-08-22 19:14:30 +0530

    Added comments
    Signed-off-by: Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>

--- src/quo2/foundations/twemoji.cljs
+++ src/quo2/foundations/twemoji.cljs
@@ -3,5 +3,10 @@
 
 (def ^:private twemoji-js (js/require "../src/js/components/twemoji.js"))
 
+;; For rendering single Twemoji
+;; Usage in: Account avatar, Channel avatar, Emoji hash and Emoji picker
 (def twemoji (reagent/adapt-react-class (.-Twemoji ^js twemoji-js)))
+
+;; For parsing the string and replacing emoji with Twemoji equivalent
+;; Usage in: Chat
 (def text (reagent/adapt-react-class (.-TwemojiText ^js twemoji-js)))
