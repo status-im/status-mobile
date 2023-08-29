0fe027e9b0fb476ad082e70e0e4b06e1db63e8d6
Siddarth Kumar <siddarthkay@gmail.com>
2023-08-29 18:58:41 +0530

    fixed `node-libs-react-native/globals` import

--- metro.config.js
+++ metro.config.js
@@ -5,5 +5,17 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
+    resolver: {
+        extraNodeModules: require('node-libs-react-native'),
+    },
+};
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
