8a2cd729fede6abed30288ce4f9a0a18b598882d
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2023-08-29 16:20:57 +0100

    lint fix

--- src/status_im2/contexts/quo_preview/wallet/wallet_activity.cljs
+++ src/status_im2/contexts/quo_preview/wallet/wallet_activity.cljs
@@ -1,6 +1,5 @@
 (ns status-im2.contexts.quo-preview.wallet.wallet-activity
   (:require [quo2.core :as quo]
-            [quo2.foundations.colors :as colors]
             [react-native.core :as rn]
             [reagent.core :as reagent]
             [status-im2.common.resources :as resources]
