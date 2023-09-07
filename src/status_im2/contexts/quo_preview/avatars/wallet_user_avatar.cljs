024852eef7ad1a0a71a42ce6c9fb0760bed1ae26
Ulises M <ulises.ssb@hotmail.com>
2023-09-07 16:34:52 -0600

    Rename wallet-user-avatar's `:color` prop to `:customization-color`

--- src/status_im2/contexts/quo_preview/avatars/wallet_user_avatar.cljs
+++ src/status_im2/contexts/quo_preview/avatars/wallet_user_avatar.cljs
@@ -18,14 +18,14 @@
               {:key :size-64}
               {:key   :x-large
                :value "X Large"}]}
-   (preview/customization-color-option {:key :color})])
+   (preview/customization-color-option {:key :customization-color})])
 
 (defn view
   []
-  (let [state (reagent/atom {:first-name "empty"
-                             :last-name  "name"
-                             :size       :x-large
-                             :color      :indigo})]
+  (let [state (reagent/atom {:first-name          "empty"
+                             :last-name           "name"
+                             :size                :x-large
+                             :customization-color :indigo})]
     (fn []
       [preview/preview-container {:state state :descriptor descriptor}
        [quo/wallet-user-avatar @state]])))
