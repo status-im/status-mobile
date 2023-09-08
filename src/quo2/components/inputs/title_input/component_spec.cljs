7bb4f9cfe8815ed0f860f67d7c9c49ed4ae9e326
Ajay Sivan <ajayesivan@gmail.com>
2023-09-08 04:59:51 -0700

    Title input component improvements (#17209)
    - Add edit icon
    - Fix disable style

--- src/quo2/components/inputs/title_input/component_spec.cljs
+++ src/quo2/components/inputs/title_input/component_spec.cljs
@@ -2,39 +2,31 @@
   (:require [quo2.components.inputs.title-input.view :as title-input]
             [test-helpers.component :as h]))
 
-(h/describe "profile: title-input"
+(h/describe "input -> title-input component"
   (h/test "renders empty"
-    (h/render [title-input/title-input
-               {:value          ""
-                :on-change-text (h/mock-fn)}])
-    (-> (js/expect (h/get-by-label-text :profile-title-input))
-        (.toBeTruthy)))
+    (h/render [title-input/view {:value ""}])
+    (h/is-truthy (h/query-by-label-text :profile-title-input)))
 
   (h/test "empty text renders with max length digits and 00"
-    (h/render [title-input/title-input
-               {:value          ""
-                :max-length     24
-                :on-change-text (h/mock-fn)}])
-    (-> (js/expect (h/get-by-text "00"))
-        (.toBeTruthy))
-    (-> (js/expect (h/get-by-text "/24"))
-        (.toBeTruthy)))
+    (h/render [title-input/view
+               {:value      ""
+                :max-length 24}])
+    (h/fire-event :on-focus (h/query-by-label-text :profile-title-input))
+    (h/wait-for #(h/is-truthy (h/query-by-text "00")))
+    (h/is-truthy (h/query-by-text "/24")))
 
   (h/test "renders with max length digits and character count"
-    (h/render [title-input/title-input
-               {:default-value  "abc"
-                :max-length     24
-                :on-change-text (h/mock-fn)} "abc"])
-    (-> (js/expect (h/get-by-text "03"))
-        (.toBeTruthy))
-    (-> (js/expect (h/get-by-text "/24"))
-        (.toBeTruthy)))
+    (h/render [title-input/view
+               {:default-value "abc"
+                :max-length    24} "abc"])
+    (h/fire-event :on-focus (h/query-by-label-text :profile-title-input))
+    (h/wait-for #(h/is-truthy (h/query-by-text "03")))
+    (h/is-truthy (h/query-by-text "/24")))
 
   (h/test "text updates on change"
     (let [on-change-text (h/mock-fn)]
-      (h/render [title-input/title-input
+      (h/render [title-input/view
                  {:value          "mock text"
                   :on-change-text on-change-text}])
       (h/fire-event :change-text (h/get-by-label-text :profile-title-input) "mock-text-new")
-      (-> (js/expect on-change-text)
-          (.toHaveBeenCalled)))))
+      (h/was-called on-change-text))))
