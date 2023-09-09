e29825d3b12363e0286b09f6901fbb64be33e881
Siddarth Kumar <siddarthkay@gmail.com>
2023-09-09 21:25:15 +0530

    fix: node deps mess during rebase :)

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin', '@babel/plugin-transform-named-capturing-groups-regex'],
+  plugins: ['react-native-reanimated/plugin', '@babel/plugin-transform-named-capturing-groups-regex','@babel/plugin-proposal-logical-assignment-operators'],
   env: {
     test: {
       presets: [
