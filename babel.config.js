ff7772bb4c160ad061e2c73f7fdb26f5587ac168
Siddarth Kumar <siddarthkay@gmail.com>
2023-09-10 10:26:51 +0530

    attempt to fix linting

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin', '@babel/plugin-transform-named-capturing-groups-regex','@babel/plugin-proposal-logical-assignment-operators'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    '@babel/plugin-transform-named-capturing-groups-regex',
+    '@babel/plugin-proposal-logical-assignment-operators',
+  ],
   env: {
     test: {
       presets: [
--- metro.config.js
+++ metro.config.js
@@ -6,19 +6,19 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-    transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: true,
-            },
-        }),
-    },
-    resolver: {
-        resolverMainFields: ['react-native', 'browser', 'main'],
-        nodeModulesPaths: ['./node_modules'],
-        extraNodeModules: require('node-libs-react-native'),
-        unstable_enableSymlinks: true
-    },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    resolverMainFields: ['react-native', 'browser', 'main'],
+    nodeModulesPaths: ['./node_modules'],
+    extraNodeModules: require('node-libs-react-native'),
+    unstable_enableSymlinks: true,
+  },
 };
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
