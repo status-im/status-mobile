91e30a9e494a870fa09d256c58c65204bca3f1a3
Milad <mmilad.sanati@gmail.com>
2023-09-11 18:33:37 +0330

    Revert "move profile-picture to component"
    This reverts commit c2a9f51bcbbb6a561019c15c8f59f85b83932ace.

--- src/quo2/components/drawers/drawer_top/view.cljs
+++ src/quo2/components/drawers/drawer_top/view.cljs
@@ -10,29 +10,26 @@
             [quo2.components.avatars.icon-avatar :as icon-avatar]
             [quo2.components.avatars.user-avatar.view :as user-avatar]
             [quo2.foundations.colors :as colors]
-            [utils.i18n :as i18n]
-            [status-im.multiaccounts.core :as multiaccounts]
-            [utils.re-frame :as rf]))
+            [utils.i18n :as i18n]))
 
 (defn- left-image
-  [{:keys [type customization-color account-avatar-emoji icon-avatar]}]
-  (let [account (rf/sub [:profile/multiaccount])]
-    (case type
-      :account         [account-avatar/view
-                        {:customization-color customization-color
-                         :size                32
-                         :emoji               account-avatar-emoji
-                         :type                :default}]
-      :keypair         [icon-avatar/icon-avatar
-                        {:icon    icon-avatar
-                         :border? true
-                         :color   :neutral}]
+  [{:keys [type customization-color account-avatar-emoji icon-avatar profile-picture]}]
+  (case type
+    :account         [account-avatar/view
+                      {:customization-color customization-color
+                       :size                32
+                       :emoji               account-avatar-emoji
+                       :type                :default}]
+    :keypair         [icon-avatar/icon-avatar
+                      {:icon    icon-avatar
+                       :border? true
+                       :color   :neutral}]
 
-      :default-keypair [user-avatar/user-avatar
-                        {:size              :small
-                         :status-indicator? false
-                         :profile-picture   (multiaccounts/displayed-photo account)}]
-      nil)))
+    :default-keypair [user-avatar/user-avatar
+                      {:size                  :small
+                       :status-indicator?     false
+                       :profile-picture       profile-picture}]
+    nil))
 
 (defn- network-view
   [network]
@@ -170,15 +167,16 @@
 (defn- view-internal
   [{:keys [title type theme description blur? community-name community-logo button-icon on-button-press
            on-button-long-press
-           button-disabled? account-avatar-emoji customization-color icon-avatar keycard? networks
-           label]}]
+           button-disabled? account-avatar-emoji customization-color icon-avatar
+           profile-picture keycard? networks label]}]
   [rn/view {:style style/container}
    [rn/view {:style style/left-container}
     [left-image
      {:type                 type
       :customization-color  customization-color
       :account-avatar-emoji account-avatar-emoji
-      :icon-avatar          icon-avatar}]]
+      :icon-avatar          icon-avatar
+      :profile-picture      profile-picture}]]
    [rn/view {:style style/body-container}
     [left-title
      {:type  type
