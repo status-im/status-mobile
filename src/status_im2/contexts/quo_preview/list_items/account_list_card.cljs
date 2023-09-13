5c53cf72698b4acf6f73d4b202e5345ba7a495a2
Omar Basem <omarbasem.0101@gmail.com>
2023-09-13 13:33:57 +0400

    account list card blur

--- src/status_im2/contexts/quo_preview/list_items/account_list_card.cljs
+++ src/status_im2/contexts/quo_preview/list_items/account_list_card.cljs
@@ -4,27 +4,31 @@
             [status-im2.contexts.quo-preview.preview :as preview]))
 
 (def descriptor
-  [{:key     :state
-    :type    :select
-    :options [{:key :default}
-              {:key :pressed}]}
-   {:key     :action
+  [{:key     :action
     :type    :select
     :options [{:key :none}
-              {:key :icon}]}])
+              {:key :icon}]}
+   {:key  :blur?
+    :type :boolean}])
 
 (defn view
   []
-  (let [state (reagent/atom {:account-props {:customization-color :purple
-                                             :size                32
-                                             :emoji               "🍑"
-                                             :type                :default
-                                             :name                "Trip to Vegas"
-                                             :address             "0x0ah...78b"}
-                             :networks      [:ethereum :optimism]
-                             :state         :default
-                             :action        :none
-                             :on-press      (fn [] (js/alert "Button pressed"))})]
+  (let [state (reagent/atom {:account-props    {:customization-color :purple
+                                                :size                32
+                                                :emoji               "🍑"
+                                                :type                :default
+                                                :name                "Trip to Vegas"
+                                                :address             "0x0ah...78b"}
+                             :networks         [:ethereum :optimism]
+                             :action           :none
+                             :on-press         (fn [] (js/alert "Item pressed"))
+                             :on-options-press (fn [] (js/alert "Options pressed"))
+                             :blur?            false})]
     (fn []
-      [preview/preview-container {:state state :descriptor descriptor}
+      [preview/preview-container
+       {:state                 state
+        :descriptor            descriptor
+        :blur?                 (:blur? @state)
+        :show-blur-background? true
+        :blur-dark-only?       true}
        [quo/account-list-card @state]])))
