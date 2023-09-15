4d9ebdb9f93eaa98b0ffe3cfadf9f65f7e78dbc0
Omar Basem <omarbasem.0101@gmail.com>
2023-09-15 07:43:48 +0400

    Quo2: Account list card blur (#17271)
    * Quo2: account list card blur

--- src/quo2/components/list_items/account_list_card/style.cljs
+++ src/quo2/components/list_items/account_list_card/style.cljs
@@ -2,19 +2,23 @@
   (:require [quo2.foundations.colors :as colors]))
 
 (defn container
-  [state theme]
+  [{:keys [state blur? theme]}]
   (let [light-bg     (if (= state :default) colors/neutral-2_5 colors/neutral-5)
         dark-bg      (if (= state :default) colors/neutral-80-opa-40 colors/neutral-80-opa-60)
+        blur-bg      (if (= state :default) colors/white-opa-5 colors/white-opa-10)
         light-border (if (= state :default) colors/neutral-10 colors/neutral-20)
         dark-border  (if (= state :default) colors/neutral-80 colors/neutral-70)]
-    {:width              319
-     :height             48
+    {:height             48
      :border-radius      12
-     :background-color   (colors/theme-colors light-bg dark-bg theme)
-     :border-width       1
+     :background-color   (if blur? blur-bg (colors/theme-colors light-bg dark-bg theme))
+     :border-width       (if blur? 0 1)
      :border-color       (colors/theme-colors light-border dark-border theme)
      :flex-direction     :row
      :align-items        :center
      :padding-horizontal 8
      :padding-vertical   6
      :justify-content    :space-between}))
+
+(def left-container
+  {:flex-direction :row
+   :align-items    :center})
--- src/quo2/components/list_items/account_list_card/view.cljs
+++ src/quo2/components/list_items/account_list_card/view.cljs
@@ -6,37 +6,45 @@
     [quo2.foundations.colors :as colors]
     [quo2.theme :as quo.theme]
     [react-native.core :as rn]
-    [quo2.components.list-items.account-list-card.style :as style]))
+    [quo2.components.list-items.account-list-card.style :as style]
+    [reagent.core :as reagent]))
 
 (defn- internal-view
-  [{:keys [theme account-props networks state action on-press]}]
-  [rn/view
-   {:style               (style/container state theme)
-    :accessibility-label :container}
-   [rn/view
-    {:style {:flex-direction :row
-             :align-items    :center}}
-    [account-avatar/view account-props]
-    [rn/view {:style {:margin-left 8}}
-     [text/text
-      {:weight :semi-bold
-       :size   :paragraph-2}
-      (:name account-props)]
-     [text/text {:size :paragraph-2}
-      (map (fn [network]
-             ^{:key (str network)}
-             [text/text
-              {:size  :paragraph-2
-               :style {:color (get colors/networks network)}} (str (subs (name network) 0 3) ":")])
-           networks)
-      [text/text
-       {:size  :paragraph-2
-        :style {:color (colors/theme-colors colors/neutral-50 colors/neutral-40 theme)}}
-       (:address account-props)]]]]
-   (when (= action :icon)
-     [rn/pressable {:on-press on-press}
-      [icon/icon :i/options
-       {:color               (colors/theme-colors colors/neutral-50 colors/neutral-40 theme)
-        :accessibility-label :icon}]])])
+  []
+  (let [state (reagent/atom :default)]
+    (fn [{:keys [action blur? account-props networks on-press on-options-press theme]}]
+      [rn/pressable
+       {:style               (style/container {:state @state :blur? blur? :theme theme})
+        :on-press-in         #(reset! state :pressed)
+        :on-press-out        #(reset! state :default)
+        :on-press            on-press
+        :accessibility-label :container}
+       [rn/view {:style style/left-container}
+        [account-avatar/view account-props]
+        [rn/view {:style {:margin-left 8}}
+         [text/text
+          {:weight :semi-bold
+           :size   :paragraph-2}
+          (:name account-props)]
+         [text/text {:size :paragraph-2}
+          (map (fn [network]
+                 ^{:key (str network)}
+                 [text/text
+                  {:size  :paragraph-2
+                   :style {:color (get colors/networks network)}} (str (subs (name network) 0 3) ":")])
+               networks)
+          [text/text
+           {:size  :paragraph-2
+            :style {:color (if blur?
+                             colors/white-opa-40
+                             (colors/theme-colors colors/neutral-50 colors/neutral-40 theme))}}
+           (:address account-props)]]]]
+       (when (= action :icon)
+         [rn/pressable {:on-press on-options-press}
+          [icon/icon :i/options
+           {:color               (if blur?
+                                   colors/white-opa-70
+                                   (colors/theme-colors colors/neutral-50 colors/neutral-40 theme))
+            :accessibility-label :icon}]])])))
 
 (def view (quo.theme/with-theme internal-view))
