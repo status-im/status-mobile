4f830f63ad253aaa58871f88ace3105b0bf36781
Hakan Akın <cehakanakin@gmail.com>
2023-09-16 04:55:36 +0300

    Did run linting.

--- src/status_im2/common/standard_auth/keycard/style.cljs
+++ src/status_im2/common/standard_auth/keycard/style.cljs
@@ -1,28 +1,28 @@
 (ns status-im2.common.standard-auth.keycard.style
   (:require
-   [quo2.foundations.colors :as colors]))
+    [quo2.foundations.colors :as colors]))
 
 (def container
   {:flex 1})
 
 (def inner-container
-  {:flex 1
+  {:flex               1
    :padding-horizontal 20})
 
 (def context-tag
-  {:margin-top 12
-   :margin-bottom 56
+  {:margin-top     12
+   :margin-bottom  56
    :flex-direction :row})
 
 (def close-button
   {:margin-bottom 12})
 
 (def secondary-text
-  {:margin-top 8
+  {:margin-top    8
    :margin-bottom 20})
 
 (def try-again-button
-  {:flex 1
+  {:flex               1
    :padding-horizontal 20})
 
 (def keycard
@@ -36,7 +36,8 @@
    :flex-start
    :margin-top 12})
 
-(defn get-digits-container-style [max-attempt-reached]
+(defn get-digits-container-style
+  [max-attempt-reached]
   {:flex               1
    :padding-horizontal 60
    :align-items        :center
@@ -44,17 +45,18 @@
    :justify-content    :space-between
    :margin-bottom      (if max-attempt-reached 0 34)})
 
-(defn digit-style [max-attempt-reached idx entered-numbers]
+(defn digit-style
+  [max-attempt-reached idx entered-numbers]
   {:width            16
    :height           16
    :border-radius    8
-   :background-color   (if (<= idx (dec (count @entered-numbers)))
-                         (if max-attempt-reached
-                           colors/danger
-                           colors/white)
-                         colors/neutral-50)})
+   :background-color (if (<= idx (dec (count @entered-numbers)))
+                       (if max-attempt-reached
+                         colors/danger
+                         colors/white)
+                       colors/neutral-50)})
 
 (def max-attempt-reached-container
-  {:align-self :center
+  {:align-self    :center
    :margin-bottom 14
-   :margin-top 8})
+   :margin-top    8})
