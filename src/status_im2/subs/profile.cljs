87f9bbda30c835b09b3053df55829f954e9f1f6e
frank <lovefree103@gmail.com>
2023-09-20 17:35:07 +0800

    fix: identifier rings are displayed on avatars for users with a set ENS

--- src/status_im2/subs/profile.cljs
+++ src/status_im2/subs/profile.cljs
@@ -247,8 +247,9 @@
    (pos? (count (get multiaccount :images)))))
 
 (defn- replace-multiaccount-image-uri
-  [multiaccount port font-file avatar-opts]
+  [multiaccount ens-names port font-file avatar-opts]
   (let [{:keys [key-uid ens-name? images]} multiaccount
+        ens-name?                          (or ens-name? (seq ens-names))
         theme                              (theme/get-theme)
         avatar-opts                        (assoc avatar-opts :override-ring? (when ens-name? false))
         images-with-uri                    (mapv (fn [{key-uid :keyUid image-name :type :as image}]
@@ -273,10 +274,11 @@
 (re-frame/reg-sub
  :profile/multiaccount
  :<- [:profile/profile]
+ :<- [:ens/names]
  :<- [:mediaserver/port]
  :<- [:initials-avatar-font-file]
- (fn [[multiaccount port font-file] [_ avatar-opts]]
-   (replace-multiaccount-image-uri multiaccount port font-file avatar-opts)))
+ (fn [[multiaccount ens-names port font-file] [_ avatar-opts]]
+   (replace-multiaccount-image-uri multiaccount ens-names port font-file avatar-opts)))
 
 (re-frame/reg-sub
  :profile/login-profile
