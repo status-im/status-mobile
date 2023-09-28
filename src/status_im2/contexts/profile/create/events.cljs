ef80fc567f28ab1a5769c255796ba4d4fd327abc
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2023-09-28 08:49:16 +0100

    Update waku nodes
    https://github.com/status-im/status-go/compare/e1354016...b8041fc8

--- src/status_im2/contexts/profile/create/events.cljs
+++ src/status_im2/contexts/profile/create/events.cljs
@@ -1,17 +1,9 @@
 (ns status-im2.contexts.profile.create.events
   (:require [utils.re-frame :as rf]
             [native-module.core :as native-module]
-            [status-im2.contexts.profile.config :as profile.config]
             [utils.security.core :as security]
             [re-frame.core :as re-frame]
-            [status-im2.contexts.shell.jump-to.utils :as shell.utils]
-            [status-im.transport.core :as transport]
-            [status-im.communities.core :as communities]
-            [status-im.data-store.chats :as data-store.chats]
-            [status-im.multiaccounts.core :as multiaccounts]
-            [status-im2.contexts.chat.messages.link-preview.events :as link-preview]
-            [status-im2.common.log :as logging]
-            [status-im2.navigation.events :as navigation]))
+            [status-im.transport.core :as transport]))
 
 (re-frame/reg-fx
  ::create-profile-and-login
@@ -28,22 +20,3 @@
            :password           (native-module/sha3 (security/safe-unmask-data password))
            :imagePath          (profile.config/strip-file-prefix image-path)
            :customizationColor color})})
-
-(rf/defn login-new-profile
-  [{:keys [db] :as cofx} recovered-account?]
-  (let [{:profile/keys  [profile profiles-overview wallet-accounts]
-         :networks/keys [current-network networks]} db
-        network-id
-        (str (get-in networks [current-network :config :NetworkId]))]
-    (shell.utils/change-selected-stack-id :communities-stack true nil)
-    (rf/merge cofx
-              {:db                (assoc db :chats/loading? false)
-               :wallet/get-tokens [network-id wallet-accounts recovered-account?]}
-              (transport/start-messenger)
-              (communities/fetch)
-              (data-store.chats/fetch-chats-preview
-               {:on-success #(re-frame/dispatch [:chats-list/load-success %])})
-              (multiaccounts/switch-preview-privacy-mode-flag)
-              (link-preview/request-link-preview-whitelist)
-              (logging/set-log-level (:log-level profile))
-              (navigation/init-root :enable-notifications))))
