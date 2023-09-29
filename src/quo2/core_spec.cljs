BunsDev/status-mobile-iOS

chore: resolve comments

src/quo2/core_spec.cljs
@@ -1,73 +1,74 @@
 (ns quo2.core-spec
   (:require
-    [quo2.components.avatars.account-avatar.component-spec]
-    [quo2.components.avatars.user-avatar.component-spec]
-    [quo2.components.banners.banner.component-spec]
-    [quo2.components.browser.browser-input.component-spec]
-    [quo2.components.buttons.button.component-spec]
-    [quo2.components.buttons.composer-button.component-spec]
-    [quo2.components.buttons.predictive-keyboard.component-spec]
-    [quo2.components.buttons.slide-button.component-spec]
-    [quo2.components.buttons.wallet-button.component-spec]
-    [quo2.components.buttons.wallet-ctas.component-spec]
-    [quo2.components.calendar.calendar.component-spec]
-    [quo2.components.calendar.calendar-day.component-spec]
-    [quo2.components.calendar.calendar.month-picker.component-spec]
-    [quo2.components.calendar.calendar-year.component-spec]
-    [quo2.components.colors.color-picker.component-spec]
-    [quo2.components.counter.counter.component-spec]
-    [quo2.components.counter.step.component-spec]
-    [quo2.components.dividers.divider-label.component-spec]
-    [quo2.components.dividers.strength-divider.component-spec]
-    [quo2.components.drawers.action-drawers.component-spec]
-    [quo2.components.drawers.documentation-drawers.component-spec]
-    [quo2.components.drawers.drawer-buttons.component-spec]
-    [quo2.components.drawers.permission-context.component-spec]
-    [quo2.components.dropdowns.network-dropdown.component-spec]
-    [quo2.components.gradient.gradient-cover.component-spec]
-    [quo2.components.graph.wallet-graph.component-spec]
-    [quo2.components.inputs.input.component-spec]
-    [quo2.components.inputs.profile-input.component-spec]
-    [quo2.components.inputs.locked-input.component-spec]
-    [quo2.components.inputs.recovery-phrase.component-spec]
-    [quo2.components.inputs.title-input.component-spec]
-    [quo2.components.keycard.component-spec]
-    [quo2.components.links.link-preview.component-spec]
-    [quo2.components.links.url-preview-list.component-spec]
-    [quo2.components.links.url-preview.component-spec]
-    [quo2.components.list-items.community.component-spec]
-    [quo2.components.list-items.dapp.component-spec]
-    [quo2.components.list-items.token-value.component-spec]
-    [quo2.components.loaders.skeleton-list.component-spec]
-    [quo2.components.markdown.text-component-spec]
-    [quo2.components.markdown.list.component-spec]
-    [quo2.components.navigation.top-nav.component-spec]
-    [quo2.components.notifications.notification.component-spec]
-    [quo2.components.numbered-keyboard.keyboard-key.component-spec]
-    [quo2.components.onboarding.small-option-card.component-spec]
-    [quo2.components.password.tips.component-spec]
-    [quo2.components.profile.select-profile.component-spec]
-    [quo2.components.profile.showcase-nav.component-spec]
-    [quo2.components.record-audio.record-audio.component-spec]
-    [quo2.components.record-audio.soundtrack.component-spec]
-    [quo2.components.selectors.disclaimer.component-spec]
-    [quo2.components.selectors.filter.component-spec]
-    [quo2.components.selectors.reactions.component-spec]
-    [quo2.components.selectors.selectors.component-spec]
-    [quo2.components.settings.reorder-item.component-spec]
-    [quo2.components.settings.settings-list.component-spec]
-    [quo2.components.settings.category.component-spec]
-    [quo2.components.settings.data-item.component-spec]
-    [quo2.components.share.share-qr-code.component-spec]
-    [quo2.components.tags.network-tags.component-spec]
-    [quo2.components.tags.status-tags-component-spec]
-    [quo2.components.wallet.account-card.component-spec]
-    [quo2.components.wallet.account-overview.component-spec]
-    [quo2.components.wallet.keypair.component-spec]
-    [quo2.components.wallet.network-amount.component-spec]
-    [quo2.components.wallet.network-bridge.component-spec]
-    [quo2.components.wallet.progress-bar.component-spec]
-    [quo2.components.wallet.summary-info.component-spec]
-    [quo2.components.wallet.token-input.component-spec]
-    [quo2.components.wallet.wallet-overview.component-spec]
-    [quo2.components.wallet.wallet-activity.component-spec]))
+   [quo2.components.avatars.account-avatar.component-spec]
+   [quo2.components.avatars.user-avatar.component-spec]
+   [quo2.components.banners.banner.component-spec]
+   [quo2.components.browser.browser-input.component-spec]
+   [quo2.components.buttons.button.component-spec]
+   [quo2.components.buttons.composer-button.component-spec]
+   [quo2.components.buttons.predictive-keyboard.component-spec]
+   [quo2.components.buttons.slide-button.component-spec]
+   [quo2.components.buttons.wallet-button.component-spec]
+   [quo2.components.buttons.wallet-ctas.component-spec]
+   [quo2.components.calendar.calendar.component-spec]
+   [quo2.components.calendar.calendar-day.component-spec]
+   [quo2.components.calendar.calendar.month-picker.component-spec]
+   [quo2.components.calendar.calendar-year.component-spec]
+   [quo2.components.colors.color-picker.component-spec]
+   [quo2.components.counter.counter.component-spec]
+   [quo2.components.counter.step.component-spec]
+   [quo2.components.dividers.divider-label.component-spec]
+   [quo2.components.dividers.strength-divider.component-spec]
+   [quo2.components.drawers.action-drawers.component-spec]
+   [quo2.components.drawers.documentation-drawers.component-spec]
+   [quo2.components.drawers.drawer-buttons.component-spec]
+   [quo2.components.drawers.permission-context.component-spec]
+   [quo2.components.dropdowns.network-dropdown.component-spec]
+   [quo2.components.gradient.gradient-cover.component-spec]
+   [quo2.components.graph.wallet-graph.component-spec]
+   [quo2.components.inputs.input.component-spec]
+   [quo2.components.inputs.profile-input.component-spec]
+   [quo2.components.inputs.locked-input.component-spec]
+   [quo2.components.inputs.recovery-phrase.component-spec]
+   [quo2.components.inputs.title-input.component-spec]
+   [quo2.components.keycard.component-spec]
+   [quo2.components.links.link-preview.component-spec]
+   [quo2.components.links.url-preview-list.component-spec]
+   [quo2.components.links.url-preview.component-spec]
+   [quo2.components.list-items.community.component-spec]
+   [quo2.components.list-items.dapp.component-spec]
+   [quo2.components.list-items.token-value.component-spec]
+   [quo2.components.loaders.skeleton-list.component-spec]
+   [quo2.components.markdown.text-component-spec]
+   [quo2.components.markdown.list.component-spec]
+   [quo2.components.navigation.top-nav.component-spec]
+   [quo2.components.notifications.notification.component-spec]
+   [quo2.components.numbered-keyboard.keyboard-key.component-spec]
+   [quo2.components.onboarding.small-option-card.component-spec]
+   [quo2.components.password.tips.component-spec]
+   [quo2.components.profile.select-profile.component-spec]
+   [quo2.components.profile.showcase-nav.component-spec]
+   [quo2.components.record-audio.record-audio.component-spec]
+   [quo2.components.record-audio.soundtrack.component-spec]
+   [quo2.components.selectors.disclaimer.component-spec]
+   [quo2.components.selectors.filter.component-spec]
+   [quo2.components.selectors.reactions.component-spec]
+   [quo2.components.selectors.selectors.component-spec]
+   [quo2.components.settings.reorder-item.component-spec]
+   [quo2.components.settings.settings-list.component-spec]
+   [quo2.components.settings.category.component-spec]
+   [quo2.components.settings.data-item.component-spec]
+   [quo2.components.share.share-qr-code.component-spec]
+   [quo2.components.switchers.base-card.component-spec]
+   [quo2.components.tags.network-tags.component-spec]
+   [quo2.components.tags.status-tags-component-spec]
+   [quo2.components.wallet.account-card.component-spec]
+   [quo2.components.wallet.account-overview.component-spec]
+   [quo2.components.wallet.keypair.component-spec]
+   [quo2.components.wallet.network-amount.component-spec]
+   [quo2.components.wallet.network-bridge.component-spec]
+   [quo2.components.wallet.progress-bar.component-spec]
+   [quo2.components.wallet.summary-info.component-spec]
+   [quo2.components.wallet.token-input.component-spec]
+   [quo2.components.wallet.wallet-overview.component-spec]
+   [quo2.components.wallet.wallet-activity.component-spec]))

status-im/status-mobile

src/quo2/core_spec.cljs
@@ -1,81 +1,81 @@
 (ns quo2.core-spec
   (:require
-    [quo2.components.avatars.account-avatar.component-spec]
-    [quo2.components.avatars.user-avatar.component-spec]
-    [quo2.components.banners.banner.component-spec]
-    [quo2.components.browser.browser-input.component-spec]
-    [quo2.components.buttons.button.component-spec]
-    [quo2.components.buttons.composer-button.component-spec]
-    [quo2.components.buttons.predictive-keyboard.component-spec]
-    [quo2.components.buttons.slide-button.component-spec]
-    [quo2.components.buttons.wallet-button.component-spec]
-    [quo2.components.buttons.wallet-ctas.component-spec]
-    [quo2.components.calendar.calendar.component-spec]
-    [quo2.components.calendar.calendar-day.component-spec]
-    [quo2.components.calendar.calendar.month-picker.component-spec]
-    [quo2.components.calendar.calendar-year.component-spec]
-    [quo2.components.colors.color-picker.component-spec]
-    [quo2.components.counter.counter.component-spec]
-    [quo2.components.counter.step.component-spec]
-    [quo2.components.dividers.divider-label.component-spec]
-    [quo2.components.dividers.strength-divider.component-spec]
-    [quo2.components.drawers.action-drawers.component-spec]
-    [quo2.components.drawers.documentation-drawers.component-spec]
-    [quo2.components.drawers.drawer-buttons.component-spec]
-    [quo2.components.drawers.drawer-top.component-spec]
-    [quo2.components.drawers.permission-context.component-spec]
-    [quo2.components.dropdowns.dropdown.component-spec]
-    [quo2.components.dropdowns.network-dropdown.component-spec]
-    [quo2.components.gradient.gradient-cover.component-spec]
-    [quo2.components.graph.wallet-graph.component-spec]
-    [quo2.components.inputs.address-input.component-spec]
-    [quo2.components.inputs.locked-input.component-spec]
-    [quo2.components.inputs.input.component-spec]
-    [quo2.components.inputs.profile-input.component-spec]
-    [quo2.components.inputs.recovery-phrase.component-spec]
-    [quo2.components.inputs.title-input.component-spec]
-    [quo2.components.keycard.component-spec]
-    [quo2.components.links.link-preview.component-spec]
-    [quo2.components.links.url-preview-list.component-spec]
-    [quo2.components.links.url-preview.component-spec]
-    [quo2.components.list-items.account.component-spec]
-    [quo2.components.list-items.channel.component-spec]
-    [quo2.components.list-items.community.component-spec]
-    [quo2.components.list-items.dapp.component-spec]
-    [quo2.components.list-items.saved-address.component-spec]
-    [quo2.components.list-items.token-value.component-spec]
-    [quo2.components.loaders.skeleton-list.component-spec]
-    [quo2.components.markdown.text-component-spec]
-    [quo2.components.markdown.list.component-spec]
-    [quo2.components.navigation.top-nav.component-spec]
-    [quo2.components.notifications.notification.component-spec]
-    [quo2.components.numbered-keyboard.keyboard-key.component-spec]
-    [quo2.components.onboarding.small-option-card.component-spec]
-    [quo2.components.password.tips.component-spec]
-    [quo2.components.profile.select-profile.component-spec]
-    [quo2.components.profile.showcase-nav.component-spec]
-    [quo2.components.record-audio.record-audio.component-spec]
-    [quo2.components.record-audio.soundtrack.component-spec]
-    [quo2.components.selectors.disclaimer.component-spec]
-    [quo2.components.selectors.filter.component-spec]
-    [quo2.components.selectors.reactions.component-spec]
-    [quo2.components.selectors.selectors.component-spec]
-    [quo2.components.settings.reorder-item.component-spec]
-    [quo2.components.settings.settings-item.component-spec]
-    [quo2.components.settings.category.component-spec]
-    [quo2.components.settings.data-item.component-spec]
-    [quo2.components.share.share-qr-code.component-spec]
-    [quo2.components.tags.network-tags.component-spec]
-    [quo2.components.tags.status-tags-component-spec]
-    [quo2.components.wallet.account-card.component-spec]
-    [quo2.components.wallet.account-overview.component-spec]
-    [quo2.components.wallet.account-origin.component-spec]
-    [quo2.components.wallet.keypair.component-spec]
-    [quo2.components.wallet.network-amount.component-spec]
-    [quo2.components.wallet.network-bridge.component-spec]
-    [quo2.components.wallet.progress-bar.component-spec]
-    [quo2.components.wallet.summary-info.component-spec]
-    [quo2.components.wallet.token-input.component-spec]
-    [quo2.components.wallet.transaction-summary.component-spec]
-    [quo2.components.wallet.wallet-overview.component-spec]
-    [quo2.components.wallet.wallet-activity.component-spec]))
+   [quo2.components.avatars.account-avatar.component-spec]
+   [quo2.components.avatars.user-avatar.component-spec]
+   [quo2.components.banners.banner.component-spec]
+   [quo2.components.browser.browser-input.component-spec]
+   [quo2.components.buttons.button.component-spec]
+   [quo2.components.buttons.composer-button.component-spec]
+   [quo2.components.buttons.predictive-keyboard.component-spec]
+   [quo2.components.buttons.slide-button.component-spec]
+   [quo2.components.buttons.wallet-button.component-spec]
+   [quo2.components.buttons.wallet-ctas.component-spec]
+   [quo2.components.calendar.calendar.component-spec]
+   [quo2.components.calendar.calendar-day.component-spec]
+   [quo2.components.calendar.calendar.month-picker.component-spec]
+   [quo2.components.calendar.calendar-year.component-spec]
+   [quo2.components.colors.color-picker.component-spec]
+   [quo2.components.counter.counter.component-spec]
+   [quo2.components.counter.step.component-spec]
+   [quo2.components.dividers.divider-label.component-spec]
+   [quo2.components.dividers.strength-divider.component-spec]
+   [quo2.components.drawers.action-drawers.component-spec]
+   [quo2.components.drawers.documentation-drawers.component-spec]
+   [quo2.components.drawers.drawer-buttons.component-spec]
+   [quo2.components.drawers.drawer-top.component-spec]
+   [quo2.components.drawers.permission-context.component-spec]
+   [quo2.components.dropdowns.dropdown.component-spec]
+   [quo2.components.dropdowns.network-dropdown.component-spec]
+   [quo2.components.gradient.gradient-cover.component-spec]
+   [quo2.components.graph.wallet-graph.component-spec]
+   [quo2.components.inputs.address-input.component-spec]
+   [quo2.components.inputs.locked-input.component-spec]
+   [quo2.components.inputs.input.component-spec]
+   [quo2.components.inputs.profile-input.component-spec]
+   [quo2.components.inputs.recovery-phrase.component-spec]
+   [quo2.components.inputs.title-input.component-spec]
+   [quo2.components.keycard.component-spec]
+   [quo2.components.links.link-preview.component-spec]
+   [quo2.components.links.url-preview-list.component-spec]
+   [quo2.components.links.url-preview.component-spec]
+   [quo2.components.list-items.account.component-spec]
+   [quo2.components.list-items.channel.component-spec]
+   [quo2.components.list-items.community.component-spec]
+   [quo2.components.list-items.dapp.component-spec]
+   [quo2.components.list-items.saved-address.component-spec]
+   [quo2.components.list-items.token-value.component-spec]
+   [quo2.components.loaders.skeleton-list.component-spec]
+   [quo2.components.markdown.text-component-spec]
+   [quo2.components.markdown.list.component-spec]
+   [quo2.components.navigation.top-nav.component-spec]
+   [quo2.components.notifications.notification.component-spec]
+   [quo2.components.numbered-keyboard.keyboard-key.component-spec]
+   [quo2.components.onboarding.small-option-card.component-spec]
+   [quo2.components.password.tips.component-spec]
+   [quo2.components.profile.select-profile.component-spec]
+   [quo2.components.profile.showcase-nav.component-spec]
+   [quo2.components.record-audio.record-audio.component-spec]
+   [quo2.components.record-audio.soundtrack.component-spec]
+   [quo2.components.selectors.disclaimer.component-spec]
+   [quo2.components.selectors.filter.component-spec]
+   [quo2.components.selectors.reactions.component-spec]
+   [quo2.components.selectors.selectors.component-spec]
+   [quo2.components.settings.reorder-item.component-spec]
+   [quo2.components.settings.settings-item.component-spec]
+   [quo2.components.settings.category.component-spec]
+   [quo2.components.settings.data-item.component-spec]
+   [quo2.components.share.share-qr-code.component-spec]
+   [quo2.components.tags.network-tags.component-spec]
+   [quo2.components.tags.status-tags-component-spec]
+   [quo2.components.wallet.account-card.component-spec]
+   [quo2.components.wallet.account-overview.component-spec]
+   [quo2.components.wallet.account-origin.component-spec]
+   [quo2.components.wallet.keypair.component-spec]
+   [quo2.components.wallet.network-amount.component-spec]
+   [quo2.components.wallet.network-bridge.component-spec]
+   [quo2.components.wallet.progress-bar.component-spec]
+   [quo2.components.wallet.summary-info.component-spec]
+   [quo2.components.wallet.token-input.component-spec]
+   [quo2.components.wallet.transaction-summary.component-spec]
+   [quo2.components.wallet.wallet-overview.component-spec]
+   [quo2.components.wallet.wallet-activity.component-spec]))