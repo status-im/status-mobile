BunsDev/status-mobile-iOS

feat: switcher base card component

src/quo2/core.cljs
@@ -1,133 +1,133 @@
 (ns quo2.core
   (:refer-clojure :exclude [filter])
   (:require
-    quo2.components.avatars.account-avatar.view
-    quo2.components.avatars.channel-avatar.view
-    quo2.components.avatars.group-avatar.view
-    quo2.components.avatars.icon-avatar
-    quo2.components.avatars.user-avatar.view
-    quo2.components.avatars.wallet-user-avatar
-    quo2.components.banners.banner.view
-    quo2.components.buttons.button.view
-    quo2.components.buttons.composer-button.view
-    quo2.components.buttons.dynamic-button.view
-    quo2.components.buttons.predictive-keyboard.view
-    quo2.components.buttons.slide-button.view
-    quo2.components.buttons.wallet-button.view
-    quo2.components.buttons.wallet-ctas.view
-    quo2.components.browser.browser-input.view
-    quo2.components.calendar.calendar.view
-    quo2.components.calendar.calendar-day.view
-    quo2.components.calendar.calendar-year.view
-    quo2.components.code.snippet
-    quo2.components.colors.color-picker.view
-    quo2.components.common.notification-dot.view
-    quo2.components.common.separator.view
-    quo2.components.community.banner.view
-    quo2.components.community.channel-actions
-    quo2.components.community.community-card-view
-    quo2.components.community.community-list-view
-    quo2.components.community.community-view
-    quo2.components.community.icon
-    quo2.components.community.token-gating
-    quo2.components.counter.counter.view
-    quo2.components.counter.step.view
-    quo2.components.dividers.date
-    quo2.components.dividers.divider-label.view
-    quo2.components.dividers.new-messages
-    quo2.components.dividers.strength-divider.view
-    quo2.components.drawers.action-drawers.view
-    quo2.components.drawers.documentation-drawers.view
-    quo2.components.drawers.drawer-buttons.view
-    quo2.components.drawers.permission-context.view
-    quo2.components.dropdowns.dropdown
-    quo2.components.dropdowns.network-dropdown.view
-    quo2.components.empty-state.empty-state.view
-    quo2.components.gradient.gradient-cover.view
-    quo2.components.graph.wallet-graph.view
-    quo2.components.header
-    quo2.components.icon
-    quo2.components.info.info-message
-    quo2.components.info.information-box.view
-    quo2.components.inputs.input.view
-    quo2.components.inputs.locked-input.view
-    quo2.components.inputs.profile-input.view
-    quo2.components.inputs.recovery-phrase.view
-    quo2.components.inputs.search-input.view
-    quo2.components.inputs.title-input.view
-    quo2.components.keycard.view
-    quo2.components.links.link-preview.view
-    quo2.components.links.url-preview-list.view
-    quo2.components.links.url-preview.view
-    quo2.components.list-items.account-list-card.view
-    quo2.components.list-items.channel
-    quo2.components.list-items.community.view
-    quo2.components.list-items.dapp.view
-    quo2.components.list-items.menu-item
-    quo2.components.list-items.preview-list.view
-    quo2.components.list-items.token-value.view
-    quo2.components.list-items.user-list
-    quo2.components.loaders.skeleton
-    quo2.components.loaders.skeleton-list.view
-    quo2.components.markdown.list.view
-    quo2.components.markdown.text
-    quo2.components.messages.author.view
-    quo2.components.messages.gap
-    quo2.components.messages.system-message
-    quo2.components.navigation.floating-shell-button.view
-    quo2.components.navigation.page-nav.view
-    quo2.components.navigation.top-nav.view
-    quo2.components.notifications.activity-log.view
-    quo2.components.notifications.activity-logs-photos.view
-    quo2.components.notifications.count-down-circle
-    quo2.components.notifications.notification.view
-    quo2.components.notifications.toast.view
-    quo2.components.numbered-keyboard.keyboard-key.view
-    quo2.components.numbered-keyboard.numbered-keyboard.view
-    quo2.components.onboarding.small-option-card.view
-    quo2.components.password.tips.view
-    quo2.components.profile.profile-card.view
-    quo2.components.profile.select-profile.view
-    quo2.components.profile.showcase-nav.view
-    quo2.components.reactions.reaction
-    quo2.components.record-audio.record-audio.view
-    quo2.components.record-audio.soundtrack.view
-    quo2.components.selectors.disclaimer.view
-    quo2.components.selectors.filter.view
-    quo2.components.selectors.reactions.view
-    quo2.components.selectors.selectors.view
-    quo2.components.settings.accounts.view
-    quo2.components.settings.data-item.view
-    quo2.components.settings.privacy-option
-    quo2.components.settings.reorder-item.view
-    quo2.components.settings.settings-list.view
-    quo2.components.settings.category.view
-    quo2.components.share.qr-code.view
-    quo2.components.share.share-qr-code.view
-    quo2.components.tabs.account-selector
-    quo2.components.tabs.segmented-tab
-    quo2.components.tabs.tabs.view
-    quo2.components.tags.context-tag.view
-    quo2.components.tags.network-tags.view
-    quo2.components.tags.number-tag.view
-    quo2.components.tags.permission-tag
-    quo2.components.tags.status-tags
-    quo2.components.tags.tag
-    quo2.components.tags.tags
-    quo2.components.tags.token-tag
-    quo2.components.text-combinations.view
-    quo2.components.wallet.account-overview.view
-    quo2.components.wallet.account-card.view
-    quo2.components.wallet.keypair.view
-    quo2.components.wallet.network-amount.view
-    quo2.components.wallet.network-bridge.view
-    quo2.components.wallet.network-link.view
-    quo2.components.wallet.progress-bar.view
-    quo2.components.wallet.summary-info.view
-    quo2.components.wallet.token-input.view
-    quo2.components.wallet.wallet-activity.view
-    quo2.components.wallet.wallet-overview.view
-    [quo2.components.graph.interactive-graph.view :as interactive-graph]))
+   quo2.components.avatars.account-avatar.view
+   quo2.components.avatars.channel-avatar.view
+   quo2.components.avatars.group-avatar.view
+   quo2.components.avatars.icon-avatar
+   quo2.components.avatars.user-avatar.view
+   quo2.components.avatars.wallet-user-avatar
+   quo2.components.banners.banner.view
+   quo2.components.buttons.button.view
+   quo2.components.buttons.composer-button.view
+   quo2.components.buttons.dynamic-button.view
+   quo2.components.buttons.predictive-keyboard.view
+   quo2.components.buttons.slide-button.view
+   quo2.components.buttons.wallet-button.view
+   quo2.components.buttons.wallet-ctas.view
+   quo2.components.browser.browser-input.view
+   quo2.components.calendar.calendar.view
+   quo2.components.calendar.calendar-day.view
+   quo2.components.calendar.calendar-year.view
+   quo2.components.code.snippet
+   quo2.components.colors.color-picker.view
+   quo2.components.common.notification-dot.view
+   quo2.components.common.separator.view
+   quo2.components.community.banner.view
+   quo2.components.community.channel-actions
+   quo2.components.community.community-card-view
+   quo2.components.community.community-list-view
+   quo2.components.community.community-view
+   quo2.components.community.icon
+   quo2.components.community.token-gating
+   quo2.components.counter.counter.view
+   quo2.components.counter.step.view
+   quo2.components.dividers.date
+   quo2.components.dividers.divider-label.view
+   quo2.components.dividers.new-messages
+   quo2.components.dividers.strength-divider.view
+   quo2.components.drawers.action-drawers.view
+   quo2.components.drawers.documentation-drawers.view
+   quo2.components.drawers.drawer-buttons.view
+   quo2.components.drawers.permission-context.view
+   quo2.components.dropdowns.dropdown
+   quo2.components.dropdowns.network-dropdown.view
+   quo2.components.empty-state.empty-state.view
+   quo2.components.gradient.gradient-cover.view
+   quo2.components.graph.wallet-graph.view
+   quo2.components.header
+   quo2.components.icon
+   quo2.components.info.info-message
+   quo2.components.info.information-box.view
+   quo2.components.inputs.input.view
+   quo2.components.inputs.locked-input.view
+   quo2.components.inputs.profile-input.view
+   quo2.components.inputs.recovery-phrase.view
+   quo2.components.inputs.search-input.view
+   quo2.components.inputs.title-input.view
+   quo2.components.keycard.view
+   quo2.components.links.link-preview.view
+   quo2.components.links.url-preview-list.view
+   quo2.components.links.url-preview.view
+   quo2.components.list-items.account-list-card.view
+   quo2.components.list-items.channel
+   quo2.components.list-items.community.view
+   quo2.components.list-items.dapp.view
+   quo2.components.list-items.menu-item
+   quo2.components.list-items.preview-list.view
+   quo2.components.list-items.token-value.view
+   quo2.components.list-items.user-list
+   quo2.components.loaders.skeleton
+   quo2.components.loaders.skeleton-list.view
+   quo2.components.markdown.list.view
+   quo2.components.markdown.text
+   quo2.components.messages.author.view
+   quo2.components.messages.gap
+   quo2.components.messages.system-message
+   quo2.components.navigation.floating-shell-button.view
+   quo2.components.navigation.page-nav.view
+   quo2.components.navigation.top-nav.view
+   quo2.components.notifications.activity-log.view
+   quo2.components.notifications.activity-logs-photos.view
+   quo2.components.notifications.count-down-circle
+   quo2.components.notifications.notification.view
+   quo2.components.notifications.toast.view
+   quo2.components.numbered-keyboard.keyboard-key.view
+   quo2.components.numbered-keyboard.numbered-keyboard.view
+   quo2.components.onboarding.small-option-card.view
+   quo2.components.password.tips.view
+   quo2.components.profile.profile-card.view
+   quo2.components.profile.select-profile.view
+   quo2.components.profile.showcase-nav.view
+   quo2.components.reactions.reaction
+   quo2.components.record-audio.record-audio.view
+   quo2.components.record-audio.soundtrack.view
+   quo2.components.selectors.disclaimer.view
+   quo2.components.selectors.filter.view
+   quo2.components.selectors.reactions.view
+   quo2.components.selectors.selectors.view
+   quo2.components.settings.accounts.view
+   quo2.components.settings.data-item.view
+   quo2.components.settings.privacy-option
+   quo2.components.settings.reorder-item.view
+   quo2.components.settings.settings-list.view
+   quo2.components.settings.category.view
+   quo2.components.share.qr-code.view
+   quo2.components.share.share-qr-code.view
+   quo2.components.tabs.account-selector
+   quo2.components.tabs.segmented-tab
+   quo2.components.tabs.tabs.view
+   quo2.components.tags.context-tag.view
+   quo2.components.tags.network-tags.view
+   quo2.components.tags.number-tag.view
+   quo2.components.tags.permission-tag
+   quo2.components.tags.status-tags
+   quo2.components.tags.tag
+   quo2.components.tags.tags
+   quo2.components.tags.token-tag
+   quo2.components.text-combinations.view
+   quo2.components.wallet.account-overview.view
+   quo2.components.wallet.account-card.view
+   quo2.components.wallet.keypair.view
+   quo2.components.wallet.network-amount.view
+   quo2.components.wallet.network-bridge.view
+   quo2.components.wallet.network-link.view
+   quo2.components.wallet.progress-bar.view
+   quo2.components.wallet.summary-info.view
+   quo2.components.wallet.token-input.view
+   quo2.components.wallet.wallet-activity.view
+   quo2.components.wallet.wallet-overview.view
+   [quo2.components.graph.interactive-graph.view :as interactive-graph]))
 
 (def separator quo2.components.common.separator.view/separator)
 

status-im/status-mobile

src/quo2/core.cljs
@@ -1,143 +1,143 @@
 (ns quo2.core
   (:refer-clojure :exclude [filter])
   (:require
-    quo2.components.avatars.account-avatar.view
-    quo2.components.avatars.channel-avatar.view
-    quo2.components.avatars.group-avatar.view
-    quo2.components.avatars.icon-avatar
-    quo2.components.avatars.user-avatar.view
-    quo2.components.avatars.wallet-user-avatar
-    quo2.components.banners.banner.view
-    quo2.components.buttons.button.view
-    quo2.components.buttons.composer-button.view
-    quo2.components.buttons.dynamic-button.view
-    quo2.components.buttons.predictive-keyboard.view
-    quo2.components.buttons.slide-button.view
-    quo2.components.buttons.wallet-button.view
-    quo2.components.buttons.wallet-ctas.view
-    quo2.components.browser.browser-input.view
-    quo2.components.calendar.calendar.view
-    quo2.components.calendar.calendar-day.view
-    quo2.components.calendar.calendar-year.view
-    quo2.components.code.snippet.view
-    quo2.components.code.snippet-preview.view
-    quo2.components.colors.color.view
-    quo2.components.colors.color-picker.view
-    quo2.components.common.notification-dot.view
-    quo2.components.common.separator.view
-    quo2.components.community.banner.view
-    quo2.components.community.channel-actions
-    quo2.components.community.community-card-view
-    quo2.components.community.community-list-view
-    quo2.components.community.community-view
-    quo2.components.community.icon
-    quo2.components.community.token-gating
-    quo2.components.counter.counter.view
-    quo2.components.counter.step.view
-    quo2.components.dividers.date
-    quo2.components.dividers.divider-label.view
-    quo2.components.dividers.divider-line.view
-    quo2.components.dividers.new-messages
-    quo2.components.dividers.strength-divider.view
-    quo2.components.drawers.action-drawers.view
-    quo2.components.drawers.documentation-drawers.view
-    quo2.components.drawers.drawer-buttons.view
-    quo2.components.drawers.drawer-top.view
-    quo2.components.drawers.permission-context.view
-    quo2.components.dropdowns.dropdown.view
-    quo2.components.dropdowns.network-dropdown.view
-    quo2.components.empty-state.empty-state.view
-    quo2.components.gradient.gradient-cover.view
-    quo2.components.graph.wallet-graph.view
-    quo2.components.header
-    quo2.components.icon
-    quo2.components.info.info-message
-    quo2.components.info.information-box.view
-    quo2.components.inputs.input.view
-    quo2.components.inputs.address-input.view
-    quo2.components.inputs.locked-input.view
-    quo2.components.inputs.profile-input.view
-    quo2.components.inputs.recovery-phrase.view
-    quo2.components.inputs.search-input.view
-    quo2.components.inputs.title-input.view
-    quo2.components.keycard.view
-    quo2.components.links.link-preview.view
-    quo2.components.links.url-preview-list.view
-    quo2.components.links.url-preview.view
-    quo2.components.list-items.account.view
-    quo2.components.list-items.account-list-card.view
-    quo2.components.list-items.channel.view
-    quo2.components.list-items.community.view
-    quo2.components.list-items.dapp.view
-    quo2.components.list-items.menu-item
-    quo2.components.list-items.preview-list.view
-    quo2.components.list-items.saved-address.view
-    quo2.components.list-items.token-value.view
-    quo2.components.list-items.user-list
-    quo2.components.loaders.skeleton-list.view
-    quo2.components.markdown.list.view
-    quo2.components.markdown.text
-    quo2.components.messages.author.view
-    quo2.components.messages.gap
-    quo2.components.messages.system-message
-    quo2.components.navigation.floating-shell-button.view
-    quo2.components.navigation.bottom-nav-tab.view
-    quo2.components.navigation.page-nav.view
-    quo2.components.navigation.top-nav.view
-    quo2.components.notifications.activity-log.view
-    quo2.components.notifications.activity-logs-photos.view
-    quo2.components.notifications.count-down-circle
-    quo2.components.notifications.notification.view
-    quo2.components.notifications.toast.view
-    quo2.components.numbered-keyboard.keyboard-key.view
-    quo2.components.numbered-keyboard.numbered-keyboard.view
-    quo2.components.onboarding.small-option-card.view
-    quo2.components.password.tips.view
-    quo2.components.profile.profile-card.view
-    quo2.components.profile.select-profile.view
-    quo2.components.profile.showcase-nav.view
-    quo2.components.reactions.reaction
-    quo2.components.record-audio.record-audio.view
-    quo2.components.record-audio.soundtrack.view
-    quo2.components.selectors.disclaimer.view
-    quo2.components.selectors.filter.view
-    quo2.components.selectors.reactions.view
-    quo2.components.selectors.selectors.view
-    quo2.components.settings.accounts.view
-    quo2.components.settings.data-item.view
-    quo2.components.settings.privacy-option.view
-    quo2.components.settings.reorder-item.view
-    quo2.components.settings.settings-item.view
-    quo2.components.settings.category.view
-    quo2.components.settings.section-label.view
-    quo2.components.share.qr-code.view
-    quo2.components.share.share-qr-code.view
-    quo2.components.tabs.account-selector
-    quo2.components.tabs.segmented-tab
-    quo2.components.tabs.tabs.view
-    quo2.components.tags.context-tag.view
-    quo2.components.tags.network-tags.view
-    quo2.components.tags.number-tag.view
-    quo2.components.tags.permission-tag
-    quo2.components.tags.status-tags
-    quo2.components.tags.tag
-    quo2.components.tags.tags
-    quo2.components.tags.token-tag
-    quo2.components.text-combinations.view
-    quo2.components.wallet.account-card.view
-    quo2.components.wallet.account-origin.view
-    quo2.components.wallet.account-overview.view
-    quo2.components.wallet.keypair.view
-    quo2.components.wallet.network-amount.view
-    quo2.components.wallet.network-bridge.view
-    quo2.components.wallet.network-link.view
-    quo2.components.wallet.progress-bar.view
-    quo2.components.wallet.summary-info.view
-    quo2.components.wallet.token-input.view
-    quo2.components.wallet.transaction-summary.view
-    quo2.components.wallet.wallet-activity.view
-    quo2.components.wallet.wallet-overview.view
-    [quo2.components.graph.interactive-graph.view :as interactive-graph]))
+   quo2.components.avatars.account-avatar.view
+   quo2.components.avatars.channel-avatar.view
+   quo2.components.avatars.group-avatar.view
+   quo2.components.avatars.icon-avatar
+   quo2.components.avatars.user-avatar.view
+   quo2.components.avatars.wallet-user-avatar
+   quo2.components.banners.banner.view
+   quo2.components.buttons.button.view
+   quo2.components.buttons.composer-button.view
+   quo2.components.buttons.dynamic-button.view
+   quo2.components.buttons.predictive-keyboard.view
+   quo2.components.buttons.slide-button.view
+   quo2.components.buttons.wallet-button.view
+   quo2.components.buttons.wallet-ctas.view
+   quo2.components.browser.browser-input.view
+   quo2.components.calendar.calendar.view
+   quo2.components.calendar.calendar-day.view
+   quo2.components.calendar.calendar-year.view
+   quo2.components.code.snippet.view
+   quo2.components.code.snippet-preview.view
+   quo2.components.colors.color.view
+   quo2.components.colors.color-picker.view
+   quo2.components.common.notification-dot.view
+   quo2.components.common.separator.view
+   quo2.components.community.banner.view
+   quo2.components.community.channel-actions
+   quo2.components.community.community-card-view
+   quo2.components.community.community-list-view
+   quo2.components.community.community-view
+   quo2.components.community.icon
+   quo2.components.community.token-gating
+   quo2.components.counter.counter.view
+   quo2.components.counter.step.view
+   quo2.components.dividers.date
+   quo2.components.dividers.divider-label.view
+   quo2.components.dividers.divider-line.view
+   quo2.components.dividers.new-messages
+   quo2.components.dividers.strength-divider.view
+   quo2.components.drawers.action-drawers.view
+   quo2.components.drawers.documentation-drawers.view
+   quo2.components.drawers.drawer-buttons.view
+   quo2.components.drawers.drawer-top.view
+   quo2.components.drawers.permission-context.view
+   quo2.components.dropdowns.dropdown.view
+   quo2.components.dropdowns.network-dropdown.view
+   quo2.components.empty-state.empty-state.view
+   quo2.components.gradient.gradient-cover.view
+   quo2.components.graph.wallet-graph.view
+   quo2.components.header
+   quo2.components.icon
+   quo2.components.info.info-message
+   quo2.components.info.information-box.view
+   quo2.components.inputs.input.view
+   quo2.components.inputs.address-input.view
+   quo2.components.inputs.locked-input.view
+   quo2.components.inputs.profile-input.view
+   quo2.components.inputs.recovery-phrase.view
+   quo2.components.inputs.search-input.view
+   quo2.components.inputs.title-input.view
+   quo2.components.keycard.view
+   quo2.components.links.link-preview.view
+   quo2.components.links.url-preview-list.view
+   quo2.components.links.url-preview.view
+   quo2.components.list-items.account.view
+   quo2.components.list-items.account-list-card.view
+   quo2.components.list-items.channel.view
+   quo2.components.list-items.community.view
+   quo2.components.list-items.dapp.view
+   quo2.components.list-items.menu-item
+   quo2.components.list-items.preview-list.view
+   quo2.components.list-items.saved-address.view
+   quo2.components.list-items.token-value.view
+   quo2.components.list-items.user-list
+   quo2.components.loaders.skeleton-list.view
+   quo2.components.markdown.list.view
+   quo2.components.markdown.text
+   quo2.components.messages.author.view
+   quo2.components.messages.gap
+   quo2.components.messages.system-message
+   quo2.components.navigation.floating-shell-button.view
+   quo2.components.navigation.bottom-nav-tab.view
+   quo2.components.navigation.page-nav.view
+   quo2.components.navigation.top-nav.view
+   quo2.components.notifications.activity-log.view
+   quo2.components.notifications.activity-logs-photos.view
+   quo2.components.notifications.count-down-circle
+   quo2.components.notifications.notification.view
+   quo2.components.notifications.toast.view
+   quo2.components.numbered-keyboard.keyboard-key.view
+   quo2.components.numbered-keyboard.numbered-keyboard.view
+   quo2.components.onboarding.small-option-card.view
+   quo2.components.password.tips.view
+   quo2.components.profile.profile-card.view
+   quo2.components.profile.select-profile.view
+   quo2.components.profile.showcase-nav.view
+   quo2.components.reactions.reaction
+   quo2.components.record-audio.record-audio.view
+   quo2.components.record-audio.soundtrack.view
+   quo2.components.selectors.disclaimer.view
+   quo2.components.selectors.filter.view
+   quo2.components.selectors.reactions.view
+   quo2.components.selectors.selectors.view
+   quo2.components.settings.accounts.view
+   quo2.components.settings.data-item.view
+   quo2.components.settings.privacy-option.view
+   quo2.components.settings.reorder-item.view
+   quo2.components.settings.settings-item.view
+   quo2.components.settings.category.view
+   quo2.components.settings.section-label.view
+   quo2.components.share.qr-code.view
+   quo2.components.share.share-qr-code.view
+   quo2.components.tabs.account-selector
+   quo2.components.tabs.segmented-tab
+   quo2.components.tabs.tabs.view
+   quo2.components.tags.context-tag.view
+   quo2.components.tags.network-tags.view
+   quo2.components.tags.number-tag.view
+   quo2.components.tags.permission-tag
+   quo2.components.tags.status-tags
+   quo2.components.tags.tag
+   quo2.components.tags.tags
+   quo2.components.tags.token-tag
+   quo2.components.text-combinations.view
+   quo2.components.wallet.account-card.view
+   quo2.components.wallet.account-origin.view
+   quo2.components.wallet.account-overview.view
+   quo2.components.wallet.keypair.view
+   quo2.components.wallet.network-amount.view
+   quo2.components.wallet.network-bridge.view
+   quo2.components.wallet.network-link.view
+   quo2.components.wallet.progress-bar.view
+   quo2.components.wallet.summary-info.view
+   quo2.components.wallet.token-input.view
+   quo2.components.wallet.transaction-summary.view
+   quo2.components.wallet.wallet-activity.view
+   quo2.components.wallet.wallet-overview.view
+   [quo2.components.graph.interactive-graph.view :as interactive-graph]))
 
 (def separator quo2.components.common.separator.view/separator)
 