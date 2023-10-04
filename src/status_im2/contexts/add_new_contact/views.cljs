c8529da003b0337657d5f241859473b8b254eb14
Ulises M <ulises.ssb@hotmail.com>
2023-10-05 03:32:22 +0900

    Use quo/input in add contact sheet

--- src/status_im2/contexts/add_new_contact/views.cljs
+++ src/status_im2/contexts/add_new_contact/views.cljs
@@ -1,16 +1,16 @@
 (ns status-im2.contexts.add-new-contact.views
-  (:require
-    [clojure.string :as string]
-    [quo.core :as quo]
-    [react-native.clipboard :as clipboard]
-    [react-native.core :as rn]
-    [reagent.core :as reagent]
-    [status-im.qr-scanner.core :as qr-scanner]
-    [status-im2.contexts.add-new-contact.style :as style]
-    [utils.address :as address]
-    [utils.debounce :as debounce]
-    [utils.i18n :as i18n]
-    [utils.re-frame :as rf]))
+  (:require [clojure.string :as string]
+            [oops.core :as oops]
+            [quo.core :as quo]
+            [react-native.clipboard :as clipboard]
+            [react-native.core :as rn]
+            [reagent.core :as reagent]
+            [status-im.qr-scanner.core :as qr-scanner]
+            [status-im2.contexts.add-new-contact.style :as style]
+            [utils.address :as address]
+            [utils.debounce :as debounce]
+            [utils.i18n :as i18n]
+            [utils.re-frame :as rf]))
 
 (defn found-contact
   [public-key]
@@ -41,71 +41,68 @@
 (defn new-contact
   []
   (let [clipboard     (reagent/atom nil)
-        default-value (reagent/atom nil)]
+        default-value (reagent/atom nil)
+        input-ref     (atom nil)
+        clear-input   (fn []
+                        (reset! clipboard nil)
+                        (reset! default-value nil)
+                        (oops/ocall @input-ref "clear")
+                        (rf/dispatch [:contacts/clear-new-identity]))]
     (fn []
       (clipboard/get-string #(reset! clipboard %))
-      (let [{:keys [input scanned public-key ens state msg]}
-            (rf/sub [:contacts/new-identity])
+      (let [{:keys [input scanned public-key ens state
+                    msg]}       (rf/sub [:contacts/new-identity])
             customization-color (rf/sub [:profile/customization-color])
-            invalid? (= state :invalid)
-            show-paste-button? (and (not (string/blank? @clipboard))
-                                    (string/blank? @default-value)
-                                    (string/blank? input))]
-        [rn/keyboard-avoiding-view
-         {:style {:flex 1}}
-         [rn/touchable-without-feedback
-          {:on-press rn/dismiss-keyboard!}
-          [rn/view (style/container-outer)
-           [rn/view style/container-inner
+            invalid?            (= state :invalid)
+            show-paste-button?  (and (not (string/blank? @clipboard))
+                                     (string/blank? @default-value)
+                                     (string/blank? input))]
+        [rn/keyboard-avoiding-view {:style {:flex 1}}
+         [rn/touchable-without-feedback {:on-press rn/dismiss-keyboard!}
+          [rn/view {:style (style/container-outer)}
+           [rn/view {:style style/container-inner}
             [quo/button
-             {:type :grey
-              :icon-only? true
+             {:type                :grey
+              :icon-only?          true
               :accessibility-label :new-contact-close-button
-              :size 32
-              :on-press
-              (fn []
-                (reset! clipboard nil)
-                (reset! default-value nil)
-                (rf/dispatch [:contacts/clear-new-identity])
-                (rf/dispatch [:navigate-back]))} :i/close]
-            [quo/text (style/text-title)
-             (i18n/label :t/add-a-contact)]
-            [quo/text (style/text-subtitle)
-             (i18n/label :t/find-your-friends)]
-            [quo/text (style/text-description)
-             (i18n/label :t/ens-or-chat-key)]
-            [rn/view style/container-text-input
-             [rn/view (style/text-input-container invalid?)
-              [rn/text-input
-               (merge (style/text-input)
-                      {:default-value   (or scanned @default-value input)
-                       :placeholder     (i18n/label :t/type-some-chat-key)
-                       :on-change-text  (fn [v]
-                                          (reset! default-value v)
-                                          (debounce/debounce-and-dispatch
-                                           [:contacts/set-new-identity v nil]
-                                           600))
-                       :blur-on-submit  true
-                       :return-key-type :done})]
-              (when show-paste-button?
-                [quo/button
-                 {:type :outline
-                  :size 24
-                  :container-style {:margin-top 6}
-                  :on-press
-                  (fn []
-                    (reset! default-value @clipboard)
-                    (rf/dispatch
-                     [:contacts/set-new-identity @clipboard nil]))}
-                 (i18n/label :t/paste)])]
-             [quo/button
-              {:type       :outline
-               :icon-only? true
-               :size       40
-               :on-press   #(rf/dispatch
-                             [::qr-scanner/scan-code
-                              {:handler :contacts/qr-code-scanned}])}
-              :i/scan]]
+              :size                32
+              :on-press            (fn []
+                                     (clear-input)
+                                     (rf/dispatch [:navigate-back]))}
+             :i/close]
+            [quo/text (style/text-title) (i18n/label :t/add-a-contact)]
+            [quo/text (style/text-subtitle) (i18n/label :t/find-your-friends)]
+            [rn/view {:style style/input-and-scan-container}
+             [quo/input
+              {:ref             #(reset! input-ref %)
+               :multiline?      true
+               :blur-on-submit  true
+               :return-key-type :done
+               :label           (i18n/label :t/ens-or-chat-key)
+               :placeholder     (i18n/label :t/type-some-chat-key)
+               :clearable?      (not show-paste-button?)
+               :on-clear        clear-input
+               :button          (when show-paste-button?
+                                  {:on-press (fn []
+                                               (reset! default-value @clipboard)
+                                               (rf/dispatch
+                                                [:contacts/set-new-identity @clipboard nil]))
+                                   :text     (i18n/label :t/paste)})
+               :default-value   (or scanned @default-value input)
+               :on-change-text  (fn [v]
+                                  (reset! default-value v)
+                                  (debounce/debounce-and-dispatch
+                                   [:contacts/set-new-identity v nil]
+                                   600))}]
+             [rn/view {:style style/scan-button-gap}]
+             [rn/view {:style style/scan-button-container}
+              [quo/button
+               {:type       :outline
+                :icon-only? true
+                :size       40
+                :on-press   #(rf/dispatch [::qr-scanner/scan-code
+                                           {:handler :contacts/qr-code-scanned}])}
+               :i/scan]]]
             (when invalid?
               [rn/view style/container-invalid
                [quo/icon :i/alert style/icon-invalid]
@@ -114,18 +111,17 @@
             (when (= state :valid)
               [found-contact public-key])]
            [quo/button
-            {:type :primary
+            {:type                :primary
              :customization-color customization-color
-             :size 40
-             :container-style style/button-view-profile
+             :size                40
+             :container-style     style/button-view-profile
              :accessibility-label :new-contact-button
-             :icon-left :i/profile
-             :disabled? (not= state :valid)
-             :on-press
-             (fn []
-               (reset! clipboard nil)
-               (reset! default-value nil)
-               (rf/dispatch [:contacts/clear-new-identity])
-               (rf/dispatch [:navigate-back])
-               (rf/dispatch [:chat.ui/show-profile public-key ens]))}
+             :icon-left           :i/profile
+             :disabled?           (not= state :valid)
+             :on-press            (fn []
+                                    (reset! clipboard nil)
+                                    (reset! default-value nil)
+                                    (rf/dispatch [:contacts/clear-new-identity])
+                                    (rf/dispatch [:navigate-back])
+                                    (rf/dispatch [:chat.ui/show-profile public-key ens]))}
             (i18n/label :t/view-profile)]]]]))))
