1e3aaffd2453decb81274c4a2e94cca2d47e3711
Cristian Lungu <lungucristian95@gmail.com>
2023-10-04 14:44:47 +0300

    chore: removed unnecessary FIXME

--- src/quo2/components/selectors/react/style.cljs
+++ src/quo2/components/selectors/react/style.cljs
@@ -5,15 +5,8 @@
    :justify-content :flex-start
    :flex            1
    :flex-wrap       :wrap
-   ;; FIXME: after upgrading to >rn71.0, use :row-gap to
-   ;; add the gap between rows and remove the artificial
-   ;; bottom margins. Needed due to the margin between
-   ;; reaction rows (if more than 1), which shouldn't
-   ;; affect the component's container.
-   ;; :row-gap 6
    :margin-bottom   -6})
 
 (def reaction-container
-  ;; FIXME: remove bottom margin after upgrading to >rn71.0
   {:margin-bottom 6
    :margin-right  6})
