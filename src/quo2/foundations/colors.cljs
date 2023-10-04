468c05ef7d16b4333964f2bb09f9c0e0f74113a1
Ky <nguyentruongky33@gmail.com>
2023-10-04 10:55:50 +0700

    add new icons + resources

--- src/quo2/foundations/colors.cljs
+++ src/quo2/foundations/colors.cljs
@@ -121,6 +121,11 @@
 (def white-opa-90 (alpha white 0.9))
 (def white-opa-95 (alpha white 0.95))
 
+(def magenta "#EC266C")
+;; with transparency
+(def magenta-opa-40 (alpha magenta 0.4))
+
+
 ;;;;Blur
 (def white-70-blur (alpha white 0.7))
 (def white-70-blur-opaque (alpha-opaque white 0.7))
