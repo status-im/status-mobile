fb4b66a55207a5738cb5fce78e5de3dd0f15d6ae
balogunofafrica <balogunakanbi.k@gmail.com>
2023-10-05 13:18:38 +0100

    fix: clear icon

--- src/status_im2/contexts/chat/composer/images/style.cljs
+++ src/status_im2/contexts/chat/composer/images/style.cljs
@@ -6,16 +6,24 @@
    :padding-bottom 8
    :padding-right  12})
 
-(def remove-photo-container
-  {:width            15
-   :height           15
+(defn remove-photo-container
+  [theme]
+  {:width            16
+   :height           16
    :border-radius    8
-   :background-color colors/neutral-50
-   :border-width     1
-   :border-color     colors/white
+   :background-color (colors/theme-colors colors/white colors/neutral-95 theme)
    :position         :absolute
    :top              5
    :right            9
+   :justify-content  :center
+   :align-items      :center
+  })
+
+(def remove-photo-inner-container
+  {:width            14
+   :height           14
+   :border-radius    7
+   :background-color colors/neutral-50
    :justify-content  :center
    :align-items      :center})
 
