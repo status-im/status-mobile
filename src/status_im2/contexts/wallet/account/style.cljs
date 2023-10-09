27c380020b1bdc37b7b0a1ee01f5f222eeb40800
Omar Basem <omarbasem.0101@gmail.com>
2023-10-09 09:17:30 +0400

    review

--- src/status_im2/contexts/wallet/account/style.cljs
+++ src/status_im2/contexts/wallet/account/style.cljs
@@ -4,11 +4,6 @@
   {:padding-left     20
    :padding-vertical 12})
 
-(def about-tab
-  {:flex               1
-   :padding-horizontal 20
-   :padding-vertical   8})
-
 (def empty-container-style
   {:justify-content :center
    :flex            1
--- src/status_im2/contexts/wallet/account/tabs/view.cljs
+++ src/status_im2/contexts/wallet/account/tabs/view.cljs
@@ -1,54 +1,11 @@
 (ns status-im2.contexts.wallet.account.tabs.view
   (:require
     [quo2.core :as quo]
-    [quo2.foundations.colors :as colors]
     [react-native.core :as rn]
     [status-im2.contexts.wallet.account.style :as style]
     [status-im2.contexts.wallet.common.temp :as temp]
     [utils.i18n :as i18n]
-    [utils.re-frame :as rf]))
-
-(defn description
-  []
-  [quo/text {:size :paragraph-2}
-   (map (fn [network]
-          ^{:key (str network)}
-          [quo/text
-           {:size   :paragraph-2
-            :weight :medium
-            :style  {:color (colors/custom-color network)}}
-           (str (subs (name network) 0 3) (when (= network :arbitrum) "1") ":")])
-        temp/network-names)
-   [quo/text
-    {:size   :paragraph-2
-     :weight :monospace}
-    temp/address]])
-
-(defn about-options
-  []
-  [quo/action-drawer
-   [[{:icon                :i/link
-      :accessibility-label :view-on-eth
-      :label               (i18n/label :t/view-on-eth)}
-     {:icon                :i/link
-      :accessibility-label :view-on-opt
-      :label               (i18n/label :t/view-on-opt)}
-     {:icon                :i/link
-      :accessibility-label :view-on-arb
-      :label               (i18n/label :t/view-on-arb)}
-     {:icon                :i/copy
-      :accessibility-label :copy-address
-      :label               (i18n/label :t/copy-address)}
-     {:icon                :i/qr-code
-      :accessibility-label :show-address-qr
-      :label               (i18n/label :t/show-address-qr)}
-     {:icon                :i/share
-      :accessibility-label :share-address
-      :label               (i18n/label :t/share-address)}]]])
-
-(defn new-account
-  []
-  [rn/view])
+    [status-im2.contexts.wallet.account.tabs.about.view :as about]))
 
 (defn view
   [{:keys [selected-tab]}]
@@ -77,10 +34,4 @@
                     :description     (i18n/label :t/no-collectibles-description)
                     :placeholder?    true
                     :container-style style/empty-container-style}]
-    [rn/view {:style style/about-tab}
-     [quo/data-item
-      (merge temp/data-item-state
-             {:custom-subtitle description
-              :container-style {:margin-bottom 12}
-              :on-press        #(rf/dispatch [:show-bottom-sheet {:content about-options}])})]
-     [quo/account-origin temp/account-origin-state]]))
+    [about/view]))
