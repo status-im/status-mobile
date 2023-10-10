c9c36760842aeb178caa46ed8c560c4c6ae25a84
Milad <mmilad.sanati@gmail.com>
2023-10-10 15:28:21 +0330

    change rf/defn to re-frame/reg-event-fx

--- src/status_im2/contexts/wallet/events.cljs
+++ src/status_im2/contexts/wallet/events.cljs
@@ -1,19 +1,18 @@
 (ns status-im2.contexts.wallet.events
   (:require [utils.re-frame :as rf]
-            [taoensso.timbre :as log]))
+            [taoensso.timbre :as log]
+            [re-frame.core :as re-frame]))
 
-(rf/defn get-wallet-token
-  {:events [:wallet-2/get-wallet-token]}
-  [_ accounts]
-  {:json-rpc/call [{:method     "wallet_getWalletToken"
-                    :params     [(map :address accounts)]
-                    :on-success #(rf/dispatch [:wallet-2/get-wallet-token-success %])
-                    :on-error   #(log/info "failed " %)}]})
+(re-frame/reg-event-fx :wallet-2/get-wallet-token
+ (fn [_ [accounts]]
+   {:json-rpc/call [{:method     "wallet_getWalletToken"
+                     :params     [(map :address accounts)]
+                     :on-success #(rf/dispatch [:wallet-2/get-wallet-token-success %])
+                     :on-error   #(log/info "failed " %)}]}))
 
-(rf/defn get-wallet-token-success
-  {:events [:wallet-2/get-wallet-token-success]}
-  [{:keys [db]} data]
-  {:db (assoc db
-              :wallet-2/tokens          data
-              :wallet-2/tokens-loading? false)})
+(re-frame/reg-event-fx :wallet-2/get-wallet-token-success
+  (fn [{:keys [db]} [data]]
+    {:db (assoc db
+                :wallet-2/tokens          data
+                :wallet-2/tokens-loading? false)}))
 
