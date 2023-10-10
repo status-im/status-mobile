3ed5545ba2956bed6df03124d87b662831ef6b4f
Icaro Motta <icaro.ldm@gmail.com>
2023-10-10 17:10:20 -0300

    Use more precise wording

--- .clj-kondo/status-im/hooks/core.clj
+++ .clj-kondo/status-im/hooks/core.clj
@@ -2,7 +2,7 @@
   (:require [clj-kondo.hooks-api :as api]))
 
 (defn i18n-label
-  "Verify call to `i18n/label` pass the translation keyword qualified with `:t`."
+  "Verify call to `i18n/label` pass the translation keyword qualified with `t`."
   [{:keys [node]}]
   (when (api/list-node? node)
     (let [[symbol-node translation-key-node & _] (:children node)]
@@ -11,7 +11,7 @@
                  (api/keyword-node? translation-key-node)
                  (not= "t" (-> translation-key-node api/sexpr namespace)))
         (let [{:keys [row col end-col]} (meta translation-key-node)]
-          (api/reg-finding! {:message "Translation keyword should be qualified with :t"
+          (api/reg-finding! {:message "Translation keyword should be qualified with \"t\""
                              :row     row
                              :col     col
                              :end-col end-col
