9db641f1d306b47ed9a5629acb6fa05cb24607f8
Icaro Motta <icaro.ldm@gmail.com>
2023-10-11 11:05:51 -0300

    Simplify and speed up implementation

--- .clj-kondo/status-im/hooks/core.clj
+++ .clj-kondo/status-im/hooks/core.clj
@@ -4,18 +4,12 @@
 (defn i18n-label
   "Verify call to `i18n/label` pass the translation keyword qualified with `t`."
   [{:keys [node]}]
-  (when (api/list-node? node)
-    (let [[symbol-node translation-key-node & _] (:children node)]
-      (when (and (api/token-node? symbol-node)
-                 (= "i18n/label" (str symbol-node))
-                 (api/keyword-node? translation-key-node)
-                 (not= "t" (-> translation-key-node api/sexpr namespace)))
-        (let [{:keys [row col end-col]} (meta translation-key-node)]
-          (api/reg-finding! {:message "Translation keyword should be qualified with \"t\""
-                             :row     row
-                             :col     col
-                             :end-col end-col
-                             :type    :status-im.linter/invalid-translation-keyword}))))))
+  (let [[_ translation-key-node & _] (:children node)]
+    (when (and (api/keyword-node? translation-key-node)
+               (not= "t" (-> translation-key-node api/sexpr namespace)))
+      (api/reg-finding! (assoc (meta translation-key-node)
+                               :message "Translation keyword should be qualified with \"t\""
+                               :type    :status-im.linter/invalid-translation-keyword)))))
 
 (comment
   ;; Valid
