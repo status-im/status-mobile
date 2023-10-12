3955faae8d91de2259829a964981a2de1a2eb6ff
Milad <mmilad.sanati@gmail.com>
2023-10-12 17:04:42 +0330

    move calculate-balance function to wallet sub

--- src/status_im2/contexts/wallet/common/utils.cljs
+++ src/status_im2/contexts/wallet/common/utils.cljs
@@ -5,27 +5,3 @@
 (defn get-first-name
   [full-name]
   (first (string/split full-name #" ")))
-
-(defn calculate-raw-balance
-  [raw-balance decimals]
-  (if (not (js/isNaN (js/parseInt raw-balance)))
-    (/ (js/parseInt raw-balance) (Math/pow 10 (js/parseInt decimals)))
-    0))
-
-(defn calculate-balance
-  [address tokens]
-  (let [token  (get tokens (keyword (string/lower-case address)))
-        result (reduce
-                (fn [acc item]
-                  (let [total-value-per-token (reduce (fn [ac balances]
-                                                        (+ (calculate-raw-balance (:rawBalance balances)
-                                                                                  (:decimals item))
-                                                           ac))
-                                                      0
-                                                      (vals (:balancesPerChain item)))
-                        total-values          (* total-value-per-token
-                                                 (get-in item [:marketValuesPerCurrency :USD :price]))]
-                    (+ acc total-values)))
-                0
-                token)]
-    (.toFixed result 2)))
--- src/status_im2/subs/wallet_2/wallet.cljs
+++ src/status_im2/subs/wallet_2/wallet.cljs
@@ -1,6 +1,31 @@
 (ns status-im2.subs.wallet-2.wallet
   (:require [re-frame.core :as re-frame]
-            [status-im2.contexts.wallet.common.utils :as utils]))
+            [clojure.string :as string]))
+
+(defn calculate-raw-balance
+  [raw-balance decimals]
+  (let [raw-number (js/parseInt raw-balance)]
+    (if (not (js/isNaN raw-number))
+      (/ raw-number (Math/pow 10 (js/parseInt decimals)))
+      0)))
+
+(defn calculate-balance
+  [address tokens]
+  (let [token  (get tokens (keyword (string/lower-case address)))
+        result (reduce
+                (fn [acc item]
+                  (let [total-value-per-token (reduce (fn [ac balances]
+                                                        (+ (calculate-raw-balance (:rawBalance balances)
+                                                                                  (:decimals item))
+                                                           ac))
+                                                      0
+                                                      (vals (:balancesPerChain item)))
+                        total-values          (* total-value-per-token
+                                                 (get-in item [:marketValuesPerCurrency :USD :price]))]
+                    (+ acc total-values)))
+                0
+                token)]
+    (.toFixed result 2)))
 
 (re-frame/reg-sub
  :wallet-2/balances
@@ -10,4 +35,4 @@
    (for [account accounts]
      (let [address (:address account)]
        {:address address
-        :balance (utils/calculate-balance address tokens)}))))
+        :balance (calculate-balance address tokens)}))))
