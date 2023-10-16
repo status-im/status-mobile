9cfbb48d263f9ef26800e636cfbb501f22b6399d
Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>
2023-10-16 21:53:11 +0530

    [Feature] Screen base for create/edit account
    Signed-off-by: Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>

---
+++ src/status_im2/contexts/wallet/common/screen_base/create_or_edit_account/style.cljs
@@ -0,0 +1,51 @@
+(ns status-im2.contexts.wallet.common.screen-base.create-or-edit-account.style)
+
+(defn root-container
+  [top]
+  {:flex       1
+   :margin-top top})
+
+(defn gradient-cover-container
+  [top]
+  {:position :absolute
+   :top      (- top)
+   :left     0
+   :right    0
+   :z-index  -1})
+
+(def account-avatar-container
+  {:padding-horizontal 20
+   :padding-top        12})
+
+(def reaction-button-container
+  {:position :absolute
+   :bottom   0
+   :left     80})
+
+(def title-input-container
+  {:padding-horizontal 20
+   :padding-top        12
+   :padding-bottom     16})
+
+(def divider-1
+  {:margin-bottom 12})
+
+(def section-container
+  {:padding-horizontal 20
+   :padding-bottom     4})
+
+(def color-picker-container
+  {:padding-vertical 12})
+
+(def color-picker
+  {:padding-horizontal 12})
+
+(def divider-2
+  {:margin-top    4
+   :margin-bottom 12})
+
+(defn bottom-action
+  [bottom]
+  {:padding-horizontal 20
+   :padding-vertical   12
+   :margin-bottom      bottom})
--- src/status_im2/contexts/wallet/edit_account/view.cljs
+++ src/status_im2/contexts/wallet/edit_account/view.cljs
@@ -1,81 +1,44 @@
 (ns status-im2.contexts.wallet.edit-account.view
   (:require [quo2.core :as quo]
             [quo2.theme :as quo.theme]
-            [react-native.core :as rn]
-            [react-native.safe-area :as safe-area]
             [reagent.core :as reagent]
+            [status-im2.contexts.wallet.common.screen-base.create-or-edit-account.view :as
+             create-or-edit-account]
             [status-im2.contexts.wallet.edit-account.style :as style]
-            [utils.i18n :as i18n]
-            [utils.re-frame :as rf]))
+            [utils.i18n :as i18n]))
 
 (defn- view-internal
   []
-  (let [{:keys [top]}   (safe-area/get-insets)
-        account-name    "Account 1"
+  (let [account-name    (reagent/atom "Account 1")
         account-color   (reagent/atom :purple)
+        account-emoji   (reagent/atom "🍑")
         account-address "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
-        account-emoji   (reagent/atom "🍑")]
+        on-change-name  #(reset! account-name %)
+        on-change-color #(reset! account-color %)
+        on-change-emoji #(reset! account-emoji %)]
     (fn []
-      [rn/keyboard-avoiding-view
-       {:style (style/root-container top)}
-       [quo/page-nav
-        {:type       :no-title
-         :background :blur
-         :right-side [{:icon-name :i/delete
-                       :on-press  #(js/alert "Delete account: to be implemented")}]
-         :icon-name  :i/close
-         :on-press   #(rf/dispatch [:navigate-back])}]
-       [quo/gradient-cover
-        {:customization-color @account-color
-         :container-style     (style/gradient-cover-container top)}]
-       [rn/scroll-view {:bounces false}
-        [rn/view {:style style/account-avatar-container}
-         [quo/account-avatar
-          {:customization-color @account-color
-           :size                80
-           :emoji               @account-emoji
-           :type                :default}]
-         [quo/button
-          {:size            32
-           :type            :grey
-           :background      :photo
-           :icon-only?      true
-           :on-press        #(rf/dispatch [:emoji-picker/open
-                                           {:on-select (fn [selected-emoji]
-                                                         (reset! account-emoji selected-emoji))}])
-           :container-style style/reaction-button-container}
-          :i/reaction]]
-        [quo/title-input
-         {:placeholder     (i18n/label :t/account-name-input-placeholder)
-          :max-length      24
-          :blur?           true
-          :default-value   account-name
-          :container-style style/title-input-container}]
-        [quo/divider-line {:container-style style/divider-1}]
-        [quo/section-label
-         {:section         (i18n/label :t/colour)
-          :container-style style/section-container}]
-        [rn/view {:style style/color-picker-container}
-         [quo/color-picker
-          {:default-selected @account-color
-           :on-change        #(reset! account-color %)
-           :container-style  style/color-picker}]]
-        [quo/divider-line {:container-style style/divider-2}]
-        [quo/section-label
-         {:section         (i18n/label :t/account-info)
-          :container-style style/section-container}]
-        [quo/data-item
-         {:status          :default
-          :size            :default
-          :description     :default
-          :label           :none
-          :blur?           false
-          :icon-right?     true
-          :right-icon      :i/advanced
-          :card?           true
-          :title           (i18n/label :t/address)
-          :subtitle        account-address
-          :on-press        #(js/alert "Network selector: to be implemented")
-          :container-style style/data-item}]]])))
+      [create-or-edit-account/view
+       {:page-nav-right-side [{:icon-name :i/delete
+                               :on-press  #(js/alert "Delete account: to be implemented")}]
+        :account-name        @account-name
+        :account-emoji       @account-emoji
+        :account-color       @account-color
+        :on-change-name      on-change-name
+        :on-change-color     on-change-color
+        :on-change-emoji     on-change-emoji
+        :section-label       :t/account-info}
+       [quo/data-item
+        {:status          :default
+         :size            :default
+         :description     :default
+         :label           :none
+         :blur?           false
+         :icon-right?     true
+         :right-icon      :i/advanced
+         :card?           true
+         :title           (i18n/label :t/address)
+         :subtitle        account-address
+         :on-press        #(js/alert "Network selector: to be implemented")
+         :container-style style/data-item}]])))
 
 (def view (quo.theme/with-theme view-internal))
