4e60d38bc240ab709c8f290f724ab0ff8e257ee4
andrey <motor4ik@gmail.com>
2023-10-16 14:22:45 +0200

    fix

--- src/status_im/ui/components/controls/styles.cljs
+++ src/status_im/ui/components/controls/styles.cljs
@@ -2,24 +2,24 @@
   (:require [status-im.ui.components.colors :as colors]))
 
 (defn switch-style
-  [state disabled]
+  [value disabled]
   {:width            52
    :height           28
    :border-radius    14
    :padding          4
-   :background-color (if state
-                       (:ui-01 @colors/theme)
+   :background-color (if value
                        (if disabled
                          (:interactive-04 @colors/theme)
-                         (:interactive-01 @colors/theme)))})
+                         (:interactive-01 @colors/theme))
+                       (:ui-01 @colors/theme))})
 
 (defn switch-bullet-style
-  [state hold]
+  [value hold]
   {:width            20
    :height           20
    :border-radius    10
    :opacity          (if hold 1 0.6)
-   :transform        [{:translateX (if state 0 24)}]
+   :transform        [{:translateX (if value 24 0)}]
    :background-color colors/white-persist
    :elevation        4
    :shadow-opacity   1
@@ -28,43 +28,30 @@
    :shadow-offset    {:width 0 :height 4}})
 
 (defn radio-style
-  [state disabled]
+  [value disabled]
   {:width            20
    :height           20
    :border-radius    10
    :padding          4
-   :background-color (if state
-                       (:ui-01 @colors/theme)
+   :background-color (if value
                        (if disabled
                          (:interactive-04 @colors/theme)
-                         (:interactive-01 @colors/theme)))})
+                         (:interactive-01 @colors/theme))
+                       (:ui-01 @colors/theme))})
 
 (defn radio-bullet-style
-  [state hold]
+  [value hold]
   {:width            12
    :height           12
    :border-radius    6
    :opacity          (if hold 1 0.6)
-   :transform        [{:scale (if state 0.0001 1)}]
+   :transform        [{:scale (if value 1 0.0001)}]
    :background-color colors/white-persist
    :elevation        4
    :shadow-opacity   1
    :shadow-radius    16
    :shadow-color     (:shadow-01 @colors/theme)
    :shadow-offset    {:width 0 :height 4}})
-
-(defn animated-checkbox-style
-  [state disabled]
-  {:width            18
-   :height           18
-   :border-radius    4
-   :justify-content  :center
-   :align-items      :center
-   :background-color (if state
-                       (:ui-01 @colors/theme)
-                       (if disabled
-                         (:interactive-04 @colors/theme)
-                         (:interactive-01 @colors/theme)))})
 
 (defn checkbox-style
   [value disabled]
@@ -78,11 +65,6 @@
                          (:interactive-04 @colors/theme)
                          (:interactive-01 @colors/theme))
                        (:ui-01 @colors/theme))})
-
-(defn animated-check-icon-style
-  [state hold]
-  {:opacity   (if hold 1 0.6)
-   :transform [{:scale (if state 0.0001 1)}]})
 
 (defn check-icon-style
   [value]
