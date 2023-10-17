782b27241353bf1cf82584d9d2882ec24e03209f
Milad <mmilad.sanati@gmail.com>
2023-10-17 15:22:57 +0330

    revert podfile.lock and fix lint issues

--- src/status_im2/subs/wallet_2/wallet_test.cljs
+++ src/status_im2/subs/wallet_2/wallet_test.cljs
@@ -1,46 +1,46 @@
-(ns status-im2.subs.wallet-2.wallet-test 
+(ns status-im2.subs.wallet-2.wallet-test
   (:require [cljs.test :refer [is testing use-fixtures]]
             [re-frame.db :as rf-db]
-            [test-helpers.unit :as h] 
+            [test-helpers.unit :as h]
             status-im2.subs.root
             [utils.re-frame :as rf]))
 
 (def tokens
-  {:0x1 [{:decimals 1,
-          :symbol "ETH",
-          :name "Ether",
-          :balancesPerChain {:1 {:rawBalance "20",
-                                  :hasError false},
-                             :2 {:rawBalance "10",
-                                     :hasError false}},
+  {:0x1 [{:decimals                1
+          :symbol                  "ETH"
+          :name                    "Ether"
+          :balancesPerChain        {:1 {:rawBalance "20"
+                                        :hasError   false}
+                                    :2 {:rawBalance "10"
+                                        :hasError   false}}
           :marketValuesPerCurrency {:USD {:price 1000}}} ;; total should be 3000
-         {:decimals 2,
-          :symbol "DAI",
-          :name "Dai Stablecoin",
-          :balancesPerChain {:1 {:rawBalance "100"
-                                 :hasError false},
-                             :2 {:rawBalance "150"
-                                  :hasError false}},
-          :marketValuesPerCurrency {:USD {:price 100}}}], ;; total should be 250
-   :0x2 [{:decimals 3,
-          :symbol "ETH",
-          :name "Ether",
-          :balancesPerChain {:1 {:rawBalance "2500",
-                                  :hasError false},
-                             :2 {:rawBalance "3000",
-                                     :hasError false}
-                             :3 {:rawBalance "<nil>",
-                                     :hasError false}},
+         {:decimals                2
+          :symbol                  "DAI"
+          :name                    "Dai Stablecoin"
+          :balancesPerChain        {:1 {:rawBalance "100"
+                                        :hasError   false}
+                                    :2 {:rawBalance "150"
+                                        :hasError   false}}
+          :marketValuesPerCurrency {:USD {:price 100}}}] ;; total should be 250
+   :0x2 [{:decimals                3
+          :symbol                  "ETH"
+          :name                    "Ether"
+          :balancesPerChain        {:1 {:rawBalance "2500"
+                                        :hasError   false}
+                                    :2 {:rawBalance "3000"
+                                        :hasError   false}
+                                    :3 {:rawBalance "<nil>"
+                                        :hasError   false}}
           :marketValuesPerCurrency {:USD {:price 200}}} ;; total should be 1100
-         {:decimals 10,
-          :symbol "DAI",
-          :name "Dai Stablecoin",
-          :balancesPerChain {:1 {:rawBalance "10000000000"
-                                 :hasError false},
-                             :2 {:rawBalance "0"
-                                  :hasError false},
-                             :3 {:rawBalance "<nil>",
-                                     :hasError false}},
+         {:decimals                10
+          :symbol                  "DAI"
+          :name                    "Dai Stablecoin"
+          :balancesPerChain        {:1 {:rawBalance "10000000000"
+                                        :hasError   false}
+                                    :2 {:rawBalance "0"
+                                        :hasError   false}
+                                    :3 {:rawBalance "<nil>"
+                                        :hasError   false}}
           :marketValuesPerCurrency {:USD {:price 1000}}}]}) ;; total should be 1000
 
 (def accounts
@@ -54,15 +54,16 @@
     :removed false}])
 
 (use-fixtures :each
-  {:before #(reset! rf-db/app-db {})})
+              {:before #(reset! rf-db/app-db {})})
 
 (h/deftest-sub :wallet-2/balances
   [sub-name]
   (testing "returns vector of maps containing :address and :balance"
-      (swap! rf-db/app-db assoc 
-             :profile/wallet-accounts accounts
-             :wallet-2/tokens tokens)
-      (is (= [{:address "0x1"
-               :balance 3250} 
-              {:address "0x2"
-               :balance 2100}] (rf/sub [sub-name])))))
+    (swap! rf-db/app-db assoc
+      :profile/wallet-accounts accounts
+      :wallet-2/tokens         tokens)
+    (is (= [{:address "0x1"
+             :balance 3250}
+            {:address "0x2"
+             :balance 2100}]
+           (rf/sub [sub-name])))))
