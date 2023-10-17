9e5a57dbfc2064d8652ec23579bf5022dfe2b5f4
Milad <mmilad.sanati@gmail.com>
2023-10-17 17:55:29 +0330

    replace re-frame/reg-event-fx with rf/defn

--- src/status_im2/contexts/wallet/events.cljs
+++ src/status_im2/contexts/wallet/events.cljs
@@ -1,22 +1,23 @@
 (ns status-im2.contexts.wallet.events
-  (:require [re-frame.core :as re-frame]
-            [taoensso.timbre :as log]
+  (:require [taoensso.timbre :as log]
             [utils.re-frame :as rf]))
 
-(re-frame/reg-event-fx :wallet-2/get-wallet-token
- (fn [_ [accounts]]
-   (let [params (map :address accounts)]
-     {:json-rpc/call [{:method     "wallet_getWalletToken"
-                       :params     [params]
-                       :on-success #(rf/dispatch [:wallet-2/get-wallet-token-success %])
-                       :on-error   (fn [error]
-                                     (log/info "failed to get wallet token"
-                                               {:event  :wallet-2/get-wallet-token
-                                                :error  error
-                                                :params params}))}]})))
+(rf/defn get-wallet-token
+  {:events [:wallet-2/get-wallet-token]}
+  [_ accounts]
+  (let [params (map :address accounts)]
+    {:json-rpc/call [{:method     "wallet_getWalletToken"
+                      :params     [params]
+                      :on-success #(rf/dispatch [:wallet-2/get-wallet-token-success %])
+                      :on-error   (fn [error]
+                                    (log/info "failed to get wallet token"
+                                              {:event  :wallet-2/get-wallet-token
+                                               :error  error
+                                               :params params}))}]}))
 
-(re-frame/reg-event-fx :wallet-2/get-wallet-token-success
- (fn [{:keys [db]} [data]]
-   {:db (assoc db
-               :wallet-2/tokens          data
-               :wallet-2/tokens-loading? false)}))
+(rf/defn get-wallet-token-success
+  {:events [:wallet-2/get-wallet-token-success]}
+  [{:keys [db]} data]
+  {:db (assoc db
+              :wallet-2/tokens          data
+              :wallet-2/tokens-loading? false)})
