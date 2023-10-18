3246d1c9c70150bb1fa32473b87b9ce5425c9421
Icaro Motta <icaro.ldm@gmail.com>
2023-10-17 22:05:06 -0300

    Use Pressable API

--- src/status_im2/contexts/chat/composer/gradients/view.cljs
+++ src/status_im2/contexts/chat/composer/gradients/view.cljs
@@ -14,11 +14,11 @@
   [:<>
    [reanimated/linear-gradient (style/top-gradient gradient-opacity @gradient-z-index)]
    (when show-bottom-gradient?
-     [rn/touchable-without-feedback
+     [rn/pressable
       {:on-press            #(when @input-ref (.focus ^js @input-ref))
        :accessibility-label :bottom-gradient}
       [linear-gradient/linear-gradient (style/bottom-gradient)]])])
+
 (defn view
   [props state animations show-bottom-gradient?]
   [:f> f-view props state animations show-bottom-gradient?])
-
