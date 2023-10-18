63aa9f9292351aa87b1ca4bef33b0c7266ae1654
Milad <mmilad.sanati@gmail.com>
2023-10-18 14:30:42 +0330

    break down calculate-balance

--- src/status_im2/subs/wallet_2/wallet.cljs
+++ src/status_im2/subs/wallet_2/wallet.cljs
@@ -9,19 +9,22 @@
     (/ n (Math/pow 10 (utils.number/parse-int decimals)))
     0))
 
+(defn- total-per-token
+  [item]
+  (reduce (fn [ac balances]
+            (+ (calculate-raw-balance (:rawBalance balances)
+                                      (:decimals item))
+               ac))
+          0
+          (vals (:balancesPerChain item))))
+
 (defn- calculate-balance
   [address tokens]
   (let [token  (get tokens (keyword (string/lower-case address)))
         result (reduce
                 (fn [acc item]
-                  (let [total-value-per-token (reduce (fn [ac balances]
-                                                        (+ (calculate-raw-balance (:rawBalance balances)
-                                                                                  (:decimals item))
-                                                           ac))
-                                                      0
-                                                      (vals (:balancesPerChain item)))
-                        total-values          (* total-value-per-token
-                                                 (get-in item [:marketValuesPerCurrency :USD :price]))]
+                  (let [total-values (* (total-per-token item)
+                                        (get-in item [:marketValuesPerCurrency :USD :price]))]
                     (+ acc total-values)))
                 0
                 token)]
@@ -32,7 +35,6 @@
  :<- [:profile/wallet-accounts]
  :<- [:wallet-2/tokens]
  (fn [[accounts tokens]]
-   (for [account accounts]
-     (let [address (:address account)]
+   (for [{:keys [address]} accounts]
        {:address address
-        :balance (calculate-balance address tokens)}))))
+        :balance (calculate-balance address tokens)})))
