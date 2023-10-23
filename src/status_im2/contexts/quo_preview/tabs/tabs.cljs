c42fc5153d9eaf8cd75aacde309d543298f71cb6
Mohsen <maxghafori@gmail.com>
2023-10-23 20:40:22 +0300

    [#17288] refactor: migrate previews to new api (#17695)

--- src/status_im2/contexts/quo_preview/tabs/tabs.cljs
+++ src/status_im2/contexts/quo_preview/tabs/tabs.cljs
@@ -1,24 +1,18 @@
 (ns status-im2.contexts.quo-preview.tabs.tabs
   (:require
     [quo.core :as quo]
-    [react-native.core :as rn]
     [reagent.core :as reagent]
     [status-im2.contexts.quo-preview.preview :as preview]))
 
 (def descriptor
-  [{:label   "Size:"
-    :key     :size
+  [{:key     :size
     :type    :select
     :options [{:key   32
                :value "32"}
               {:key   24
                :value "24"}]}
-   {:label "Show unread indicators?"
-    :key   :unread-indicators?
-    :type  :boolean}
-   {:label "Scrollable:"
-    :key   :scrollable?
-    :type  :boolean}])
+   {:key :unread-indicators? :type :boolean}
+   {:key :scrollable? :type :boolean}])
 
 (defn generate-tab-items
   [length unread-indicators?]
@@ -28,27 +22,25 @@
      :label             (str "Tab " (inc index))
      :notification-dot? (and unread-indicators? (zero? (rem index 2)))}))
 
-(defn preview-tabs
+(defn view
   []
   (let [state (reagent/atom {:size        32
                              :scrollable? false})]
     (fn []
       [preview/preview-container
-       {:state      state
-        :descriptor descriptor}
-       [rn/view {:padding-bottom 150}
-        [rn/view
-         {:padding-vertical   60
-          :padding-horizontal 20
-          :flex-direction     :row
-          :justify-content    :center}
-         [quo/tabs
-          (merge @state
-                 {:default-active 1
-                  :data           (generate-tab-items (if (:scrollable? @state) 15 4)
-                                                      (:unread-indicators? @state))
-                  :on-change      #(println "Active tab" %)}
-                 (when (:scrollable? @state)
-                   {:scroll-on-press?    true
-                    :fade-end-percentage 0.4
-                    :fade-end?           true}))]]]])))
+       {:state                     state
+        :descriptor                descriptor
+        :component-container-style {:padding-vertical   60
+                                    :padding-horizontal 20
+                                    :flex-direction     :row
+                                    :justify-content    :center}}
+       [quo/tabs
+        (merge @state
+               {:default-active 1
+                :data           (generate-tab-items (if (:scrollable? @state) 15 4)
+                                                    (:unread-indicators? @state))
+                :on-change      #(println "Active tab" %)}
+               (when (:scrollable? @state)
+                 {:scroll-on-press?    true
+                  :fade-end-percentage 0.4
+                  :fade-end?           true}))]])))
