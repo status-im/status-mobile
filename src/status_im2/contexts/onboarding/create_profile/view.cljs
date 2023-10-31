7dec4a66086cb1559ae98234db38e40ac2974485
Ibrkhalil <vampirekid017@gmail.com>
2023-10-31 17:39:44 +0200

    Clean

--- src/status_im2/contexts/onboarding/create_profile/view.cljs
+++ src/status_im2/contexts/onboarding/create_profile/view.cljs
@@ -34,12 +34,6 @@
 (defn length-not-valid [s] (< (count (string/trim (str s))) min-length))
 (def scroll-view-height (reagent/atom 0))
 (def content-container-height (reagent/atom 0))
-(defn iphone-11-Pro-20-pixel-from-width
-  [window-width]
-  ;; Divide iPhone 11 Pro VW by the desired value
-  (let [calculate-ratio (/ constants/IPHONE_11_PRO_VIEWPORT_WIDTH 20)]
-    ;; Divide window width by the ratio to get a dynamic value. Based on the VW width
-    (int (/ window-width calculate-ratio))))
 
 (defn validation-message
   [s]
