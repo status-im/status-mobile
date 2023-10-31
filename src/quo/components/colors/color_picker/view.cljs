2f0047e518c1506f6f05b6ff2e43ca7c4b4d2479
tumanov-alex <uwontrememberthis@gmail.com>
2023-10-31 14:21:07 +0100

    rmv

--- src/quo/components/colors/color_picker/view.cljs
+++ src/quo/components/colors/color_picker/view.cljs
@@ -20,19 +20,4 @@
   [{:keys [default-selected window-width]}]
   (let [selected (reagent/atom default-selected)]
     (fn [{:keys [blur? on-change feng-shui? container-style]}]
-      [rn/scroll-view
-       {:horizontal                        true
-        :shows-horizontal-scroll-indicator false
-        :content-container-style           container-style}
-       (doall (map-indexed (fn [idx color]
-                             [color/view
-                              {:selected?    (= color @selected)
-                               :on-press     #(on-change-handler selected % on-change)
-                               :blur?        blur?
-                               :key          color
-                               :color        color
-                               :idx          idx
-                               :window-width window-width}])
-                           ;; TODO: using :feng-shui? temporarily while b & w is being developed.
-                           ;; https://github.com/status-im/status-mobile/discussions/16676
-                           (if feng-shui? (conj color-list :feng-shui) color-list)))])))
+      )))
