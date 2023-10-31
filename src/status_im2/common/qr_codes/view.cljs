4a874ce48dbe2b21827bdd5105dc545f4ea136d3
Ulises Manuel <90291778+ulisesmac@users.noreply.github.com>
2023-10-31 12:35:28 -0600

    [#17023] Share qr code variants (#17736)
    * Align docstring
    
    * Create share-qr-code component
    
    * Remove empty style file
    
    * Implement common.share-qr-code including call to media server
    
    * Add share-qr-code preview screen
    
    * Use share-qr-code component in shell's share screen
    
    * Add tests and some fixes

--- src/status_im2/common/qr_codes/view.cljs
+++ src/status_im2/common/qr_codes/view.cljs
@@ -36,3 +36,64 @@
                               :qr-size     (or 400 (int size))
                               :error-level :highest})]
     [quo/qr-code (assoc props :qr-image-uri qr-media-server-uri)]))
+
+(defn- get-network-short-name-url
+  [network]
+  (case network
+    :ethereum "eth:"
+    :optimism "opt:"
+    :arbitrum "arb1:"
+    (str (name network) ":")))
+
+(defn- get-qr-data-for-wallet-multichain
+  [qr-data networks]
+  (as-> networks $
+    (map get-network-short-name-url $)
+    (apply str $)
+    (str $ qr-data)))
+
+(defn share-qr-code
+  "Receives the following properties:
+   - type:                :profile | :wallet-legacy | :wallet-multichain
+   - qr-data:             Text to encode in the QR code
+   - qr-data-label-shown: Text to show below the QR code
+   - on-text-press:       Callback for the `qr-data-label-shown` text.
+   - on-text-long-press:  Callback for the `qr-data-label-shown` text.
+   - on-share-press:      Callback for the share button.
+   - customization-color: Custom color for the QR code component.
+   - unblur-on-android?:  [Android only] disables blur for this component.
+
+   Depending on the `type`, different properties are accepted:
+   `:profile`
+     - full-name:       User full name.
+     - profile-picture: map ({:source image-source}) or any image source.
+   `:wallet-legacy`
+     - emoji:               Emoji in a string to show in the QR code.
+     - on-info-press:       Callback for the info icon.
+     - on-legacy-press:     Callback for the legacy tab.
+     - on-multichain-press: Callback for the multichain tab.
+   `:wallet-multichain`
+     - networks:            A vector of network names (`[:ethereum, :optimism, :my-net, ,,,]`)
+                            they will add network prefixes to the address in
+                            `qr-data-label-shown` and the QR code generated.
+     - on-settings-press:   Callback for the settings button.
+     - emoji:               Emoji in a string to show in the QR code.
+     - on-info-press:       Callback for the info icon.
+     - on-legacy-press:     Callback for the legacy tab.
+     - on-multichain-press: Callback for the multichain tab."
+  [{:keys         [qr-data qr-data-label-shown networks]
+    share-qr-type :type
+    :as           props}]
+  (let [label               (or qr-data-label-shown qr-data)
+        string-to-encode    (if (= share-qr-type :wallet-multichain)
+                              (get-qr-data-for-wallet-multichain qr-data networks)
+                              qr-data)
+        qr-media-server-uri (image-server/get-qr-image-uri-for-any-url
+                             {:url         string-to-encode
+                              :port        (rf/sub [:mediaserver/port])
+                              :qr-size     600
+                              :error-level :highest})]
+    [quo/share-qr-code
+     (assoc props
+            :qr-data      label
+            :qr-image-uri qr-media-server-uri)]))
