6467342b4bd7ea7cabd97e161604b36903296b52
Icaro Motta <icaro.ldm@gmail.com>
2023-11-01 13:02:42 -0300

    Fix: only add extra top space when editing or replying

--- src/status_im2/contexts/chat/composer/gradients/style.cljs
+++ src/status_im2/contexts/chat/composer/gradients/style.cljs
@@ -4,24 +4,27 @@
     [react-native.reanimated :as reanimated]
     [status-im2.contexts.chat.composer.constants :as constants]))
 
-(defn top-gradient-style
-  [opacity z-index]
+(defn- top-gradient-style
+  [opacity z-index showing-extra-space?]
   (reanimated/apply-animations-to-style
    {:opacity opacity}
    {:height   (when (pos-int? z-index) 80)
     :position :absolute
     :z-index  z-index
-    :top      (+ constants/bar-container-height constants/edit-container-height)
+    :top      (+ constants/bar-container-height
+                 (if showing-extra-space?
+                   constants/edit-container-height
+                   0))
     :left     0
     :right    0}))
 
 (defn top-gradient
-  [opacity z-index]
+  [opacity z-index showing-extra-space?]
   {:colors [(colors/theme-colors colors/white-opa-0 colors/neutral-95-opa-0)
             (colors/theme-colors colors/white colors/neutral-95)]
    :start  {:x 0 :y 1}
    :end    {:x 0 :y 0}
-   :style  (top-gradient-style opacity z-index)})
+   :style  (top-gradient-style opacity z-index showing-extra-space?)})
 
 (def bottom-gradient-style
   {:height   constants/line-height
