e4db9f4ef0855817bfd851ac510f5eb3cd0955ab
Dmitri Akatov <dmitri@akatov.com>
2023-11-03 20:30:00 +0000

    only print out missing translations for now, but add back code to print out (probably) unused and non-namespaced translation keys to be used to remove/fix them in the future

--- scripts/lint_translations.clj
+++ scripts/lint_translations.clj
@@ -33,8 +33,8 @@
   (-> file slurp json/parse-string keys))
 
 
-(def ^:private possibly-unused-warning
-  (format "Possibly Unused Translation Key in %s:" translation-file))
+(def ^:private probably-unused-warning
+  (format "Probably unused translation key in %s:" translation-file))
 
 (defn -main
   [& _args]
@@ -46,10 +46,39 @@
         file-translation-keys            (apply sorted-set (extract-translation-keys translation-file))
         missing-translations             (remove (comp file-translation-keys :name) used-translations)
         used-translation-keys            (set (map :name used-translations))
-        possibly-unused-translation-keys (set/difference file-translation-keys used-translation-keys)]
-    (run! #(println possibly-unused-warning %) possibly-unused-translation-keys)
+        possibly-unused-translation-keys (set/difference file-translation-keys used-translation-keys)
+        non-namespaced-translations      (filter
+                                          (fn [kw]
+                                            (and (not (:ns kw))
+                                                 (possibly-unused-translation-keys (:name kw))))
+                                          all-keywords)
+        unused-translation-keys          (set/difference possibly-unused-translation-keys
+                                                         (set (map :name non-namespaced-translations)))]
+
+    ;; uncomment to print a list of all non-namespaced translation keys
+    #_(run! #(println "Probably non-namespaced key" %)
+            (apply sorted-set (map :name non-namespaced-translations)))
+
+    ;; uncomment to print all potential usages of non-namespaced translation keys
+    ;; note that this will list a lot of false positives, e.g. for `:default` or `:bold`
+    #_(report-issues (map #(assoc % :reason "Probably Non-namespaced Translation Key")
+                          non-namespaced-translations))
+
+    ;; uncomment to print all occurrences of probably unused translation keys
+    #_(run! #(println probably-unused-warning %) unused-translation-keys)
+
+    ;; remove this line once the above one is uncommented
+    ;; this line is merely kept to prevent unused variable warnings
+    (run! (constantly probably-unused-warning) unused-translation-keys)
+
     (report-issues (map #(assoc % :reason "Undefined Translation Key") missing-translations))
-    (if (empty? missing-translations)
+
+    ;; uncomment more individual tests once we are ready for the script to actually fail on finding
+    ;; corresponding occurrences
+    (if (and
+         (empty? missing-translations)
+         #_(empty? possibly-unused-translation-keys)
+         #_(empty? unused-translation-keys))
       0
       1)))
 
