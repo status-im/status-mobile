17c6ee2920c0841d6e718e312d37fa260a3a9adc
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2023-11-06 10:59:45 +0000

    review fixes

--- src/status_im2/contexts/wallet/common/collectibles_tab/view.cljs
+++ src/status_im2/contexts/wallet/common/collectibles_tab/view.cljs
@@ -1,18 +1,10 @@
 (ns status-im2.contexts.wallet.common.collectibles-tab.view
   (:require
-    [clojure.string :as string]
     [quo.core :as quo]
     [react-native.core :as rn]
     [status-im2.contexts.wallet.common.empty-tab.view :as empty-tab]
     [utils.i18n :as i18n]
     [utils.re-frame :as rf]))
-
-(defn- collectible-image-url
-  [{:keys [image-url animation-url animation-media-type]}]
-  (if (and (not (string/blank? animation-url))
-           (not= animation-media-type "image/svg+xml"))
-    animation-url
-    image-url))
 
 (defn view
   []
@@ -27,9 +19,9 @@
         :style                   {:flex 1}
         :content-container-style {:align-items :center}
         :num-columns             2
-        :render-fn               (fn [collectible]
+        :render-fn               (fn [{:keys [preview-url]}]
                                    [quo/collectible
-                                    {:images   [(collectible-image-url collectible)]
+                                    {:images   [preview-url]
                                      :on-press #(rf/dispatch
                                                  [:navigate-to
                                                   :wallet-collectible])}])}])))
