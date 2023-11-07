a871e18aeb8353060f6c9db8b7e9985c6beb6ff5
Icaro Motta <icaro.ldm@gmail.com>
2023-11-06 21:09:42 -0300

    Remove unused macro

--- src/schema/core.clj
+++ src/schema/core.clj
@@ -1,16 +1,5 @@
 (ns schema.core)
 
-(defmacro =>
-  "Same as `malli.core/=>`, but instrumentation code will be completely
-  removed in non-debug environments.
-
-  Note: the type hint is mandatory so the Closure compiler can remove the code
-  at compile time."
-  [sym value]
-  `(if ^boolean js/goog.DEBUG
-     (malli.core/=> ~sym (malli.core/schema ~value))
-     ~sym))
-
 (defmacro instrument
   "Same as `schema.core/-instrument`, but instrumentation code will be completely
   removed in non-debug environments."
