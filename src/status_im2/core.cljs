ddad778cf24d42699c7f3fc8a616b74c076d7410
andrey <motor4ik@gmail.com>
2023-11-08 18:58:43 +0100

    revert

--- src/status_im2/core.cljs
+++ src/status_im2/core.cljs
@@ -1,5 +1,9 @@
 (ns status-im2.core
   (:require
+    ;; NOTE: Do NOT sort i18n-resources because it MUST be loaded first.
+    [status-im2.setup.i18n-resources :as i18n-resources]
+
+    #_{:clj-kondo/ignore [:unsorted-required-namespaces]}
     [native-module.core :as native-module]
     [re-frame.core :as re-frame]
     [re-frame.interop :as interop]
@@ -12,7 +16,7 @@
     status-im.events
     status-im.subs.root
     [status-im2.common.log :as log]
-    [status-im2.common.universal-links :as utils.universal-links]
+    [status-im2.common.universal-links :as universal-links]
     [status-im2.config :as config]
     [status-im2.contexts.push-notifications.events :as notifications]
     [status-im2.contexts.shell.jump-to.state :as shell.state]
@@ -21,7 +25,6 @@
     status-im2.navigation.core
     [status-im2.setup.dev :as dev]
     [status-im2.setup.global-error :as global-error]
-    [status-im2.setup.i18n-resources :as i18n-resources]
     [status-im2.setup.interceptors :as interceptors]
     status-im2.subs.root
     [utils.i18n :as i18n]))
@@ -44,7 +47,7 @@
   (i18n/set-language "en")
   (i18n-resources/load-language "en")
   (react-native-shake/add-shake-listener #(re-frame/dispatch [:shake-event]))
-  (utils.universal-links/initialize)
+  (universal-links/initialize)
   (interceptors/register-global-interceptors)
 
   ;; Shell
