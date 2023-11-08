cae055dc680cd083179d49a52e311893527b7b48
Yevheniia Berdnyk <ie.berdnyk@gmail.com>
2023-11-08 18:12:13 +0200

    e2e: temp fix for opening a link in Status

--- test/appium/tests/critical/chats/test_1_1_public_chats.py
+++ test/appium/tests/critical/chats/test_1_1_public_chats.py
@@ -160,16 +160,18 @@ class TestOneToOneChatMultipleSharedDevicesNewUi(MultipleSharedDeviceTestCase):
             self.errors.append("Link message reaction is not shown for the sender")
 
         self.home_2.just_fyi("Check 'Open in Status' option")
-        url_message = 'http://status.im'
-        self.chat_1.send_message(url_message)
-        try:
-            element = self.chat_2.chat_view_element_starts_with_text(url_message)
-            element.wait_for_visibility_of_element(120)
-            element.click_inside_element_by_coordinate(0.2, 0.5)
+        # url_to_open = 'http://status.im' # ToDo: enable when a bug with Status link is fixed
+        url_to_open = 'https://coinmarketcap.com/'
+        self.chat_1.send_message(url_to_open)
+        chat_element = self.chat_2.chat_element_by_text(url_to_open)
+        if chat_element.is_element_displayed(120):
+            chat_element.click_on_link_inside_message_body()
             web_view = self.chat_2.open_in_status_button.click()
-            if not web_view.element_by_text('Private, Secure Communication').is_element_displayed(60):
+            # if not web_view.element_by_text('Private, Secure Communication').is_element_displayed(60):
+            if not web_view.element_by_text_part(
+                    "Today's Cryptocurrency Prices by Market Cap").is_element_displayed(30):
                 self.errors.append('URL was not opened from 1-1 chat')
-        except TimeoutException:
+        else:
             self.errors.append("Message with URL was not received")
 
         self.errors.verify_no_errors()
