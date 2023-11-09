e59976c103e12f84fec2cd402784ec78a6974dee
Ajay Sivan <ajayesivan@gmail.com>
2023-11-09 16:05:39 +0530

    Quo community-stat component

--- src/quo/core_spec.cljs
+++ src/quo/core_spec.cljs
@@ -15,6 +15,7 @@
     [quo.components.calendar.calendar.component-spec]
     [quo.components.calendar.calendar.month-picker.component-spec]
     [quo.components.colors.color-picker.component-spec]
+    [quo.components.community.community-stat.component-spec]
     [quo.components.counter.counter.component-spec]
     [quo.components.counter.step.component-spec]
     [quo.components.dividers.divider-label.component-spec]
