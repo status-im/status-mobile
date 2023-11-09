b6e5f0fda5c298751ed378e91e3743f5c485a5dc
Icaro Motta <icaro.ldm@gmail.com>
2023-11-09 14:25:32 -0300

    Use bottom inset (esp. important for iOS)

--- src/schema/view.cljs
+++ src/schema/view.cljs
@@ -1,6 +1,7 @@
 (ns schema.view
   (:require
     [react-native.core :as rn]
+    [react-native.safe-area :as safe-area]
     schema.state
     [schema.style :as style]))
 
@@ -9,6 +10,8 @@
   (let [on-press #(reset! schema.state/errors #{})]
     (fn []
       (when (seq @schema.state/errors)
-        [rn/pressable {:on-press on-press :style style/container}
+        [rn/pressable
+         {:on-press on-press
+          :style    (style/container {:bottom-inset (safe-area/get-bottom)})}
          [rn/text {:style style/text}
           "Schema error"]]))))
