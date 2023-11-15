56ccccc471403a8f462ddbf67b2c6ad5da87f7ab
Icaro Motta <icaro.ldm@gmail.com>
2023-11-15 06:39:12 -0300

    Fix: unable to join token-gated communities (#17894)
    Fixes the bug by explicitly passing all available addresses to be revealed
    to wakuext_requestToJoinCommunity and picking up the first available address
    as the airdrop address. This is a temporary solution while we work on the
    feature to allow users to choose which addresses to expose.
    
    Fixes https://github.com/status-im/status-mobile/issues/17861
    
    *Areas that may be impacted*: join community flows.
    
        ----------------------------------------------------------------------
        Community "Request to join" option is enabled.
        User holds more than X ETH.
        Anyone who holds <X> ETH is allowed to Become member in <COMMUNITY>.
        
        Expected: request to join is received by desktop client and accepted,
        mobile user joins the community.
        ----------------------------------------------------------------------
        
        ----------------------------------------------------------------------
        Community "Request to join" option is enabled.
        User holds less than X ETH.
        Anyone who holds <X> ETH is allowed to View and post in <CHANNEL>.
        
        Expected: request to join is received by desktop client and accepted, mobile
        user joins the community, but can't post in <CHANNEL>.
        ----------------------------------------------------------------------
        
        ----------------------------------------------------------------------
        Community "Request to join" option is enabled.
        No token permissions.
        
        Expected: request to join is received by desktop client and accepted,
        mobile user joins the community.
        ----------------------------------------------------------------------

--- src/status_im2/contexts/communities/overview/events.cljs
+++ src/status_im2/contexts/communities/overview/events.cljs
@@ -42,26 +42,31 @@
                                                community-id %])
                     :on-error   #(log/error "failed to request to join community" community-id %)}]})
 
+(defn request-to-join
+  [{:keys [db]} [{:keys [community-id password]}]]
+  (let [pub-key             (get-in db [:profile/profile :public-key])
+        addresses-to-reveal []]
+    {:fx [[:json-rpc/call
+           [{:method     "wakuext_generateJoiningCommunityRequestsForSigning"
+             :params     [pub-key community-id addresses-to-reveal]
+             :on-success [:communities/sign-data community-id password]
+             :on-error   [:communities/requested-to-join-error community-id]}]]]}))
+
 ;; Event to be called to request to join a community.
 ;; This event will generate the data to be signed and then call the sign-data event.
 ;; This is the only event that should be called from the UI.
-(rf/reg-event-fx :communities/request-to-join
- (fn [{:keys [db]} [{:keys [community-id password]}]]
-   (let [pub-key             (get-in db [:profile/profile :public-key])
-         addresses-to-reveal []]
-     {:fx [[:json-rpc/call
-            [{:method     "wakuext_generateJoiningCommunityRequestsForSigning"
-              :params     [pub-key community-id addresses-to-reveal]
-              :on-success [:communities/sign-data community-id password]
-              :on-error   [:communities/requested-to-join-error community-id]}]]]})))
+(rf/reg-event-fx :communities/request-to-join request-to-join)
 
-(rf/reg-event-fx :communities/sign-data
- (fn [_ [community-id password sign-params]]
-   {:fx [[:json-rpc/call
-          [{:method     "wakuext_signData"
-            :params     [(map #(assoc % :password password) sign-params)]
-            :on-success [:communities/request-to-join-with-signatures community-id]
-            :on-error   [:communities/requested-to-join-error community-id]}]]]}))
+(defn sign-data
+  [_ [community-id password sign-params]]
+  (let [addresses-to-reveal (map :account sign-params)]
+    {:fx [[:json-rpc/call
+           [{:method     "wakuext_signData"
+             :params     [(map #(assoc % :password password) sign-params)]
+             :on-success [:communities/request-to-join-with-signatures community-id addresses-to-reveal]
+             :on-error   [:communities/requested-to-join-error community-id]}]]]}))
+
+(rf/reg-event-fx :communities/sign-data sign-data)
 
 (rf/reg-event-fx :communities/requested-to-join-error
  (fn [{:keys [db]} [community-id error]]
@@ -71,11 +76,19 @@
                :event        :communities/requested-to-join-error})
    {:db (assoc-in db [:password-authentication :error] error)}))
 
-(rf/reg-event-fx :communities/request-to-join-with-signatures
- (fn [_ [community-id signatures]]
-   {:fx [[:json-rpc/call
-          [{:method      "wakuext_requestToJoinCommunity"
-            :params      [{:communityId community-id :signatures signatures}]
-            :js-response true
-            :on-success  [:communities/requested-to-join]
-            :on-error    [:communities/requested-to-join-error community-id]}]]]}))
+(defn request-to-join-with-signatures
+  [_ [community-id addresses-to-reveal signatures]]
+  {:fx [[:json-rpc/call
+         [{:method      "wakuext_requestToJoinCommunity"
+           :params      [{:communityId       community-id
+                          :signatures        signatures
+                          :addressesToReveal addresses-to-reveal
+                          ;; NOTE: At least one airdrop address is required.
+                          ;; This is a temporary solution while the address
+                          ;; selection feature is not implemented in mobile.
+                          :airdropAddress    (first addresses-to-reveal)}]
+           :js-response true
+           :on-success  [:communities/requested-to-join]
+           :on-error    [:communities/requested-to-join-error community-id]}]]]})
+
+(rf/reg-event-fx :communities/request-to-join-with-signatures request-to-join-with-signatures)
