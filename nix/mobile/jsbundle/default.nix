fdb94ae93759f8221022c96d3f43f41bb9ae75e7
Icaro Motta <icaro.ldm@gmail.com>
2023-11-15 12:58:00 -0300

    Fix: resolve our source dependencies first in the classpath

--- nix/mobile/jsbundle/default.nix
+++ nix/mobile/jsbundle/default.nix
@@ -68,8 +68,8 @@ stdenv.mkDerivation {
   buildPhase = ''
     # Assemble CLASSPATH from available clojure dependencies.
     # We append 'src' so it can find the local sources.
-    export CLASS_PATH="$(find ${deps.clojure} \
-      -iname '*.jar' | tr '\n' ':')src"
+    export CLASS_PATH="src:$(find ${deps.clojure} \
+      -iname '*.jar' | tr '\n' ':')"
 
     # target must be one of the builds defined in shadow-cljs.edn
     java -cp "$CLASS_PATH" clojure.main \
