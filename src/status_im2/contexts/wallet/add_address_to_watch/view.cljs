a6667481a61b03ac361ede7c67a9e57f7ef15db7
Ibrkhalil <vampirekid017@gmail.com>
2023-11-18 14:23:25 +0200

    Continue button is invisible behind keyboard in Add Address To Watch screen

--- src/status_im2/contexts/wallet/add_address_to_watch/view.cljs
+++ src/status_im2/contexts/wallet/add_address_to_watch/view.cljs
@@ -6,6 +6,7 @@
     [re-frame.core :as re-frame]
     [react-native.clipboard :as clipboard]
     [react-native.core :as rn]
+    [react-native.safe-area :as safe-area]
     [reagent.core :as reagent]
     [status-im2.contexts.wallet.add-address-to-watch.style :as style]
     [utils.i18n :as i18n]
@@ -16,37 +17,40 @@
   (let [input-value         (reagent/atom "")
         customization-color (rf/sub [:profile/customization-color])]
     (fn []
-      [rn/view
-       {:style {:flex 1}}
-       [quo/page-nav
-        {:type      :no-title
-         :icon-name :i/close
-         :on-press  #(rf/dispatch [:navigate-back])}]
-       [quo/text-combinations
-        {:container-style style/header-container
-         :title           (i18n/label :t/add-address)
-         :description     (i18n/label :t/enter-eth)}]
-       [rn/view {:style style/input-container}
-        [quo/input
-         {:label           (i18n/label :t/eth-or-ens)
-          :button          {:on-press (fn [] (clipboard/get-string #(reset! input-value %)))
-                            :text     (i18n/label :t/paste)}
-          :placeholder     (str "0x123abc... " (string/lower-case (i18n/label :t/or)) " bob.eth")
-          :container-style {:margin-right 12
-                            :flex         1}
-          :weight          :monospace
-          :on-change-text  #(reset! input-value %)
-          :default-value   @input-value}]
+      [rn/keyboard-avoiding-view
+       {:style                  {:flex 1}
+        :keyboardVerticalOffset (- (safe-area/get-bottom) 40)}
+       [rn/view
+        {:style {:flex 1}}
+        [quo/page-nav
+         {:type      :no-title
+          :icon-name :i/close
+          :on-press  #(rf/dispatch [:navigate-back])}]
+        [quo/text-combinations
+         {:container-style style/header-container
+          :title           (i18n/label :t/add-address)
+          :description     (i18n/label :t/enter-eth)}]
+        [rn/view {:style style/input-container}
+         [quo/input
+          {:label           (i18n/label :t/eth-or-ens)
+           :button          {:on-press (fn [] (clipboard/get-string #(reset! input-value %)))
+                             :text     (i18n/label :t/paste)}
+           :placeholder     (str "0x123abc... " (string/lower-case (i18n/label :t/or)) " bob.eth")
+           :container-style {:margin-right 12
+                             :flex         1}
+           :weight          :monospace
+           :on-change-text  #(reset! input-value %)
+           :default-value   @input-value}]
+         [quo/button
+          {:icon-only? true
+           :type       :outline} :i/scan]]
         [quo/button
-         {:icon-only? true
-          :type       :outline} :i/scan]]
-       [quo/button
-        {:customization-color customization-color
-         :disabled?           (clojure.string/blank? @input-value)
-         :on-press            #(re-frame/dispatch [:navigate-to
-                                                   :confirm-address-to-watch
-                                                   {:address @input-value}])
-         :container-style     style/button-container}
-        (i18n/label :t/continue)]])))
+         {:customization-color customization-color
+          :disabled?           (clojure.string/blank? @input-value)
+          :on-press            #(re-frame/dispatch [:navigate-to
+                                                    :confirm-address-to-watch
+                                                    {:address @input-value}])
+          :container-style     style/button-container}
+         (i18n/label :t/continue)]]])))
 
 (def view (quo.theme/with-theme view-internal))
