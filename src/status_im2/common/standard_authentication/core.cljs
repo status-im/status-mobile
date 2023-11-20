a7b0f83f7c0954c85d365ac36c4e967b998689c7
tumanov-alex <uwontrememberthis@gmail.com>
2023-11-20 14:33:17 +0100

    Change share-qr-code component to reflect design changes

---
+++ src/status_im2/common/standard_authentication/core.cljs
@@ -0,0 +1,7 @@
+(ns status-im2.common.standard-authentication.core
+  (:require
+    status-im2.common.standard-authentication.password-input.view
+    status-im2.common.standard-authentication.standard-auth.slide-button.view))
+
+(def password-input status-im2.common.standard-authentication.password-input.view/view)
+(def slide-button status-im2.common.standard-authentication.standard-auth.slide-button.view/view)
