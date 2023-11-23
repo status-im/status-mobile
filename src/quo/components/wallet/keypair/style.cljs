4c7663eb8a98b7e7f2f569ab1bdad113d45f6072
Omar Basem <omarbasem.0101@gmail.com>
2023-11-23 10:00:22 +0400

    Wallet: Keypair follow up (#17953)
    * Wallet: Keypair follow up

--- src/quo/components/wallet/keypair/style.cljs
+++ src/quo/components/wallet/keypair/style.cljs
@@ -28,3 +28,9 @@
   {:flex-direction  :row
    :align-items     :center
    :justify-content :space-between})
+
+(defn subtitle
+  [blur? theme]
+  {:color (if blur?
+            colors/white-opa-40
+            (colors/theme-colors colors/neutral-50 colors/neutral-40 theme))})
