b2719743c9d1a917e2a1661aadede905a4ffb19a
Jamie Caprani <jamiecaprani@gmail.com>
2023-11-27 17:16:31 +0000

    chore(wallet): add common page-nav component for account switcher (#17946)

---
+++ src/status_im2/contexts/wallet/common/account_switcher/view.cljs
@@ -0,0 +1,22 @@
+(ns status-im2.contexts.wallet.common.account-switcher.view
+  (:require [quo.core :as quo]
+            [status-im2.contexts.wallet.common.sheets.account-options.view :as account-options]
+            [utils.re-frame :as rf]))
+
+(defn view
+  [{:keys [on-press accessibility-label] :or {accessibility-label :top-bar}}]
+  (let [{:keys [color emoji]} (rf/sub [:wallet/current-viewing-account])
+        networks              (rf/sub [:wallet/network-details])]
+    [quo/page-nav
+     {:icon-name           :i/close
+      :on-press            on-press
+      :accessibility-label accessibility-label
+      :networks            networks
+      :networks-on-press   #(js/alert "Pressed Networks")
+      :right-side          :account-switcher
+      :account-switcher    {:customization-color color
+                            :on-press            #(rf/dispatch [:show-bottom-sheet
+                                                                {:content account-options/view
+                                                                 :gradient-cover? true
+                                                                 :customization-color color}])
+                            :emoji               emoji}}]))
