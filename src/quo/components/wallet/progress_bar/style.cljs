493a1edc020992bd12b710f64ed9df33fa752890
Milad <mmilad.sanati@gmail.com>
2023-11-28 18:37:22 +0330

    fix colors/resolve-color usage

--- src/quo/components/wallet/progress_bar/style.cljs
+++ src/quo/components/wallet/progress_bar/style.cljs
@@ -3,27 +3,27 @@
     [quo.foundations.colors :as colors]))
 
 (defn- border-and-background-color
-  [customization-color]
+  [customization-color theme]
   {:light {:pending   {:border-color     colors/neutral-80-opa-5
                        :background-color colors/neutral-5}
            :confirmed {:border-color     colors/neutral-80-opa-5
-                       :background-color (colors/resolve-color :success 50)}
+                       :background-color (colors/resolve-color :success theme 50)}
            :finalized {:border-color     colors/neutral-80-opa-5
-                       :background-color (colors/resolve-color customization-color 50)}
+                       :background-color (colors/resolve-color customization-color theme 50)}
            :error     {:border-color     colors/neutral-80-opa-5
-                       :background-color (colors/resolve-color :danger 50)}}
+                       :background-color (colors/resolve-color :danger theme 50)}}
    :dark  {:pending   {:border-color     colors/neutral-70
                        :background-color colors/neutral-80}
            :confirmed {:border-color     colors/white-opa-5
-                       :background-color (colors/resolve-color :success 60)}
+                       :background-color (colors/resolve-color :success theme 60)}
            :finalized {:border-color     colors/neutral-80-opa-5
-                       :background-color (colors/resolve-color customization-color 60)}
+                       :background-color (colors/resolve-color customization-color theme 60)}
            :error     {:border-color     colors/white-opa-5
-                       :background-color (colors/resolve-color :danger 60)}}})
+                       :background-color (colors/resolve-color :danger theme 60)}}})
 
 (defn root-container
   [{:keys [customization-color state theme full-width?]}]
-  (let [{:keys [background-color border-color]} (get-in (border-and-background-color customization-color)
+  (let [{:keys [background-color border-color]} (get-in (border-and-background-color customization-color theme)
                                                         [theme (if full-width? :pending state)])]
     {:height            12
      :flex              (when full-width? 1)
@@ -37,7 +37,7 @@
 
 (defn progressed-bar
   [{:keys [customization-color state theme progressed-value]}]
-  (let [{:keys [background-color]} (get-in (border-and-background-color customization-color)
+  (let [{:keys [background-color]} (get-in (border-and-background-color customization-color theme)
                                            [theme state])
         progress                   (if (> progressed-value 100) 100 progressed-value)]
     {:height           12
