3fee21c73a75a9babb51e8b8050ba19fa7fa7d01
Ulises Manuel <90291778+ulisesmac@users.noreply.github.com>
2023-12-01 15:49:49 -0600

    [#17923] text combinations - standard title component (#17939)
    * Simplify tag components by turning them to from-1
    
    * Add standard-title component, preview screen and tests

---
+++ src/quo/components/text_combinations/standard_title/style.cljs
@@ -0,0 +1,24 @@
+(ns quo.components.text-combinations.standard-title.style
+  (:require [quo.foundations.colors :as colors]))
+
+(def container
+  {:flex-direction  :row
+   :justify-content :space-between})
+
+(def right-container {:margin-left 20})
+
+(def right-counter
+  {:padding-top    12
+   :padding-bottom 2})
+
+(defn right-counter-text
+  [blur? theme]
+  {:color (if blur?
+            (colors/theme-colors colors/neutral-80-opa-40 colors/white-opa-40 theme)
+            (colors/theme-colors colors/neutral-40 colors/neutral-50 theme))})
+
+(defn right-tag-icon-color
+  [blur? theme]
+  (if blur?
+    (colors/theme-colors colors/neutral-80-opa-70 colors/white-opa-70 theme)
+    (colors/theme-colors colors/neutral-50 colors/neutral-40 theme)))
