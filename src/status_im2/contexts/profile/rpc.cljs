f695dbf1156222e1921476fbfe2c35b853dd18c9
Mohsen <maxghafori@gmail.com>
2023-12-04 17:48:45 +0300

    [#17909] fix: unhandled error when app launched in offline mode (#17973)

--- src/status_im2/contexts/profile/rpc.cljs
+++ src/status_im2/contexts/profile/rpc.cljs
@@ -5,8 +5,10 @@
 
 (defn rpc->profiles-overview
   [{:keys [customizationColor keycard-pairing] :as profile}]
-  (-> profile
-      (dissoc :customizationColor)
-      (assoc :customization-color (keyword customizationColor))
-      (assoc :ens-name? (utils.ens/is-valid-eth-name? (:name profile)))
-      (assoc :keycard-pairing (when-not (string/blank? keycard-pairing) keycard-pairing))))
+  (if (map? profile)
+    (-> profile
+        (dissoc :customizationColor)
+        (assoc :customization-color (keyword customizationColor))
+        (assoc :ens-name? (utils.ens/is-valid-eth-name? (:name profile)))
+        (assoc :keycard-pairing (when-not (string/blank? keycard-pairing) keycard-pairing)))
+    profile))
