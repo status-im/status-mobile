405c7cafb208c8d3384b6c06dabd6072f1c55e85
Omar Basem <omarbasem.0101@gmail.com>
2023-12-05 10:34:40 +0400

    lint

--- src/status_im2/contexts/wallet/common/utils_test.cljs
+++ src/status_im2/contexts/wallet/common/utils_test.cljs
@@ -1,10 +1,8 @@
-(ns status-im2.contexts.wallet.common.utils-test)
+(ns status-im2.contexts.wallet.common.utils-test
+  (:require [cljs.test :refer-macros [deftest is]]
+            [status-im2.contexts.wallet.common.utils :as utils]
+            [utils.money :as money]))
 
-;(deftest test-calculate-raw-balance
-;  (is (= 500 (utils/calculate-raw-balance "50000" 2)))
-;  (is (= 0 (utils/calculate-raw-balance "abc" 2)))
-;  (is (= 0 (utils/calculate-raw-balance "50000" "abc"))))
-;
-;(deftest test-calculate-fiat-change
-;  (is (= 5 (utils/calculate-fiat-change 105 5)))
-;  (is (= -10 (utils/calculate-fiat-change 90 -10))))
+(deftest test-calculate-fiat-change
+  (is (= (money/bignumber 5) (utils/calculate-fiat-change 105 5)))
+  (is (= (money/bignumber -10) (utils/calculate-fiat-change 90 -10))))
