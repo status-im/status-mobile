0307acd71f8dbb6bbd67b46ba08194c3a0d0cb17
Omar Basem <omarbasem.0101@gmail.com>
2023-12-05 10:56:37 +0400

    Wallet: account real data (#17821)
    * wallet: account real data

--- src/status_im2/contexts/wallet/account/tabs/view.cljs
+++ src/status_im2/contexts/wallet/account/tabs/view.cljs
@@ -1,34 +1,28 @@
 (ns status-im2.contexts.wallet.account.tabs.view
   (:require
-    [quo.theme]
     [react-native.core :as rn]
-    [status-im2.common.resources :as resources]
     [status-im2.contexts.wallet.account.tabs.about.view :as about]
     [status-im2.contexts.wallet.account.tabs.dapps.view :as dapps]
     [status-im2.contexts.wallet.common.activity-tab.view :as activity]
     [status-im2.contexts.wallet.common.collectibles-tab.view :as collectibles]
     [status-im2.contexts.wallet.common.empty-tab.view :as empty-tab]
-    [status-im2.contexts.wallet.common.temp :as temp]
     [status-im2.contexts.wallet.common.token-value.view :as token-value]
-    [utils.i18n :as i18n]))
+    [utils.i18n :as i18n]
+    [utils.re-frame :as rf]))
 
-(defn- view-internal
-  [{:keys [selected-tab theme]}]
-  (case selected-tab
-    :assets       [rn/flat-list
-                   {:render-fn               token-value/view
-                    :data                    temp/tokens
-                    :content-container-style {:padding-horizontal 8}}]
-    :collectibles [collectibles/view]
-    :activity     [activity/view]
-    :permissions  [empty-tab/view
-                   {:title       (i18n/label :t/no-permissions)
-                    :description (i18n/label :t/no-collectibles-description)
-                    :image       (resources/get-image
-                                  (quo.theme/theme-value :no-permissions-light
-                                                         :no-permissions-dark
-                                                         theme))}]
-    :dapps        [dapps/view]
-    [about/view]))
-
-(def view (quo.theme/with-theme view-internal))
+(defn view
+  [{:keys [selected-tab]}]
+  (let [tokens (filter identity (rf/sub [:wallet/account-token-values]))]
+    (case selected-tab
+      :assets       [rn/flat-list
+                     {:render-fn               token-value/view
+                      :data                    tokens
+                      :content-container-style {:padding-horizontal 8}}]
+      :collectibles [collectibles/view]
+      :activity     [activity/view]
+      :permissions  [empty-tab/view
+                     {:title        (i18n/label :t/no-permissions)
+                      :description  (i18n/label :t/no-collectibles-description)
+                      :placeholder? true}]
+      :dapps        [dapps/view]
+      [about/view])))
