65771ab2d1704ba33d21aafe7c8f2a3ebc46c75e
Omar Basem <omarbasem.0101@gmail.com>
2023-12-05 10:39:22 +0400

    lint

--- src/status_im2/contexts/wallet/common/utils_test.cljs
+++
@@ -1,8 +0,0 @@
-(ns status-im2.contexts.wallet.common.utils-test
-  (:require [cljs.test :refer-macros [deftest is]]
-            [status-im2.contexts.wallet.common.utils :as utils]
-            [utils.money :as money]))
-
-(deftest test-calculate-fiat-change
-  (is (= (money/bignumber 5) (utils/calculate-fiat-change 105 5)))
-  (is (= (money/bignumber -10) (utils/calculate-fiat-change 90 -10))))
