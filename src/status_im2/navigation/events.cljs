b3f54d0f7ab61d87b61ffaad8a4dad8ca11a5913
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2023-12-05 22:27:03 +0530

    fix messages are marked as read automatically in 1-1/group chats after opening a link (#18066)

--- src/status_im2/navigation/events.cljs
+++ src/status_im2/navigation/events.cljs
@@ -60,12 +60,15 @@
 (rf/defn pop-to-root
   {:events [:pop-to-root]}
   [{:keys [db]} tab]
-  {:pop-to-root-fx            tab
-   :db                        (-> db
-                                  (dissoc :shell/floating-screens)
-                                  (dissoc :shell/loaded-screens)
-                                  (assoc :view-id (or @shell.state/selected-stack-id :shell)))
-   :effects.shell/pop-to-root nil})
+  (merge
+   {:pop-to-root-fx            tab
+    :db                        (-> db
+                                   (dissoc :shell/floating-screens)
+                                   (dissoc :shell/loaded-screens)
+                                   (assoc :view-id (or @shell.state/selected-stack-id :shell)))
+    :effects.shell/pop-to-root nil}
+   (when (:current-chat-id db)
+     {:dispatch-n [[:chat/close]]})))
 
 (rf/defn init-root
   {:events [:init-root]}
