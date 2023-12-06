aa75ac49f1c0ba69baf965ea5f1f73d1d9830b05
Jamie Caprani <jamiecaprani@gmail.com>
2023-12-06 11:33:38 +0000

    chore: fix after onboarding

--- src/status_im2/contexts/onboarding/events.cljs
+++ src/status_im2/contexts/onboarding/events.cljs
@@ -133,13 +133,18 @@
   {:db       (dissoc db :onboarding/profile)
    :dispatch [:navigate-to-within-stack [:create-profile :new-to-status]]})
 
+(rf/reg-event-fx :onboarding/set-auth-method
+ (fn [{:keys [db]} [auth-method]]
+   {:db (assoc db :auth-method auth-method)}))
+
 (rf/defn onboarding-new-account-finalize-setup
   {:events [:onboarding/finalize-setup]}
   [{:keys [db]}]
   (let [masked-password    (get-in db [:onboarding/profile :password])
         key-uid            (get-in db [:profile/profile :key-uid])
         syncing?           (get-in db [:onboarding/profile :syncing?])
-        biometric-enabled? (= (get-in db [:onboarding/profile :auth-method])
+        auth-method        (get-in db [:onboarding/profile :auth-method])
+        biometric-enabled? (= auth-method
                               constants/auth-method-biometric)]
     (cond-> {:db (assoc db :onboarding/generated-keys? true)}
       biometric-enabled?
@@ -151,6 +156,7 @@
               :on-success      (fn []
                                  (if syncing?
                                    (rf/dispatch [:onboarding/navigate-to-enable-notifications])
+                                   (rf/dispatch [:onboarding/set-auth-method auth-method])
                                    (log/error "successfully saved biometrics")))
               :on-error        #(log/error "failed to save biometrics"
                                            {:key-uid key-uid
