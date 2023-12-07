890a18b3b6dcad3394a1bff13fd77c8a65f99a96
andrey <motor4ik@gmail.com>
2023-12-07 15:50:20 +0100

    move blob to rn, remove unused code, move images events to status-im2

--- src/status_im2/contexts/chat/lightbox/effects.cljs
+++ src/status_im2/contexts/chat/lightbox/effects.cljs
@@ -6,20 +6,24 @@
             [react-native.share :as share]
             [utils.re-frame :as rf]))
 
+(def config
+  {:trusty platform/ios?
+   :path   (str (fs/cache-dir) "/StatusIm_Image.jpeg")})
+
 (rf/reg-fx :effects.chat/share-image
  (fn [uri]
-   (blob/get uri
-             platform/ios?
-             (fn [downloaded-url]
-               (share/open {:url       (str (when platform/android? "file://") downloaded-url)
-                            :isNewTask true}
-                           #(fs/unlink downloaded-url)
-                           #(fs/unlink downloaded-url))))))
+   (blob/fetch uri
+               config
+               (fn [downloaded-url]
+                 (share/open {:url       (str (when platform/android? "file://") downloaded-url)
+                              :isNewTask true}
+                             #(fs/unlink downloaded-url)
+                             #(fs/unlink downloaded-url))))))
 
 (rf/reg-fx :effects.chat/save-image-to-gallery
  (fn [[uri on-success]]
-   (blob/get uri
-             platform/ios?
-             (fn [downloaded-url]
-               (cameraroll/save-image downloaded-url)
-               (on-success)))))
+   (blob/fetch uri
+               config
+               (fn [downloaded-url]
+                 (cameraroll/save-image downloaded-url)
+                 (on-success)))))
