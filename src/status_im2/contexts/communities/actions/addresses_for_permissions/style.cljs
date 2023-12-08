dd324e449c07c947820331d221cd46b7f661a5c5
Ajay Sivan <ajayesivan@gmail.com>
2023-12-08 12:50:15 +0530

    wip: Address for permissions header

---
+++ src/status_im2/contexts/communities/actions/addresses_for_permissions/style.cljs
@@ -0,0 +1,9 @@
+(ns status-im2.contexts.communities.actions.addresses-for-permissions.style)
+
+(def screen-horizontal-padding 20)
+
+(def container {:flex 1})
+
+(def page-top
+  {:padding-vertical   12
+   :padding-horizontal screen-horizontal-padding})
--- src/status_im2/contexts/communities/actions/addresses_for_permissions/view.cljs
+++ src/status_im2/contexts/communities/actions/addresses_for_permissions/view.cljs
@@ -1,6 +1,30 @@
 (ns status-im2.contexts.communities.actions.addresses-for-permissions.view
-  (:require [react-native.core :as rn]))
+  (:require [react-native.core :as rn]
+            [status-im2.contexts.communities.actions.addresses-for-permissions.style :as style]
+            [quo.core :as quo]
+            [utils.i18n :as i18n]
+            [utils.re-frame :as rf]))
+
+(defn- page-top ;; TODO:: Make this a common component
+  [{:keys [community-name logo-uri]}]
+  [rn/view {:style style/page-top}
+   [quo/text
+    {:size   :heading-2
+     :weight :semi-bold}
+    (i18n/label :t/addresses-for-permissions)]
+   [quo/context-tag
+    {:type            :community
+     :size            24
+     :community-logo  logo-uri
+     :community-name  community-name
+     :container-style {:margin-top 8}}]])
 
 (defn view
   []
-  [rn/text "Addresses for permissions"])
+  (let [{id :community-id}          (rf/sub [:get-screen-params])
+        {:keys [name color images]} (rf/sub [:communities/community id])]
+    [rn/view {:style style/container}
+     [page-top
+      {:community-name name
+       :logo-uri       (get-in images [:thumbnail :uri])}]
+    ]))
