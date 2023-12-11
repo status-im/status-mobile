9f2ee36e0beeaf6fe0576ce90ca3c1137240cace
Milad <mmilad.sanati@gmail.com>
2023-12-11 19:32:23 +0330

    move variants to props

--- src/quo/components/profile/link_card/view.cljs
+++ src/quo/components/profile/link_card/view.cljs
@@ -1,39 +1,14 @@
 (ns quo.components.profile.link-card.view
-  (:require [clojure.core :as core]
-            [clojure.string :as string]
-            [quo.components.icon :as icons]
-            [quo.components.markdown.text :as text]
+  (:require [quo.components.markdown.text :as text]
             [quo.components.profile.link-card.properties :as properties]
             [quo.components.profile.link-card.style :as style]
             [quo.components.utilities.social.view :as social]
-            [quo.foundations.colors :as colors]
             [quo.theme]
             [react-native.core :as rn]
             [react-native.linear-gradient :as linear-gradient]))
 
-(defn title
-  [link]
-  (case link
-    :link      "Website"
-    :github    "GitHub"
-    :linkedin  "LinkedIn"
-    :superrare "SuperRare"
-    :youtube   "YouTube"
-    (string/capitalize (core/name link))))
-
-(defn website-icon
-  [theme]
-  [icons/icon
-   :i/link
-   {:accessibility-label :website-icon
-    :color               (colors/theme-colors colors/neutral-50
-                                              colors/neutral-40
-                                              theme)
-    :size                20}])
-
 (defn- view-internal
-  [{:keys [address link theme on-press customization-color]
-    :or   {link :link}}]
+  [{:keys [address theme on-press icon title customization-color]}]
   [rn/pressable
    {:accessibility-label :link-card
     :on-press            on-press}
@@ -44,15 +19,13 @@
      :end    {:x 1 :y 1}
      :style  (style/container theme)}
     [rn/view {:style style/icon-container}
-     (if (= link :link)
-       [website-icon]
-       [social/view
-        {:accessibility-label :social-icon
-         :social              link}])]
+     [social/view
+      {:accessibility-label :social-icon
+       :social              icon}]]
     [text/text
      {:accessibility-label :title
       :weight              :semi-bold}
-     (title link)]
+     title]
     [text/text
      {:accessibility-label :address
       :size                :paragraph-2}
