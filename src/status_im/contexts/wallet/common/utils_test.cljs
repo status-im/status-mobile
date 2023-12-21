d79c9fc90db7620d39fba5a9d8c922dab86e161d
Omar Basem <omarbasem.0101@gmail.com>
2023-12-21 14:10:48 +0400

    tests

--- src/status_im/contexts/wallet/common/utils_test.cljs
+++ src/status_im/contexts/wallet/common/utils_test.cljs
@@ -16,3 +16,18 @@
 
       (is (= (utils/get-wallet-qr wallet-singlechain)
              "x000")))))
+
+(deftest test-extract-exponent
+  (testing "extract-exponent function"
+    (is (= (utils/extract-exponent "123.456") nil))
+    (is (= (utils/extract-exponent "2.5e-2") "2"))
+    (is (= (utils/extract-exponent "4.567e-10") "10"))))
+
+(deftest test-calc-max-crypto-decimals
+  (testing "calc-max-crypto-decimals function"
+    (is (= (utils/calc-max-crypto-decimals 0.00323) 2))
+    (is (= (utils/calc-max-crypto-decimals 0.00123) 3))
+    (is (= (utils/calc-max-crypto-decimals 0.00000423) 5))
+    (is (= (utils/calc-max-crypto-decimals 1.23e-6) 6))
+    (is (= (utils/calc-max-crypto-decimals 1.13e-6) 7))))
+
