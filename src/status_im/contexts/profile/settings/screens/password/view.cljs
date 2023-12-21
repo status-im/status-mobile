a8696f7f0d7702e0abb0d1d366efea239d575de6
Cristian Lungu <lungucristian95@gmail.com>
2023-12-21 11:47:56 +0200

    ref: addressed review comments

--- src/status_im/contexts/profile/settings/screens/password/view.cljs
+++ src/status_im/contexts/profile/settings/screens/password/view.cljs
@@ -7,6 +7,7 @@
             [status-im.common.not-implemented :as not-implemented]
             [status-im.common.standard-authentication.standard-auth.authorize :as authorize]
             [status-im.constants :as constants]
+            [status-im.contexts.profile.settings.screens.password.style :as style]
             [utils.i18n :as i18n]
             [utils.re-frame :as rf]
             [utils.security.core :as security]))
@@ -30,10 +31,10 @@
   [theme]
   (let [auth-method    (rf/sub [:auth-method])
         biometric-type (rf/sub [:biometric/supported-type])
-        supported?     (boolean biometric-type)
-        enabled?       (= auth-method constants/auth-method-biometric)
         label          (biometric/get-label-by-type biometric-type)
         icon           (biometric/get-icon-by-type biometric-type)
+        supported?     (boolean biometric-type)
+        enabled?       (= auth-method constants/auth-method-biometric)
         biometric-on?  (and supported? enabled?)
         press-handler  (if biometric-on?
                          (fn [] (rf/dispatch [:biometric/disable]))
@@ -45,7 +46,7 @@
      :action       :selector
      :action-props {:disabled? (not supported?)
                     :on-change press-handler
-                    :checked?  (and supported? enabled?)}
+                    :checked?  biometric-on?}
      :on-press     press-handler}))
 
 (defn- get-change-password-item
@@ -62,20 +63,19 @@
   (let [insets (safe-area/get-insets)]
     [quo/overlay {:type :shell}
      [rn/view
-      {:style {:padding-top (:top insets)}}
+      {:style (style/navigation (:top insets))}
       [quo/page-nav
        {:background :blur
         :icon-name  :i/arrow-left
         :on-press   #(rf/dispatch [:navigate-back])}]]
      [rn/view
-      {:style {:padding-horizontal 20
-               :padding-bottom     8
-               :padding-top        12}}
+      {:style style/header}
       [quo/text
        {:accessibility-label :password-settings-label
         :weight              :semi-bold
         :number-of-lines     1
-        :size                :heading-1} (i18n/label :t/password)]]
+        :size                :heading-1}
+       (i18n/label :t/password)]]
      [quo/category
       {:data      [(get-biometric-item theme)
                    (get-change-password-item)]
