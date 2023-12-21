4c3d1667caa99bfdade03651a326e005e4264d26
Jamie Caprani <jamiecaprani@gmail.com>
2023-12-21 15:37:18 +0000

    chore: add outline for transacation-progress page (#18217)

--- src/status_im/contexts/status_im_preview/common/floating_button_page/view.cljs
+++ src/status_im/contexts/status_im_preview/common/floating_button_page/view.cljs
@@ -9,8 +9,9 @@
 
 (defn view
   []
-  (let [content-height (reagent/atom 450)
-        slide?         (reagent/atom false)]
+  (let [content-height  (reagent/atom 450)
+        gradient-cover? (reagent/atom false)
+        slide?          (reagent/atom false)]
     (fn []
       [rn/view {:style (style/container)}
        (when-not @slide?
@@ -18,28 +19,30 @@
           {:style  style/background-image
            :source (resources/get-mock-image :dark-blur-bg)}])
        [floating-button-page/view
-        {:header [quo/page-nav
-                  {:type        :title-description
-                   :title       "floating button page"
-                   :description "press right icon to swap button type"
-                   :text-align  :left
-                   :right-side  [{:icon-name :i/swap
-                                  :on-press  #(swap! slide? not)}]
-                   :background  :blur
-                   :icon-name   :i/close
-                   :on-press    #(rf/dispatch [:navigate-back])}]
-         :footer (if @slide?
-                   [quo/slide-button
-                    {:track-text          "We gotta slide"
-                     :track-icon          :face-id
-                     :container-style     {:z-index 2}
-                     :customization-color :blue
-                     :on-complete         #(js/alert "button slid")}
-                    "Save address"]
-                   [quo/button
-                    {:container-style {:z-index 2}
-                     :on-press        #(js/alert "button pressed")}
-                    "Save address"])}
+        {:header              [quo/page-nav
+                               {:type        :title-description
+                                :title       "floating button page"
+                                :description "press right icon to swap button type"
+                                :text-align  :left
+                                :right-side  [{:icon-name :i/swap
+                                               :on-press  #(swap! slide? not)}]
+                                :background  :blur
+                                :icon-name   :i/close
+                                :on-press    #(rf/dispatch [:navigate-back])}]
+         :footer              (if @slide?
+                                [quo/slide-button
+                                 {:track-text          "We gotta slide"
+                                  :track-icon          :face-id
+                                  :container-style     {:z-index 2}
+                                  :customization-color :blue
+                                  :on-complete         #(js/alert "button slid")}
+                                 "Save address"]
+                                [quo/button
+                                 {:container-style {:z-index 2}
+                                  :on-press        #(js/alert "button pressed")}
+                                 "Save address"])
+         :gradient-cover?     @gradient-cover?
+         :customization-color :orange}
         [rn/view {:style (style/page-content @content-height)}
          [quo/text {:size :heading-1} "Page Content"]
          [quo/input
@@ -52,4 +55,8 @@
          [quo/button
           {:type     :outline
            :on-press #(swap! content-height (fn [v] (- v 10)))}
-          "decrease height"]]]])))
+          "decrease height"]
+         [quo/button
+          {:type     :outline
+           :on-press #(swap! gradient-cover? not)}
+          "gradient cover?"]]]])))
