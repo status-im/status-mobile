d51f0aa8da25cf018dcc472f1aee26247deff331
Mohsen <maxghafori@gmail.com>
2024-01-02 18:19:57 +0300

    [#18182] test: implement integration test for status-go

---
+++ src/status_im/integration_test/profile_test.cljs
@@ -0,0 +1,22 @@
+(ns status-im.integration-test.profile-test
+  (:require
+    [cljs.test :refer [deftest is]]
+    [day8.re-frame.test :as rf-test]
+    [status-im.contexts.profile.utils :as profile.utils]
+    [test-helpers.integration :as h]
+    [utils.re-frame :as rf]))
+
+(deftest edit-profile-name-test
+  (h/log-headline :edit-profile-name-test)
+  (let [new-name "New Name"]
+    (rf-test/run-test-async
+     (h/with-app-initialized
+      (h/with-account
+       (rf/dispatch [:profile/edit-name new-name])
+       (rf-test/wait-for
+         [:navigate-back]
+         (rf-test/wait-for
+           [:toasts/upsert]
+           (let [profile      (rf/sub [:profile/profile])
+                 display-name (profile.utils/displayed-name profile)]
+             (is (= new-name display-name))))))))))
