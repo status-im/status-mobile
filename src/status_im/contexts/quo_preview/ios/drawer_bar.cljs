f016a9c33c9a882ea0c1e466330b60b7709d712b
Jamie Caprani <jamiecaprani@gmail.com>
2024-01-03 10:25:49 +0000

    chore: clean up contexts folder to only include larger feature sets (#18286)

--- src/status_im/contexts/quo_preview/ios/drawer_bar.cljs
+++
@@ -1,9 +0,0 @@
-(ns status-im.contexts.quo-preview.ios.drawer-bar
-  (:require
-    [quo.core :as quo]
-    [status-im.contexts.quo-preview.preview :as preview]))
-
-(defn view
-  []
-  [preview/preview-container {}
-   [quo/drawer-bar]])
