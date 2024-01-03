7d1ff4d72f874b432fa0e8d894481da269744e45
yqrashawn <namy.19@gmail.com>
2024-01-03 11:10:08 +0800

    fix: generate_profile_url.cljs$core$IFn$_invoke$arity$2 crash when login
    'status_im.common.universal_links.generate_profile_url.cljs$core$IFn$_invoke$arity$2' is undefined
    
    update the function to a fixed arity function

--- src/status_im/common/universal_links_test.cljs
+++ src/status_im/common/universal_links_test.cljs
@@ -5,10 +5,13 @@
     [re-frame.core :as re-frame]
     [status-im.common.universal-links :as links]))
 
+(def pubkey
+  "0x04fbce10971e1cd7253b98c7b7e54de3729ca57ce41a2bfb0d1c4e0a26f72c4b6913c3487fa1b4bb86125770f1743fb4459da05c1cbe31d938814cfaf36e252073")
+
 (deftest handle-url-test
   (testing "the user is not logged in"
     (testing "it stores the url for later processing"
-      (is (= {:db {:universal-links/url "some-url"}}
+      (is (match? {:db {:universal-links/url "some-url"}}
              (links/handle-url {:db {}} "some-url")))))
   (testing "the user is logged in"
     (let [db {:profile/profile     {:public-key "pk"}
@@ -18,7 +21,7 @@
         (is (nil? (get-in (links/handle-url {:db db} "some-url")
                           [:db :universal-links/url]))))
       (testing "Handle a custom string"
-        (is (= (get-in (links/handle-url {:db db} "https://status.app/u#statuse2e")
+        (is (match? (get-in (links/handle-url {:db db} "https://status.app/u#statuse2e")
                        [:router/handle-uri :uri])
                "https://status.app/u#statuse2e"))))))
 
@@ -28,77 +31,58 @@
       (let [actual (atom nil)]
         (with-redefs [re-frame/dispatch #(reset! actual %)]
           (links/url-event-listener #js {:url "some-url"})
-          (is (= [:universal-links/handle-url "some-url"] @actual))))))
+          (is (match? [:universal-links/handle-url "some-url"] @actual))))))
   (testing "the url is nil"
     (testing "it does not dispatches the url"
       (let [actual (atom nil)]
         (with-redefs [re-frame/dispatch #(reset! actual %)]
           (links/url-event-listener #js {})
-          (is (= nil @actual)))))))
+          (is (match? nil @actual)))))))
 
 (deftest generate-profile-url
   (testing "user has ens name"
     (testing "it calls the ens rpc method with ens name as param"
-      (let [pubkey "pubkey"
-            db     {:profile/profile {:ens-name? true :public-key pubkey}}
-            rst    (links/generate-profile-url {:db db})]
-        (are [result expected] (= result expected)
+      (let [db  {:profile/profile {:ens-name? true :public-key pubkey}}
+            rst (links/generate-profile-url {:db db} [])]
+        (are [result expected] (match? result expected)
          "wakuext_shareUserURLWithENS" (-> rst :json-rpc/call first :method)
          pubkey                        (-> rst :json-rpc/call first :params first)))))
   (testing "user has no ens name"
     (testing "it calls the ens rpc method with public keyas param"
-      (let [pubkey "pubkey"
-            db     {:profile/profile {:public-key pubkey}}
-            rst    (links/generate-profile-url {:db db})]
-        (are [result expected] (= result expected)
+      (let [db  {:profile/profile {:public-key pubkey}}
+            rst (links/generate-profile-url {:db db} [])]
+        (are [result expected] (match? result expected)
          "wakuext_shareUserURLWithData" (-> rst :json-rpc/call first :method)
          pubkey                         (-> rst :json-rpc/call first :params first)))))
   (testing "contact has ens name"
     (testing "it calls the ens rpc method with ens name as param"
-      (let [pubkey "pubkey"
-            ens    "ensname.eth"
-            db     {:contacts/contacts {pubkey {:ens-name ens}}}
-            rst    (links/generate-profile-url {:db db} [{:public-key pubkey}])]
-        (are [result expected] (= result expected)
+      (let [ens "ensname.eth"
+            db  {:contacts/contacts {pubkey {:ens-name ens}}}
+            rst (links/generate-profile-url {:db db} [{:public-key pubkey}])]
+        (are [result expected] (match? result expected)
          "wakuext_shareUserURLWithENS" (-> rst :json-rpc/call first :method)
          pubkey                        (-> rst :json-rpc/call first :params first)))))
   (testing "contact has no ens name"
     (testing "it calls the ens rpc method with public keyas param"
-      (let [pubkey "pubkey"
-            db     {:contacts/contacts {pubkey {:public-key pubkey}}}
-            rst    (links/generate-profile-url {:db db} [{:public-key pubkey}])]
-        (are [result expected] (= result expected)
+      (let [db  {:contacts/contacts {pubkey {:public-key pubkey}}}
+            rst (links/generate-profile-url {:db db} [{:public-key pubkey}])]
+        (are [result expected] (match? result expected)
          "wakuext_shareUserURLWithData" (-> rst :json-rpc/call first :method)
          pubkey                         (-> rst :json-rpc/call first :params first))))))
 
 (deftest save-profile-url
   (testing "given a contact public key and profile url"
     (testing "it updates the contact in db"
-      (let [pubkey "pubkey"
-            url    "url"
-            db     {:contacts/contacts {pubkey {:public-key pubkey}}}
-            rst    (links/save-profile-url {:db db} [pubkey url])]
-        (is (= (get-in rst [:db :contacts/contacts pubkey :universal-profile-url]) url)))))
+      (let [url "url"
+            db  {:contacts/contacts {pubkey {:public-key pubkey}}}
+            rst (links/save-profile-url {:db db} [pubkey url])]
+        (is (match? (get-in rst [:db :contacts/contacts pubkey :universal-profile-url]) url)))))
   (testing "given a user public key and profile url"
     (testing "it updates the user profile in db"
-      (let [pubkey "pubkey"
-            url    "url"
-            db     {:profile/profile {:public-key pubkey}}
-            rst    (links/save-profile-url {:db db} [pubkey url])]
-        (is (= (get-in rst [:db :profile/profile :universal-profile-url]) url)))))
-  (testing "given a invalid url"
-    (testing "it returns the db untouched"
-      (let [pubkey "pubkey"
-            url    "url"
-            db     {:profile/profile {:public-key pubkey}}
-            rst    (links/save-profile-url {:db db} ["invalid pubkey" url])]
-        (is (= (:db rst) db)))))
-  (testing "given a nil as url"
-    (testing "it returns nil"
-      (let [pubkey "pubkey"
-            db     {:profile/profile {:public-key pubkey}}
-            rst    (links/save-profile-url {:db db} ["invalid pubkey"])]
-        (is (nil? rst))))))
+      (let [url "url"
+            db  {:profile/profile {:public-key pubkey}}
+            rst (links/save-profile-url {:db db} [pubkey url])]
+        (is (match? (get-in rst [:db :profile/profile :universal-profile-url]) url))))))
 
 (deftest universal-link-test
   (testing "universal-link?"
