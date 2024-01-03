59f822241118979ae38d25f45893dfdf982ce9e5
Omar Basem <omarbasem.0101@gmail.com>
2024-01-03 15:25:45 +0400

    lint

--- src/status_im/contexts/wallet/common/utils.cljs
+++ src/status_im/contexts/wallet/common/utils.cljs
@@ -45,25 +45,25 @@
 
 (defn calc-max-crypto-decimals
   [value]
-  (let [str-representation (str value)
-        decimal-part       (second (clojure.string/split str-representation #"\."))
-        exponent           (extract-exponent str-representation)
-        zeroes-count       (count (take-while #(= \0 %) decimal-part))
-        max-decimals       (or exponent zeroes-count)]
-    (let [first-non-zero-digit (first (filter #(not (= \0 %)) decimal-part))]
-      (if (= \1 first-non-zero-digit)
-        (inc max-decimals)
-        max-decimals))))
+  (let [str-representation   (str value)
+        decimal-part         (second (clojure.string/split str-representation #"\."))
+        exponent             (extract-exponent str-representation)
+        zeroes-count         (count (take-while #(= \0 %) decimal-part))
+        max-decimals         (or exponent zeroes-count)
+        first-non-zero-digit (first (filter #(not (= \0 %)) decimal-part))]
+    (if (= \1 first-non-zero-digit)
+      (inc max-decimals)
+      max-decimals)))
 
 (defn get-standard-crypto-format
   "For full details: https://github.com/status-im/status-mobile/issues/18225"
   [{:keys [market-values-per-currency]} token-units]
   (let [price          (get-in market-values-per-currency [:usd :price])
         one-cent-value (if (pos? price) (/ 0.01 price) 0)
-        count          (calc-max-crypto-decimals one-cent-value)]
+        decimals-count (calc-max-crypto-decimals one-cent-value)]
     (if (< token-units one-cent-value)
-      (str "<" (.toFixed one-cent-value count))
-      (.toFixed token-units count))))
+      (str "<" (.toFixed one-cent-value decimals-count))
+      (.toFixed token-units decimals-count))))
 
 (defn total-token-units-in-all-chains
   [{:keys [balances-per-chain decimals] :as _token}]
