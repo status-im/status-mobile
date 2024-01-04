5843647d158a9e366d6b3ffac1f5dd8577e3ed80
Omar Basem <omarbasem.0101@gmail.com>
2024-01-04 17:18:12 +0400

    fix test

--- src/status_im/contexts/wallet/common/utils_test.cljs
+++ src/status_im/contexts/wallet/common/utils_test.cljs
@@ -37,10 +37,10 @@
 
 (deftest test-total-raw-balance-in-all-chains
   (testing "total-raw-balance-in-all-chains function"
-    (let [balances-per-chain {1 {:raw-balance (money/bignumber 1000000000000)}
-                              10 {:raw-balance (money/bignumber 2645130235566666)}
-                              42161 {:raw-balance (money/bignumber 900000000000000)}}]
-      (is (= (utils/total-raw-balance-in-all-chains balances-per-chain) (money/bignumber 3546130235566666))))))
+    (let [balances-per-chain {1 {:raw-balance 100}
+                              10 {:raw-balance 200}
+                              42161 {:raw-balance 300}}]
+      (is (= (utils/total-raw-balance-in-all-chains balances-per-chain) 600)))))
 
 (deftest test-extract-exponent
   (testing "extract-exponent function"
