e98425987e63f11e8287a228f55029c8c0f8cf8f
Ulises M <ulises.ssb@hotmail.com>
2024-01-05 13:35:31 -0600

    Add empty state image

--- src/status_im/contexts/wallet/common/collectibles_tab/view.cljs
+++ src/status_im/contexts/wallet/common/collectibles_tab/view.cljs
@@ -9,25 +9,30 @@
 
 (defn- view-internal
   [{:keys [theme collectibles filtered? on-collectible-press]}]
-  (cond
-    (and filtered? (empty? collectibles))
-    [rn/view]
+  (let [no-results-match-query? (and filtered? (empty? collectibles))]
+    (cond
+      no-results-match-query?
+      [rn/view {:style {:flex 1 :justify-content :center}}
+       [quo/empty-state
+        {:title       (i18n/label :t/nothing-found)
+         :description (i18n/label :t/try-to-search-something-else)
+         :image       (resources/get-themed-image :no-collectibles theme)}]]
 
-    (empty? collectibles)
-    [empty-tab/view
-     {:title       (i18n/label :t/no-collectibles)
-      :description (i18n/label :t/no-collectibles-description)
-      :image       (resources/get-themed-image :no-collectibles theme)}]
+      (empty? collectibles)
+      [empty-tab/view
+       {:title       (i18n/label :t/no-collectibles)
+        :description (i18n/label :t/no-collectibles-description)
+        :image       (resources/get-themed-image :no-collectibles theme)}]
 
-    :else
-    [rn/flat-list
-     {:data                    collectibles
-      :style                   {:flex 1}
-      :content-container-style {:align-items :center}
-      :num-columns             2
-      :render-fn               (fn [{:keys [preview-url id]}]
-                                 [quo/collectible
-                                  {:images   [preview-url]
-                                   :on-press #(on-collectible-press id)}])}]))
+      :else
+      [rn/flat-list
+       {:data                    collectibles
+        :style                   {:flex 1}
+        :content-container-style {:align-items :center}
+        :num-columns             2
+        :render-fn               (fn [{:keys [preview-url id]}]
+                                   [quo/collectible
+                                    {:images   [preview-url]
+                                     :on-press #(on-collectible-press id)}])}])))
 
 (def view (quo.theme/with-theme view-internal))
