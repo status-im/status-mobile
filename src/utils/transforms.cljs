27d20e95fe52179f80fed66a664ad095074a79c7
Ulises M <ulises.ssb@hotmail.com>
2024-01-05 14:50:41 -0600

    Add docstrings and remove function indirection

--- src/utils/transforms.cljs
+++ src/utils/transforms.cljs
@@ -3,8 +3,8 @@
   (:require
     [cljs-bean.core :as clj-bean]
     [oops.core :as oops]
-    [reagent.impl.template]
-    [reagent.impl.util]))
+    [reagent.impl.template :as reagent.template]
+    [reagent.impl.util :as reagent.util]))
 
 (defn js->clj [data] (cljs.core/js->clj data :keywordize-keys true))
 
@@ -25,28 +25,30 @@
   (when-not (= json "undefined")
     (try (.parse js/JSON json) (catch js/Error _ (when (string? json) json)))))
 
-;; Functions took from reagent and modified to also transform vectors contained in styles
-
-(def ^:private js-val? reagent.impl.util/js-val?)
-(def ^:private named? reagent.impl.util/named?)
-(def ^:private cached-prop-name reagent.impl.template/cached-prop-name)
 (declare styles-with-vectors)
 
-(defn ^:private kv-conv
-  [o k v]
-  (doto o
-    (oops/gobj-set (cached-prop-name k) (styles-with-vectors v))))
+(defn ^:private convert-keys-and-values
+  "Takes a JS Object a key and a value.
+   Transforms the key from a Clojure style prop to a JS style prop, using the reagent cache.
+   Performs a mutual recursion transformation on the value using `styles-with-vectors`.
+
+   Based on `reagent.impl.template/kv-conv`."
+  [obj k v]
+  (doto obj
+    (oops/gobj-set (reagent.template/cached-prop-name k) (styles-with-vectors v))))
 
 (defn styles-with-vectors
+  "Takes a Clojure style map or a Clojure vector of style maps and returns a JS Object
+   valid to use as React Native styles.
+   The transformation is done by performing mutual recursive calls with `convert-keys-and-values`.
+
+   Based on `reagent.impl.template/convert-prop-value`."
   [x]
-  (cond (js-val? x) x
-        (named? x)  (name x)
-        (map? x)    (reduce-kv kv-conv #js {} x)
-        (vector? x) (to-array (map styles-with-vectors x))
-        (coll? x)   (clj->js x)
-        (ifn? x)    (fn [& args]
-                      (apply x args))
-        :else       (clj->js x)))
+  (cond (reagent.util/js-val? x) x
+        (reagent.util/named? x)  (name x)
+        (map? x)                 (reduce-kv convert-keys-and-values #js {} x)
+        (vector? x)              (to-array (mapv styles-with-vectors x))
+        :else                    (clj->js x)))
 
 (defn copy-js-obj-to-map
   "Copy `obj` keys and values into `m` if `(pred obj-key)` is satisfied."
