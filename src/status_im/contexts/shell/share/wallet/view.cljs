d1d993f3de5aa9477055b9415a78938804ddb29a
Paul Fitzgerald <paulfitz99@gmail.com>
2024-01-06 14:55:16 +0000

    update view

--- src/status_im/contexts/shell/share/wallet/view.cljs
+++ src/status_im/contexts/shell/share/wallet/view.cljs
@@ -1,6 +1,5 @@
 (ns status-im.contexts.shell.share.wallet.view
   (:require
-    [legacy.status-im.ui.components.react :as react]
     [quo.core :as quo]
     [react-native.core :as rn]
     [react-native.platform :as platform]
@@ -45,10 +44,9 @@
                                           (reset! selected-networks (map #(get utils/id->network %)
                                                                          chain-ids)))}])}]))
 (defn wallet-qr-code-item
-  [account]
+  [account width index]
   (let [selected-networks (reagent/atom [:ethereum :optimism :arbitrum])
-        wallet-type       (reagent/atom :wallet-legacy)
-        width             (rf/sub [:dimensions/window-width])]
+        wallet-type       (reagent/atom :wallet-legacy)]
     (fn []
       (let [share-title         (str (:name account) " " (i18n/label :t/address))
             qr-url              (utils/get-wallet-qr {:wallet-type       @wallet-type
@@ -59,7 +57,7 @@
                                   :port        (rf/sub [:mediaserver/port])
                                   :qr-size     qr-size
                                   :error-level :highest})]
-        [rn/view {:style {:width width}}
+        [rn/view {:style {:width width :margin-left (if (zero? index) 0 -30)}}
          [rn/view {:style style/qr-code-container}
           [quo/share-qr-code
            {:type                @wallet-type
@@ -79,8 +77,19 @@
 
 (defn wallet-tab
   []
-  (let [accounts (rf/sub [:wallet/accounts])]
-    [react/scroll-view {:horizontal true}
-     (for [account accounts]
-       ^{:key (:address account)}
-       [wallet-qr-code-item account])]))
+  (let [accounts (rf/sub [:wallet/accounts])
+        width    (rf/sub [:dimensions/window-width])]
+    [rn/view {:style {:flex 1}}
+     [rn/scroll-view
+      {:horizontal                true
+       :deceleration-rate         0.9
+       :snap-to-alignment         "start"
+       :snap-to-interval          (- 500 30)
+       :disable-interval-momentum true
+       :scroll-event-throttle     64
+      }
+      (map-indexed
+       (fn [index account]
+         ^{:key (:address account)}
+         [wallet-qr-code-item account width index])
+       accounts)]]))
