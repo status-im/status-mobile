60bd74c23492b5fa12e871351e22efa57f2544e0
Ibrahem Khalil <vampirekid017@gmail.com>
2024-01-09 14:31:00 +0200

    Add internal-link component for #17116 (#18109)

---
+++ src/quo/components/links/internal_link_card/schema.cljs
@@ -0,0 +1,23 @@
+(ns quo.components.links.internal-link-card.schema)
+
+(def ?schema
+  [:=>
+   [:catn
+    [:props
+     [:map {:closed true}
+      [:title {:optional true} [:maybe :string]]
+      [:description {:optional true} [:maybe :string]]
+      [:channel-name {:optional true} [:maybe :string]]
+      [:loading? {:optional true} [:maybe :boolean]]
+      [:subtitle {:optional true} [:maybe :string]]
+      [:icon {:optional true} [:maybe [:or :string :int]]]
+      [:banner {:optional true} [:maybe [:or :string :int]]]
+      [:type {:optional true} [:maybe :keyword]]
+      [:on-press {:optional true} [:maybe fn?]]
+      [:members-count {:optional true} [:maybe [:or :int :string]]]
+      [:active-members-count {:optional true} [:maybe [:or :int :string]]]
+      [:customization-color {:optional true} [:maybe :schema.common/customization-color]]
+      [:emoji-hash {:optional true} [:maybe :string]]
+      [:size {:optional true} [:maybe :keyword]]
+      [:theme :schema.common/theme]]]]
+   :any])
---
+++ src/status_im/contexts/preview/quo/links/internal_link_card.cljs
@@ -0,0 +1,60 @@
+(ns status-im.contexts.preview.quo.links.internal-link-card
+  (:require
+    [quo.core :as quo]
+    [reagent.core :as reagent]
+    [status-im.common.resources :as resources]
+    [status-im.contexts.preview.quo.preview :as preview]))
+
+(def descriptor
+  [{:key     :size
+    :type    :select
+    :options [{:key :message}
+              {:key :full-page}]}
+   {:type :text
+    :key  :title}
+   {:type :text
+    :key  :subtitle}
+   {:type :text
+    :key  :description}
+   {:type :text
+    :key  :emoji-hash}
+   {:type :boolean
+    :key  :loading?}
+   {:type :number
+    :key  :members-count}
+   {:type :number
+    :key  :active-members-count}
+   {:key     :type
+    :type    :select
+    :options [{:key :community}
+              {:key :channel}
+              {:key :user}]}
+   {:key     :banner
+    :type    :select
+    :options (mapv (fn [k] {:key k})
+                   (keys resources/mock-images))}
+   (preview/customization-color-option)])
+
+(defn view
+  []
+  (let [state (reagent/atom
+               {:title                "Doodles"
+                :description          "Coloring the world with joy • ᴗ •"
+                :members-count        24
+                :emoji-hash           "🌟🚀🐠🌈🏰🔮🦉🐼🍉🎨🚲🌙🍔🌵"
+                :active-members-count 12
+                :loading?             false
+                :customization-color  :purple
+                :banner               :light-blur-background
+                :type                 :user
+                :subtitle             "Web 3.0 Designer @ethstatus - DJ, Producer - Dad - YouTuber"})]
+    (fn []
+      (let [banner (get resources/mock-images (:banner @state) :bored-ape)]
+        [preview/preview-container
+         {:state      state
+          :descriptor descriptor}
+         [quo/internal-link-card
+          (assoc @state
+                 :banner   banner
+                 :icon     (resources/get-mock-image :status-logo)
+                 :on-press #(js/alert "You clicked me!"))]]))))
