504d92fd57616b39d6b453478f32c9df6d48da89
Omar Basem <omarbasem.0101@gmail.com>
2024-01-09 15:12:11 +0400

    Wallet: suggested routes list (#18388)
    * wallet: suggested routes list

--- src/status_im/contexts/wallet/send/routes/view.cljs
+++ src/status_im/contexts/wallet/send/routes/view.cljs
@@ -2,31 +2,53 @@
   (:require
     [quo.core :as quo]
     [react-native.core :as rn]
+    [status-im.contexts.wallet.common.utils :as utils]
     [status-im.contexts.wallet.send.routes.style :as style]
-    [utils.i18n :as i18n]))
+    [utils.i18n :as i18n]
+    [utils.re-frame :as rf]))
+
+(defn route-item
+  [{:keys [amount from-network to-network status]}]
+  [rn/view {:style style/routes-inner-container}
+   [quo/network-bridge
+    {:amount  amount
+     :network from-network
+     :status  status}]
+   [quo/network-link
+    {:shape           :linear
+     :source          from-network
+     :destination     to-network
+     :container-style style/network-link}]
+   [quo/network-bridge
+    {:amount          amount
+     :network         to-network
+     :status          status
+     :container-style {:right 12}}]])
 
 (defn view
-  [{:keys [amount from-network to-network]}]
-  [rn/view {:style style/routes-container}
-   [rn/view {:style style/routes-header-container}
-    [quo/section-label
-     {:section         (i18n/label :t/from-label)
-      :container-style (style/section-label 0)}]
-    [quo/section-label
-     {:section         (i18n/label :t/to-label)
-      :container-style (style/section-label 64)}]]
-   [rn/view {:style style/routes-inner-container}
-    [quo/network-bridge
-     {:amount  amount
-      :network from-network
-      :status  :default}]
-    [quo/network-link
-     {:shape           :linear
-      :source          from-network
-      :destination     to-network
-      :container-style style/network-link}]
-    [quo/network-bridge
-     {:amount          amount
-      :network         to-network
-      :status          :default
-      :container-style {:right 12}}]]])
+  [{:keys [amount routes]}]
+  (let [loading-suggested-routes? (rf/sub [:wallet/wallet-send-loading-suggested-routes?])
+        candidates                (:candidates routes)]
+    (if (and (not loading-suggested-routes?) (not-empty candidates))
+      [rn/flat-list
+       {:data                    candidates
+        :content-container-style style/routes-container
+        :header                  [rn/view {:style style/routes-header-container}
+                                  [quo/section-label
+                                   {:section         (i18n/label :t/from-label)
+                                    :container-style (style/section-label 0)}]
+                                  [quo/section-label
+                                   {:section         (i18n/label :t/to-label)
+                                    :container-style (style/section-label 64)}]]
+        :render-fn               (fn [route]
+                                   [route-item
+                                    {:amount       amount
+                                     :status       :default
+                                     :from-network (utils/id->network (get-in route [:from :chain-id]))
+                                     :to-network   (utils/id->network (get-in route
+                                                                              [:to :chain-id]))}])}]
+      [rn/view {:style style/empty-container}
+       (if loading-suggested-routes?
+         [rn/activity-indicator]
+         (when (not (nil? candidates))
+           [quo/text (i18n/label :t/no-routes-found)]))])))
