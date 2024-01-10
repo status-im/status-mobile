5b8907848678a129cdb3e7273138161a2159ab3e
Milad <mmilad.sanati@gmail.com>
2024-01-10 20:19:24 +0330

    fix lint issues

--- src/status_im/contexts/wallet/effects.cljs
+++ src/status_im/contexts/wallet/effects.cljs
@@ -1,7 +1,7 @@
-(ns status-im.contexts.wallet.effects 
-  (:require    [re-frame.core :as rf]
-               [react-native.share :as share]))
+(ns status-im.contexts.wallet.effects
+  (:require [re-frame.core :as rf]
+            [react-native.share :as share]))
 
 (rf/reg-fx :effects.share/open
-           (fn [content]
-             (share/open content)))
+ (fn [content]
+   (share/open content)))
