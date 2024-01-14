afb11ccc314340a30ccf71695ce6ec52b484ecea
Ibrkhalil <vampirekid017@gmail.com>
2024-01-14 21:44:29 +0200

    Add notes

--- src/status_im/contexts/communities/actions/channel_view_details/view.cljs
+++ src/status_im/contexts/communities/actions/channel_view_details/view.cljs
@@ -18,18 +18,23 @@
        :on-press   #(rf/dispatch [:navigate-back])
        :right-side [{:icon-name :i/options}]}]
      [quo/text-combinations
+      ;; Use style file
       {:container-style                 {:margin-top 12 :margin-horizontal 20}
        :title                           [quo/channel-name {:channel-name chat-name :unlocked? true}]
        :avatar                          emoji
        :title-accessibility-label       :welcome-title
        :description                     description
        :description-accessibility-label :welcome-sub-title}]
+     ;; Use style file
      [rn/view {:style {:padding 20}}
       [rn/view
+       ;; Use style file
        {:style {:height 102}}
        [quo/channel-actions
         {:actions [{:big?          true
+                    ;; Use i18n
                     :label         "Pinned\nMessages"
+                    ;; Use correct color
                     :color         :blue
                     :icon          :i/pin
                     :counter-value pins-count
@@ -37,15 +42,14 @@
                                      (rf/dispatch [:dismiss-keyboard])
                                      (rf/dispatch [:pin-message/show-pins-bottom-sheet
                                                    chat-id]))}
+                   ;; Use i18n
                    {:label    "Mute channel"
                     :color    :blue
                     :icon     (if muted :i/muted :i/activity-center)
                     :on-press (fn []
                                 (if muted
                                   (home.actions/unmute-chat-action chat-id)
-                                  (home.actions/mute-chat-action chat-id
-                                                                 chat-type
-                                                                 muted)))}]}]]]]))
+                                  (home.actions/mute-chat-action chat-id chat-type muted)))}]}]]]]))
 
 (defn internal-view
   [args]
