3e5b33b1814c9b07213d7829504021c6cefff948
Siddarth Kumar <siddarthkay@gmail.com>
2024-01-14 16:50:35 +0530

    chore: bump go to 20 and update patch

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -72,15 +72,21 @@ in {
     version = "15.0";
     allowHigher = true;
   };
-  go = super.go_1_19;
+  go = super.go_1_20;
   clang = super.clang_15;
-  buildGoPackage = super.buildGo119Package;
-  buildGoModule = super.buildGo119Module;
+  buildGoPackage = super.buildGo120Package;
+  buildGoModule = super.buildGo120Module;
   gomobile = (super.gomobile.overrideAttrs (old: {
-    patches = self.fetchurl { # https://github.com/golang/mobile/pull/84
-      url = "https://github.com/golang/mobile/commit/f20e966e05b8f7e06bed500fa0da81cf6ebca307.patch";
-      sha256 = "sha256-TZ/Yhe8gMRQUZFAs9G5/cf2b9QGtTHRSObBFD5Pbh7Y=";
-    };
+    patches = [
+      (self.fetchurl { # https://github.com/golang/mobile/pull/84
+        url = "https://github.com/golang/mobile/commit/f20e966e05b8f7e06bed500fa0da81cf6ebca307.patch";
+        sha256 = "sha256-TZ/Yhe8gMRQUZFAs9G5/cf2b9QGtTHRSObBFD5Pbh7Y=";
+      })
+      (self.fetchurl { # https://github.com/golang/go/issues/58426
+        url = "https://github.com/golang/mobile/commit/406ed3a7b8e44dc32844953647b49696d8847d51.patch";
+        sha256 = "sha256-dqbYukHkQEw8npOkKykOAzMC3ot/Y4DEuh7fE+ptlr8=";
+      })
+    ];
   })).override {
     # FIXME: No Android SDK packages for aarch64-darwin.
     withAndroidPkgs = stdenv.system != "aarch64-darwin";
