7ffff8173ecee7f7901159dc33bf0fea22ed4d78
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2024-01-16 15:14:44 +0530

    Organize composer worklets

--- src/utils/worklets/chat/lightbox.cljs
+++
@@ -1,7 +0,0 @@
-(ns utils.worklets.chat.lightbox)
-
-(def ^:private layout-worklets (js/require "../src/js/worklets/chat/lightbox.js"))
-
-(defn info-layout
-  [input top?]
-  (.infoLayout ^js layout-worklets input top?))
