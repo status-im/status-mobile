ce7d722ed0069afa861dc870afbd714e0cefbedb
Paul Fitzgerald <paulfitz99@gmail.com>
2024-01-18 15:04:33 +0000

    remove unused border radius

--- src/quo/components/share/share_qr_code/view.cljs
+++ src/quo/components/share/share_qr_code/view.cljs
@@ -129,8 +129,7 @@
            profile-picture emoji on-share-press theme]
     :as   props}]
   [linear-gradient/linear-gradient
-   {:colors [(colors/resolve-color customization-color theme 15) :transparent]
-    :style  {:border-radius 16}}
+   {:colors [(colors/resolve-color customization-color theme 15) (colors/resolve-color customization-color theme 10) :transparent]}
    [rn/view {:style style/content-container}
     [rn/view
      {:style style/share-qr-container}
