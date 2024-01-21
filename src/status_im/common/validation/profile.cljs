b8a0e80ee1e47e7227e7e4a3673440df97c6d512
du82 <ducheng0@protonmail.com>
2024-01-20 20:28:08 -0600

    Re-add space

--- src/status_im/common/validation/profile.cljs
+++ src/status_im/common/validation/profile.cljs
@@ -10,7 +10,7 @@
 (def emoji-regex
   #"(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])")
 
-(def status-regex #"^[a-zA-Z0-9\-_\u3400-\u9FFF]+$")
+(def status-regex #"^[a-zA-Z0-9\-_\u3400-\u9FFF ]+$")
 
 (def common-names ["Ethereum" "Bitcoin"])
 
