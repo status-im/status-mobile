c07f4d781cbe3369bfa1a9d5a6589bcc051b0ae5
Paul Fitzgerald <paulfitz99@gmail.com>
2024-01-23 19:18:18 +0000

    androi

--- src/status_im/contexts/shell/share/wallet/view.cljs
+++ src/status_im/contexts/shell/share/wallet/view.cljs
@@ -68,7 +68,6 @@
             :networks            @selected-networks
             :on-share-press      #(share-action qr-url share-title)
             :profile-picture     nil
-            :unblur-on-android?  true
             :full-name           (:name account)
             :customization-color (:color account)
             :emoji               (:emoji account)
