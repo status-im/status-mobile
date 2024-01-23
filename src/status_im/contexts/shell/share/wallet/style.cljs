a32e50f983d82ea8fe3f86bc6405559b62cbf571
Paul Fitzgerald <paulfitz99@gmail.com>
2024-01-23 09:33:47 +0000

    address comments

--- src/status_im/contexts/shell/share/wallet/style.cljs
+++ src/status_im/contexts/shell/share/wallet/style.cljs
@@ -3,12 +3,12 @@
     [legacy.status-im.ui.components.colors :as colors]))
 
 (defn indicator-wrapper-style
-  [is-active]
+  [active?]
   {:width            8
    :height           8
    :border-radius    4
    :background-color colors/white
-   :opacity          (if is-active 1.0 0.5)})
+   :opacity          (if active? 1.0 0.5)})
 
 (def indicator-list-style
   {:display         :flex
