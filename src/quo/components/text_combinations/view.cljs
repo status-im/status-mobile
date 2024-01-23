05a0495cc03e4875ac6a068baba7225986bd291d
Ibrkhalil <vampirekid017@gmail.com>
2024-01-23 14:11:24 +0200

    Improvements

--- src/quo/components/text_combinations/view.cljs
+++ src/quo/components/text_combinations/view.cljs
@@ -7,16 +7,30 @@
     [react-native.core :as rn]))
 
 (defn icon
-  [source size]
-  [rn/image
-   {:source (if (string? source)
-              {:uri source}
-              source)
-    :style  {:border-radius 50
-             :border-width  0
-             :border-color  :transparent
-             :width         size
-             :height        size}}])
+  [source size emoji? emoji-background-color]
+  (if emoji?
+    [rn/view
+     {:style {:border-radius    50
+              :border-width     0
+              :border-color     :transparent
+              :width            size
+              :height           size
+              :justify-content  :center
+              :align-items      :center
+              :background-color emoji-background-color}}
+     [text/text
+      {:style {:margin-left 1
+               :margin-top  1}}
+      source]]
+    [rn/image
+     {:source (if (string? source)
+                {:uri source}
+                source)
+      :style  {:border-radius 50
+               :border-width  0
+               :border-color  :transparent
+               :width         size
+               :height        size}}]))
 
 (defn view-internal
   [{:keys [container-style
@@ -29,8 +43,14 @@
            button-icon
            button-on-press
            customization-color
+<<<<<<< HEAD
            emoji-hash]
     :or   {title-number-of-lines 1}}]
+=======
+           emoji-hash
+           emoji
+           emoji-background-color]}]
+>>>>>>> 5c0581223 (Improvements)
   [rn/view {:style container-style}
    [rn/view
     {:style {:flex-direction  :row
@@ -38,7 +58,10 @@
     [rn/view {:style style/title-container}
      (when avatar
        [rn/view {:style style/avatar-container}
-        [icon avatar 32]])
+        [icon avatar 32 nil nil]])
+     (when emoji
+       [rn/view {:style style/avatar-container}
+        [icon emoji 32 true emoji-background-color]])
      [text/text
       {:accessibility-label title-accessibility-label
        :weight              :semi-bold
--- src/status_im/contexts/communities/actions/channel_view_details/view.cljs
+++ src/status_im/contexts/communities/actions/channel_view_details/view.cljs
@@ -1,10 +1,12 @@
 (ns status-im.contexts.communities.actions.channel-view-details.view
   (:require [quo.core :as quo]
+            [quo.foundations.colors :as colors]
             [quo.theme :as quo.theme]
             [react-native.core :as rn]
             [status-im.common.contact-list-item.view :as contact-list-item]
             [status-im.common.contact-list.view :as contact-list]
             [status-im.common.home.actions.view :as home.actions]
+            [status-im.contexts.communities.actions.channel-view-details.style :as style]
             [utils.i18n :as i18n]
             [utils.re-frame :as rf]))
 
@@ -16,10 +18,10 @@
         [primary-name secondary-name] (rf/sub [:contacts/contact-two-names-by-identity public-key])
         {:keys [ens-verified added?]} (rf/sub [:contacts/contact-by-address public-key])]
     [contact-list-item/contact-list-item
-     {:on-press       #(rf/dispatch [:chat.ui/show-profile public-key])
-      :on-long-press  show-profile-actions
-      :accessory      {:type     :options
-                       :on-press show-profile-actions}}
+     {:on-press      #(rf/dispatch [:chat.ui/show-profile public-key])
+      :on-long-press show-profile-actions
+      :accessory     {:type     :options
+                      :on-press show-profile-actions}}
      {:primary-name   primary-name
       :secondary-name secondary-name
       :public-key     public-key
@@ -27,80 +29,85 @@
       :added?         added?}
      {:public-key public-key}]))
 
-(defn footer []
-  [rn/view {:style {:margin-top 8}}])
+(defn footer
+  []
+  [rn/view {:style style/footer}])
 
 (defn members
-  [{:keys [theme chat-id]}]
-  (let [{:keys [community-id]} (rf/sub [:get-screen-params])
-        items                  (rf/sub [:communities/sorted-community-members-section-list
-                                        community-id chat-id])]
-    [rn/section-list
-     {:key-fn                            :public-key
-      :content-container-style           {:padding-bottom 20}
-      :get-item-layout                   (fn [_ index]
-                                           #js
-                                            {:length 200
-                                             :offset (* 200 index)
-                                             :index  index})
-      :content-inset-adjustment-behavior :never
-      :sections                          items
-      :sticky-section-headers-enabled    false
-      :render-section-header-fn          contact-list/contacts-section-header
-      :render-section-footer-fn          footer
-      :render-fn                         contact-item-render
-      :scroll-event-throttle             8}]))
+  [items]
+  [rn/section-list
+   {:key-fn                            :public-key
+    :content-container-style           {:padding-bottom 20}
+    :get-item-layout                   (fn [_ index]
+                                         #js
+                                          {:length 200
+                                           :offset (* 200 index)
+                                           :index  index})
+    :content-inset-adjustment-behavior :never
+    :sections                          items
+    :sticky-section-headers-enabled    false
+    :render-section-header-fn          contact-list/contacts-section-header
+    :render-section-footer-fn          footer
+    :render-fn                         contact-item-render
+    :scroll-event-throttle             8}])
 
 (defn f-view
-  [{:keys [theme chat-id]}]
+  [{:keys [chat-id community-id theme]}]
   (let [{:keys [description chat-name emoji muted
-                chat-type]} (rf/sub [:chats/chat-by-id
-                                     chat-id])
-        pins-count          (rf/sub [:chats/pin-messages-count chat-id])]
-    [rn/view {:style {:flex 1}}
-     [quo/page-nav
-      {:background :blur
-       :icon-name  :i/arrow-left
-       :on-press   #(rf/dispatch [:navigate-back])
-       :right-side [{:icon-name :i/options}]}]
-     [quo/text-combinations
-      ;; Use style file
-      {:container-style                 {:margin-top 12 :margin-horizontal 20}
-       :title                           [quo/channel-name {:channel-name chat-name :unlocked? true}]
-       :avatar                          emoji
-       :title-accessibility-label       :welcome-title
-       :description                     description
-       :description-accessibility-label :welcome-sub-title}]
-     ;; Use style file
-     [rn/view {:style {:padding 20}}
-      [rn/view
-       ;; Use style file
-       {:style {:height 102}}
-       [quo/channel-actions
-        {:actions [{:big?          true
-                    :label         (i18n/label :t/pinned-messages-2)
-                    ;; Use correct color
-                    :color         :blue
-                    :icon          :i/pin
-                    :counter-value pins-count
-                    :on-press      (fn []
-                                     (rf/dispatch [:dismiss-keyboard])
-                                     (rf/dispatch [:pin-message/show-pins-bottom-sheet
-                                                   chat-id]))}
-                   {:label    (i18n/label :t/mute-channel)
-                    :color    :blue
-                    :icon     (if muted :i/muted :i/activity-center)
-                    :on-press (fn []
-                                (if muted
-                                  (home.actions/unmute-chat-action chat-id)
-                                  (home.actions/mute-chat-action chat-id chat-type muted)))}]}]]]
-     [members
-      {:theme      theme
-       :chat-id chat-id}]]))
+                chat-type]
+         :as   chat}  (rf/sub [:chats/chat-by-id
+                               chat-id])
+        pins-count    (rf/sub [:chats/pin-messages-count chat-id])
+        items         (rf/sub [:communities/sorted-community-members-section-list
+                               community-id])
+        profile-color (rf/sub [:profile/customization-color])]
+    (fn [chat-id]
+      [rn/view {:style {:flex 1}}
+       [quo/page-nav
+        {:background :blur
+         :icon-name  :i/arrow-left
+         :on-press   #(rf/dispatch [:navigate-back])
+         :right-side [{:icon-name :i/options
+                       :on-press  #(rf/dispatch [:show-bottom-sheet
+                                                 {:content (fn [] [home.actions/chat-actions
+                                                                   chat
+                                                                   false
+                                                                   true])}])}]}]
+       [quo/text-combinations
+        {:container-style                 style/text-combinations
+         :title                           [quo/channel-name
+                                           {:channel-name chat-name
+                                            :unlocked?    true}]
+         :emoji                           (when (seq emoji) emoji)
+         :emoji-background-color          (colors/resolve-color profile-color theme 10)
+         :title-accessibility-label       :welcome-title
+         :description                     description
+         :description-accessibility-label :welcome-sub-title}]
+       [rn/view {:style style/wrapper}
+        [rn/view
+         {:style style/channel-actions-wrapper}
+         [quo/channel-actions
+          {:actions [{:big?          true
+                      :label         (i18n/label :t/pinned-messages-2)
+                      :color         profile-color
+                      :icon          :i/pin
+                      :counter-value pins-count
+                      :on-press      (fn []
+                                       (rf/dispatch [:dismiss-keyboard])
+                                       (rf/dispatch [:pin-message/show-pins-bottom-sheet
+                                                     chat-id]))}
+                     {:label    (i18n/label :t/mute-channel)
+                      :color    profile-color
+                      :icon     (if muted :i/muted :i/activity-center)
+                      :on-press (fn []
+                                  (if muted
+                                    (home.actions/unmute-chat-action chat-id)
+                                    (home.actions/mute-chat-action chat-id chat-type muted)))}]}]]]
+       [members items profile-color]])))
 
-(defn internal-view
+(defn view-internal
   [args]
   (let [screen-params (rf/sub [:get-screen-params :view-channel-members-and-details])]
     [:f> f-view (merge args screen-params)]))
 
-(def view (quo.theme/with-theme internal-view))
+(defn view [] (quo.theme/with-theme view-internal))
