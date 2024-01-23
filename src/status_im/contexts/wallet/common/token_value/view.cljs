dae0b86de475179972c4416d1f3ef601821da270
Milad <mmilad.sanati@gmail.com>
2024-01-23 17:58:24 +0330

    add functionality

--- src/status_im/contexts/wallet/common/token_value/view.cljs
+++ src/status_im/contexts/wallet/common/token_value/view.cljs
@@ -41,10 +41,12 @@
 
 (defn view
   [item]
-  [quo/token-value
-   (merge item
-          {:on-long-press
-           #(rf/dispatch
-             [:show-bottom-sheet
-              {:content       (fn [] [token-value-drawer item])
-               :selected-item (fn [] [quo/token-value item])}])})])
+  (let [{:keys [watch-only?]} (rf/sub [:wallet/current-viewing-account])
+        on-long-press         (when (not watch-only?)
+                                #(rf/dispatch
+                                  [:show-bottom-sheet
+                                   {:content       (fn [] [token-value-drawer item])
+                                    :selected-item (fn [] [quo/token-value item])}]))]
+    [quo/token-value
+     (merge item
+            {:on-long-press on-long-press})]))
