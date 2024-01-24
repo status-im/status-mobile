e603202a486c8b4ef942a906d224e8caf0387880
Paul Fitzgerald <paulfitz99@gmail.com>
2024-01-24 14:45:30 +0000

    lint

--- src/quo/components/list_items/network_list/component_spec.cljs
+++ src/quo/components/list_items/network_list/component_spec.cljs
@@ -18,27 +18,27 @@
 
 
 (h/describe "List items: Network List"
-            (h/test "default state explicit"
-                    (render [network-list/view props])
-                    (h/is-truthy (h/get-by-text "Mainnet")))
+  (h/test "default state explicit"
+    (render [network-list/view props])
+    (h/is-truthy (h/get-by-text "Mainnet")))
 
-            (h/test "Pressed state"
-                    (h/render [network-list/view props])
-                    (h/fire-event :on-press-in (h/get-by-label-text ::network-list))
-                    (h/wait-for #(h/has-style (h/query-by-label-text ::network-list)
-                                              {:backgroundColor (colors/resolve-color :blue :light 5)})))
+  (h/test "Pressed state"
+    (h/render [network-list/view props])
+    (h/fire-event :on-press-in (h/get-by-label-text ::network-list))
+    (h/wait-for #(h/has-style (h/query-by-label-text ::network-list)
+                              {:backgroundColor (colors/resolve-color :blue :light 5)})))
 
-            (h/test "Active state"
-                    (h/render [network-list/view (assoc props :state :active)])
-                    (h/has-style (h/query-by-label-text ::network-list)
-                                 {:backgroundColor (colors/resolve-color :blue :light 10)}))
+  (h/test "Active state"
+    (h/render [network-list/view (assoc props :state :active)])
+    (h/has-style (h/query-by-label-text ::network-list)
+                 {:backgroundColor (colors/resolve-color :blue :light 10)}))
 
-            (h/test "Call on-press"
-                    (let [on-press (h/mock-fn)]
-                      (h/render [network-list/view (assoc props :on-press on-press)])
-                      (h/fire-event :on-press (h/get-by-label-text ::network-list))
-                      (h/was-called on-press)))
+  (h/test "Call on-press"
+    (let [on-press (h/mock-fn)]
+      (h/render [network-list/view (assoc props :on-press on-press)])
+      (h/fire-event :on-press (h/get-by-label-text ::network-list))
+      (h/was-called on-press)))
 
-            (h/test "Empty props"
-                    (h/render [network-list/view {}])
-                    (h/is-truthy (h/get-by-label-text ::network-list-item))))
+  (h/test "Empty props"
+    (h/render [network-list/view {}])
+    (h/is-truthy (h/get-by-label-text ::network-list-item))))
