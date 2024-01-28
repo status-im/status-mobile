733e841d27982b2c967cfde61b87acc56b577bfd
Ibrkhalil <vampirekid017@gmail.com>
2024-01-28 19:05:25 +0200

    Bring back enable-notifications onboarding screen static image

--- src/status_im/common/resources.cljs
+++ src/status_im/common/resources.cljs
@@ -26,7 +26,8 @@
    :discover                      (js/require "../resources/images/ui2/discover.png")
    :invite-friends                (js/require "../resources/images/ui2/invite-friends.png")
    :transaction-progress          (js/require "../resources/images/ui2/transaction-progress.png")
-   :welcome-illustration          (js/require "../resources/images/ui2/welcome_illustration.png")})
+   :welcome-illustration          (js/require "../resources/images/ui2/welcome_illustration.png")
+   :notifications                 (js/require "../resources/images/ui2/notifications.png")})
 
 (def ui-themed
   {:angry-man
