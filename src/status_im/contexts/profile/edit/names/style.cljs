d9830de727d21d9bed688582093a3549d57b043e
Sean Hagstrom <sean@seanhagstrom.com>
2024-01-29 17:17:51 +0000

    wip: add initial names management view and styles

---
+++ src/status_im/contexts/profile/edit/names/style.cljs
@@ -0,0 +1,19 @@
+(ns status-im.contexts.profile.edit.names.style)
+
+(defn page-wrapper
+  [insets]
+  {:padding-top        (:top insets)
+   :padding-bottom     (:bottom insets)
+   :padding-horizontal 1
+   :flex               1})
+
+(def screen-container
+  {:flex               1
+   :padding-top        14
+   :padding-horizontal 20
+   :justify-content    :space-between})
+
+(def item-container
+  {:background-color "rgba(255, 255, 255, 0.05)"
+   :border-radius    16
+   :margin-bottom    8})
---
+++ src/status_im/contexts/profile/edit/names/view.cljs
@@ -0,0 +1,53 @@
+(ns status-im.contexts.profile.edit.names.view
+  (:require
+   [quo.core :as quo]
+   [quo.theme :as quo.theme]
+   [react-native.core :as rn]
+   [react-native.safe-area :as safe-area]
+   [status-im.contexts.profile.edit.names.style :as style]
+   [utils.i18n :as i18n]
+   [utils.re-frame :as rf]))
+
+(defn- name-item
+  [{:keys [name ens-name? ]}]
+  ^{:key name}
+  [quo/settings-item
+   {:title           name
+    :image-props     (if ens-name? :i/chevron-right :i/edit)
+    :image           :icon
+    :blur?           true
+    :image-right     true
+    :on-press        #(rf/dispatch [:open-modal (if ens-name? :edit-ens :edit-name) name])
+    :action          :selector
+    :action-props    {:type :radio :blur? true}
+    :container-style style/item-container}])
+
+(defn- header-view []
+  [rn/view {:style {:margin-bottom 8}}
+   [quo/text-combinations {:title (i18n/label :t/name)}]])
+
+(defn- internal-view
+  [_theme]
+  (let [insets             (safe-area/get-insets)
+        profile-user-names (rf/sub [:profile/profile-user-names])]
+    (fn []
+      [quo/overlay
+       {:type            :shell
+        :container-style (style/page-wrapper insets)}
+       [quo/page-nav
+        {:key        :header
+         :background :blur
+         :icon-name  :i/arrow-left
+         :on-press   #(rf/dispatch [:navigate-back])}]
+       [rn/view
+        {:key  :content
+         :style style/screen-container}
+        [rn/flat-list
+         {:key                             :list
+          :header                          (header-view)
+          :data                            profile-user-names
+          :key-fn                          :name
+          :shows-vertical-scroll-indicator false
+          :render-fn                       name-item}]]])))
+
+(def view (quo.theme/with-theme internal-view))
