5311eee1598038fb09db63d09e8e3412bbc16cee
Jamie Caprani <jamiecaprani@gmail.com>
2024-01-29 18:51:36 +0000

    feat: add basic infrastructure for contract tests

--- src/status_im/integration_test/wallet_test.cljs
+++ src/status_im/integration_test/wallet_test.cljs
@@ -1,7 +1,6 @@
 (ns status-im.integration-test.wallet-test
   (:require
-    [cljs.test :refer [is]]
-    [clojure.string :as string]
+    [cljs.test :refer [deftest is]]
     [day8.re-frame.test :as rf-test]
     legacy.status-im.events
     [legacy.status-im.multiaccounts.logout.core :as logout]
@@ -12,40 +11,21 @@
     status-im.subs.root
     [test-helpers.integration :as h]))
 
-;; Workaround to skip test. Switch to `deftest` when test is fixed.
-(defn create-wallet-account-test
-  []
+(defn assert-accounts-contract
+  [result]
+  (is (true? (some :wallet result)))
+  (is (true? (some :chat result)))
+  (is (= 2 (count result))))
+
+(deftest check-wallet-accounts-contract
   (h/log-headline :create-wallet-account-test)
   (rf-test/run-test-async
    (h/with-app-initialized
     (h/with-account
-     (h/create-new-account!)
+     (rf/dispatch [:integration-test/dispatch-rpc
+                   {:rpc-endpoint "accounts_getAccounts"
+                    :check-result assert-accounts-contract}])
      (rf-test/wait-for
-       [:wallet-legacy.accounts/account-stored]
-       (h/assert-new-account-created)
+       [:integration-test/rpc-checked]
        (h/logout)
        (rf-test/wait-for [::logout/logout-method]))))))
-
-;; Workaround to skip test. Switch to `deftest` when test is fixed.
-(defn back-up-seed-phrase-test
-  []
-  (h/log-headline :back-up-seed-phrase-test)
-  (rf-test/run-test-async
-   (h/with-app-initialized
-    (h/with-account
-     (rf/dispatch-sync [:set-in [:my-profile/seed :step] :12-words]) ; display seed phrase to user
-     (rf/dispatch-sync [:my-profile/enter-two-random-words]) ; begin prompting user for seed words
-     (let [{:keys [mnemonic]} @(rf/subscribe [:profile/profile])
-           seed               @(rf/subscribe [:my-profile/seed])
-           word1              (second (:first-word seed))
-           word2              (second (:second-word seed))]
-       (is (= 12 (count (string/split mnemonic #" "))))
-       (rf/dispatch-sync [:set-in [:my-profile/seed :word] word1])
-       (rf/dispatch-sync [:my-profile/set-step :second-word])
-       (rf/dispatch-sync [:set-in [:my-profile/seed :word] word2])
-       (rf/dispatch [:my-profile/finish])
-       (rf-test/wait-for
-         [:my-profile/finish-success]
-         (is (nil? @(rf/subscribe [:mnemonic]))) ; assert seed phrase has been removed
-         (h/logout)
-         (rf-test/wait-for [::logout/logout-method])))))))
