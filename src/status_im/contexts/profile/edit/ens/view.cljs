0717ba11d261e62a1dce8b4e469897df7d78e696
Sean Hagstrom <sean@seanhagstrom.com>
2024-01-30 13:24:24 +0000

    tidy: refactor profile edit ens view

--- src/status_im/contexts/profile/edit/ens/view.cljs
+++ src/status_im/contexts/profile/edit/ens/view.cljs
@@ -11,26 +11,25 @@
   []
   (let [insets    (safe-area/get-insets)
         user-name (rf/sub [:get-screen-params :edit-ens])]
-    (fn []
-      [quo/overlay
-       {:type            :shell
-        :container-style (style/page-wrapper insets)}
-       [quo/page-nav
-        {:key        :header
-         :background :blur
-         :icon-name  :i/close
-         :on-press   #(rf/dispatch [:navigate-back])}]
-       [rn/view
-        {:key   :content
-         :style style/screen-container}
-        [rn/view {:style {:gap 22}}
-         [quo/text-combinations {:title user-name}]]
-        [rn/view {:style style/button-wrapper}
-         [quo/button
-          {:type      :danger
-           :blur?     true
-           :icon-left :i/delete
-           :on-press  (fn []
-                        (rf/dispatch [:ens/remove-ens user-name])
-                        (rf/dispatch [:navigate-back]))}
-          (i18n/label :t/remove)]]]])))
+    [quo/overlay
+     {:type            :shell
+      :container-style (style/page-wrapper insets)}
+     [quo/page-nav
+      {:key        :header
+       :background :blur
+       :icon-name  :i/close
+       :on-press   #(rf/dispatch [:navigate-back])}]
+     [rn/view
+      {:key   :content
+       :style style/screen-container}
+      [rn/view {:style {:gap 22}}
+       [quo/text-combinations {:title user-name}]]
+      [rn/view {:style style/button-wrapper}
+       [quo/button
+        {:type      :danger
+         :blur?     true
+         :icon-left :i/delete
+         :on-press  (fn []
+                      (rf/dispatch [:ens/remove-ens user-name])
+                      (rf/dispatch [:navigate-back]))}
+        (i18n/label :t/remove)]]]]))
