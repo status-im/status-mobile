ac8f652079c5c0a2676a46ecbcc075b966245660
Ibrkhalil <vampirekid017@gmail.com>
2024-01-31 12:53:43 +0200

    Refactors

--- src/status_im/subs/general.cljs
+++ src/status_im/subs/general.cljs
@@ -19,6 +19,11 @@
      my-status-update
      (get status-updates public-key))))
 
+(defn online?
+  [visibility-status-type]
+  (or (= visibility-status-type constants/visibility-status-automatic)
+      (= visibility-status-type constants/visibility-status-always-online)))
+
 (re-frame/reg-sub
  :visibility-status-updates/online?
  (fn [[_ public-key]]
