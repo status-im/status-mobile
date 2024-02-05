408ad9da58b8f9bac2d086c760d1a66313745852
Omar Basem <omarbasem.0101@gmail.com>
2024-02-05 09:54:03 +0400

    feat: bridge assets

--- src/status_im/config.cljs
+++ src/status_im/config.cljs
@@ -169,3 +169,10 @@
 
 (def community-accounts-selection-enabled? (enabled? (get-config :ACCOUNT_SELECTION_ENABLED "0")))
 (def fetch-messages-enabled? (enabled? (get-config :FETCH_MESSAGES_ENABLED "1")))
+<<<<<<< HEAD
+=======
+
+(def wallet-feature-flags
+  {:edit-default-keypair false
+   :bridge-token         true})
+>>>>>>> a622d61d8 (feat: bridge assets)
---
+++ src/status_im/contexts/wallet/common/asset_list/view.cljs
@@ -0,0 +1,37 @@
+(ns status-im.contexts.wallet.common.asset-list.view
+  (:require
+    [quo.core :as quo]
+    [react-native.core :as rn]
+    [status-im.contexts.wallet.common.utils :as utils]
+    [utils.re-frame :as rf]))
+
+(defn- asset-component
+  [token _ _ {:keys [currency currency-symbol on-token-press]}]
+  (let [token-units      (utils/total-token-units-in-all-chains token)
+        crypto-formatted (utils/get-standard-crypto-format token token-units)
+        fiat-value       (utils/total-token-fiat-value currency token)
+        fiat-formatted   (utils/get-standard-fiat-format crypto-formatted currency-symbol fiat-value)]
+    [quo/token-network
+     {:token       (:symbol token)
+      :label       (:name token)
+      :token-value (str crypto-formatted " " (:symbol token))
+      :fiat-value  fiat-formatted
+      :networks    (:networks token)
+      :on-press    #(on-token-press token)}]))
+
+(defn view
+  [{:keys [search-text on-token-press]}]
+  (let [filtered-tokens (rf/sub [:wallet/tokens-filtered search-text])
+        currency        (rf/sub [:profile/currency])
+        currency-symbol (rf/sub [:profile/currency-symbol])]
+    [rn/flat-list
+     {:data                         filtered-tokens
+      :render-data                  {:currency        currency
+                                     :currency-symbol currency-symbol
+                                     :on-token-press  on-token-press}
+      :style                        {:flex 1}
+      :content-container-style      {:padding-horizontal 8}
+      :keyboard-should-persist-taps :handled
+      :key-fn                       :id
+      :on-scroll-to-index-failed    identity
+      :render-fn                    asset-component}]))
