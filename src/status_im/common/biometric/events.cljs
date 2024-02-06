35ef1d007f751cc76b68b4cf0d35e8f62be0e101
Lungu Cristian <lungucristian95@gmail.com>
2024-02-06 16:01:56 +0200

    test(biometrics): tests & schemas for events
    Signed-off-by: Cristian Lungu <lungucristian95@gmail.com>

--- src/status_im/common/biometric/events.cljs
+++ src/status_im/common/biometric/events.cljs
@@ -7,7 +7,8 @@
     [status-im.constants :as constants]
     [taoensso.timbre :as log]
     [utils.i18n :as i18n]
-    [utils.re-frame :as rf]))
+    [utils.re-frame :as rf]
+    [utils.security.core :as security]))
 
 (rf/reg-fx
  :biometric/get-supported-type
@@ -20,10 +21,23 @@
             (.then (fn [type]
                      (rf/dispatch [:biometric/set-supported-type type])))))))))
 
-(rf/reg-event-fx
- :biometric/set-supported-type
- (fn [{:keys [db]} [supported-type]]
-   {:db (assoc db :biometric/supported-type supported-type)}))
+(defn set-supported-type
+  [{:keys [db]} [supported-type]]
+  {:db (assoc db :biometric/supported-type supported-type)})
+
+(schema/=> set-supported-type
+  [:=>
+   [:catn
+    [:cofx :schema.re-frame/cofx]
+    [:args
+     [:tuple
+      [:enum
+       constants/biometrics-type-android
+       constants/biometrics-type-face-id
+       constants/biometrics-type-touch-id]]]]
+   :schema.re-frame/event-fx])
+
+(rf/reg-event-fx :biometric/set-supported-type set-supported-type)
 
 (defn show-message
   [_ [code]]
@@ -61,26 +75,59 @@
                  (when on-fail
                    (on-fail err)))))))
 
-(rf/reg-event-fx
- :biometric/authenticate
- (fn [_ [opts]]
-   {:biometric/authenticate opts}))
+(defn authenticate
+  [_ [opts]]
+  {:fx [[:biometric/authenticate opts]]})
 
-(rf/reg-event-fx
- :biometric/enable
- (fn [{:keys [db]} [password]]
-   (let [key-uid (get-in db [:profile/profile :key-uid])]
-     {:db       (assoc db :auth-method constants/auth-method-biometric)
-      :dispatch [:keychain/save-password-and-auth-method
-                 {:key-uid         key-uid
-                  :masked-password password}]})))
+(schema/=> authenticate
+  [:=>
+   [:catn
+    [:cofx :schema.re-frame/cofx]
+    [:args
+     [:tuple
+      [:map {:closed true}
+       [:on-success {:optional true} fn?]
+       [:on-fail {:optional true} fn?]
+       [:prompt-message {:optional true} :string]]]]]
+   :schema.re-frame/event-fx])
 
-(rf/reg-event-fx
- :biometric/disable
- (fn [{:keys [db]}]
-   (let [key-uid (get-in db [:profile/profile :key-uid])]
-     {:db                           (assoc db :auth-method constants/auth-method-none)
-      :keychain/clear-user-password key-uid})))
+(rf/reg-event-fx :biometric/authenticate authenticate)
+
+(defn enable-biometrics
+  [{:keys [db]} [password]]
+  (let [key-uid (get-in db [:profile/profile :key-uid])]
+    (println "inside: " password)
+    {:db (assoc db :auth-method constants/auth-method-biometric)
+     :fx [[:dispatch
+           [:keychain/save-password-and-auth-method
+            {:key-uid         key-uid
+             :masked-password password}]]]}))
+
+(schema/=> enable-biometrics
+  [:=>
+   [:catn
+    [:cofx :schema.re-frame/cofx]
+    [:args
+     [:tuple
+      [:fn {:error/message "Should be an instance of security/MaskedData"}
+       (fn [pw] (instance? security/MaskedData pw))]]]]
+   :schema.re-frame/event-fx])
+
+(rf/reg-event-fx :biometric/enable enable-biometrics)
+
+(defn disable-biometrics
+  [{:keys [db]}]
+  (let [key-uid (get-in db [:profile/profile :key-uid])]
+    {:db (assoc db :auth-method constants/auth-method-none)
+     :fx [[:keychain/clear-user-password key-uid]]}))
+
+(schema/=> disable-biometrics
+  [:=>
+   [:catn
+    [:cofx :schema.re-frame/cofx]]
+   :schema.re-frame/event-fx])
+
+(rf/reg-event-fx :biometric/disable disable-biometrics)
 
 (rf/reg-fx
  :biometric/check-if-available
