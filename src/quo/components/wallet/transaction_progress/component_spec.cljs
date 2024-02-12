abe0342be0a561f88e7da40aa39d88552704273c
Shivek Khurana <khuranashivek@outlook.com>
2024-02-12 14:44:40 +0000

    🚟 Add schema batch 2 (#18696)
    * 🚟 Add schema batch 2
    
    * ⏬ Add maybe and optionals
    
    * 🧑‍⚖️ Make theme a required prop
    
    * 🍙 Fix misplaced square brackets that broke spec
    
    * 🎨 Assume default theme and fix tests
    
    - Fixes #18734
    
    * ⬆️ Update schema and rebase
    
    * 🧪 Update tests
    
    * 🆙 Update progress bar value to be string or int
    
    * 🔩 Tighten schema

--- src/quo/components/wallet/transaction_progress/component_spec.cljs
+++ src/quo/components/wallet/transaction_progress/component_spec.cljs
@@ -2,8 +2,6 @@
   (:require [quo.core :as quo]
             [test-helpers.component :as h]))
 
-(def ^:private theme :light)
-
 (defn- get-test-data
   [{:keys [state network]
     :or   {state :pending network :mainnet}}]
@@ -31,195 +29,206 @@
 
 (h/describe "Transaction Progress"
   (h/test "component renders without props"
-    (h/render-with-theme-provider [quo/transaction-progress] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is pending and network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:network :optimism-arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :optimism-arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is sending and network is optimism-arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :sending
-                                                   :network :optimism-arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :sending
+                      :network :optimism-arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is confirmed and network is optimism-arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :confirmed
-                                                   :network :optimism-arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :confirmed
+                      :network :optimism-arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is finalising and network is optimism-arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :finalising
-                                                   :network :optimism-arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :finalising
+                      :network :optimism-arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is finalized and network is optimism-arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :finalized
-                                                   :network :optimism-arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :finalized
+                      :network :optimism-arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is error and network is optimism-arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :error
-                                                   :network :optimism-arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :error
+                      :network :optimism-arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is sending and network is optimism"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :sending
-                                                   :network :optimism})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :sending
+                      :network :optimism})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is confirmed and network is optimism"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :confirmed
-                                                   :network :optimism})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :confirmed
+                      :network :optimism})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is finalising and network is optimism"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :finalising
-                                                   :network :optimism})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :finalising
+                      :network :optimism})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is finalized and network is optimism"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :finalized
-                                                   :network :optimism})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :finalized
+                      :network :optimism})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is error and network is optimism"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :error
-                                                   :network :optimism})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :error
+                      :network :optimism})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is sending and network is arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :sending
-                                                   :network :arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :sending
+                      :network :arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is confirmed and network is arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :confirmed
-                                                   :network :arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :confirmed
+                      :network :arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is finalising and network is arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :finalising
-                                                   :network :arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :finalising
+                      :network :arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is finalized and network is arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :finalized
-                                                   :network :arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :finalized
+                      :network :arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is error and network is arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:state   :error
-                                                   :network :arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress
+      (get-test-data {:state   :error
+                      :network :arbitrum})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is pending and network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is sending and network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:state :sending})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:state :sending})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is confirmed and network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:state :confirmed})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:state :confirmed})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is finalising and network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:state :finalising})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:state :finalising})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is finalized and network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:state :finalized})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:state :finalized})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "component renders when state is error and network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:state :error})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:state :error})])
     (h/is-truthy (h/get-by-label-text :transaction-progress)))
 
   (h/test "mainnet progress box is visible network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {})])
     (h/is-truthy (h/get-by-label-text :mainnet-progress-box)))
 
   (h/test "arbitrum-optimism progress box is visible network is optimism-arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:network :optimism-arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :optimism-arbitrum})])
     (h/is-truthy (h/get-all-by-label-text :progress-box)))
 
   (h/test "arbitrum progress box is visible network is arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:network :arbitrum})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :arbitrum})])
     (h/is-truthy (h/get-all-by-label-text :progress-box)))
 
   (h/test "optimism progress box is visible network is optimism"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:network :optimism})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :optimism})])
     (h/is-truthy (h/get-all-by-label-text :progress-box)))
 
   (h/test "title is visible network is optimism-arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:network :optimism-arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :optimism-arbitrum})])
     (h/is-truthy (h/get-by-text "Title")))
 
   (h/test "title is visible network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {})])
     (h/is-truthy (h/get-by-text "Title")))
 
   (h/test "title is visible network is optimism"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:network :optimism})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :optimism})])
     (h/is-truthy (h/get-by-text "Title")))
 
   (h/test "title is visible network is arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:network :arbitrum})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :arbitrum})])
     (h/is-truthy (h/get-by-text "Title")))
 
   (h/test "context tag is visible network is optimism-arbitrum"
-    (h/render-with-theme-provider [quo/transaction-progress
-                                   (get-test-data {:network :optimism-arbitrum})]
-                                  theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :optimism-arbitrum})])
     (h/is-truthy (h/get-by-label-text :context-tag)))
 
   (h/test "context tag is visible network is mainnet"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {})])
     (h/is-truthy (h/get-by-label-text :context-tag)))
 
   (h/test "context tag is visible network is optimism"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:network :optimism})] theme)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :optimism})])
     (h/is-truthy (h/get-by-label-text :context-tag)))
 
   (h/test "context tag is visible network is optimism"
-    (h/render-with-theme-provider [quo/transaction-progress (get-test-data {:network :arbitrum})] theme)
-    (h/is-truthy (h/get-by-label-text :context-tag)))
-)
+    (h/render-with-theme-provider
+     [quo/transaction-progress (get-test-data {:network :arbitrum})])
+    (h/is-truthy (h/get-by-label-text :context-tag))))
