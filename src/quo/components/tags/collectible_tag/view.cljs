7c1f7c3b60c30f681bdb63c74b19b3f004e9ed4f
Lungu Cristian <lungucristian95@gmail.com>
2024-02-13 12:48:50 +0200

    fix(collectible-tag): handle default size when nil

--- src/quo/components/tags/collectible_tag/view.cljs
+++ src/quo/components/tags/collectible_tag/view.cljs
@@ -29,43 +29,43 @@
   (let [container-width (r/atom 0)
         on-layout       #(->> (oops/oget % :nativeEvent :layout :width)
                               (reset! container-width))]
-    (fn [{:keys [options size blur? theme collectible-img-src collectible-name collectible-id]
-          :or   {size :size-24}}]
-      [rn/view
-       {:on-layout on-layout}
-       [hole-view/hole-view
-        {:holes (if options
-                  [{:x            (- @container-width
-                                     (case size
-                                       :size-24 10
-                                       :size-32 12
-                                       nil))
-                    :y            (case size
-                                    :size-24 -6
-                                    :size-32 -4
-                                    nil)
-                    :width        16
-                    :height       16
-                    :borderRadius 8}]
-                  [])}
-        [rn/view {:style (style/container size options blur? theme)}
-         [rn/image {:style (style/collectible-img size) :source collectible-img-src}]
-         [text/text
-          {:size   :paragraph-2
-           :weight :medium
-           :style  (style/label theme)}
-          collectible-name]
-         [text/text
-          {:size        :paragraph-2
-           :weight      :medium
-           :margin-left 5
-           :style       (style/label theme)}
-          collectible-id]]]
-       (when options
-         [rn/view {:style (style/options-icon size)}
-          [icons/icon (if (= options :hold) :i/hold :i/add-token)
-           {:size     20
-            :no-color true}]])])))
+    (fn [{:keys [options blur? theme collectible-img-src collectible-name collectible-id] :as props}]
+      (let [size (or (:size props) :size-24)]
+        [rn/view
+         {:on-layout on-layout}
+         [hole-view/hole-view
+          {:holes (if options
+                    [{:x            (- @container-width
+                                       (case size
+                                         :size-24 10
+                                         :size-32 12
+                                         nil))
+                      :y            (case size
+                                      :size-24 -6
+                                      :size-32 -4
+                                      nil)
+                      :width        16
+                      :height       16
+                      :borderRadius 8}]
+                    [])}
+          [rn/view {:style (style/container size options blur? theme)}
+           [rn/image {:style (style/collectible-img size) :source collectible-img-src}]
+           [text/text
+            {:size   :paragraph-2
+             :weight :medium
+             :style  (style/label theme)}
+            collectible-name]
+           [text/text
+            {:size        :paragraph-2
+             :weight      :medium
+             :margin-left 5
+             :style       (style/label theme)}
+            collectible-id]]]
+         (when options
+           [rn/view {:style (style/options-icon size)}
+            [icons/icon (if (= options :hold) :i/hold :i/add-token)
+             {:size     20
+              :no-color true}]])]))))
 
 (def view
   (quo.theme/with-theme
