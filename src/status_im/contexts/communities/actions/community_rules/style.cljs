b89b4745d99f7fe94f20c1592f1fc3947e7866d1
Flavio Fraschetti <frasqueti@gmail.com>
2024-02-14 16:40:55 +0000

    adapt space according to platform

--- src/status_im/contexts/communities/actions/community_rules/style.cljs
+++ src/status_im/contexts/communities/actions/community_rules/style.cljs
@@ -1,8 +1,9 @@
 (ns status-im.contexts.communities.actions.community-rules.style)
 
-(def community-rule
+(defn community-rule
+  [rules-bottom-space]
   {:flex               1
    :align-items        :flex-start
    :padding-top        8
-   :margin-bottom      42
+   :margin-bottom      rules-bottom-space
    :padding-horizontal 20})
