72c6beec22c58d07148cb15b3b968f614235d4ed
alwx <alwxndr@gmail.com>
2024-02-14 11:48:07 +0100

    Fixes

--- src/status_im/contexts/shell/qr_reader/view.cljs
+++ src/status_im/contexts/shell/qr_reader/view.cljs
@@ -9,7 +9,9 @@
     [status-im.contexts.communities.events]
     [status-im.contexts.wallet.common.validation :as wallet-validation]
     [utils.debounce :as debounce]
-    [utils.i18n :as i18n]))
+    [utils.ethereum.eip.eip681 :as eip681]
+    [utils.i18n :as i18n]
+    [utils.url :as url]))
 
 (def invalid-qr-toast
   {:type  :negative
@@ -29,9 +31,24 @@
   [scanned-text]
   (wallet-validation/eth-address? scanned-text))
 
+(defn eip681-address?
+  [scanned-text]
+  (-> scanned-text
+      eip681/parse-uri
+      :address
+      boolean))
+
 (defn pairing-qr-code?
   [_]
   false)
+
+(defn wallet-connect-code?
+  [scanned-text]
+  (string/starts-with? scanned-text "wc:"))
+
+(defn url?
+  [scanned-text]
+  (url/url? scanned-text))
 
 (defn load-and-show-profile
   [address]
@@ -53,32 +70,46 @@
 
 (defn on-qr-code-scanned
   [scanned-text]
-  (let [address (extract-id scanned-text)]
-    (cond
-      (text-for-url-path? scanned-text router/community-with-data-path)
-      ;; TODO: https://github.com/status-im/status-mobile/issues/18743
-      nil
+  (cond
+    (text-for-url-path? scanned-text router/community-with-data-path)
+    ;; TODO: https://github.com/status-im/status-mobile/issues/18743
+    nil
 
-      (text-for-url-path? scanned-text router/channel-path)
-      ;; TODO: https://github.com/status-im/status-mobile/issues/18743
-      nil
+    (text-for-url-path? scanned-text router/channel-path)
+    ;; TODO: https://github.com/status-im/status-mobile/issues/18743
+    nil
 
-      (text-for-url-path? scanned-text router/user-with-data-path)
-      (load-and-show-profile address)
+    (text-for-url-path? scanned-text router/user-with-data-path)
+    (let [address (extract-id scanned-text)]
+      (load-and-show-profile address))
 
-      (or (validators/valid-public-key? scanned-text)
-          (validators/valid-compressed-key? scanned-text))
-      (load-and-show-profile scanned-text)
+    (or (validators/valid-public-key? scanned-text)
+        (validators/valid-compressed-key? scanned-text))
+    (load-and-show-profile scanned-text)
 
-      (eth-address? scanned-text)
-      (debounce/debounce-and-dispatch [:navigate-to :wallet-accounts address] 300)
+    (eth-address? scanned-text)
+    (debounce/debounce-and-dispatch [:navigate-to :wallet-accounts scanned-text] 300)
 
-      (pairing-qr-code? scanned-text)
-      ;; TODO: https://github.com/status-im/status-mobile/issues/18744
-      nil
+    (eip681-address? scanned-text)
+    (do
+      (debounce/debounce-and-dispatch [:wallet-legacy/request-uri-parsed
+                                       (eip681/parse-uri scanned-text)]
+                                      300)
+      (debounce/debounce-and-dispatch [:navigate-change-tab :wallet-stack] 300))
 
-      :else
-      (show-invalid-qr-toast))))
+    (pairing-qr-code? scanned-text)
+    ;; TODO: https://github.com/status-im/status-mobile/issues/18744
+    nil
+
+    (wallet-connect-code? scanned-text)
+    ;; WalletConnect is not working yet, this flow should be updated once WalletConnect is ready
+    nil
+
+    (url? scanned-text)
+    (debounce/debounce-and-dispatch [:browser.ui/open-url scanned-text] 300)
+
+    :else
+    (show-invalid-qr-toast)))
 
 (defn- f-internal-view
   []
