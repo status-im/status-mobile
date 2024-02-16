54758d340a331128448238d76578f87265d35d11
Ajay Sivan <ajayesivan@gmail.com>
2024-02-16 19:13:57 +0530

    Request to join - slide button bottom space fix

--- src/status_im/contexts/communities/actions/accounts_selection/style.cljs
+++ src/status_im/contexts/communities/actions/accounts_selection/style.cljs
@@ -1,15 +1,12 @@
 (ns status-im.contexts.communities.actions.accounts-selection.style
   (:require
-    [quo.foundations.colors :as colors]))
+    [quo.foundations.colors :as colors]
+    [react-native.safe-area :as safe-area]))
 
 (def screen-horizontal-padding 20)
 
 (def container
   {:flex 1})
-
-(def page-top
-  {:padding-vertical   12
-   :padding-horizontal screen-horizontal-padding})
 
 (def section-title
   {:padding-top        12
@@ -20,9 +17,10 @@
   []
   {:position           :absolute
    :background-color   (colors/theme-colors colors/white colors/neutral-95)
+   :padding-bottom     (safe-area/get-bottom)
    :bottom             0
    :left               0
    :right              0
    :padding-horizontal screen-horizontal-padding
-   :padding-vertical   12
+   :padding-top        12
    :flex               1})
