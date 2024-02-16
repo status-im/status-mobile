e151f9f0bb7291dba8b11d925be9a474476ebffa
Omar Basem <omarbasem.0101@gmail.com>
2024-02-16 08:01:16 +0400

    review

--- src/status_im/contexts/wallet/create_account/view.cljs
+++ src/status_im/contexts/wallet/create_account/view.cljs
@@ -53,98 +53,16 @@
         bottom                (safe-area/get-bottom)
         account-color         (reagent/atom (rand-nth colors/account-colors))
         emoji                 (reagent/atom (emoji-picker.utils/random-emoji))
-        number-of-accounts    (count (rf/sub [:wallet/accounts-without-watched-accounts]))
-        account-name          (reagent/atom "")
-        placeholder           (i18n/label :t/default-account-placeholder
-                                          {:number (inc number-of-accounts)})
+        number-of-accounts    (count (rf/sub [:wallet/accounts]))
+        account-name          (reagent/atom (i18n/label :t/default-account-name
+                                                        {:number (inc number-of-accounts)}))
         derivation-path       (reagent/atom (utils/get-derivation-path number-of-accounts))
         {:keys [public-key]}  (rf/sub [:profile/profile])
         on-change-text        #(reset! account-name %)
         primary-name          (first (rf/sub [:contacts/contact-two-names-by-identity public-key]))
         {window-width :width} (rn/get-window)]
     (fn [{:keys [theme]}]
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-      [rn/view {:style {:flex 1}}
-       [quo/page-nav
-        {:type       :no-title
-         :background :blur
-         :right-side [{:icon-name :i/info
-                       :on-press  #(rf/dispatch [:show-bottom-sheet
-                                                 {:content account-origin/view}])}]
-         :icon-name  :i/close
-         :on-press   #(rf/dispatch [:navigate-back])}]
-       [quo/gradient-cover
-        {:customization-color @account-color
-         :container-style     (style/gradient-cover-container top)}]
-       [rn/view
-        {:style style/account-avatar-container}
-        [quo/account-avatar
-         {:customization-color @account-color
-          :size                80
-          :emoji               @emoji
-          :type                :default}]
-        [quo/button
-         {:size            32
-          :type            :grey
-          :background      :photo
-          :icon-only?      true
-          :on-press        #(rf/dispatch [:emoji-picker/open
-                                          {:on-select (fn [selected-emoji]
-                                                        (reset! emoji selected-emoji))}])
-          :container-style style/reaction-button-container} :i/reaction]]
-       [quo/title-input
-        {:customization-color @account-color
-         :placeholder         placeholder
-         :on-change-text      on-change-text
-         :max-length          constants/wallet-account-name-max-length
-         :blur?               true
-         :disabled?           false
-         :auto-focus          true
-         :default-value       @account-name
-         :container-style     style/title-input-container}]
-       [quo/divider-line]
-       [rn/view
-        {:style style/color-picker-container}
-        [quo/text
-         {:size   :paragraph-2
-          :weight :medium
-          :style  (style/color-label theme)}
-         (i18n/label :t/colour)]
-        [quo/color-picker
-         {:default-selected @account-color
-          :on-change        #(reset! account-color %)
-          :container-style  {:padding-vertical 12
-                             :padding-left     (iphone-11-Pro-20-pixel-from-width window-width)}}]]
-       [quo/divider-line]
-       [quo/category
-        {:list-type :settings
-         :label     (i18n/label :t/origin)
-         :data      (get-keypair-data primary-name @derivation-path @account-color)}]
-       [standard-auth/slide-button
-        {:size                :size-48
-         :track-text          (i18n/label :t/slide-to-create-account)
-         :customization-color @account-color
-         :on-auth-success     (fn [entered-password]
-                                (rf/dispatch [:wallet/derive-address-and-add-account
-                                              {:sha3-pwd     (security/safe-unmask-data entered-password)
-                                               :emoji        @emoji
-                                               :color        @account-color
-                                               :path         @derivation-path
-                                               :account-name @account-name}]))
-         :auth-button-label   (i18n/label :t/confirm)
-         ;; TODO (@rende11) Add this property when sliding button issue will fixed
-         ;; https://github.com/status-im/status-mobile/pull/18683#issuecomment-1941564785
-         ;; :disabled?           (empty? @account-name)
-         :container-style     (style/slide-button-container bottom)}]])))
-=======
-=======
->>>>>>> d48b12035 (lint)
-      (let [{:keys [new-keypair]} (rf/sub [:wallet/ui])]
-=======
       (let [{:keys [new-keypair]} (rf/sub [:wallet/create-account])]
->>>>>>> 49af521e5 (review)
         (rn/use-effect (fn [] #(rf/dispatch [:wallet/clear-new-keypair])))
         [rn/view {:style {:flex 1}}
          [quo/page-nav
