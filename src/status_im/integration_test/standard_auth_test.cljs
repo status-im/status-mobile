26008901f9d7589b68af9c1ce6b2f504f6cf33b6
Lungu Cristian <lungucristian95@gmail.com>
2024-02-19 15:01:14 +0200

    test: removed clearing fixture and small refactor
    Signed-off-by: Cristian Lungu <lungucristian95@gmail.com>

--- src/status_im/integration_test/standard_auth_test.cljs
+++ src/status_im/integration_test/standard_auth_test.cljs
@@ -1,18 +1,10 @@
 (ns status-im.integration-test.standard-auth-test
   (:require
-    [cljs.test :refer [deftest testing is use-fixtures]]
+    [cljs.test :refer [deftest testing is]]
     [day8.re-frame.test :as rf-test]
     re-frame.core
     [test-helpers.integration :as h]
     [utils.re-frame :as rf]))
-
-(defn fixture-re-frame
-  []
-  (let [restore-re-frame (atom nil)]
-    {:before #(reset! restore-re-frame (re-frame.core/make-restore-fn))
-     :after  #(@restore-re-frame)}))
-
-(use-fixtures :each (fixture-re-frame))
 
 (def default-args
   {:on-auth-success       identity
@@ -23,42 +15,50 @@
    :blur?                 false
    :theme                 :light})
 
+(defn auth-success-fixtures
+  []
+  (rf/reg-fx :biometric/check-if-available
+   (fn [{:keys [on-success]}] (on-success)))
+  (rf/reg-event-fx :biometric/authenticate
+   (fn [_ [{:keys [on-success]}]] (on-success)))
+  (rf/reg-fx :keychain/get-user-password
+   (fn [[_ on-success]] (on-success))))
+
 (deftest standard-auth-biometric-authorize-success
   (testing "calling success callback when completing biometric authentication"
     (h/log-headline :standard-auth-authorize-success)
     (rf-test/run-test-async
-     (do
-       (rf/reg-fx :biometric/check-if-available
-        (fn [{:keys [on-success]}]
-          (on-success)))
-       (rf/reg-event-fx :biometric/authenticate
-        (fn [_ [{:keys [on-success]}]]
-          (on-success)))
-       (rf/reg-fx :keychain/get-user-password
-        (fn [[_ on-success]]
-          (on-success))))
+     (auth-success-fixtures)
      (let [on-success-called? (atom false)
            args               (assoc default-args :on-auth-success #(reset! on-success-called? true))]
        (rf/dispatch [:standard-auth/authorize args])
        (rf-test/wait-for [:standard-auth/on-biometric-success]
          (is @on-success-called?))))))
 
+(defn auth-cancel-fixtures
+  []
+  (rf/reg-fx :biometric/check-if-available
+   (fn [{:keys [on-success]}] (on-success)))
+  (rf/reg-event-fx :biometric/authenticate
+   (fn [_ [{:keys [on-cancel]}]] (on-cancel)))
+  (rf/reg-event-fx :show-bottom-sheet identity))
+
 (deftest standard-auth-biometric-authorize-cancel
   (testing "falling back to password authorization when biometrics canceled"
     (h/log-headline :standard-auth-authorize-cancel)
     (rf-test/run-test-async
-     (do
-       (rf/reg-fx :biometric/check-if-available
-        (fn [{:keys [on-success]}]
-          (on-success)))
-       (rf/reg-event-fx :biometric/authenticate
-        (fn [_ [{:keys [on-cancel]}]]
-          (on-cancel)))
-       (rf/reg-event-fx :show-bottom-sheet
-        (fn [_])))
+     (auth-cancel-fixtures)
      (rf/dispatch [:standard-auth/authorize default-args])
      (rf-test/wait-for [:show-bottom-sheet]
        (is true)))))
+
+(defn auth-fail-fixtures
+  [expected-error-cause]
+  (rf/reg-fx :biometric/check-if-available
+   (fn [{:keys [on-success]}] (on-success)))
+  (rf/reg-event-fx :biometric/authenticate
+   (fn [_ [{:keys [on-fail]}]] (on-fail (ex-info "error" {} expected-error-cause))))
+  (rf/reg-fx :biometric/show-message identity))
 
 (deftest standard-auth-biometric-authorize-fail
   (testing "showing biometric error message when authorization failed"
@@ -72,29 +72,23 @@
                                        (fn [err]
                                          (reset! on-fail-called? true)
                                          (reset! error err)))]
-       (rf/reg-fx :biometric/check-if-available
-        (fn [{:keys [on-success]}]
-          (on-success)))
-       (rf/reg-event-fx :biometric/authenticate
-        (fn [_ [{:keys [on-fail]}]]
-          (on-fail (ex-info "error" {} expected-error-cause))))
-       (rf/reg-fx :biometric/show-message
-        (fn [_]))
+       (auth-fail-fixtures expected-error-cause)
        (rf/dispatch [:standard-auth/authorize args])
        (rf-test/wait-for [:biometric/show-message]
          (is @on-fail-called?)
          (is (= expected-error-cause (ex-cause @error))))))))
 
+(defn auth-password-fallback-fixtures
+  []
+  (rf/reg-fx :biometric/check-if-available
+   (fn [{:keys [on-fail]}] (on-fail)))
+  (rf/reg-event-fx :show-bottom-sheet identity))
+
 (deftest standard-auth-password-authorize-fallback
   (testing "falling back to password when biometrics is not available"
     (h/log-headline :standard-auth-password-authorize-fallback)
     (rf-test/run-test-async
-     (do
-       (rf/reg-fx :biometric/check-if-available
-        (fn [{:keys [on-fail]}]
-          (on-fail)))
-       (rf/reg-event-fx :show-bottom-sheet
-        (fn [_])))
+     (auth-password-fallback-fixtures)
      (rf/dispatch [:standard-auth/authorize default-args])
      (rf-test/wait-for [:standard-auth/authorize-with-password]
        (is true)))))
