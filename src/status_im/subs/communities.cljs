9478ced3efcea0f08b82fe4c6ee295b2d3efdc34
Ibrkhalil <vampirekid017@gmail.com>
2024-02-22 11:15:04 +0200

    lint

--- src/status_im/subs/communities.cljs
+++ src/status_im/subs/communities.cljs
@@ -113,7 +113,8 @@
    (if (or (empty? @memo-communities-stack-items) (= view-id :communities-stack))
      (let [grouped-communities (->> communities
                                     vals
-                                    (sort-by (fn [{:keys [requested-to-join-at last-opened-at joined-at]}]
+                                    (sort-by (fn [{:keys [requested-to-join-at last-opened-at
+                                                          joined-at]}]
                                                (or requested-to-join-at last-opened-at joined-at))
                                              #(compare %2 %1))
                                     (group-by #(group-communities-by-status requests %)))]
