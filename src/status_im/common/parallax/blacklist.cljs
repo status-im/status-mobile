cc12cc02b8db6cf24b1556d6deea3723a8610c09
balogunofafrica <balogunakanbi.k@gmail.com>
2024-02-24 10:19:39 +0100

    make only blacklisted? public

--- src/status_im/common/parallax/blacklist.cljs
+++ src/status_im/common/parallax/blacklist.cljs
@@ -2,16 +2,16 @@
   (:require
     [native-module.core :as native-module]))
 
-(def ^:const device-id (:device-id (native-module/get-device-model-info)))
+(def ^:private device-id (:device-id (native-module/get-device-model-info)))
 
-(defn get-model-code
+(defn- get-model-code
   [model]
   (let [match (re-find #"iPhone(\d+)" model)]
     (if match
       (js/parseInt (second match))
       0)))
 
-(def ^:const minimum-device-code 11)
+(def ^:private minimum-device-code 11)
 
 (def blacklisted?
   (-> device-id
