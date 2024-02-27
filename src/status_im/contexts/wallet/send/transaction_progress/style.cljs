249b535a397b803b0505b626ecbbbf47df9e7d1f
Shivek Khurana <khuranashivek@outlook.com>
2024-02-27 16:34:39 +0000

    🚧 Save address UI reached teenage years

--- src/status_im/contexts/wallet/send/transaction_progress/style.cljs
+++ src/status_im/contexts/wallet/send/transaction_progress/style.cljs
@@ -3,3 +3,10 @@
 (def content-container
   {:flex              1
    :margin-horizontal 20})
+
+(def footer
+  {:flex-direction  :row
+   :border-color    "red"
+   :border-width    0
+   :justify-content :space-between
+   :width           "100%"})
--- src/status_im/contexts/wallet/send/transaction_progress/view.cljs
+++ src/status_im/contexts/wallet/send/transaction_progress/view.cljs
@@ -1,13 +1,13 @@
 (ns status-im.contexts.wallet.send.transaction-progress.view
   (:require
-    [quo.core :as quo]
-    [react-native.core :as rn]
-    [react-native.safe-area :as safe-area]
-    [status-im.common.floating-button-page.view :as floating-button-page]
-    [status-im.common.resources :as resources]
-    [status-im.contexts.wallet.send.transaction-progress.style :as style]
-    [utils.i18n :as i18n]
-    [utils.re-frame :as rf]))
+   [quo.core :as quo]
+   [react-native.core :as rn]
+   [react-native.safe-area :as safe-area]
+   [status-im.common.floating-button-page.view :as floating-button-page]
+   [status-im.common.resources :as resources]
+   [status-im.contexts.wallet.send.transaction-progress.style :as style]
+   [utils.i18n :as i18n]
+   [utils.re-frame :as rf]))
 
 (defn titles
   [status]
@@ -25,10 +25,17 @@
     (some (fn [[_k v]] (= (:status v) :confirmed)) transaction-details)   :confirmed
     :else                                                                 nil))
 
+(defn footer [{:keys [color leave-page]}]
+  [rn/view {:style style/footer}
+   [quo/button {:customization-color color
+                :on-press            leave-page}
+    (i18n/label :t/done)]])
+
 (defn view
   []
   (let [leave-page      #(rf/dispatch [:wallet/close-transaction-progress-page])
-        {:keys [color]} (rf/sub [:wallet/current-viewing-account])]
+        {:keys [color]} {:color "red"} ;; (rf/sub [:wallet/current-viewing-account])
+        ]
     (fn []
       (let [transaction-details (rf/sub [:wallet/send-transaction-progress])]
         [floating-button-page/view
@@ -40,10 +47,14 @@
                                       :margin-top          (safe-area/get-top)
                                       :on-press            leave-page
                                       :accessibility-label :top-bar}]
-          :footer                   [quo/button
-                                     {:customization-color color
-                                      :on-press            leave-page}
-                                     (i18n/label :t/done)]
+          :footer                   [rn/view {:style style/footer}
+                                     [quo/button {:type     :grey
+                                                  :on-press leave-page}
+                                      (i18n/label :t/save-address)]
+                                     [quo/button {:customization-color color
+                                                  :type                :primary
+                                                  :on-press            leave-page}
+                                      (i18n/label :t/done)]]
           :customization-color      color
           :gradient-cover?          true}
          [rn/view {:style style/content-container}
@@ -52,3 +63,6 @@
             :style  {:margin-bottom 12}}]
           [quo/standard-title
            {:title (titles (combined-status-overview transaction-details))}]]]))))
+
+(comment
+  ,)
