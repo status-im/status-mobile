1cef57c5c395f50b1f19ff60fa16e48fdac6ec92
Cristian Lungu <lungucristian95@gmail.com>
2024-03-01 10:21:24 +0200

    style: formatting

--- src/quo/components/community/channel_action/style.cljs
+++ src/quo/components/community/channel_action/style.cljs
@@ -6,10 +6,6 @@
   [{:keys [big? disabled?]}]
   (cond-> {}
     disabled?  (assoc :opacity 0.3)
-    ;; NOTE: The big action has to fill the available space when there's a small
-    ;; action next to it. If alone, take an approx. proportion of space ~2/3.
-    ;; Using fixed sizes according to the designs will make it look wrong on
-    ;; all other devices.
     big?       (assoc :flex-grow 1)
     (not big?) (assoc :width 104)))
 
--- src/status_im/contexts/preview/quo/community/channel_actions.cljs
+++ src/status_im/contexts/preview/quo/community/channel_actions.cljs
@@ -15,15 +15,33 @@
                 :counter-value       0}]}]
    [rn/view {:height 50}]
    [quo/channel-actions
-    {:actions [{:label "Pinned Messages" :customization-color :blue :icon :i/pin :counter-value 5}
-               {:label "Mute chat" :customization-color :blue :icon :i/muted}]}]
+    {:actions [{:label               "Pinned Messages"
+                :customization-color :blue
+                :icon                :i/pin
+                :counter-value       5}
+               {:label               "Mute chat"
+                :customization-color :blue
+                :icon                :i/muted}]}]
    [rn/view {:height 50}]
    [quo/channel-actions
     {:actions
-     [{:big? true :label "Pinned Messages" :customization-color :blue :icon :i/pin :counter-value 5}
-      {:label "Mute chat" :customization-color :blue :icon :i/muted}]}]
+     [{:big?                true
+       :label               "Pinned Messages"
+       :customization-color :blue
+       :icon                :i/pin
+       :counter-value       5}
+      {:label               "Mute chat"
+       :customization-color :blue
+       :icon                :i/muted}]}]
    [rn/view {:height 50}]
    [quo/channel-actions
-    {:actions [{:label "Pinned Messages" :customization-color :blue :icon :i/pin :counter-value 5}
-               {:label "Mute chat" :customization-color :blue :icon :i/muted}
-               {:label "Something else" :customization-color :blue :icon :i/placeholder}]}]])
+    {:actions [{:label               "Pinned Messages"
+                :customization-color :blue
+                :icon                :i/pin
+                :counter-value       5}
+               {:label               "Mute chat"
+                :customization-color :blue
+                :icon                :i/muted}
+               {:label               "Something else"
+                :customization-color :blue
+                :icon                :i/placeholder}]}]])
