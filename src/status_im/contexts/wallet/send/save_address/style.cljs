dff2c1ac883464e633677a7063b540b0ff4c0432
Shivek Khurana <khuranashivek@outlook.com>
2024-03-01 18:40:42 +0000

    🎨 Design in place

--- src/status_im/contexts/wallet/send/save_address/style.cljs
+++ src/status_im/contexts/wallet/send/save_address/style.cljs
@@ -1,7 +1,30 @@
 (ns status-im.contexts.wallet.send.save-address.style)
 
-(def absolutify
-  {:position :absolute
-   :top      0
-   :left     0
-   :right    0})
+(def title-input-container
+  {:padding-horizontal 20
+   :padding-top        12
+   :padding-bottom     16})
+
+(def account-avatar-container
+  {:padding-horizontal 20
+   :padding-top        12})
+
+(def divider-1
+  {:margin-bottom 12})
+
+(def color-picker-container
+  {:padding-vertical 8})
+
+(def section-container
+  {:padding-horizontal 20
+   :padding-bottom     4})
+
+(def color-picker
+  {:padding-horizontal 20})
+
+(def divider-2
+  {:margin-top    12
+   :margin-bottom 12})
+
+(def save-address-button
+  {:padding-horizontal 20})
--- src/status_im/contexts/wallet/send/save_address/view.cljs
+++ src/status_im/contexts/wallet/send/save_address/view.cljs
@@ -2,11 +2,9 @@
   (:require
    [quo.core :as quo]
    [re-frame.core :as rf]
-   [reagent.core :as reagent]
    [react-native.core :as rn]
    [status-im.contexts.wallet.send.save-address.style :as style]
-   [utils.i18n :as i18n]
-   ))
+   [utils.i18n :as i18n]))
 
 (defn view []
   (let [[address-label set-address-label] (rn/use-state "")
@@ -19,27 +17,38 @@
        :on-press            #(rf/dispatch [:navigate-back])
        :accessibility-label :save-address-top-bar}]
 
-     [quo/user-avatar
-      {:full-name           address-label
-       :customization-color address-color
-       :size                :big}]
+     [rn/view
+      {:style style/account-avatar-container}
+      [quo/user-avatar
+       {:full-name           address-label
+        :customization-color address-color
+        :size                :big}]]
 
      [quo/title-input
       {:blur?               true
+       :max-length          24
        :size                :heading-1
        :placeholder         (i18n/label :t/address-name)
        :default-value       address-label
        :on-change-text      set-address-label
-       :customization-color address-color}]
+       :customization-color address-color
+       :container-style     style/title-input-container}]
 
-     [quo/divider-line {}]
+     [quo/divider-line {:container-style style/divider-1}]
+
+     [quo/section-label
+      {:section         (i18n/label :t/colour)
+       :container-style style/section-container}]
+
      [quo/color-picker
       {:default-selected address-color
        :on-change        set-address-color
-       }]
-     [quo/divider-line {}]
-     ]))
+       :container-style  style/color-picker}]
 
+     [quo/divider-line {:container-style style/divider-2}]
 
-(comment
-  ,)
+     [quo/button
+      {:accessibility-label :save-address-button
+       :type                :primary
+       :container-style     style/save-address-button}
+      (i18n/label :t/save-address)]]))
