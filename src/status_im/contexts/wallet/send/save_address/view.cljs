26416d91994000b476ab5dd105ea8b696a89461a
Shivek Khurana <khuranashivek@outlook.com>
2024-03-01 17:47:00 +0000

    📝 Can't get title input to work

--- src/status_im/contexts/wallet/send/save_address/view.cljs
+++ src/status_im/contexts/wallet/send/save_address/view.cljs
@@ -2,16 +2,44 @@
   (:require
    [quo.core :as quo]
    [re-frame.core :as rf]
+   [reagent.core :as reagent]
    [react-native.core :as rn]
-   [status-im.contexts.wallet.send.save-address.style :as style]))
+   [status-im.contexts.wallet.send.save-address.style :as style]
+   [utils.i18n :as i18n]
+   ))
 
 (defn view []
-  [:<>
-   [rn/view {:style style/absolutify}
-    [quo/page-nav
-     {:type                :no-title
-      :background          :blur
-      :icon-name           :i/close
-      :on-press            #(rf/dispatch [:navigate-back])
-      :accessibility-label :save-address-top-bar}]]
-   ])
+  (let [[address-label set-address-label] (rn/use-state "")
+        [address-color set-address-color] (rn/use-state :blue)]
+    [:<>
+     [quo/page-nav
+      {:type                :no-title
+       :background          :blur
+       :icon-name           :i/close
+       :on-press            #(rf/dispatch [:navigate-back])
+       :accessibility-label :save-address-top-bar}]
+
+     [quo/user-avatar
+      {:full-name           address-label
+       :customization-color address-color
+       :size                :big}]
+
+     [quo/title-input
+      {:blur?               true
+       :size                :heading-1
+       :placeholder         (i18n/label :t/address-name)
+       :default-value       address-label
+       :on-change-text      set-address-label
+       :customization-color address-color}]
+
+     [quo/divider-line {}]
+     [quo/color-picker
+      {:default-selected address-color
+       :on-change        set-address-color
+       }]
+     [quo/divider-line {}]
+     ]))
+
+
+(comment
+  ,)
