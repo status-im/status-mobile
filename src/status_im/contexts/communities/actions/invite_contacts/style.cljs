638583c08e0fd27e0b6868f911749827655c3323
Ibrkhalil <vampirekid017@gmail.com>
2024-03-03 15:27:40 +0200

    Refactors

--- src/status_im/contexts/communities/actions/invite_contacts/style.cljs
+++ src/status_im/contexts/communities/actions/invite_contacts/style.cljs
@@ -1,5 +1,6 @@
 (ns status-im.contexts.communities.actions.invite-contacts.style
   (:require
+    [quo.foundations.colors :as colors]
     [react-native.safe-area :as safe-area]))
 
 (def contact-selection-heading
@@ -21,3 +22,27 @@
    :flex            1
    :justify-content :center
    :align-items     :center})
+
+(def context-tag
+  {:align-self    :flex-start
+   :margin-top    -8
+   :margin-bottom 12})
+
+(def no-contacts-text
+  {:margin-bottom 2
+   :margin-top    12})
+
+(def no-contacts-button-container
+  {:margin-top    20
+   :margin-bottom 12})
+
+(defn section-list-container-style
+  [theme]
+  {:padding-bottom   70
+   :background-color (colors/theme-colors colors/white
+                                          colors/neutral-95
+                                          theme)})
+
+(defn invite-to-community-text
+  [theme]
+  {:color (colors/theme-colors colors/neutral-100 colors/white theme)})
