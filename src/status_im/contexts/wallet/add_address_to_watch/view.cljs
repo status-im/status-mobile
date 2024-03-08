44b1c6d6f18000704080d3be025d3c72ab0d9df4
Ulises M <ulises.ssb@hotmail.com>
2024-03-08 10:38:48 -0600

    Add subscription to be loaded

--- src/status_im/contexts/wallet/add_address_to_watch/view.cljs
+++ src/status_im/contexts/wallet/add_address_to_watch/view.cljs
@@ -8,6 +8,7 @@
     [status-im.common.floating-button-page.view :as floating-button-page]
     [status-im.contexts.wallet.add-address-to-watch.style :as style]
     [status-im.contexts.wallet.common.validation :as validation]
+    [status-im.subs.wallet.add-account.address-to-watch]
     [utils.debounce :as debounce]
     [utils.i18n :as i18n]
     [utils.re-frame :as rf]))
