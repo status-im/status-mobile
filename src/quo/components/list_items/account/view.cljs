7934faf9954660807fb6f9f042ab5085528274c3
andrey <motor4ik@gmail.com>
2024-03-11 15:18:07 +0100

    fix

--- src/quo/components/list_items/account/view.cljs
+++ src/quo/components/list_items/account/view.cljs
@@ -102,35 +102,34 @@
            state               :default
            blur?               false}
     :as   props}]
-  (let [theme                   (quo.theme/use-theme-value)
-        [pressed? set-pressed?] (rn/use-state false)
-        on-press-in             (rn/use-callback #(set-pressed? true))
-        on-press-out            (rn/use-callback #(set-pressed? false))
-        props                   (assoc props :theme theme)]
-    (fn []
-      [rn/pressable
-       {:style               (style/container
-                              {:state               state
-                               :blur?               blur?
-                               :customization-color customization-color
-                               :pressed?            pressed?})
-        :on-press-in         on-press-in
-        :on-press            on-press
-        :on-press-out        on-press-out
-        :accessibility-label :container}
-       [account-view props]
-       [rn/view {:style (when (= type :tag) style/token-tag-container)}
-        (cond
-          (#{:balance-neutral :balance-negative :balance-positive} type)
-          [balance-view props]
+  (let [theme                  (quo.theme/use-theme-value)
+        [pressed? set-pressed] (rn/use-state false)
+        on-press-in            (rn/use-callback #(set-pressed true))
+        on-press-out           (rn/use-callback #(set-pressed false))
+        props                  (assoc props :theme theme)]
+    [rn/pressable
+     {:style               (style/container
+                            {:state               state
+                             :blur?               blur?
+                             :customization-color customization-color
+                             :pressed?            pressed?})
+      :on-press-in         on-press-in
+      :on-press            on-press
+      :on-press-out        on-press-out
+      :accessibility-label :container}
+     [account-view props]
+     [rn/view {:style (when (= type :tag) style/token-tag-container)}
+      (cond
+        (#{:balance-neutral :balance-negative :balance-positive} type)
+        [balance-view props]
 
-          (= type :tag)
-          [token-tag props]
+        (= type :tag)
+        [token-tag props]
 
-          (= type :action)
-          [options-button props]
+        (= type :action)
+        [options-button props]
 
-          (and (= type :default) (= state :selected))
-          [check-icon props])]])))
+        (and (= type :default) (= state :selected))
+        [check-icon props])]]))
 
 (def view (schema/instrument #'internal-view component-schema/?schema))
