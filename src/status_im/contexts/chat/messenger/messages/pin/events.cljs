f3a209b2aa241e0bc7074e92b9d5d52d5ebb1d9a
codemaster115 <cryptodev1010@gmail.com>
2024-03-15 10:28:26 -0700

    fix: remove log

--- src/status_im/contexts/chat/messenger/messages/pin/events.cljs
+++ src/status_im/contexts/chat/messenger/messages/pin/events.cljs
@@ -128,7 +128,6 @@
 (rf/defn show-pins-bottom-sheet
   {:events [:pin-message/show-pins-bottom-sheet]}
   [cofx chat-id & {:keys [inside-chat?] :or {inside-chat? true}}]
-  (println inside-chat?)
   (navigation/show-bottom-sheet cofx
                                 {:content (fn [] [pinned-messages-menu/view
                                                   {:chat-id      chat-id
