219dd7cb46c3631610dde8a29ca9ff64fa460198
Jamie Caprani <jamiecaprani@gmail.com>
2024-03-15 20:58:04 +0000

    chore(quo): add profile - collectibles list item component (#19125)

---
+++ src/quo/foundations/gradients.cljs
@@ -0,0 +1,34 @@
+(ns quo.foundations.gradients
+  (:require
+    [quo.foundations.colors :as colors]
+    [quo.theme]
+    [react-native.linear-gradient :as linear-gradient]))
+
+(defn- gradient-colors
+  [index theme]
+  (case index
+    :gradient-1 [(colors/resolve-color :yellow theme)
+                 (colors/resolve-color :sky theme)
+                 (colors/resolve-color :purple theme)]
+    :gradient-2 [(colors/resolve-color :orange theme)
+                 (colors/resolve-color :purple theme)
+                 (colors/resolve-color :blue theme)]
+    :gradient-3 [(colors/resolve-color :blue theme)
+                 (colors/resolve-color :magenta theme)
+                 (colors/resolve-color :yellow theme)]
+    :gradient-4 [(colors/resolve-color :army theme)
+                 (colors/resolve-color :orange theme)
+                 (colors/resolve-color :blue theme)]
+    [(colors/resolve-color :purple theme)
+     (colors/resolve-color :army theme)
+     (colors/resolve-color :yellow theme)]))
+
+(defn view
+  [{:keys [color-index container-style] :or {color-index 1}}]
+  (let [theme (quo.theme/use-theme-value)]
+    [linear-gradient/linear-gradient
+     {:style               (merge {:border-radius 16} container-style)
+      :accessibility-label :gradient-overlay
+      :colors              (gradient-colors color-index theme)
+      :start               {:x 0 :y 0}
+      :end                 {:x 0 :y 1}}]))
