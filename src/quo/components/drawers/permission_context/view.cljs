fd48717a001aa07cf01833a0a7c4008597349535
Cristian Lungu <lungucristian95@gmail.com>
2024-03-15 13:42:24 +0200

    fix: refactored quo component

--- src/quo/components/drawers/permission_context/view.cljs
+++ src/quo/components/drawers/permission_context/view.cljs
@@ -13,15 +13,51 @@
     [schema.core :as schema]
     [utils.i18n :as i18n]))
 
+(defn- single-token-gating
+  [{:keys [blur? token-value token-symbol]}]
+  [rn/view {:style {:flex-direction :row}}
+   [text/text {:style {:margin-right 3}}
+    (i18n/label :t/hold-to-post-1)]
+   [token-tag/view
+    {:size         :size-24
+     :option       false
+     :blur?        blur?
+     :token-value  token-value
+     :token-symbol token-symbol}]
+   [text/text {:style {:margin-left 3}}
+    (i18n/label :t/hold-to-post-2)]])
+
 (defn- token-group
-  [idx group]
+  [blur? idx group]
   ^{:key idx}
-  [rn/view
-   {:style {:flex-direction :row
-            :margin-left    3}}
+  [rn/view {:style style/token-group}
    (when-not (= idx 0)
-     [text/text {:style {:margin-right 3}} (string/lower-case (i18n/label :t/or))])
-   [preview-list/view {:type :tokens :size :size-24} group]])
+     [text/text {:style {:margin-right 3}}
+      (string/lower-case (i18n/label :t/or))])
+   [preview-list/view
+    {:type  :tokens
+     :blur? blur?
+     :size  :size-24} group]])
+
+(defn- multiple-token-gating
+  [{:keys [token-groups blur?]}]
+  (let [visible-token-groups (take 2 token-groups)
+        extra-groups?        (-> token-groups count (> 2))]
+    [rn/view {:style {:flex-direction :row}}
+     [text/text (i18n/label :t/hold-to-post-1)]
+     [rn/view {:style {:flex-direction :row}}
+      (map-indexed (partial token-group blur?) visible-token-groups)]
+     (when extra-groups?
+       [:<>
+        [text/text {:style {:margin-horizontal 3}}
+         (string/lower-case (i18n/label :t/or))]
+        [number-tag/view
+         {:size   :size-24
+          :number "9999" ;; show the options tag
+          :blur?  blur?
+          :type   :rounded}]])
+     [text/text {:style {:margin-left 3}}
+      (i18n/label :t/hold-to-post-2)]]))
 
 (defn- view-internal
   [{:keys [on-press blur? type container-style] :as props}]
@@ -35,30 +71,9 @@
                     (:action-icon props))}
       (condp = type
         :action                (:action-label props)
-        :single-token-gating   [rn/view {:style {:flex-direction :row}}
-                                [text/text {:style {:margin-right 3}} (i18n/label :t/hold-to-post-1)]
-                                [token-tag/view
-                                 {:size         :size-24
-                                  :option       false
-                                  :blur?        blur?
-                                  :token-value  (:token-value props)
-                                  :token-symbol (:token-symbol props)}]
-                                [text/text {:style {:margin-left 3}} (i18n/label :t/hold-to-post-2)]]
-        :multiple-token-gating [rn/view {:style {:flex-direction :row}}
-                                [text/text (i18n/label :t/hold-to-post-1)]
-                                [rn/view
-                                 {:style {:flex-direction :row}}
-                                 (map-indexed token-group
-                                              (->> props
-                                                   :token-groups
-                                                   (take 2)))]
-                                (when (-> props :token-groups count (> 2))
-                                  [:<>
-                                   [text/text {:style {:margin-horizontal 3}}
-                                    (string/lower-case (i18n/label :t/or))]
-                                   [number-tag/view
-                                    {:size :size-24 :number "9999" :blur? blur? :type :rounded}]])
-                                [text/text {:style {:margin-left 3}}
-                                 (i18n/label :t/hold-to-post-2)]])]]))
+        :single-token-gating   [single-token-gating
+                                (select-keys props [:token-value :token-symbol :blur?])]
+        :multiple-token-gating [multiple-token-gating
+                                (select-keys props [:token-groups :blur?])])]]))
 
 (def view (schema/instrument #'view-internal component-schema/?schema))
