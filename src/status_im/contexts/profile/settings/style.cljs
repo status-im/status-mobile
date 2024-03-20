dbf038ef19782d7a43a94bfedbc808aba527192b
alwx <alwxndr@gmail.com>
2024-03-20 16:48:09 +0100

    Fixes

--- src/status_im/contexts/profile/settings/style.cljs
+++ src/status_im/contexts/profile/settings/style.cljs
@@ -1,13 +1,13 @@
 (ns status-im.contexts.profile.settings.style
   (:require [quo.foundations.colors :as colors]
-            [react-native.platform :as platform]))
+            [react-native.platform :as platform]
+            [status-im.contexts.shell.jump-to.constants :as jump-to.constants]))
 
 (defn navigation-wrapper
   [{:keys [customization-color inset theme]}]
   {:padding-top      inset
    :background-color (colors/resolve-color customization-color theme 40)})
 
-(def ^:const jump-to-button-height 44)
 (def ^:const footer-padding 20)
 (def ^:const ios-bottom-offset -10)
 
@@ -15,7 +15,7 @@
   [bottom]
   {:padding-horizontal footer-padding
    :padding-top        footer-padding
-   :padding-bottom     (+ jump-to-button-height
+   :padding-bottom     (+ jump-to.constants/floating-shell-button-height
                           footer-padding
                           (if platform/ios? ios-bottom-offset bottom))})
 
