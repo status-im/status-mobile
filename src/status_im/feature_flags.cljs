b980906c203a1d4a4a91882b280ae894a09d215b
Omar Basem <omarbasem.0101@gmail.com>
2024-03-21 20:21:45 +0400

    lint

--- src/status_im/feature_flags.cljs
+++ src/status_im/feature_flags.cljs
@@ -10,11 +10,7 @@
 
 (defonce ^:private feature-flags-config
   (reagent/atom
-<<<<<<< HEAD
-   {::wallet.edit-default-keypair      (enabled-in-env? :FLAG_EDIT_DEFAULT_KEYPAIR)
-=======
    {::wallet.bridge-token              (enabled-in-env? :FLAG_BRIDGE_TOKEN_ENABLED)
->>>>>>> 2c98ea046 (remove feature flag keypair)
     ::wallet.edit-derivation-path      (enabled-in-env? :FLAG_EDIT_DERIVATION_PATH)
     ::wallet.remove-account            (enabled-in-env? :FLAG_REMOVE_ACCOUNT_ENABLED)
     ::wallet.network-filter            (enabled-in-env? :FLAG_NETWORK_FILTER_ENABLED)
