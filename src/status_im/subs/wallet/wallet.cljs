e583a4ef71483cae567f5b3cb4db796a8a732379
Ulises M <ulises.ssb@hotmail.com>
2024-03-25 12:07:29 -0600

    Refactor :wallet/tokens-filtered sub to filter only when `query` is provided

--- src/status_im/subs/wallet/wallet.cljs
+++ src/status_im/subs/wallet/wallet.cljs
@@ -222,18 +222,18 @@
  :<- [:wallet/current-viewing-account]
  :<- [:wallet/network-details]
  (fn [[account networks] [_ query]]
-   (let [tokens          (map (fn [token]
-                                (assoc token
-                                       :networks      (utils/network-list token networks)
-                                       :total-balance (utils/calculate-total-token-balance token)))
-                              (:tokens account))
-         sorted-tokens   (sort-by :name compare tokens)
-         filtered-tokens (filter #(or (string/starts-with? (string/lower-case (:name %))
-                                                           (string/lower-case query))
-                                      (string/starts-with? (string/lower-case (:symbol %))
-                                                           (string/lower-case query)))
-                                 sorted-tokens)]
-     filtered-tokens)))
+   (let [tokens        (map (fn [token]
+                              (assoc token
+                                :networks           (utils/network-list token networks)
+                                :total-balance      (utils/calculate-total-token-balance token)))
+                            (:tokens account))
+         sorted-tokens (sort-by :name compare tokens)]
+     (if query
+       (let [query-string (string/lower-case query)]
+         (filter #(or (string/starts-with? (string/lower-case (:name %)) query-string)
+                      (string/starts-with? (string/lower-case (:symbol %)) query-string))
+                 sorted-tokens))
+       sorted-tokens))))
 
 (rf/reg-sub
  :wallet/token-by-symbol
