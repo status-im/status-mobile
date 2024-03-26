510c425e078fca52169fad00c6a6a67b7e4e875c
Mohsen <maxghafori@gmail.com>
2024-03-26 12:12:36 +0300

    [#19351] fix: use animation worklet

--- src/js/worklets/profile_header.js
+++ src/js/worklets/profile_header.js
@@ -2,6 +2,7 @@ import { useAnimatedStyle, withTiming } from 'react-native-reanimated';
 
 export function profileHeaderAnimation(scrollY, threshold, topBarHeight) {
   return useAnimatedStyle(() => {
+    'worklet';
     const opacity = scrollY.value < threshold ? withTiming(0) : withTiming(1);
     const translateY = scrollY.value < threshold ? withTiming(topBarHeight) : withTiming(0);
 
