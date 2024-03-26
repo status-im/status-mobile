8a23a3a818e8a338449846b16a168e5a08bbcd92
Siddarth Kumar <siddarthkay@gmail.com>
2024-03-27 00:26:18 +0530

    kotlinify parts of status module, package & Utils
    parent issue: https://github.com/status-im/status-mobile/issues/18310
    
    This PR converts the following `Java` files to `Kotlin` :
    
    - modules/react-native-status/android/src/main/java/im/status/ethereum/module/StatusModule.java
    - modules/react-native-status/android/src/main/java/im/status/ethereum/module/StatusPackage.java
    - modules/react-native-status/android/src/main/java/im/status/ethereum/module/Utils.java
    
    This PR converts Java code to `Kotlin` which involves a helper function that is used to execute `statusgo` methods.
    It may impact everything or nothing.
    
    - Android

---
+++ modules/react-native-status/android/src/main/java/im/status/ethereum/module/StatusModule.kt
@@ -0,0 +1,114 @@
+package im.status.ethereum.module
+
+import android.app.Activity
+import android.os.Build
+import android.util.Log
+import com.facebook.react.bridge.*
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import statusgo.SignalHandler
+import statusgo.Statusgo
+import org.json.JSONException
+import android.view.WindowManager
+
+class StatusModule(private val reactContext: ReactApplicationContext, private val rootedDevice: Boolean) : ReactContextBaseJavaModule(reactContext), LifecycleEventListener, SignalHandler {
+
+    companion object {
+        private const val TAG = "StatusModule"
+        private var module: StatusModule? = null
+    }
+
+    private val utils: Utils = Utils(reactContext)
+    private var background: Boolean = false
+
+    init {
+        reactContext.addLifecycleEventListener(this)
+    }
+
+    override fun getName(): String {
+        return "Status"
+    }
+
+    override fun onHostResume() {
+        module = this
+        background = false
+        Statusgo.setMobileSignalHandler(this)
+    }
+
+    override fun onHostPause() {
+        background = true
+    }
+
+    override fun onHostDestroy() {
+        Log.d(TAG, "******************* ON HOST DESTROY *************************")
+    }
+
+    override fun handleSignal(jsonEventString: String) {
+        Log.d(TAG, "Signal event")
+        val params = Arguments.createMap()
+        params.putString("jsonEvent", jsonEventString)
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit("gethEvent", params)
+    }
+
+    @ReactMethod
+    fun closeApplication() {
+        System.exit(0)
+    }
+
+    @ReactMethod
+    fun connectionChange(type: String, isExpensive: Boolean) {
+        Log.d(TAG, "ConnectionChange: $type, is expensive $isExpensive")
+        Statusgo.connectionChange(type, if (isExpensive) 1 else 0)
+    }
+
+    @ReactMethod
+    fun appStateChange(type: String) {
+        Log.d(TAG, "AppStateChange: $type")
+        Statusgo.appStateChange(type)
+    }
+
+    @ReactMethod
+    fun startLocalNotifications() {
+        Log.d(TAG, "startLocalNotifications")
+        Statusgo.startLocalNotifications()
+    }
+
+    @ReactMethod
+    fun getNodeConfig(callback: Callback) {
+        utils.executeRunnableStatusGoMethod({ Statusgo.getNodeConfig() }, callback)
+    }
+
+    @ReactMethod
+    fun deleteImportedKey(keyUID: String, address: String, password: String, callback: Callback) {
+        val keyStoreDir = utils.getKeyStorePath(keyUID)
+        utils.executeRunnableStatusGoMethod({ Statusgo.deleteImportedKey(address, password, keyStoreDir) }, callback)
+    }
+
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    fun fleets(): String {
+        return Statusgo.fleets()
+    }
+
+    override fun getConstants(): Map<String, Any>? {
+        return hashMapOf(
+            "is24Hour" to utils.is24Hour(),
+            "model" to Build.MODEL,
+            "brand" to Build.BRAND,
+            "buildId" to Build.ID,
+            "deviceId" to Build.BOARD
+        )
+    }
+
+    @ReactMethod
+    fun isDeviceRooted(callback: Callback) {
+        callback.invoke(rootedDevice)
+    }
+
+    @ReactMethod
+    fun deactivateKeepAwake() {
+        val activity = currentActivity
+
+        activity?.runOnUiThread {
+            activity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
+    }
+}
---
+++ modules/react-native-status/android/src/main/java/im/status/ethereum/module/StatusPackage.kt
@@ -0,0 +1,38 @@
+package im.status.ethereum.module
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+import statusgo.Statusgo
+
+class StatusPackage(private val rootedDevice: Boolean) : ReactPackage {
+
+    companion object {
+        fun getImageTLSCert(): String = Statusgo.imageServerTLSCert()
+    }
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        val modules = mutableListOf<NativeModule>()
+
+        modules.apply {
+            add(StatusModule(reactContext, rootedDevice))
+            add(AccountManager(reactContext))
+            add(EncryptionUtils(reactContext))
+            add(DatabaseManager(reactContext))
+            add(UIHelper(reactContext))
+            add(LogManager(reactContext))
+            add(Utils(reactContext))
+            add(NetworkManager(reactContext))
+            add(RNSelectableTextInputModule(reactContext))
+        }
+
+        return modules
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return listOf(
+            RNSelectableTextInputViewManager()
+        )
+    }
+}
