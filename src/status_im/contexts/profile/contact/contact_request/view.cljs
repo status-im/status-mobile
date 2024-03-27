423a6f75216071caa25579c6af5c7ef3801d1f92
Sean Hagstrom <sean@seanhagstrom.com>
2024-03-27 13:47:59 +0000

    tweak: remove extra ideas

--- src/status_im/contexts/profile/contact/contact_request/view.cljs
+++ src/status_im/contexts/profile/contact/contact_request/view.cljs
@@ -1,7 +1,6 @@
 (ns status-im.contexts.profile.contact.contact-request.view
   (:require [clojure.string :as string]
             [quo.core :as quo]
-            [re-frame.core :as re-frame]
             [react-native.core :as rn]
             [reagent.ratom]
             [status-im.constants :as constants]
@@ -10,75 +9,22 @@
             [utils.i18n :as i18n]
             [utils.re-frame :as rf]))
 
-(defn make-stateful-callbacks
-  [state-ratom]
-  (let [state-ref (atom nil)]
-    (js/console.log "init make-stateful-callbacks")
-    (let [reaction-sub (reagent.ratom/track!
-                        (fn [state-ratom]
-                          (js/console.log "reagent reaction")
-                          (let [state @state-ratom]
-                            (js/console.log
-                             (if (nil? @state-ref)
-                               "state init"
-                               "state update")
-                             (clj->js state))
-                            (reset! state-ref state)))
-                        state-ratom)]
-      @reaction-sub
-      {:sub reaction-sub
-       :factory (memoize
-                 (fn [callback]
-                   (js/console.log "init callback")
-                   (fn [event]
-                     (callback @state-ref event))))})))
-
-(defn use-bind [state-ratom]
-  (let [{:keys [factory sub]} (rn/use-memo (fn []
-                                         (make-stateful-callbacks state-ratom))
-                                       [state-ratom])] 
-    (rn/use-unmount (fn []
-                      (js/console.log "unmount sub")
-                      (reagent.ratom/dispose! sub)))
-    factory))
-
-(defonce contact-request-message-store
-  (reagent.ratom/atom {}))
-
-(defn access-message-store
-  ([k] (get-in @contact-request-message-store k ""))
-  ([k v] (swap! contact-request-message-store assoc-in k v)))
-
-(defn get-message-sub [public-key]
-  (reagent.ratom/cursor access-message-store public-key))
-
 (defn on-press-test
-  [state event]
-  (js/console.log "on-press state" (clj->js state))
+  [event]
   (js/console.log "on-press event" #js{:target (.-target event)})
-  (access-message-store (:public-key state) "")
   (rf/dispatch [:hide-bottom-sheet]))
-
-(defn combine-subs [message-sub profile-sub]
-  {:message @message-sub
-   :public-key @(reagent.ratom/cursor profile-sub [:public-key])})
 
 (defn view
   []
-  (let [profile-sub           (re-frame/subscribe [:contacts/current-contact])
-        {:keys [public-key customization-color]
-         :as   profile}       (deref profile-sub)
+  (let [{:keys [public-key customization-color]
+         :as   profile}       (rf/sub [:contacts/current-contact])
         ;; TODO: remove default color when #18733 merged.
         customization-color   (or customization-color constants/profile-default-color)
         full-name             (profile.utils/displayed-name profile)
         profile-picture       (profile.utils/photo profile)
-        message-sub           (get-message-sub public-key)
         [message set-message] (rn/use-state "")
         on-message-change     (rn/use-callback #(do
-                                                  (reset! message-sub %)
                                                   (set-message %)))
-        callback-state-sub    (reagent.ratom/track combine-subs message-sub profile-sub)
-        bind                  (use-bind callback-state-sub)
         on-message-submit     (rn/use-callback (fn []
                                                  (rf/dispatch [:hide-bottom-sheet])
                                                  (rf/dispatch [:contact.ui/send-contact-request
@@ -109,7 +55,6 @@
         :auto-focus          true
         :accessibility-label :contact-request-message
         :label               (i18n/label :t/message)
-        :value               @message-sub
         :on-change-text      on-message-change}]]
      [quo/bottom-actions
       {:actions          :two-actions
@@ -120,6 +65,6 @@
        :button-one-label "Test Button One"
        :button-two-props {:accessibility-label :test-button
                           :customization-color :danger
-                          :on-press            (bind on-press-test)}
+                          :on-press            on-press-test}
        :button-two-label "Test Button Two"}]]))
 
