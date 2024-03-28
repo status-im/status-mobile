568fc10985ab07a19d66c7f4e563f46a5ae40540
Jamie Caprani <jamiecaprani@gmail.com>
2024-03-28 17:54:25 +0000

    chore(wallet): move bridge to its own folder

--- src/status_im/contexts/wallet/bridge/input_amount/view.cljs
+++ src/status_im/contexts/wallet/bridge/input_amount/view.cljs
@@ -1,8 +1,8 @@
-(ns status-im.contexts.wallet.account.bridge-send.view
+(ns status-im.contexts.wallet.bridge.input-amount.view
   (:require
     [quo.theme]
     [react-native.core :as rn]
-    [status-im.contexts.wallet.account.bridge-send.style :as style]
+    [status-im.contexts.wallet.bridge.input-amount.style :as style]
     [status-im.contexts.wallet.send.input-amount.view :as input-amount]
     [utils.i18n :as i18n]
     [utils.re-frame :as rf]))
@@ -11,7 +11,7 @@
   []
   [rn/view {:style style/bridge-send-wrapper}
    [input-amount/view
-    {:current-screen-id :screen/wallet.bridge-send
+    {:current-screen-id :screen/wallet.bridge-input-amount
      :button-one-label  (i18n/label :t/confirm-bridge)
      :button-one-props  {:icon-left :i/bridge}
      :on-navigate-back  (fn []
