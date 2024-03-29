0c26b952b4fc4ccd7b621ed4fb570f69ffc8f24f
Mohsen <maxghafori@gmail.com>
2024-03-29 15:43:27 +0300

    [#19264] fix: use floating button page

--- src/status_im/contexts/chat/home/add_new_contact/style.cljs
+++ src/status_im/contexts/chat/home/add_new_contact/style.cljs
@@ -1,6 +1,5 @@
 (ns status-im.contexts.chat.home.add-new-contact.style
-  (:require [quo.foundations.colors :as colors]
-            [react-native.safe-area :as safe-area]))
+  (:require [quo.foundations.colors :as colors]))
 
 (defn container-outer
   []
@@ -8,29 +7,13 @@
    :background-color   (colors/theme-colors colors/white colors/neutral-95)
    :justify-content    :space-between
    :align-items        :center
+   :margin-top         2
    :padding-horizontal 20})
-
-(def container-inner {:align-items :flex-start})
 
 (def container-invalid
   {:style {:flex-direction :row
            :align-items    :center
            :margin-top     8}})
-
-(defn text-title
-  []
-  {:size   :heading-1
-   :weight :semi-bold
-   :style  {:margin-top    28
-            :margin-bottom 6
-            :color         (colors/theme-colors colors/neutral-100 colors/white)}})
-
-(defn text-subtitle
-  []
-  {:size   :paragraph-1
-   :weight :regular
-   :style  {:margin-bottom 20
-            :color         (colors/theme-colors colors/neutral-100 colors/white)}})
 
 (defn text-description
   []
@@ -100,6 +83,5 @@
            colors/neutral-40)})
 
 (def button-view-profile
-  {:margin-top    24
-   :width         "100%"
-   :margin-bottom (+ (safe-area/get-bottom) 12)})
+  {:margin-top 24
+   :width      "100%"})
