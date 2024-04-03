934adbc3538be490c90c8c54c28e5aa507db7975
Sean Hagstrom <sean@seanhagstrom.com>
2024-04-03 09:12:05 +0100

    tweak: re-structure layout of quo/author component to use flex-baseline for alignment

--- src/quo/components/messages/author/style.cljs
+++ src/quo/components/messages/author/style.cljs
@@ -1,18 +1,17 @@
 (ns quo.components.messages.author.style
   (:require
-    [quo.foundations.colors :as colors]))
+    [quo.foundations.colors :as colors]
+    [react-native.platform :as platform]))
 
 (def container
-  {:flex-wrap      :nowrap
+  {:flex-shrink    1
+   :flex-wrap      :nowrap
    :flex-direction :row
-   :align-items    :flex-end})
+   :align-items    :baseline})
 
-(def name-container
-  {:margin-right   8
-   :flex-shrink    1
-   :flex-direction :row
-   :align-items    :flex-end
-  })
+(def details-container
+  {:flex-direction :row
+   :margin-left    8})
 
 (defn middle-dot
   [theme]
@@ -24,11 +23,15 @@
   {:color (colors/theme-colors colors/neutral-40 colors/neutral-50 theme)})
 
 (defn primary-name
-  [muted? theme]
+  [muted? theme size]
   {:color       (if muted?
                   colors/neutral-50
                   (colors/theme-colors colors/neutral-100 colors/white theme))
-   :flex-shrink 1})
+   :flex-shrink 1
+   :top         (when (= size 15)
+                  (cond platform/ios?     1
+                        platform/android? -0.5
+                        :else             0))})
 
 (defn secondary-name
   [theme]
@@ -38,7 +41,10 @@
 (defn icon-container
   [is-first?]
   {:margin-left   (if is-first? 4 2)
-   :margin-bottom 2})
+   :margin-bottom 0
+   :top           (cond platform/ios?     1
+                        platform/android? 2
+                        :else             0)})
 
 (defn time-text
   [theme]
