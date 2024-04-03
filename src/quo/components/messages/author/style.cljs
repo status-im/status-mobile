ab11fc1d8359849744e0414b37fd3f27b5b49ac6
Sean Hagstrom <sean@seanhagstrom.com>
2024-04-03 11:12:40 +0100

    tweak: add comments and fix layout glitch

--- src/quo/components/messages/author/style.cljs
+++ src/quo/components/messages/author/style.cljs
@@ -3,12 +3,25 @@
     [quo.foundations.colors :as colors]
     [react-native.platform :as platform]))
 
-(defn- primary-name-offset
+(defn- primary-name-top-offset
   [size]
   (when (= size 15)
     (cond platform/ios?     1
           platform/android? -0.5
           :else             0)))
+
+(defn- primary-name-margin-bottom-offset
+  [size]
+  (when (and (= size 15)
+             (or platform/ios? platform/android?))
+    -0.25))
+
+(defn- primary-name-layout-offsets
+  [size]
+  ;; NOTE(seanstrom): We need to sometimes offset the primary-name to align the baseline of the text
+  ;; while avoiding shifting elements downward.
+  {:top           (primary-name-top-offset size)
+   :margin-bottom (primary-name-margin-bottom-offset size)})
 
 (defn container
   [size]
@@ -16,7 +29,9 @@
    :flex-wrap      :nowrap
    :flex-direction :row
    :align-items    :baseline
-   :top            (* -1 (primary-name-offset size))})
+   ;; NOTE(seanstrom): Because we're offseting the primary-name we need to inverse the offset on the
+   ;; container to avoid shifting elements downward
+   :top            (* -1 (primary-name-top-offset size))})
 
 (def details-container
   {:flex-direction :row
@@ -33,11 +48,11 @@
 
 (defn primary-name
   [muted? theme size]
-  {:color       (if muted?
-                  colors/neutral-50
-                  (colors/theme-colors colors/neutral-100 colors/white theme))
-   :flex-shrink 1
-   :top         (primary-name-offset size)})
+  (merge (primary-name-layout-offsets size)
+         {:color       (if muted?
+                         colors/neutral-50
+                         (colors/theme-colors colors/neutral-100 colors/white theme))
+          :flex-shrink 1}))
 
 (defn secondary-name
   [theme]
@@ -46,10 +61,12 @@
 
 (defn icon-container
   [is-first?]
-  {:margin-left   (if is-first? 4 2)
-   :top           (cond platform/ios?     1
-                        platform/android? 2
-                        :else             0)})
+  {:margin-left (if is-first? 4 2)
+   ;; NOTE(seanstrom): Because we're using flex baseline to align elements
+   ;; we need to offset the icon container to match the designs.
+   :top         (cond platform/ios?     1
+                      platform/android? 2
+                      :else             0)})
 
 (defn time-text
   [theme]
