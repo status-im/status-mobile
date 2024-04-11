7d093864e70e3ad235d9477763b65fd11a971f6f
Milad <mmilad.sanati@gmail.com>
2024-04-11 16:10:12 +0330

    remove extra code

--- src/status_im/core_spec.cljs
+++ src/status_im/core_spec.cljs
@@ -6,7 +6,4 @@
     [status-im.contexts.shell.share.wallet.component-spec]
     [status-im.contexts.wallet.add-account.add-address-to-watch.component-spec]
     [status-im.contexts.wallet.add-account.add-address-to-watch.confirm-address.component-spec]
-    [status-im.contexts.wallet.add-address-to-watch.component-spec]
-    [status-im.contexts.wallet.add-address-to-watch.confirm-address.component-spec]
-    [status-im.contexts.wallet.create-account.edit-derivation-path.component-spec]
     [status-im.contexts.wallet.send.input-amount.component-spec]))
