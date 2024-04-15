e73cd73c1ce02e5eea4b2fbe430a06f70aec1a63
Siddarth Kumar <siddarthkay@gmail.com>
2024-04-15 16:46:40 +0200

    override gomobile to enable & use cgo

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -64,22 +64,26 @@ in {
   clang = super.clang_15;
   buildGoPackage = super.buildGo120Package;
   buildGoModule = super.buildGo120Module;
-  gomobile = (super.gomobile.overrideAttrs (old: {
-    patches = [
-      (self.fetchurl { # https://github.com/golang/mobile/pull/84
-        url = "https://github.com/golang/mobile/commit/f20e966e05b8f7e06bed500fa0da81cf6ebca307.patch";
-        sha256 = "sha256-TZ/Yhe8gMRQUZFAs9G5/cf2b9QGtTHRSObBFD5Pbh7Y=";
-      })
-      (self.fetchurl { # https://github.com/golang/go/issues/58426
-        url = "https://github.com/golang/mobile/commit/406ed3a7b8e44dc32844953647b49696d8847d51.patch";
-        sha256 = "sha256-dqbYukHkQEw8npOkKykOAzMC3ot/Y4DEuh7fE+ptlr8=";
-      })
-    ];
-  })).override {
-    # FIXME: No Android SDK packages for aarch64-darwin.
-    withAndroidPkgs = stdenv.system != "aarch64-darwin";
-    androidPkgs = self.androidEnvCustom.compose;
-  };
+
+  gomobile = super.gomobile.override {
+      # FIXME: No Android SDK packages for aarch64-darwin.
+      withAndroidPkgs = stdenv.system != "aarch64-darwin";
+      androidPkgs = self.androidEnvCustom.compose;
+      buildGoModule = args: super.buildGo120Module ( args // rec {
+        CGO_ENABLED=1;
+        GODEBUG="netdns=cgo";
+        patches = [
+          (self.fetchurl { # https://github.com/golang/mobile/pull/84
+            url = "https://github.com/golang/mobile/commit/f20e966e05b8f7e06bed500fa0da81cf6ebca307.patch";
+            sha256 = "sha256-TZ/Yhe8gMRQUZFAs9G5/cf2b9QGtTHRSObBFD5Pbh7Y=";
+          })
+          (self.fetchurl { # https://github.com/golang/go/issues/58426
+            url = "https://github.com/golang/mobile/commit/406ed3a7b8e44dc32844953647b49696d8847d51.patch";
+            sha256 = "sha256-dqbYukHkQEw8npOkKykOAzMC3ot/Y4DEuh7fE+ptlr8=";
+          })
+        ];
+      });
+    };
 
   # Android environement
   androidEnvCustom = callPackage ./pkgs/android-sdk { };
