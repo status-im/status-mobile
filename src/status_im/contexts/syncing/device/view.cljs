28f2425b9de017318342a310c11d47e4c56658b1
Lungu Cristian <lungucristian95@gmail.com>
2024-04-17 10:51:43 +0200

    feat: added pairing/unpairing of synced devices

--- src/status_im/contexts/syncing/device/view.cljs
+++ src/status_im/contexts/syncing/device/view.cljs
@@ -2,16 +2,22 @@
   (:require
     [quo.core :as quo]
     [status-im.contexts.syncing.device.style :as style]
-    [utils.i18n :as i18n]))
+    [utils.i18n :as i18n]
+    [utils.re-frame :as rf]))
 
 (defn view
   [{:keys [name
+           installation-id
            this-device?
            device-type
            enabled?
            show-button?]}]
-  (let [paired?   (and (not this-device?) enabled?)
-        unpaired? (not enabled?)]
+  (let [paired?       (and (not this-device?) enabled?)
+        unpaired?     (not enabled?)
+        unpair-device (fn []
+                        (rf/dispatch [:syncing/disable-installation installation-id]))
+        pair-device   (fn []
+                        (rf/dispatch [:syncing/enable-installation installation-id]))]
     [quo/settings-item
      (cond->
        {:container-style style/device-container
@@ -24,13 +30,13 @@
        (and show-button? unpaired?) (assoc
                                      :action :button
                                      :action-props
-                                     {:title    (i18n/label :t/pair)
-                                      :on-press #(js/alert "feature not added yet")})
+                                     {:button-text (i18n/label :t/pair)
+                                      :on-press    pair-device})
        (and show-button? paired?)   (assoc
                                      :action :button
                                      :action-props
                                      {:button-text (i18n/label :t/unpair)
-                                      :on-press    #(js/alert "feature not added yet")})
+                                      :on-press    unpair-device})
        this-device?                 (assoc
                                      :tag       :positive
                                      :tag-props {:label (i18n/label :t/this-device)}))]))
