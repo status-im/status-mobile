30b070b85e231103104b9d56f04962140df516a5
Omar Basem <omarbasem.0101@gmail.com>
2024-04-19 16:08:27 +0400

    lint

--- src/status_im/contexts/wallet/collectible/view.cljs
+++ src/status_im/contexts/wallet/collectible/view.cljs
@@ -90,8 +90,7 @@
                                          :header       collectible-name
                                          :description  collection-name}
             total-owned                 (utils/total-owned-collectible (:ownership collectible)
-                                                                       wallet-address)
-            customization-color        (rf/sub [:profile/customization-color])]
+                                                                       wallet-address)]
         (rn/use-unmount #(rf/dispatch [:wallet/clear-last-collectible-details]))
         [scroll-page/scroll-page
          {:navigate-back? true
