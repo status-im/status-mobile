cd54aa9b92dc10acf1bd55299dc0f9f2c392d778
Lungu Cristian <lungucristian95@gmail.com>
2024-04-26 13:24:52 +0200

    Add pairing/unpairing of devices (#19692)
    * feat: added pairing/unpairing of synced devices
    
    * feat: remove flatlists & add confirmation/toast

--- src/status_im/contexts/syncing/device/view.cljs
+++ src/status_im/contexts/syncing/device/view.cljs
@@ -1,17 +1,47 @@
 (ns status-im.contexts.syncing.device.view
   (:require
     [quo.core :as quo]
+    [react-native.core :as rn]
     [status-im.contexts.syncing.device.style :as style]
-    [utils.i18n :as i18n]))
+    [utils.i18n :as i18n]
+    [utils.re-frame :as rf]))
+
+(defn- hide-sheet
+  []
+  (rf/dispatch [:hide-bottom-sheet]))
+
+(defn- unpair-confirmation
+  [installation-id name]
+  (fn []
+    (let [unpair-device #(rf/dispatch [:syncing/disable-installation installation-id])]
+      [rn/view
+       [quo/drawer-top
+        {:title       (i18n/label :t/unpair-device)
+         :description (i18n/label :t/unpair-device-description {:name name})}]
+       [quo/bottom-actions
+        {:actions          :two-actions
+         :button-one-label (i18n/label :t/unpair)
+         :button-one-props {:type     :danger
+                            :on-press unpair-device}
+         :button-two-label (i18n/label :t/cancel)
+         :button-two-props {:type     :grey
+                            :on-press hide-sheet}}]])))
 
 (defn view
   [{:keys [name
+           installation-id
            this-device?
            device-type
            enabled?
            show-button?]}]
-  (let [paired?   (and (not this-device?) enabled?)
-        unpaired? (not enabled?)]
+  (let [paired?                  (and (not this-device?) enabled?)
+        unpaired?                (not enabled?)
+        show-unpair-confirmation (fn []
+                                   (rf/dispatch [:show-bottom-sheet
+                                                 {:theme   :dark
+                                                  :content (unpair-confirmation installation-id name)}]))
+        pair-device              (fn []
+                                   (rf/dispatch [:syncing/enable-installation installation-id]))]
     [quo/settings-item
      (cond->
        {:container-style style/device-container
@@ -24,13 +54,13 @@
        (and show-button? unpaired?) (assoc
                                      :action :button
                                      :action-props
-                                     {:title    (i18n/label :t/pair)
-                                      :on-press #(js/alert "feature not added yet")})
+                                     {:button-text (i18n/label :t/pair)
+                                      :on-press    pair-device})
        (and show-button? paired?)   (assoc
                                      :action :button
                                      :action-props
                                      {:button-text (i18n/label :t/unpair)
-                                      :on-press    #(js/alert "feature not added yet")})
+                                      :on-press    show-unpair-confirmation})
        this-device?                 (assoc
                                      :tag       :positive
                                      :tag-props {:label (i18n/label :t/this-device)}))]))
