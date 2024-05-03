95475aa66fc09621a46ad8c1ba3494b183c9b033
Omar Basem <omarbasem.0101@gmail.com>
2024-05-03 15:54:42 +0400

    lint

--- src/status_im/contexts/wallet/common/wizard/events.cljs
+++ src/status_im/contexts/wallet/common/wizard/events.cljs
@@ -1,11 +1,13 @@
 (ns status-im.contexts.wallet.common.wizard.events
   (:require [status-im.contexts.wallet.send.flow-config :as wallet-send-flow]
+            [status-im.contexts.wallet.bridge.flow-config :as wallet-bridge-flow]
             [utils.re-frame :as rf]))
 
 (defn- wizard-find-next-screen
   [db flow-id current-screen]
   (let [flow-config (case flow-id
-                      :wallet-send-flow wallet-send-flow/steps
+                      :wallet-send-flow   wallet-send-flow/steps
+                      :wallet-bridge-flow wallet-bridge-flow/steps
                       nil)]
     (first (filter (fn [{:keys [skip-step? screen-id]}]
                      (and (not= screen-id current-screen)
@@ -13,10 +15,10 @@
                    flow-config))))
 
 (rf/reg-event-fx
- :wallet/wizard-navigate-forward
- (fn [{:keys [db]} [{:keys [current-screen flow-id start-flow?]}]]
-   (let [next-screen (wizard-find-next-screen db flow-id current-screen)]
-     {:fx [[:dispatch
-            (if start-flow?
-              [:open-modal (:screen-id next-screen)]
-              [:navigate-to-within-stack [(:screen-id next-screen) current-screen]])]]})))
+  :wallet/wizard-navigate-forward
+  (fn [{:keys [db]} [{:keys [current-screen flow-id start-flow?]}]]
+    (let [next-screen (wizard-find-next-screen db flow-id current-screen)]
+      {:fx [[:dispatch
+             (if start-flow?
+               [:open-modal (:screen-id next-screen)]
+               [:navigate-to-within-stack [(:screen-id next-screen) current-screen]])]]})))
