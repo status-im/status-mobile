fc9b1a99fcd75e39e812e7277ecc144603215871
Milad <mmilad.sanati@gmail.com>
2024-05-03 17:28:43 +0330

    draft

--- src/status_im/contexts/wallet/home/view.cljs
+++ src/status_im/contexts/wallet/home/view.cljs
@@ -3,6 +3,7 @@
     [quo.core :as quo]
     [quo.foundations.colors :as colors]
     [react-native.core :as rn]
+    [reagent.core :as reagent]
     [status-im.common.home.top-nav.view :as common.top-nav]
     [status-im.contexts.wallet.home.style :as style]
     [status-im.contexts.wallet.home.tabs.view :as tabs]
@@ -38,6 +39,27 @@
    (when (ff/enabled? ::ff/wallet.home-activity)
      {:id :activity :label (i18n/label :t/activity) :accessibility-label :activity-tab})])
 
+(defn- render-cards
+  [cards ref]
+  [rn/flat-list
+   {:ref                               #(reset! ref %)
+    :style                             style/accounts-list
+    :content-container-style           style/accounts-list-container
+    :data                              cards
+    :horizontal                        true
+    :separator                         [rn/view {:style style/separator}]
+    :render-fn                         (fn [item] [quo/account-card item])
+    :shows-horizontal-scroll-indicator false}])
+
+(defn- render-tabs
+  [data on-change default-active]
+  [quo/tabs
+   {:style          style/tabs
+    :size           32
+    :default-active default-active
+    :data           data
+    :on-change      #(on-change %)}])
+
 (defn view
   []
   (let [[selected-tab set-selected-tab] (rn/use-state (:id (first tabs-data)))
@@ -61,36 +83,29 @@
      [tokens-loading?])
     [rn/view {:style (style/home-container)}
      [common.top-nav/view]
-     [quo/refreshable-scroll-view
-      {:refresh-control [rn/refresh-control
-                         {:refreshing (and tokens-loading? init-loaded?)
-                          :colors     colors/neutral-40
-                          :tint-color colors/neutral-40
-                          :on-refresh #(rf/dispatch [:wallet/get-accounts])}]
-       :style           {:flex 1}}
-      [:<>
-       [rn/view
-        [quo/wallet-overview
-         {:state             (if tokens-loading? :loading :default)
-          :time-frame        :none
-          :metrics           :none
-          :balance           formatted-balance
-          :networks          networks
-          :dropdown-on-press #(rf/dispatch [:show-bottom-sheet {:content network-filter/view}])}]]
-       (when (ff/enabled? ::ff/wallet.graph) [quo/wallet-graph {:time-frame :empty}])
-       [rn/flat-list
-        {:ref                               #(reset! account-list-ref %)
-         :style                             style/accounts-list
-         :content-container-style           style/accounts-list-container
-         :data                              cards
-         :horizontal                        true
-         :separator                         [rn/view {:style style/separator}]
-         :render-fn                         (fn [item] [quo/account-card item])
-         :shows-horizontal-scroll-indicator false}]
-       [quo/tabs
-        {:style          style/tabs
-         :size           32
-         :default-active selected-tab
-         :data           tabs-data
-         :on-change      #(set-selected-tab %)}]
-       [tabs/view {:selected-tab selected-tab}]]]]))
+     [rn/flat-list
+      {:refresh-control (reagent/as-element
+                         [rn/refresh-control
+                          {:refreshing (and tokens-loading? init-loaded?)
+                           :colors     colors/neutral-40
+                           :tint-color colors/neutral-40
+                           :on-refresh #(rf/dispatch [:wallet/get-accounts])}])
+       :style           {:flex 1}
+       :header  (reagent/as-element
+                 [:<>
+                  [rn/view
+                   [quo/wallet-overview
+                    {:state             (if tokens-loading? :loading :default)
+                     :time-frame        :none
+                     :metrics           :none
+                     :balance           formatted-balance
+                     :networks          networks
+                     :dropdown-on-press #(rf/dispatch [:show-bottom-sheet {:content network-filter/view}])}]]
+                  (when (ff/enabled? ::ff/wallet.graph) [quo/wallet-graph {:time-frame :empty}])]) 
+       :data [{:cards cards
+               :tabs tabs-data}]
+       :render-fn (fn [item]
+                    [:<>
+                     [render-cards (:cards item) account-list-ref]
+                     [render-tabs (:tabs item) set-selected-tab selected-tab]])
+       :footer [tabs/view {:selected-tab selected-tab}]}]]))
