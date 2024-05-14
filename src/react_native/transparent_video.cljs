37bc24faa2f1732e04e33b8aa67ff47603e2ba38
Jamie Caprani <jamiecaprani@gmail.com>
2024-05-14 22:02:55 +0100

    chore(onboarding): removing parallax

--- src/react_native/transparent_video.cljs
+++
@@ -1,6 +0,0 @@
-(ns react-native.transparent-video
-  (:require
-    ["react-native-transparent-video" :default TV]
-    [reagent.core :as reagent]))
-
-(def view (reagent/adapt-react-class TV))
--- src/status_im/navigation/screens.cljs
+++ src/status_im/navigation/screens.cljs
@@ -303,7 +303,7 @@
                  :popGesture         false
                  :hardwareBackButton {:dismissModalOnPress false
                                       :popStackOnPress     false}}
-     :component generating-keys/generating-keys}
+     :component generating-keys/view}
 
     {:name      :screen/onboarding.enter-seed-phrase
      :options   {:theme      :dark
