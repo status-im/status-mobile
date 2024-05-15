0d138b71d128fcf67ae10334ecc982b627a39691
Icaro Motta <icaro.ldm@gmail.com>
2024-05-14 21:58:42 -0300

    Update lint rule to ignore preview namespaces

--- scripts/lint/direct-require-component-outside-quo.sh
+++ scripts/lint/direct-require-component-outside-quo.sh
@@ -2,8 +2,11 @@
 
 # quo components namespace ends with style|utils|types|reaction-resource
 # are not the component view, they are usually utils fns or styles
+#
+# We also ignore preview namespaces from this linter because they may require
+# ?schema vars to auto-generate preview descriptors.
 
-if rg --pcre2 --glob '!src/quo/**/*' 'quo\.components(\.[\w-]+)*\.[\w-]*(?<!style|utils|types|reaction-resource)(\s|\]|\n)' src; then
+if rg --pcre2 --glob '!src/quo/**/*' --glob '!src/status_im/contexts/preview/quo/**/*' 'quo\.components(\.[\w-]+)*\.[\w-]*(?<!style|utils|types|reaction-resource)(\s|\]|\n)' src; then
   echo "Found above direct require of quo component outside src/quo/"
   echo "For more info, check the style guide https://github.com/status-im/status-mobile/blob/develop/doc/new-guidelines.md#requiring-quo-components"
   exit 1
