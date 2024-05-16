c05be0f2c207c98f4963441450a3461b9d285ffd
Lungu Cristian <lungucristian95@gmail.com>
2024-05-16 18:02:44 +0300

    Fix Wallet Connect structure  (#20042)
    * fix: wallet-connect structure & small bugs
    
    * fix: formatting issue with sorting wc dependency

--- src/status_im/contexts/wallet/wallet_connect/effects.cljs
+++ src/status_im/contexts/wallet/wallet_connect/effects.cljs
@@ -1,18 +1,27 @@
 (ns status-im.contexts.wallet.wallet-connect.effects
   (:require [promesa.core :as promesa]
             [re-frame.core :as rf]
-            [status-im.contexts.wallet.wallet-connect.utils :as wallet-connect.utils]))
+            [react-native.wallet-connect :as wallet-connect]
+            [status-im.config :as config]
+            [status-im.constants :as constants]
+            [utils.i18n :as i18n]))
 
 (rf/reg-fx
  :effects.wallet-connect/init
  (fn [{:keys [on-success on-fail]}]
-   (-> (wallet-connect.utils/init)
-       (promesa/then on-success)
-       (promesa/catch on-fail))))
+   (let
+     [project-id config/WALLET_CONNECT_PROJECT_ID
+      metadata   {:name        (i18n/label :t/status)
+                  :description (i18n/label :t/status-is-a-secure-messaging-app)
+                  :url         constants/wallet-connect-metadata-url
+                  :icons       [constants/wallet-connect-metadata-icon]}]
+     (-> (wallet-connect/init project-id metadata)
+         (promesa/then on-success)
+         (promesa/catch on-fail)))))
 
 (rf/reg-fx
  :effects.wallet-connect/register-event-listener
- (fn [web3-wallet wc-event handler]
+ (fn [[web3-wallet wc-event handler]]
    (.on web3-wallet
         wc-event
         (fn [js-proposal]
@@ -32,8 +41,8 @@
  :effects.wallet-connect/approve-session
  (fn [{:keys [web3-wallet proposal supported-namespaces on-success on-fail]}]
    (let [{:keys [params id]} proposal
-         approved-namespaces (wallet-connect.utils/build-approved-namespaces params
-                                                                             supported-namespaces)]
+         approved-namespaces (wallet-connect/build-approved-namespaces params
+                                                                       supported-namespaces)]
      (-> (.approveSession web3-wallet
                           (clj->js {:id         id
                                     :namespaces approved-namespaces}))
