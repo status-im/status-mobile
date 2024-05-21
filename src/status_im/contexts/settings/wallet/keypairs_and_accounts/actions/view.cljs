5b201ae76248d6c8f5bcb9a179f99b55bd6ec527
Sean Hagstrom <sean@seanhagstrom.com>
2024-05-21 12:58:28 +0100

    tidy: revert change for checking for not default-keypair in actions menu

--- src/status_im/contexts/settings/wallet/keypairs_and_accounts/actions/view.cljs
+++ src/status_im/contexts/settings/wallet/keypairs_and_accounts/actions/view.cljs
@@ -22,7 +22,7 @@
            :accessibility-label :show-key-pr-qr
            :label               (i18n/label :t/show-encrypted-qr-of-key-pairs)
            :on-press            #(on-show-qr data)}])
-       (when (not= (:type props) :default-keypair)
+       (when (= (:type props) :keypair)
          [{:icon                :i/edit
            :accessibility-label :rename-key-pair
            :label               (i18n/label :t/rename-key-pair)
