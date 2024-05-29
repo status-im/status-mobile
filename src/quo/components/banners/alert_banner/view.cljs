c3a28226582a88c21e78ed54786d69e819a97864
Milad <mmilad.sanati@gmail.com>
2024-05-29 19:55:04 +0200

    feat(wallet): add tests

--- src/quo/components/banners/alert_banner/view.cljs
+++ src/quo/components/banners/alert_banner/view.cljs
@@ -5,32 +5,36 @@
             [quo.components.markdown.text :as text]
             [quo.foundations.colors :as colors]
             [quo.theme :as quo.theme]
+            [react-native.core :as rn]
             [react-native.linear-gradient :as linear-gradient]))
 
 (defn view
   [{:keys [action? text button-text on-button-press]}]
   (let [theme (quo.theme/use-theme)]
-    [linear-gradient/linear-gradient
-     {:style  style/container
-      :start  {:x 0 :y 0}
-      :end    {:x 0 :y 1}
-      :colors [(colors/theme-colors 
-                colors/danger-50-opa-5 
-                colors/danger-50-opa-10 
-                theme) 
-               colors/danger-50-opa-0]}
-     [icon/icon 
-      :i/alert
-      {:color (colors/resolve-color :danger theme)
-       :size  16}]
-     [text/text 
-      {:style (style/label theme)
-       :number-of-lines 1} 
-      text]
-     (when action?
-       [button/button
-        {:type :danger
-         :size 24
-         :on-press on-button-press}
-        [text/text {:style style/button-text}
-         button-text]])]))
+    [rn/view
+     {:accessibility-label :alert-banner}
+     [linear-gradient/linear-gradient
+      {:style  style/container
+       :start  {:x 0 :y 0}
+       :end    {:x 0 :y 1}
+       :colors [(colors/theme-colors
+                 colors/danger-50-opa-5
+                 colors/danger-50-opa-10
+                 theme)
+                colors/danger-50-opa-0]}
+      [icon/icon
+       :i/alert
+       {:color (colors/resolve-color :danger theme)
+        :size  16}]
+      [text/text
+       {:style (style/label theme)
+        :number-of-lines 1}
+       text]
+      (when action?
+        [button/button
+         {:accessibility-label :button
+          :type :danger
+          :size 24 
+          :on-press on-button-press}
+         [text/text {:style style/button-text}
+          button-text]])]]))
