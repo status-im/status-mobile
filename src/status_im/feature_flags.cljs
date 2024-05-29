899add3dd61750f8a9ee32c4da0b90c64060647d
Sean Hagstrom <sean@seanhagstrom.com>
2024-05-29 17:36:44 +0100

    chore: remove feature-flag for wallet-settings inside settings

--- src/status_im/feature_flags.cljs
+++ src/status_im/feature_flags.cljs
@@ -11,7 +11,6 @@
 
 (def ^:private initial-flags
   {::community.edit-account-selection   (enabled-in-env? :FLAG_EDIT_ACCOUNT_SELECTION_ENABLED)
-   ::settings.wallet-settings           (enabled-in-env? :FLAG_WALLET_SETTINGS_ENABLED)
    ::settings.keypairs-and-accounts     (enabled-in-env?
                                          :FLAG_WALLET_SETTINGS_KEYPAIRS_AND_ACCOUNTS_ENABLED)
    ::settings.saved-addresses           (enabled-in-env?
