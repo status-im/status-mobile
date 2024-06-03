c7da16d624b80175409c94598c111c2ef7f92659
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2024-06-03 15:26:57 +0100

    Icon added to disclaimer

--- src/quo/components/selectors/disclaimer/view.cljs
+++ src/quo/components/selectors/disclaimer/view.cljs
@@ -1,13 +1,15 @@
 (ns quo.components.selectors.disclaimer.view
   (:require
+    [quo.components.icon :as icons]
     [quo.components.markdown.text :as text]
     [quo.components.selectors.disclaimer.style :as style]
     [quo.components.selectors.selectors.view :as selectors]
+    [quo.foundations.colors :as colors]
     [quo.theme]
     [react-native.core :as rn]))
 
 (defn view
-  [{:keys [checked? blur? accessibility-label container-style on-change]} label]
+  [{:keys [checked? blur? accessibility-label container-style on-change icon]} label]
   (let [theme (quo.theme/use-theme)]
     [rn/touchable-without-feedback
      {:on-press            on-change
@@ -22,4 +24,9 @@
       [text/text
        {:size  :paragraph-2
         :style style/text}
-       label]]]))
+       label]
+      (when icon
+        [rn/view {:style style/icon-container}
+         [icons/icon icon
+          {:accessibility-label :disclaimer-icon
+           :color               (colors/theme-colors colors/neutral-50 colors/neutral-40 theme)}]])]]))
