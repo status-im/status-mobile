c9651f1978ee13a6aee590c52c50e0a00e3bf382
Brian Sztamfater <brian@status.im>
2024-06-05 19:46:31 +0200

    feat(swap): implement network selection drawer

--- src/status_im/contexts/wallet/swap/events.cljs
+++ src/status_im/contexts/wallet/swap/events.cljs
@@ -1,18 +1,35 @@
 (ns status-im.contexts.wallet.swap.events
-  (:require [re-frame.core :as rf]))
+  (:require [re-frame.core :as rf]
+            [status-im.contexts.wallet.sheets.network-selection.view :as network-selection]))
 
 (rf/reg-event-fx :wallet.swap/start
  (fn [{:keys [_db]}]
    {:fx [[:dispatch [:open-modal :screen/wallet.swap-select-asset-to-pay]]]}))
 
 (rf/reg-event-fx :wallet.swap/select-asset-to-pay
- (fn [{:keys [db]} token]
-   {:db (assoc-in db [:wallet :ui :swap :asset-to-pay] token)
-    :fx [[:dispatch
-          [:toasts/upsert
-           {:id   :swap-error
-            :type :negative
-            :text "Not implemented yet"}]]]}))
+ (fn [{:keys [db]} [{:keys [token network]}]]
+   {:db (-> db
+            (assoc-in [:wallet :ui :swap :asset-to-pay] token)
+            (assoc-in [:wallet :ui :swap :network] network))
+    :fx [(if network
+           [:dispatch
+            [:toasts/upsert
+             {:id   :swap-error
+              :type :negative
+              :text "Not implemented yet"}]]
+           [:dispatch
+            [:show-bottom-sheet
+             {:content (fn []
+                         [network-selection/view
+                          {:token-symbol      (:symbol token)
+                           :on-select-network (fn [network]
+                                                (rf/dispatch [:hide-bottom-sheet])
+                                                (rf/dispatch
+                                                 [:wallet.swap/select-asset-to-pay
+                                                  {:token token
+                                                   :network network
+                                                   :stack-id
+                                                   :screen/wallet.swap-select-asset-to-pay}]))}])}]])]}))
 
 (rf/reg-event-fx :wallet.swap/clean-asset-to-pay
  (fn [{:keys [db]}]
