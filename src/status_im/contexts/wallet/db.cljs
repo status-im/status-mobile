769d870b0ff2bf9878152f9eaee7b1f7d92614d3
Omar Basem <omarbasem.0101@gmail.com>
2024-06-05 09:12:39 +0400

    lint

--- src/status_im/contexts/wallet/db.cljs
+++ src/status_im/contexts/wallet/db.cljs
@@ -7,12 +7,18 @@
 
 (def defaults
 <<<<<<< HEAD
+<<<<<<< HEAD
   {:ui {:network-filter network-filter-defaults}})
 =======
+=======
+>>>>>>> 2d5d98343 (lint)
 <<<<<<< HEAD
   {:ui {:network-filter  network-filter-defaults
 =======
   {:ui {:network-filter network-filter-defaults
 >>>>>>> c7cd8e811 (lint)
+=======
+  {:ui {:network-filter  network-filter-defaults
+>>>>>>> 54fa87586 (lint)
         :tokens-loading? true}})
 >>>>>>> 3498d19b2 (lint)
