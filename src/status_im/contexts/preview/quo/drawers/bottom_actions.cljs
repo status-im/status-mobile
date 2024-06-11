6a32006e6abae5aee76c419b7a568845f434ec18
Ajay Sivan <ajayesivan@gmail.com>
2024-06-11 19:13:12 +0530

    Drawer/Bottom Actions - context-tag-props (#20388)

--- src/status_im/contexts/preview/quo/drawers/bottom_actions.cljs
+++ src/status_im/contexts/preview/quo/drawers/bottom_actions.cljs
@@ -1,7 +1,7 @@
 (ns status-im.contexts.preview.quo.drawers.bottom-actions
   (:require
     [quo.core :as quo]
-    [reagent.core :as reagent]
+    [react-native.core :as rn]
     [status-im.contexts.preview.quo.preview :as preview]))
 
 (def button-two "Cancel")
@@ -58,7 +58,9 @@
 (def role-descriptor
   {:key     :role
    :type    :select
-   :options [{:key :owner}
+   :options [{:key   nil
+              :value :none}
+             {:key :owner}
              {:key :token-master}
              {:key :admin}
              {:key :member}]})
@@ -77,35 +79,40 @@
 
 (defn view
   []
-  (let [state (reagent/atom {:actions              :two-actions
-                             :description          :bottom
-                             :description-text     description
-                             :description-top-text "Eligible to join as"
-                             :error-message        "Error message"
-                             :button-one-label     button-one
-                             :button-two-label     button-two
-                             :button-one-props     {:on-press (button-press 1)
-                                                    :type     :primary}
-                             :button-two-props     {:on-press (button-press 2)
-                                                    :type     :grey}
-                             :blur?                false
-                             :role                 :owner
-                             :scroll?              false})]
-    (fn []
-      [preview/preview-container
-       {:state                     state
-        :descriptor                (cond-> descriptor
-                                     (= (:description @state) :top)
-                                     (conj role-descriptor description-top-descriptor)
+  (let [[state set-state] (rn/use-state
+                           {:actions              :two-actions
+                            :description          :bottom
+                            :description-text     description
+                            :description-top-text "Eligible to join as"
+                            :error-message        "Error message"
+                            :button-one-label     button-one
+                            :button-two-label     button-two
+                            :button-one-props     {:on-press (button-press 1)
+                                                   :type     :primary}
+                            :button-two-props     {:on-press (button-press 2)
+                                                   :type     :grey}
+                            :blur?                false
+                            :role                 nil
+                            :context-tag-props    {:size   24
+                                                   :type   :token
+                                                   :token  "USDT"
+                                                   :amount "99.97"}
+                            :scroll?              false})]
+    [preview/preview-container
+     {:state                     state
+      :set-state                 set-state
+      :descriptor                (cond-> descriptor
+                                   (= (:description state) :top)
+                                   (conj role-descriptor description-top-descriptor)
 
-                                     (= (:description @state) :bottom)
-                                     (conj description-descriptor)
+                                   (= (:description state) :bottom)
+                                   (conj description-descriptor)
 
-                                     (= (:description @state) :top-error)
-                                     (conj error-descriptor))
-        :blur?                     (:blur? @state)
-        :show-blur-background?     true
-        :blur-dark-only?           true
-        :component-container-style {:margin-top         40
-                                    :padding-horizontal 0}}
-       [quo/bottom-actions @state]])))
+                                   (= (:description state) :top-error)
+                                   (conj error-descriptor))
+      :blur?                     (:blur? state)
+      :show-blur-background?     true
+      :blur-dark-only?           true
+      :component-container-style {:margin-top         40
+                                  :padding-horizontal 0}}
+     [quo/bottom-actions state]]))
