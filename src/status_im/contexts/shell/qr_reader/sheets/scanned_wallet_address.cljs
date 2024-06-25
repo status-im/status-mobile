d2de9b331916e44da42cc6b32c6230e0aa0f7c3e
Milad <mmilad.sanati@gmail.com>
2024-06-25 19:13:19 +0200

    fix(wallet): fix issue 1

--- src/status_im/contexts/shell/qr_reader/sheets/scanned_wallet_address.cljs
+++ src/status_im/contexts/shell/qr_reader/sheets/scanned_wallet_address.cljs
@@ -2,28 +2,30 @@
   (:require
     [quo.core :as quo]
     [status-im.contexts.wallet.common.utils :as utils]
+    [status-im.contexts.wallet.common.utils.networks :as network-utils]
     [utils.i18n :as i18n]
     [utils.re-frame :as rf]))
 
 (defn view
   [address]
-  [:<>
-   [quo/drawer-top
-    {:title address
-     :type  :address}]
-   [quo/action-drawer
-    [[{:icon                :i/send
-       :accessibility-label :send-asset
-       :label               (i18n/label :t/send-to-this-address)
-       :on-press            (fn []
-                              (rf/dispatch [:wallet/select-send-address
-                                            {:address     address
-                                             :recipient   {:recipient-type :address
-                                                           :label          (utils/get-shortened-address
-                                                                            address)}
-                                             :stack-id    :wallet-select-address
-                                             :start-flow? true}]))}
-      {:icon                :i/save
-       :accessibility-label :save-address
-       :label               (i18n/label :t/save-address)
-       :on-press            #(js/alert "feature not implemented")}]]]])
+  (let [[_ splitted-address] (network-utils/split-network-full-address address)]
+    [:<>
+     [quo/drawer-top
+      {:title address
+       :type  :address}]
+     [quo/action-drawer
+      [[{:icon                :i/send
+         :accessibility-label :send-asset
+         :label               (i18n/label :t/send-to-this-address)
+         :on-press            (fn []
+                                (rf/dispatch [:wallet/select-send-address
+                                              {:address     address
+                                               :recipient   {:recipient-type :address
+                                                             :label          (utils/get-shortened-address
+                                                                              splitted-address)}
+                                               :stack-id    :wallet-select-address
+                                               :start-flow? true}]))}
+        {:icon                :i/save
+         :accessibility-label :save-address
+         :label               (i18n/label :t/save-address)
+         :on-press            #(js/alert "feature not implemented")}]]]]))
