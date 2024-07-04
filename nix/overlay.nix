147676ae8cf9cd89004f357fa45ab1f77c360d7a
Siddarth Kumar <siddarthkay@gmail.com>
2024-07-04 14:39:23 +0530

    chore: add Xcode 15.4 to allowed versions (#20641)
    This commit adds Xcode 15.4 to allowed versions in xcode-wrapper overlay.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -57,7 +57,7 @@ in {
   yarn = super.yarn.override { nodejs = super.nodejs-18_x; };
   openjdk = super.openjdk11_headless;
   xcodeWrapper = callPackage ./pkgs/xcodeenv/compose-xcodewrapper.nix { } {
-    versions = ["15.1" "15.2" "15.3"];
+    versions = ["15.1" "15.2" "15.3" "15.4"];
   };
   go = super.go_1_21;
   clang = super.clang_15;
