e50e6de86764cae658748e00d69866e0bdf5c04b
Shivek Khurana <khuranashivek@outlook.com>
2024-07-08 21:09:10 +0530

    🅰️ Annotate

--- src/react_native/wallet_connect.cljs
+++ src/react_native/wallet_connect.cljs
@@ -7,23 +7,18 @@
 
 (defn- wallet-connect-core
   [project-id]
-  (Core. #js {:projectId project-id}))
+  (new ^js Core (clj->js {:projectId project-id})))
 
 (defn init
   [project-id metadata]
-  (js/alert "rn.wc/metadata" metadata)
-  (js/alert "rn.wc/wallet" Web3Wallet)
-  (js/alert "rn.wc/-init" Web3Wallet.-init)
-  (js/alert "rn.wc/init fn" Web3Wallet.init)
   (let [core (wallet-connect-core project-id)]
-    (js/alert "rn.wc/core" core)
-    (Web3Wallet.init
-     (clj->js {:core     core
-               :metadata metadata}))))
+    (.init ^js Web3Wallet
+           (clj->js {:core     core
+                     :metadata metadata}))))
 
 (defn build-approved-namespaces
   [proposal supported-namespaces]
-  (buildApprovedNamespaces
+  (^js buildApprovedNamespaces
    (clj->js {:proposal            proposal
              :supportedNamespaces supported-namespaces})))
 
@@ -31,10 +26,10 @@
 ;; https://github.com/WalletConnect/walletconnect-monorepo/blob/c6e9529418a0c81d4efcc6ac4e61f242a50b56c5/packages/utils/src/errors.ts
 (defn get-sdk-error
   [error-key]
-  (getSdkError error-key))
+  (^js getSdkError error-key))
 
 (defn parse-uri
   [uri]
   (-> uri
-      parseUri
+      (^js parseUri)
       (js->clj :keywordize-keys true)))
