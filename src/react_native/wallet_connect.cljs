05d903d80a20dc681a852cb9b4ec31680d1c4280
Shivek Khurana <khuranashivek@outlook.com>
2024-07-09 20:45:38 +0530

    📤 Don't refer

--- src/react_native/wallet_connect.cljs
+++ src/react_native/wallet_connect.cljs
@@ -1,24 +1,23 @@
 (ns react-native.wallet-connect
   (:require
-   ["@walletconnect/core" :refer [Core]]
-   ["@walletconnect/utils" :refer
-    [buildApprovedNamespaces getSdkError parseUri]]
-   ["@walletconnect/web3wallet" :refer [Web3Wallet]]))
+   ["@walletconnect/core" :as wc-core]
+   ["@walletconnect/utils" :as wc-utils]
+   ["@walletconnect/web3wallet" :as wc-wallet]))
 
 (defn- wallet-connect-core
   [project-id]
-  (new ^js Core (clj->js {:projectId project-id})))
+  (new ^js wc-core/Core (clj->js {:projectId project-id})))
 
 (defn init
   [project-id metadata]
   (let [core (wallet-connect-core project-id)]
-    (.init ^js Web3Wallet
+    (.init ^js wc-wallet/Web3Wallet
            (clj->js {:core     core
                      :metadata metadata}))))
 
 (defn build-approved-namespaces
   [proposal supported-namespaces]
-  (^js buildApprovedNamespaces
+  (^js wc-utils/buildApprovedNamespaces
    (clj->js {:proposal            proposal
              :supportedNamespaces supported-namespaces})))
 
@@ -26,10 +25,10 @@
 ;; https://github.com/WalletConnect/walletconnect-monorepo/blob/c6e9529418a0c81d4efcc6ac4e61f242a50b56c5/packages/utils/src/errors.ts
 (defn get-sdk-error
   [error-key]
-  (^js getSdkError error-key))
+  (^js wc-utils/getSdkError error-key))
 
 (defn parse-uri
   [uri]
   (-> uri
-      (^js parseUri)
+      (^js wc-utils/parseUri)
       (js->clj :keywordize-keys true)))
