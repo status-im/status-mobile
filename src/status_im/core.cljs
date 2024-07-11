f878d3bdbc0bf1e3068fc7ad7964784756efa88d
Icaro Motta <icaro.ldm@gmail.com>
2024-07-11 10:52:37 -0300

    Configure oops to not throw exceptions in dev builds

--- src/status_im/core.cljs
+++ src/status_im/core.cljs
@@ -7,6 +7,7 @@
     legacy.status-im.events
     legacy.status-im.subs.root
     [native-module.core :as native-module]
+    oops.config
     [re-frame.core :as re-frame]
     [re-frame.interop :as interop]
     [react-native.async-storage :as async-storage]
@@ -65,6 +66,21 @@
   (react-native-shake/add-shake-listener #(re-frame/dispatch [:shake-event]))
   (universal-links/initialize)
   (interceptors/register-global-interceptors)
+
+  ;; Change oops defaults to warn and print instead of throwing exceptions during development.
+  (oops.config/update-current-runtime-config!
+   merge
+   {:error-reporting              :console
+    :expected-function-value      :warn
+    :invalid-selector             :warn
+    :missing-object-key           :warn
+    :object-is-frozen             :warn
+    :object-is-sealed             :warn
+    :object-key-not-writable      :warn
+    :unexpected-empty-selector    :warn
+    :unexpected-object-value      :warn
+    :unexpected-punching-selector :warn
+    :unexpected-soft-selector     :warn})
 
   ;; Shell
   (async-storage/get-item :selected-stack-id #(shell.utils/change-selected-stack-id % nil nil))
