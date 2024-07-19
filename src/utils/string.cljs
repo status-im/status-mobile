e4018b13be3cb476be19b5dade2204c80ccd1883
Shivek Khurana <khuranashivek@outlook.com>
2024-07-19 12:04:28 +0200

    🌎 Add utils to remove http:// prefix

--- src/utils/string.cljs
+++ src/utils/string.cljs
@@ -81,3 +81,10 @@
   (if (and (string? url) (string/ends-with? url "/"))
     (subs url 0 (dec (count url)))
     url))
+
+(defn remove-http-prefix
+  "Given a URL, removes the 'http://' or 'https://' prefix if present.
+  Returns the URL without the prefix."
+  [url]
+  (when (string? url)
+    (string/replace url #"^https?://" "")))
