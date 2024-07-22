5357952f062bb10de5d2b250aefb62b3163b4748
Icaro Motta <icaro.ldm@gmail.com>
2024-07-21 23:23:31 -0300

    Rename test function

--- src/utils/transforms_test.cljs
+++ src/utils/transforms_test.cljs
@@ -3,7 +3,7 @@
     [cljs.test :refer [are deftest is testing]]
     [utils.transforms :as sut]))
 
-(defn map=
+(defn equals-as-json
   [m1 m2]
   (= (js/JSON.stringify (clj->js m1))
      (js/JSON.stringify (clj->js m2))))
@@ -11,27 +11,27 @@
 (deftest <-js-map-test
   (testing "without transforming keys/values"
     (are [expected m]
-     (is (map= expected (sut/<-js-map m)))
+     (is (equals-as-json expected (sut/<-js-map m)))
      nil               nil
      nil               #js []
      #js {}            #js {}
      #js {"a" 1 "b" 2} #js {"a" 1 "b" 2}))
 
   (testing "with key/value transformation"
-    (is (map= {"aa" [1] "bb" [2]}
-              (sut/<-js-map #js {"a" 1 "b" 2}
-                            {:key-fn (fn [k] (str k k))
-                             :val-fn (fn [_ v] (vector v))}))))
+    (is (equals-as-json {"aa" [1] "bb" [2]}
+                        (sut/<-js-map #js {"a" 1 "b" 2}
+                                      {:key-fn (fn [k] (str k k))
+                                       :val-fn (fn [_ v] (vector v))}))))
 
   (testing "it is non-recursive"
-    (is (map= {:a 1 :b #js {"c" 3}}
-              (sut/<-js-map #js {"a" 1 "b" #js {"c" 3}}
-                            {:key-fn (fn [k] (keyword k))}))))
+    (is (equals-as-json {:a 1 :b #js {"c" 3}}
+                        (sut/<-js-map #js {"a" 1 "b" #js {"c" 3}}
+                                      {:key-fn (fn [k] (keyword k))}))))
 
   (testing "value transformation based on the key"
-    (is (map= {"a" 1 "b" "banana"}
-              (sut/<-js-map #js {"a" 1 "b" 2}
-                            {:val-fn (fn [k v]
-                                       (if (= "b" k)
-                                         "banana"
-                                         v))})))))
+    (is (equals-as-json {"a" 1 "b" "banana"}
+                        (sut/<-js-map #js {"a" 1 "b" 2}
+                                      {:val-fn (fn [k v]
+                                                 (if (= "b" k)
+                                                   "banana"
+                                                   v))})))))
