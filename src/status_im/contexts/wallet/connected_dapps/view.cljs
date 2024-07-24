1386a91d29b8002ccc9812cf95d45116604797cf
Shivek Khurana <khuranashivek@outlook.com>
2024-07-24 14:08:32 +0200

    🖊️ Fix lint

--- src/status_im/contexts/wallet/connected_dapps/view.cljs
+++ src/status_im/contexts/wallet/connected_dapps/view.cljs
@@ -1,18 +1,18 @@
 (ns status-im.contexts.wallet.connected-dapps.view
   (:require
-   [quo.core :as quo]
-   [quo.foundations.colors :as colors]
-   [quo.theme]
-   [react-native.core :as rn]
-   [react-native.safe-area :as safe-area]
-   [status-im.common.plus-button.view :as plus-button]
-   [status-im.common.resources :as resources]
-   [status-im.contexts.wallet.connected-dapps.disconnect-dapp.view :as disconnect-dapp]
-   [status-im.contexts.wallet.connected-dapps.style :as style]
-   [status-im.contexts.wallet.wallet-connect.core :as core]
-   [utils.i18n :as i18n]
-   [utils.re-frame :as rf]
-   [utils.string]))
+    [quo.core :as quo]
+    [quo.foundations.colors :as colors]
+    [quo.theme]
+    [react-native.core :as rn]
+    [react-native.safe-area :as safe-area]
+    [status-im.common.plus-button.view :as plus-button]
+    [status-im.common.resources :as resources]
+    [status-im.contexts.wallet.connected-dapps.disconnect-dapp.view :as disconnect-dapp]
+    [status-im.contexts.wallet.connected-dapps.style :as style]
+    [status-im.contexts.wallet.wallet-connect.core :as core]
+    [utils.i18n :as i18n]
+    [utils.re-frame :as rf]
+    [utils.string]))
 
 (defn- on-disconnect
   [wallet-account {:keys [name topic]}]
@@ -107,12 +107,12 @@
           :content-container-style (style/dapps-list theme)
           :render-fn               (fn [{:keys [topic pairingTopic name url iconUrl]}]
                                      [quo/dapp
-                                      {:dapp                {:avatar              (core/compute-dapp-icon-path iconUrl
-                                                                                                               url)
-                                                             :name                (core/compute-dapp-name name url)
-                                                             :value               url
-                                                             :topic               topic
-                                                             :pairing-topic       pairingTopic
+                                      {:dapp                {:avatar (core/compute-dapp-icon-path iconUrl
+                                                                                                  url)
+                                                             :name (core/compute-dapp-name name url)
+                                                             :value url
+                                                             :topic topic
+                                                             :pairing-topic pairingTopic
                                                              :customization-color customization-color}
                                        :accessibility-label (str "dapp-" topic)
                                        :state               :default
@@ -126,9 +126,9 @@
                                                                              wallet-account
                                                                              dapp))}
                                                                [quo/icon :i/disconnect
-                                                                {:color               (colors/theme-colors
-                                                                                       colors/neutral-50
-                                                                                       colors/neutral-40
-                                                                                       theme)
+                                                                {:color (colors/theme-colors
+                                                                         colors/neutral-50
+                                                                         colors/neutral-40
+                                                                         theme)
                                                                  :accessibility-label :icon}]])}])
           :separator               [rn/view {:style (style/separator theme)}]}]])]))
