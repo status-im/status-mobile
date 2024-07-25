dc2899529f3d1d9a7f4e9077e29d35d86b3a4a47
Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>
2024-07-25 21:28:30 +0530

    feat(wallet)_: Add beta info box in activity tab (#20873)
    This commit adds the beta info box with links to chain explorers to the activity tab in the wallet.
    
    Signed-off-by: Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>

--- src/status_im/contexts/wallet/common/activity_tab/view.cljs
+++ src/status_im/contexts/wallet/common/activity_tab/view.cljs
@@ -1,9 +1,11 @@
 (ns status-im.contexts.wallet.common.activity-tab.view
   (:require
+    [clojure.string :as string]
     [quo.core :as quo]
     [quo.theme]
     [react-native.core :as rn]
     [status-im.common.resources :as resources]
+    [status-im.constants :as constants]
     [status-im.contexts.shell.jump-to.constants :as jump-to.constants]
     [status-im.contexts.wallet.common.empty-tab.view :as empty-tab]
     [utils.i18n :as i18n]
@@ -80,6 +82,10 @@
 ;                        :network-logo network-logo}
 ;    :blur?             false}])
 
+(defn- section-header
+  [{:keys [title]}]
+  [quo/divider-date title])
+
 (defn activity-item
   [{:keys [transaction] :as activity}]
   (case transaction
@@ -88,20 +94,63 @@
     ;; :mint            [mint-activity activity]
     nil))
 
+(defn- pressable-text
+  [{:keys [on-press text]}]
+  [rn/text
+   {:style    {:text-decoration-line :underline}
+    :on-press on-press}
+   text])
+
 (defn view
   []
-  (let [theme         (quo.theme/use-theme)
-        activity-list (rf/sub [:wallet/activities-for-current-viewing-account])]
-    (if (empty? activity-list)
-      [empty-tab/view
-       {:title       (i18n/label :t/no-activity)
-        :description (i18n/label :t/empty-tab-description)
-        :image       (resources/get-themed-image :no-activity theme)}]
-      [rn/section-list
-       {:sections                       activity-list
-        :sticky-section-headers-enabled false
-        :style                          {:flex               1
-                                         :padding-horizontal 8}
-        :content-container-style        {:padding-bottom jump-to.constants/floating-shell-button-height}
-        :render-fn                      activity-item
-        :render-section-header-fn       (fn [{:keys [title]}] [quo/divider-date title])}])))
+  (let [theme                    (quo.theme/use-theme)
+        address                  (rf/sub [:wallet/current-viewing-account-address])
+        activity-list            (rf/sub [:wallet/activities-for-current-viewing-account])
+        open-eth-chain-explorer  (rn/use-callback
+                                  #(rf/dispatch [:wallet/navigate-to-chain-explorer
+                                                 {:address address
+                                                  :network constants/mainnet-network-name}])
+                                  [address])
+        open-oeth-chain-explorer (rn/use-callback
+                                  #(rf/dispatch [:wallet/navigate-to-chain-explorer
+                                                 {:address address
+                                                  :network constants/optimism-network-name}])
+                                  [address])
+        open-arb-chain-explorer  (rn/use-callback
+                                  #(rf/dispatch [:wallet/navigate-to-chain-explorer
+                                                 {:address address
+                                                  :network constants/arbitrum-network-name}])
+                                  [address])]
+    [:<>
+     [quo/information-box
+      {:type      :informative
+       :icon      :i/info
+       :closable? false
+       :style     {:margin-horizontal 20 :margin-vertical 8}}
+      [:<>
+       (str (i18n/label :t/wallet-activity-beta-message) " ")
+       [pressable-text
+        {:on-press open-eth-chain-explorer
+         :text     (i18n/label :t/etherscan)}]
+       ", "
+       [pressable-text
+        {:on-press open-oeth-chain-explorer
+         :text     (i18n/label :t/op-explorer)}]
+       (str ", " (string/lower-case (i18n/label :t/or)) " ")
+       [pressable-text
+        {:on-press open-arb-chain-explorer
+         :text     (i18n/label :t/arbiscan)}]
+       "."]]
+     (if (empty? activity-list)
+       [empty-tab/view
+        {:title       (i18n/label :t/no-activity)
+         :description (i18n/label :t/empty-tab-description)
+         :image       (resources/get-themed-image :no-activity theme)}]
+       [rn/section-list
+        {:sections                       activity-list
+         :sticky-section-headers-enabled false
+         :style                          {:flex               1
+                                          :padding-horizontal 8}
+         :content-container-style        {:padding-bottom jump-to.constants/floating-shell-button-height}
+         :render-fn                      activity-item
+         :render-section-header-fn       section-header}])]))
