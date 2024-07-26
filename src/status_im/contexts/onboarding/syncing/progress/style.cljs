2f64da5af6acec2b2da082730f2398d2882ae226
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2024-07-26 19:31:42 +0530

    fix broken screen and navigation when syncing fails (#20887)

--- src/status_im/contexts/onboarding/syncing/progress/style.cljs
+++ src/status_im/contexts/onboarding/syncing/progress/style.cljs
@@ -20,14 +20,13 @@
    :padding-bottom   20
    :background-color (when-not in-onboarding? colors/neutral-80-opa-80-blur)})
 
-(def page-illustration
-  {:flex              1
-   :background-color  colors/danger-50
-   :align-items       :center
-   :margin-horizontal 20
-   :border-radius     20
-   :margin-top        20
-   :justify-content   :center})
+(defn page-illustration
+  [width]
+  {:flex            1
+   :width           width
+   :align-items     :center
+   :align-self      :center
+   :justify-content :center})
 
 (def try-again-button
   {:margin-top         20
