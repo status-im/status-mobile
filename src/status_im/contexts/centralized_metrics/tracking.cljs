f25d1aae5201fcfd507b3703d22e39ca4a6fa23b
Shivek Khurana <khuranashivek@outlook.com>
2024-07-30 20:23:21 +0200

    ⏩ Faster lookup

--- src/status_im/contexts/centralized_metrics/tracking.cljs
+++ src/status_im/contexts/centralized_metrics/tracking.cljs
@@ -14,31 +14,31 @@
 (def ^:const app-started-event "app-started")
 
 (def ^:const view-ids-to-track
-  [;; Tabs
-   :communities-stack
-   :chats-stack
-   :wallet-stack
+  #{;; Tabs
+    :communities-stack
+    :chats-stack
+    :wallet-stack
 
-   ;; Onboarding
-   :screen/onboarding.intro
-   :screen/onboarding.new-to-status
-   :screen/onboarding.sync-or-recover-profile
-   :screen/onboarding.enter-seed-phrase
-   :screen/onboarding.create-profile
-   :screen/onboarding.create-profile-password
-   :screen/onboarding.enable-biometrics
-   :screen/onboarding.generating-keys
-   :screen/onboarding.enable-notifications
-   :screen/onboarding.sign-in-intro
-   :screen/onboarding.sign-in
-   :screen/onboarding.syncing-progress
-   :screen/onboarding.syncing-progress-intro
-   :screen/onboarding.syncing-results
-   :screen/onboarding.welcome])
+    ;; Onboarding
+    :screen/onboarding.intro
+    :screen/onboarding.new-to-status
+    :screen/onboarding.sync-or-recover-profile
+    :screen/onboarding.enter-seed-phrase
+    :screen/onboarding.create-profile
+    :screen/onboarding.create-profile-password
+    :screen/onboarding.enable-biometrics
+    :screen/onboarding.generating-keys
+    :screen/onboarding.enable-notifications
+    :screen/onboarding.sign-in-intro
+    :screen/onboarding.sign-in
+    :screen/onboarding.syncing-progress
+    :screen/onboarding.syncing-progress-intro
+    :screen/onboarding.syncing-results
+    :screen/onboarding.welcome})
 
 (defn track-view-id-event
   [view-id]
-  (when (some #{view-id} view-ids-to-track)
+  (when (contains? view-ids-to-track view-id)
     (user-journey-event (str "view-id." (name view-id)))))
 
 (defn tracked-event
