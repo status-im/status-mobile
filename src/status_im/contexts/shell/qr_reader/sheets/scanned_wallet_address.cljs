6dc289300372ff977223697b8b6ffa18408be24a
Ulises M <ulises.ssb@hotmail.com>
2024-08-07 15:45:27 -0600

    fix(wallet): Remove redirection to "send flow" when the user scans an address QR code

--- src/status_im/contexts/shell/qr_reader/sheets/scanned_wallet_address.cljs
+++ src/status_im/contexts/shell/qr_reader/sheets/scanned_wallet_address.cljs
@@ -1,33 +1,40 @@
 (ns status-im.contexts.shell.qr-reader.sheets.scanned-wallet-address
   (:require
-    [quo.core :as quo]
-    [status-im.contexts.wallet.common.utils :as utils]
-    [status-im.contexts.wallet.common.utils.networks :as network-utils]
-    [status-im.feature-flags :as ff]
-    [utils.i18n :as i18n]
-    [utils.re-frame :as rf]))
+   [quo.core :as quo]
+   [react-native.clipboard :as clipboard]
+   [status-im.contexts.wallet.common.utils :as utils]
+   [status-im.contexts.wallet.common.utils.networks :as network-utils]
+   [status-im.feature-flags :as ff]
+   [utils.i18n :as i18n]
+   [utils.re-frame :as rf]))
+
+(defn- on-press [address]
+  ;; Originally, the flow went to the send flow, but it has been removed to avoid bugs.
+  ;; Please check https://github.com/status-im/status-mobile/issues/20972
+  ;; The previous code has been commented out.
+  (let [#_#_[_ split-address] (network-utils/split-network-full-address address)]
+    (clipboard/set-string address)
+    (rf/dispatch [:toasts/upsert {:type :positive
+                                  :text (i18n/label :t/address-copied)}])
+    #_(rf/dispatch
+       [:wallet/select-send-address
+        {:address     address
+         :recipient   {:recipient-type :address
+                       :label          (utils/get-shortened-address split-address)}
+         :stack-id    :wallet-select-address
+         :start-flow? true}])))
 
 (defn view
   [address]
-  (let [[_ splitted-address] (network-utils/split-network-full-address address)]
-    [:<>
-     [quo/drawer-top
-      {:title address
-       :type  :address}]
-     [quo/action-drawer
-      [[{:icon                :i/send
-         :accessibility-label :send-asset
-         :label               (i18n/label :t/send-to-this-address)
-         :on-press            (fn []
-                                (rf/dispatch [:wallet/select-send-address
-                                              {:address     address
-                                               :recipient   {:recipient-type :address
-                                                             :label          (utils/get-shortened-address
-                                                                              splitted-address)}
-                                               :stack-id    :wallet-select-address
-                                               :start-flow? true}]))}
-        (when (ff/enabled? :ff/wallet.saved-addresses)
-          {:icon                :i/save
-           :accessibility-label :save-address
-           :label               (i18n/label :t/save-address)
-           :on-press            #(js/alert "feature not implemented")})]]]]))
+  [:<>
+   [quo/drawer-top {:title address :type :address}]
+   [quo/action-drawer
+    [[{:icon                :i/copy
+       :accessibility-label :send-asset
+       :label               (i18n/label :t/copy-address)
+       :on-press            #(on-press address)}
+      (when (ff/enabled? :ff/wallet.saved-addresses)
+        {:icon                :i/save
+         :accessibility-label :save-address
+         :label               (i18n/label :t/save-address)
+         :on-press            #(js/alert "feature not implemented")})]]]])
