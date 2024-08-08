b7b5714705bd2d18245c1013fc41baf3109cfa38
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2024-08-08 17:52:18 +0530

    Add docstring

--- src/status_im/common/theme/events.cljs
+++ src/status_im/common/theme/events.cljs
@@ -12,6 +12,11 @@
 (re-frame/reg-event-fx
  :theme/switch
  (fn [{db :db} [{:keys [theme view-id theme-type]}]]
+   "Switches the theme and triggers related effects.
+   Parameters:
+   - theme: Optional theme keyword to apply. If not provided, defaults to the theme determined by theme-type.
+   - theme-type: Optional theme type to determine the theme. If not provided, defaults to the theme type from the user's profile.
+   - view-id: Optional view ID for updating status color. If not provided, uses the current view ID from the database."
    (let [theme-type (or theme-type (get-in db [:profile/profile :appearance]))
          theme      (or theme (utils/theme-type->theme-value theme-type))]
      {:db (assoc db :theme theme)
