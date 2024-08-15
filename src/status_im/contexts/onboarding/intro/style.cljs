0c6c616cf0ab2ef781278fbc140eeb1230385064
Sean Hagstrom <sean@seanhagstrom.com>
2024-08-15 10:41:12 +0100

    feature: require manual acceptance of terms-of-use and privacy-statement (#21010)
    This change changes the initial onboarding screen to require users to manually accept the terms-of-use and privacy-statement before continuing to onboard into the app.
    
    ---------
    
    Co-authored-by: Yevheniia Berdnyk <ie.berdnyk@gmail.com>

--- src/status_im/contexts/onboarding/intro/style.cljs
+++ src/status_im/contexts/onboarding/intro/style.cljs
@@ -7,16 +7,21 @@
    :justify-content :flex-end})
 
 (def text-container
-  {:text-align :center
-   :margin-top 4
-   :flex-wrap  :wrap})
+  {:flex           1
+   :flex-direction :row
+   :flex-wrap      :wrap
+   :align-self     :center})
+
+(def terms-privacy-container
+  {:gap                8
+   :padding-horizontal 20
+   :padding-vertical   8})
 
 (def plain-text
   {:color colors/white-opa-70})
 
 (def highlighted-text
-  {:flex  1
-   :color colors/white})
+  {:color colors/white})
 
 (defn bottom-actions-container
   [bottom-insets]
---
+++ src/status_im/contexts/onboarding/privacy/view.cljs
@@ -0,0 +1,11 @@
+(ns status-im.contexts.onboarding.privacy.view
+  (:require-macros [legacy.status-im.utils.slurp :refer [slurp]])
+  (:require [quo.core :as quo]
+            [react-native.gesture :as gesture]))
+
+(def privacy-statement-text (slurp "resources/privacy.mdwn"))
+
+(defn privacy-statement
+  []
+  [gesture/scroll-view {:margin 20}
+   [quo/text privacy-statement-text]])
