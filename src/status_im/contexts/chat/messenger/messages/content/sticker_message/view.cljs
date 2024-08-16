6ed7e7a930add7532406eff735fbc0a4ab60cd1f
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2024-08-16 14:07:45 +0530

    Logs for failed stickers

--- src/status_im/contexts/chat/messenger/messages/content/sticker_message/view.cljs
+++ src/status_im/contexts/chat/messenger/messages/content/sticker_message/view.cljs
@@ -1,10 +1,15 @@
 (ns status-im.contexts.chat.messenger.messages.content.sticker-message.view
   (:require [react-native.core :as rn]
-            [react-native.fast-image :as fast-image]))
+            [react-native.fast-image :as fast-image]
+            [oops.core :as  oops]))
 
 (defn view
   [{:keys [url]}]
   [rn/view {:style {:margin-top 6 :margin-bottom 4}}
-   [fast-image/fast-image
+   (print "------------ parvesh" url)
+   [rn/image
     {:style  {:width 120 :height 120}
+     :on-error (fn [e]
+                 (print "------ parvesh error"
+                        (oops/oget e "nativeEvent.error")))
      :source {:uri (str url "&download=true")}}]])
