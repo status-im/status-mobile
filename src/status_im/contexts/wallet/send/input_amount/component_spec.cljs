98ab9581cf08ef57aecaeddef14c54cb6bc66d8a
Ulises M <ulises.ssb@hotmail.com>
2024-09-06 18:58:33 -0600

    Fix tests

--- src/status_im/contexts/wallet/send/input_amount/component_spec.cljs
+++ src/status_im/contexts/wallet/send/input_amount/component_spec.cljs
@@ -101,6 +101,7 @@
    :wallet/send-amount                             nil
    :wallet/wallet-send-tx-type                     :tx/send
    :wallet/wallet-send-fee-fiat-formatted          "$5,00"
+   :wallet/sending-collectible?                    false
    :wallet/total-amount                            (money/bignumber "250")})
 
 (h/describe "Send > input amount screen"
