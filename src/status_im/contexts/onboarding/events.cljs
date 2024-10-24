5a1e163723fb98e77d5295d9266d2e188633fea7
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2024-10-24 19:01:14 +0530

    review changes

--- src/status_im/contexts/onboarding/events.cljs
+++ src/status_im/contexts/onboarding/events.cljs
@@ -70,24 +70,24 @@
 ;; Navigate to next screen once onboarding animation and account creation both are complete
 ;; Note: If account creation fails, user will get stuck on loading screen
 ;; TODO: Show error screen in case account creation/restoration fails
-(rf/defn account-creation-complete
-  {:events [:onboarding/account-creation-complete]}
-  [{:keys [db] :as cofx} {:keys [loading-animation-complete? loading-screen login-signal-received?]}]
-  (let [loading-animation-complete? (or loading-animation-complete?
-                                        (:onboarding/loading-animation-complete? db))
-        loading-screen              (or loading-screen (:onboarding/loading-screen db))
-        login-signal-received?      (or login-signal-received? (:onboarding/login-signal-received? db))]
-    (if (and loading-animation-complete? login-signal-received?)
-      {:db (-> db
-               (dissoc :onboarding/loading-screen)
-               (dissoc :onboarding/loading-animation-complete?)
-               (dissoc :onboarding/login-signal-received?))
-       :fx [[:dispatch
-             [:navigate-to-within-stack [:screen/onboarding.enable-notifications loading-screen]]]]}
-      {:db (-> db
-               (assoc :onboarding/loading-screen loading-screen)
-               (assoc :onboarding/loading-animation-complete? loading-animation-complete?)
-               (assoc :onboarding/login-signal-received? login-signal-received?))})))
+(rf/reg-event-fx :onboarding/account-creation-complete
+ (fn [{:keys [db]} {:keys [opts]}]
+   (let [loading-animation-complete? (or (:loading-animation-complete? opts)
+                                         (:onboarding/loading-animation-complete? db))
+         loading-screen              (or (:loading-screen opts) (:onboarding/loading-screen db))
+         login-signal-received?      (or (:login-signal-received? opts)
+                                         (:onboarding/login-signal-received? db))]
+     (if (and loading-animation-complete? login-signal-received?)
+       {:db (-> db
+                (dissoc :onboarding/loading-screen)
+                (dissoc :onboarding/loading-animation-complete?)
+                (dissoc :onboarding/login-signal-received?))
+        :fx [[:dispatch
+              [:navigate-to-within-stack [:screen/onboarding.enable-notifications loading-screen]]]]}
+       {:db (-> db
+                (assoc :onboarding/loading-screen loading-screen)
+                (assoc :onboarding/loading-animation-complete? loading-animation-complete?)
+                (assoc :onboarding/login-signal-received? login-signal-received?))}))))
 
 (rf/defn create-account-and-login
   {:events [:onboarding/create-account-and-login]}
