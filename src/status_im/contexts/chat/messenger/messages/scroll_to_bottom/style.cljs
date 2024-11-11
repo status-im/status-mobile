b6c7a5bd21b226dc4e2971918ce1441ff63d28dd
Ulises M <ulises.ssb@hotmail.com>
2024-11-11 13:31:26 -0600

    Extract drawer height to a var

--- src/status_im/contexts/chat/messenger/messages/scroll_to_bottom/style.cljs
+++ src/status_im/contexts/chat/messenger/messages/scroll_to_bottom/style.cljs
@@ -2,11 +2,13 @@
   (:require [react-native.safe-area :as safe-area]
             [status-im.contexts.shell.jump-to.constants :as shell.constants]))
 
+(def ^:private bottom-drawer-height 46)
+
 (defn shell-button-container
   [able-to-send-messages?]
   {:z-index 1
    :bottom  (+ (safe-area/get-bottom)
-               (when-not able-to-send-messages? 46)
+               (when-not able-to-send-messages? bottom-drawer-height)
                shell.constants/floating-shell-button-height)})
 
 (def scroll-to-bottom-button
