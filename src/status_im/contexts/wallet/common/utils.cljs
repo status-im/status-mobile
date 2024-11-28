402901ca5c58289653e5de1a6a4fba5238bffeee
Mohsen <maxghafori@gmail.com>
2024-11-28 15:37:53 +0300

    [#21644] fix: modify current func

--- src/status_im/contexts/wallet/common/utils.cljs
+++ src/status_im/contexts/wallet/common/utils.cljs
@@ -264,18 +264,12 @@
       (str $ address))
     address))
 
-(defn asset-fiat-formatted-for-ui
+(defn fiat-formatted-for-ui
   [currency-symbol fiat-value]
   (cond
     (money/equal-to fiat-value 0)     (str currency-symbol "0.00")
     (money/less-than fiat-value 0.01) (str "<" currency-symbol "0.01")
     :else                             (prettify-balance currency-symbol fiat-value)))
-
-(defn fiat-formatted-for-ui
-  [currency-symbol fiat-value]
-  (if (money/less-than fiat-value 0.01)
-    (str "<" currency-symbol "0.01")
-    (prettify-balance currency-symbol fiat-value)))
 
 (defn prettify-percentage-change
   "Returns unsigned precentage"
@@ -301,8 +295,8 @@
         formatted-token-price    (prettify-balance currency-symbol price)
         percentage-change        (prettify-percentage-change change-pct-24h)
         crypto-value             (get-standard-crypto-format token balance prices-per-token)
-        fiat-value               (asset-fiat-formatted-for-ui currency-symbol
-                                                              fiat-unformatted-value)]
+        fiat-value               (fiat-formatted-for-ui currency-symbol
+                                                        fiat-unformatted-value)]
     {:token               (:symbol token)
      :token-name          (:name token)
      :state               :default
