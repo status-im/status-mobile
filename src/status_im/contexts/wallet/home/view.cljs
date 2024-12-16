f7b1571f2e55933e64147f51f4d29f8abdd4c98b
andrey <motor4ik@gmail.com>
2024-12-16 17:17:42 +0100

    :wallet.new-account

--- src/status_im/contexts/wallet/home/view.cljs
+++ src/status_im/contexts/wallet/home/view.cljs
@@ -23,7 +23,8 @@
         :sub-label           (i18n/label :t/add-account-description)
         :on-press            (fn []
                                (if keycard?
-                                 (rf/dispatch [:keycard/feature-unavailable-show {:feature-name :chat.new-chat}])
+                                 (rf/dispatch [:keycard/feature-unavailable-show
+                                               {:feature-name :wallet.new-account}])
                                  (rf/dispatch [:navigate-to :screen/wallet.create-account])))}
        (when (ff/enabled? ::ff/wallet.add-watched-address)
          {:icon                :i/reveal
