4bf79fafbb122e1815956577f5e34692636d602d
Sean Hagstrom <sean@seanhagstrom.com>
2025-01-08 14:16:14 -0800

    chore: format bottom-drawer component

--- src/status_im/contexts/chat/messenger/messages/contact_requests/bottom_drawer/view.cljs
+++ src/status_im/contexts/chat/messenger/messages/contact_requests/bottom_drawer/view.cljs
@@ -28,7 +28,7 @@
                                        constants/contact-request-state-received)
         contact-request-pending?    (= contact-request-state
                                        constants/contact-request-state-sent)
-        contact-request-dismissed?   (= contact-request-state
+        contact-request-dismissed?  (= contact-request-state
                                        constants/contact-request-state-dismissed)
         keycard?                    (rf/sub [:keycard/keycard-profile?])
         keycard-feature-unavailable (rn/use-callback
@@ -73,7 +73,7 @@
 
                        contact-request-pending?
                        (i18n/label :t/contact-request-chat-pending)
-                       
+
                        contact-request-dismissed?
                        (i18n/label :t/contact-request-chat-add {:name primary-name}))}]]))
-                       
+
