e198f4ac9f8a224667743d4e731c0d8f643e34fb
Sean Hagstrom <sean@seanhagstrom.com>
2025-01-10 10:37:07 -0800

    fix: unnest views for quo/tag component when using rn/pressable

--- src/quo/components/tags/base_tag.cljs
+++ src/quo/components/tags/base_tag.cljs
@@ -23,15 +23,14 @@
    children]
   [rn/pressable
    (merge {:disabled            disabled?
-           :accessibility-label accessibility-label}
+           :accessibility-label accessibility-label
+           :style               (style-container size
+                                                 disabled?
+                                                 border-color
+                                                 border-width
+                                                 background-color
+                                                 labelled?
+                                                 type)}
           (when on-press
             {:on-press #(on-press id)}))
-   [rn/view
-    {:style (merge (style-container size
-                                    disabled?
-                                    border-color
-                                    border-width
-                                    background-color
-                                    labelled?
-                                    type))}
-    children]])
+   children])
