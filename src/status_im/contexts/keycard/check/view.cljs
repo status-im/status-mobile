f25d50ff9b4cc55ffe31b3b2d710a1a452cd9ca7
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2025-01-29 14:40:11 +0530

    Add Documentation drawer on Check your Keycard screen

--- src/status_im/contexts/keycard/check/view.cljs
+++ src/status_im/contexts/keycard/check/view.cljs
@@ -3,16 +3,39 @@
             [react-native.core :as rn]
             [status-im.common.events-helper :as events-helper]
             [status-im.common.resources :as resources]
+            [status-im.constants :as constants]
             [utils.i18n :as i18n]
             [utils.re-frame :as rf]))
+
+(defn- keycard-documentation
+  []
+  [quo/documentation-drawers
+   {:title  (i18n/label :t/keycard)
+    :shell? true}
+   [rn/view
+    [quo/text {:size :paragraph-2}
+     (i18n/label :t/keycard-documentation)]
+    [quo/button
+     {:size                40
+      :type                :primary
+      :container-style     {:margin-top 24 :margin-bottom 12}
+      :background          :blur
+      :icon-right          :i/external
+      :on-press            #(rf/dispatch [:browser.ui/open-url constants/get-keycard-url])
+      :accessibility-label :get-keycard}
+     (i18n/label :t/buy-keycard)]]])
 
 (defn view
   []
   (let [{:keys [on-press]} (rf/sub [:get-screen-params])]
     [:<>
      [quo/page-nav
-      {:icon-name :i/arrow-left
-       :on-press  events-helper/navigate-back}]
+      {:icon-name  :i/arrow-left
+       :on-press   events-helper/navigate-back
+       :right-side [{:icon-name :i/info
+                     :on-press  #(rf/dispatch [:show-bottom-sheet
+                                               {:content keycard-documentation
+                                                :shell?  true}])}]}]
      [quo/page-top
       {:title            (i18n/label :t/check-keycard)
        :description      :text
