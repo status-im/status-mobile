37c25b93515862701a6146562e663d33073c8556
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2025-02-25 09:36:21 +0000

    wip

---
+++ src/status_im/ui/core.cljs
@@ -0,0 +1,11 @@
+(ns status-im.ui.core
+  (:require [re-frame.db :as rf.db]
+            [reagent.core :as reagent]
+            #_[utils.re-frame :as rf]
+            [utils.re-frame :as rf]))
+
+
+
+
+
+
