aa6c1130d5fdbcc51f93119b5b4d974fa088fa8a
Cristian Lungu <lungucristian95@gmail.com>
2025-02-28 12:12:07 +0200

    fix: moved slide-button reset from standard-auth to quo

--- src/quo/components/buttons/slide_button/constants.cljs
+++ src/quo/components/buttons/slide_button/constants.cljs
@@ -1,15 +1,15 @@
 (ns quo.components.buttons.slide-button.constants)
 
-(def track-padding 4)
-
-(def small-dimensions
+(def ^:constant small-dimensions
   {:track-height 40
    :thumb        32})
 
-(def large-dimensions
+(def ^:constant large-dimensions
   {:track-height 48
    :thumb        40})
 
-(def disable-opacity 0.3)
+(def ^:constant track-padding 4)
+(def ^:constant disable-opacity 0.3)
+(def ^:constant default-width 300)
 
-(def default-width 300)
+(def ^:constant reset-timeout-ms 500)
