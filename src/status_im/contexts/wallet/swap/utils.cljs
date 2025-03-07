f0c9539f49225ecff47ae3182388ed75ec2826b5
Lungu Cristian <lungucristian95@gmail.com>
2025-03-07 11:02:41 +0200

    fix: exchange rate was always 0

--- src/status_im/contexts/wallet/swap/utils.cljs
+++ src/status_im/contexts/wallet/swap/utils.cljs
@@ -67,9 +67,8 @@
 (defn updated-token-price
   "Returns the updated token price (number) from the routes."
   [token updated-prices]
-  (some->> token
-           :symbol
-           string/lower-case
-           keyword
-           (get updated-prices)
-           (or 0)))
+  (let [token-symbol (some-> token
+                             :symbol
+                             string/lower-case
+                             keyword)]
+    (get updated-prices token-symbol 0)))
--- src/status_im/subs/wallet/swap.cljs
+++ src/status_im/subs/wallet/swap.cljs
@@ -371,10 +371,10 @@
  :<- [:wallet/swap-updated-token-prices-usd]
  (fn [[pay-token-price receive-token-price asset-to-pay token-prices]]
    (when (and token-prices asset-to-pay)
-     (some->> asset-to-pay
-              :decimals
-              (utils/token-exchange-rate receive-token-price pay-token-price)
-              (or 0)))))
+     (let [exchange-rate (some->> asset-to-pay
+                                  :decimals
+                                  (utils/token-exchange-rate receive-token-price pay-token-price))]
+       (or exchange-rate 0)))))
 
 (rf/reg-sub
  :wallet/swap-exchange-rate-fiat
