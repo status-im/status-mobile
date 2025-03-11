c55be2c4e6da52b04f0ea286e75c63ac8dc4de3b
frank <lovefree103@gmail.com>
2025-03-11 16:35:30 +0800

    feat(log)_: Improve log management across platforms
    - Add native methods for setting log level and enabling/disabling logs to apply log setting without logout
    - Update log management in ClojureScript to use new native module methods
    - Remove restart confirmation translations for log level changes
    - Refactor log-related code to simplify log configuration

--- src/legacy/status_im/log_level/core.cljs
+++ src/legacy/status_im/log_level/core.cljs
@@ -3,40 +3,28 @@
     [legacy.status-im.multiaccounts.update.core :as multiaccounts.update]
     [re-frame.core :as re-frame]
     [taoensso.timbre :as log]
+    [native-module.core :as native-module]
     [utils.i18n :as i18n]
     [utils.re-frame :as rf]))
 
 (rf/defn save-log-level
   {:events [:log-level.ui/change-log-level-confirmed]}
-  [{:keys [db]} log-level]
+  [{:keys [db] :as cofx} log-level]
   (let [old-log-level (get-in db [:profile/profile :log-level])]
     (when (not= old-log-level log-level)
       (let [need-set-log-enabled? (or (empty? old-log-level) (empty? log-level))
-            log-enabled?          (boolean (seq log-level))
-            rpc-calls             (cond-> []
-                                    log-enabled?
-                                    (conj {:method   "wakuext_setLogLevel"
-                                           :params   [{:logLevel log-level}]
-                                           :on-error #(log/error "Failed to set log level" %)})
+            log-enabled?          (boolean (seq log-level))]
+        (when log-enabled?
+          (native-module/set-log-level log-level))
+        
+        (when need-set-log-enabled?
+          (native-module/set-log-enabled log-enabled?))
 
-                                    need-set-log-enabled?
-                                    (conj {:method   "wakuext_setLogEnabled"
-                                           :params   [log-enabled?]
-                                           :on-error #(log/error "Failed to set log enabled" %)}))]
-        {:fx [[:json-rpc/call
-               (conj (vec (map #(assoc % :on-success nil) (butlast rpc-calls)))
-                     (assoc (last rpc-calls)
-                            :on-success
-                            #(rf/dispatch [:log-level/update-multiaccount log-level])))]]}))))
-
-(rf/defn update-multiaccount
-  {:events [:log-level/update-multiaccount]}
-  [cofx log-level]
-  (multiaccounts.update/multiaccount-update
-   cofx
-   :log-level
-   log-level
-   {:on-success #(rf/dispatch [:profile/logout])}))
+        (multiaccounts.update/multiaccount-update
+         cofx
+         :log-level
+         log-level
+         {:on-success #()})))))
 
 (rf/defn show-change-log-level-confirmation
   {:events [:log-level.ui/log-level-selected]}
