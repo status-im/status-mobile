5a298bbbee1025d20aac9cea03946a85ca27f372
Siddarth Kumar <siddarthkay@gmail.com>
2025-03-13 16:25:01 +0530

    perf: enable -0fast optimisation flag in status-go

--- nix/status-go/mobile/build.nix
+++ nix/status-go/mobile/build.nix
@@ -48,6 +48,9 @@ in buildGoPackage rec {
   # TODO: try removing when go is upgraded to 1.22
   GODEBUG = "netdns=cgo+2";
 
+  # https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html#index-Ofast
+  CGO_CFLAGS = "-Ofast";
+
   # Sentry for status-go
   SENTRY_CONTEXT_NAME = "status-mobile";
   SENTRY_CONTEXT_VERSION = version;
