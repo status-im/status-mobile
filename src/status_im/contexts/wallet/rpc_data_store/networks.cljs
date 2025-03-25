f3a9b4520664a60e463204e4fccdf11c41ad711e
Cristian Lungu <lungucristian95@gmail.com>
2025-03-25 13:31:48 +0200

    feat: added schema for network rpc data-store

--- src/status_im/contexts/wallet/rpc_data_store/networks.cljs
+++ src/status_im/contexts/wallet/rpc_data_store/networks.cljs
@@ -1,5 +1,6 @@
 (ns status-im.contexts.wallet.rpc-data-store.networks
-  (:require [status-im.contexts.wallet.networks.config :as networks.config]
+  (:require [schema.core :as schema]
+            [status-im.contexts.wallet.networks.config :as networks.config]
             [status-im.contexts.wallet.rpc-data-store.utils :as utils]))
 
 (defn- make-network
@@ -18,6 +19,38 @@
         :nativeCurrencyName     :native-currency-name
         :nativeCurrencyDecimals :native-currency-decimals})
       (merge (get networks.config/networks (:chainId network-data)))))
+
+(schema/=> make-network
+  [:=>
+   [:cat
+    [:map
+     [:isTest boolean?]
+     [:chainId int?]
+     [:relatedChainId int?]
+     [:layer [:enum 1 2]]
+     [:shortName string?]
+     [:chainColor string?]
+     [:chainName string?]
+     [:blockExplorerUrl string?]
+     [:nativeCurrencySymbol string?]
+     [:nativeCurrencyName string?]
+     [:nativeCurrencyDecimals int?]]]
+   [:map {:closed true}
+    [:testnet? boolean?]
+    [:chain-id int?]
+    [:related-chain-id int?]
+    [:layer [:enum 1 2]]
+    [:short-name string?]
+    [:chain-color string?]
+    [:full-name string?]
+    [:block-explorer-url string?]
+    [:native-currency-symbol string?]
+    [:native-currency-name string?]
+    [:native-currency-decimals int?]
+    [:network-name keyword?]
+    [:source :schema.common/image-source]
+    [:abbreviated-name string?]
+    [:block-explorer-name string?]]])
 
 (defn- sort-networks
   [networks]
