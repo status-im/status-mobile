7ea361724a2c29593d60da96a67a6ada7f5700ac
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2025-03-26 09:34:17 +0000

    rebase fix

--- src/status_im/contexts/wallet/db.cljs
+++ src/status_im/contexts/wallet/db.cljs
@@ -1,9 +1,4 @@
-(ns status-im.contexts.wallet.db
-  (:require [status-im.constants :as constants]))
-
-(def network-filter-defaults
-  {:selector-state    :default
-   :selected-networks (set constants/default-network-names)})
+(ns status-im.contexts.wallet.db)
 
 (def defaults
   {:ui {;; Note: we set it to nil by default to differentiate when the user logs
