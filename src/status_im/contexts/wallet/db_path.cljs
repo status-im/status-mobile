f58a7282d652f1d4f1f8127aaacc901884fbd245
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2025-04-03 11:19:37 +0100

    marked path defs as constants

--- src/status_im/contexts/wallet/db_path.cljs
+++ src/status_im/contexts/wallet/db_path.cljs
@@ -1,4 +1,4 @@
 (ns status-im.contexts.wallet.db-path)
 
-(def swap [:wallet :ui :swap])
-(def send [:wallet :ui :send])
+(def ^:const swap [:wallet :ui :swap])
+(def ^:const send [:wallet :ui :send])
