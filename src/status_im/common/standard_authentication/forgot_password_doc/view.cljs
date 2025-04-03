138af200a14082b26850109f43c881833f65e57a
Sean Hagstrom <sean@seanhagstrom.com>
2025-04-03 14:45:06 +0100

    chore: update the forgot-password modal verbiage

--- src/status_im/common/standard_authentication/forgot_password_doc/view.cljs
+++ src/status_im/common/standard_authentication/forgot_password_doc/view.cljs
@@ -15,32 +15,24 @@
 
     [rn/view {:style style/step-container}
      [quo/step {:in-blur-view? shell?} 1]
-     [rn/view {:style style/step-content}
-      [quo/text {:size :paragraph-2 :weight :semi-bold}
-       (i18n/label :t/forgot-your-password-info-remove-app)]
-      [quo/text {:size :paragraph-2} (i18n/label :t/forgot-your-password-info-remove-app-description)]]]
-
-    [rn/view {:style style/step-container}
-     [quo/step {:in-blur-view? shell?} 2]
      [rn/view
       {:style style/step-content}
       [quo/text {:size :paragraph-2 :weight :semi-bold}
-       (i18n/label :t/forgot-your-password-info-reinstall-app)]
+       (i18n/label :t/forgot-your-password-info-remove-profile)]
       [quo/text {:size :paragraph-2}
-       (i18n/label :t/forgot-your-password-info-reinstall-app-description)]]]
+       (i18n/label :t/forgot-your-password-info-remove-profile-description)]]]
+
+    [rn/view {:style style/step-container}
+     [quo/step {:in-blur-view? shell?} 2]
+     [rn/view {:style style/step-content}
+      [rn/view {:style style/step-title}
+       [quo/text {:size :paragraph-2 :weight :semi-bold}
+        (i18n/label :t/forgot-your-password-info-recover-profile)]]
+      [quo/text {:size :paragraph-2}
+       (i18n/label :t/forgot-your-password-info-recover-profile-description)]]]
 
     [rn/view {:style style/step-container}
      [quo/step {:in-blur-view? shell?} 3]
-     [rn/view {:style style/step-content}
-      [rn/view {:style style/step-title}
-       [quo/text {:size :paragraph-2} (str (i18n/label :t/sign-up) " ")]
-       [quo/text {:size :paragraph-2 :weight :semi-bold}
-        (i18n/label :t/forgot-your-password-info-signup-with-key)]]
-      [quo/text {:size :paragraph-2}
-       (i18n/label :t/forgot-your-password-info-signup-with-key-description)]]]
-
-    [rn/view {:style style/step-container}
-     [quo/step {:in-blur-view? shell?} 4]
      [rn/view {:style style/step-content}
       [quo/text {:size :paragraph-2 :weight :semi-bold}
        (i18n/label :t/forgot-your-password-info-create-new-password)]
