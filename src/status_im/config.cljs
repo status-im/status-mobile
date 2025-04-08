8f3de01497292b94ecf6bbe2eead6aeaabca4d42
frank <lovefree103@gmail.com>
2025-04-08 21:02:04 +0800

    fix_: lint issue

--- src/status_im/config.cljs
+++ src/status_im/config.cljs
@@ -133,8 +133,8 @@
 (def sentry-enabled? (enabled? (get-config :SENTRY_ENABLED "0")))
 (def sentry-dsn-status-go SENTRY_DSN_STATUS_GO)
 
-;; this flag should only be used for e2e builds, because 
-;; relying on UI to pull logs, share them somewhere, and 
-;; download via Appium doesn't seem like a solid long-term 
+;; this flag should only be used for e2e builds, because
+;; relying on UI to pull logs, share them somewhere, and
+;; download via Appium doesn't seem like a solid long-term
 ;; approach for QA team
 (def use-public-log-dir? (enabled? (get-config :USE_PUBLIC_LOG_DIR "0")))
