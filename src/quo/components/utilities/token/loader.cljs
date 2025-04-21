0a4f47b5d025b7a73c43ebb54a87d182566b18c0
Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>
2025-04-21 23:59:13 +0530

    fix(wallet)_: Issue 6
    Signed-off-by: Mohamed Javid <19339952+smohamedjavid@users.noreply.github.com>

--- src/quo/components/utilities/token/loader.cljs
+++ src/quo/components/utilities/token/loader.cljs
@@ -5,11 +5,18 @@
 (def ^:private tokens (loader/resolve-tokens))
 (def ^:private safe-lower-case (comp string/lower-case str))
 
+;; NOTE: temporarily workaround added to show USDC and USDT on different chains
+;; with different decimals. This should be removed when the we move to CoinGecko API
+(def ^:private dt-tokens #{"usdc(6)" "usdc(18)" "usdt(6)" "usdt(18)"})
+
 (defn- get-token-image*
   [token]
-  (let [token-symbol (cond-> token
-                       (keyword? token) name
-                       :always          safe-lower-case)]
+  (let [lower-case-token-symbol (cond-> token
+                                  (keyword? token) name
+                                  :always          safe-lower-case)
+        token-symbol            (if (dt-tokens lower-case-token-symbol)
+                                  (string/replace lower-case-token-symbol #"\(\d+\)" "")
+                                  lower-case-token-symbol)]
     (get tokens token-symbol)))
 
 (def get-token-image (memoize get-token-image*))
